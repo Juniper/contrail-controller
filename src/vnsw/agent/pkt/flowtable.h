3ddcd921098ed477467dbad3e20e3bbbfe958bc8
Prabhjot Singh Sethi <prabhjot@contrailsystems.com>
2013-12-23 22:05:48 -0800

    Change to avoid initialization and copying of MatchPolicy object.

--- src/vnsw/agent/pkt/flowtable.h
+++ src/vnsw/agent/pkt/flowtable.h
@@ -356,8 +356,8 @@ class FlowEntry {
     uint64_t teardown_time;
     uint64_t last_modified_time; //used for aging
 
-    bool ActionRecompute(MatchPolicy *policy);
-    void CompareAndModify(const MatchPolicy &m_policy, bool create);
+    bool ActionRecompute();
+    void CompareAndModify(bool create);
     void UpdateKSync(FlowTableKSyncEntry *entry, bool create);
     int GetRefCount() { return refcount_; }
     bool ShortFlow() const {return short_flow;};
@@ -373,13 +373,15 @@ class FlowEntry {
         dport = key.dst_port;
     }
     void SetEgressUuid();
-    void GetPolicyInfo(MatchPolicy *policy);
+    void GetPolicyInfo();
+    void GetPolicyInfo(const VnEntry *vn);
 
-    void GetPolicy(const VnEntry *vn, MatchPolicy *policy);
-    void GetSgList(const Interface *intf, MatchPolicy *policy);
-    bool DoPolicy(const PacketHeader &hdr, MatchPolicy *policy, bool ingress);
-    uint32_t MatchAcl(const PacketHeader &hdr, MatchPolicy *policy,
+    void GetPolicy(const VnEntry *vn);
+    void GetSgList(const Interface *intf);
+    bool DoPolicy(const PacketHeader &hdr, bool ingress);
+    uint32_t MatchAcl(const PacketHeader &hdr,
                       std::list<MatchAclParams> &acl, bool add_implicit_deny);
+    void ResetPolicy();
     void set_deleted(bool deleted) { deleted_ = deleted; }
     bool deleted() { return deleted_; }
 private:
@@ -528,7 +530,7 @@ private:
     void DecrVnFlowCounter(VnFlowInfo *vn_flow_info, const FlowEntry *fe);
     void ResyncVnFlows(const VnEntry *vn);
     void ResyncRouteFlows(RouteFlowKey &key, SecurityGroupList &sg_l);
-    void ResyncAFlow(FlowEntry *fe, MatchPolicy &policy, bool create);
+    void ResyncAFlow(FlowEntry *fe, bool create);
     void ResyncVmPortFlows(const VmInterface *intf);
     void ResyncRpfNH(const RouteFlowKey &key, const Inet4UnicastRouteEntry *rt);
     void DeleteRouteFlows(const RouteFlowKey &key);
