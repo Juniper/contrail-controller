d452d3254797c7a348fe0766b37d6f9180e8beec
Nischal Sheth <nsheth@contrailsystems.com>
2014-01-03 17:11:13 -0800

    Add some comments for RtGroup and RTargetGroupMgr.

--- src/bgp/routing-instance/rtarget_group.h
+++ src/bgp/routing-instance/rtarget_group.h
@@ -13,14 +13,42 @@
 #include "bgp/bgp_table.h"
 #include "bgp/rtarget/rtarget_address.h"
 
-// RouteTarget Group for a given RouteTarget
-// Contains two lists of tables 
-//       1. Tables that imports the route belonging to this RouteTarget
-//       2. Tables to which route needs to be exported
-
 class BgpRoute;
 class RTargetRoute;
 
+//
+// This class keeps track of state per RouteTarget.  It maintains three main
+// pieces of information.
+//
+// 1. The RtGroupMembers map and the RtGroupMemberList set are used to keep
+//    per address family lists of import and export BgpTables. The lists are
+//    updated from the RoutePathReplicator.
+//
+// 2. The RTargetDepRouteList and the RouteList are used to maintain a per
+//    partition list of dependent BgpRoutes i.e. routes with the RouteTarget
+//    as one of their route targets.  Each entry in the RTargetDepRouteList
+//    vector corresponds to a DB partition.
+//
+//    The list of dependent BgpRoutes is updated from DB task when handling
+//    notifications for BgpRoutes in various VPN tables e.g. bgp.l3vpn.0 and
+//    bgp.evpn.0.  Since we could be processing multiple notifications (each
+//    from a different partition) in parallel, we keep a separate RouteList
+//    per partition id. The RouteLists are accessed from the RTFilter task
+//    to trigger re-evaluation of export policy for dependent BgpRoutes based
+//    on receiving advertisements/withdrawals for RTargetRoutes.
+//
+// 3. The InterestedPeerList map and RTargetRouteList set are used to keep a
+//    list of peers interested in this RouteTarget.  Since a peer could send
+//    multiple RTargetRoutes for a given RouteTarget, we maintain a set of
+//    RTargetRoutes per peer. A peer is added to the InterestedPeerList map
+//    when the first RTargetRoute is added and removed from the map when the
+//    last RTargetRoute is removed.
+//
+// Note that this class does not take any references on dependent BgpRoutes
+// or RTargetRoutes.  It is the RTargetGroupManager's job to do that.  Note
+// that each dependent BgpRoute may have multiple RouteTargets, so it doesn't
+// make sense to take a reference to the dependent route for each RouteTarget.
+//
 class RtGroup {
 public:
     typedef std::set<BgpTable *> RtGroupMemberList;
@@ -63,6 +91,7 @@ private:
     tbb::spin_rw_mutex rw_mutex_;
     RTargetDepRouteList dep_;
     InterestedPeerList peer_list_;
+
     DISALLOW_COPY_AND_ASSIGN(RtGroup);
 };
 
