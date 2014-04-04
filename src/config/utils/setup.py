be94427507ad1b5ec1bd223fcee8fc76287048b1
Noel Burton-Krahn <noel@burton-krahn.com>
2014-04-03 22:35:29 -0700

    added setup.py to install src/config/utils scripts in contrail_utils Python package

---
+++ src/config/utils/setup.py
@@ -0,0 +1,15 @@
+#
+# Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
+#
+from setuptools import setup
+
+setup(
+    name='contrail_utils',
+    version='0.1dev',
+    author='contrail',
+    author_email='contrail-sw@juniper.net',
+    url='http://opencontrail.org/',
+    packages=['contrail_utils',
+              ],
+    long_description="Contrail Utility scripts",
+)
