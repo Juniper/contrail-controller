8eda9043f316d493507dde67531a25ba03bc49d5
Sundaresan Rajangam <srajanga@juniper.net>
2014-04-14 11:35:15 -0700

    contrail-database-nodemgr changes

---
+++ src/analytics/database/setup.py
@@ -0,0 +1,19 @@
+#
+# Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
+#
+
+from setuptools import setup
+
+setup(
+    name='database',
+    version='0.1dev',
+    packages=['database',
+              'database.sandesh',
+              'database.sandesh.viz',
+              'database.sandesh.database_cpuinfo',
+              'database.sandesh.database_cpuinfo.cpuinfo'
+              ],
+    package_data={'': ['*.html', '*.css', '*.xml']},
+    zip_safe=False,
+    long_description="database package",
+)
