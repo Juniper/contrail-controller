14b587ff036488b06d05f6be04566ffbdeeb1382
Ashok Singh R <ashoksr@juniper.net>
2014-04-23 23:40:47 -0700

    1. Rename --conf_file  argument to --config_file 2. Use cout instead of LOG until the Logging Module is initialized 3. Continue parsing even when there is error while parsing VHost IP 4. Add default values in command line parser initialization list

--- src/vnsw/agent/init/test/test_agent_init.cc
+++ src/vnsw/agent/init/test/test_agent_init.cc
@@ -53,43 +53,48 @@ namespace opt = boost::program_options;
 class FlowTest : public ::testing::Test {
 public:
     virtual void SetUp() {
-        desc.add_options()
+        uint16_t http_server_port = ContrailPorts::HttpPortAgent;
 
+        desc.add_options()
         ("help", "help message")
-        ("conf_file", opt::value<string>()->default_value(Agent::DefaultConfigFile()), 
+        ("config_file", 
+         opt::value<string>()->default_value(Agent::DefaultConfigFile()), 
          "Configuration file")
-        ("disable-vhost", "Create vhost interface")
-        ("disable-ksync", "Disable kernel synchronization")
-        ("disable-services", "Disable services")
-        ("disable-packet", "Disable packet services")
+        ("version", "Display version information")
         ("COLLECTOR.server", opt::value<string>(), 
          "IP address of sandesh collector")
         ("COLLECTOR.port", opt::value<uint16_t>(), "Port of sandesh collector")
-        ("CONTROL-NODE.server", opt::value<std::vector<std::string> >()->multitoken(),
-         "IP addresses of control nodes. Max of 2 Ip addresses can be configured")
-        ("DEFAULT.flow_cache_timeout", opt::value<uint16_t>(), 
+        ("CONTROL-NODE.server", 
+         opt::value<std::vector<std::string> >()->multitoken(),
+         "IP addresses of control nodes."
+         " Max of 2 Ip addresses can be configured")
+        ("DEFAULT.debug", "Enable debug logging")
+        ("DEFAULT.flow_cache_timeout", 
+         opt::value<uint16_t>()->default_value(Agent::kDefaultFlowCacheTimeout),
          "Flow aging time in seconds")
         ("DEFAULT.hostname", opt::value<string>(), 
          "Hostname of compute-node")
-        ("DEFAULT.http_server_port", opt::value<uint16_t>(), 
+        ("DEFAULT.http_server_port", 
+         opt::value<uint16_t>()->default_value(http_server_port), 
          "Sandesh HTTP listener port")
-        ("DEFAULT.log_category", opt::value<string>(),
+        ("DEFAULT.log_category", opt::value<string>()->default_value("*"),
          "Category filter for local logging of sandesh messages")
-        ("DEFAULT.log_file", opt::value<string>(),
+        ("DEFAULT.log_file", 
+         opt::value<string>()->default_value(Agent::DefaultLogFile()),
          "Filename for the logs to be written to")
-        ("DEFAULT.log_level", opt::value<string>(),
+        ("DEFAULT.log_level", opt::value<string>()->default_value("SYS_DEBUG"),
          "Severity level for local logging of sandesh messages")
         ("DEFAULT.log_local", "Enable local logging of sandesh messages")
-        ("DEFAULT.tunnel_type", opt::value<string>(),
+        ("DEFAULT.tunnel_type", opt::value<string>()->default_value("MPLSoGRE"),
          "Tunnel Encapsulation type <MPLSoGRE|MPLSoUDP|VXLAN>")
         ("DISCOVERY.server", opt::value<string>(), 
          "IP address of discovery server")
         ("DISCOVERY.max_control_nodes", opt::value<uint16_t>(), 
-         "Maximum number of control node info to be provided by discovery service <1|2>")
+         "Maximum number of control node info to be provided by discovery "
+         "service <1|2>")
         ("DNS.server", opt::value<std::vector<std::string> >()->multitoken(),
          "IP addresses of dns nodes. Max of 2 Ip addresses can be configured")
-        ("host-name", opt::value<string>(), "Specific Host Name")
-        ("HYPERVISOR.type", opt::value<string>(), 
+        ("HYPERVISOR.type", opt::value<string>()->default_value("kvm"), 
          "Type of hypervisor <kvm|xen|vmware>")
         ("HYPERVISOR.xen_ll_interface", opt::value<string>(), 
          "Port name on host for link-local network")
@@ -113,7 +118,6 @@ public:
          "Gateway IP address for virtual host")
         ("VIRTUAL-HOST-INTERFACE.physical_interface", opt::value<string>(), 
          "Physical interface name to which virtual host interface maps to")
-        ("version", "Display version information")
             ;
     }
 
@@ -155,6 +159,7 @@ TEST_F(FlowTest, Agent_Conf_file_1) {
     EXPECT_STREQ(param.metadata_shared_secret().c_str(), "contrail");
     EXPECT_EQ(param.linklocal_system_flows(), 1024);
     EXPECT_EQ(param.linklocal_vm_flows(), 512);
+    EXPECT_EQ(param.flow_cache_timeout(), 30);
     EXPECT_STREQ(param.config_file().c_str(), 
                  "controller/src/vnsw/agent/init/test/cfg.ini");
     EXPECT_STREQ(param.program_name().c_str(), "test-param");
@@ -225,7 +230,7 @@ TEST_F(FlowTest, Agent_Param_1) {
     int argc = 16;
     char *argv[] = {
         (char *) "",
-        (char *) "--conf-file", 
+        (char *) "--config-file", 
                         (char *)"controller/src/vnsw/agent/init/test/cfg.ini",
         (char *) "--DEFAULT.log_local",
         (char *) "--DEFAULT.log_level",     (char *)"SYS_DEBUG",
@@ -263,7 +268,7 @@ TEST_F(FlowTest, Agent_Param_1) {
 TEST_F(FlowTest, Agen_Arg_Override_Config_1) {
     int argc = 8;
     char *argv[] = {
-        (char *) "--conf_file",
+        (char *) "--config_file",
                         (char *)"controller/src/vnsw/agent/init/test/cfg.ini",
         (char *) "--HYPERVISOR.type",    (char *)"xen", 
         (char *) "--HYPERVISOR.xen_ll_interface",   (char *)"xenport",
@@ -295,7 +300,7 @@ TEST_F(FlowTest, Agen_Arg_Override_Config_1) {
 }
 
 TEST_F(FlowTest, Agen_Arg_Override_Config_2) {
-    int argc = 8;
+    int argc = 6;
     char *argv[] = {
         (char *) "--DNS.server",    (char *)"20.1.1.1 21.1.1.1", 
         (char *) "--CONTROL-NODE.server",   (char *)"22.1.1.1 23.1.1.1",
@@ -322,6 +327,83 @@ TEST_F(FlowTest, Agen_Arg_Override_Config_2) {
               Ip4Address::from_string("20.1.1.1").to_ulong());
     EXPECT_EQ(param.dns_server_2().to_ulong(),
               Ip4Address::from_string("21.1.1.1").to_ulong());
+}
+
+/* Some command line args have default values. If user has not passed these
+ * command line args, but has specified values in config file, then values
+ * specified config file should be taken */
+TEST_F(FlowTest, Default_Cmdline_arg1) {
+    int argc = 2;
+    char *argv[] = {
+        (char *) "--config_file",
+                        (char *)"controller/src/vnsw/agent/init/test/cfg-default1.ini",
+    };
+
+    try {
+        opt::store(opt::parse_command_line(argc, argv, desc), var_map);
+        opt::notify(var_map);
+    } catch (...) {
+        cout << "Invalid arguments. ";
+        cout << desc << endl;
+        exit(0);
+    }
+
+    AgentParam param(Agent::GetInstance());
+    param.Init("controller/src/vnsw/agent/init/test/cfg-default1.ini", "test-param",
+               var_map);
+    EXPECT_EQ(param.flow_cache_timeout(), 60);
+    EXPECT_EQ(param.http_server_port(), 10001);
+    EXPECT_STREQ(param.log_category().c_str(), "abc");
+    EXPECT_STREQ(param.log_file().c_str(), "/var/log/contrail/vrouter2.log");
+    EXPECT_STREQ(param.log_level().c_str(), "SYS_ERR");
+    EXPECT_TRUE(param.isXenMode());
+}
+
+/* Some command line args have default values. If user has not passed these
+ * command line args, and has NOT specified values in config file, then
+ * verify that default value from command line args is picked up */
+TEST_F(FlowTest, Default_Cmdline_arg2) {
+    uint16_t http_server_port = ContrailPorts::HttpPortAgent;
+    uint16_t flow_timeout = Agent::kDefaultFlowCacheTimeout;
+    AgentParam param(Agent::GetInstance());
+    param.Init("controller/src/vnsw/agent/init/test/cfg-default2.ini", "test-param",
+               var_map);
+    EXPECT_EQ(param.flow_cache_timeout(), flow_timeout);
+    EXPECT_EQ(param.http_server_port(), http_server_port);
+    EXPECT_STREQ(param.log_category().c_str(), "*");
+    EXPECT_STREQ(param.log_file().c_str(), Agent::DefaultLogFile().c_str());
+    EXPECT_STREQ(param.log_level().c_str(), "SYS_DEBUG");
+    EXPECT_TRUE(param.isKvmMode());
+}
+
+/* Some command line args have default values. If user has explicitly passed 
+ * values for these command line args and has also specified values in config 
+ * file, then values specified on command line should be taken */
+TEST_F(FlowTest, Default_Cmdline_arg3) {
+    int argc = 8;
+    char *argv[] = {
+        (char *) "--DEFAULT.flow_cache_timeout", (char *)"100",
+        (char *) "--DEFAULT.http_server_port", (char *)"20001",
+        (char *) "--DEFAULT.log_file", (char *)"3.log",
+        (char *) "--HYPERVISOR.type", (char *)"vmware",
+    };
+
+    try {
+        opt::store(opt::parse_command_line(argc, argv, desc), var_map);
+        opt::notify(var_map);
+    } catch (...) {
+        cout << "Invalid arguments. ";
+        cout << desc << endl;
+        exit(0);
+    }
+
+    AgentParam param(Agent::GetInstance());
+    param.Init("controller/src/vnsw/agent/init/test/cfg-default1.ini", "test-param",
+               var_map);
+    EXPECT_EQ(param.flow_cache_timeout(), 100);
+    EXPECT_EQ(param.http_server_port(), 20001);
+    EXPECT_STREQ(param.log_file().c_str(), "3.log");
+    EXPECT_TRUE(param.isVmwareMode());
 }
 
 int main(int argc, char **argv) {
