9fc8630792f2ffb77d343294d57cfefaf90a19c0
Pedro Marques <roque@juniper.net>
2014-05-06 15:35:39 -0700

    Restore https://github.com/Juniper/contrail-controller/pull/712

--- src/config/api-server/setup.py
+++ src/config/api-server/setup.py
@@ -15,12 +15,12 @@ setup(
     long_description="VNC Configuration API Server Implementation",
     install_requires=[
         'lxml>=2.3.2',
-        'gevent==0.13.6',
+        'gevent>=0.13.6',
         'geventhttpclient>=1.0a',
         'pycassa>=1.7.2',
         'netaddr>=0.7.5',
-        'bitarray==0.8.0',
-        'psutil==0.4.1',
+        'bitarray>=0.8.0',
+        'psutil>=0.4.1',
     ],
     entry_points = {
         'console_scripts' : [
