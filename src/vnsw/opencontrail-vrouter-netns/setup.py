79228a81068495748b7097524b79a5d239194115
Édouard Thuleau <edouard.thuleau@cloudwatt.com>
2014-06-15 16:48:50 +0200

    Add vrouter netns script manager
    Command line to set up a Linux network namespace between two virtual
    networks. And set up all netns configuration to source NAT traffic comes
    from left network to the right network. That script could evolves to
    manage LBaaS (HAproxy) too.
    TODO: if the IP address is not provide, it should start a DHCP client to
    obtain the IP configuration.
    
    Change-Id: Ibc169ed1cf569dce1eb094f765e45288c68e2c80
    Signed-off-by: Édouard Thuleau <edouard.thuleau@cloudwatt.com>

---
+++ src/vnsw/opencontrail-vrouter-netns/setup.py
@@ -0,0 +1,31 @@
+#
+# Copyright (c) 2014 Juniper Networks, Inc.
+#
+
+import setuptools
+
+
+def requirements(filename):
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    return lines
+
+setuptools.setup(
+    name='opencontrail-vrouter-netns',
+    version='0.1',
+    packages=setuptools.find_packages(),
+
+    # metadata
+    author="OpenContrail",
+    author_email="dev@lists.opencontrail.org",
+    license="Apache Software License",
+    url="http://www.opencontrail.org/",
+    long_description="Script to manage Linux network namespaces",
+    install_requires=requirements('requirements.txt'),
+
+    entry_points = {
+        'console_scripts': [
+            'opencontrail-vrouter-netns = opencontrail_vrouter_netns.vrouter_netns:main'
+        ],
+    },
+)
