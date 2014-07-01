155a4e3655475fdaefe29eaf6ada46c133282dac
Pedro Marques <roque@juniper.net>
2014-06-30 20:50:08 -0700

    Create framework for unittests
    Change-Id: I693ffceab06a3b0ce4601009b37a8ecdd09e7451

--- src/config/svc-monitor/setup.py
+++ src/config/svc-monitor/setup.py
@@ -2,22 +2,32 @@
 # Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
 #
 
-from setuptools import setup
+import setuptools
 
-setup(
-    name='svc_monitor',
+def requirements(filename):
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    return lines
+
+setuptools.setup(
+    name='svc-monitor',
     version='0.1dev',
-    packages=['svc_monitor',
-              'svc_monitor.sandesh',
-              'svc_monitor.sandesh.svc_mon_introspect',
-
-              ],
+    packages=setuptools.find_packages(),
     package_data={'': ['*.html', '*.css', '*.xml']},
-    zip_safe=False,
+
+    # metadata
+    author="OpenContrail",
+    author_email="dev@lists.opencontrail.org",
+    license="Apache Software License",
+    url="http://www.opencontrail.org/",
+
     long_description="VNC Service Monitor",
-    install_requires=[
-        'zope.interface',
-    ],
+
+    install_requires=requirements('requirements.txt'),
+    tests_require=requirements('test-requirements.txt'),
+
+    test_suite='svc_monitor.tests',
+
     entry_points = {
         'console_scripts' : [
             'contrail-svc-monitor = svc_monitor.svc_monitor:server_main',
