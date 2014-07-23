62832b809643b4c871b014aff8c2391f98b9ab0d
Édouard Thuleau <edouard.thuleau@cloudwatt.com>
2014-07-23 19:34:54 +0200

    [SNAT SVC monitor] Improve scheduling on VRouter
    Add a scheduler base class.
    Check if the VRouter is up and running before selected it for running a
    network namespace of the SI.
    Destroy and recreate the network namespace if the VRouter goes down.
    This patch request the vrouter uve on the analytics API, if it receive a
    HTTP 200 code, the VRouter is considered up and down otherwise.
    
    Cherry-pick from https://review.opencontrail.org/#/c/956/
    
    Conflicts:
    	src/config/svc-monitor/svc_monitor/tests/test_init.py
    
    Change-Id: Ie5c933ee3bd0a7cdac076289b4e9030db8833c82

---
+++ src/config/svc-monitor/svc_monitor/scheduler/__init__.py
@@ -0,0 +1,3 @@
+#
+# Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
+#
--- src/config/svc-monitor/svc_monitor/tests/test_init.py
+++ src/config/svc-monitor/svc_monitor/tests/test_init.py
@@ -19,6 +19,11 @@ class Arguments(object):
         self.log_file = '/var/log/contrail/svc_monitor.log'
         self.use_syslog = False
         self.syslog_facility = Sandesh._DEFAULT_SYSLOG_FACILITY
+        self.cluster_id = None
+        self.si_netns_scheduler_driver = \
+            'svc_monitor.scheduler.vrouter_scheduler.ChanceScheduler'
+        self.analytics_server_ip = '127.0.0.1'
+        self.analytics_server_port = '8081'
 
 class SvcMonitorInitTest(unittest.TestCase):
     def setUp(self):
