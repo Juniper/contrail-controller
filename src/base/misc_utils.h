b8883a7de516eb3a906bce0afd105dc0389251fa
ashoksingh <ashoksr@juniper.net>
2014-08-25 15:33:34 +0530

    Closes-Bug :# 1319008 --Check for presense of contrail-version script before using it. --Send rpm-version and build-id as 'unknown' if contrail-version script does not exist
    Change-Id: Idecfdb5a2eec6f6bf8c4dd4bcdb5b5c092b64195

--- src/base/misc_utils.h
+++ src/base/misc_utils.h
@@ -51,6 +51,7 @@ public:
     }
     static const std::map<BuildModule, std::string> BuildModuleNames;
     typedef std::multimap<std::time_t, std::string> FileMMap;
+    static const std::string ContrailVersionCmd;
     static const std::string CoreFileDir;
     static const int MaxCoreFiles;
 
