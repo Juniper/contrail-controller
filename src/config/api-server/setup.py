16bfa38e5d885d4e3ae444a17ea0195f808fe401
Hampapur Ajay <hajay@juniper.net>
2014-09-06 22:56:28 -0700

    Run config unit tests from scons
    Currently they have been added to flaky-tests and will be promoted
    once we have it run successfully consistently. The workflow is to have
     * scons controller/src/api-lib
     * scons controller/src/config/api-server
     * scons controller/src/config/schema-transformer
    run the unit tests. A future commit will try to have this run from
    within source using ./run_tests.sh
    Also add explicit list of tests in common as test_utils/test_common are helpers instead of tests
    
    Change-Id: I5b72c6a18e8be9bcdf6d68146b9c220a9196746e

--- src/config/api-server/setup.py
+++ src/config/api-server/setup.py
@@ -1,12 +1,23 @@
 #
 # Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
 #
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
+import os
+
+class RunTestsCommand(Command):
+    description = "Test command to run testr in virtualenv"
+    user_options = []
+    def initialize_options(self):
+        self.cwd = None
+    def finalize_options(self):
+        self.cwd = os.getcwd()
+    def run(self):
+        os.system('./run_tests.sh -V')
 
 setup(
     name='vnc_cfg_api_server',
     version='0.1dev',
-    packages=find_packages(),
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     package_data={'': ['*.html', '*.css', '*.xml']},
     zip_safe=False,
     long_description="VNC Configuration API Server Implementation",
@@ -24,5 +35,8 @@ setup(
         'console_scripts' : [
             'contrail-api = vnc_cfg_api_server.vnc_cfg_api_server:server_main',
         ],
+    },
+    cmdclass={
+       'run_tests': RunTestsCommand,
     },
 )
