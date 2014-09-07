a31c0debd8c7e15f447c633485260f27389df537
Hampapur Ajay <hajay@juniper.net>
2014-09-06 22:56:28 -0700

    Run config unit tests from scons
    Change-Id: Ie3364cdbe0336335e55bbe707556cc2d27ec26f0

--- src/config/api-server/setup.py
+++ src/config/api-server/setup.py
@@ -1,7 +1,18 @@
 #
 # Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
 #
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
+import os
+
+class RunTestsCommand(Command):
+    description = "Test command to run testr in virtualenv"
+    user_options = []
+    def initialize_options(self):
+        self.cwd = None
+    def finalize_options(self):
+        self.cwd = os.getcwd()
+    def run(self):
+        os.system('./run_tests.sh -V')
 
 setup(
     name='vnc_cfg_api_server',
@@ -24,5 +35,8 @@ setup(
         'console_scripts' : [
             'contrail-api = vnc_cfg_api_server.vnc_cfg_api_server:server_main',
         ],
+    },
+    cmdclass={
+       'run_tests': RunTestsCommand,
     },
 )
