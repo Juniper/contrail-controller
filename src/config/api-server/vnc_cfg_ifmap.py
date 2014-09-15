c5881d06e7e02e0202517d503445dc9e466f5d2a
anbu <anbu@enovance.com>
2014-09-15 12:56:38 +0000

    Fix to create instance ip from subnet-uuid. Also modified the port_create function in vnc_opentstack to use this fix
    Closes-bug: #1369841
    
    Change-Id: Id19efca8e0061a7564673b9e27ec3ba48172b00f

--- src/config/api-server/vnc_cfg_ifmap.py
+++ src/config/api-server/vnc_cfg_ifmap.py
@@ -24,6 +24,7 @@ import socket
 import errno
 import subprocess
 import netaddr
+from netaddr import IPNetwork
 from bitarray import bitarray
 
 from cfgm_common import ignore_exceptions
@@ -1529,7 +1530,36 @@ class VncDbClient(object):
             return True
 
         return False
-    # end
+    # end match_uuid
+
+    def _update_subnet_uuid(self, vn_dict):
+        vn_uuid = vn_dict['uuid']
+
+        def _get_subnet_key(subnet):
+            pfx = subnet['subnet']['ip_prefix']
+            pfx_len = subnet['subnet']['ip_prefix_len']
+
+            network = IPNetwork('%s/%s' % (pfx, pfx_len))
+            return '%s %s/%s' % (vn_uuid, str(network.ip), pfx_len)
+
+        ipam_refs = vn_dict['network_ipam_refs']
+        updated = False
+        for ipam in ipam_refs:
+            vnsn = ipam['attr']
+            subnets = vnsn['ipam_subnets']
+            for subnet in subnets:
+                if subnet.get('subnet_uuid'):
+                    continue
+
+                subnet_key = _get_subnet_key(subnet)
+                subnet_uuid = self.useragent_kv_retrieve(subnet_key)
+                subnet['subnet_uuid'] = subnet_uuid
+                if not updated:
+                    updated = True
+
+        if updated:
+            self._cassandra_db._cassandra_virtual_network_update(vn_uuid, vn_dict)
+    # end _update_subnet_uuid
 
     def _dbe_resync(self, obj_uuid, obj_cols):
         obj_type = None
@@ -1548,6 +1578,9 @@ class VncDbClient(object):
                                                    'logical_router',
                                                    router['uuid'])
 
+            if (obj_type == 'virtual_network' and
+                'network_ipam_refs' in obj_dict):
+                self._update_subnet_uuid(obj_dict)
         except Exception as e:
             self.config_object_error(
                 obj_uuid, None, obj_type, 'dbe_resync:cassandra_read', str(e))
