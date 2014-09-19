316020332c3f8e1c4bfe9e60a564ffa097293ec9
sbalineni <sbalineni@juniper.net>
2014-09-19 11:28:43 -0700

    added a new test case dst cidr based policy rule match, and added decorator functions for  all retry tests
    Change-Id: I1d7971fa5fcc45fc191dfc0ac1b016b944a6b811

--- src/config/schema-transformer/test/test_case.py
+++ src/config/schema-transformer/test/test_case.py
@@ -82,6 +82,79 @@ class STTestCase(test_common.TestCase):
         return np
     # end create_network_policy
 
+    def create_virtual_machine(self, name, vn, ipaddress):
+        vm_instance = VirtualMachine(name)
+        self._vnc_lib.virtual_machine_create(vm_instance)
+        fq_name = [name]
+        fq_name.append('0')
+        vmi = VirtualMachineInterface(parent_type = 'virtual-machine', fq_name = fq_name)
+        vmi.set_virtual_network(vn)
+        self._vnc_lib.virtual_machine_interface_create(vmi)
+        ip = InstanceIp(vm_instance.name + '.0')
+        ip.set_virtual_machine_interface(vmi)
+        ip.set_virtual_network(vn)
+        ip.set_instance_ip_address(ipaddress)
+        uuid = self._vnc_lib.instance_ip_create(ip)
+        return vm_instance
+
+    def vmi_clean(self, vm_instance):
+        fq_name = vm_instance.fq_name
+        fq_name.append('0')
+        try:
+            vmi = self._vnc_lib.virtual_machine_interface_read(fq_name = fq_name)
+        except NoIdError:
+            return
+
+        ips = vmi.get_instance_ip_back_refs()
+        for ref in ips:
+            self._vnc_lib.instance_ip_delete(id = ref['uuid'])
+
+        self._vnc_lib.virtual_machine_interface_delete(id = vmi.uuid)
+
+    def delete_virtual_machine(self, vm_instance):
+        self.vmi_clean(vm_instance)
+        self._vnc_lib.virtual_machine_delete(id = vm_instance.uuid)
+
+    def create_network_policy_with_multiple_rules(self, rules):
+        pentrys = []
+        for rule in rules:
+            src_addr = rule["src"]
+            if src_addr["type"] == "vn":
+                vn = src_addr["value"]
+                addr1 = AddressType(virtual_network=vn.get_fq_name_str())
+            else:
+                cidr = src_addr["value"].split('/')
+                pfx = cidr[0]
+                pfx_len = int(cidr[1])
+                addr1 = AddressType(subnet=SubnetType(pfx, pfx_len))
+
+            dst_addr = rule["dst"]
+            if dst_addr["type"] == "vn":
+                vn = dst_addr["value"]
+                addr2 = AddressType(virtual_network=vn.get_fq_name_str())
+            else:
+                cidr = dst_addr["value"].split('/')
+                pfx = cidr[0]
+                pfx_len = int(cidr[1])
+                addr2 = AddressType(subnet=SubnetType(pfx, pfx_len))
+            #src_port = rule["src-port"]
+            src_port = PortType(-1, 0)
+            #dst_port = rule["dst-port"]
+            dst_port = PortType(-1, 0)
+            action = rule["action"]
+            action_list = ActionListType(simple_action=action)
+            prule = PolicyRuleType(direction=rule["direction"], protocol=rule["protocol"],
+                               src_addresses=[addr1], dst_addresses=[addr2],
+                               src_ports=[src_port], dst_ports=[dst_port],
+                               action_list=action_list)
+            pentrys.append(prule)
+
+        pentry = PolicyEntriesType(pentrys)
+        np = NetworkPolicy(str(uuid.uuid4()), network_policy_entries=pentry)
+        self._vnc_lib.network_policy_create(np)
+        return np
+    # end create_network_policy_with_multiple_rules
+
     def delete_network_policy(self, policy, auto_policy=False):
         action_list = policy.network_policy_entries.policy_rule[0].action_list
         if action_list:
