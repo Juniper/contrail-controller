a59ce719c34a6e91eeb214dd384c41de981fd9aa
Tapan Karwa <tkarwa@juniper.net>
2014-10-02 15:02:19 -0700

    Graph-walker changes for bare-metal servers.
    Change-Id: I36f62da02a90f572accd8b3c12d8bc03699f8f43

--- src/ifmap/ifmap_graph_walker.cc
+++ src/ifmap/ifmap_graph_walker.cc
@@ -256,6 +256,8 @@ void IFMapGraphWalker::AddLinksToWhitelist() {
     traversal_white_list_->include_edge.insert(
         "source=virtual-router,target=provider-attachment");
     traversal_white_list_->include_edge.insert(
+        "source=virtual-router,target=physical-router");
+    traversal_white_list_->include_edge.insert(
         "source=virtual-machine,target=service-instance");
     traversal_white_list_->include_edge.insert(
         "source=virtual-machine,target=virtual-machine-interface");
@@ -282,13 +284,19 @@ void IFMapGraphWalker::AddLinksToWhitelist() {
     traversal_white_list_->include_edge.insert(
         "source=virtual-machine-interface,target=interface-route-table");       
     traversal_white_list_->include_edge.insert(
+        "source=virtual-machine-interface,target=logical-interface");
+    traversal_white_list_->include_edge.insert(
         "source=physical-router,target=physical-interface");
+    traversal_white_list_->include_edge.insert(
+        "source=physical-router,target=logical-interface");
+    traversal_white_list_->include_edge.insert(
+        "source=physical-interface,target=logical-interface");
     traversal_white_list_->include_edge.insert(
         "source=service-template,target=domain");
     traversal_white_list_->include_edge.insert(
         "source=virtual-network,target=floating-ip-pool");
     traversal_white_list_->include_edge.insert(
-        "source=virtual-network,target=logical-interface");
+        "source=virtual-network,target=physical-router");
     traversal_white_list_->include_edge.insert(
         "source=virtual-network,target=virtual-network-network-ipam");
     traversal_white_list_->include_edge.insert(
