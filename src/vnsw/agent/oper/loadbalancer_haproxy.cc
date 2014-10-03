f538103cbaf4dc8bf9e135545824eb7702ae77e1
Divakar <ddivakar@juniper.net>
2014-10-03 07:55:23 -0700

    Generating the listen config for Haproxy
    Change-Id: I7e12e12fd5eef103a2316525f460e4d32a6f7235

--- src/vnsw/agent/oper/loadbalancer_haproxy.cc
+++ src/vnsw/agent/oper/loadbalancer_haproxy.cc
@@ -77,6 +77,17 @@ void LoadbalancerHaproxy::GenerateDefaults(
     *out << endl;
 }
 
+void LoadbalancerHaproxy::GenerateListen(
+    ostream *out, const LoadbalancerProperties &props) const {
+
+    *out << "listen contrail-config-stats :5937" << endl;
+    *out << string(4, ' ') << "mode http" << endl;
+    *out << string(4, ' ') << "stats enable" << endl;
+    *out << string(4, ' ') << "stats uri /" << endl;
+    *out << string(4, ' ') << "stats auth haproxy:contrail123" << endl;
+    *out << endl;
+}
+
 /*
  * frontend vip_id
  *     bind address:port
@@ -174,6 +185,7 @@ void LoadbalancerHaproxy::GenerateConfig(
 
     GenerateGlobal(&fs, props);
     GenerateDefaults(&fs, props);
+    GenerateListen(&fs, props);
     GenerateFrontend(&fs, pool_id, props);
     GenerateBackend(&fs, pool_id, props);
     fs.close();
