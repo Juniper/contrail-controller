7994ae0645ffc5cad2c9a17a28aea6fe514a7fda
Naveen N <naveenn@juniper.net>
2014-10-05 00:39:26 -0700

    * Get the ecmp index for a ingress VM from ingress route,   instead of looking for route in egress vrf which may not have   the route. Closes-bug:#1361010
    Change-Id: Ibbaea9a4086ac42b48f1a39c5f524f7703dcd8e8

--- src/vnsw/agent/pkt/pkt_flow_info.cc
+++ src/vnsw/agent/pkt/pkt_flow_info.cc
@@ -306,34 +306,8 @@ static void SetInEcmpIndex(const PktInfo *pkt, PktFlowInfo *flow_info,
         //TODO::ECMP for v6
     }
     Agent *agent = static_cast<AgentRouteTable *>(in->rt_->get_table())->agent();
-    //Get route for source IP route, which would be used to forward
-    //reverse flow traffic
-    const InetUnicastRouteEntry *rt;
-    FlowTable *ftable = agent->pkt()->flow_table();
-    if (flow_info->nat_done) {
-        VrfEntry *nat_vrf = NULL;
-        if (flow_info->fip_snat) {
-            nat_vrf = agent->vrf_table()->FindVrfFromId(flow_info->nat_vrf);
-        } else {
-            nat_vrf = agent->vrf_table()->FindVrfFromId(flow_info->nat_dest_vrf);
-        }
-        if (nat_vrf == NULL) {
-            return;
-        }
-        rt = static_cast<InetUnicastRouteEntry *>
-            (ftable->GetUcRoute(nat_vrf, Ip4Address(flow_info->nat_ip_saddr.to_v4())));
-    } else {
-        if (out->vrf_ == NULL) {
-            return;
-        }
-        rt = static_cast<InetUnicastRouteEntry *>
-            (ftable->GetUcRoute(out->vrf_, Ip4Address(pkt->ip_saddr.to_v4())));
-    }
-
-    if (rt == NULL) {
-        return;
-    }
-
+    const InetUnicastRouteEntry *rt =
+        static_cast<const InetUnicastRouteEntry *>(in->rt_);
     NextHop *component_nh_ptr = NULL;
     uint32_t label;
     //Frame key for component NH
