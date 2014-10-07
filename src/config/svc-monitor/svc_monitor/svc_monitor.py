dbf4e3d04015f679a07931f0ea045b733a13820d
Rudra Rugge <rrugge@juniper.net>
2014-10-06 18:27:19 -0700

    Use interface type as part of name for interface route table in service instance
    Change-Id: I480a6f3da644de71801b44447d212393895db9fe
    Closes-Bug: #1378100

--- src/config/svc-monitor/svc_monitor/svc_monitor.py
+++ src/config/svc-monitor/svc_monitor/svc_monitor.py
@@ -395,7 +395,7 @@ class SvcMonitor(object):
             return
 
         #update static routes
-        self._update_static_routes(si_obj)
+        self.vm_manager.update_static_routes(si_obj)
 
     def _addmsg_project_virtual_network(self, idents):
         vn_fq_str = idents['virtual-network']
@@ -491,33 +491,6 @@ class SvcMonitor(object):
             # end for meta
         # end for result_type
     # end process_poll_result
-
-    def _update_static_routes(self, si_obj):
-        # get service instance interface list
-        si_props = si_obj.get_service_instance_properties()
-        si_if_list = si_props.get_interface_list()
-        if not si_if_list:
-            return
-
-        for idx in range(0, len(si_if_list)):
-            si_if = si_if_list[idx]
-            static_routes = si_if.get_static_routes()
-            if not static_routes:
-                static_routes = {'route':[]}
-
-            # update static routes
-            try:
-                domain_name, proj_name = si_obj.get_parent_fq_name()
-                rt_name = si_obj.uuid + ' ' + str(idx)
-                rt_fq_name = [domain_name, proj_name, rt_name]
-                rt_obj = self._vnc_lib.interface_route_table_read(
-                    fq_name=rt_fq_name)
-                rt_obj.set_interface_route_table_routes(static_routes)
-                self._vnc_lib.interface_route_table_update(rt_obj)
-            except NoIdError:
-                pass
-    # end _update_static_routes
-
 # end class svc_monitor
 
 
