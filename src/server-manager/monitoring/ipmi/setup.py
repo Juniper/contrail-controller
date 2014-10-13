32c5a1e3920d57b1d1e6a34b03fb718972012ec4
shreya12 <shreyak@juniper.net>
2014-10-12 20:13:26 -0700

    Partial-Bug: #1378454  -Feature: Send IPMI information of the cluster from server-manager            to Cassandra database  -Fix: Created ipmi.sandesh file to hold the structure to be sent.        Modified viz.sandesh and vns.sandesh to have the sandesh structure        elements.   Made changes for review-1   Made changes for review-2 Change-Id: I62b15ff36e535ad70cc209f43aae6200243a4b1e

---
+++ src/server-manager/monitoring/ipmi/setup.py
@@ -0,0 +1,14 @@
+#
+# Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
+#
+
+from setuptools import setup
+import setuptools
+
+setup(
+    name='contrail-sm-monitoring',
+    version='0.1dev',
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    long_description="SM Monitoring Statistics",
+)
