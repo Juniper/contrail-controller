cb99510d0b3a833147ebcb5c055f3f445b3a0842
Nipa Kumar <nipak@juniper.net>
2014-10-14 16:25:45 -0700

    Use flexmock Api Infrastructure for config tests. To run tests,    1) Setup the environment using ./run_tests.sh    2) source .venv/bin/activate    3) eg: python -m testtools.run tests.test_crud_basic.Test
       Tests that are updated to use new infra
       1) test_askip.py
       2) test_subnet_ip_count.py
       3) Added test_ip_alloc.py to test for ipv6 allocated addresses.
    
    Change-Id: I0fe854734d6acc0db455e80b46875a3bf7694bc0

---
+++ src/config/api-server/tests/test_ip_alloc.py
@@ -0,0 +1,135 @@
+#
+# Copyright (c) 2013,2014 Juniper Networks, Inc. All rights reserved.
+#
+import gevent
+import os
+import sys
+import socket
+import errno
+import uuid
+import logging
+import coverage
+
+import cgitb
+cgitb.enable(format='text')
+
+import testtools
+from testtools.matchers import Equals, MismatchError, Not, Contains
+from testtools import content, content_type, ExpectedException
+import unittest
+import re
+import json
+import copy
+import inspect
+import pycassa
+import kombu
+import requests
+
+from vnc_api.vnc_api import *
+from vnc_api.common import exceptions as vnc_exceptions
+import vnc_api.gen.vnc_api_test_gen
+from vnc_api.gen.resource_test import *
+import cfgm_common
+
+sys.path.append('../common/tests')
+from test_utils import *
+import test_common
+import test_case
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+class TestIpAlloc(test_case.ApiServerTestCase):
+
+    def test_ip_alloction(self):
+        print 'test ip allocation'
+
+        # Create Domain
+        domain = Domain('my-v4-v6-domain')
+        self._vnc_lib.domain_create(domain)
+        print 'Created domain '
+       
+        # Create Project
+        project = Project('my-v4-v6-proj', domain)
+        self._vnc_lib.project_create(project)
+        print 'Created Project'
+
+        # Create NetworkIpam
+        ipam = NetworkIpam('default-network-ipam', project, IpamType("dhcp"))
+        self._vnc_lib.network_ipam_create(ipam)
+        print 'Created network ipam'
+
+        ipam = self._vnc_lib.network_ipam_read(fq_name=['my-v4-v6-domain', 'my-v4-v6-proj',
+                                                        'default-network-ipam'])
+        print 'Read network ipam'
+
+        # Create subnets
+        ipam_sn_v4 = IpamSubnetType(subnet=SubnetType('11.1.1.0', 24))
+        ipam_sn_v6 = IpamSubnetType(subnet=SubnetType('fd14::', 120))
+
+        # Create VN
+        vn = VirtualNetwork('my-v4-v6-vn', project)
+        vn.add_network_ipam(ipam, VnSubnetsType([ipam_sn_v4, ipam_sn_v6]))
+        self._vnc_lib.virtual_network_create(vn)
+        print 'Created Virtual Network object', vn.uuid
+        net_obj = self._vnc_lib.virtual_network_read(id = vn.uuid)
+  
+        # Create v4 Ip object
+        ip_obj1 = InstanceIp(name=str(uuid.uuid4()), instance_ip_family='v4')
+        ip_obj1.uuid = ip_obj1.name
+        print 'Created Instance IP object 1 ', ip_obj1.uuid
+
+        # Create v6 Ip object
+        ip_obj2 = InstanceIp(name=str(uuid.uuid4()), instance_ip_family='v6') 
+        ip_obj2.uuid = ip_obj2.name
+        print 'Created Instance IP object 2', ip_obj2.uuid
+
+        # Create VM
+        vm_inst_obj1 = VirtualMachine(str(uuid.uuid4()))
+        vm_inst_obj1.uuid = vm_inst_obj1.name
+        self._vnc_lib.virtual_machine_create(vm_inst_obj1)
+
+        id_perms = IdPermsType(enable=True)
+        port_obj1 = VirtualMachineInterface(
+            str(uuid.uuid4()), vm_inst_obj1, id_perms=id_perms)
+        port_obj1.uuid = port_obj1.name
+        port_obj1.set_virtual_network(vn)
+        ip_obj1.set_virtual_machine_interface(port_obj1)
+        ip_obj1.set_virtual_network(net_obj)
+        ip_obj2.set_virtual_machine_interface(port_obj1)
+        ip_obj2.set_virtual_network(net_obj)
+        port_id1 = self._vnc_lib.virtual_machine_interface_create(port_obj1)
+        
+        'print Allocting an IP4 address for first VM'
+        ip_id1 = self._vnc_lib.instance_ip_create(ip_obj1) 
+        ip_obj1 = self._vnc_lib.instance_ip_read(id=ip_id1)
+        ip_addr1 = ip_obj1.get_instance_ip_address()
+        print 'got v4 IP Address for first instance', ip_addr1
+
+        'print Allocting an IP6 address for first VM'
+        ip_id2 = self._vnc_lib.instance_ip_create(ip_obj2) 
+        ip_obj2 = self._vnc_lib.instance_ip_read(id=ip_id2)
+        ip_addr2 = ip_obj2.get_instance_ip_address()
+        print 'got v6 IP Address for first instance', ip_addr2
+
+        #cleanup
+        print 'Cleaning up'
+        self._vnc_lib.instance_ip_delete(id=ip_id1)
+        self._vnc_lib.instance_ip_delete(id=ip_id2)
+        self._vnc_lib.virtual_machine_interface_delete(id=port_obj1.uuid)
+        self._vnc_lib.virtual_machine_delete(id=vm_inst_obj1.uuid)
+        self._vnc_lib.virtual_network_delete(id=vn.uuid)
+        self._vnc_lib.network_ipam_delete(id=ipam.uuid)
+        self._vnc_lib.project_delete(id=project.uuid)
+        self._vnc_lib.domain_delete(id=domain.uuid)
+    #end
+
+#end class TestIpAlloc
+
+if __name__ == '__main__':
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    logger.addHandler(ch)
+
+    unittest.main()        
