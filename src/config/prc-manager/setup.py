6d52bbba7ea321b31420785cd4c477d7bc6a873a
Sachin Bansal <sachin@contrailsystems.com>
2014-10-14 14:22:08 -0700

    Adding skeleton for a new daemon: prc-manager
    The name stands for 'physical router configuration manager'.
    It listens to rabbit-mq updates from API server. When messages are received, it reads corresponding information from cassandra. It manages configuration for physical routers (MX only for now) using netconf utility.
    
    Change-Id: I36ac5f8999f8f921f4ecec1bc87d4c7742ed88df

---
+++ src/config/prc-manager/setup.py
@@ -0,0 +1,22 @@
+#
+# Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+#
+from setuptools import setup
+
+setup(
+    name='prc_manager',
+    version='0.1dev',
+    packages=['prc_manager',
+              'prc_manager.sandesh',
+              'prc_manager.sandesh.prc_introspect',
+              ],
+    package_data={'': ['*.html', '*.css', '*.xml']},
+    zip_safe=False,
+    long_description="VNC Configuration Physical Router Configuration Manager",
+    entry_points = {
+         # Please update sandesh/common/vns.sandesh on process name change
+         'console_scripts' : [
+             'contrail-prc-manager = prc_manager.prc_manager:server_main',
+         ],
+    },
+)
