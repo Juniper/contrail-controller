627957a8cf499b765446bb7569eb60f9b5037b70
Sachin Bansal <sachin@contrailsystems.com>
2014-10-14 14:22:08 -0700

    Adding skeleton for a new daemon: prc-manager
    The name stands for 'physical router configuration manager'.
    It listens to rabbit-mq updates from API server. When messages are received, it reads corresponding information from cassandra. It manages configuration for physical routers (MX only for now) using netconf utility.
    
    Change-Id: I36ac5f8999f8f921f4ecec1bc87d4c7742ed88df

---
+++ src/config/prc-manager/prc_manager/prc_manager.py
@@ -0,0 +1,316 @@
+#
+# Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+#
+
+"""
+This file contains implementation of managing physical router configuration
+"""
+
+from cfgm_common.vnc_cassandra import VncCassandraClient
+from cfgm_common.vnc_kombu import VncKombuClient
+import cgitb
+
+from pysandesh.sandesh_base import *
+from pysandesh.sandesh_logger import *
+from pysandesh.gen_py.sandesh.ttypes import SandeshLevel
+from cfgm_common.uve.virtual_network.ttypes import *
+from sandesh_common.vns.ttypes import Module, NodeType
+from sandesh_common.vns.constants import ModuleNames, Module2NodeType, \
+    NodeTypeNames, INSTANCE_ID_DEFAULT
+import discoveryclient.client as client
+
+
+class PrcManager(object):
+    def __init__(self, args=None):
+        self._args = args
+
+        # Initialize discovery client
+        self._disc = None
+        if self._args.disc_server_ip and self._args.disc_server_port:
+            self._disc = client.DiscoveryClient(
+                self._args.disc_server_ip,
+                self._args.disc_server_port,
+                ModuleNames[Module.PRC_MANAGER])
+
+        self._sandesh = Sandesh()
+        module = Module.SCHEMA_TRANSFORMER
+        module_name = ModuleNames[module]
+        node_type = Module2NodeType[module]
+        node_type_name = NodeTypeNames[node_type]
+        instance_id = INSTANCE_ID_DEFAULT
+        hostname = socket.gethostname()
+        _sandesh.init_generator(
+            module_name, hostname, node_type_name, instance_id,
+            self._args.collectors, 'to_bgp_context',
+            int(args.http_server_port),
+            ['cfgm_common', 'prc_manager.sandesh'], self._disc)
+        _sandesh.set_logging_params(enable_local_log=args.log_local,
+                                    category=args.log_category,
+                                    level=args.log_level,
+                                    file=args.log_file,
+                                    enable_syslog=args.use_syslog,
+                                    syslog_facility=args.syslog_facility)
+        ConnectionState.init(
+            _sandesh, hostname, module_name, instance_id,
+            staticmethod(ConnectionState.get_process_state_cb),
+            NodeStatusUVE, NodeStatus)
+
+        rabbit_server = self._args.rabbit_server
+        rabbit_port = self._args.rabbit_port
+        rabbit_user = self._args.rabbit_user
+        rabbit_password = self._args.rabbit_password
+        rabbit_vhost = self._args.rabbit_vhost
+
+        q_name = 'prc_manager.%s' % (socket.gethostname())
+        self._vnc_kombu = VncKombuClient(rabbit_server, rabbit_port,
+                                         rabbit_user, rabbit_password,
+                                         rabbit_vhost, q_name,
+                                         self._vnc_subscribe_callback,
+                                         self.config_log)
+
+        cass_server_list = self._args.cassandra_server_list
+        reset_config = self._args.reset_config
+        self._cassandra = VncCassandraClient(cass_server_list, reset_config,
+                                             self._args.cluster_id, None,
+                                             self.config_log)
+        self._vnc_kombu.connect(delete_old_q=True)
+    # end __init__
+
+    def config_log(self, msg, level):
+        self._sandesh.logger().log(SandeshLogger.get_py_logger_level(level),
+                                   msg)
+
+    def _vnc_subscribe_callback(self, oper_info):
+        try:
+            msg = "Notification Message: %s" % (pformat(oper_info))
+            self.config_log(msg, level=SandeshLevel.SYS_DEBUG)
+
+            if oper_info['oper'] == 'CREATE':
+                self._dbe_create_notification(oper_info)
+            if oper_info['oper'] == 'UPDATE':
+                self._dbe_update_notification(oper_info)
+            elif oper_info['oper'] == 'DELETE':
+                self._dbe_delete_notification(oper_info)
+
+        except Exception:
+            string_buf = cStringIO.StringIO()
+            cgitb.Hook(file=string_buf, format="text").handle(sys.exc_info())
+            self.config_log(string_buf.getvalue(),
+                            level=SandeshLevel.SYS_ERR)
+    # end _vnc_subscribe_callback
+
+
+def parse_args(args_str):
+    '''
+    Eg. python prc_manager.py --ifmap_server_ip 192.168.1.17
+                         --ifmap_server_port 8443
+                         --ifmap_username test
+                         --ifmap_password test
+                         --cassandra_server_list 10.1.2.3:9160
+                         --api_server_ip 10.1.2.3
+                         --api_server_port 8082
+                         --zk_server_ip 10.1.2.3
+                         --zk_server_port 2181
+                         --collectors 127.0.0.1:8086
+                         --disc_server_ip 127.0.0.1
+                         --disc_server_port 5998
+                         --http_server_port 8090
+                         --log_local
+                         --log_level SYS_DEBUG
+                         --log_category test
+                         --log_file <stdout>
+                         --use_syslog
+                         --syslog_facility LOG_USER
+                         --cluster_id <testbed-name>
+                         [--reset_config]
+    '''
+
+    # Source any specified config/ini file
+    # Turn off help, so we      all options in response to -h
+    conf_parser = argparse.ArgumentParser(add_help=False)
+
+    conf_parser.add_argument("-c", "--conf_file", action='append',
+                             help="Specify config file", metavar="FILE")
+    args, remaining_argv = conf_parser.parse_known_args(args_str.split())
+
+    defaults = {
+        'ifmap_server_ip': '127.0.0.1',
+        'ifmap_server_port': '8443',
+        'ifmap_username': 'schema-transformer',
+        'ifmap_password': 'schema-transformer',
+        'cassandra_server_list': '127.0.0.1:9160',
+        'api_server_ip': '127.0.0.1',
+        'api_server_port': '8082',
+        'zk_server_ip': '127.0.0.1',
+        'zk_server_port': '2181',
+        'collectors': None,
+        'disc_server_ip': None,
+        'disc_server_port': None,
+        'http_server_port': '8087',
+        'log_local': False,
+        'log_level': SandeshLevel.SYS_DEBUG,
+        'log_category': '',
+        'log_file': Sandesh._DEFAULT_LOG_FILE,
+        'use_syslog': False,
+        'syslog_facility': Sandesh._DEFAULT_SYSLOG_FACILITY,
+        'cluster_id': '',
+    }
+    secopts = {
+        'use_certs': False,
+        'keyfile': '',
+        'certfile': '',
+        'ca_certs': '',
+        'ifmap_certauth_port': "8444",
+    }
+    ksopts = {
+        'admin_user': 'user1',
+        'admin_password': 'password1',
+        'admin_tenant_name': 'default-domain'
+    }
+
+    if args.conf_file:
+        config = ConfigParser.SafeConfigParser()
+        config.read(args.conf_file)
+        defaults.update(dict(config.items("DEFAULTS")))
+        if ('SECURITY' in config.sections() and
+                'use_certs' in config.options('SECURITY')):
+            if config.getboolean('SECURITY', 'use_certs'):
+                secopts.update(dict(config.items("SECURITY")))
+        if 'KEYSTONE' in config.sections():
+            ksopts.update(dict(config.items("KEYSTONE")))
+
+    # Override with CLI options
+    # Don't surpress add_help here so it will handle -h
+    parser = argparse.ArgumentParser(
+        # Inherit options from config_parser
+        parents=[conf_parser],
+        # print script description with -h/--help
+        description=__doc__,
+        # Don't mess with format of description
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    defaults.update(secopts)
+    defaults.update(ksopts)
+    parser.set_defaults(**defaults)
+
+    parser.add_argument(
+        "--ifmap_server_ip", help="IP address of ifmap server")
+    parser.add_argument("--ifmap_server_port", help="Port of ifmap server")
+
+    # TODO should be from certificate
+    parser.add_argument("--ifmap_username",
+                        help="Username known to ifmap server")
+    parser.add_argument("--ifmap_password",
+                        help="Password known to ifmap server")
+    parser.add_argument(
+        "--cassandra_server_list",
+        help="List of cassandra servers in IP Address:Port format",
+        nargs='+')
+    parser.add_argument(
+        "--reset_config", action="store_true",
+        help="Warning! Destroy previous configuration and start clean")
+    parser.add_argument("--api_server_ip",
+                        help="IP address of API server")
+    parser.add_argument("--api_server_port",
+                        help="Port of API server")
+    parser.add_argument("--zk_server_ip",
+                        help="IP address:port of zookeeper server")
+    parser.add_argument("--collectors",
+                        help="List of VNC collectors in ip:port format",
+                        nargs="+")
+    parser.add_argument("--disc_server_ip",
+                        help="IP address of the discovery server")
+    parser.add_argument("--disc_server_port",
+                        help="Port of the discovery server")
+    parser.add_argument("--http_server_port",
+                        help="Port of local HTTP server")
+    parser.add_argument("--log_local", action="store_true",
+                        help="Enable local logging of sandesh messages")
+    parser.add_argument(
+        "--log_level",
+        help="Severity level for local logging of sandesh messages")
+    parser.add_argument(
+        "--log_category",
+        help="Category filter for local logging of sandesh messages")
+    parser.add_argument("--log_file",
+                        help="Filename for the logs to be written to")
+    parser.add_argument("--use_syslog", action="store_true",
+                        help="Use syslog for logging")
+    parser.add_argument("--syslog_facility",
+                        help="Syslog facility to receive log lines")
+    parser.add_argument("--admin_user",
+                        help="Name of keystone admin user")
+    parser.add_argument("--admin_password",
+                        help="Password of keystone admin user")
+    parser.add_argument("--admin_tenant_name",
+                        help="Tenant name for keystone admin user")
+    parser.add_argument("--cluster_id",
+                        help="Used for database keyspace separation")
+    args = parser.parse_args(remaining_argv)
+    if type(args.cassandra_server_list) is str:
+        args.cassandra_server_list = args.cassandra_server_list.split()
+    if type(args.collectors) is str:
+        args.collectors = args.collectors.split()
+
+    return args
+# end parse_args
+
+
+def main(args_str=None):
+    global _zookeeper_client
+    if not args_str:
+        args_str = ' '.join(sys.argv[1:])
+    args = parse_args(args_str)
+    if args.cluster_id:
+        client_pfx = args.cluster_id + '-'
+        zk_path_pfx = args.cluster_id + '/'
+    else:
+        client_pfx = ''
+        zk_path_pfx = ''
+    _zookeeper_client = ZookeeperClient(client_pfx+"prc-manager",
+                                        args.zk_server_ip)
+    _zookeeper_client.master_election(zk_path_pfx+"/prc-manager",
+                                      os.getpid(), run_prc_manager,
+                                      args)
+# end main
+
+
+def run_prc_manager(args):
+    global _vnc_lib
+
+    def connection_state_update(status, message=None):
+        ConnectionState.update(
+            conn_type=ConnectionType.APISERVER, name='ApiServer',
+            status=status, message=message or '',
+            server_addrs=['%s:%s' % (args.api_server_ip,
+                                     args.api_server_port)])
+    # end connection_state_update
+
+    # Retry till API server is up
+    connected = False
+    connection_state_update(ConnectionStatus.INIT)
+    while not connected:
+        try:
+            _vnc_lib = VncApi(
+                args.admin_user, args.admin_password, args.admin_tenant_name,
+                args.api_server_ip, args.api_server_port)
+            connected = True
+            connection_state_update(ConnectionStatus.UP)
+        except requests.exceptions.ConnectionError as e:
+            # Update connection info
+            connection_state_update(ConnectionStatus.DOWN, str(e))
+            time.sleep(3)
+        except ResourceExhaustionError:  # haproxy throws 503
+            time.sleep(3)
+
+    prc_manager = PrcManager(args)
+# end run_schema_transformer
+
+
+def server_main():
+    cgitb.enable(format='text')
+    main()
+# end server_main
+
+if __name__ == '__main__':
+    server_main()
