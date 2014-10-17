f2fd11ca72ea673b19917f645c1ae9124d20e57b
anbu <anbu@enovance.com>
2014-10-17 06:03:01 +0000

    Handling no-security-group on VMIs
    When there are not security groups for a port (even the default
    one), contrail allows all sorts of packets to PASS, but
    Openstack expects that, on such occasion, any packet should be
    DROPPED.
    
    This is solved using a internal-only security group which is not
    visible to the client programs, and has empty rule-set. When a
    port wants no-security-group attached to it, the no-rule group
    is associated to it internally.
    
    Closes-bug: #1381901
    
    Change-Id: I5348e05682d97773760ffefa7d988468adf08c13

--- src/config/vnc_openstack/vnc_openstack/neutron_plugin_db.py
+++ src/config/vnc_openstack/vnc_openstack/neutron_plugin_db.py
@@ -21,6 +21,7 @@ from neutron.api.v2 import attributes as attr
 
 from cfgm_common import exceptions as vnc_exc
 from vnc_api.vnc_api import *
+from vnc_api.common import SG_NO_RULE_FQ_NAME, SG_NO_RULE_NAME
 
 _DEFAULT_HEADERS = {
     'Content-type': 'application/json; charset="UTF-8"', }
@@ -1622,6 +1623,31 @@ class DBInterface(object):
                 except RefsExistError:
                     pass
 
+    def _create_no_rule_sg(self):
+        domain_obj = Domain(SG_NO_RULE_FQ_NAME[0])
+        proj_obj = Project(SG_NO_RULE_FQ_NAME[1], domain_obj)
+        proj = self._vnc_lib.project_read(fq_name=SG_NO_RULE_FQ_NAME[:2])
+        sg_rules = PolicyEntriesType([])
+        id_perms = IdPermsType(enable=True,
+                               description="Security group with no rules",
+                               user_visible=False)
+        sg_obj = SecurityGroup(name=SG_NO_RULE_NAME,
+                               parent_obj=proj_obj,
+                               security_group_entries=sg_rules,
+                               id_perms=id_perms)
+        sg_uuid = self._vnc_lib.security_group_create(sg_obj)
+        return self._vnc_lib.security_group_read(id=sg_uuid)
+    # end _create_no_rule_sg
+
+    def _get_no_rule_security_group(self):
+        try:
+            sg_obj = self._vnc_lib.security_group_read(fq_name=SG_NO_RULE_FQ_NAME)
+        except NoIdError:
+            sg_obj = self._create_no_rule_sg()
+
+        return sg_obj
+    # end _get_no_rule_security_group
+
     def _port_neutron_to_vnc(self, port_q, net_obj, oper):
         if oper == CREATE:
             project_id = str(uuid.UUID(port_q['tenant_id']))
@@ -1663,6 +1689,12 @@ class DBInterface(object):
             for sg_id in port_q.get('security_groups') or []:
                 # TODO optimize to not read sg (only uuid/fqn needed)
                 sg_obj = self._vnc_lib.security_group_read(id=sg_id)
+                port_obj.add_security_group(sg_obj)
+
+            # When there is no-security-group for a port,the internal
+            # no_rule group should be used.
+            if not port_q['security_groups']:
+                sg_obj = self._get_no_rule_security_group()
                 port_obj.add_security_group(sg_obj)
 
         id_perms = port_obj.get_id_perms()
