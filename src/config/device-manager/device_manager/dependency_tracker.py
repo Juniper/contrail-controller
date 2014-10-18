c1aa25712784d35a55426fd830525c1756fd6a57
Sachin Bansal <sachin@contrailsystems.com>
2014-10-18 09:45:56 -0700

    Basic functionality of device manager
    Change-Id: Ied0de2226f9562b84a9ba6cd77bb930c896ffb20

---
+++ src/config/device-manager/device_manager/dependency_tracker.py
@@ -0,0 +1,83 @@
+#
+## Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+#
+
+"""
+This file contains implementation of dependency tracker
+for physical router configuration manager
+"""
+
+class DependencyTracker(object):
+    _REACTION_MAP = {
+        'physical_router': {
+            'self': ['bgp_router', 'physical_interface', 'logical_interface'],
+            'bgp_router': [],
+            'physical_interface': [],
+            'logical_interface': [],
+            'virtual_network': [],
+        },
+        'bgp_router': {
+            'self': ['bgp_router', 'physical_router'],
+            'physical_router': [],
+        },
+        'physical_interface': {
+            'self': ['physical_router', 'logical_interface'],
+            'physical_router': ['logical_interface'],
+            'logical_interface': ['physical_router'],
+        },
+        'logical_interface': {
+            'self': ['physical_router', 'physical_interface', 'virtual_machine_interface'],
+            'physical_interface': ['virtual_machine_interface'],
+            'virtual_machine_interface': ['physical_router', 'physical_interface'],
+            'physical_router': ['virtual_machine_interface']
+        },
+        'virtual_machine_interface': {
+            'self': ['logical_interface', 'virtual_network'],
+            'logical_interface': ['virtual_network'],
+            'virtual_network': ['logical_interface']
+        },
+        'virtual_network': {
+            'self': ['physical_router', 'virtual_machine_interface'],
+            'physical_router': [],
+            'virtual_machine_interface': []
+        }
+    }
+    
+    def __init__(self, object_class_map, reaction_map=None):
+        self._reaction_map = reaction_map or self._REACTION_MAP
+        self._object_class_map = object_class_map
+        self._resources = {}
+    # end __init__
+    
+    def _add_resource(self, obj_type, obj_uuid):
+        if obj_type in self._resources:
+            if obj_uuid in self._resources[obj_type]:
+                # already visited
+                return False
+            self._resources[obj_type].append(obj_uuid)
+        else:
+            self._resources[obj_type] = [obj_uuid]
+        return True
+    # end _add_resource
+
+    def evaluate(self, obj_type, obj, from_type='self'):
+        if obj_type not in self._reaction_map:
+            return
+        if not self._add_resource(obj_type, obj.uuid):
+            return
+        
+        for ref_type in self._reaction_map[obj_type][from_type]:
+            ref = getattr(type(obj), ref_type, None)
+            if ref is None:
+                refs = getattr(type(obj), ref_type+'s', [])
+            else:
+                refs = [ref]
+                    
+            ref_class = self._object_class_map[ref_type]
+            for ref in refs:
+                ref_obj = ref_class.get(ref)
+                if ref_obj is None:
+                    return
+                self.evaluate(ref_type, ref_obj, obj_type)
+    # end evaluate
+# end DependencyTracker
---
+++ src/config/device-manager/device_manager/physical_router_config.py
@@ -0,0 +1,148 @@
+#
+# Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+#
+
+"""
+This file contains implementation of inetconf interface for physical router configuration manager
+"""
+
+from lxml import etree
+from ncclient import manager
+import copy
+
+class PhysicalRouterConfig(object):
+    def __init__(self, management_ip, user_creds, logger=None):
+        self.management_ip = management_ip
+        self.user_creds = user_creds
+        self.bgp_config = None
+        self.bgp_params = None
+        self.bgp_peers = {}
+        self.routing_instances = {}
+        self._logger = logger
+    # end __init__
+    
+    def send_netconf(self, new_config, default_operation="merge"):
+        try:
+            with manager.connect(host=self.management_ip, port=22,
+                                 username=self.user_creds['username'],
+                                 password=self.user_creds['password'],
+                                 unknown_host_cb=lambda x, y: True) as m:
+                add_config = etree.Element(
+                    "config",
+                    nsmap={"xc": "urn:ietf:params:xml:ns:netconf:base:1.0"})
+                config = etree.SubElement(add_config, "configuration")
+                if isinstance(new_config, list):
+                    for nc in new_config:
+                        config.append(nc)
+                else:
+                    config.append(new_config)
+                m.edit_config(
+                    target='candidate', config=etree.tostring(add_config),
+                    test_option='test-then-set',
+                    default_operation=default_operation)
+                m.commit()
+        except Exception as e:
+            if self._logger:
+                self._logger.error("Router %s: %s" % (self.management_ip,
+                                                      e.message))
+    # end send_config
+    
+    def add_routing_instance(self, name, route_target, prefixes=[], interfaces=[]):
+        self.routing_instances[name] = {'route_target': route_target,
+                                        'prefixes': prefixes,
+                                        'interfaces': interfaces}
+        ri_config = etree.Element("routing-instances")
+        ri = etree.SubElement(ri_config, "instance", operation="replace")
+        etree.SubElement(
+            ri, "name").text = "__contrail__" + name.replace(':', '_')
+        etree.SubElement(ri, "instance-type").text = "vrf"
+        for interface in interfaces:
+            if_element = etree.SubElement(ri, "interface")
+            etree.SubElement(if_element, "name").text = interface
+        rt_element = etree.SubElement(ri, "vrf-target")
+        etree.SubElement(rt_element, "community").text = route_target
+        etree.SubElement(ri, "vrf-table-label")
+        if prefixes:
+            ri_opt = etree.SubElement(ri, "routing-options")
+            static_config = etree.SubElement(ri_opt, "static")
+            for prefix in prefixes:
+                route_config = etree.SubElement(static_config, "route")
+                etree.SubElement(route_config, "name").text = prefix
+                etree.SubElement(route_config, "discard")
+
+        self.send_netconf(ri_config)
+    # end add_routing_instance
+
+    def delete_routing_instance(self, name):
+        if name not in self.routing_instances:
+            return
+        ri_config = etree.Element("routing-instances")
+        ri = etree.SubElement(ri_config, "instance", operation="delete")
+        etree.SubElement(ri, "name").text = "__contrail__" + name
+        self.send_netconf(ri_config, default_operation="none")
+    # end delete_routing_instance
+
+    def set_bgp_config(self, params):
+        self.bgp_params = params
+        if params['vendor'] != "mx" or not params['vnc_managed']:
+            if self.bgp_config:
+                self.delete_bgp_config()
+            return
+
+        self.bgp_config = etree.Element("group", operation="replace")
+        etree.SubElement(self.bgp_config, "name").text = "__contrail__"
+        etree.SubElement(self.bgp_config, "type").text = "internal"
+        etree.SubElement(self.bgp_config, "local-address").text = params['address']
+        if (params['address_families']):
+            family_etree = etree.SubElement(self.bgp_config, "family")
+            for family in params['address_families']['family']:
+                family_subtree = etree.SubElement(family_etree, family)
+                if family != 'route-target':
+                    etree.SubElement(family_subtree, "unicast")
+        etree.SubElement(self.bgp_config, "keep").text = "all"
+        self.send_bgp_config()
+    # end set_bgp_config
+
+    def delete_bgp_config(self):
+        if not self.bgp_config:
+            return
+        for routing_instance in self.routing_instances:
+            self.delete_routing_instance(routing_instance)
+
+        self.bgp_config = None
+        proto_config = etree.Element("protocols")
+        bgp = etree.SubElement(proto_config, "bgp")
+        group = etree.SubElement(bgp, "group", operation="delete")
+        etree.SubElement(group, "name").text = "__contrail__"
+        self.send_netconf(proto_config, default_operation="none")
+    # end delete_config
+
+    def add_bgp_peer(self, router, params):
+        self.bgp_peers[router] = params
+        self.send_bgp_config()
+    # end add_peer
+
+    def delete_bgp_peer(self, router):
+        if router in self.bgp_peers:
+            del self.bgp_peers[router]
+            self.push_config()
+    # end delete_bgp_peer
+
+    def send_bgp_config(self):
+        if self.bgp_config is None:
+            return
+        group_config = copy.deepcopy(self.bgp_config)
+        proto_config = etree.Element("protocols")
+        bgp = etree.SubElement(proto_config, "bgp")
+        bgp.append(group_config)
+        for peer, params in self.bgp_peers.items():
+            nbr = etree.SubElement(group_config, "neighbor")
+            etree.SubElement(nbr, "name").text = peer
+        routing_options_config = etree.Element("routing-options")
+        etree.SubElement(
+            routing_options_config,
+            "route-distinguisher-id").text = self.bgp_params['identifier']
+        self.send_netconf([proto_config, routing_options_config])
+    # end send_bgp_config
+
+# end PhycalRouterConfig
