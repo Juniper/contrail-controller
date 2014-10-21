01a77ce7eb24a957887c2c96d1101b44bb317095
Sylvain Afchain <sylvain.afchain@enovance.com>
2014-10-21 12:23:25 +0200

    Limits fields that can be updated
    This patch limits the fields that can
    be updated during update operations.
    currenlty limits to enable and description
    field.
    
    Partial-Bugs: #1378376
    (cherry picked from commit 3c8b404089b4305b3224f0d8676ee4f194e128e3)
    Change-Id: I39883d9010397f0a82d99bbe3e09b4d8a1dc1d31

--- src/config/api-server/vnc_cfg_api_server.py
+++ src/config/api-server/vnc_cfg_api_server.py
@@ -1000,7 +1000,7 @@ class VncApiServer(VncApiServerGen):
         self._db_conn = db_conn
     # end _db_connect
 
-    def _ensure_id_perms_present(self, obj_type, obj_dict):
+    def _ensure_id_perms_present(self, obj_type, obj_dict, update=False):
         """
         Called at resource creation to ensure that id_perms is present in obj
         """
@@ -1035,9 +1035,15 @@ class VncApiServer(VncApiServerGen):
             except NoIdError:
                 pass
 
+        # not all fields can be updated
+        if update:
+            field_list = ['enable', 'description']
+        else:
+            field_list = ['enable', 'description', 'user_visible']
+
         # Start from default and update from obj_dict
         req_id_perms = obj_dict['id_perms']
-        for key in ('enable', 'description', 'user_visible'):
+        for key in field_list:
             if key in req_id_perms:
                 id_perms[key] = req_id_perms[key]
         # TODO handle perms present in req_id_perms
@@ -1234,7 +1240,7 @@ class VncApiServer(VncApiServerGen):
             obj_type = obj_type.replace('_', '-')
 
             # Ensure object has at least default permissions set
-            self._ensure_id_perms_present(obj_type, obj_dict)
+            self._ensure_id_perms_present(obj_type, obj_dict, update=True)
 
             apiConfig = VncApiCommon()
             apiConfig.object_type = obj_type.replace('-', '_')
