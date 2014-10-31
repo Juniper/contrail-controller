26aeac5d43cd8578281e5bc779969b734adecdd0
manishsingh <manishs@juniper.net>
2014-10-31 14:37:18 +0530

    Fix#1: L2 multicast route not getting deleted. When VRF delete is issued, agent unsubscribes from VRF and issues a walk to clear all externally(BGP peer) created paths to be deleted. For L2 multicast fabric tree path, the peer is MULTICAST_TREE_BUILDER. This was not getting deleted. So multicast route used to retain the same resulting in VRF not getting deleted. Previous to EVPN code it used to work as multicast route had only one path and on VRF delete it used to be flushed, however EVPN brings in path in multicast route for every composite NH created for replication.
    Fix#2: Parsing of ethernet tag and mac address in retract message
    Reference
    https://github.com/manishsing/contrail-controller/commit/5d097d9000de0c91bdbad67c8bb52db061e3f0a8
    
    Change-Id: I97583ebefdcf647276b13fdd70893709ccba7fc9
    Closes-Bug: 1386924

--- src/vnsw/agent/test/test_route.cc
+++ src/vnsw/agent/test/test_route.cc
@@ -1516,7 +1516,7 @@ TEST_F(RouteTest, PathPreference_1) {
     DeleteVmportEnv(input, 1, true);
     client->WaitForIdle();
     //Make sure vrf and all routes are deleted
-    EXPECT_TRUE(VrfFind("vrf1", true) == NULL);
+    EXPECT_TRUE(VrfFind("vrf1", true) == false);
 }
 
 int main(int argc, char *argv[]) {
