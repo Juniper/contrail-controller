0fe5be1a4e7163c12795fe28f8d1dd098efc3604
Deepinder Setia <dsetia@juniper.net>
2014-11-17 17:20:16 -0800

    Fix for 1391667: Retry cassandra ops 5 times (also the default value)
    Change-Id: Iea8e464ebdc78b01356b817af5379ad4f2deed8f

--- src/discovery/disc_cassdb.py
+++ src/discovery/disc_cassdb.py
@@ -44,7 +44,7 @@ class DiscoveryCassendraClient():
                                       server_list, max_overflow=10,
                                       use_threadlocal=True, prefill=True,
                                       pool_size=10, pool_timeout=20,
-                                      max_retries=-1, timeout=0.5)
+                                      max_retries=5, timeout=0.5)
         rd_consistency = pycassa.cassandra.ttypes.ConsistencyLevel.QUORUM
         wr_consistency = pycassa.cassandra.ttypes.ConsistencyLevel.QUORUM
         self._disco_cf = pycassa.ColumnFamily(pool, self._disco_cf_name,
