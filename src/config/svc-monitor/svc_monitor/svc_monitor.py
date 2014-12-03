061f6f3ca9bb4d7e9afbe2994273ac54ce04c84a
Rudra Rugge <rrugge@juniper.net>
2014-12-03 13:33:28 -0800

    Upgrade service monitor database for 2.0
    Add interface UUID also to the table on startup for
    service VMs that are running. VMI UUID need to be stored
    because in the new scheme VMI names are not fixed for
    a particular service VM.
    
    Change-Id: Ie06fb4644e6635dae28f87cd9f24dbb7c4e4761a
    Closes-Bug: #1398968

--- src/config/svc-monitor/svc_monitor/svc_monitor.py
+++ src/config/svc-monitor/svc_monitor/svc_monitor.py
@@ -145,6 +145,40 @@ class SvcMonitor(object):
             self._vnc_lib, self.db, self.logger,
             self.vrouter_scheduler, self._nova_client, self._args)
 
+        # resync db
+        self.db_resync()
+
+    def db_resync(self):
+        si_list = self.db.service_instance_list()
+        for si_fq_str, si in si_list or []:
+            for idx in range(0, int(si.get('max-instances', '0'))):
+                prefix = self.db.get_vm_db_prefix(idx)
+                vm_key = prefix + 'uuid'
+                if vm_key not in si.keys():
+                    continue
+
+                try:
+                    vm_obj = self._vnc_lib.virtual_machine_read(id=si[vm_key])
+                except NoIdError:
+                    continue
+
+                vmi_back_refs = vm_obj.get_virtual_machine_interface_back_refs()
+                for vmi_back_ref in vmi_back_refs or []:
+                    try:
+                        vmi_obj = self._vnc_lib.virtual_machine_interface_read(
+                            id=vmi_back_ref['uuid'])
+                    except NoIdError:
+                        continue
+                    vmi_props = vmi_obj.get_virtual_machine_interface_properties()
+                    if not vmi_props:
+                        continue
+                    if_type = vmi_props.get_service_interface_type()
+                    if not if_type:
+                        continue
+                    key = prefix + 'if-' + if_type
+                    self.db.service_instance_insert(si_fq_str,
+                                                    {key:vmi_obj.uuid})
+
     # create service template
     def _create_default_template(self, st_name, svc_type, svc_mode=None,
                                  hypervisor_type='virtual-machine',
