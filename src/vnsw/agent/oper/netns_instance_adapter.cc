3ff12604a6031364ea2007a1be0a5ff8425f2b46
Mateusz Zalega <mateusz.zalega@codilime.com>
2014-12-04 23:19:23 +0000

    vrouter agent: split instance_manager_adapter
    Signed-off-by: Mateusz Zalega <mateusz.zalega@codilime.com>
    Change-Id: If04a7fbfc5009ccadf058a28e4c24e55be831242

---
+++ src/vnsw/agent/oper/netns_instance_adapter.cc
@@ -0,0 +1,80 @@
+#include "oper/netns_instance_adapter.h"
+#include "oper/service_instance.h"
+#include "oper/instance_task.h"
+
+InstanceTask* NetNSInstanceAdapter::CreateStartTask(const ServiceInstance::Properties &props, bool update) {
+    std::stringstream cmd_str;
+
+    if (netns_cmd_.length() == 0) {
+        return NULL;
+    }
+    cmd_str << netns_cmd_ << " create";
+
+    cmd_str << " " << props.ServiceTypeString();
+    cmd_str << " " << UuidToString(props.instance_id);
+    cmd_str << " " << UuidToString(props.vmi_inside);
+    cmd_str << " " << UuidToString(props.vmi_outside);
+
+    if (props.ip_prefix_len_inside != -1)  {
+        cmd_str << " --vmi-left-ip " << props.ip_addr_inside << "/";
+        cmd_str << props.ip_prefix_len_inside;
+    } else {
+        cmd_str << " --vmi-left-ip 0.0.0.0/0";
+    }
+    cmd_str << " --vmi-right-ip " << props.ip_addr_outside << "/";
+    cmd_str << props.ip_prefix_len_outside;
+
+    if (!props.mac_addr_inside.empty()) {
+        cmd_str << " --vmi-left-mac " << props.mac_addr_inside;
+    } else {
+        cmd_str << " --vmi-left-mac 00:00:00:00:00:00";
+    }
+    cmd_str << " --vmi-right-mac " << props.mac_addr_outside;
+
+    if (props.service_type == ServiceInstance::LoadBalancer) {
+        cmd_str << " --cfg-file " << loadbalancer_config_path_ <<
+            props.pool_id << "/etc/haproxy/haproxy.cfg";
+        cmd_str << " --pool-id " << props.pool_id;
+        cmd_str << " --gw-ip " << props.gw_ip;
+    }
+
+    if (update) {
+        cmd_str << " --update";
+    }
+
+    return new InstanceTask(cmd_str.str(), START, agent_->event_manager());
+}
+
+InstanceTask* NetNSInstanceAdapter::CreateStopTask(const ServiceInstance::Properties &props) {
+    std::stringstream cmd_str;
+
+    if (netns_cmd_.length() == 0) {
+        return NULL;
+    }
+    cmd_str << netns_cmd_ << " destroy";
+
+    if (props.instance_id.is_nil() ||
+        props.vmi_outside.is_nil()) {
+        return NULL;
+    }
+
+    if (props.interface_count == 2 && props.vmi_inside.is_nil()) {
+        return NULL;
+    }
+
+    cmd_str << " " << props.ServiceTypeString();
+    cmd_str << " " << UuidToString(props.instance_id);
+    cmd_str << " " << UuidToString(props.vmi_inside);
+    cmd_str << " " << UuidToString(props.vmi_outside);
+    if (props.service_type == ServiceInstance::LoadBalancer) {
+        cmd_str << " --cfg-file " << loadbalancer_config_path_ <<
+            props.pool_id << "/etc/haproxy/haproxy.cfg";
+        cmd_str << " --pool-id " << props.pool_id;
+    }
+
+    return new InstanceTask(cmd_str.str(), STOP, agent_->event_manager());
+}
+
+bool NetNSInstanceAdapter::isApplicable(const ServiceInstance::Properties &props) {
+    return (props.virtualization_type == ServiceInstance::NetworkNamespace);
+}
