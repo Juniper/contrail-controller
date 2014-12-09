c8f8cdd62686c24081cec32e28d068ffe47d91a6
Babu Shanmugam <anbu@enovance.com>
2014-12-09 12:02:19 +0000

    Openstack port-create with pre-existing mac-address is not denied
    There should not be two ports with the same mac-address in the same network.
    
    Change-Id: I77287d9fe2fbc87d339c480abc0a13f1b078c84c
    Closes-bug: #1400683

--- src/config/vnc_openstack/vnc_openstack/neutron_plugin_db.py
+++ src/config/vnc_openstack/vnc_openstack/neutron_plugin_db.py
@@ -3824,6 +3824,18 @@ class DBInterface(object):
         tenant_id = self._get_tenant_id_for_create(context, port_q);
         proj_id = str(uuid.UUID(tenant_id))
 
+        # if mac-address is specified, check against the exisitng ports
+        # to see if there exists a port with the same mac-address
+        if 'mac_address' in port_q:
+            ports = self._vnc_lib.virtual_machine_interfaces_list(
+                parent_id=proj_id, back_ref_id=net_id, detail=True)
+            for port in ports:
+                macs = port.get_virtual_machine_interface_mac_addresses()
+                for mac in macs.get_mac_address():
+                    if mac == port_q['mac_address']:
+                        raise self._raise_contrail_exception("MacAddressInUse",
+                            net_id=net_id, mac=port_q['mac_address'])
+
         # initialize port object
         port_obj = self._port_neutron_to_vnc(port_q, net_obj, CREATE)
 
