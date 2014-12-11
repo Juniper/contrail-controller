1a43ecfd45ebd011ef56f5f6fcb8fa9f6f417b03
Shabbir Hussain <shabbirh@juniper.net>
2014-12-11 06:34:49 -0800

    Enhancement to contrail-logs - add options to list valid message types Closes-Bug: #1335320
    Change-Id: I774d48ed1e4f29b7887ba95b58f8f8524b8d7031

--- src/opserver/log.py
+++ src/opserver/log.py
@@ -25,6 +25,8 @@ import sandesh.viz.constants as VizConstants
 from pysandesh.gen_py.sandesh.ttypes import SandeshType, SandeshLevel
 from pysandesh.sandesh_logger import SandeshLogger
 from pysandesh.util import UTCTimestampUsec
+import commands
+import ast
 
 OBJECT_TYPE_LIST = [table_info.log_query_name for table_info in \
     VizConstants._OBJECT_TABLES.values()]
@@ -115,6 +117,8 @@ class LogQuerier(object):
             type=int, default=514)
         parser.add_argument("--f", help="Tail logs from now", action="store_true")
         parser.add_argument("--keywords", help="comma seperated list of keywords")
+        parser.add_argument("--message-types", \
+         help="Display list of message type", action="store_true")
         self._args = parser.parse_args()
         return 0
     # end parse_args
@@ -134,7 +138,20 @@ class LogQuerier(object):
                  invalid_combination += ", --end-time"
             print "Combination of options" + invalid_combination + " are not valid."
             return -1
-        start_time, end_time = self._start_time, self._end_time 
+        start_time, end_time = self._start_time, self._end_time
+        if self._args.message_types is True:
+            command_str = ("contrail-stats --table FieldNames.fields" +
+               " --where name=MessageTable:Messagetype --select name fields.value" +
+               " --start-time " + str(start_time) +
+               " --end-time " + str(end_time) +
+               " --analytics-api-ip " + str(self._args.analytics_api_ip) +
+               " --analytics-api-port " + str(self._args.analytics_api_port))
+            res = commands.getoutput(command_str)
+            res = res.splitlines()
+            res = res[1:]
+            for r in res:
+                print ast.literal_eval(r)['fields.value']
+            return None
         messages_url = OpServerUtils.opserver_query_url(
             self._args.analytics_api_ip,
             self._args.analytics_api_port)
