c65d2af96f9e217e55c089f3e493b03e9011b62f
akumari <akumari@juniper.net>
2014-12-26 04:40:31 -0800

    Safeguard checks for qed crash when given invalid parameters Closes-Bug: #1344534
    Change-Id: I4f94d49e3957242c8e225bb92c4c61848dda7472

--- src/query_engine/select.cc
+++ src/query_engine/select.cc
@@ -55,8 +55,6 @@ SelectQuery::SelectQuery(QueryUnit *main_query,
     QE_PARSE_ERROR(json_select_fields.IsArray());
     QE_TRACE(DEBUG, "# of select fields is " << json_select_fields.Size());
 
-    QE_INVALIDARG_ERROR(m_query->is_valid_query(json_select_fields));
-
     if (m_query->is_stat_table_query()) {
         for (rapidjson::SizeType i = 0; i < json_select_fields.Size(); i++) {
             select_column_fields.push_back(json_select_fields[i].GetString());
@@ -120,7 +118,14 @@ SelectQuery::SelectQuery(QueryUnit *main_query,
             agg_stats.push_back(agg_stats_entry);
             QE_INVALIDARG_ERROR(
                 m_query->table() == g_viz_constants.FLOW_SERIES_TABLE);
-        }      
+        }
+        else if ((json_select_fields[i].GetString() ==
+                std::string(SELECT_FLOW_COUNT)) ||
+                (json_select_fields[i].GetString() ==
+                std::string(SELECT_FLOW_CLASS_ID))) {
+            QE_INVALIDARG_ERROR(
+                m_query->table() == g_viz_constants.FLOW_SERIES_TABLE);
+        }
         // processing other select fields
         else {
             QE_INVALIDARG_ERROR(is_valid_select_field(
@@ -134,6 +139,8 @@ SelectQuery::SelectQuery(QueryUnit *main_query,
 
     if (m_query->table() == g_viz_constants.FLOW_SERIES_TABLE) {
         evaluate_fs_query_type();
+        if (fs_query_type_ == SelectQuery::FS_SELECT_INVALID)
+            QE_INVALIDARG_ERROR(false);
     }
 
     if ((m_query->table() == g_viz_constants.FLOW_TABLE) && !uuid_key_selected) {
@@ -219,6 +226,30 @@ void SelectQuery::evaluate_fs_query_type() {
             for (it = agg_stats.begin(); it != agg_stats.end(); ++it) {
                 if ((*it).agg_op == RAW) {
                     fs_query_type_ |= SelectQuery::FS_SELECT_T;
+                    for (it = agg_stats.begin(); it != agg_stats.end(); ++it) {
+                        if ((*it).agg_op == SUM) {
+                            fs_query_type_ = SelectQuery::FS_SELECT_INVALID;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    if (provide_timeseries && agg_stats.size()) {
+        std::vector<agg_stats_t>::const_iterator it;
+        if (granularity) {
+            for (it = agg_stats.begin(); it != agg_stats.end(); ++it) {
+                if ((*it).agg_op == RAW) {
+                    fs_query_type_ = SelectQuery::FS_SELECT_INVALID;
+                    break;
+                }
+            }
+        } else {
+            for (it = agg_stats.begin(); it != agg_stats.end(); ++it) {
+                if ((*it).agg_op == SUM) {
+                    fs_query_type_ = SelectQuery::FS_SELECT_INVALID;
                     break;
                 }
             }
