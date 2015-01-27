d38a683418952695f8523cdc6c1afd438c5509bf
manishsingh <manishs@juniper.net>
2015-01-27 17:22:00 +0530

    DHCP in l2 mode:
    Use DHCP flag from IPAM to enable DHCP mode for VMI running in
    l2 mode(i.e. no l3 routes programmed).
    Closes-bug: #1415014
    
    Change-Id: I6ad9813580dc2478d1a2a81ca01c204cef197f5f

--- src/vnsw/agent/services/dhcp_handler.cc
+++ src/vnsw/agent/services/dhcp_handler.cc
@@ -363,7 +363,7 @@ bool DhcpHandler::HandleVmRequest() {
         return true;
     }
     vm_itf_ = static_cast<VmInterface *>(itf);
-    if (!vm_itf_->layer3_forwarding()) {
+    if (!vm_itf_->dhcp_enabled()) {
         DHCP_TRACE(Error, "DHCP request on VM port with disabled ipv4 service: "
                    << GetInterfaceIndex());
         return true;
