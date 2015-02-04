5f85ceeb25025a260af0b62220d74d6ddaa77246
Anish Mehta <amehta00@gmail.com>
2015-02-04 11:41:12 -0800

    This is the initial commit of the alarm generator changes.
    - The collector now publishes UVE changes to kafka, and generator connect/disconnect notifications as well.
      It also writes key-to-partition mapping info to redis.
      For now, the number of partitions default to 100.
    
    - The alarm-generator waits for partition activation/deactivation commands via introspect.
      Based on this it, it can read what are the known UVEs for that partition from redis.
      It then listens for further UVE updates from Kafka.
      Based on UVE updates, generator connect/disconnect updates, and partition sync, we print a list
      of potentially impacted UVE keys
    
    - alarm-generator is added to systemless test, but kafka is not added yet
    
    - The tests in analytics_redistest are folded into analytics_uvetest, which will also have alarm tests
    
    Next steps for alarmgen:
      - Using mackkafka for enabling tests for the above functionality
      - Code for registering which UVE updates we are interested in for evaluating alarms, and callbacks for
      doing the evaluation.
      - Generating alarms based on UVEs (as per new sandesh type)
      - Integration with partitioning library
    
    Change-Id: Ie662109a4593122e05f8dd37ec76eb6032d2ae8c
    
    Adding files
    
    Change-Id: I842736b0608113e640a34b9c76618225220712ad

--- src/analytics/uvedelete.lua
+++ src/analytics/uvedelete.lua
@@ -34,7 +34,14 @@ local _origins = KEYS[4]
 local _table = KEYS[5]
 local _deleted = KEYS[6]
 
-redis.log(redis.LOG_NOTICE,"DelUVE on "..sm.." for "..key)
+local part = redis.call('hget',"KEY2PART:"..sm..":"..typ, key)
+if not part then
+    part = "NULL"
+else
+   redis.call('hrem', "KEY2PART:"..sm..":"..typ, key)
+   redis.call('srem', "PART2KEY:"..part, sm..":"..typ..":"..key)
+end
+redis.log(redis.LOG_NOTICE,"DelUVE on "..sm.." for "..key.." part "..part)
 redis.call('select',db)
 sub_del(_values)
 redis.call('rename', _values, _del)
