500cb9f50ef824442e6d5d13576f6ec152fa0cff
Sachin Bansal <sachin@contrailsystems.com>
2015-02-13 09:39:08 -0800

    Check for connected_network before creating connection
    Change-Id: Ib3d69f735923a430a58a397cd8c632e57366b427
    Closes-Bug: 1421113

--- src/config/schema-transformer/to_bgp.py
+++ src/config/schema-transformer/to_bgp.py
@@ -3040,6 +3040,7 @@ class SchemaTransformer(object):
                         action = arule.get_action_list()
                         if action.simple_action == 'deny':
                             continue
+                        connected_network = None
                         if (match.dst_address.virtual_network in
                                 [network_name, "any"]):
                             connected_network =\
@@ -3056,7 +3057,7 @@ class SchemaTransformer(object):
                             action.apply_service = []
                             continue
 
-                        if action.simple_action:
+                        if connected_network and action.simple_action:
                             virtual_network.add_connection(connected_network)
 
                     # end for acl_rule_list
