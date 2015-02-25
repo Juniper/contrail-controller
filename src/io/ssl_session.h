924645b87fe89ecd94b2e4e31a1e509626c1b8af
Nipa Kumar <nipak@juniper.net>
2015-02-25 15:09:21 -0800

    Xmpp SSL support
    6 Add class SslSession::SslReader, to distinguish if the SslSession class
      needs to put fd back to select or not.
      Eg: For delayed Ssl handshake case,
          1) fd will be put to select for initial message exchanges by the
             SslSession class.
          2) Once ssl handshake is triggered, the boost:ssl library  manages the
             fd, where in the SslSession class need to backoff.
          3) Once ssl handshake is complete the SslSession class manages the
             fd to read/write
    7 Add test_delayed_ssl_handshake test, that sends some plain-text messages
      on connection success, trigers ssl handhshake and on handshake success,
      encrypted data is sent over the socket.
    
    Change-Id: I16f1c881a55e59e5b412c632ad33917def527464

--- src/io/ssl_session.h
+++ src/io/ssl_session.h
@@ -8,13 +8,17 @@
 #include "io/tcp_session.h"
 #include "io/ssl_server.h"
 
+class SslSession;
+typedef boost::intrusive_ptr<SslSession> SslSessionPtr;
+
 class SslSession : public TcpSession {
 public:
     typedef boost::asio::ssl::stream<boost::asio::ip::tcp::socket> SslSocket;
 
     // SslSession constructor takes ownership of socket.
     SslSession(SslServer *server, SslSocket *socket,
-               bool async_read_ready = true, bool ssl_handshake_delayed = false);
+               bool async_read_ready = true, bool ssl_disabled = false,
+               bool ssl_handshake_delayed = false);
 
     virtual Socket *socket() const;
 
@@ -24,27 +28,64 @@ public:
     // Override to trigger handshake
     virtual void Accepted();
 
+    // Trigger delayed SslHandShake
+    typedef boost::function<void(SslSessionPtr, 
+        const boost::system::error_code& error)> SslHandShakeCallbackHandler;
+    void TriggerServerSslHandShake(SslHandShakeCallbackHandler);
+    void TriggerClientSslHandShake(SslHandShakeCallbackHandler);
+
     // Additional states to determine the trigger of SSL handshake
+    bool IsSslDisabled() {
+        return ssl_disabled_;
+    }
+
     bool IsSslHandShakeDelayed() {
         return ssl_handshake_delayed_;
+    }
+
+    bool IsSslHandShakeSuccess() {
+        return ssl_handshake_success_;
+    }
+
+    void SetSslHandShakeStatus(bool success) {
+        tbb::mutex::scoped_lock lock(mutex_); 
+        ssl_handshake_success_ = success;
     }
 
 protected:
     virtual ~SslSession();
 
 private:
+    class SslReader;
+    friend class Sslserver;
     static void AcceptHandShakeHandler(TcpSessionPtr session,
                                        const boost::system::error_code& error);
     static void ConnectHandShakeHandler(TcpSessionPtr session, Endpoint remote,
                                         const boost::system::error_code& error);
+
+    static void AsyncReadHandler(SslSessionPtr session,
+                                 boost::asio::mutable_buffer buffer,
+                                 const boost::system::error_code &error,
+                                 size_t size);
 
     void AsyncReadSome(boost::asio::mutable_buffer buffer);
     std::size_t WriteSome(const uint8_t *data, std::size_t len,
                           boost::system::error_code &error);
     void AsyncWrite(const u_int8_t *data, std::size_t size);
 
+    virtual void OnRead(Buffer buffer) = 0;
+
+    SslServerPtr server_;
     boost::scoped_ptr<SslSocket> ssl_socket_;
-    bool ssl_handshake_delayed_;
+
+    /**************** protected by mutex_ *************************/ 
+    bool ssl_handshake_success_;      // ssl handshake success
+    /**************** end protected by mutex_ *********************/
+
+    /**************** config knobs ********************************/
+    bool ssl_disabled_;              // default false
+    bool ssl_handshake_delayed_;     // default false
+    /**************************************************************/ 
 
     DISALLOW_COPY_AND_ASSIGN(SslSession);
 };
