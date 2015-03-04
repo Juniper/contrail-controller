90341b0510c45c92574d6ac395b3d9b0232a7085
manishsingh <manishs@juniper.net>
2015-03-03 22:17:31 -0800

    Problem: Floating IP unreachable due to wrong tunnel type computed by vRouter agent
    In case of ECMP composite NH sent by control node, the path was storing default MPLS tunnel
    bitmap of (GRE+UDP). Encap sent by control node was directly coded in component NH.
    On recomputation of encapsulation priorities change walk was issued which in turn uses encap from path.
    Component NH was rebaked with the tunnel type derived from path tunnel bitmap. Tunnel types sent by control node
    for these external routes are lost because of this rebake.
    
    Change-Id: I298041d3790d7629549019a7fba42a44f0bfc035
    Fix: Store the encap sent by control node in path.
    Closes-bug: #1427849

--- src/vnsw/agent/controller/controller_route_path.h
+++ src/vnsw/agent/controller/controller_route_path.h
@@ -123,10 +123,12 @@ public:
                         bool local_ecmp_nh, const string &vrf_name,
                         SecurityGroupList sg_list,
                         const PathPreference &path_preference,
+                        uint32_t tunnel_bmap,
                         DBRequest &nh_req) :
         ControllerPeerPath(peer), dest_addr_(dest_addr), plen_(plen),
         vn_name_(vn_name), label_(label), local_ecmp_nh_(local_ecmp_nh),
-        vrf_name_(vrf_name), sg_list_(sg_list), path_preference_(path_preference)
+        vrf_name_(vrf_name), sg_list_(sg_list),
+        path_preference_(path_preference), tunnel_bmap_(tunnel_bmap)
         {nh_req_.Swap(&nh_req);}
 
     virtual ~ControllerEcmpRoute() { }
@@ -144,6 +146,7 @@ private:
     string vrf_name_;
     SecurityGroupList sg_list_;
     PathPreference path_preference_;
+    uint32_t tunnel_bmap_;
     DBRequest nh_req_;
     DISALLOW_COPY_AND_ASSIGN(ControllerEcmpRoute);
 };
