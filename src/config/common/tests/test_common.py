6bd66159ac238a8145aa667fa29455bed54987ab
Hampapur Ajay <hajay@juniper.net>
2015-04-06 19:07:56 -0700

    config-perf: add support for multikey get in useragent kv table
    neutron net-list first finds list of network and subnet ids and
    then issues subnet list with list of subnet UUIDs. The neutron
    backend in subnets_list() was doing as many roundtrips as # of
    subnet UUIDs to useragent-kv table to get subnet details.
    This is much faster by adding support for RETRIEVE with
    multiple keys. Test for the same.
    
    Change-Id: Ie001bcbbb63f4b885e265a1a2815f650a257c1d3
    Closes-Bug: #1433979

--- src/config/common/tests/test_common.py
+++ src/config/common/tests/test_common.py
@@ -46,16 +46,34 @@ sys.path.insert(0, '../../../../build/production/api-lib/vnc_api')
 sys.path.insert(0, '../../../../distro/openstack/')
 sys.path.append('../../../../build/production/config/api-server/vnc_cfg_api_server')
 
-import vnc_cfg_api_server
-if not hasattr(vnc_cfg_api_server, 'main'):
-    from vnc_cfg_api_server import vnc_cfg_api_server
+try:
+    import vnc_cfg_api_server
+    if not hasattr(vnc_cfg_api_server, 'main'):
+        from vnc_cfg_api_server import vnc_cfg_api_server
+except ImportError:
+    vnc_cfg_api_server = 'vnc_cfg_api_server could not be imported'
+
 try:
     import to_bgp
 except ImportError:
-    from schema_transformer import to_bgp
-import svc_monitor
-if not hasattr(svc_monitor, 'main'):
-    from svc_monitor import svc_monitor
+    try:
+        from schema_transformer import to_bgp
+    except ImportError:
+        to_bgp = 'to_bgp could not be imported'
+
+try:
+    import svc_monitor
+    if not hasattr(svc_monitor, 'main'):
+        from svc_monitor import svc_monitor
+except ImportError:
+    svc_monitor = 'svc_monitor could not be imported'
+
+try:
+    import device_manager
+    if not hasattr(device_manager, 'main'):
+        from device_manager import device_manager
+except ImportError:
+    device_manager = 'device_manager could not be imported'
 
 def generate_conf_file_contents(conf_sections):
     cfg_parser = ConfigParser.RawConfigParser()
@@ -173,9 +191,6 @@ def launch_schema_transformer(api_server_ip, api_server_port):
 # end launch_schema_transformer
 
 def launch_device_manager(api_server_ip, api_server_port):
-    import device_manager
-    if not hasattr(device_manager, 'main'):
-        from device_manager import device_manager
     args_str = ""
     args_str = args_str + "--api_server_ip %s " % (api_server_ip)
     args_str = args_str + "--api_server_port %s " % (api_server_port)
