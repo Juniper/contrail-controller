7370c304205d792c37eff05f2cf0e5101b308c29
Hampapur Ajay <hajay@juniper.net>
2015-04-27 10:22:24 -0700

    config: escape any quotes in name of resource on ifmap write.
    Define a version of escape/unescape that invokes the sax library
    based ones with additional escapes for single and double-quotes.
    
    A 'patch' to do the same on already deployed systems is at
    https://gist.github.com/ajayhn/603c3f8168132aba6e5d
    
    Change-Id: Ifcae17e80ffc1528908f37f79c3215887487ca14
    Partial-Bug: #1449156

--- src/config/api-server/tests/test_crud_basic.py
+++ src/config/api-server/tests/test_crud_basic.py
@@ -932,21 +932,21 @@ class TestVncCfgApiServer(test_case.ApiServerTestCase):
     # end test_floatingip_as_instanceip
 
     def test_name_with_reserved_xml_char(self):
-        vn_name = self.id()+'-&vn<1>'
+        vn_name = self.id()+'-&vn<1>"2\''
         vn_obj = VirtualNetwork(vn_name)
         # fq_name, fq_name_str has non-escape val, ifmap-id has escaped val
         ifmap_id = cfgm_common.imid.get_ifmap_id_from_fq_name(vn_obj.get_type(),
                        vn_obj.get_fq_name())
-        self.assertIsNot(re.search("&amp;vn&lt;1&gt;", ifmap_id), None)
+        self.assertIsNot(re.search("&amp;vn&lt;1&gt;&quot;2&apos;", ifmap_id), None)
         fq_name_str = cfgm_common.imid.get_fq_name_str_from_ifmap_id(ifmap_id)
-        self.assertIsNone(re.search("&amp;vn&lt;1&gt;", fq_name_str))
+        self.assertIsNone(re.search("&amp;vn&lt;1&gt;&quot;2&apos;", fq_name_str))
 
         self._add_detail('Creating network with name %s expecting success' %(vn_name))
         self._vnc_lib.virtual_network_create(vn_obj)
         self.assertTill(self.ifmap_has_ident, obj=vn_obj)
         ident_elem = FakeIfmapClient._graph[ifmap_id]['ident']
         ident_str = etree.tostring(ident_elem)
-        mch = re.search("&amp;vn&lt;1&gt;", ident_str)
+        mch = re.search("&amp;vn&lt;1&gt;&quot;2&apos;", ident_str)
         self.assertIsNot(mch, None)
         self._vnc_lib.virtual_network_delete(id=vn_obj.uuid)
 
