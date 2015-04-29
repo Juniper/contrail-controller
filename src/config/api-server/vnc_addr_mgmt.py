Juniper/contrail-controller

commit 3d48f30de8ad47249c7c444d9baeea393001d501

Read only required fields in dbe_read of virtual-network
In most of the cases, we don't need the backrefs to instance ip or vmis
while reading the VN

By specifying the required field read from cassandra is optimised.

Also, avoid duplicate read of VN by passing vn_dict to ip_alloc_req
Closes-Bug: 1433928

Change-Id: If64eb98987fa2e1f7a10275908d98228a2c8674d

src/config/api-server/vnc_addr_mgmt.py
@@ -350,10 +350,10 @@ class AddrMgmt(object):
 
     def _get_subnet_dicts(self, vn_fq_name, vn_dict=None):
         db_conn = self._get_db_conn()
-        vn_uuid = db_conn.fq_name_to_uuid('virtual-network', vn_fq_name)
 
         # Read in the VN details if not passed in
         if not vn_dict:
+            vn_uuid = db_conn.fq_name_to_uuid('virtual-network', vn_fq_name)
             (ok, result) = self._db_conn.dbe_read(
                                 obj_type='virtual-network',
                                 obj_ids={'uuid': vn_uuid},
@@ -571,7 +571,8 @@ class AddrMgmt(object):
         for network in proj_dict.get('virtual_networks', []):
             if network['uuid'] == db_vn_dict['uuid']:
                 continue
-            ok, net_dict = db_conn.dbe_read('virtual-network', network)
+            ok, net_dict = db_conn.dbe_read('virtual-network', network, 
+                                            obj_fields=['network_ipam_refs'])
             if not ok:
                 continue
             vn_subnets = self._vn_to_subnets(net_dict)
@@ -745,10 +746,10 @@ class AddrMgmt(object):
 
     # allocate an IP address for given virtual network
     # we use the first available subnet unless provided
-    def ip_alloc_req(self, vn_fq_name, sub=None, asked_ip_addr=None, 
+    def ip_alloc_req(self, vn_fq_name, vn_dict=None, sub=None, asked_ip_addr=None, 
                      asked_ip_version=4):
         vn_fq_name_str = ':'.join(vn_fq_name)
-        subnet_dicts = self._get_subnet_dicts(vn_fq_name)
+        subnet_dicts = self._get_subnet_dicts(vn_fq_name, vn_dict)
 
         if not subnet_dicts:
             raise AddrMgmtSubnetUndefined(vn_fq_name_str)

src/config/api-server/vnc_cfg_types.py
@@ -33,7 +33,8 @@ class GlobalSystemConfigServer(GlobalSystemConfigServerGen):
         if not ok:
             return (ok, result)
         for vn_name, vn_uuid in result:
-            ok, result = db_conn.dbe_read('virtual-network', {'uuid': vn_uuid})
+            ok, result = db_conn.dbe_read('virtual-network', {'uuid': vn_uuid}, 
+                                           obj_fields=['route_target_list'])
             if not ok:
                 return ok, result
             rt_dict = result.get('route_target_list', {})
@@ -200,7 +201,8 @@ class InstanceIpServer(InstanceIpServerGen):
         req_ip = obj_dict.get("instance_ip_address", None)
 
         vn_id = {'uuid': db_conn.fq_name_to_uuid('virtual-network', vn_fq_name)}
-        (read_ok, vn_dict) = db_conn.dbe_read('virtual-network', vn_id)
+        (read_ok, vn_dict) = db_conn.dbe_read('virtual-network', vn_id, 
+                            obj_fields=['router_external', 'network_ipam_refs'])
         if not read_ok:
             return (False, (500, 'Internal error : ' + pformat(vn_dict)))
 
@@ -227,7 +229,7 @@ class InstanceIpServer(InstanceIpServerGen):
 
         try:
             ip_addr = cls.addr_mgmt.ip_alloc_req(
-                vn_fq_name, sub=sub, asked_ip_addr=req_ip,
+                vn_fq_name, vn_dict=vn_dict, sub=sub, asked_ip_addr=req_ip,
                 asked_ip_version=req_ip_version)
         except Exception as e:
             return (False, (500, str(e)))
@@ -341,7 +343,8 @@ class VirtualMachineInterfaceServer(VirtualMachineInterfaceServerGen):
             if not vn_fq_name:
                 return (False, (500, 'Internal error : ' + pformat(vn_dict)))
             vn_uuid = db_conn.fq_name_to_uuid('virtual-network', vn_fq_name)
-        (ok, vn_dict) = db_conn.dbe_read('virtual-network', {'uuid':vn_uuid})
+        (ok, vn_dict) = db_conn.dbe_read('virtual-network', {'uuid':vn_uuid}, 
+                                         obj_fields=['parent_uuid'])
         if not ok:
             return (False, (500, 'Internal error : ' + pformat(vn_dict)))
 
@@ -486,7 +489,8 @@ class VirtualNetworkServer(VirtualNetworkServerGen):
             return True,  ""
 
         vn_id = {'uuid': id}
-        (read_ok, read_result) = db_conn.dbe_read('virtual-network', vn_id)
+        (read_ok, read_result) = db_conn.dbe_read('virtual-network', vn_id, 
+                                               obj_fields=['network_ipam_refs'])
         if not read_ok:
             return (False, (500, read_result))
 
@@ -533,7 +537,8 @@ class VirtualNetworkServer(VirtualNetworkServerGen):
             return True,  ""
 
         vn_id = {'uuid': id}
-        (read_ok, read_result) = db_conn.dbe_read('virtual-network', vn_id)
+        (read_ok, read_result) = db_conn.dbe_read('virtual-network', vn_id, 
+                                               obj_fields=['network_ipam_refs'])
         if not read_ok:
             return (False, (500, read_result))
         cls.addr_mgmt.net_update_req(fq_name, obj_dict, read_result, id)

commit c08c04d58c8ce96ec4df896bf2cda117d317e402

Read the instance ip and floating ip pool back_ref for the VN when subnet is being deleted from the VN
This issue is introduced by "https://github.com/Juniper/contrail-controller/commit/3d48f30de8ad47249c7c444d9baeea393001d501"
Closes-bug: #1447923

Change-Id: I575f6f34be63949526ec15df52db552fd2c1119f

src/config/api-server/vnc_addr_mgmt.py
@@ -689,6 +689,9 @@ class AddrMgmt(object):
         # eg. existing [1.1.1.0/24, 2.2.2.0/24],
         #     requested [1.1.1.0/24] OR
         #     requested [1.1.1.0/28, 2.2.2.0/24]
+        existing_subnets = self._vn_to_subnets(db_vn_dict)
+        if not existing_subnets:
+            return True, ""
         requested_subnets = self._vn_to_subnets(req_vn_dict)
         if requested_subnets is None:
             # subnets not modified in request
@@ -701,6 +704,20 @@ class AddrMgmt(object):
                 return False, "Cannot Delete IP Block, Instance IP(s) in use"
             if db_vn_dict.get('floating_ip_pools'):
                 return False, "Cannot Delete IP Block, Floating Pool(s) in use"
+
+        delete_set = set(existing_subnets) - set(requested_subnets)
+
+        if len(delete_set):
+            # read the instance ip and floating ip pool only if subnet is being
+            # deleted. Skip the port check if no subnet is being deleted
+            vn_id = {'uuid': db_vn_dict['uuid']}
+            obj_fields = ['network_ipam_refs', 'instance_ip_back_refs', 'floating_ip_pools']
+            (read_ok, db_vn_dict) = db_conn.dbe_read('virtual-network', vn_id, obj_fields)
+            if not read_ok:
+                return (False, (500, db_vn_dict))
+        else:
+            return True, ""
+
 
         instip_refs = db_vn_dict.get('instance_ip_back_refs', [])
         for ref in instip_refs: