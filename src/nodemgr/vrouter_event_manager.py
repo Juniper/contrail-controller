be26601ac721df5910080b76f32076c01acc5f47
Shabbir Hussain <shabbirh@juniper.net>
2015-05-04 02:38:20 -0700

    Fix module name in nodemgr package.
    Fix: Module names are changed to standard specification.
    Run pep8 for each file in nodemgr package and fix the issues
    reported.
    
    Change-Id: I7505d5fb54b020fd7ea8d0278c68a441606f1e75
    Closes-Bug: #1448957

--- src/nodemgr/vrouter_event_manager.py
+++ src/nodemgr/vrouter_event_manager.py
@@ -1,6 +1,7 @@
 doc = " "
 
-from gevent import monkey; monkey.patch_all()
+from gevent import monkey
+monkey.patch_all()
 import os
 import sys
 import socket
@@ -13,8 +14,8 @@ import select
 import gevent
 import ConfigParser
 
-from nodemgr.EventManager import EventManager
-from nodemgr.VrouterProcessStat import VrouterProcessStat
+from nodemgr.event_manager import EventManager
+from nodemgr.vrouter_process_stat import VrouterProcessStat
 
 from ConfigParser import NoOptionError
 
@@ -36,36 +37,46 @@ from vrouter.vrouter.process_info.ttypes import \
 from vrouter.vrouter.process_info.constants import \
     ProcessStateNames
 
+
 def usage():
     print doc
     sys.exit(255)
 
+
 class VrouterEventManager(EventManager):
-    def __init__(self, rule_file, discovery_server, discovery_port, collector_addr):
-        EventManager.__init__(self, rule_file, discovery_server, discovery_port, collector_addr)
+    def __init__(self, rule_file, discovery_server,
+                 discovery_port, collector_addr):
+        EventManager.__init__(self, rule_file, discovery_server,
+                              discovery_port, collector_addr)
         self.node_type = "contrail-vrouter"
         self.module = Module.COMPUTE_NODE_MGR
-        self.module_id =  ModuleNames[self.module]
+        self.module_id = ModuleNames[self.module]
 
         self.supervisor_serverurl = "unix:///tmp/supervisord_vrouter.sock"
         self.add_current_process()
 
         os_nova_comp = VrouterProcessStat('openstack-nova-compute')
-        (os_nova_comp_state, error_value) = Popen("openstack-status | grep openstack-nova-compute | cut -d ':' -f2", shell=True, stdout=PIPE).communicate()
+        popen_cmd = "openstack-status | grep openstack-nova-compute" + \
+            " | cut -d ':' -f2"
+        (os_nova_comp_state, error_value) = \
+            Popen(popen_cmd, shell=True, stdout=PIPE).communicate()
         os_nova_comp.process_state = os_nova_comp_state.strip()
         if (os_nova_comp.process_state == 'active'):
             os_nova_comp.process_state = 'PROCESS_STATE_RUNNING'
-            os_nova_comp.start_time = str(int(time.time()*1000000))
+            os_nova_comp.start_time = str(int(time.time() * 1000000))
             os_nova_comp.start_count += 1
         if (os_nova_comp.process_state == 'dead'):
             os_nova_comp.process_state = 'PROCESS_STATE_FATAL'
-        sys.stderr.write('Openstack Nova Compute status:' + os_nova_comp.process_state + "\n")
+        sys.stderr.write('Openstack Nova Compute status:' +
+                         os_nova_comp.process_state + "\n")
         self.process_state_db['openstack-nova-compute'] = os_nova_comp
-    #end __init__
+    # end __init__
 
     def process(self):
         if self.rule_file is '':
-            self.rule_file = "/etc/contrail/supervisord_vrouter_files/contrail-vrouter.rules"
+            self.rule_file = \
+                "/etc/contrail/supervisord_vrouter_files/" + \
+                "contrail-vrouter.rules"
         json_file = open(self.rule_file)
         self.rules_data = json.load(json_file)
         node_type = Module2NodeType[self.module]
@@ -74,32 +85,40 @@ class VrouterEventManager(EventManager):
         Config = self.read_config_data(config_file)
         self.get_collector_list(Config)
         _disc = self.get_discovery_client(Config)
-        sandesh_global.init_generator(self.module_id, socket.gethostname(),
+        sandesh_global.init_generator(
+            self.module_id, socket.gethostname(),
             node_type_name, self.instance_id, self.collector_addr,
-            self.module_id, 8102, ['vrouter.vrouter'],_disc)
-        #sandesh_global.set_logging_params(enable_local_log=True)
+            self.module_id, 8102, ['vrouter.vrouter'], _disc)
+        # sandesh_global.set_logging_params(enable_local_log=True)
         self.sandesh_global = sandesh_global
 
     def send_process_state_db(self, group_names):
-        self.send_process_state_db_base(group_names, ProcessInfo, NodeStatus, NodeStatusUVE)
+        self.send_process_state_db_base(
+            group_names, ProcessInfo, NodeStatus, NodeStatusUVE)
 
     def send_nodemgr_process_status(self):
-        self.send_nodemgr_process_status_base(ProcessStateNames, ProcessState, ProcessStatus, NodeStatus, NodeStatusUVE)
+        self.send_nodemgr_process_status_base(
+            ProcessStateNames, ProcessState, ProcessStatus,
+            NodeStatus, NodeStatusUVE)
 
     def get_process_state(self, fail_status_bits):
-        return self.get_process_state_base(fail_status_bits, ProcessStateNames, ProcessState)
+        return self.get_process_state_base(
+            fail_status_bits, ProcessStateNames, ProcessState)
 
     def send_disk_usage_info(self):
-        self.send_disk_usage_info_base(NodeStatusUVE, NodeStatus, DiskPartitionUsageStats)
+        self.send_disk_usage_info_base(
+            NodeStatusUVE, NodeStatus, DiskPartitionUsageStats)
 
     def get_process_stat_object(self, pname):
         return VrouterProcessStat(pname)
 
-    # overridden delete_process_handler - ignore delete in case of openstack-nova-compute
+    # overridden delete_process_handler -
+    # ignore delete in case of openstack-nova-compute
     def delete_process_handler(self, deleted_process):
         if deleted_process == 'openstack-nova-compute':
             return
-        super(VrouterEventManager, self).delete_process_handler(deleted_process)
+        super(VrouterEventManager,
+              self).delete_process_handler(deleted_process)
     # end delete_process_handler
 
     def runforever(self, test=False):
@@ -108,14 +127,15 @@ class VrouterEventManager(EventManager):
             gevent.sleep(1)
             # we explicitly use self.stdin, self.stdout, and self.stderr
             # instead of sys.* so we can unit test this code
-            headers, payload = self.listener_nodemgr.wait(self.stdin, self.stdout)
+            headers, payload = \
+                self.listener_nodemgr.wait(self.stdin, self.stdout)
 
-            #self.stderr.write("headers:\n" + str(headers) + '\n')
-            #self.stderr.write("payload:\n" + str(payload) + '\n')
+            # self.stderr.write("headers:\n" + str(headers) + '\n')
+            # self.stderr.write("payload:\n" + str(payload) + '\n')
 
-            pheaders, pdata = childutils.eventdata(payload+'\n')
-            #self.stderr.write("pheaders:\n" + str(pheaders)+'\n')
-            #self.stderr.write("pdata:\n" + str(pdata))
+            pheaders, pdata = childutils.eventdata(payload + '\n')
+            # self.stderr.write("pheaders:\n" + str(pheaders)+'\n')
+            # self.stderr.write("pdata:\n" + str(pdata))
 
             # check for process state change events
             if headers['eventname'].startswith("PROCESS_STATE"):
@@ -130,7 +150,10 @@ class VrouterEventManager(EventManager):
             # do periodic events
             if headers['eventname'].startswith("TICK_60"):
                 os_nova_comp = self.process_state_db['openstack-nova-compute']
-                (os_nova_comp_state, error_value) = Popen("openstack-status | grep openstack-nova-compute | cut -d ':' -f2", shell=True, stdout=PIPE).communicate()
+                popen_cmd = "openstack-status | " + \
+                    "grep openstack-nova-compute | cut -d ':' -f2"
+                (os_nova_comp_state, error_value) = \
+                    Popen(popen_cmd, shell=True, stdout=PIPE).communicate()
                 if (os_nova_comp_state.strip() == 'active'):
                     os_nova_comp_state = 'PROCESS_STATE_RUNNING'
                 if (os_nova_comp_state.strip() == 'dead'):
@@ -139,20 +162,26 @@ class VrouterEventManager(EventManager):
                     os_nova_comp_state = 'PROCESS_STATE_STOPPED'
                 if (os_nova_comp.process_state != os_nova_comp_state):
                     os_nova_comp.process_state = os_nova_comp_state.strip()
-                    sys.stderr.write('Openstack Nova Compute status changed to:' + os_nova_comp.process_state + "\n")
+                    msg = 'Openstack Nova Compute status changed to:'
+                    sys.stderr.write(msg + os_nova_comp.process_state + "\n")
                     if (os_nova_comp.process_state == 'PROCESS_STATE_RUNNING'):
-                        os_nova_comp.start_time = str(int(time.time()*1000000))
+                        os_nova_comp.start_time = \
+                            str(int(time.time() * 1000000))
                         os_nova_comp.start_count += 1
                     if (os_nova_comp.process_state == 'PROCESS_STATE_FATAL'):
-                        os_nova_comp.exit_time = str(int(time.time()*1000000))
+                        os_nova_comp.exit_time = \
+                            str(int(time.time() * 1000000))
                         os_nova_comp.exit_count += 1
                     if (os_nova_comp.process_state == 'PROCESS_STATE_STOPPED'):
-                        os_nova_comp.stop_time = str(int(time.time()*1000000))
+                        os_nova_comp.stop_time = \
+                            str(int(time.time() * 1000000))
                         os_nova_comp.stop_count += 1
-                    self.process_state_db['openstack-nova-compute'] = os_nova_comp
+                    self.process_state_db['openstack-nova-compute'] = \
+                        os_nova_comp
                     self.send_process_state_db('vrouter_group')
                 else:
-                    sys.stderr.write('Openstack Nova Compute status unchanged at:' + os_nova_comp.process_state + "\n")
+                    msg = 'Openstack Nova Compute status unchanged at:'
+                    sys.stderr.write(msg + os_nova_comp.process_state + "\n")
                 self.process_state_db['openstack-nova-compute'] = os_nova_comp
                 prev_current_time = self.event_tick_60(prev_current_time)
             self.listener_nodemgr.ok(self.stdout)
