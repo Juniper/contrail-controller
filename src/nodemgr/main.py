caa2953b71dfb13579979ee6a431c3f290e4cf69
Shabbir Hussain <shabbirh@juniper.net>
2015-05-04 02:38:20 -0700

    Fix module name in nodemgr package.
    Fix: Module names are changed to standard specification.
    Run pep8 for each file in nodemgr package and fix the issues
    reported.
    
    Change-Id: I7505d5fb54b020fd7ea8d0278c68a441606f1e75
    Closes-Bug: #1448957

--- src/nodemgr/main.py
+++ src/nodemgr/main.py
@@ -1,37 +1,41 @@
 #!/usr/bin/python
 
-from gevent import monkey; monkey.patch_all()
+from gevent import monkey
+monkey.patch_all()
 import os
 import sys
 import argparse
 import socket
 import gevent
-from nodemgr.AnalyticsNodemgr import AnalyticsEventManager
-from nodemgr.ControlNodemgr import ControlEventManager
-from nodemgr.ConfigNodemgr import ConfigEventManager
-from nodemgr.VrouterNodemgr import VrouterEventManager
-from nodemgr.DatabaseNodemgr import DatabaseEventManager
+from nodemgr.analytics_event_manager import AnalyticsEventManager
+from nodemgr.control_event_manager import ControlEventManager
+from nodemgr.config_event_manager import ConfigEventManager
+from nodemgr.vrouter_event_manager import VrouterEventManager
+from nodemgr.database_event_manager import DatabaseEventManager
+
 
 def main(argv=sys.argv):
-# Parse Arguments
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    # Parse Arguments
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument("--rules",
-                        default = '',
-                        help = 'Rules file to use for processing events')
+                        default='',
+                        help='Rules file to use for processing events')
     parser.add_argument("--nodetype",
-                        default = 'contrail-analytics',
-                        help = 'Type of node which nodemgr is managing')
+                        default='contrail-analytics',
+                        help='Type of node which nodemgr is managing')
     parser.add_argument("--discovery_server",
-                        default = socket.gethostname(),
-                        help = 'IP address of Discovery Server')
+                        default=socket.gethostname(),
+                        help='IP address of Discovery Server')
     parser.add_argument("--collectors",
-                        default = '',
-                        help = 'Collector addresses in format ip1:port1 ip2:port2')
+                        default='',
+                        help='Collector addresses in format' +
+                             'ip1:port1 ip2:port2')
     parser.add_argument("--discovery_port",
-                        type = int,
-                        default = 5998,
-                        help = 'Port of Discovery Server')
+                        type=int,
+                        default=5998,
+                        help='Port of Discovery Server')
     try:
         _args = parser.parse_args()
     except:
@@ -47,7 +51,7 @@ def main(argv=sys.argv):
     else:
         collector_addr = _args.collectors.split()
     sys.stderr.write("Collector address: " + str(collector_addr) + "\n")
-    #done parsing arguments
+    # done parsing arguments
 
     if not 'SUPERVISOR_SERVER_URL' in os.environ:
         sys.stderr.write('Node manager must be run as a supervisor event '
@@ -56,17 +60,27 @@ def main(argv=sys.argv):
         return
     prog = None
     if (node_type == 'contrail-analytics'):
-        prog = AnalyticsEventManager(rule_file, discovery_server, discovery_port, collector_addr)
+        prog = AnalyticsEventManager(
+            rule_file, discovery_server,
+            discovery_port, collector_addr)
     elif (node_type == 'contrail-config'):
-        prog = ConfigEventManager(rule_file, discovery_server, discovery_port, collector_addr)
+        prog = ConfigEventManager(
+            rule_file, discovery_server,
+            discovery_port, collector_addr)
     elif (node_type == 'contrail-control'):
-        prog = ControlEventManager(rule_file, discovery_server, discovery_port, collector_addr)
+        prog = ControlEventManager(
+            rule_file, discovery_server,
+            discovery_port, collector_addr)
     elif (node_type == 'contrail-vrouter'):
-        prog = VrouterEventManager(rule_file, discovery_server, discovery_port, collector_addr)
+        prog = VrouterEventManager(
+            rule_file, discovery_server,
+            discovery_port, collector_addr)
     elif (node_type == 'contrail-database'):
-        prog = DatabaseEventManager(rule_file, discovery_server, discovery_port, collector_addr)
+        prog = DatabaseEventManager(
+            rule_file, discovery_server,
+            discovery_port, collector_addr)
     else:
-       return
+        return
     prog.process()
     prog.send_nodemgr_process_status()
     prog.send_process_state_db(prog.group_names)
--- src/nodemgr/vrouter_process_stat.py
+++ src/nodemgr/vrouter_process_stat.py
@@ -4,7 +4,8 @@ import ConfigParser
 import sys
 import socket
 
-from nodemgr.ProcessStat import ProcessStat
+from nodemgr.process_stat import ProcessStat
+
 
 class VrouterProcessStat(ProcessStat):
     def __init__(self, pname):
@@ -12,43 +13,54 @@ class VrouterProcessStat(ProcessStat):
         (self.group, self.name) = self.get_vrouter_process_info(pname)
 
     def get_vrouter_process_info(self, proc_name):
-        for root, dirs, files in os.walk("/etc/contrail/supervisord_vrouter_files"):
+        vrouter_file = "/etc/contrail/supervisord_vrouter_files"
+        for root, dirs, files in os.walk(vrouter_file):
             for file in files:
                 if file.endswith(".ini"):
-                    filename = '/etc/contrail/supervisord_vrouter_files/' + file
-                    data = StringIO('\n'.join(line.strip() for line in open(filename)))
+                    filename = \
+                        '/etc/contrail/supervisord_vrouter_files/' + file
+                    data = StringIO('\n'.join(line.strip()
+                                    for line in open(filename)))
                     Config = ConfigParser.SafeConfigParser()
                     Config.readfp(data)
                     sections = Config.sections()
                     if not sections[0]:
-                        sys.stderr.write("Section not present in the ini file : " + filename + "\n")
+                        msg = "Section not present in the ini file : "
+                        sys.stderr.write(msg + filename + "\n")
                         continue
                     name = sections[0].split(':')
                     if len(name) < 2:
-                        sys.stderr.write("Incorrect section name in the ini file : " + filename + "\n")
+                        msg = "Incorrect section name in the ini file : "
+                        sys.stderr.write(msg + filename + "\n")
                         continue
                     if name[1] == proc_name:
                         command = Config.get(sections[0], "command")
                         if not command:
-                            sys.stderr.write("Command not present in the ini file : " + filename + "\n")
+                            msg = "Command not present in the ini file : "
+                            sys.stderr.write(msg + filename + "\n")
                             continue
                         args = command.split()
                         if (args[0] == '/usr/bin/contrail-tor-agent'):
                             try:
                                 index = args.index('--config_file')
-                                agent_name = self.get_vrouter_tor_agent_name(args[index + 1])
+                                args_val = args[index + 1]
+                                agent_name = \
+                                    self.get_vrouter_tor_agent_name(args_val)
                                 return (proc_name, agent_name)
                             except Exception, err:
-                                sys.stderr.write("Tor Agent command does not have config file : " + command + "\n")
+                                msg = "Tor Agent command does " + \
+                                      "not have config file : "
+                                sys.stderr.write(msg + command + "\n")
         return ('vrouter_group', socket.gethostname())
-    #end get_vrouter_process_info
+    # end get_vrouter_process_info
 
     # Read agent_name from vrouter-tor-agent conf file
     def get_vrouter_tor_agent_name(self, conf_file):
         tor_agent_name = None
         if conf_file:
             try:
-                data = StringIO('\n'.join(line.strip() for line in open(conf_file)))
+                data = StringIO('\n'.join(line.strip()
+                                for line in open(conf_file)))
                 Config = ConfigParser.SafeConfigParser()
                 Config.readfp(data)
             except Exception, err:
@@ -57,4 +69,4 @@ class VrouterProcessStat(ProcessStat):
                 return tor_agent_name
             tor_agent_name = Config.get("DEFAULT", "agent_name")
         return tor_agent_name
-    #end get_vrouter_tor_agent_name
+    # end get_vrouter_tor_agent_name
