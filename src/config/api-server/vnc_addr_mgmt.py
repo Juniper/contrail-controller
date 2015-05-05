3a3f6561f2f424b3f3413fc6addb8e3aead4635d
Édouard Thuleau <edouard.thuleau@cloudwatt.com>
2015-05-05 18:24:25 +0200

    Authorise IndexAllocator to reserve index outside the allocation list
    The IndexAllocator class does not authorize to reserve an index outside
    allocation pools. But for Subnet, we should be able to request an IP
    in the subnet cidr outside allocation pool.
    
    (cherry picked from commit 1da34668c501a37dc722d0096ff0e02341c8e871)
    Conflicts:
    	src/config/api-server/vnc_addr_mgmt.py
    	src/config/api-server/vnc_cfg_ifmap.py
    	src/config/common/zkclient.py
    
    Change-Id: I1fe97001714d102574142c8c6de445f110dbdb87
    Closes-Bug: #1451701

--- src/config/api-server/vnc_addr_mgmt.py
+++ src/config/api-server/vnc_addr_mgmt.py
@@ -243,7 +243,9 @@ class Subnet(object):
             if alloc_int['start'] <= int(service_node_address) <= alloc_int['end']:
                 exclude.append(service_node_address)
         self._db_conn.subnet_create_allocator(name, alloc_int_list,
-                                              addr_from_start)
+                                              addr_from_start,
+                                              network.first,
+                                              network.size)
 
         # reserve excluded addresses
         for addr in exclude:
