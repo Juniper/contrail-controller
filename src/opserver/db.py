05c825d36904d7153f2154e8959e133dafe5c727
akumari <akumari@juniper.net>
2015-05-07 04:54:25 -0700

    Add commands contrail-db show/purge
    Added CLI: contrail-db
    usage: contrail-db [-h] [--analytics-api-ip ANALYTICS_API_IP]
                       [--analytics-api-port ANALYTICS_API_PORT]
                       [--show | --purge PURGE]
    
    optional arguments:
      -h, --help            show this help message and exit
      --analytics-api-ip ANALYTICS_API_IP
                            IP address of Analytics API Server (default:
                            127.0.0.1)
      --analytics-api-port ANALYTICS_API_PORT
                            Port of Analytics API Server (default: 8081)
      --show                show database usage and purge stats (default: False)
      --purge PURGE         Purge data from contrail-db (default: None)
    Closes-bug: #1403979
    
    Change-Id: I9d9ab352235db1d853ee4681ac8888186a3821ec

---
+++ src/opserver/db.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
+#
+
+import sys
+import json
+import argparse
+import requests
+import urllib2
+from prettytable import PrettyTable
+
+class ContrailDb(object):
+
+    def __init__(self):
+        self._args = None
+    # end __init__
+
+    def parse_args(self):
+        """
+        Eg. python db.py --analytics-api-ip 127.0.0.1
+                          --analytics-api-port 8081
+                          --show
+                          --purge
+        """
+        defaults = {
+            'analytics_api_ip': '127.0.0.1',
+            'analytics_api_port': '8081',
+        }
+
+        parser = argparse.ArgumentParser(
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        parser.set_defaults(**defaults)
+        parser.add_argument("--analytics-api-ip",
+                            help="IP address of Analytics API Server")
+        parser.add_argument("--analytics-api-port",
+                            help="Port of Analytics API Server")
+        group = parser.add_mutually_exclusive_group()
+        group.add_argument("--show", action='store_true',
+                            help="show database usage and purge stats")
+        group.add_argument("--purge", help='Purge data from contrail-db')
+        self._args = parser.parse_args()
+        return 0
+    # end parse_args
+
+    def contrail_db_show(self):
+        uves = ["DatabaseUsageInfo", "DatabasePurgeInfo"]
+        for uve in uves:
+            try:
+                url = "http://%s:%s/analytics/uves/database-nodes?cfilt=%s" \
+                    % (self._args.analytics_api_ip, self._args.analytics_api_port, uve)
+                node_dburls = json.loads(urllib2.urlopen(url).read())
+                for node_dburl in node_dburls:
+                    stats = json.loads(urllib2.urlopen(node_dburl['href']).read())
+                    if "DatabaseUsageInfo" in uve:
+                        db =  stats[uve]['database_usage'][0]
+                    elif "DatabasePurgeInfo" in uve:
+                        db = stats[uve]['stats'][0]
+                    db['hostname'] = node_dburl['name']
+                    x = PrettyTable(db.keys())
+                    x.add_row(db.values())
+                    print x
+            except Exception as e:
+                print "Exception: Unable to fetch db information. %s" % e
+    # end contrail_db_show
+
+    def start_purge(self, params):
+        url = "http://%s:%s/analytics/operation/database-purge" \
+            % (self._args.analytics_api_ip, self._args.analytics_api_port)
+        hdrs = {'Content-type': 'application/json; charset="UTF-8"',
+                'Expect': '202-accepted'}
+        try:
+            response = requests.post(url,
+                       data=json.dumps({'purge_input': params}), headers=hdrs)
+            res = json.loads(response.text)
+            x = PrettyTable(res.keys())
+            x.add_row(res.values())
+            print x
+        except requests.exceptions.ConnectionError, e:
+            print "Connection to %s failed %s" % (url, str(e))
+    # end start_purge
+
+# end class ContrailDb
+
+def main(args_str=None):
+    try:
+        db = ContrailDb()
+        if db.parse_args() != 0:
+             return
+
+        if db._args.show:
+             db.contrail_db_show()
+
+        if db._args.purge:
+            if (db._args.purge.isdigit()):
+                input = int(db._args.purge)
+            else:
+                input = db._args.purge
+            db.start_purge(input)
+    except KeyboardInterrupt:
+        return
+# end main
+
+if __name__ == "__main__":
+    main()
