d64475e315a99b5b0a148332660ff81fd82a9ea1
Shabbir Hussain <shabbirh@juniper.net>
2015-05-11 22:42:13 -0700

    Command line arguments should override the config file options for contrail-nodemgr
    Fix: Create a dictionary of options with their default values,
    override values with conf file values for options present in conf file,
    pass this dictionary to ArgumentParser as default values.
    
    Change-Id: I83188d0e5d7499f81e087964f8df9fc2b2fc5566
    Closes-Bug: #1445426
    (cherry picked from commit 2d3f520f167e4da16553b3a8a0155337cf93aa27)

--- src/nodemgr/main.py
+++ src/nodemgr/main.py
@@ -4,6 +4,35 @@
 
 #!/usr/bin/python
 
+
+doc = """\
+Node manager listens to process state change events and
+other flag value change events to provide advanced service
+management functionality.
+
+Rules files looks like following:
+====================
+{ "Rules": [
+    {"process_name": "contrail-query-engine",
+     "process_state": "PROCESS_STATE_FATAL",
+     "action": "supervisorctl -s http://localhost:9002 """ + \
+    """\stop contrail-analytics-api"},
+    {"process_name": "contrail-query-engine",
+     "process_state": "PROCESS_STATE_STOPPED",
+     "action": "supervisorctl -s http://localhost:9002 """ + \
+    """\stop contrail-analytics-api"},
+    {"processname": "contrail-collector",
+     "process_state": "PROCESS_STATE_RUNNING",
+     "action": "/usr/bin/echo collector is starting >> /tmp/log"},
+    {"flag_name": "test", "flag_value":"true",
+     "action": "/usr/bin/echo flag test is set true >> /tmp/log.1"}
+     ]
+}
+====================
+
+"""
+
+
 from gevent import monkey
 monkey.patch_all()
 import os
@@ -11,6 +40,7 @@ import sys
 import argparse
 import socket
 import gevent
+import ConfigParser
 from nodemgr.analytics_event_manager import AnalyticsEventManager
 from nodemgr.control_event_manager import ControlEventManager
 from nodemgr.config_event_manager import ConfigEventManager
@@ -18,42 +48,87 @@ from nodemgr.vrouter_event_manager import VrouterEventManager
 from nodemgr.database_event_manager import DatabaseEventManager
 
 
-def main(argv=sys.argv):
-    # Parse Arguments
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+def usage():
+    print doc
+    sys.exit(255)
 
+
+def main(args_str=' '.join(sys.argv[1:])):
+    # Parse Arguments
+    node_parser = argparse.ArgumentParser(add_help=False)
+    node_parser.add_argument("--nodetype",
+                             default='contrail-analytics',
+                             help='Type of node which nodemgr is managing')
+    try:
+        args, remaining_argv = node_parser.parse_known_args(args_str.split())
+    except:
+        usage()
+    disc_options = {'server': socket.gethostname(), 'port': 5998}
+    default = {'rules': '',
+               'collectors': [],
+               'hostip': '127.0.0.1',
+               'minimum_diskgb': 256
+              }
+    node_type = args.nodetype
+    if (node_type == 'contrail-analytics'):
+        config_file = '/etc/contrail/contrail-analytics-nodemgr.conf'
+        default['collectors'] = ['127.0.0.1:8086']
+    elif (node_type == 'contrail-config'):
+        config_file = '/etc/contrail/contrail-config-nodemgr.conf'
+    elif (node_type == 'contrail-control'):
+        config_file = '/etc/contrail/contrail-control-nodemgr.conf'
+    elif (node_type == 'contrail-vrouter'):
+        config_file = '/etc/contrail/contrail-vrouter-nodemgr.conf'
+    elif (node_type == 'contrail-database'):
+        config_file = '/etc/contrail/contrail-database-nodemgr.conf'
+    else:
+        sys.stderr.write("Node type" + str(node_type) + "is incorrect" + "\n")
+        return
+    config = ConfigParser.SafeConfigParser()
+    config.read([config_file])
+    if 'DEFAULT' in config.sections():
+        default.update(dict(config.items('DEFAULT')))
+    if 'DISCOVERY' in config.sections():
+        disc_options.update(dict(config.items('DISCOVERY')))
+    disc_options['discovery_server'] = disc_options.pop('server')
+    disc_options['discovery_port'] = disc_options.pop('port')
+    if 'COLLECTOR' in config.sections():
+        try:
+            collector = config.get('COLLECTOR', 'server_list')
+            default['collectors'] = collector.split()
+        except ConfigParser.NoOptionError as e:
+            pass
+    parser = argparse.ArgumentParser(parents=[node_parser],
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    default.update(disc_options)
+    parser.set_defaults(**default)
     parser.add_argument("--rules",
-                        default='',
                         help='Rules file to use for processing events')
-    parser.add_argument("--nodetype",
-                        default='contrail-analytics',
-                        help='Type of node which nodemgr is managing')
     parser.add_argument("--discovery_server",
-                        default=socket.gethostname(),
                         help='IP address of Discovery Server')
-    parser.add_argument("--collectors",
-                        default='',
-                        help='Collector addresses in format' +
-                             'ip1:port1 ip2:port2')
     parser.add_argument("--discovery_port",
                         type=int,
-                        default=5998,
                         help='Port of Discovery Server')
+    parser.add_argument("--collectors",
+                        nargs='+',
+                        help='Collector addresses in format' +
+                             'ip1:port1 ip2:port2')
+    if (node_type == 'contrail-database'):
+        parser.add_argument("--minimum_diskgb",
+                            type=int,
+                            help="Minimum disk space in GB's")
+        parser.add_argument("--hostip",
+                            help="IP address of host")
     try:
-        _args = parser.parse_args()
+        _args = parser.parse_args(remaining_argv)
     except:
         usage()
     rule_file = _args.rules
-    node_type = _args.nodetype
     discovery_server = _args.discovery_server
     sys.stderr.write("Discovery server: " + discovery_server + "\n")
     discovery_port = _args.discovery_port
     sys.stderr.write("Discovery port: " + str(discovery_port) + "\n")
-    if _args.collectors is "":
-        collector_addr = []
-    else:
-        collector_addr = _args.collectors.split()
+    collector_addr = _args.collectors
     sys.stderr.write("Collector address: " + str(collector_addr) + "\n")
     # done parsing arguments
 
@@ -80,9 +155,12 @@ def main(argv=sys.argv):
             rule_file, discovery_server,
             discovery_port, collector_addr)
     elif (node_type == 'contrail-database'):
+        hostip = _args.hostip
+        minimum_diskgb = _args.minimum_diskgb
         prog = DatabaseEventManager(
             rule_file, discovery_server,
-            discovery_port, collector_addr)
+            discovery_port, collector_addr,
+            hostip, minimum_diskgb)
     else:
         sys.stderr.write("Node type" + str(node_type) + "is incorrect" + "\n")
         return
