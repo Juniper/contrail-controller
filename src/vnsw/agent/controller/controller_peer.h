89405e4d59d6818741f5523014c1b8f9e721bcf3
Manish <manishs@juniper.net>
2015-06-11 15:24:04 +0530

    Tor agent didnt unsubscribe for multicast route.
    Problem:
    When OVSDB connection went off route was deleted from TOR agent
    however it didnt send the unsubscription to controller. Now this
    resulted in multiple TSN in olist of multicast route.
    This was happening because agent was deriving prefix using tunnel
    NH in multicast route(of tor agent). The destination of tunnel
    NH is used to reate the EVPN prefix. Since at time of deletion
    path was not present tunnel NH used to be NULL and in absence of information
    agent skipped sending unsubscribe.
    
    Solution:
    Store destination and source of tunnel NH in state and use it during deletion.
    
    Closes-bug: 1463799
    (cherry picked from commit 38b64f4c186614ba791905deb6215e3db3235ef7)
    
    Conflicts:
    	src/vnsw/agent/controller/controller_export.cc
    	src/vnsw/agent/controller/controller_peer.cc
    	src/vnsw/agent/controller/controller_peer.h
    
    Change-Id: Iaa0e5d3aaf276c075b9f31eb6ce5e6d7d0119bc9

--- src/vnsw/agent/controller/controller_peer.h
+++ src/vnsw/agent/controller/controller_peer.h
@@ -78,7 +78,9 @@ public:
                                            std::string vn,
                                            uint32_t mpls_label,
                                            uint32_t tunnel_bmap,
-                                           const SecurityGroupList *sg_list);
+                                           const SecurityGroupList *sg_list,
+                                           const std::string &destination,
+                                           const std::string &source);
     static bool ControllerSendMcastRouteAdd(AgentXmppChannel *peer,
                                             AgentRoute *route);
     //Deletes to control node
@@ -94,6 +96,8 @@ public:
                                               AgentRoute *route,
                                               std::string vn,
                                               uint32_t mpls_label,
+                                              const std::string &destination,
+                                              const std::string &source,
                                               uint32_t tunnel_bmap);
     static bool ControllerSendMcastRouteDelete(AgentXmppChannel *peer,
                                                AgentRoute *route);
@@ -130,6 +134,8 @@ public:
                                        const SecurityGroupList *sg_list,
                                        uint32_t mpls_label,
                                        uint32_t tunnel_bmap,
+                                       const std::string &destination,
+                                       const std::string &source,
                                        bool associate);
     bool ControllerSendMcastRouteCommon(AgentRoute *route,
                                         bool associate);
@@ -167,6 +173,8 @@ private:
                                   const SecurityGroupList *sg_list,
                                   uint32_t label,
                                   uint32_t tunnel_bmap,
+                                  const std::string &destination,
+                                  const std::string &source,
                                   bool associate);
     bool BuildEvpnMulticastMessage(autogen::EnetItemType &item,
                                    std::stringstream &node_id,
