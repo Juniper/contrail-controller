8ba08408737fbc35caae7ab1376a5e258857ee0f
Deepinder Setia <dsetia@juniper.net>
2015-06-11 11:17:27 -0700

    bug 1459473: Add off-line command to load-balance
    Change-Id: I901f58dd1c95b6f89527c863e51083932246ae40

---
+++ src/discovery/tests/test_publish.py
@@ -0,0 +1,49 @@
+import sys
+import time
+sys.path.append("../config/common/tests")
+from test_utils import *
+import fixtures
+import testtools
+import test_common
+import test_discovery
+
+import discoveryclient.client as client
+
+def info_callback(info):
+    # print 'In subscribe callback handler'
+    # print '%s' % (info)
+    pass
+
+
+class DiscoveryServerTestCase(test_discovery.TestCase, fixtures.TestWithFixtures):
+    def test_publish_json(self):
+        service_type = 'foobar'
+        payload = { '%s' % service_type: { "ip-addr" : "1.1.1.1", "port"    : "1234" } }
+        puburl = '/publish/test_discovery'
+        (code, msg) = self._http_post(puburl, json.dumps(payload))
+        self.assertEqual(code, 200)
+
+        time.sleep(1)
+        (code, msg) = self._http_get('/services.json')
+        self.assertEqual(code, 200)
+
+        response = json.loads(msg)
+        self.assertEqual(len(response['services']), 1)
+        self.assertEqual(response['services'][0]['service_type'], service_type)
+
+    def test_publish_xml(self):
+        service_type = 'foobar2'
+        payload = '<%s><ip-addr>2.2.2.2</ip-addr><port>4321</port></%s>' \
+            % (service_type, service_type)
+        puburl = '/publish/test_discovery'
+        self._set_content_type('application/xml')
+        (code, msg) = self._http_post(puburl, payload)
+        self.assertEqual(code, 200)
+
+        time.sleep(1)
+        (code, msg) = self._http_get('/services.json')
+        self.assertEqual(code, 200)
+
+        response = json.loads(msg)
+        self.assertEqual(len(response['services']), 1)
+        self.assertEqual(response['services'][0]['service_type'], service_type)
