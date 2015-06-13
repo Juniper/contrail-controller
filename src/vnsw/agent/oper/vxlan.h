d44fe9a71850555a2dedbc4c74996bd7b889e1d3
Praveen K V <kvpraveen@juniper.net>
2015-06-13 14:39:24 +0530

    Handle multiple-vn with same vxlan-id
    Ideally, the vxlan-id is different for every vlan. However, either due
    to invalid configuration or transition of vxlan-mode from "automatic" to
    "configured", more than one VN can have same vxlan.
    
    Added a ConfigTree in VxLanTable. ConfigTree contains <vxlan-id + vn> as
    key. If more than one VN has same vxlan, the first entry added into the
    tree becomes "active" entry. Other entries become "inactive" entries.
    When first entry is deleted, the first-entry in ConfigTree after
    deletion becomes new "active" entry.
    
    Closes-Bug: #1462799
    
    Change-Id: Iadee455d6ae88408346630645e2627eb303faaf3

--- src/vnsw/agent/oper/vxlan.h
+++ src/vnsw/agent/oper/vxlan.h
@@ -28,11 +28,6 @@ public:
     uint32_t vxlan_id() const {return vxlan_id_;};
     const NextHop *nexthop() const {return nh_.get();};
 
-    static void Create(uint32_t vxlan_id, const std::string &vrf_name,
-                       bool flood_unknown_unicast);
-    // Delete vxlan_id vxlan_id entry
-    static void Delete(uint32_t vxlan_id);
-
     uint32_t GetRefCount() const {
         return AgentRefCount<VxLanId>::GetRefCount();
     }
@@ -78,6 +73,57 @@ private:
 /////////////////////////////////////////////////////////////////////////////
 class VxLanTable : public AgentDBTable {
 public:
+    //////////////////////////////////////////////////////////////////////////
+    // VxLan entries are created from VN.
+    // Ideally, each VN should have a different VxLan configured. However,
+    // there can be transition where more than one VN can have same VxLan.
+    // A config tree is maintained to handle the transition cases.
+    //
+    // The config tree is keyed with <vxlan-id, vn-name> and data is
+    // <vrf-name, flood_unknown_unicast, active>
+    //
+    // For a given vxlan, there can be more than one ConfigEntries. The active
+    // flag speicified, which config-entry is master for the vxlan.
+    //
+    // When first config entry is added for a vxlan, it becomes "active" and
+    // also creates a vxlan entry.
+    //
+    // When subsequent config entry are added, they become "inactive" and are
+    // added to config-tree. There is no change done to vxlan entry
+    //
+    // If an "active" config-entry is deleted, we will pick the first entry
+    // for given vxlan and make it "active"
+    //////////////////////////////////////////////////////////////////////////
+    struct ConfigKey {
+        ConfigKey(uint32_t vxlan_id, const boost::uuids::uuid &vn) :
+            vxlan_id_(vxlan_id), vn_(vn) {
+        }
+        ConfigKey() : vxlan_id_(0), vn_() { }
+
+        bool operator()(const ConfigKey &lhs, const ConfigKey &rhs) {
+            if (lhs.vxlan_id_ != rhs.vxlan_id_)
+                return lhs.vxlan_id_ < rhs.vxlan_id_;
+
+            return lhs.vn_ < rhs.vn_;
+        }
+
+        uint32_t vxlan_id_;
+        boost::uuids::uuid vn_;
+    };
+
+    struct ConfigEntry {
+        ConfigEntry(const std::string &vrf, bool flood_unknown_unicast,
+                    bool active) :
+            vrf_(vrf), flood_unknown_unicast_(flood_unknown_unicast),
+            active_(active) {
+        }
+
+        std::string vrf_;
+        bool flood_unknown_unicast_;
+        bool active_;
+    };
+    typedef std::map<ConfigKey, ConfigEntry, ConfigKey> ConfigTree;
+
     static const uint32_t kInvalidvxlan_id = 0;
     VxLanTable(DB *db, const std::string &name) : AgentDBTable(db, name) { };
     virtual ~VxLanTable() { };
@@ -95,11 +141,20 @@ public:
 
     void Process(DBRequest &req);
 
+    void Create(uint32_t vxlan_id, const std::string &vrf_name,
+                bool flood_unknown_unicast);
+    void Delete(uint32_t vxlan_id);
+
     VxLanId *Find(uint32_t vxlan_id);
+    VxLanId *Locate(uint32_t vxlan_id, const boost::uuids::uuid &vn,
+                    const std::string &vrf, bool flood_unknown_unicast);
+    VxLanId *Delete(uint32_t vxlan_id, const boost::uuids::uuid &vn);
+    const ConfigTree &config_tree() const { return config_tree_; }
     static DBTableBase *CreateTable(DB *db, const std::string &name);
 
 private:
     bool ChangeHandler(VxLanId *vxlan_id, const DBRequest *req);
+    ConfigTree config_tree_;
     DBTableBase::ListenerId vn_table_listener_id_;
     DISALLOW_COPY_AND_ASSIGN(VxLanTable);
 };
