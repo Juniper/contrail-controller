41ea0c08f8be1a83bc127e939dc4e5c2e9a72467
Prabhjot Singh Sethi <prabhjot@juniper.net>
2015-06-13 23:00:51 +0530

    test_agent_route_export enchancements
    - Allow utility to use same ip for all exported routes
    - Provide option to withdraw routes without restart.
    
    Closes-Bug: 1464889
    Change-Id: I32aed2c680f536c2f04cedfc1dd3846a65e6b75b

--- src/vnsw/agent/ovs_tor_agent/ovsdb_client/test/test_ovsdb_sandesh.cc
+++ src/vnsw/agent/ovs_tor_agent/ovsdb_client/test/test_ovsdb_sandesh.cc
@@ -15,10 +15,12 @@ OvsPeer *test_local_peer = NULL;
 class TestOvsdbRouteExportTask : public Task {
 public:
     TestOvsdbRouteExportTask(string resp_ctx, string vn_uuid, string mac,
-                             string dest_ip, uint16_t count)
+                             string dest_ip, uint16_t count, bool deleted,
+                             bool use_same_dest)
         : Task((TaskScheduler::GetInstance()->GetTaskId("db::DBTable")), 0),
         resp_(new TestOvsdbRouteExportResp()), resp_data_(resp_ctx),
-        vn_uuid_(vn_uuid), mac_(mac), dest_ip_(dest_ip), count_(count) {
+        vn_uuid_(vn_uuid), mac_(mac), dest_ip_(dest_ip), count_(count),
+        deleted_(deleted), same_dest_(use_same_dest) {
     }
     virtual ~TestOvsdbRouteExportTask() {}
     virtual bool Run() {
@@ -27,8 +29,8 @@ public:
             resp_->set_error("Route Peer Not Available");
         } else {
             VnKey key(StringToUuid(vn_uuid_));
-            VnEntry *vn =
-                static_cast<VnEntry *>(Agent::GetInstance()->vn_table()->FindActiveEntry(&key));
+            VnEntry *vn = static_cast<VnEntry *>(
+                    Agent::GetInstance()->vn_table()->FindActiveEntry(&key));
             if (vn == NULL) {
                 resp_->set_success(false);
                 resp_->set_error("VN not Available");
@@ -37,8 +39,16 @@ public:
                 Ip4Address dest = Ip4Address::from_string(dest_ip_, err);
                 MacAddress mac_addr(mac_);
                 for (uint16_t i = 0; i < count_; i++) {
-                    test_local_peer->AddOvsRoute(vn->GetVrf(), vn->GetVxLanId(),
-                                                 vn->GetName(), mac_addr, dest);
+                    if (deleted_) {
+                        test_local_peer->DeleteOvsRoute(vn->GetVrf(),
+                                                        vn->GetVxLanId(),
+                                                        mac_addr);
+                    } else {
+                        test_local_peer->AddOvsRoute(vn->GetVrf(),
+                                                     vn->GetVxLanId(),
+                                                     vn->GetName(), mac_addr,
+                                                     dest);
+                    }
                     // increment mac
                     if (mac_addr[5] != 0xff) {
                         mac_addr[5]++;
@@ -67,10 +77,12 @@ public:
                         }
                     }
 
-                    unsigned long dest_long = dest.to_ulong();
-                    dest_long++;
-                    // increment IP
-                    dest = Ip4Address(dest_long);
+                    if (!same_dest_) {
+                        unsigned long dest_long = dest.to_ulong();
+                        dest_long++;
+                        // increment IP
+                        dest = Ip4Address(dest_long);
+                    }
                 }
                 resp_->set_success(true);
                 resp_->set_error("Route Export Done");
@@ -92,13 +104,25 @@ private:
     string mac_;
     string dest_ip_;
     uint16_t count_;
+    bool deleted_;
+    bool same_dest_;
     DISALLOW_COPY_AND_ASSIGN(TestOvsdbRouteExportTask);
 };
 
 void TestOvsdbRouteExportReq::HandleRequest() const {
     TestOvsdbRouteExportTask *task =
         new TestOvsdbRouteExportTask(context(), get_vn_uuid(), get_mac_addr(),
-                                     get_dest_ip(), get_number_of_routes());
+                                     get_dest_ip(), get_number_of_routes(),
+                                     false, get_use_same_dest_ip());
+    TaskScheduler *scheduler = TaskScheduler::GetInstance();
+    scheduler->Enqueue(task);
+}
+
+void TestOvsdbRouteWithdrawReq::HandleRequest() const {
+    TestOvsdbRouteExportTask *task =
+        new TestOvsdbRouteExportTask(context(), get_vn_uuid(), get_mac_addr(),
+                                     get_dest_ip(), get_number_of_routes(),
+                                     true, false);
     TaskScheduler *scheduler = TaskScheduler::GetInstance();
     scheduler->Enqueue(task);
 }
