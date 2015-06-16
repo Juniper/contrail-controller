717a2859bd937ff71d3fee0b22549e751c5e6b0a
Chandan Mishra <cdmishra@juniper.net>
2015-06-15 18:05:49 -0700

    This commit makes sure that nodemgr handles all pending events before going to sleep. Current nodemgr design is such that it sleeps for 1 second between processing of each event. This fix takes care of that.
    Overall it will make processing of events by nodemgr more efficient.
    
    Change-Id: I0566d605b0b83d01b0965c358f786d49a9104ae9
    Closes-Bug: 1461119

--- src/nodemgr/common/event_listener_protocol_nodemgr.py
+++ src/nodemgr/common/event_listener_protocol_nodemgr.py
@@ -12,14 +12,14 @@ class EventListenerProtocolNodeMgr(childutils.EventListenerProtocol):
     def wait(self, stdin=sys.stdin, stdout=sys.stdout):
         self.ready(stdout)
         while 1:
-            gevent.sleep(1)
-            if select.select([sys.stdin, ], [], [], 1)[0]:
+            if select.select([sys.stdin, ], [], [], 0)[0]:
                 line = stdin.readline()
                 if line is not None:
                     sys.stderr.write("wokeup and found a line\n")
                     break
                 else:
                     sys.stderr.write("wokeup from select just like that\n")
+            gevent.sleep(1)
         headers = childutils.get_headers(line)
         payload = stdin.read(int(headers['len']))
         return headers, payload
