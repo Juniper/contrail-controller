c9fda95d3c57138e43eaf998247396b06f2e1eb3
Sylvain Baubeau <sbaubeau@redhat.com>
2015-06-24 14:34:27 +0200

    Fix 'self' usage in static methods
    Change-Id: Ib705b342d90753acd0e05002c804f2c460fffcc5
    Closes-Bug: #1468367

--- src/config/svc-monitor/svc_monitor/config_db.py
+++ src/config/svc-monitor/svc_monitor/config_db.py
@@ -653,7 +653,7 @@ class ProjectSM(DBBase):
         if uuid not in cls._dict:
             return
         obj = cls._dict[uuid]
-        self.update_multiple_refs('service_instance', {})
+        obj.update_multiple_refs('service_instance', {})
         del cls._dict[uuid]
     # end delete
 # end ProjectSM
@@ -846,7 +846,7 @@ class LogicalRouterSM(DBBase):
         cls._manager.snat_agent.delete_snat_instance(obj)
         obj.update_single_ref('service_instance', {})
         obj.update_single_ref('virtual_network', {})
-        self.update_multiple_refs('virtual_machine_interface', {})
+        obj.update_multiple_refs('virtual_machine_interface', {})
         del cls._dict[uuid]
     # end delete
 # end LogicalRouterSM
