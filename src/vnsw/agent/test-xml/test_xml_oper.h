62c4e1d35b67be78332d66cd35700d230eb31e03
Naveen N <naveenn@juniper.net>
2015-06-25 14:33:57 +0530

    * Re-evaluate layer2 flow for RPF NH change
    Layer 2 flows were not added in layer 3 route dependent list
    when rpf nexthop was picked from layer 3 route, hence when
    nexthop is changed flow were not getting synced
    Fix: Store prefix len of the route used for rpf calculation
    and add the flow to the route tree,  resulting in re-evaluation
    of rpf NH.
    Unit test case for same.
    Closes-bug:#1403602
    
    Change-Id: I9b3698d2c67cab3dd89ed192e7dfdc7a4676558c

--- src/vnsw/agent/test-xml/test_xml_oper.h
+++ src/vnsw/agent/test-xml/test_xml_oper.h
@@ -411,4 +411,52 @@ private:
     uint16_t intf_uuid_;
 };
 
+class AgentUtXmlL3Route : public AgentUtXmlNode {
+public:
+    AgentUtXmlL3Route(const std::string &name, const boost::uuids::uuid &uuid,
+                      const pugi::xml_node &node,
+                      AgentUtXmlTestCase *test_case);
+    ~AgentUtXmlL3Route();
+
+    virtual bool ReadXml();
+    virtual bool ToXml(pugi::xml_node *parent);
+    virtual std::string NodeType();
+    virtual void ToString(std::string *str);
+    virtual bool Run();
+
+private:
+    std::string src_ip_;
+    std::string vrf_;
+    std::string vn_name_;
+    std::string ip_;
+    uint16_t vxlan_id_;
+    std::string vn_;
+    std::string tunnel_dest_;
+    std::string tunnel_type_;
+    uint16_t sg_id_;
+    uint16_t label_;
+    uint16_t plen_;
+};
+
+
+class AgentUtXmlL3RouteValidate : public AgentUtXmlValidationNode {
+public:
+    AgentUtXmlL3RouteValidate(const std::string &name,
+                              const pugi::xml_node &node);
+    virtual ~AgentUtXmlL3RouteValidate();
+
+    virtual bool ReadXml();
+    virtual bool Validate();
+    virtual const std::string ToString();
+private:
+    std::string src_ip_;
+    std::string vrf_;
+    uint16_t vxlan_id_;
+    IpAddress ip_;
+    std::string vn_;
+    std::string nh_type_;
+    std::string tunnel_dest_;
+    std::string tunnel_type_;
+    uint16_t intf_uuid_;
+};
 #endif //vnsw_agent_test_xml_test_xml_oper_h
