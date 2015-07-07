aeef933ba987ac922b5dd10c9a836132c4837dcc
Nischal Sheth <nsheth@juniper.net>
2015-07-06 17:43:26 -0700

    Add logs for xmpp peer create and delete
    Change-Id: Ic7b368038b822bc34a252261240703c3c1097e23

--- src/bgp/bgp_xmpp_channel.cc
+++ src/bgp/bgp_xmpp_channel.cc
@@ -483,6 +483,8 @@ BgpXmppChannel::BgpXmppChannel(XmppChannel *channel, BgpServer *bgp_server,
       lb_mgr_(new LabelBlockManager()) {
     channel_->RegisterReceive(peer_id_,
          boost::bind(&BgpXmppChannel::ReceiveUpdate, this, _1));
+    BGP_LOG_PEER(Event, peer_.get(), SandeshLevel::SYS_INFO, BGP_LOG_FLAG_ALL,
+        BGP_PEER_DIR_NA, "Created");
 }
 
 BgpXmppChannel::~BgpXmppChannel() {
@@ -496,6 +498,8 @@ BgpXmppChannel::~BgpXmppChannel() {
         manager_->decrement_closing_count();
     STLDeleteElements(&defer_q_);
     assert(peer_->IsDeleted());
+    BGP_LOG_PEER(Event, peer_.get(), SandeshLevel::SYS_INFO, BGP_LOG_FLAG_ALL,
+        BGP_PEER_DIR_NA, "Deleted");
     channel_->UnRegisterReceive(peer_id_);
 }
 
