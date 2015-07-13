2ad42f2923b1b2f5f526ade5cade15bd0593a81a
Prakash Bailkeri <prakashmb@juniper.net>
2015-07-13 01:13:21 -0700

    Subnet uuid passed in network-ipam-refs by WebUI/any vnc api Client can be invalid.
    Reject subnet create(network-update) operation in such cases.
    
    Change-Id: Iee97cd82c3a94fa183d713af19a0a36cbcc66dd9
    Closes-bug: #1473857

--- src/config/api-server/vnc_addr_mgmt.py
+++ src/config/api-server/vnc_addr_mgmt.py
@@ -674,6 +674,16 @@ class AddrMgmt(object):
                 subnet_name = subnet_dict['ip_prefix'] + '/' + str(
                     subnet_dict['ip_prefix_len'])
 
+                # check subnet-uuid
+                ipam_cfg_subnet_uuid = ipam_subnet.get('subnet_uuid', None)
+                try:
+                    if ipam_cfg_subnet_uuid:
+                        subnet_uuid = uuid.UUID(ipam_cfg_subnet_uuid)
+                except ValueError:
+                    err_msg = "Invalid subnet-uuid %s in subnet:%s" \
+                        %(ipam_cfg_subnet_uuid, subnet_name)
+                    return False, err_msg
+
                 # check allocation-pool
                 alloc_pools = ipam_subnet.get('allocation_pools', None)
                 for pool in alloc_pools or []:
