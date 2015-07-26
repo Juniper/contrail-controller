5fb301aa6e968201408f35b615772be77861b80b
Deepinder Setia <dsetia@juniper.net>
2015-07-26 15:22:56 -0700

    Related-Bug: #1459473 - add unit test cases
    Change-Id: I15968ffc1c2d7959f77e5008a111bfc6ae202634

---
+++ src/discovery/tests/test_publish.py
@@ -0,0 +1,150 @@
+import sys
+import time
+sys.path.append("../config/common/tests")
+from test_utils import *
+import fixtures
+import testtools
+import test_common
+import test_discovery
+
+import discoveryclient.client as client
+
+def info_callback(info):
+    # print 'In subscribe callback handler'
+    # print '%s' % (info)
+    pass
+
+
+class DiscoveryServerTestCase(test_discovery.TestCase, fixtures.TestWithFixtures):
+    def test_publish_json(self):
+        service_type = 'foobar'
+        payload = {
+            '%s' % service_type: { "ip-addr" : "1.1.1.1", "port"    : "1234" }
+        }
+        puburl = '/publish/test_discovery'
+        (code, msg) = self._http_post(puburl, json.dumps(payload))
+        self.assertEqual(code, 200)
+
+        time.sleep(1)
+        (code, msg) = self._http_get('/services.json')
+        self.assertEqual(code, 200)
+
+        response = json.loads(msg)
+        self.assertEqual(len(response['services']), 1)
+        self.assertEqual(response['services'][0]['service_type'], service_type)
+
+    def test_publish_json_new(self):
+        service_type = 'foobar'
+        payload = {
+            '%s' % service_type: { "ip-addr" : "1.1.1.1", "port"    : "1234" },
+            'service-type' : '%s' % service_type,
+        }
+        puburl = '/publish/test_discovery'
+        (code, msg) = self._http_post(puburl, json.dumps(payload))
+        self.assertEqual(code, 200)
+
+        time.sleep(1)
+        (code, msg) = self._http_get('/services.json')
+        self.assertEqual(code, 200)
+
+        response = json.loads(msg)
+        self.assertEqual(len(response['services']), 1)
+        self.assertEqual(response['services'][0]['service_type'], service_type)
+
+    def test_publish_xml(self):
+        service_type = 'foobar2'
+        payload = '\
+          <%s>\
+            <ip-addr>2.2.2.2</ip-addr>\
+            <port>4321</port>\
+          </%s>\
+          ' % (service_type, service_type)
+        puburl = '/publish/test_discovery'
+        self._set_content_type('application/xml')
+        (code, msg) = self._http_post(puburl, payload)
+        self.assertEqual(code, 200)
+
+        time.sleep(1)
+        (code, msg) = self._http_get('/services.json')
+        self.assertEqual(code, 200)
+
+        response = json.loads(msg)
+        self.assertEqual(len(response['services']), 1)
+        self.assertEqual(response['services'][0]['service_type'], service_type)
+
+    def test_publish_xml_new(self):
+        service_type = 'foobar2'
+        payload = '\
+          <publish>\
+            <%s>\
+              <ip-addr>2.2.2.2</ip-addr>\
+              <port>4321</port>\
+            </%s>\
+            <service-type>%s</service-type>\
+          </publish>\
+          ' % (service_type, service_type, service_type)
+        puburl = '/publish/test_discovery'
+        self._set_content_type('application/xml')
+        (code, msg) = self._http_post(puburl, payload)
+        self.assertEqual(code, 200)
+
+        time.sleep(1)
+        (code, msg) = self._http_get('/services.json')
+        self.assertEqual(code, 200)
+
+        response = json.loads(msg)
+        self.assertEqual(len(response['services']), 1)
+        self.assertEqual(response['services'][0]['service_type'], service_type)
+
+    def test_publish_ip_addr(self):
+        service_type = 'foobar'
+        my_ip_addr = 'dummy-string'
+        payload = {
+            '%s' % service_type: { "ip-addr" : "1.1.1.1", "port"    : "1234" },
+            'service-type' : '%s' % service_type,
+            'remote-addr'  : '%s' % my_ip_addr,
+        }
+        puburl = '/publish/test_discovery'
+        (code, msg) = self._http_post(puburl, json.dumps(payload))
+        self.assertEqual(code, 200)
+
+        time.sleep(1)
+        (code, msg) = self._http_get('/services.json')
+        self.assertEqual(code, 200)
+
+        response = json.loads(msg)
+        self.assertEqual(len(response['services']), 1)
+
+        entry = response['services'][0]
+        self.assertEqual(entry['remote'], my_ip_addr)
+
+    def test_publish_admin_state(self):
+        service_type = 'foobar'
+        payload = {
+            '%s' % service_type: { "ip-addr" : "1.1.1.1", "port"    : "1234" },
+            'service-type' : '%s' % service_type,
+        }
+        puburl = '/publish/test_discovery'
+        (code, msg) = self._http_post(puburl, json.dumps(payload))
+        self.assertEqual(code, 200)
+
+        (code, msg) = self._http_get('/services.json')
+        self.assertEqual(code, 200)
+        response = json.loads(msg)
+        self.assertEqual(len(response['services']), 1)
+
+        # service is up by default
+        entry = response['services'][0]
+        self.assertEqual(entry['admin_state'], 'up')
+
+        payload['admin-state'] = 'down'
+        (code, msg) = self._http_post(puburl, json.dumps(payload))
+        self.assertEqual(code, 200)
+
+        (code, msg) = self._http_get('/services.json')
+        self.assertEqual(code, 200)
+        response = json.loads(msg)
+        self.assertEqual(len(response['services']), 1)
+
+        entry = response['services'][0]
+        self.assertEqual(entry['admin_state'], 'down')
