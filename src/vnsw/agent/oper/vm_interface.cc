573e0b817b3e9bca82adad4eaa381da8d54c6841
Ashok Singh <ashoksr@juniper.net>
2015-07-30 03:53:10 -0700

    Send VM interface active and ipv4_active status in VMI UVE
    Uve code will consider interface as active when any of ipv4, ipv6 or l2_active flags are true. VN UVE will send VMI
     list as long as VMI is associated with VN (regardless of VMI active status) Remove unused API IsL3Active()
    Updated UT
    
    Closes-Bug: #1470067
    (cherry picked from commit 2fc30b230828853c6137d02806694da7c18642a5)
    
    Conflicts:
    
    	src/vnsw/agent/uve/interface.sandesh
    
    Change-Id: Idf12fdb201599940a212905a4d272c9db064d394

--- src/vnsw/agent/oper/vm_interface.cc
+++ src/vnsw/agent/oper/vm_interface.cc
@@ -2023,6 +2023,9 @@ bool VmInterface::IsActive()  const {
     if (os_index_ == kInvalidIndex)
         return false;
 
+    if (os_oper_state_ == false)
+        return false;
+
     return mac_set_;
 }
 
@@ -2039,9 +2042,6 @@ bool VmInterface::IsIpv4Active() const {
         return false;
     }
 
-    if (os_oper_state_ == false)
-        return false;
-
     return IsActive();
 }
 
@@ -2050,20 +2050,6 @@ bool VmInterface::IsIpv6Active() const {
         return false;
     }
 
-    if (os_oper_state_ == false)
-        return false;
-
-    return IsActive();
-}
-
-bool VmInterface::IsL3Active() const {
-    if (!layer3_forwarding() || (ip6_addr_.is_unspecified())) {
-        return false;
-    }
-
-    if (os_oper_state_ == false)
-        return false;
-
     return IsActive();
 }
 
@@ -2071,9 +2057,6 @@ bool VmInterface::IsL2Active() const {
     if (!bridging()) {
         return false;
     }
-
-    if (os_oper_state_ == false)
-        return false;
 
     return IsActive();
 }
