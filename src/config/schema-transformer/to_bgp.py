e9fda7d318ab0b9076da3d3c94f24bbe83063974
Sachin Bansal <sachin@contrailsystems.com>
2015-08-11 15:59:21 -0700

    Remove static routes if a VN is deleted with exising ref to route table
    If we received networn delete before its link to route table is deleted, we were
    not cleaning up corresponding static routes. With this change, we take care of
    it. Also add a unit test case for it.
    
    Change-Id: Iafbb808a0a12e0523bc79a9aa46adf15794f9ceb
    Closes-Bug: 1483456
    (cherry picked from commit 6db79ac72c901dbec8e71196dd9afd20bb6eb4ac)

--- src/config/schema-transformer/to_bgp.py
+++ src/config/schema-transformer/to_bgp.py
@@ -318,6 +318,9 @@ class VirtualNetworkST(DBBaseST):
                 if name in policy.analyzer_vn_set:
                     analyzer_vn_set |= policy.networks_back_ref
                     policy.analyzer_vn_set.discard(name)
+
+            vn.route_table_refs = set()
+            vn.update_route_table()
             del cls._dict[name]
             vn.uve_send(deleted=True)
         return analyzer_vn_set
