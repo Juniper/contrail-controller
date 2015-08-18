27924675ed731aa45847f69f26bd358c3764f567
Sundaresan Rajangam <srajanga@juniper.net>
2015-08-18 06:13:47 -0700

    Unit test for sFlow Parser
    - Code refactoring
      * Removed class SFlowListener
      * SFlowCollector derived from UdpServer
    - Code cleanup and error handling in SFlowParser
    - Unit test for SFlowParser
    
    Change-Id: I44535bc714c66bbeffc72a9b86a6d70635c2f569
    Partial-Bug: #1420680
    (cherry picked from commit 4f3f846ed677a5210f1160f3e3d97eece12d5748)

--- src/analytics/sflow.cc
+++ src/analytics/sflow.cc
@@ -2,26 +2,17 @@
  * Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
  */
 
+#include "base/string_util.h"
 #include "analytics/sflow.h"
 
-#include <iomanip>
 
-#include "base/string_util.h"
-
-std::ostream &operator<<(std::ostream &out, const SFlowMacaddress &sflow_mac) {
-    std::stringstream mac_str;
-    for (int i = 0; i < 6; ++i) {
-        mac_str << std::setfill('0') << std::setw(2) << std::hex <<
-                int(sflow_mac.addr[i]) << (i < 5 ? ":":"");
-    }
-    out << mac_str.str();
-    return out;
-}
-
-std::ostream& operator<<(std::ostream& out,
-                         const SFlowIpaddress &sflow_ipaddr) {
-    out << sflow_ipaddr.ToString();
-    return out;
+bool SFlowHeader::operator==(const SFlowHeader& rhs) const {
+    return (version == rhs.version &&
+            agent_ip_address == rhs.agent_ip_address &&
+            agent_subid == rhs.agent_subid &&
+            seqno == rhs.seqno &&
+            uptime == rhs.uptime &&
+            nsamples == rhs.nsamples);
 }
 
 std::ostream &operator<<(std::ostream &out, 
@@ -35,6 +26,47 @@ std::ostream &operator<<(std::ostream &out,
     out << "Uptime: " << sflow_header.uptime << std::endl;
     out << "Num samples: " << sflow_header.nsamples << std::endl;
     return out;
+}
+
+bool SFlowFlowSample::operator==(const SFlowFlowSample& rhs) const {
+    if (!(seqno == rhs.seqno &&
+          sourceid_type == rhs.sourceid_type &&
+          sourceid_index == rhs.sourceid_index &&
+          sample_rate == rhs.sample_rate &&
+          sample_pool == rhs.sample_pool &&
+          drops == rhs.drops &&
+          input_port_format == rhs.input_port_format &&
+          input_port == rhs.input_port &&
+          output_port_format == rhs.output_port_format &&
+          output_port == rhs.output_port &&
+          nflow_records == rhs.nflow_records)) {
+        return false;
+    }
+    if (flow_records.size() != rhs.flow_records.size()) {
+        return false;
+    }
+    boost::ptr_vector<SFlowFlowRecord>::const_iterator it1 =
+        flow_records.begin();
+    boost::ptr_vector<SFlowFlowRecord>::const_iterator it2 =
+        rhs.flow_records.begin();
+    for (; it1 != flow_records.end(); ++it1, ++it2) {
+        if (!(it1->type == it2->type && it1->length == it2->length)) {
+            return false;
+        }
+        switch(it1->type) {
+        case SFLOW_FLOW_HEADER: {
+            SFlowFlowHeader& sflow_header1 = (SFlowFlowHeader&)*it1;
+            SFlowFlowHeader& sflow_header2 = (SFlowFlowHeader&)*it2;
+            if (!(sflow_header1 == sflow_header2)) {
+                return false;
+            }
+            break;
+        }
+        default:
+            assert(0);
+        }
+    }
+    return true;
 }
 
 std::ostream &operator<<(std::ostream &out, 
@@ -52,18 +84,49 @@ std::ostream &operator<<(std::ostream &out,
         << std::endl;
     out << "Output port: " << flow_sample.output_port << std::endl;
     out << "Num Flow records: " << flow_sample.nflow_records << std::endl;
+    boost::ptr_vector<SFlowFlowRecord>::const_iterator it =
+        flow_sample.flow_records.begin();
+    for (; it != flow_sample.flow_records.end(); ++it) {
+        switch(it->type) {
+        case SFLOW_FLOW_HEADER: {
+            SFlowFlowHeader& sflow_header = (SFlowFlowHeader&)*it;
+            out << sflow_header;
+            break;
+        }
+        default:
+            assert(0);
+        }
+    }
     return out;
+}
+
+bool SFlowFlowEthernetData::operator==(const SFlowFlowEthernetData& rhs) const {
+    return (src_mac == rhs.src_mac &&
+            dst_mac == rhs.dst_mac &&
+            vlan_id == rhs.vlan_id &&
+            ether_type == rhs.ether_type);
 }
 
 std::ostream &operator<<(std::ostream &out,
                          const SFlowFlowEthernetData &eth_data) {
     out << "== Ethernet Data ==" << std::endl;
-    out << "Source Mac: " << eth_data.src_mac << std::endl;
-    out << "Destination Mac: " << eth_data.dst_mac << std::endl;
+    out << "Source Mac: " << eth_data.src_mac.ToString() << std::endl;
+    out << "Destination Mac: " << eth_data.dst_mac.ToString() << std::endl;
     out << "Vlan Id: " << eth_data.vlan_id << std::endl;
     out << "Ether type: " << integerToHexString(eth_data.ether_type)
         << std::endl;
     return out;
+}
+
+bool SFlowFlowIpData::operator==(const SFlowFlowIpData& rhs) const {
+    return (length == rhs.length &&
+            protocol == rhs.protocol &&
+            src_ip == rhs.src_ip &&
+            dst_ip == rhs.dst_ip &&
+            src_port == rhs.src_port &&
+            dst_port == rhs.dst_port &&
+            tcp_flags == rhs.tcp_flags &&
+            tos == rhs.tos);
 }
 
 std::ostream &operator<<(std::ostream &out, 
@@ -76,6 +139,33 @@ std::ostream &operator<<(std::ostream &out,
     out << "Destination port: " << ip_data.dst_port << std::endl;
     out << "Tos: " << ip_data.tos << std::endl;
     return out;
+}
+
+bool SFlowFlowHeader::operator==(const SFlowFlowHeader& rhs) const {
+    if (!(protocol == rhs.protocol &&
+          frame_length == rhs.frame_length &&
+          stripped == rhs.stripped &&
+          header_length == rhs.header_length)) {
+        return false;
+    }
+    if (memcmp(header, rhs.header, header_length)) {
+        return false;
+    }
+    if (is_eth_data_set == rhs.is_eth_data_set) {
+        if (!(decoded_eth_data == rhs.decoded_eth_data)) {
+            return false;
+        }
+    } else {
+        return false;
+    }
+    if (is_ip_data_set == rhs.is_ip_data_set) {
+        if (!(decoded_ip_data == rhs.decoded_ip_data)) {
+            return false;
+        }
+    } else {
+        return false;
+    }
+    return true;
 }
 
 std::ostream &operator<<(std::ostream &out,
--- src/analytics/sflow_collector.h
+++ src/analytics/sflow_collector.h
@@ -14,47 +14,29 @@
 
 class SFlowGenerator; 
 
-class SFlowListener : public UdpServer {
+class SFlowCollector : public UdpServer {
 public:
-    explicit SFlowListener(EventManager* evm);
-    virtual ~SFlowListener();
-    virtual void Start(const std::string& ip_address, int port);
+    explicit SFlowCollector(EventManager* evm, DbHandler* db_handler,
+                            const std::string& ip_address, int port);
+    ~SFlowCollector();
+    virtual void Start();
     virtual void Shutdown();
-protected:
-    virtual void ProcessSFlowPacket(boost::asio::const_buffer& buffer,
-                                    size_t length,
-                                    const std::string& generator_ip) = 0;
+
 private:
     void HandleReceive(boost::asio::const_buffer& buffer,
                        boost::asio::ip::udp::endpoint remote_endpoint,
                        size_t bytes_transferred,
                        const boost::system::error_code& error);
-    
-    DISALLOW_COPY_AND_ASSIGN(SFlowListener);
-};
-
-class SFlowCollector : public SFlowListener {
-public:
-    explicit SFlowCollector(EventManager* evm, DbHandler* db_handler,
-                            int port, int generator_inactive_timeout);
-    ~SFlowCollector();
-    virtual void Start();
-    virtual void Shutdown();
-protected:
-    virtual void ProcessSFlowPacket(boost::asio::const_buffer& buffer,
-                                    size_t length,
-                                    const std::string& generator_ip);
-private:
+    void ProcessSFlowPacket(boost::asio::const_buffer& buffer,
+                            size_t length,
+                            const std::string& generator_ip);
     SFlowGenerator* GetSFlowGenerator(const std::string& generator_ip);
-    void SFlowGeneratorCleanupHandler();
 
     typedef boost::ptr_map<std::string, SFlowGenerator> SFlowGeneratorMap;
    
     DbHandler* const db_handler_;
     std::string ip_address_;
     int port_;
-    int generator_inactive_timeout_;
-    Timer* generator_cleanup_timer_;
     SFlowGeneratorMap generator_map_;
     uint64_t num_packets_;
     uint64_t time_first_pkt_seen_;
