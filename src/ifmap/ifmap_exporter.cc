2d9567062f5131a1515820ee9d19f73e9718ce5e
Tapan Karwa <tkarwa@juniper.net>
2015-09-09 09:40:35 -0700

    Delete state only after processing a DELETE IFMapUpdate.
    There can be cases when while processing an UPDATE-IFMapUpdate dequeue event,
    we find that the state is ready to be deleted. We want to wait until we have
    processed the 'DELETE' IFMapUpdate event to finally delete the state.
    
    EG: A node add, link add with that node, link delete, node delete, exporter
    processes node delete before link delete i.e. dependency is still set and
    exporter does not enqueue a DELETE-UPDATE-IFMapUpdate. Now, if we dequeue
    UPDATE-IFMapUpdate, the state is ready to be deleted. But, we want to wait
    until exporter processes the link-delete and triggers a node-change via
    MaybeNotifyOnLinkDelete() to delete the state.
    
    Change-Id: I42ebbd5a9cff824c9732c52a773b6bc9ee36dc1a
    Closes-Bug: #1484784

--- src/ifmap/ifmap_exporter.cc
+++ src/ifmap/ifmap_exporter.cc
@@ -333,6 +333,16 @@ void IFMapExporter::ProcessAdjacentNode(IFMapNode *node, const BitSet &add_set,
     }
 }
 
+void IFMapExporter::DeleteStateIfAppropriate(DBTable *table, DBEntryBase *entry,
+                                             IFMapState *state) {
+    if (state->CanDelete()) {
+        assert(state->advertised().empty());
+        assert(state->interest().empty());
+        entry->ClearState(table, TableListenerId(table));
+        delete state;
+    }
+}
+
 // Propagate changes to all the interested peers.
 //
 // Update order:
@@ -397,10 +407,10 @@ void IFMapExporter::NodeTableExport(DBTablePartBase *partition,
         state->ClearValid();
         if (!state->HasDependents()) {
             // enqueue delete.
+            state->InterestReset(state->interest());
             EnqueueDelete(node, state);
             if (state->update_list().empty()) {
-                entry->ClearState(table, tinfo->id());
-                delete state;
+                DeleteStateIfAppropriate(table, entry, state);
             }
         }
     }
@@ -501,6 +511,8 @@ void IFMapExporter::LinkTableExport(DBTablePartBase *partition,
         IFMapNodeState *s_right = state->right();
         assert((right != NULL) && (s_right != NULL));
         BitSet interest = s_left->interest() & s_right->interest();
+        state->InterestReset(state->interest());
+
         IFMAP_DEBUG(LinkOper, "LinkRemove", left->ToString(), right->ToString(),
             s_left->interest().ToString(), s_right->interest().ToString());
         walker_->LinkRemove(interest);
@@ -511,8 +523,7 @@ void IFMapExporter::LinkTableExport(DBTablePartBase *partition,
         // enqueue update.
         EnqueueDelete(link, state);
         if (state->update_list().empty()) {
-            entry->ClearState(table, tinfo->id());
-            delete state;            
+            DeleteStateIfAppropriate(table, entry, state);
         }
 
         MaybeNotifyOnLinkDelete(left, s_left);
@@ -542,12 +553,11 @@ void IFMapExporter::StateUpdateOnDequeue(IFMapUpdate *update,
     } else {
         state->AdvertisedOr(dequeue_set);
     }
+
     if (update->advertise().empty()) {
         state->Remove(update);
-        if (state->CanDelete()) {
-            assert(state->advertised().empty());
-            db_entry->ClearState(table, TableListenerId(table));
-            delete state;
+        if (update->IsDelete()) {
+            DeleteStateIfAppropriate(table, db_entry, state);
         }
         delete update;
     }
