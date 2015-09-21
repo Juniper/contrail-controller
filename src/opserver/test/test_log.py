ad40fc84b7f173d352a49eb75ee48c90d5009528
Raj Reddy <rajreddy@juniper.net>
2015-09-21 09:58:38 -0700

    Adding tests to cover contrail-logs, contrail-flows and contrail-stats utilities
    Change-Id: I66c2b9578c9957aa1206dbe98915ae9e05366610
    Closes-Bug: #1438434

---
+++ src/opserver/test/test_log.py
@@ -0,0 +1,145 @@
+import sys
+import json
+import gevent
+from gevent import monkey
+monkey.patch_all()
+
+import unittest
+from flexmock import flexmock
+
+from opserver.log import LogQuerier
+from opserver.opserver_util import OpServerUtils
+
+test_num = 0
+query_dict = {}
+result_1 = [
+{u'Category': None, u'NodeType': u'Config', u'Level': 2147483647, u'InstanceId': u'0', u'Messagetype': u'UveVirtualNetworkConfigTrace', u'Source': u'a6s45', u'SequenceNum': 6867683, u'MessageTS': 1442429588898861, u'Xmlmessage': u'<UveVirtualNetworkConfigTrace type="sandesh"><data type="struct" identifier="1"><UveVirtualNetworkConfig><name type="string" identifier="1" key="ObjectVNTable">default-domain:demo:svc-vn-left</name><connected_networks type="list" identifier="4" aggtype="union"><list type="string" size="0" /></connected_networks><partially_connected_networks type="list" identifier="5" aggtype="union"><list type="string" size="0" /></partially_connected_networks><routing_instance_list type="list" identifier="6" aggtype="union"><list type="string" size="1"><element>svc-vn-left</element></list></routing_instance_list><total_acl_rules type="i32" identifier="7">0</total_acl_rules></UveVirtualNetworkConfig></data></UveVirtualNetworkConfigTrace>', u'Type': 6, u'ModuleId': u'contrail-schema'}
+    ]
+result_2 = [
+{u'Category': None, u'NodeType': u'Analytics', u'Level': 2147483647, u'InstanceId': u'0', u'Messagetype': u'GeneratorDbStatsUve', u'Source': u'a6s45', u'SequenceNum': 56411, u'MessageTS': 1442429889555171, u'Xmlmessage': u'<GeneratorDbStatsUve type="sandesh"><data type="struct" identifier="1"><GeneratorDbStats><name type="string" identifier="1" key="ObjectGeneratorInfo">a6s10:Compute:contrail-vrouter-agent:0</name><table_info type="list" identifier="3" tags=".table_name"><list type="struct" size="12"><DbTableInfo><table_name type="string" identifier="1">MessageTable</table_name><reads type="u64" identifier="2">0</reads><read_fails type="u64" identifier="3">0</read_fails><writes type="u64" identifier="4">12</writes><write_fails type="u64" identifier="5">0</write_fails></DbTableInfo><DbTableInfo><table_name type="string" identifier="1">MessageTableCategory</table_name><reads type="u64" identifier="2">0</reads><read_fails type="u64" identifier="3">0</read_fails><writes type="u64" identifier="4">12</writes><write_fails type="u64" identifier="5">0</write_fails></DbTableInfo><DbTableInfo><table_name type="string" identifier="1">MessageTableKeyword</table_name><reads type="u64" identifier="2">0</reads><read_fails type="u64" identifier="3">0</read_fails><writes type="u64" identifier="4">0</writes><write_fails type="u64" identifier="5">0</write_fails></DbTableInfo><DbTableInfo><table_name type="string" identifier="1">MessageTableMessageType</table_name><reads type="u64" identifier="2">0</reads><read_fails type="u64" identifier="3">0</read_fails><writes type="u64" identifier="4">12</writes><write_fails type="u64" identifier="5">0</write_fails></DbTableInfo><DbTableInfo><table_name type="string" identifier="1">MessageTableModuleId</table_name><reads type="u64" identifier="2">0</reads><read_fails type="u64" identifier="3">0</read_fails><writes type="u64" identifier="4">12</writes><write_fails type="u64" identifier="5">0</write_fails></DbTableInfo><DbTableInfo><table_name type="string" identifier="1">MessageTableSource</table_name><reads type="u64" identifier="2">0</reads><read_fails type="u64" identifier="3">0</read_fails><writes type="u64" identifier="4">12</writes><write_fails type="u64" identifier="5">0</write_fails></DbTableInfo><DbTableInfo><table_name type="string" identifier="1">MessageTableTimestamp</table_name><reads type="u64" identifier="2">0</reads><read_fails type="u64" identifier="3">0</read_fails><writes type="u64" identifier="4">12</writes><write_fails type="u64" identifier="5">0</write_fails></DbTableInfo><DbTableInfo><table_name type="string" identifier="1">ObjectTable</table_name><reads type="u64" identifier="2">0</reads><read_fails type="u64" identifier="3">0</read_fails><writes type="u64" identifier="4">12</writes><write_fails type="u64" identifier="5">0</write_fails></DbTableInfo><DbTableInfo><table_name type="string" identifier="1">ObjectValueTable</table_name><reads type="u64" identifier="2">0</reads><read_fails type="u64" identifier="3">0</read_fails><writes type="u64" identifier="4">12</writes><write_fails type="u64" identifier="5">0</write_fails></DbTableInfo><DbTableInfo><table_name type="string" identifier="1">StatsTableByDblTagV3</table_name><reads type="u64" identifier="2">0</reads><read_fails type="u64" identifier="3">0</read_fails><writes type="u64" identifier="4">1</writes><write_fails type="u64" identifier="5">0</write_fails></DbTableInfo><DbTableInfo><table_name type="string" identifier="1">StatsTableByStrTagV3</table_name><reads type="u64" identifier="2">0</reads><read_fails type="u64" identifier="3">0</read_fails><writes type="u64" identifier="4">102</writes><write_fails type="u64" identifier="5">0</write_fails></DbTableInfo><DbTableInfo><table_name type="string" identifier="1">StatsTableByU64TagV3</table_name><reads type="u64" identifier="2">0</reads><read_fails type="u64" identifier="3">0</read_fails><writes type="u64" identifier="4">2</writes><write_fails type="u64" identifier="5">0</write_fails></DbTableInfo></list></table_info><errors type="list" identifier="4" tags=""><list type="struct" size="1"><DbErrors><write_tablespace_fails type="u64" identifier="1">0</write_tablespace_fails><read_tablespace_fails type="u64" identifier="2">0</read_tablespace_fails><write_table_fails type="u64" identifier="3">0</write_table_fails><read_table_fails type="u64" identifier="4">0</read_table_fails><write_column_fails type="u64" identifier="5">0</write_column_fails><write_batch_column_fails type="u64" identifier="6">0</write_batch_column_fails><read_column_fails type="u64" identifier="7">0</read_column_fails></DbErrors></list></errors><statistics_table_info type="list" identifier="5" tags=".table_name"><list type="struct" size="3"><DbTableInfo><table_name type="string" identifier="1">ComputeCpuState:cpu_info</table_name><reads type="u64" identifier="2">0</reads><read_fails type="u64" identifier="3">0</read_fails><writes type="u64" identifier="4">5</writes><write_fails type="u64" identifier="5">0</write_fails></DbTableInfo><DbTableInfo><table_name type="string" identifier="1">FieldNames:fields</table_name><reads type="u64" identifier="2">0</reads><read_fails type="u64" identifier="3">0</read_fails><writes type="u64" identifier="4">96</writes><write_fails type="u64" identifier="5">0</write_fails></DbTableInfo><DbTableInfo><table_name type="string" identifier="1">VrouterStatsAgent:flow_rate</table_name><reads type="u64" identifier="2">0</reads><read_fails type="u64" identifier="3">0</read_fails><writes type="u64" identifier="4">4</writes><write_fails type="u64" identifier="5">0</write_fails></DbTableInfo></list></statistics_table_info></GeneratorDbStats></data></GeneratorDbStatsUve>', u'Type': 6, u'ModuleId': u'contrail-collector'}
+]
+result_3 = [
+{u'ObjectId': u'virtual_network:default-domain:admin:vn1-take2'}
+]
+result_4 = [
+{u'ObjectLog': u'<VncApiConfigLog type="sandesh"><api_log type="struct" identifier="1"><VncApiCommon><object_type type="string" identifier="2" key="ConfigObjectTable">virtual_network</object_type><identifier_name type="string" identifier="3" key="ConfigObjectTable">default-domain:admin:vn1-take2</identifier_name><url type="string" identifier="4">http://127.0.0.1:9100/virtual-networks</url><operation type="string" identifier="5">post</operation><useragent type="string" identifier="6">a6s45:/usr/bin/contrail-api</useragent><remote_ip type="string" identifier="7">127.0.0.1:9100</remote_ip><body type="string" identifier="9">{&apos;virtual-network&apos;: {&apos;fq_name&apos;: [&apos;default-domain&apos;, &apos;admin&apos;, &apos;vn1-take2&apos;], &apos;uuid&apos;: None, &apos;network_policy_refs&apos;: [], &apos;router_external&apos;: False, &apos;parent_type&apos;: &apos;project&apos;, &apos;id_perms&apos;: {u&apos;enable&apos;: True, u&apos;uuid&apos;: None, u&apos;creator&apos;: None, u&apos;created&apos;: 0, u&apos;user_visible&apos;: True, u&apos;last_modified&apos;: 0, u&apos;permissions&apos;: {u&apos;owner&apos;: u&apos;cloud-admin&apos;, u&apos;owner_access&apos;: 7, u&apos;other_access&apos;: 7, u&apos;group&apos;: u&apos;cloud-admin-group&apos;, u&apos;group_access&apos;: 7}, u&apos;description&apos;: None}, &apos;display_name&apos;: &apos;vn1-take2&apos;, &apos;is_shared&apos;: False}}</body><domain type="string" identifier="10" key="ConfigObjectTableByUser">default-domain</domain></VncApiCommon></api_log></VncApiConfigLog>', u'Messagetype': u'VncApiConfigLog', u'Source': u'a6s45', u'MessageTS': 1442434711187905, u'SystemLog': None, u'ModuleId': u'contrail-api'}
+]
+
+class LogQuerierTest(unittest.TestCase):
+
+    @staticmethod
+    def custom_post_url_http(url, params):
+        global query_dict
+        query_dict = json.loads(params)
+        return '{"href": "/analytics/query/a415fe1e-51cb-11e5-aab0-00000a540d2d"}'
+
+    @staticmethod
+    def custom_get_query_result(opserver_ip, opserver_port, qid):
+        if (test_num == 1):
+            return result_1
+        elif (test_num == 2):
+            return result_2
+        elif (test_num == 3):
+            return result_3
+        elif (test_num == 4):
+            return result_4
+        else:
+            return []
+
+    def custom_display(self, result):
+        if (test_num == 1):
+            self.assertTrue(result == result_1)
+            return
+        elif (test_num == 2):
+            self.assertTrue(result == result_2)
+            return
+        elif (test_num == 3):
+            self.assertTrue(result == result_3)
+            return
+        elif (test_num == 4):
+            self.assertTrue(result == result_4)
+            return
+        else:
+            self.assertTrue(False)
+
+    def setUp(self):
+        self._querier = LogQuerier()
+
+        flexmock(OpServerUtils).should_receive('post_url_http').replace_with(lambda x, y: self.custom_post_url_http(x, y))
+        flexmock(OpServerUtils).should_receive('get_query_result').replace_with(lambda x, y, z: self.custom_get_query_result(x, y, z))
+        flexmock(self._querier).should_receive('display').replace_with(lambda x: self.custom_display(x))
+
+
+    #@unittest.skip("skip test_1_no_arg")
+    def test_1_no_arg(self):
+        global test_num
+        global query_dict
+        test_num = 1
+
+        argv = sys.argv
+        sys.argv = "contrail-logs".split()
+        self._querier.run()
+        sys.argv = argv
+
+        expected_result_str = '{"sort": 1, "start_time": "now-10m", "sort_fields": ["MessageTS"], "end_time": "now", "select_fields": ["MessageTS", "Source", "ModuleId", "Category", "Messagetype", "SequenceNum", "Xmlmessage", "Type", "Level", "NodeType", "InstanceId"], "table": "MessageTable"}'
+        expected_result_dict = json.loads(expected_result_str)
+        for key in expected_result_dict:
+            self.assertTrue(key in query_dict)
+            self.assertTrue(expected_result_dict[key] == query_dict[key])
+
+    # a few args
+    #@unittest.skip("skip test_2_message_query")
+    def test_2_message_query(self):
+        global test_num
+        global query_dict
+        test_num = 2
+
+        argv = sys.argv
+        sys.argv = "contrail-logs --source a6s45 --node-type Analytics --module contrail-collector --instance-id 0 --message-type GeneratorDbStatsUve".split()
+        self._querier.run()
+        sys.argv = argv
+
+        expected_result_str = '{"sort": 1, "start_time": "now-10m", "sort_fields": ["MessageTS"], "end_time": "now", "select_fields": ["MessageTS", "Source", "ModuleId", "Category", "Messagetype", "SequenceNum", "Xmlmessage", "Type", "Level", "NodeType", "InstanceId"], "table": "MessageTable"}'
+        expected_result_dict = json.loads(expected_result_str)
+        for key in expected_result_dict:
+            self.assertTrue(key in query_dict)
+            self.assertTrue(expected_result_dict[key] == query_dict[key])
+
+    # a object values query
+    #@unittest.skip("skip test_3_object_value")
+    def test_3_object_value(self):
+        global test_num
+        global query_dict
+        test_num = 3
+
+        argv = sys.argv
+        sys.argv = "contrail-logs --object-type config --object-values".split()
+        self._querier.run()
+        sys.argv = argv
+
+        expected_result_str = '{"table": "ConfigObjectTable", "start_time": "now-10m", "end_time": "now", "select_fields": ["ObjectId"]}'
+        expected_result_dict = json.loads(expected_result_str)
+        for key in expected_result_dict:
+            self.assertTrue(key in query_dict)
+            self.assertTrue(expected_result_dict[key] == query_dict[key])
+
+    # a object id query
+    #@unittest.skip("skip test_4_object_id")
+    def test_4_object_id(self):
+        global test_num
+        global query_dict
+        test_num = 4
+
+        argv = sys.argv
+        sys.argv = "contrail-logs --object-type config --object-id virtual_network:default-domain:admin:vn1-take2".split()
+        self._querier.run()
+        sys.argv = argv
+
+        expected_result_str = '{"sort": 1, "start_time": "now-10m", "sort_fields": ["MessageTS"], "end_time": "now", "select_fields": ["MessageTS", "Source", "ModuleId", "Messagetype", "ObjectLog", "SystemLog"], "table": "ConfigObjectTable", "where": [[{"suffix": null, "value2": null, "name": "ObjectId", "value": "virtual_network:default-domain:admin:vn1-take2", "op": 1}]]}'
+        expected_result_dict = json.loads(expected_result_str)
+        for key in expected_result_dict:
+            self.assertTrue(key in query_dict)
+            self.assertTrue(expected_result_dict[key] == query_dict[key])
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/opserver/test/test_stats.py
@@ -0,0 +1,61 @@
+import sys
+import json
+import gevent
+from gevent import monkey
+monkey.patch_all()
+
+import unittest
+from flexmock import flexmock
+
+from opserver.stats import StatQuerier
+from opserver.opserver_util import OpServerUtils
+
+test_num = 0
+query_dict = {}
+got_expected_log_str = False
+result_1 =\
+'{"value": [\n{"T=":1442446560000000,"SUM(cpu_info.cpu_share)":0}, {"T=":1442446620000000,"SUM(cpu_info.cpu_share)":4.16222}, {"T=":1442446680000000,"SUM(cpu_info.cpu_share)":4.10806}, {"T=":1442446740000000,"SUM(cpu_info.cpu_share)":1.71222}, {"T=":1442446800000000,"SUM(cpu_info.cpu_share)":1.68722}, {"T=":1442446860000000,"SUM(cpu_info.cpu_share)":1.64973}, {"T=":1442446920000000,"SUM(cpu_info.cpu_share)":1.67889}, {"T=":1442446980000000,"SUM(cpu_info.cpu_share)":1.65}, {"T=":1442447040000000,"SUM(cpu_info.cpu_share)":1.66639}, {"T=":1442447100000000,"SUM(cpu_info.cpu_share)":1.70805}, {"T=":1442447160000000,"SUM(cpu_info.cpu_share)":1.65806}\n]}'
+
+class StatQuerierTest(unittest.TestCase):
+
+    @staticmethod
+    def custom_post_url_http(url, params, sync = True):
+        global query_dict
+        query_dict = json.loads(params)
+        if (test_num == 1):
+            return result_1
+
+        return []
+
+    def custom_display(self, result):
+        if (test_num == 1):
+            expect_result = json.loads(result_1)
+            expect_result = expect_result['value']
+            self.assertTrue(result == expect_result)
+
+    def setUp(self):
+        self._querier = StatQuerier()
+
+        flexmock(OpServerUtils).should_receive('post_url_http').once().replace_with(lambda x, y, **kwargs: self.custom_post_url_http(x, y, kwargs))
+        flexmock(self._querier).should_receive('display').replace_with(lambda x: self.custom_display(x))
+
+
+    #@unittest.skip("skip test_1_analytics_cpu_query")
+    def test_1_analytics_cpu_query(self):
+        global test_num
+        global query_dict
+        test_num = 1
+
+        argv = sys.argv
+        sys.argv = "contrail-stats --table AnalyticsCpuState.cpu_info --select T=60 SUM(cpu_info.cpu_share) --where name=*".split()
+        self._querier.run()
+        sys.argv = argv
+
+        expected_result_str = '{"start_time": "now-10m", "end_time": "now", "select_fields": ["T=60", "SUM(cpu_info.cpu_share)"], "table": "StatTable.AnalyticsCpuState.cpu_info", "where": [[{"suffix": null, "value2": null, "name": "name", "value": "", "op": 7}]]}'
+        expected_result_dict = json.loads(expected_result_str)
+        for key in expected_result_dict:
+            self.assertTrue(key in query_dict)
+            self.assertTrue(expected_result_dict[key] == query_dict[key])
+
+if __name__ == '__main__':
+    unittest.main()
