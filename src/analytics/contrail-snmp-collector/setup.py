f4487a2417a21e0f15cc92dc057c6b90dd73207a
Ted Ghose <rintu@hotmail.com>
2015-09-21 15:24:25 -0700

    Analytics: python code coverage infrastructure
    - scons ut/coverage support for snmp collector
    - parametric run_tests.sh to reuse across contrail
    Partial-Bug: #1480463
    
    Change-Id: I263bae1e68738f568e6d03cd7297708de5073a87

--- src/analytics/contrail-snmp-collector/setup.py
+++ src/analytics/contrail-snmp-collector/setup.py
@@ -1,7 +1,40 @@
 #
 # Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
 #
-import re, setuptools
+import re, setuptools, os, distutils
+from setuptools import setup, find_packages, Command
+
+class RunTestsCommand(Command):
+    description = "Test command to run testr in virtualenv"
+    user_options = [
+        ('coverage', 'c', "Generate code coverage report"),
+        ('testrun=', None, "Run a specific test"),
+        ]
+    boolean_options = ['coverage']
+    def initialize_options(self):
+        self.cwd = None
+        self.coverage = False
+        self.testrun = None
+
+    def finalize_options(self):
+        self.cwd = os.getcwd()
+        if self.testrun:
+            self.announce('Running test: %s' % str(self.testrun),
+                            level=distutils.log.INFO)
+
+    def run(self):
+        logfname = 'test.log'
+        args = '-V'
+        if self.coverage:
+            logfname = 'coveragetest.log'
+            args += ' -c'
+        if self.testrun:
+            logfname = self.testrun + '.log'
+            args = self.testrun
+        os.system('./run_tests.sh %s' % args)
+        with open(logfname) as f:
+            if not re.search('\nOK', ''.join(f.readlines())):
+                os._exit(1)
 
 def requirements(filename):
     with open(filename) as f:
@@ -10,7 +43,7 @@ def requirements(filename):
     return filter(bool, map(lambda y: c.sub('', y).strip(), lines))
 
 setuptools.setup(
-        name='contrail-snmp-collector',
+        name='contrail_snmp_collector',
         version='0.2.0',
         description='contrail snmp collector package.',
         long_description=open('README.txt').read(),
@@ -24,13 +57,14 @@ setuptools.setup(
 
         install_requires=requirements('requirements.txt'),
 
-        test_suite='contrail_snmp_collector.tests',
-        tests_require=requirements('test-requirements.txt'),
         entry_points = {
           'console_scripts' : [
             'contrail-snmp-collector = contrail_snmp_collector.main:emain',
             'contrail-snmp-scanner = contrail_snmp_collector.scanner:main',
             ],
         },
+    cmdclass={
+       'run_tests': RunTestsCommand,
+    },
     )
 
