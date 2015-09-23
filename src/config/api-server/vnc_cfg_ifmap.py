0b6077dbed30a4e43ce2c0ac7cc663359345c092
Édouard Thuleau <edouard.thuleau@cloudwatt.com>
2015-09-23 16:05:27 +0200

    [VNC API] Catch all not found cases on cassandra get
    Only the 'get' method of pycassa can raise NotFoundException.
    
    Change-Id: If7b561002de298189e30f76ec7408755f7bc079e
    Partial-Bug: #1498925

--- src/config/api-server/vnc_cfg_ifmap.py
+++ src/config/api-server/vnc_cfg_ifmap.py
@@ -872,8 +872,11 @@ class VncServerCassandraClient(VncCassandraClient):
     # end _delete_ref
 
     def is_latest(self, id, tstamp):
-        id_perms_json = self._obj_uuid_cf.get(
-            id, columns=['prop:id_perms'])['prop:id_perms']
+        try:
+            id_perms_json = self._obj_uuid_cf.get(
+                id, columns=['prop:id_perms'])['prop:id_perms']
+        except pycassa.NotFoundException:
+            raise NoIdError(id)
         id_perms = json.loads(id_perms_json)
         if id_perms['last_modified'] == tstamp:
             return True
@@ -883,9 +886,12 @@ class VncServerCassandraClient(VncCassandraClient):
 
     def update_last_modified(self, bch, obj_uuid, id_perms=None):
         if id_perms is None:
-            id_perms = json.loads(
-                self._obj_uuid_cf.get(obj_uuid,
-                                      ['prop:id_perms'])['prop:id_perms'])
+            try:
+                id_perms_json = self._obj_uuid_cf.get(
+                    obj_uuid, ['prop:id_perms'])['prop:id_perms']
+            except pycassa.NotFoundException:
+                raise NoIdError(obj_uuid)
+            id_perms = json.loads(id_perms_json)
         id_perms['last_modified'] = datetime.datetime.utcnow().isoformat()
         self._update_prop(bch, obj_uuid, 'id_perms', {'id_perms': id_perms})
     # end update_last_modified
