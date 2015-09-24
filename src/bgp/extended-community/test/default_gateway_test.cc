e31a64f3479780660c5ebeb4545ef3c5f91ff2e0
Ananth Suryanarayana <anantha@juniper.net>
2015-09-24 01:25:27 -0700

    Add support to exchange load-balance bgp extended community
    o Exchange list of fields to hash upon during ecmp load-balancing
    o Fields: L2 Source Address, L2 Dest Address, L3 Source Address, L3 Dest Address,
              L4 Protocol, L4 Source Port, L4 Dest Port
    o Room for future load-balance fields addition such as interface
    o Additional actions field such as source_bias boolean
    o Room for future actions such as urpf
    o This info is sent to agents via xmpp
    o This info is sent to other bgp peers via opaque extended community
    
    o Cleanup hard-coded extended community types with defines
    
    Change-Id: I22371305eb9721ed621979f53382f1fdc495fc1b
    Closes-Bug: #1498765

--- src/bgp/extended-community/test/default_gateway_test.cc
+++ src/bgp/extended-community/test/default_gateway_test.cc
@@ -2,9 +2,8 @@
  * Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
  */
 
-#include "bgp/extended-community/default_gateway.h"
-
 #include "base/logging.h"
+#include "bgp/extended-community/default_gateway.h"
 #include "testing/gunit.h"
 
 class DefaultGatewayTest : public ::testing::Test {
@@ -12,14 +11,18 @@ class DefaultGatewayTest : public ::testing::Test {
 
 TEST_F(DefaultGatewayTest, ByteArray_1) {
     DefaultGateway::bytes_type data =
-        { { 0x06, 0x0d, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 } };
+        { { BGP_EXTENDED_COMMUNITY_TYPE_OPAQUE,
+            BGP_EXTENDED_COMMUNITY_OPAQUE_DEFAULT_GATEWAY,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00 } };
     DefaultGateway dgw(data);
     EXPECT_EQ("defaultgw:0", dgw.ToString());
 }
 
 TEST_F(DefaultGatewayTest, ByteArray_2) {
     DefaultGateway::bytes_type data =
-        { { 0x06, 0x0d, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 } };
+        { { BGP_EXTENDED_COMMUNITY_TYPE_OPAQUE,
+            BGP_EXTENDED_COMMUNITY_TYPE_OPAQUE,
+            0x01, 0x02, 0x03, 0x04, 0x05, 0x06 } };
     DefaultGateway dgw(data);
     EXPECT_EQ("defaultgw:0", dgw.ToString());
 }
