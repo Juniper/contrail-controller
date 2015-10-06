7cc26b9325e4e87b17b057a5f7bb730c0267dc33
Sachin Bansal <sachin@contrailsystems.com>
2015-10-06 12:14:43 -0700

    Use SIGTERM to kill config daemons and kill all greenlets when SIGTERM is received
    Change-Id: Id29300c4ed64ea237829f02f691dd18fff14ebe5
    Closes-Bug: 1503407
    (cherry picked from commit b5bd298348df6bb52d31040ae3b7d0a923f41d4f)

--- src/config/common/zkclient.py
+++ src/config/common/zkclient.py
@@ -189,11 +189,13 @@ class ZookeeperClient(object):
         logger = logging.getLogger(module)
         logger.setLevel(logging.INFO)
         try:
-            handler = logging.handlers.RotatingFileHandler(LOG_DIR + module + '-zk.log', maxBytes=10*1024*1024, backupCount=5)
+            handler = logging.handlers.RotatingFileHandler(
+                LOG_DIR + module + '-zk.log', maxBytes=10*1024*1024, backupCount=5)
         except IOError:
             print "Cannot open log file in %s" %(LOG_DIR)
         else:
-            log_format = logging.Formatter('%(asctime)s [%(name)s]: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+            log_format = logging.Formatter('%(asctime)s [%(name)s]: %(message)s',
+                                           datefmt='%m/%d/%Y %I:%M:%S %p')
             handler.setFormatter(log_format)
             logger.addHandler(handler)
 
@@ -202,8 +204,7 @@ class ZookeeperClient(object):
         else:
             self.log = self.syslog
 
-        self._zk_client = \
-            kazoo.client.KazooClient(
+        self._zk_client = kazoo.client.KazooClient(
                 server_list,
                 timeout=400,
                 handler=kazoo.handlers.gevent.SequentialGeventHandler(),
