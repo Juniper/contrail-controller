10ab99ec640663ba1fe1aca33820333d3b998b97
Divakar <ddivakar@juniper.net>
2015-10-15 15:38:52 +0530

    Taking the haproxy process id from config.pid file
    The haproxy config is updated by providing the new configuration file
    and old haproxy process id. This starts a new haproxy process letting
    the old haproxy process to handle the old sessions. New haproxy handles
    the new sessions. If a new configuration update comes while both old and
    new haproxy processes are runnning, another haproxy process needs to be started
    by providing the latest running haproxy process id.
    
    In the existing update code, the haproxy process id is found by "ps"
    output. This is resulating in not getting the latest haproxy process id,
    but an older process id. This is leading to second haproxy process never
    getting killed leaving two haproxies running for ever.
    
    The latest haproxy process that is handling the new session is always
    updated in a file by haproxy itself. As a fix, this process id used to
    start a new haproxy rather taking the pid from "ps". When the
    loadbalancer VIP is deleted, Agent deletes the directory where haproxy
    config file, stats file and pid file are stored. This can result in
    vrouter_netns.py script failing as pid is not found to stop the haproxy.
    To overcome this, the config file, pid file and sock file are stored not
    in an explicit directory corresponding to the pool. Only config fike and
    sock file are deleted by Agent and pid file is deleted by vrouter_netns
    script.
    
    Change-Id: Ic27b1122199e7f4fd106f944a9a034f7ff2e2e25
    closes-bug: #1495702

--- src/nodemgr/haproxy_stats.py
+++ src/nodemgr/haproxy_stats.py
@@ -33,7 +33,7 @@ class HaproxyStats(object):
         pass
 
     def get_stats(self, pool_id):
-        sock_path = os.path.join(LB_BASE_DIR, pool_id, 'etc/haproxy/haproxy.cfg.sock')
+        sock_path  = LB_BASE_DIR  + pool_id  + ".haproxy.cfg.sock";
         if not os.path.exists(sock_path):
             sys.stderr.write('\nStats socket not found for pool ' + pool_id)
             return {}
