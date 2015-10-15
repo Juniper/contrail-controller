7e4eaf74a324ae76cbca5e222e4033a5e612e5f7
Ashok Singh <ashoksr@juniper.net>
2015-10-15 03:30:00 -0700

    Fix bad_cast exception happening during IpAddress conversion to v6.
    If default Gateway is not configured in an ipam having v6 subnet we were
    attempting to convert the unspecified IP Address to v6. This was resulting
    in bad_cast exception
    Closes-Bug: #1506330
    
    Change-Id: Ic5a556fc16784c961157c0f2a9ae24ebbcb7b35e

--- src/vnsw/agent/services/dhcpv6_handler.cc
+++ src/vnsw/agent/services/dhcpv6_handler.cc
@@ -482,7 +482,10 @@ bool Dhcpv6Handler::FindLeaseData() {
             }
             if (IsIp6SubnetMember(ip, ipam[i].ip_prefix.to_v6(), 
                                   ipam[i].plen)) {
-                Ip6Address default_gw = ipam[i].default_gw.to_v6();
+                Ip6Address default_gw;
+                if (ipam[i].default_gw.is_v6()) {
+                    default_gw = ipam[i].default_gw.to_v6();
+                }
                 Ip6Address service_address;
                 if (ipam[i].dns_server.is_unspecified()) {
                     service_address = default_gw;
