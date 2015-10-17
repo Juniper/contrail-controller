a7db8b212069de4b070bcc06df2bd55d4356c096
Karl Klashinsky <klash@juniper.net>
2015-10-16 18:42:18 -0700

    Detect unit-test failure in svc-monitor
    All other usage of testr/run_test.sh infra does an
    explicit search for \nOK as a check for unit-test
    failure (in setup.py's RunTestsCommand.run()).
    
    A couple of setup.py's were missed, and the result
    of os.system() is not being checked. This results in
    unit-test failures being silenlty missed.
    
    Change-Id: I08c2bab5e6266c91f599bd1efbea44f89d655a2e
    Closes-Bug: #1507070

--- src/config/svc-monitor/setup.py
+++ src/config/svc-monitor/setup.py
@@ -18,10 +18,15 @@ class RunTestsCommand(setuptools.Command):
     def finalize_options(self):
         self.cwd = os.getcwd()
     def run(self):
+        logfname = 'test.log'
         args = '-V'
         if self.coverage:
+            logfname = 'coveragetest.log'
             args += ' -c'
         os.system('./run_tests.sh %s' % args)
+        with open(logfname) as f:
+            if not re.search('\nOK', ''.join(f.readlines())):
+                os._exit(1)
 
 def requirements(filename):
     with open(filename) as f:
