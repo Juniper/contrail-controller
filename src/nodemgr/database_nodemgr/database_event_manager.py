4c3941279fa2d61536f1bed635d46748a5b8eddb
Kasey Alusi <kasey.alusi@workday.com>
2015-10-27 17:15:22 -0700

    Seperate analytics database nodemgr check
    If the analytics service is not present on the node, nodemgr will
    not report initializing status when the cassandra data directory
    for analytics is not present.
    
    Closes-Bug:1510736
    
    Change-Id: I587fe85ed3bd8496f57fdaab834d215d7c2d3d67

--- src/nodemgr/database_nodemgr/database_event_manager.py
+++ src/nodemgr/database_nodemgr/database_event_manager.py
@@ -87,6 +87,7 @@ class DatabaseEventManager(EventManager):
                 Popen(popen_cmd, shell=True, stdout=PIPE).communicate()
             cassandra_data_dir = cassandra_data_dir.strip()
             analytics_dir = cassandra_data_dir + '/ContrailAnalytics'
+            analytics_socket = "/tmp/supervisord_analytics.sock"
             if os.path.exists(analytics_dir):
                 self.stderr.write("analytics_dir is " + analytics_dir + "\n")
                 popen_cmd = "set `df -Pk " + analytics_dir + " | grep % | awk '{s+=$3}END{print s}'` && echo $1"
@@ -107,6 +108,8 @@ class DatabaseEventManager(EventManager):
                     (ret_value, error_value) = Popen(
                         cmd_str, shell=True, stdout=PIPE).communicate()
                     self.fail_status_bits |= self.FAIL_STATUS_DISK_SPACE
+                self.fail_status_bits &= ~self.FAIL_STATUS_DISK_SPACE_NA
+            elif not os.path.exists(analytics_socket):
                 self.fail_status_bits &= ~self.FAIL_STATUS_DISK_SPACE_NA
             else:
                 self.fail_status_bits |= self.FAIL_STATUS_DISK_SPACE_NA
@@ -154,6 +157,7 @@ class DatabaseEventManager(EventManager):
                 Popen(popen_cmd, shell=True, stdout=PIPE).communicate()
             cassandra_data_dir = cassandra_data_dir.strip()
             analytics_dir = cassandra_data_dir + '/ContrailAnalytics'
+            analytics_socket = "/tmp/supervisord_analytics.sock"
             if os.path.exists(analytics_dir):
                 popen_cmd = "set `df -Pk " + analytics_dir + " | grep % | awk '{s+=$3}END{print s}'` && echo $1"
                 (disk_space_used, error_value) = \
@@ -177,6 +181,8 @@ class DatabaseEventManager(EventManager):
                 db_info.database_usage = [db_stat]
                 usage_stat = DatabaseUsage(data=db_info)
                 usage_stat.send()
+            elif not os.path.exists(analytics_socket):
+                self.fail_status_bits &= ~self.FAIL_STATUS_DISK_SPACE_NA
             else:
                 self.fail_status_bits |= self.FAIL_STATUS_DISK_SPACE_NA
         except:
