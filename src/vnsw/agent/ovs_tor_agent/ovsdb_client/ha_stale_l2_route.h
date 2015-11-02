a3bf35a1534d6989c56fb497b9913416b5f450b6
Prabhjot Singh Sethi <prabhjot@juniper.net>
2015-11-02 11:19:35 +0530

    Fix ToR-Agent crash on withdrawing HA stale route
    Issue:
    ------
    since the route export operations were done async, it
    resulted in releasing the ovsdb route peer due to cleanup
    before completion of the route operations, causing invalid
    pointer access while withdrawing routes
    
    Fix:
    ----
    on DB notification to KSync, enqueue export event for the
    entry to a KSync task workqueue, also hold the state of
    KSync entry which assures that entry is held in the state
    till operation is complete, allowing route operations to
    complete before route peer cleanup.
    
    Conflicts:
    	src/vnsw/agent/ovs_tor_agent/ovsdb_client/ha_stale_l2_route.cc
    	src/vnsw/agent/ovs_tor_agent/ovsdb_client/ovsdb_entry.h
    
    Closes-Bug: 1503124
    Change-Id: I32732ce9e32a6fa8bddd3ee2cc8c0bd783afe945
    (cherry picked from commit 734223864ec70ca8e04056e8aae4fc7a1e854c4d)
    (cherry picked from commit 28043f285a55769195b28a56030fc3d4d91fde61)

--- src/vnsw/agent/ovs_tor_agent/ovsdb_client/ha_stale_l2_route.h
+++ src/vnsw/agent/ovs_tor_agent/ovsdb_client/ha_stale_l2_route.h
@@ -44,6 +44,12 @@ private:
     void OvsdbNotify(OvsdbClientIdl::Op, struct ovsdb_idl_row *) {}
     OvsdbDBEntry *AllocOvsEntry(struct ovsdb_idl_row *row) { return NULL; }
 
+    // Enqueue entry to route export queue
+    void EnqueueExportEntry(HaStaleL2RouteEntry *entry);
+
+    // API to process route export queue entries
+    bool ProcessExportEntry(HaStaleL2RouteEntry *entry);
+
     LifetimeRef<HaStaleL2RouteTable> table_delete_ref_;
     // take reference to a dummy dev_vn entry to hold peer till
     // route table is deleted. route object cannot hold reference
@@ -56,6 +62,7 @@ private:
     // of vrf pointer even if Add route request fails, due to any reason
     VrfEntryRef vrf_;
     std::string vn_name_;
+    WorkQueue<HaStaleL2RouteEntry *> *route_export_queue_;
     DISALLOW_COPY_AND_ASSIGN(HaStaleL2RouteTable);
 };
 
@@ -65,9 +72,9 @@ public:
                           const std::string &mac);
     ~HaStaleL2RouteEntry();
 
-    void AddMsg(struct ovsdb_idl_txn *);
-    void ChangeMsg(struct ovsdb_idl_txn *);
-    void DeleteMsg(struct ovsdb_idl_txn *);
+    bool Add();
+    bool Change();
+    bool Delete();
 
     bool Sync(DBEntry*);
     bool IsLess(const KSyncEntry&) const;
@@ -78,14 +85,15 @@ public:
     uint32_t vxlan_id() const;
     bool IsStale() const;
 
-protected:
-    virtual bool IsNoTxnEntry() { return true; }
-
 private:
     friend class HaStaleL2RouteTable;
     friend class VrfRouteReflectorTable;
 
     void StaleClearCb();
+
+    void AddEvent();
+    void ChangeEvent();
+    void DeleteEvent();
 
     std::string mac_;
     uint32_t path_preference_;
