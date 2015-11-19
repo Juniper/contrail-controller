Juniper/contrail-analytics

1. Fix --json option of contrail-logs to actually output json, scrub    the output so that it is more usable. 2. Change api-server to output json string in VncApiCommon body instead    of python string. Closes-Bug: #1484276
Change-Id: I654ffffbf5b993904bcdcb18e2f376a77b90e386

contrail-opserver/log.py
@@ -491,6 +491,10 @@ class LogQuerier(object):
             logger.addHandler(syslog_handler)
             self._logger = logger
 
+        # For json we will be outputting list of dicts so open the list here
+        if self._args.json:
+            first = True
+            self.output('[', SandeshLevel.INVALID)
         for messages_dict in messages_dict_list:
 
             if VizConstants.TIMESTAMP in messages_dict:
@@ -504,6 +508,7 @@ class LogQuerier(object):
                 message_ts = message_dt.strftime(OpServerUtils.TIME_FORMAT_STR)
             else:
                 message_ts = 'Time: NA'
+            messages_dict[VizConstants.TIMESTAMP] = message_ts
             if VizConstants.SOURCE in messages_dict:
                 source = messages_dict[VizConstants.SOURCE]
             else:
@@ -540,6 +545,7 @@ class LogQuerier(object):
                     level = SandeshLevel._VALUES_TO_NAMES[sandesh_level]
                 else:
                     level = 'Level: NA'
+                messages_dict[VizConstants.LEVEL] = level
                 if VizConstants.SEQUENCE_NUM in messages_dict:
                     seq_num = messages_dict[VizConstants.SEQUENCE_NUM]
                 else:
@@ -560,6 +566,11 @@ class LogQuerier(object):
                 else:
                     data_str = 'Data not present'
                 if self._args.json:
+                    if not first:
+                        self.output(", ", sandesh_level)
+                    else:
+                        first = False
+                    OpServerUtils.messages_dict_scrub(messages_dict)
                     self.output(messages_dict, sandesh_level)
                 else:
                     if self._args.trace is not None:
@@ -599,9 +610,17 @@ class LogQuerier(object):
                                 message_ts, source, node_type, module,
                                 instance_id, message_type, data_str)
                             if self._args.json:
-                                self.output(messages_dict[obj_sel_field], sandesh_level)
+                                if not first:
+                                    self.output(", ", sandesh_level)
+                                else:
+                                    first = False
+                                OpServerUtils.messages_dict_scrub(messages_dict)
+                                self.output(messages_dict, sandesh_level)
                             else:
                                 self.output(obj_str, sandesh_level)
+        # For json we will be outputting list of dicts so close the list here
+        if self._args.json:
+            self.output(']', SandeshLevel.INVALID)
     # end display
 
 # end class LogQuerier

Juniper/contrail-controller

Conflicts:
	src/config/api-server/vnc_cfg_api_server.py
	src/opserver/log.py
	src/opserver/opserver_util.py
(cherry picked from commit 13f4688e93aee3006410eef6be4c54a9f159f770)

src/opserver/log.py
@@ -454,6 +454,10 @@ class LogQuerier(object):
             logger.addHandler(syslog_handler)
             self._logger = logger
 
+        # For json we will be outputting list of dicts so open the list here
+        if self._args.json:
+            first = True
+            self.output('[', SandeshLevel.INVALID)
         for messages_dict in messages_dict_list:
 
             if VizConstants.TIMESTAMP in messages_dict:
@@ -467,6 +471,7 @@ class LogQuerier(object):
                 message_ts = message_dt.strftime(OpServerUtils.TIME_FORMAT_STR)
             else:
                 message_ts = 'Time: NA'
+            messages_dict[VizConstants.TIMESTAMP] = message_ts
             if VizConstants.SOURCE in messages_dict:
                 source = messages_dict[VizConstants.SOURCE]
             else:
@@ -503,6 +508,7 @@ class LogQuerier(object):
                     level = SandeshLevel._VALUES_TO_NAMES[sandesh_level]
                 else:
                     level = 'Level: NA'
+                messages_dict[VizConstants.LEVEL] = level
                 if VizConstants.SEQUENCE_NUM in messages_dict:
                     seq_num = messages_dict[VizConstants.SEQUENCE_NUM]
                 else:
@@ -523,6 +529,11 @@ class LogQuerier(object):
                 else:
                     data_str = 'Data not present'
                 if self._args.json:
+                    if not first:
+                        self._output(", ", sandesh_level)
+                    else:
+                        first = False
+                    OpServerUtils.messages_dict_scrub(messages_dict)
                     self._output(messages_dict, sandesh_level)
                 else:
                     if self._args.trace is not None:
@@ -562,9 +573,17 @@ class LogQuerier(object):
                                 message_ts, source, node_type, module,
                                 instance_id, message_type, data_str)
                             if self._args.json:
-                                self._output(messages_dict[obj_sel_field], sandesh_level)
+                                if not first:
+                                    self._output(", ", sandesh_level)
+                                else:
+                                    first = False
+                                OpServerUtils.messages_dict_scrub(messages_dict)
+                                self._output(messages_dict, sandesh_level)
                             else:
                                 self._output(obj_str, sandesh_level)
+        # For json we will be outputting list of dicts so close the list here
+        if self._args.json:
+            self._output(']', SandeshLevel.INVALID)
     # end display
 
 # end class LogQuerier