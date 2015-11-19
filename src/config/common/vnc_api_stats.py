127903b8fa89e6685547ac3776b91fb80a55d77d
Ignatious Johnson Christopher <ijohnson@juniper.net>
2015-11-19 15:58:32 -0800

    Enabling the contrail log query of vnc api stats through webui Closes-Bug: 1481116
    Calculating api response time in microseconds, instead of using datetime's
    total_seconds() method, because the total_seconds() method is not available
    in python version less than 2.7
    Closes-Bug: 1500674
    
    Change-Id: I63830d5c9f4848209495dc67eae84c16ee32f6a3

---
+++ src/config/common/vnc_api_stats.py
@@ -0,0 +1,73 @@
+#
+# Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
+#
+
+import bottle
+from datetime import datetime
+
+from uve.vnc_api.ttypes import VncApiStats, VncApiStatsLog
+from pysandesh.gen_py.sandesh.ttypes import SandeshLevel
+
+
+def log_api_stats(func):
+    def wrapper(api_server_obj, resource_type, *args, **kwargs):
+        try:
+            statistics = VncApiStatistics(
+                obj_type=resource_type.replace('-', '_'))
+            response = func(api_server_obj, resource_type, *args, **kwargs)
+            statistics.response_size = len(str(response))
+            statistics.response_code = bottle.response.status_code
+            return response
+        except Exception as err_response:
+            if isinstance(err_response, bottle.HTTPError):
+                statistics.response_size = len(err_response.body)
+                statistics.response_code = err_response.status_code
+            else:
+                statistics.response_size = 0
+                # 520 Unknown Error
+                statistics.response_code = 520
+            raise
+        finally:
+            # Collect api stats and send to analytics
+            statistics.collect()
+            statistics.sendwith(api_server_obj._sandesh)
+    return wrapper
+
+
+class VncApiStatistics(object):
+    def __init__(self, obj_type):
+        self.obj_type = obj_type
+        self.response_size = 0
+        self.time_start = datetime.now()
+
+    def collect(self):
+        self.time_finish = datetime.now()
+        response_time = (self.time_finish - self.time_start)
+        response_time_in_usec = ((response_time.days*24*60*60) +
+                                 (response_time.seconds*1000000) +
+                                 response_time.microseconds)
+        domain_name = bottle.request.headers.get('X-Domain-Name', 'None')
+        if domain_name.lower() == 'none':
+            domain_name = 'default-domain'
+        project_name = bottle.request.headers.get('X-Project-Name', 'None')
+        if project_name.lower() == 'none':
+            project_name = 'default-project'
+
+        # Create api stats object
+        self.api_stats = VncApiStats(
+            object_type=self.obj_type,
+            operation_type=bottle.request.method,
+            user=bottle.request.headers.get('X-User-Name'),
+            useragent=bottle.request.headers.get('X-Contrail-Useragent',
+                bottle.request.headers.get('User-Agent')),
+            remote_ip=bottle.request.headers.get('Host'),
+            domain_name=domain_name,
+            project_name=project_name,
+            response_time_in_usec=response_time_in_usec,
+            response_size=self.response_size,
+            response_code=self.response_code,
+        )
+
+    def sendwith(self, sandesh):
+        stats_log = VncApiStatsLog(api_stats=self.api_stats, sandesh=sandesh)
+        stats_log.send(sandesh=sandesh)
