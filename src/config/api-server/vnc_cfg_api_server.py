87c4023c904ffbc5265a4d430d984f444ddc4322
Hampapur Ajay <hajay@juniper.net>
2015-11-23 13:31:25 -0800

    To have a single api-server for all config unit tests.
    Partial-Bug: #1521385
    
    Change-Id: Ic57c2bb9ee3549a5590327aeeda4c9059ca56865

--- src/config/api-server/vnc_cfg_api_server.py
+++ src/config/api-server/vnc_cfg_api_server.py
@@ -2311,7 +2311,6 @@ class VncApiServer(object):
     # end sigchld_handler
 
     def sigterm_handler(self):
-        self.cleanup()
         exit()
 
     def _load_extensions(self):
@@ -2972,10 +2971,11 @@ class VncApiServer(object):
         return (True, uuid_in_req)
     # end _http_post_common
 
-    def cleanup(self):
-        # TODO cleanup sandesh context
-        pass
-    # end cleanup
+    def reset(self):
+        # cleanup internal state/in-flight operations
+        if self._db_conn:
+            self._db_conn.reset()
+    # end reset
 
     # allocate block of IP addresses from VN. Subnet info expected in request
     # body
@@ -3177,7 +3177,7 @@ def main(args_str=None):
         raise
     finally:
         # always cleanup gracefully
-        vnc_api_server.cleanup()
+        vnc_api_server.reset()
 
 # end main
 
