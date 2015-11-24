5c7ab0126bdc2cc40b5e3a45a390617cb95663f4
facetothefate <facetothefate@gmail.com>
2015-11-24 16:07:59 -0500

    :Upgrade PNF function with newest Schema
    1. add the port tuple
    2. delete the reference between the service instance and virtual machine interface
    
    Change-Id: I4b81f91a48b9792f07ff293f1efe0d1c4cea5e95
    close-bug: #1516147

--- src/config/svc-monitor/svc_monitor/svc_monitor.py
+++ src/config/svc-monitor/svc_monitor/svc_monitor.py
@@ -90,7 +90,7 @@ class SvcMonitor(object):
             'loadbalancer_pool': []
         },
         "service_instance": {
-            'self': ['virtual_machine', 'port_tuple', 'virtual_machine_interface'],
+            'self': ['virtual_machine', 'port_tuple'],
             'virtual_machine': [],
             'port_tuple': [],
             'virtual_machine_interface' : []
@@ -110,7 +110,7 @@ class SvcMonitor(object):
         "physical_interface": {
             'self': [],
             'service_appliance':['virtual_machine_interface'],
-            'virtual_machine_interfaces':['service_appliance'],
+            'virtual_machine_interface':['service_appliance'],
         },
         "logical_interface": {
             'self': [],
@@ -135,8 +135,7 @@ class SvcMonitor(object):
             'self': ['interface_route_table', 'virtual_machine', 'port_tuple'],
             'interface_route_table': [],
             'virtual_machine': [],
-            'port_tuple': [],
-            'service_instance': ['physical_interface'],
+            'port_tuple': ['physical_interface'],
             'physical_interface': ['service_instance']
         },
         "interface_route_table": {
