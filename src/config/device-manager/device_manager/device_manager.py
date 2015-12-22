d0cbd20b5aeb892264230e9f4e6872c107146dc6
facetothefate <facetothefate@gmail.com>
2015-12-22 13:57:51 -0500

    Upgrade PNF function with newest Schema
    1. add the port tuple
    2. delete the reference between the service instance and virtual machine interface
    
    Change-Id: I4b81f91a48b9792f07ff293f1efe0d1c4cea5e95
    close-bug: #1516147

--- src/config/device-manager/device_manager/device_manager.py
+++ src/config/device-manager/device_manager/device_manager.py
@@ -39,7 +39,7 @@ from cfgm_common.uve.cfgm_cpuinfo.ttypes import NodeStatusUVE, \
 from db import DBBaseDM, BgpRouterDM, PhysicalRouterDM, PhysicalInterfaceDM,\
     ServiceInstanceDM, LogicalInterfaceDM, VirtualMachineInterfaceDM, \
     VirtualNetworkDM, RoutingInstanceDM, GlobalSystemConfigDM, \
-    GlobalVRouterConfigDM, FloatingIpDM, InstanceIpDM, DMCassandraDB
+    GlobalVRouterConfigDM, FloatingIpDM, InstanceIpDM, DMCassandraDB, PortTupleDM
 from physical_router_config import PushConfigState
 from cfgm_common.dependency_tracker import DependencyTracker
 from sandesh.dm_introspect import ttypes as sandesh
@@ -90,17 +90,22 @@ class DeviceManager(object):
                      'virtual_network',
                      'floating_ip',
                      'instance_ip',
-                     'service_instance'],
+                     'port_tuple'],
             'logical_interface': ['virtual_network'],
             'virtual_network': ['logical_interface'],
             'floating_ip': ['virtual_network'],
             'instance_ip': ['virtual_network'],
-            'routing_instance': ['physical_interface'],
-            'service_instance': ['physical_interface']
+            'routing_instance': ['port_tuple','physical_interface'],
+            'port_tuple': ['physical_interface']
         },
         'service_instance': {
-            'self': ['virtual_machine_interface'],
-            'virtual_machine_interface': []
+            'self': ['port_tuple'],
+            'port_tuple':[],
+        },
+        'port_tuple':{
+            'self':['virtual_machine_interface','service_instance'],
+            'service_instance':['virtual_machine_interface'],
+            'virtual_machine_interface':['service_instance']
         },
         'virtual_network': {
             'self': ['physical_router',
@@ -229,6 +234,9 @@ class DeviceManager(object):
         pr_obj_list = PhysicalRouterDM.list_obj()
         pr_uuid_set = set([pr_obj['uuid'] for pr_obj in pr_obj_list])
         self._cassandra.handle_pr_deletes(pr_uuid_set)
+
+        for obj in PortTupleDM.list_obj():
+            PortTupleDM.locate(obj['uuid'],obj)
 
         for obj in pr_obj_list:
             pr = PhysicalRouterDM.locate(obj['uuid'], obj)
