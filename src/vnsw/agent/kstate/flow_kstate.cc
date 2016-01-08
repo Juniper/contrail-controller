1c6adaa8728d0054d54f04bd59f9c1d259395ce8
Ashok Singh <ashoksr@juniper.net>
2016-01-08 07:34:24 -0800

    Agent Flow Kstate introspect update
    Display the following additional information in Kstate introspect
    1. NH
    2. TCP flags
    3. Drop reason
    4. Underlay UDP source ports
    Also Update the flags field to display the full list of flags present in vrouter.
    
    Change-Id: I908690abfe775bc06b3ec2443a56979cecbbc06f
    Partial-Bug: #1532128

--- src/vnsw/agent/kstate/flow_kstate.cc
+++ src/vnsw/agent/kstate/flow_kstate.cc
@@ -54,20 +54,108 @@ const string FlowKState::FlagToStr(unsigned int flag) const {
             return " DNAT ";
         case VR_FLOW_FLAG_DPAT:
             return " DPAT ";
+        case VR_FLOW_FLAG_LINK_LOCAL:
+            return " LINK_LOCAL ";
+        case VR_FLOW_FLAG_EVICTED:
+            return " EVICTED ";
+        case VR_FLOW_FLAG_EVICT_CANDIDATE:
+            return " EVICT_CANDIDATE ";
+        case VR_FLOW_FLAG_NEW_FLOW:
+            return " NEW_FLOW ";
+        case VR_FLOW_FLAG_MODIFIED:
+            return " MODIFIED ";
+        case VR_RFLOW_VALID:
+            return " RFLOW_VALID ";
         default:
             return " INVALID ";
     }
 }
 
-void FlowKState::UpdateFlagStr(string &str, bool &set, unsigned sflag, 
+const string FlowKState::TcpFlagToStr(unsigned int flag) const {
+    switch(flag) {
+        case VR_FLOW_TCP_SYN:
+            return " SYN ";
+        case VR_FLOW_TCP_SYN_R:
+            return " SYN_R ";
+        case VR_FLOW_TCP_ESTABLISHED:
+            return " ESTB ";
+        case VR_FLOW_TCP_ESTABLISHED_R:
+            return " ESTB_R ";
+        case VR_FLOW_TCP_FIN:
+            return " FIN ";
+        case VR_FLOW_TCP_FIN_R:
+            return " FIN_R ";
+        case VR_FLOW_TCP_RST:
+            return " RST ";
+        case VR_FLOW_TCP_HALF_CLOSE:
+            return " HALF_CLOSE ";
+        case VR_FLOW_TCP_DEAD:
+            return " DEAD ";
+        default:
+            return " INVALID ";
+    }
+}
+
+void FlowKState::UpdateFlagStr(string &str, bool &set, bool tcp, unsigned sflag,
                                unsigned cflag) const {
+    string flag_str;
     if (sflag & cflag) {
-        if (set) {
-            str.append("|" + FlagToStr(cflag));
+        if (tcp) {
+            flag_str.assign(TcpFlagToStr(cflag));
         } else {
-            str.assign(FlagToStr(cflag));
+            flag_str.assign(FlagToStr(cflag));
+        }
+        if (set) {
+            str.append("|" + flag_str);
+        } else {
+            str.assign(flag_str);
             set = true;
         }
+    }
+}
+
+const std::string FlowKState::DropCodeToStr(uint8_t drop_code) const {
+    switch (drop_code) {
+    case VR_FLOW_DR_UNKNOWN:
+        return "Unknown";
+    case VR_FLOW_DR_UNAVIALABLE_INTF:
+        return "IntfErr";
+    case VR_FLOW_DR_IPv4_FWD_DIS:
+        return "Ipv4Dis";
+    case VR_FLOW_DR_UNAVAILABLE_VRF:
+        return "VrfErr";
+    case VR_FLOW_DR_NO_SRC_ROUTE:
+        return "NoSrcRt";
+    case VR_FLOW_DR_NO_DST_ROUTE:
+        return "NoDstRt";
+    case VR_FLOW_DR_AUDIT_ENTRY:
+        return "Audit";
+    case VR_FLOW_DR_VRF_CHANGE:
+        return "VrfChange";
+    case VR_FLOW_DR_NO_REVERSE_FLOW:
+        return "NoRevFlow";
+    case VR_FLOW_DR_REVERSE_FLOW_CHANGE:
+        return "RevFlowChng";
+    case VR_FLOW_DR_NAT_CHANGE:
+        return "NatChng";
+    case VR_FLOW_DR_FLOW_LIMIT:
+        return "FlowLim";
+    case VR_FLOW_DR_LINKLOCAL_SRC_NAT:
+        return "LinkSrcNatErr";
+    case VR_FLOW_DR_POLICY:
+        return "Policy";
+    case VR_FLOW_DR_OUT_POLICY:
+        return "OutPolicy";
+    case VR_FLOW_DR_SG:
+        return "SG";
+    case VR_FLOW_DR_OUT_SG:
+        return "OutSG";
+    case VR_FLOW_DR_REVERSE_SG:
+        return "RevSG";
+    case VR_FLOW_DR_REVERSE_OUT_SG:
+        return "RevOutSG";
+    default:
+        return "Unknown";
     }
 }
 
@@ -75,6 +163,7 @@ void FlowKState::SetFlowData(vector<KFlowInfo> &list,
                              const vr_flow_entry *k_flow, 
                              const int index) const {
     KFlowInfo data;
+    bool action_drop = false;
     int family = (k_flow->fe_key.flow_family == AF_INET)? Address::INET :
         Address::INET6;
     IpAddress sip, dip;
@@ -82,7 +171,7 @@ void FlowKState::SetFlowData(vector<KFlowInfo> &list,
                   family, &sip, &dip);
 
     string action_str;
-    string flag_str;
+    string flag_str, tcp_flags;
     data.set_index((unsigned int)index);
     data.set_sport((unsigned)ntohs(k_flow->fe_key.flow4_sport));
     data.set_dport((unsigned)ntohs(k_flow->fe_key.flow4_dport));
@@ -90,12 +179,14 @@ void FlowKState::SetFlowData(vector<KFlowInfo> &list,
     data.set_dip(dip.to_string());
     data.set_vrf_id(k_flow->fe_vrf);
     data.set_proto(k_flow->fe_key.flow4_proto);
+    data.set_nhid(k_flow->fe_key.flow_nh_id);
     switch (k_flow->fe_action) {
         case VR_FLOW_ACTION_FORWARD:
             action_str.assign("FORWARD");
             break;
         case VR_FLOW_ACTION_DROP:
             action_str.assign("DROP");
+            action_drop = true;
             break;
         case VR_FLOW_ACTION_NAT:
             action_str.assign("NAT");
@@ -108,14 +199,59 @@ void FlowKState::SetFlowData(vector<KFlowInfo> &list,
     }
     data.set_action(action_str);
     bool assigned = false;
-    UpdateFlagStr(flag_str, assigned, k_flow->fe_flags, VR_FLOW_FLAG_ACTIVE);
-    UpdateFlagStr(flag_str, assigned, k_flow->fe_flags, VR_FLOW_FLAG_MIRROR);
-    UpdateFlagStr(flag_str, assigned, k_flow->fe_flags, VR_FLOW_FLAG_VRFT);
-    UpdateFlagStr(flag_str, assigned, k_flow->fe_flags, VR_FLOW_FLAG_SNAT);
-    UpdateFlagStr(flag_str, assigned, k_flow->fe_flags, VR_FLOW_FLAG_SPAT);
-    UpdateFlagStr(flag_str, assigned, k_flow->fe_flags, VR_FLOW_FLAG_DNAT);
-    UpdateFlagStr(flag_str, assigned, k_flow->fe_flags, VR_FLOW_FLAG_SPAT);
+    UpdateFlagStr(flag_str, assigned, false, k_flow->fe_flags,
+                  VR_FLOW_FLAG_ACTIVE);
+    UpdateFlagStr(flag_str, assigned, false, k_flow->fe_flags,
+                  VR_FLOW_FLAG_MIRROR);
+    UpdateFlagStr(flag_str, assigned, false, k_flow->fe_flags,
+                  VR_FLOW_FLAG_VRFT);
+    UpdateFlagStr(flag_str, assigned, false, k_flow->fe_flags,
+                  VR_FLOW_FLAG_SNAT);
+    UpdateFlagStr(flag_str, assigned, false, k_flow->fe_flags,
+                  VR_FLOW_FLAG_SPAT);
+    UpdateFlagStr(flag_str, assigned, false, k_flow->fe_flags,
+                  VR_FLOW_FLAG_DNAT);
+    UpdateFlagStr(flag_str, assigned, false, k_flow->fe_flags,
+                  VR_FLOW_FLAG_SPAT);
+    UpdateFlagStr(flag_str, assigned, false, k_flow->fe_flags,
+                  VR_FLOW_FLAG_LINK_LOCAL);
+    UpdateFlagStr(flag_str, assigned, false, k_flow->fe_flags,
+                  VR_FLOW_FLAG_EVICTED);
+    UpdateFlagStr(flag_str, assigned, false, k_flow->fe_flags,
+                  VR_FLOW_FLAG_EVICT_CANDIDATE);
+    UpdateFlagStr(flag_str, assigned, false, k_flow->fe_flags,
+                  VR_FLOW_FLAG_NEW_FLOW);
+    UpdateFlagStr(flag_str, assigned, false, k_flow->fe_flags,
+                  VR_FLOW_FLAG_MODIFIED);
+    UpdateFlagStr(flag_str, assigned, false, k_flow->fe_flags, VR_RFLOW_VALID);
     data.set_flags(flag_str);
+
+    if (k_flow->fe_key.flow4_proto == IPPROTO_TCP) {
+        assigned = false;
+        UpdateFlagStr(tcp_flags, assigned, true, k_flow->fe_tcp_flags,
+                      VR_FLOW_TCP_SYN);
+        UpdateFlagStr(tcp_flags, assigned, true, k_flow->fe_tcp_flags,
+                      VR_FLOW_TCP_SYN_R);
+        UpdateFlagStr(tcp_flags, assigned, true, k_flow->fe_tcp_flags,
+                      VR_FLOW_TCP_ESTABLISHED);
+        UpdateFlagStr(tcp_flags, assigned, true, k_flow->fe_tcp_flags,
+                      VR_FLOW_TCP_ESTABLISHED_R);
+        UpdateFlagStr(tcp_flags, assigned, true, k_flow->fe_tcp_flags,
+                      VR_FLOW_TCP_FIN);
+        UpdateFlagStr(tcp_flags, assigned, true, k_flow->fe_tcp_flags,
+                      VR_FLOW_TCP_FIN_R);
+        UpdateFlagStr(tcp_flags, assigned, true, k_flow->fe_tcp_flags,
+                      VR_FLOW_TCP_RST);
+        UpdateFlagStr(tcp_flags, assigned, true, k_flow->fe_tcp_flags,
+                      VR_FLOW_TCP_HALF_CLOSE);
+        UpdateFlagStr(tcp_flags, assigned, true, k_flow->fe_tcp_flags,
+                      VR_FLOW_TCP_DEAD);
+        data.set_tcp_flags(tcp_flags);
+    }
+    if (action_drop) {
+        data.set_drop_reason(DropCodeToStr(k_flow->fe_drop_reason));
+    }
+    data.set_underlay_udp_sport(k_flow->fe_udp_src_port);
     data.set_rflow(k_flow->fe_rflow);
     data.set_d_vrf_id(k_flow->fe_dvrf);
     data.set_bytes(k_flow->fe_stats.flow_bytes);
