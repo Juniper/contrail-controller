6f97f2db2213eb3ce93bf973d468714e528f5529
Ashok Singh <ashoksr@juniper.net>
2016-01-08 03:24:08 -0800

    Fix Centos 65 agent compilation issues. Closes-Bug: #1532159
    Change-Id: Ia8b77619c80737000d5bd949534c9e0a16967359

--- src/ksync/ksync_sock_user.cc
+++ src/ksync/ksync_sock_user.cc
@@ -614,9 +614,9 @@ void KSyncSockTypeMap::Init(boost::asio::io_service &ios, int count) {
     assert(singleton_ == NULL);
     singleton_ = new KSyncSockTypeMap(ios);
 
-    singleton_->local_ep_.address(ip::address::from_string("127.0.0.1"));
+    singleton_->local_ep_.address(boost::asio::ip::address::from_string("127.0.0.1"));
     singleton_->local_ep_.port(0);
-    singleton_->sock_.open(ip::udp::v4());
+    singleton_->sock_.open(boost::asio::ip::udp::v4());
     singleton_->sock_.bind(singleton_->local_ep_);
     singleton_->local_ep_ = singleton_->sock_.local_endpoint();
 
