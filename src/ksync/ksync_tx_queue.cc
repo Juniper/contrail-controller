16b26a0151174a62d5626d3e366b72336ab96112
Praveen K V <kvpraveen@juniper.net>
2016-01-10 10:29:37 +0530

    Ignore ksync write failures due to signal interrupts.
    Change-Id: Ibbcb83885dcfe3ea1581b68a3991cb779c620427
    Closes-Bug: 1520084

--- src/ksync/ksync_tx_queue.cc
+++ src/ksync/ksync_tx_queue.cc
@@ -96,7 +96,16 @@ bool KSyncTxQueue::EnqueueInternal(IoContext *io_context) {
         max_queue_len_ = ncount;
     if (ncount == 1) {
         uint64_t u = 1;
-        assert(write(event_fd_, &u, sizeof(u)) == sizeof(u));
+        int res = 0;
+        while ((res = write(event_fd_, &u, sizeof(u))) < (int)sizeof(u)) {
+            int ec = errno;
+            if (ec != EINTR && ec != EIO) {
+                LOG(ERROR, "KsyncTxQueue write failure : " << ec << " : "
+                    << strerror(ec));
+                assert(0);
+            }
+        }
+
         write_events_++;
     }
     return true;
