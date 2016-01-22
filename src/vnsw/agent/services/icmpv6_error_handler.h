2f1d72587010d8a8ab7a8070fe19ef7fd4e21c31
Jayaram <sajayaram@juniper.net>
2016-01-22 15:10:19 +0530

    icmpv6 too big packets error handling code changes are incorporated Vrouter-agent default gate way will respond for the packets bigger than mtu size. And also crash is addressed in agent code when v6 ping happens between the two VMS, agent crashes Close-Bug: #1424995
    Change-Id: Ibecfa9e6ac9034d1530d86d499b6933bf937b36e

---
+++ src/vnsw/agent/services/icmpv6_error_handler.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
+ */
+
+#ifndef VNSW_AGENT_SERVICES_ICMPV6_ERROR_HANDLER_H_
+#define VNSW_AGENT_SERVICES_ICMPV6_ERROR_HANDLER_H_
+
+#include "pkt/proto_handler.h"
+#include "services/icmp_proto.h"
+
+class Icmpv6ErrorProto;
+
+// ICMP protocol handler
+class Icmpv6ErrorHandler : public ProtoHandler {
+ public:
+    static const int ICMPV6_PAYLOAD_LEN = 64;
+    Icmpv6ErrorHandler(Agent *agent, Icmpv6ErrorProto *proto,
+                       boost::shared_ptr<PktInfo> info,
+                       boost::asio::io_service *io);
+    virtual ~Icmpv6ErrorHandler();
+
+    bool Run();
+
+ private:
+    bool ValidatePacket();
+    bool SendIcmpv6Error(VmInterface *intf);
+
+    Icmpv6ErrorProto *proto_;
+    DISALLOW_COPY_AND_ASSIGN(Icmpv6ErrorHandler);
+};
+
+#endif  // VNSW_AGENT_SERVICES_ICMPV6_ERROR_HANDLER_H_
