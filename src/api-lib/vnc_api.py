bbba519e1e5ae92b766bfd392595683fe7ef17f6
Antoine Eiche <antoine.eiche@cloudwatt.com>
2016-02-16 12:47:46 +0100

    Add vnc_api method set_user_roles
    This allows to manually set the roles of the user in the HTTP header.
    
    Change-Id: Ifdf28ce18de55b3bed3e59de56de415da1839c2f
    Partial-Bug: 1546057

--- src/api-lib/vnc_api.py
+++ src/api-lib/vnc_api.py
@@ -308,7 +308,7 @@ class VncApi(object):
             if 'user' in self._user_info:
                 self._headers['X-API-USER'] = self._user_info['user']
             if 'role' in self._user_info:
-                self._headers['X-API-ROLE'] = self._user_info['role']
+                self.set_user_roles([self._user_info['role']])
 
         #self._http = HTTPClient(self._web_host, self._web_port,
         #                        network_timeout = 300)
@@ -1136,6 +1136,14 @@ class VncApi(object):
     def set_auth_token(self, token):
         """Park user token for forwarding to API server for RBAC."""
         self._headers['X-AUTH-TOKEN'] = token
+    #end set_auth_token
+
+    def set_user_roles(self, roles):
+        """Park user roles for forwarding to API server for RBAC.
+
+        :param roles: list of roles
+        """
+        self._headers['X-API-ROLE'] = (',').join(roles)
     #end set_auth_token
 
 #end class VncApi
