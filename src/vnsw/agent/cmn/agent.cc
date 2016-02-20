300d59837c3767941fd3cc4e80d2f99c76f0c2a6
Hari <haripk@juniper.net>
2016-02-20 19:50:50 +0530

    Profile task needs to have exclusion with DB task.
    Change-Id: Ifac46b3e28b20f76b739ae8e058a7e3abf4200d9
    closes-bug: 1546929

--- src/vnsw/agent/cmn/agent.cc
+++ src/vnsw/agent/cmn/agent.cc
@@ -135,6 +135,7 @@ void Agent::SetAgentTaskPolicy() {
         "Agent::Uve",
         "Agent::KSync",
         "Agent::PktFlowResponder",
+        agent_profile,
         kTaskHealthCheck,
         AGENT_SHUTDOWN_TASKNAME,
         AGENT_INIT_TASKNAME
