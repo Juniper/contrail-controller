57e2f510853b9f24415b22827e11d56bacd745c4
varun_lodaya <varun_lodaya@symantec.com>
2016-02-22 10:00:06 -0800

    Fix to support multiple cert managers with LBaaS
    This fix allows users to be able to use differet cert managers
    with LBaaS custom attributes. It adds a Generic Cert Manager to
    the code which can copy files from a specific folder.
    Adding metaclass to cert_manager.
    It also fixes the crt file name from generic to instance specific
    to support multiple certs under the same folder.
    Modified log formatter to provide file/line_nos.
    Also, added few unittests to custom attribute code.
    
    Conflicts:
    	src/vnsw/agent/oper/instance_manager.cc
    	src/vnsw/agent/oper/netns_instance_adapter.cc
    	src/vnsw/opencontrail-vrouter-netns/opencontrail_vrouter_netns/haproxy_config.py
    	src/vnsw/opencontrail-vrouter-netns/opencontrail_vrouter_netns/haproxy_process.py
    	src/vnsw/opencontrail-vrouter-netns/opencontrail_vrouter_netns/vrouter_netns.py
    
    Closes-Bug: #1547645
    Change-Id: I65cef1690cad6bad229e176615e18cad246334eb

--- src/vnsw/opencontrail-vrouter-netns/opencontrail_vrouter_netns/vrouter_netns.py
+++ src/vnsw/opencontrail-vrouter-netns/opencontrail_vrouter_netns/vrouter_netns.py
@@ -61,7 +61,7 @@ class NetnsManager(object):
     def __init__(self, vm_uuid, nic_left, nic_right, other_nics=None,
                  root_helper='sudo', cfg_file=None, update=False,
                  pool_id=None, gw_ip=None, namespace_name=None,
-                 keystone_auth_cfg_file=None, loadbalancer_id=None):
+                 custom_attr_cfg_file=None, loadbalancer_id=None):
         self.vm_uuid = vm_uuid
         if namespace_name is None:
             self.namespace = self.NETNS_PREFIX + self.vm_uuid
@@ -88,7 +88,7 @@ class NetnsManager(object):
         self.cfg_file = cfg_file
         self.update = update
         self.gw_ip = gw_ip
-        self.keystone_auth_cfg_file = keystone_auth_cfg_file
+        self.custom_attr_cfg_file = custom_attr_cfg_file
 
     def _get_tap_name(self, uuid_str):
             return (self.TAP_PREFIX + uuid_str)[:self.DEV_NAME_LEN]
@@ -138,7 +138,7 @@ class NetnsManager(object):
             self.create()
 
         haproxy_process.start_update_haproxy(self.cfg_file, self.namespace, True,
-                                             self.keystone_auth_cfg_file)
+                                             self.custom_attr_cfg_file)
 
         try:
             self.ip_ns.netns.execute(['route', 'add', 'default', 'gw', self.gw_ip])
@@ -330,9 +330,9 @@ class VRouterNetns(object):
             default=None,
             help=("Loadbalancer"))
         create_parser.add_argument(
-            "--keystone-auth-cfg-file",
+            "--custom-attr-cfg-file",
             default=None,
-            help=("Keystone auth config file for lbaas"))
+            help=("Custom Attributes config file for lbaas"))
         create_parser.set_defaults(func=self.create)
 
         destroy_parser = subparsers.add_parser('destroy')
@@ -400,7 +400,7 @@ class VRouterNetns(object):
                              update=self.args.update, gw_ip=self.args.gw_ip,
                              pool_id=self.args.pool_id,
                              loadbalancer_id=self.args.loadbalancer_id,
-                             keystone_auth_cfg_file=self.args.keystone_auth_cfg_file)
+                             custom_attr_cfg_file=self.args.custom_attr_cfg_file)
 
         if (self.args.update is False):
             if netns_mgr.is_netns_already_exists():
