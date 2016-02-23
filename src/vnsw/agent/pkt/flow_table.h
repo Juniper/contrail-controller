eb253ee49ffa230d123effbb48b368347f2f8354
Ashok Singh <ashoksr@juniper.net>
2016-02-23 01:02:36 -0800

    Make UUID random generator as non-static member of FlowTable
    With multi-thread processing of flows, the static members cannot be used
    anymore. Define random_generator per flow-table and move UUID generation
    for egress UUID to flow module from FlowStatsCollector module
    
    Closes-Bug: #1548615
    (cherry picked from commit e8512920c8aede492983b5866f5433037a18e06f)
    
    Change-Id: Ic391920a13b873cc0e920a53a93e1434abc6c91f

--- src/vnsw/agent/pkt/flow_table.h
+++ src/vnsw/agent/pkt/flow_table.h
@@ -128,7 +128,6 @@ struct Inet4FlowKeyCmp {
 
 class FlowTable {
 public:
-    static boost::uuids::random_generator rand_gen_;
 
     typedef std::map<FlowKey, FlowEntry *, Inet4FlowKeyCmp> FlowEntryMap;
     typedef std::pair<FlowKey, FlowEntry *> FlowEntryMapPair;
@@ -220,6 +219,7 @@ public:
     bool RevaluateSgList(FlowEntry *flow, const AgentRoute *rt,
                          const SecurityGroupList &sg_list);
     bool RevaluateRpfNH(FlowEntry *flow, const AgentRoute *rt);
+    boost::uuids::uuid rand_gen();
 
     void UpdateKSync(FlowEntry *flow, bool update);
     void DeleteKSync(FlowEntry *flow);
@@ -259,6 +259,7 @@ private:
     void GetMutexSeq(tbb::mutex &mutex1, tbb::mutex &mutex2,
                      tbb::mutex **mutex_ptr_1, tbb::mutex **mutex_ptr_2);
     Agent *agent_;
+    boost::uuids::random_generator rand_gen_;
     uint16_t table_index_;
     FlowTableKSyncObject *ksync_object_;
     FlowEntryMap flow_entry_map_;
