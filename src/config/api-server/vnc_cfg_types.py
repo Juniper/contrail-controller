00f48ce8529bd46768822e5f6300d6e6fa6e237f
Ignatious Johnson Christopher <ijohnson@juniper.net>
2016-02-24 12:53:15 -0800

    Do not allow configuring multi_policy_service_chain flag on a network if it has external route targets configured either in the 'import-and-export' table or in both 'import' and 'export' tables.
    Change-Id: I34355240bd17cc1c3d21296d00a4bc413671af13
    Closes-Bug: 1547751

--- src/config/api-server/vnc_cfg_types.py
+++ src/config/api-server/vnc_cfg_types.py
@@ -681,9 +681,26 @@ class VirtualNetworkServer(Resource, VirtualNetwork):
 
         return (True, '')
 
+    @classmethod
+    def _is_multi_policy_service_chain_supported(cls, obj_dict, read_result={}):
+        read_result.update(obj_dict)
+        if read_result.get('multi_policy_service_chains_enabled'):
+            import_export_targets = read_result.get('route_target_list', {})
+            import_targets = read_result.get('import_route_target_list', {})
+            export_targets = read_result.get('export_route_target_list', {})
+            if (import_export_targets.get('route_target', []) or
+                    import_targets.get('route_target', []) and
+                    export_targets.get('route_target', [])):
+                msg = "Multi policy service chains are not supported, "
+                msg += "with both import export external route targets"
+                return (False, (500, msg))
+        return (True, '')
 
     @classmethod
     def pre_dbe_create(cls, tenant_name, obj_dict, db_conn):
+        (ok, response) = cls._is_multi_policy_service_chain_supported(obj_dict)
+        if not ok:
+            return (ok, response)
         user_visibility = obj_dict['id_perms'].get('user_visible', True)
         verify_quota_kwargs = {'db_conn': db_conn,
                                'fq_name': obj_dict['fq_name'],
@@ -770,6 +787,10 @@ class VirtualNetworkServer(Resource, VirtualNetwork):
         if not read_ok:
             return (False, (500, read_result))
 
+        (ok, response) = cls._is_multi_policy_service_chain_supported(obj_dict,
+                                                                      read_result)
+        if not ok:
+            return (ok, response)
 
         (ok, result) = cls.addr_mgmt.net_check_subnet(obj_dict)
         if not ok:
