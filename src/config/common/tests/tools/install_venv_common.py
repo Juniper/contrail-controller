4cebabb9bf609be8567a1bb1fe9c8dd400102343
Ignatious Johnson Christopher <ijohnson@juniper.net>
2016-02-24 18:10:03 -0800

    1. python-keystoneclient latest has moved middleware to keystonemiddleware. fixing it in unit-test and code. Also detecting failure of .venv building is broken so invoke testr run with pipefail. 2. Also clearing pip cache entry for contrail python packages before installing requiements in VENV. 3. Removing distribute from test-requirements, becaause distribute is deprecated in favour of setuptools 4. fixed test failues in test_perms2 test module
    Closes-Bug: 1549065
    Change-Id: Iabc8231eb1b6e0051e07744621440ddb0f828164

--- src/config/common/tests/tools/install_venv_common.py
+++ src/config/common/tests/tools/install_venv_common.py
@@ -30,6 +30,15 @@ import optparse
 import os
 import subprocess
 import sys
+import fnmatch
+import re
+
+
+def find_files(directory, file_pattern):
+    for root, dirs, files in os.walk(directory):
+        for afile in files:
+            if fnmatch.fnmatch(afile, file_pattern):
+                yield os.path.join(root, afile)
 
 
 class InstallVenv(object):
@@ -115,6 +124,33 @@ class InstallVenv(object):
         self.run_command(cmd_array + list(args),
                           redirect_output=False)
 
+    def get_requirements(self):
+        with open(self.requirements, 'r+') as fd:
+            reqs = [req.strip() for req in fd.readlines()]
+        gteq_or_lteq_or_eqeq = re.compile('(.*)[>=<]+[>=<]+')
+        gt_or_lt_or_eq = re.compile('(.*)[>=<]+')
+        requirements = []
+        for req in reqs:
+            for regexp in [gteq_or_lteq_or_eqeq, gt_or_lt_or_eq]:
+                match = regexp.match(req)
+                if match:
+                    requirements.append(match.group(1))
+                    continue
+            else:
+                requirements.append(req)
+        return requirements
+
+    def pip_clear_cache(self, find_links):
+        contrail_reqs = []
+        for req in self.get_requirements():
+            for find_link in find_links:
+                if find_files(find_link, req):
+                    contrail_reqs.append(req)
+        for contrail_req in contrail_reqs:
+            cache_dir = os.path.expanduser('~/.cache/pip')
+            for cachefile in find_files(cache_dir, contrail_req):
+                os.remove(cachefile)
+
     def install_dependencies(self, find_links):
         print('Installing dependencies with pip (this can take a while)...')
 
@@ -124,6 +160,7 @@ class InstallVenv(object):
         self.pip_install(find_links, 'setuptools')
         self.pip_install(find_links, 'pbr')
 
+        self.pip_clear_cache(find_links)
         self.pip_install(find_links, '-r', self.requirements,
                          '-r', self.test_requirements, '--pre')
 
