b7322c455d3ecb2f6dbab81bd6bce72dbf6c8a0b
Ranjeet R <rranjeet@juniper.net>
2016-03-08 16:28:17 -0800

    Fixes: API server: A slash is appended to vhost name for accessing rabbitmq
    RabbitMQ URL has an extra / which fails RMQ connection
    
    Change-Id: I265bd3d02f33a5e7a1165e6fc269ef8ff8592912
    ClosesBug:1477814

--- src/config/api-server/tests/test_kombu.py
+++ src/config/api-server/tests/test_kombu.py
@@ -3,12 +3,14 @@ from gevent import monkey
 monkey.patch_all()
 import kombu
 import sys
+import logging
 
 import unittest
 from flexmock import flexmock
 from cfgm_common import vnc_kombu
 from vnc_cfg_api_server import vnc_cfg_ifmap
 from distutils.version import LooseVersion
+
 
 if LooseVersion(kombu.__version__) >= LooseVersion("2.5.0"):
     is_kombu_client_v1 = False
@@ -38,7 +40,7 @@ class TestIfmapKombuClient(unittest.TestCase):
         self.db_client_mgr = flexmock(operational=True, _sandesh=None,
                        config_log=lambda *args, **kwargs: None,
                        get_server_port=lambda: 8082)
-        self._url_template = "pyamqp://%s:%s@%s:%d/%s/"
+        self._url_template = "pyamqp://%s:%s@%s:%d/%s"
         self.mock_producer = flexmock(operational = True)
         self.mock_consumer = flexmock(operational = True)
         flexmock(vnc_kombu.kombu.Connection, __new__ = lambda *args, **kwargs: self.mock_connect)
@@ -60,7 +62,7 @@ class TestIfmapKombuClient(unittest.TestCase):
         check_value = []
         def Connection(self, urls):
             if set(urls) != set(check_value):
-                raise WrongValueException()
+                raise WrongValueException('expected %s - received %s', str(check_value), str(urls))
             else:
                 raise CorrectValueException()
 
