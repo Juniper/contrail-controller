32f8880e4e034fec50215ca8bc4498900cc4a638
Ignatious Johnson Christopher <ijohnson@juniper.net>
2016-03-21 21:43:35 +0000

    Removing distribute from test-requirements, becaause distribute is deprecated in favour of setuptools
    python-keystoneclient latest has moved middleware to keystonemiddleware.
    fixing it in unit-test and code. Also detecting failure of .venv building
    is broken so invoke testr run with pipefail.
    
    Closes-Bug: 1559867
    
    Fixes: API server: A slash is appended to vhost name for accessing rabbitmq
    RabbitMQ URL has an extra / which fails RMQ connection
    Closes-Bug:1477814
    
    Change-Id: I8a82905ff960cf73ec8f4d6702ae8d2b087adbfa

--- src/config/api-server/tests/test_crud_basic.py
+++ src/config/api-server/tests/test_crud_basic.py
@@ -1412,7 +1412,7 @@ class TestStaleLockRemoval(test_case.ApiServerTestCase):
         with test_common.flexmocks([
             (self._api_server._db_conn, 'dbe_create', stub),
             (self._api_server.get_resource_class('virtual-network'),
-             'post_dbe_create', stub)]):
+             'http_post_collection', stub)]):
             self._create_test_object()
             with ExpectedException(RefsExistError) as e:
                 self._create_test_object()
@@ -1566,7 +1566,7 @@ class TestLocalAuth(test_case.ApiServerTestCase):
                                    ('KEYSTONE', 'admin_password', 'bar'),])
 
     def setup_flexmock(self):
-        from keystoneclient.middleware import auth_token
+        from keystonemiddleware import auth_token
         class FakeAuthProtocol(object):
             def __init__(self, app, *args, **kwargs):
                 self._app = app
