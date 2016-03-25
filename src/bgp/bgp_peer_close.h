d2229079e156aea3b376d2a0347d72d9f3ab536e
Ananth Suryanarayana <anantha@juniper.net>
2016-03-25 15:43:41 -0700

    Add support to exchange BGP GR Capability with supported address families
    o Send and receive GR Capability with restarttime and supported adresss families
    o Add exchanged GR families to sandesh messages
    o Add necessary tests to bgp_proto_test
    o If GR families is not the same as exchanged multi-protocol families, don't do
      GR because the code currently does not support afi specific GR (yet)
    o Most use cases shall have identical set of supported families for route
      exchange (as in MP Protocol Capability) and those supported with graceful
      restart? (as in Graceful Restart Capability)
    
    TODO
    ====
    o Handle afi specific closure
    o No tests are there yet for mis-match between multi-protocol afis and gr afis
    o More test scenarios with different GR families advertised before and after
      GR not there yet
    o LLGR, etc.
    
    Change-Id: I1177544ad7a140a001975a1d0485b77a4add49f6
    Partial-Bug: #1537933

--- src/bgp/bgp_peer_close.h
+++ src/bgp/bgp_peer_close.h
@@ -37,7 +37,8 @@ class PeerCloseManager {
 public:
     enum State { NONE, STALE, GR_TIMER, SWEEP, DELETE };
 
-    static const int kDefaultGracefulRestartTimeMsecs = 60*1000;
+    // RestartTime field in BGP GR capability is only 12 bits wide.
+    static const int kDefaultGracefulRestartTimeMsecs = 0x0fff*1000;
 
     // thread: bgp::StateMachine
     explicit PeerCloseManager(IPeer *peer);
