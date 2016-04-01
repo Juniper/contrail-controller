e7c08ec70dbba726ddfc1c6d29d4898e81a6a2f4
Senthilnathan Murugappan <msenthil@juniper.net>
2016-04-01 15:46:27 -0700

    Parse and send session persistence info to lbaas driver in case of lbaasv2
    Change-Id: Ie089f8481dc076aef78c87b7d5bfeb2fc3b6e116
    Partial-Bug:#1565135

--- src/config/svc-monitor/svc_monitor/tests/test_ha_proxy.py
+++ src/config/svc-monitor/svc_monitor/tests/test_ha_proxy.py
@@ -171,7 +171,8 @@ OpencontrailLoadbalancerDriver")
         pool_obj['loadbalancer_pool_provider'] = 'opencontrail'
         pool_obj['loadbalancer_pool_properties'] = \
             {'protocol': 'HTTP', 'subnet_id': 'subnet-id',
-             'loadbalancer_method': 'ROUND_ROBIN', 'admin_state': 'true'}
+             'loadbalancer_method': 'ROUND_ROBIN', 'admin_state': 'true',
+             'session_persistence': None, 'persistence_cookie_name': None}
         if vip:
             pool_obj['virtual_ip_back_refs']=[{'uuid': vip.uuid}]
         if hm:
