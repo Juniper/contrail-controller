f9c5c67e249df350ae1fe9b3043456d05a2b8652
Manish Singh <manishs@juniper.net>
2016-04-04 14:46:00 +0530

    Deadlock in agent.
    Problem:
    It was seen that if a packet is trapped for flow setup with same source IP and
    destination IP is classified as nat flow then rflow key becomes similar to flow key.
    This in turn used to enter in deadlock as agent will try to attempt lock both on
    flow and rflow located using above keys. (Both will be same).
    
    Mentioned bug exposed this issue.
    In bug ICMP TTL expired was received for which agent generated a packet to
    switch to VM-port from where packet with TTL 1 was generated. This packet
    resulted in flow trap and agent processed the same. (agent packet was
    generated with same SIP and DIP).
    Deadlock results in flows going to hold state.
    
    This fix does not cover agent generating packet with same SIP and DIP and
    flow trap for agent generated packet.
    It only fixes deadlock.
    
    Solution:
    In packet flow when flow and rflow keys are created, match them and if they are
    same, nullify rflow and mark flow as short flow.
    
    Change-Id: I52af611229ea858abdce444911a59accd4058a36
    Partial-bug: #1556290

--- src/vnsw/agent/pkt/flow_entry.h
+++ src/vnsw/agent/pkt/flow_entry.h
@@ -291,6 +291,7 @@ class FlowEntry {
         SHORT_LINKLOCAL_SRC_NAT,
         SHORT_FAILED_VROUTER_INSTALL,
         SHORT_INVALID_L2_FLOW,
+        SHORT_SAME_FLOW_RFLOW_KEY,
         SHORT_MAX
     };
 
