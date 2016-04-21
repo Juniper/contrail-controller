abf6a0bf33ec3edfd6ae5d55cb668023c4614311
Manish Singh <manishs@juniper.net>
2016-04-21 15:49:33 +0530

    Dependant commit.
    https://review.opencontrail.org/#/c/19524/
    Partial-bug: #1551576
    
    Change-Id: I961a0fbf20b2733f5500daf3cf50c648283b9427

---
+++ src/vnsw/agent/vrouter/ksync/nat_port_ksync.cc
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2016 Juniper Networks, Inc. All rights reserved.
+ */
+
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+
+#include <base/logging.h>
+#include <ksync/ksync_index.h>
+#include <ksync/ksync_entry.h>
+#include <ksync/ksync_object.h>
+#include <ksync/ksync_netlink.h>
+#include <ksync/ksync_sock.h>
+
+#include "ksync_init.h"
+
+void vr_nat_port_req::Process(SandeshContext *context) {
+}
