29aaa5639224eaf4c273bb68c7719ab36280c8cf
Ravi BK <ravibk@juniper.net>
2016-04-22 12:43:14 +0530

    Issue: debug value specified in config file(contrail-vrouter-agent.conf)     is not honoured properly. If debug keyword is specified in the file,     irrespective of the value, debug flags at other classes(Agent, KSyncDebug) are set.     Fix: Definition of debug program option is modified as a name/value pair     and flag is set properly by retrieving the value.     closes-bug: #1561005
        Change-Id: Ibc837537fac857157ba5c54030a679e4bf6ed011
    
    Change-Id: Ia7fe9061c62fcd96b4582c266007a2d0508567e8

--- src/vnsw/agent/init/test/test_agent_init.cc
+++ src/vnsw/agent/init/test/test_agent_init.cc
@@ -64,6 +64,7 @@ TEST_F(FlowTest, Agent_Conf_file_1) {
     EXPECT_STREQ(param.agent_base_dir().c_str(), "/var/lib/contrail");
     EXPECT_EQ(param.subnet_hosts_resolvable(), true);
     EXPECT_EQ(param.pkt0_tx_buffer_count(), 2000);
+    EXPECT_TRUE(param.debug());
 }
 
 TEST_F(FlowTest, Agent_Conf_file_2) {
@@ -316,6 +317,7 @@ TEST_F(FlowTest, Default_Cmdline_arg1) {
     EXPECT_STREQ(param.log_level().c_str(), "SYS_ERR");
     EXPECT_TRUE(param.isXenMode());
     EXPECT_EQ(param.agent_mode(), AgentParam::TSN_AGENT);
+    EXPECT_FALSE(param.debug());
 }
 
 /* Some command line args have default values. If user has not passed these
