922775d81c9f88a4d838cbb75d71d6ed8e1acd0c
Sachin Bansal <sbansal@juniper.net>
2016-04-26 12:17:58 -0700

    Do not update bgpaas-client objects if not necessary
    Currently, bgpaas-client objects get updated with global asn when schema
    transformer restarts. They get updated again later with correct asn, but this
    causes a connection reset. We should avoid both these updates.
    
    Change-Id: Ia61c6434a59978d083fed847ef0955f7049f8d3c
    Closes-Bug: 1575323

--- src/config/schema-transformer/config_db.py
+++ src/config/schema-transformer/config_db.py
@@ -2797,6 +2797,7 @@ class BgpRouterST(DBBaseST):
         self.source_port = params.source_port
         if self.router_type not in ('bgpaas-client', 'bgpaas-server'):
             if self.vendor == 'contrail':
+                self.asn = int(params.autonomous_system)
                 self.update_global_asn(
                     GlobalSystemConfigST.get_autonomous_system())
             else:
@@ -2805,6 +2806,8 @@ class BgpRouterST(DBBaseST):
 
     def update_global_asn(self, asn):
         if self.vendor != 'contrail' or self.asn == int(asn):
+            return
+        if self.router_type in ('bgpaas-client', 'bgpaas-server'):
             return
         router_obj = self.read_vnc_obj(fq_name=self.name)
         params = router_obj.get_bgp_router_parameters()
@@ -2864,9 +2867,8 @@ class BgpRouterST(DBBaseST):
             return -1
         update = False
         params = self.obj.get_bgp_router_parameters()
-        if self.asn != bgpaas.asn:
+        if params.autonomous_system != int(bgpaas.asn):
             params.autonomous_system = int(bgpaas.asn)
-            self.asn = bgpaas.asn
             update = True
         ip = bgpaas.ip_address or vmi.get_primary_instance_ip_address()
         if params.address != ip:
@@ -3030,6 +3032,7 @@ class BgpAsAServiceST(DBBaseST):
         router_fq_name = ri.obj.get_fq_name_str() + ':' + vmi.obj.name
         bgpr = BgpRouterST.get(router_fq_name)
         create = False
+        update = False
         src_port = None
         if not bgpr:
             bgp_router = BgpRouter(vmi.obj.name, parent_obj=ri.obj)
@@ -3045,11 +3048,13 @@ class BgpAsAServiceST(DBBaseST):
             identifier=ip,
             source_port=src_port,
             router_type='bgpaas-client')
-        bgp_router.set_bgp_router_parameters(params)
-        bgp_router.set_bgp_router(server_router, self.peering_attribs)
-        if not create:
+        if bgp_router.get_bgp_router_parameters() != params:
+            bgp_router.set_bgp_router_parameters(params)
+            bgp_router.set_bgp_router(server_router, self.peering_attribs)
+            update = True
+        if update:
             self._vnc_lib.bgp_router_update(bgp_router)
-        else:
+        elif create:
             self._vnc_lib.bgp_router_create(bgp_router)
             bgpr = BgpRouterST.locate(router_fq_name, bgp_router)
             self.obj.add_bgp_router(bgp_router)
