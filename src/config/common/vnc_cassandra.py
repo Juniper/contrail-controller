313777c20e0e197f3359b46002765326d4bc35bb
Sachin Bansal <sbansal@juniper.net>
2016-05-02 09:45:07 -0700

    Add check for object type before returning the list
    Change-Id: I8371cbd481e4a11f8613488fa703d84070b87fb2
    Closes-Bug: 1575473

--- src/config/common/vnc_cassandra.py
+++ src/config/common/vnc_cassandra.py
@@ -534,6 +534,8 @@ class VncCassandraClient(object):
         for row_key in obj_rows:
             obj_uuid = row_key
             obj_cols = obj_rows[obj_uuid]
+            if obj_type != json.loads(obj_cols['type'][0]):
+                continue
             result = {}
             result['uuid'] = obj_uuid
             result['fq_name'] = json.loads(obj_cols['fq_name'][0])
@@ -816,6 +818,8 @@ class VncCassandraClient(object):
             ret_list = []
             for obj_uuid in obj_uuids:
                 try:
+                    if obj_type != self.uuid_to_obj_type(obj_uuid):
+                        continue
                     obj_fq_name = self.uuid_to_fq_name(obj_uuid)
                     ret_list.append((obj_fq_name, obj_uuid))
                 except NoIdError:
