b7cc0d7cd984d89b382b163d5b636a88814288f5
Manish <manishs@juniper.net>
2016-05-06 00:51:14 +0530

    In BGP service if VM sends TTL 1 session doesnt come up.
    If TTL is sent as 1 by VM, modify it to 255 and for reverse keep it at 2.
    (Logic for 2 is that vrouter decrements TTL because of routing and hence VM
     should get packet back with 1).
    If TTL is anything else than 1, then honor it.
    
    Closes-bug: #1567586
    
    Conflicts:
    	src/vnsw/agent/pkt/flow_entry.cc
    	src/vnsw/agent/pkt/pkt_flow_info.h
    	src/vnsw/agent/vrouter/ksync/flowtable_ksync.cc
    Change-Id: Ie2f5e5c3513d2e1a1b26d0c47f906a3a70f32abb

--- src/vnsw/agent/vrouter/ksync/flowtable_ksync.cc
+++ src/vnsw/agent/vrouter/ksync/flowtable_ksync.cc
@@ -327,6 +327,7 @@ int FlowTableKSyncEntry::Encode(sandesh_op::type op, char *buf, int buf_len) {
                     flags |= VR_FLOW_FLAG_DPAT;
                 }
             }
+
             //TODO Seperate flags for BgpRouterService??
             if (nat_flow->is_flags_set(FlowEntry::LinkLocalBindLocalSrcPort) ||
                 nat_flow->is_flags_set(FlowEntry::BgpRouterService)) {
@@ -370,6 +371,7 @@ int FlowTableKSyncEntry::Encode(sandesh_op::type op, char *buf, int buf_len) {
         req.set_fr_action(action);
         req.set_fr_drop_reason(drop_reason);
         req.set_fr_qos_id(qos_config_idx);
+        req.set_fr_ttl(flow_entry_->data().ttl);
     }
 
     FlowProto *proto = ksync_obj_->ksync()->agent()->pkt()->get_flow_proto();
