defbc7ad0f70351d9312b900db351a6cd98c6fec
ashoksingh <ashoksr@juniper.net>
2016-05-05 12:27:33 +0530

    Fix agent crash at StatsManager::GetInterfaceStats
    AgentStatsCollector Object should not be created in TSN
    mode. The creation of this was enabled by mistake in one
    of earlier commits.
    
    Added UT to ensure that AgentStatsCollector is not created
    in TSN mode.
    
    Change-Id: Ia1ad3594131a3492746348acb925dffeeaee63f8
    Closes-Bug: #1578362

--- src/vnsw/agent/contrail/contrail_agent_init.cc
+++ src/vnsw/agent/contrail/contrail_agent_init.cc
@@ -75,9 +75,12 @@ void ContrailAgentInit::CreateModules() {
                 AgentUveBase::kIncrementalInterval));
     agent()->set_uve(uve_.get());
 
-    stats_collector_.reset(new AgentStatsCollector(*(agent()->event_manager()->
-                                                     io_service()), agent()));
-    agent()->set_stats_collector(stats_collector_.get());
+    if (agent()->tsn_enabled() == false) {
+        stats_collector_.reset(new AgentStatsCollector
+                                   (*(agent()->event_manager()->io_service()),
+                                    agent()));
+        agent()->set_stats_collector(stats_collector_.get());
+    }
     flow_stats_manager_.reset(new FlowStatsManager(agent()));
     flow_stats_manager_->Init(agent()->params()->flow_stats_interval(),
                               agent()->params()->flow_cache_timeout());
