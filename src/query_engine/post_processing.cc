ff13562a899d0d925ff31a73bc445a860048f490
Sundaresan Rajangam <srajanga@juniper.net>
2016-05-12 18:15:49 -0700

    Add support for <=, >=, regex filter operations in stats query
    Change-Id: I3532580dc49377adf898e6a8d3fddfd449838c2f
    Closes-Bug: #1580393
    (cherry picked from commit d13510fb5186def37124d40c00f9d611d62b5507)

--- src/query_engine/post_processing.cc
+++ src/query_engine/post_processing.cc
@@ -456,6 +456,58 @@ query_status_t PostProcessingQuery::process_query() {
                                 and_check = false;
                               }
                             break;
+                        case LEQ:
+                            if (iter->second.which() ==
+                                QEOpServerProxy::UINT64) {
+                                uint64_t filter_val;
+                                stringToInteger(filter_and[k].value,
+                                                filter_val);
+                                uint64_t col_val =
+                                    boost::get<uint64_t>(iter->second);
+                                if (col_val > filter_val) {
+                                    and_check = false;
+                                }
+                            } else if (iter->second.which() ==
+                                       QEOpServerProxy::DOUBLE) {
+                                double filter_val;
+                                stringToInteger(filter_and[k].value,
+                                                filter_val);
+                                double col_val =
+                                    boost::get<double>(iter->second);
+                                if (col_val > filter_val) {
+                                    and_check = false;
+                                }
+                            }
+                            break;
+                        case GEQ:
+                            if (iter->second.which() ==
+                                QEOpServerProxy::UINT64) {
+                                uint64_t filter_val;
+                                stringToInteger(filter_and[k].value,
+                                                filter_val);
+                                uint64_t col_val =
+                                    boost::get<uint64_t>(iter->second);
+                                if (col_val < filter_val) {
+                                    and_check = false;
+                                }
+                            } else if (iter->second.which() ==
+                                       QEOpServerProxy::DOUBLE) {
+                                double filter_val;
+                                stringToInteger(filter_and[k].value,
+                                                filter_val);
+                                double col_val =
+                                    boost::get<double>(iter->second);
+                                if (col_val < filter_val) {
+                                    and_check = false;
+                                }
+                            }
+                            break;
+                        case REGEX_MATCH:
+                            if (!boost::regex_match(vstream.str(),
+                                                    filter_and[k].match_e)) {
+                                and_check = false;
+                            }
+                            break;
                         default:
                             // upsupported filter operation
                             QE_TRACE(DEBUG, "Unsupported filter operation " << filter_and[k].op);
