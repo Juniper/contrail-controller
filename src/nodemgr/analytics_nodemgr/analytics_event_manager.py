d7530b981070975b0763fb8ba1c83b34dc7518f0
Nikhil B <nikhilb@juniper.net>
2016-05-24 10:40:14 +0530

    Moving the supervisord socket files
    At present, supervisord socket files are created at /tmp which may get
    deleted. Those socket files are being moved to /var/run instead
    Partial-Bug: 1582116
    
    Conflicts:
    	src/nodemgr/config_nodemgr/config_event_manager.py
    	src/nodemgr/database_nodemgr/database_event_manager.py
    	src/opserver/contrail-alarm-gen.initd.supervisord
    
    Change-Id: Ib327d7e6dea7bffdc508fed5400c1a26c5bd24e4
    (cherry picked from commit 75dd9c81cb97cc3b69e3e08086e497db94d2663c)

--- src/nodemgr/analytics_nodemgr/analytics_event_manager.py
+++ src/nodemgr/analytics_nodemgr/analytics_event_manager.py
@@ -40,7 +40,7 @@ class AnalyticsEventManager(EventManager):
         self.node_type = 'contrail-analytics'
         self.module = Module.ANALYTICS_NODE_MGR
         self.module_id = ModuleNames[self.module]
-        self.supervisor_serverurl = "unix:///tmp/supervisord_analytics.sock"
+        self.supervisor_serverurl = "unix:///var/run/supervisord_analytics.sock"
         self.add_current_process()
         node_type = Module2NodeType[self.module]
         node_type_name = NodeTypeNames[node_type]
