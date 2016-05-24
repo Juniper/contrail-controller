6ccf3c411fa416a8d59bf4ebae6a9ef25d92c97c
Nischal Sheth <nsheth@juniper.net>
2016-05-24 13:18:48 -0700

    Include AS path length check in ECMP decision
    Note that this changes ecmp behavior from older releases, but this is
    deemed to be fine since the new behavior is what most users will expect
    by default.
    
    A new knob/option to ignore AS path length can be added if/when needed.
    
    Change-Id: I394f0a683d1ef1fa3db5bd983829acf4aab2c684
    Closes-Bug: 1582452

--- src/bgp/bgp_path.cc
+++ src/bgp/bgp_path.cc
@@ -61,11 +61,11 @@ int BgpPath::PathCompare(const BgpPath &rhs, bool allow_ecmp) const {
     // Compare sequence_number in reverse order as larger is better.
     KEY_COMPARE(rattr->sequence_number(), attr_->sequence_number());
 
-    // For ECMP paths, above checks should suffice
+    KEY_COMPARE(attr_->as_path_count(), rattr->as_path_count());
+
+    // For ECMP paths, above checks should suffice.
     if (allow_ecmp)
         return 0;
-
-    KEY_COMPARE(attr_->as_path_count(), rattr->as_path_count());
 
     KEY_COMPARE(attr_->origin(), rattr->origin());
 
