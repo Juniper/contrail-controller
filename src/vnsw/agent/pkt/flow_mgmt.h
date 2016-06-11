507f3fa0078445430e559dd1cb9f5b1a793ed7eb
Manish Singh <manishs@juniper.net>
2016-06-11 14:37:57 +0530

    In BGP flow mgmt key add CN index.
    Done to ensure different flow keys are created for session established to
    primary or secondary bgp peer from same VM using same source port.
    
    Partial-bug: #1584224
    
    Conflicts:
    	src/vnsw/agent/pkt/test/SConscript
    Change-Id: Iaf5ef824087b290d9be801cdd2392631e9ecaa41

--- src/vnsw/agent/pkt/flow_mgmt.h
+++ src/vnsw/agent/pkt/flow_mgmt.h
@@ -924,12 +924,13 @@ private:
 class BgpAsAServiceFlowMgmtKey : public FlowMgmtKey {
 public:
     BgpAsAServiceFlowMgmtKey(const boost::uuids::uuid &uuid,
-                             uint32_t source_port) :
+                             uint32_t source_port,
+                             uint8_t cn_index) :
         FlowMgmtKey(FlowMgmtKey::BGPASASERVICE, NULL), uuid_(uuid),
-        source_port_(source_port) { }
+        source_port_(source_port), cn_index_(cn_index) { }
     virtual ~BgpAsAServiceFlowMgmtKey() { }
     virtual FlowMgmtKey *Clone() {
-        return new BgpAsAServiceFlowMgmtKey(uuid_, source_port_);
+        return new BgpAsAServiceFlowMgmtKey(uuid_, source_port_, cn_index_);
     }
     virtual bool UseDBEntry() const { return false; }
     virtual bool Compare(const FlowMgmtKey *rhs) const {
@@ -941,10 +942,12 @@ public:
     }
     const boost::uuids::uuid &uuid() const { return uuid_; }
     uint32_t source_port() const { return source_port_; }
+    uint8_t cn_index() const { return cn_index_; }
 
 private:
     boost::uuids::uuid uuid_;
     uint32_t source_port_;
+    uint8_t cn_index_; //Control node index
     DISALLOW_COPY_AND_ASSIGN(BgpAsAServiceFlowMgmtKey);
 };
 
@@ -970,6 +973,8 @@ public:
     bool BgpAsAServiceDelete(BgpAsAServiceFlowMgmtKey &key,
                              const FlowMgmtRequest *req);
     void DeleteAll();
+    //Gets CN index from flow.
+    static uint8_t GetCNIndex(const FlowEntry *flow);
     // Called just before entry is deleted. Used to implement cleanup operations
     virtual void FreeNotify(FlowMgmtKey *key, uint32_t gen_id);
 private:
