1c1c8b66b3785af905578d8746295fd00d25f791
Nikhil B <nikhilb@juniper.net>
2016-06-13 12:28:13 +0530

    Updating Vrouter Interface Alarm to check for down_interface_count
    Existing VrouterInterface alarm looks at VrouterAgent.error_intf_list
    attribtute whereas, UI looks at VrouterAgent.down_interface_count attribute.
    Alarm should also be checking for VrouterAgent.down_interface_count attribute.
    Closes-Bug: 1591885
    
    Change-Id: I6d1ecda86701fedc89ab7f1c1c2d77d6a850ffe5

--- src/opserver/plugins/alarm_vrouter_interface/main.py
+++ src/opserver/plugins/alarm_vrouter_interface/main.py
@@ -2,23 +2,20 @@ from  opserver.plugins.alarm_base import *
 from opserver.sandesh.alarmgen_ctrl.sandesh_alarm_base.ttypes import *
 
 class VrouterInterface(AlarmBase):
-    """Vrouter interface(s) in error state.
-       VrouterAgent has interfaces in error state in VrouterAgent.error_intf_list"""
+    """Vrouter interface(s) down.
+       VrouterAgent has down interfaces in VrouterAgent.down_interface_count"""
 
     _RULES = {
         'or_list': [
             {
                 'and_list': [
                     {
-                        'operand1': 'VrouterAgent.error_intf_list',
-                        'operation': '!=',
-                        'operand2': 'null'
+                        'operand1': 'VrouterAgent.down_interface_count',
+                        'operation': '>',
+                        'operand2': '0',
+                        'vars': ['VrouterAgent.error_intf_list', \
+                                 'VrouterAgent.no_config_intf_list']
                     },
-                    {
-                        'operand1': 'VrouterAgent.error_intf_list',
-                        'operation': '!=',
-                        'operand2': '[]'
-                    }
                 ]
             }
         ]
