2ff4a038cdf34661fe6b629fa3295a6f10991077
Saju Madhavan <sajuptpm@gmail.com>
2016-06-20 15:32:20 +0530

    standardize quota error message
    send overs=[resource_type] to match standard neutron
    implementation of OverQuota exception
    
    Change-Id: I4286fd3fc4c8687beb175de97d5d87ea51ea1f0c
    Closes-Bug: #1590930

--- src/config/vnc_openstack/vnc_openstack/neutron_plugin_db.py
+++ src/config/vnc_openstack/vnc_openstack/neutron_plugin_db.py
@@ -214,7 +214,7 @@ class DBInterface(object):
                 resource='security_group_rule', msg=str(e))
         except OverQuota as e:
             self._raise_contrail_exception('OverQuota',
-                resource='security_group_rule', msg=str(e))
+                overs=['security_group_rule'], msg=str(e))
         except RefsExistError as e:
             try:
                 rule_uuid = str(e).split(':')[1].strip()
@@ -296,7 +296,7 @@ class DBInterface(object):
                 resource=resource_type, msg=str(e))
         except OverQuota as e:
             self._raise_contrail_exception('OverQuota',
-                resource=resource_type, msg=str(e))
+                overs=[resource_type], msg=str(e))
         return obj_uuid
     #end _resource_create
 
@@ -3373,7 +3373,7 @@ class DBInterface(object):
             fip_uuid = self._vnc_lib.floating_ip_create(fip_obj)
         except OverQuota as e:
             self._raise_contrail_exception('OverQuota',
-                resource='floatingip', msg=str(e))
+                overs=['floatingip'], msg=str(e))
         except Exception as e:
             self._raise_contrail_exception('IpAddressGenerationFailure',
                                            net_id=fip_q['floating_network_id'])
