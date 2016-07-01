933d30c7363d7c42eb3128f19b6492c72f3a1fe2
Prakash Bailkeri <prakashmb@juniper.net>
2016-07-01 14:29:02 +0530

    Prevent route replication loop
    Scenario:
    With two VRF importing each other and with routing policy attached to both VRF
    to edit the BgpAttr of the routes, each DBEntry notification for route replication
    would end up deleting and re-adding back the replicated route and notifying the
    secondary route. This would cause an endless route replication loop
    
    Solution:
    Search for secondary path while replicating the route, should compare the BgpAttr
    with original BgpAttr of the secondary path.
    
    Added test to validate the above fix. i.e. time stamp of the replicated path is
    not updated on triggering the notification of the primary route.
    
    Change-Id: I3e6b31e8b36efbc091353e828b4f9dfa809cf5ff
    Closes-Bug: #1597687
    (cherry picked from commit ca58c3d77f09098cf3e056e93591ed2d0163e942)

--- src/bgp/ermvpn/ermvpn_table.cc
+++ src/bgp/ermvpn/ermvpn_table.cc
@@ -120,7 +120,7 @@ BgpRoute *ErmVpnTable::RouteReplicate(BgpServer *server,
             src_path->GetSource(), src_path->GetPeer(),
             src_path->GetPathId());
     if (dest_path != NULL) {
-        if (new_attr != dest_path->GetAttr()) {
+        if (new_attr != dest_path->GetOriginalAttr()) {
             bool success = dest_route->RemoveSecondaryPath(src_rt,
                 src_path->GetSource(), src_path->GetPeer(),
                 src_path->GetPathId());
