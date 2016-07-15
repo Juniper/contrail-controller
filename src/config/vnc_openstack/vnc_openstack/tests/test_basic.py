16aaa54dc373b02a4e89d9e81e3b1018ab208e74
Sachin Bansal <sbansal@juniper.net>
2016-07-15 14:13:16 -0700

    Fix vnc_openstack unit test failures
    Change-Id: Idaa8e856e368c7301eb8c76fad29b25d99cbb413
    Closes-Bug: 1603380

--- src/config/vnc_openstack/vnc_openstack/tests/test_basic.py
+++ src/config/vnc_openstack/vnc_openstack/tests/test_basic.py
@@ -264,6 +264,12 @@ class TestBasic(test_case.NeutronBackendTestCase):
                          vnc_api.SubnetType('1.1.1.0', 24))]))
         self._vnc_lib.virtual_network_create(vn_obj)
 
+        sg_obj = vnc_api.SecurityGroup('default')
+        try:
+            self._vnc_lib.security_group_create(sg_obj)
+        except vnc_api.RefsExistError:
+            pass
+
         proj_uuid = self._vnc_lib.fq_name_to_id('project',
             fq_name=['default-domain', 'default-project'])
 
