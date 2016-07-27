1ba10144a97fbdf2f7db950ddc87ebcf19ce0358
Naveen N <naveenn@juniper.net>
2016-07-27 10:21:57 +0530

    * Add checks for DSCP, MPLS exp and 802.1p values
    Change-Id: I70a944978e1c12f0e25d6adf1e7427954ee16f63
    Closes-bug:#1605195

--- src/config/api-server/vnc_cfg_types.py
+++ src/config/api-server/vnc_cfg_types.py
@@ -2016,10 +2016,6 @@ class ForwardingClassServer(Resource, ForwardingClass):
 
     @classmethod
     def pre_dbe_update(cls, id, fq_name, obj_dict, db_conn, **kwargs):
-        ok, forwarding_class = cls.dbe_read(db_conn, 'forwarding_class', id)
-        if not ok:
-            return ok, read_result
-
         if 'forwarding_class_id' in obj_dict:
             fc_id = obj_dict['forwarding_class_id']
             if 'forwarding_class_id' in forwarding_class:
@@ -2027,3 +2023,38 @@ class ForwardingClassServer(Resource, ForwardingClass):
                     return cls._check_fc_id(obj_dict, db_conn)
         return (True, '')
 # end class ForwardingClassServer
+
+class QosConfigServer(Resource, QosConfig):
+    @classmethod
+    def _check_qos_values(cls, obj_dict, db_conn):
+        fc_pair = 'qos_id_forwarding_class_pair'
+        if 'dscp_entries' in obj_dict:
+            for qos_id_pair in obj_dict['dscp_entries'].get(fc_pair) or []:
+                dscp = qos_id_pair.get('key')
+                if dscp and dscp < 0 or dscp > 63:
+                    return (False, (400, "Invalid DSCP value %d"
+                                   % qos_id_pair.get('key')))
+
+        if 'vlan_priority_entries' in obj_dict:
+            for qos_id_pair in obj_dict['vlan_priority_entries'].get(fc_pair) or []:
+                vlan_priority = qos_id_pair.get('key')
+                if vlan_priority and vlan_priority < 0 or vlan_priority > 7:
+                    return (False, (400, "Invalid 802.1p value %d"
+                                    % qos_id_pair.get('key')))
+
+        if 'mpls_exp_entries' in obj_dict:
+            for qos_id_pair in obj_dict['mpls_exp_entries'].get(fc_pair) or []:
+                mpls_exp = qos_id_pair.get('key')
+                if mpls_exp and mpls_exp < 0 or mpls_exp > 7:
+                    return (False, (400, "Invalid MPLS EXP value %d"
+                                          % qos_id_pair.get('key')))
+        return (True, '')
+
+    @classmethod
+    def pre_dbe_create(cls, tenant_name, obj_dict, db_conn):
+        return cls._check_qos_values(obj_dict, db_conn)
+
+    @classmethod
+    def pre_dbe_update(cls, id, fq_name, obj_dict, db_conn, **kwargs):
+        return cls._check_qos_values(obj_dict, db_conn)
+#end class QosConfigServer
