5455487d4dcb162c11301c0ce90b2cfb76b5fb46
Nipa Kumar <nipak@juniper.net>
2016-08-03 12:05:24 -0700

    Agent crash@ DiscoveryServiceClient::Subscribe
    <Agent::ControllerXmpp> task and <xmpp::StateMachine> are not
    exclusive and hence add/delete to inuse_service_list_ can happen
    in parallel and hence reason for the crash, protect this
    vector manipulation via a mutex.
    
    Note: <http client> and <xmpp::StateMachine> where most
    manipulation of inuse_service_list_ happen is already exclusive.
    
    Change-Id: I48a4cd4c0c85e328c65fd7908608be7b7c0e9562
    Closes-Bug:1608791

--- src/discovery/client/discovery_client.cc
+++ src/discovery/client/discovery_client.cc
@@ -825,6 +825,7 @@ void DiscoveryServiceClient::Subscribe(std::string serviceName) {
                 }
             }
 
+            tbb::mutex::scoped_lock lock(mutex_);
             if (resp->inuse_service_list_.size()) {
                 pugi::xml_node node_service = pugi->FindNode(serviceName);
                 if (!pugi->IsNull(node_service)) {
@@ -1235,6 +1236,7 @@ void DiscoveryServiceClient::AddSubscribeInUseServiceList(
 
     DSSubscribeResponse *resp = GetSubscribeResponse(serviceName);
     if (resp) {
+        tbb::mutex::scoped_lock lock(mutex_);
         resp->AddInUseServiceList(ep);
     }
 }
@@ -1244,6 +1246,7 @@ void DiscoveryServiceClient::DeleteSubscribeInUseServiceList(
 
     DSSubscribeResponse *resp = GetSubscribeResponse(serviceName);
     if (resp) {
+        tbb::mutex::scoped_lock lock(mutex_);
         resp->DeleteInUseServiceList(ep);
     }
 }
