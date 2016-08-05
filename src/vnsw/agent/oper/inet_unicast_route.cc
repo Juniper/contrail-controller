06352c136b47bf125644a32f6edfe149b1d1c50f
Prabhjot Singh Sethi <prabhjot@juniper.net>
2016-08-05 12:32:40 +0530

    Metadata-IP access without policy on interface
    Adding support for metadata IP access without policy on
    interface.
    enable relaxed policy for service IP of virtual network
    so that reverse packet goes through flow lookup even
    when policy on interface is disabled.
    
    Closes-Bug: 1597540
    Change-Id: I7d5afc42bccfa6bfbd672e22236b2ea36c4a02f0
    (cherry picked from commit 73cb8ba22db00a377c82c16484a89c26cbdd0d19)

--- src/vnsw/agent/oper/inet_unicast_route.cc
+++ src/vnsw/agent/oper/inet_unicast_route.cc
@@ -1309,7 +1309,8 @@ void
 InetUnicastAgentRouteTable::AddHostRoute(const string &vrf_name,
                                          const IpAddress &addr,
                                          uint8_t plen,
-                                         const std::string &dest_vn_name) {
+                                         const std::string &dest_vn_name,
+                                         bool relaxed_policy) {
     Agent *agent = Agent::GetInstance();
     DBRequest req(DBRequest::DB_ENTRY_ADD_CHANGE);
     req.key.reset(new InetUnicastRouteKey(agent->local_peer(), vrf_name,
@@ -1317,6 +1318,7 @@ InetUnicastAgentRouteTable::AddHostRoute(const string &vrf_name,
 
     PacketInterfaceKey intf_key(nil_uuid(), agent->GetHostInterfaceName());
     HostRoute *data = new HostRoute(intf_key, dest_vn_name);
+    data->set_relaxed_policy(relaxed_policy);
     req.data.reset(data);
 
     InetUnicastTableEnqueue(Agent::GetInstance(), vrf_name, &req);
