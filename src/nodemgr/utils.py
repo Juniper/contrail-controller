dd0298118fc46cbd98efef35f9d7b6ee52e94c67
mkheni <mkheni@juniper.net>
2016-08-09 12:37:33 -0700

    Added an alarm to compare version of running process vs installed package version.
    To debug issues where a daemon/process is not restarted after upgrade,
    nodemgr sends the newly installed package version, which can be compared against the package version of running process,
    and an alarm can be raised if they do not match.
    
    Change-Id: I443e0b9ba9491cdd8795b7ebb2e5a76d022d8374
    Closes-Bug: #1556313

---
+++ src/nodemgr/utils.py
@@ -0,0 +1,13 @@
+#
+# Copyright (c) 2016 Juniper Networks, Inc. All rights reserved.
+#
+
+import os
+
+class NodeMgrUtils(object):
+    @staticmethod
+    def get_package_version(pkg):
+        #retrieve current installed version of pkg
+        cmd = "contrail-version %s | grep %s" % (pkg,pkg)
+        version = os.popen(cmd).read()
+        return version.split()[1]
