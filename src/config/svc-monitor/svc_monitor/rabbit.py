c6579f3a3934f583fed61e9f0c8a0dbb75b0cfd5
Sachin Bansal <sbansal@juniper.net>
2016-08-09 15:40:49 -0700

    Build a uuid-fqname cache in all config daemons on start up
    This can help in converting from uuid to type or fq name later
    Partial-Bug: 1589014
    
    Change-Id: Ib984d16a75e61dbfe732c199770ff71967e3b2f8
    (cherry picked from commit 7dc702bc1bce98a0955401fe355c5da7a8a947f8)

--- src/config/svc-monitor/svc_monitor/rabbit.py
+++ src/config/svc-monitor/svc_monitor/rabbit.py
@@ -55,6 +55,8 @@ class RabbitConnection(object):
         if oper_info['oper'] == 'CREATE':
             obj_dict = oper_info['obj_dict']
             obj_id = oper_info['uuid']
+            DBBaseSM._cassandra.cache_uuid_to_fq_name_add(
+                    obj_id, obj_dict['fq_name'], obj_type)
             obj = obj_class.locate(obj_id)
             if obj is not None:
                 dependency_tracker = DependencyTracker(
@@ -90,6 +92,7 @@ class RabbitConnection(object):
                                 set(ids))
         elif oper_info['oper'] == 'DELETE':
             obj_id = oper_info['uuid']
+            DBBaseSM._cassandra.cache_uuid_to_fq_name_del(obj_id)
             obj = obj_class.get(obj_id)
             if obj is None:
                 return
