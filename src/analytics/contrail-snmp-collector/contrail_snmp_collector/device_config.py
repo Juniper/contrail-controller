acc009da89b43c22b61c908d31b49d89071387cb
Ted Ghose <rintu@hotmail.com>
2016-08-09 11:51:54 -0700

    snmp-collector should keep retrying for ever
    retry vnc connection instead of giving up
    Closes-Bug: 1559983
    
    Change-Id: I255812e017a5296ed46cb48abb858c78fb868975

--- src/analytics/contrail-snmp-collector/contrail_snmp_collector/device_config.py
+++ src/analytics/contrail-snmp-collector/contrail_snmp_collector/device_config.py
@@ -89,7 +89,7 @@ class DeviceConfig(object):
     def get_vnc(usr, passwd, tenant, api_servers, auth_host=None,
             auth_port=None, auth_protocol=None, notifycb=None):
         e = IOError('Api servers (%s) not reachable' % ','.join(api_servers))
-        for rt in (5, 2, 7, 9, 16, 25):
+        while True:
           for api_server in api_servers:
             srv = api_server.split(':')
             if len(srv) == 2:
@@ -108,8 +108,7 @@ class DeviceConfig(object):
                 if callable(notifycb):
                     notifycb('api', 'Not connected', servers=api_server,
                             up=False)
-                time.sleep(rt)
-        raise e
+                time.sleep(3)
 
     @staticmethod
     def fom_api_server(api_servers, usr, passwd, tenant, auth_host=None,
