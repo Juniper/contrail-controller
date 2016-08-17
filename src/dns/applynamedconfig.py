d733a7769447cac01bd3eb5f2951de26b3f59ba4
Nipa Kumar <nipak@juniper.net>
2016-08-16 17:16:38 -0700

    Support dynamically updating base named configuration
    An additional file contrail-named-base.conf will contain the base named configurtion
    such as options stanza, key stanza, controls stanza and logging stanza.
    
    All configs generated due to vdns configuration resulting in view stanza will be written
    to contrail-named.conf
    
    Before sending "reconfig" command to named, both the files will be merged
    into contrail-named.conf
    
    The above gives flexibility to user to update and apply changes without restarting
    the daemon.
    
    An additional helper script(applynamedconfig.py) will apply the config changes in
    contrail-named-base.conf to named daemon.
    
    Change-Id: If6089189a1104f648d8d7d3f638c58f9883deb50
    Closes-Bug:1605341

---
+++ src/dns/applynamedconfig.py
@@ -0,0 +1,29 @@
+import sys
+import os
+
+# open contrail-named-base.conf
+file1=open('contrail-named-base.conf', 'r')
+file1_contents = file1.read()
+file1.close()
+
+# open contrail-named.conf to seek the view stanza
+file2 = open('contrail-named.conf', 'r')
+last_pos = file2.tell()
+for line in file2:
+    if 'view' in line:
+        file2.seek(last_pos)
+        file2_contents = file2.read()
+        break
+    last_pos = file2.tell()
+file2.close()
+
+# rewrite contrail-named.conf concatening
+# contrail-named-base.conf and contrail-named.conf from view stanza
+file3 = open('contrail-named.conf', 'w')
+file3.truncate()
+file3.write(file1_contents + file2_contents)
+file3.close()
+
+# apply config
+os.system('/usr/bin/contrail-rndc -c /etc/contrail/dns/contrail-rndc.conf reconfig')
+
