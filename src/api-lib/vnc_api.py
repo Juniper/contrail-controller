9a0c7e056750c2bcb61f9680393c30ef5acc2d58
Megh Bhatt <meghb@juniper.net>
2016-08-25 18:25:12 -0700

    Check for presence of 'X-USER-TOKEN' in self._headers before deleting
    self._headers can be set to empty if self._authenticate raises an
    Exception and hence we need to check if key 'X-USER-TOKEN' is
    present before trying to delete it
    
    Change-Id: I31bba531667f424de30c0c03302301c0c171a60d
    Partial-Bug: #1616797
    (cherry picked from commit 681872321c672a4d581211bbda418e1b0d474983)

--- src/api-lib/vnc_api.py
+++ src/api-lib/vnc_api.py
@@ -1206,7 +1206,8 @@ class VncApi(object):
         except PermissionDenied:
             rv = None
         finally:
-            del self._headers['X-USER-TOKEN']
+            if 'X-USER-TOKEN' in self._headers:
+                del self._headers['X-USER-TOKEN']
         return rv
 
     # change object ownsership
