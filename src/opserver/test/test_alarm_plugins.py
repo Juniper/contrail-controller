3fd01845143e91c5c193a1d4af1f1dcdccc23c89
Raj Reddy <rajreddy@juniper.net>
2016-08-31 15:09:39 -0700

    Currently the self_ip_list does not always carry configured virtual_router_ip_address.. The control_ip is matches the configured virtual_router_ip_address, the condition is already present. This commit removes self_ip_list comparison.
    Closes-Bug: #1616587
    (cherry picked from commit 3306df1020ec759477a693f38e1faabaf33ea50f)
    
    Conflicts:
    	src/opserver/plugins/alarm_address_mismatch/main.py
    Change-Id: I0e13852cc023dcdc72370862e99afaf2d424cfef

--- src/opserver/test/test_alarm_plugins.py
+++ src/opserver/test/test_alarm_plugins.py
@@ -271,36 +271,11 @@ class TestAlarmPlugins(unittest.TestCase):
 
         tests = [
             TestCase(
-                name='ContrailConfig == null',
-                input=TestInput(uve_key='ObjectVRouter:host1',
-                    uve_data={
-                        'VrouterAgent': {
-                            'self_ip_list': ['10.1.1.1']
-                        }
-                    }
-                ),
-                output=TestOutput(or_list=None)
-            ),
-            TestCase(
-                name='VrouterAgent == null',
-                input=TestInput(uve_key='ObjectVRouter:host1',
-                    uve_data={
-                        'ContrailConfig': {
-                            'elements': {
-                                'virtual_router_ip_address': '"10.1.1.1"'
-                            }
-                        }
-                    }
-                ),
-                output=TestOutput(or_list=None)
-            ),
-            TestCase(
                 name='ContrailConfig.elements.virtual_router_ip_address ' +\
                     '== null',
                 input=TestInput(uve_key='ObjectVRouter:host1',
                     uve_data={
                         'VrouterAgent': {
-                            'self_ip_list': ['10.1.1.1'],
                             'control_ip': '10.1.1.1'
                         },
                         'ContrailConfig': {
@@ -310,14 +285,6 @@ class TestAlarmPlugins(unittest.TestCase):
                     }
                 ),
                 output=TestOutput(or_list=[
-                    {
-                        'and_list': [
-                            ('ContrailConfig.elements.' +\
-                                'virtual_router_ip_address not in '
-                                'VrouterAgent.self_ip_list', None,
-                             [('null', '["10.1.1.1"]', None)])
-                        ]
-                    },
                     {
                         'and_list': [
                             ('ContrailConfig.elements.' +\
@@ -329,36 +296,10 @@ class TestAlarmPlugins(unittest.TestCase):
                 ])
             ),
             TestCase(
-                name='VrouterAgent.self_ip_list == null',
-                input=TestInput(uve_key='ObjectVRouter:host1',
-                    uve_data={
-                        'VrouterAgent': {
-                            'control_ip': '10.1.1.1'
-                        },
-                        'ContrailConfig': {
-                            'elements': {
-                                'virtual_router_ip_address': '"10.1.1.1"'
-                            }
-                        }
-                    }
-                ),
-                output=TestOutput(or_list=[
-                    {
-                        'and_list': [
-                            ('ContrailConfig.elements.' +\
-                                'virtual_router_ip_address not in '
-                                'VrouterAgent.self_ip_list', None,
-                             [('"10.1.1.1"', 'null', None)])
-                        ]
-                    }
-                ])
-            ),
-            TestCase(
                 name='VrouterAgent.control_ip == null',
                 input=TestInput(uve_key='ObjectVRouter:host1',
                     uve_data={
                         'VrouterAgent': {
-                            'self_ip_list': ['1.1.1.1', '10.1.1.1'],
                         },
                         'ContrailConfig': {
                             'elements': {
@@ -368,39 +309,6 @@ class TestAlarmPlugins(unittest.TestCase):
                     }
                 ),
                 output=TestOutput(or_list=[
-                    {
-                        'and_list': [
-                            ('ContrailConfig.elements.' +\
-                                'virtual_router_ip_address != '
-                                'VrouterAgent.control_ip', None,
-                             [('"10.1.1.1"', 'null', None)])
-                        ]
-                    }
-                ])
-            ),
-            TestCase(
-                name='VrouterAgent.control_ip == null, ' +\
-                    'VrouterAgent.self_ip_list == null',
-                input=TestInput(uve_key='ObjectVRouter:host1',
-                    uve_data={
-                        'VrouterAgent': {
-                        },
-                        'ContrailConfig': {
-                            'elements': {
-                                'virtual_router_ip_address': '"10.1.1.1"'
-                            }
-                        }
-                    }
-                ),
-                output=TestOutput(or_list=[
-                    {
-                        'and_list': [
-                            ('ContrailConfig.elements.' +\
-                                'virtual_router_ip_address not in '
-                                'VrouterAgent.self_ip_list', None,
-                             [('"10.1.1.1"', 'null', None)])
-                        ]
-                    },
                     {
                         'and_list': [
                             ('ContrailConfig.elements.' +\
@@ -417,7 +325,6 @@ class TestAlarmPlugins(unittest.TestCase):
                 input=TestInput(uve_key='ObjectVRouter:host1',
                     uve_data={
                         'VrouterAgent': {
-                            'self_ip_list': ['1.1.1.1', '1.1.1.2'],
                             'control_ip': '1.1.1.1'
                         },
                         'ContrailConfig': {
@@ -439,58 +346,10 @@ class TestAlarmPlugins(unittest.TestCase):
                 ])
             ),
             TestCase(
-                name='ContrailConfig.elements.virtual_router_ip_address ' +\
-                    'not in VrouterAgent.self_ip_list',
-                input=TestInput(uve_key='ObjectVRouter:host1',
-                    uve_data={
-                        'VrouterAgent': {
-                            'self_ip_list': ['10.1.1.1'],
-                            'control_ip': '1.1.1.2'
-                        },
-                        'ContrailConfig': {
-                            'elements': {
-                                'virtual_router_ip_address': '"1.1.1.2"'
-                            }
-                        }
-                    }
-                ),
-                output=TestOutput(or_list=[
-                    {
-                        'and_list': [
-                            ('ContrailConfig.elements.' +\
-                                'virtual_router_ip_address not in '
-                                'VrouterAgent.self_ip_list', None,
-                             [('"1.1.1.2"', '["10.1.1.1"]', None)])
-                        ]
-                    }
-                ])
-            ),
-            TestCase(
-                name='ContrailConfig.elements.virtual_router_ip_address ' +\
-                    'in VrouterAgent.self_ip_list, ' +\
-                    'ContrailConfig.elements.virtual_router_ip_address ' +\
-                    '== VrouterAgent.control_ip',
-                input=TestInput(uve_key='ObjectVRouter:host1',
-                    uve_data={
-                        'VrouterAgent': {
-                            'self_ip_list': ['1.1.1.1', '10.1.1.1'],
-                            'control_ip': '10.1.1.1'
-                        },
-                        'ContrailConfig': {
-                            'elements': {
-                                'virtual_router_ip_address': '"10.1.1.1"'
-                            }
-                        }
-                    }
-                ),
-                output=TestOutput(or_list=None)
-            ),
-            TestCase(
                 name='ContrailConfig.elements is a list of list - no match',
                 input=TestInput(uve_key='ObjectVRouter:host1',
                     uve_data={
                         'VrouterAgent': {
-                            'self_ip_list': ['1.1.1.1', '10.1.1.1'],
                             'control_ip': '10.1.1.1'
                         },
                         'ContrailConfig': {
@@ -522,7 +381,6 @@ class TestAlarmPlugins(unittest.TestCase):
                 input=TestInput(uve_key='ObjectVRouter:host1',
                     uve_data={
                         'VrouterAgent': {
-                            'self_ip_list': ['1.1.1.1', '10.1.1.1'],
                             'control_ip': '10.1.1.1'
                         },
                         'ContrailConfig': {
@@ -548,14 +406,6 @@ class TestAlarmPlugins(unittest.TestCase):
                     }
                 ),
                 output=TestOutput(or_list=[
-                    {
-                        'and_list': [
-                            ('ContrailConfig.elements.' +\
-                                'virtual_router_ip_address not in '
-                                'VrouterAgent.self_ip_list', None,
-                             [('"10.1.1.2"', '["1.1.1.1", "10.1.1.1"]', None)])
-                        ]
-                    },
                     {
                         'and_list': [
                             ('ContrailConfig.elements.' +\
