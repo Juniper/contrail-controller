d1cc95482c2e8297f736b0902df22fab1e2ab155
Deepinder Setia <dsetia@juniper.net>
2016-09-01 16:42:07 -0700

    Contrail plugin uses useragent-kv for maintaining state in API server
    Change-Id: I3ec7ee629684e6429f25df3d2af1006f5b79784b
    Closes-Bug: #1619500

--- src/config/api-server/vnc_cfg_api_server.py
+++ src/config/api-server/vnc_cfg_api_server.py
@@ -2777,6 +2777,11 @@ class VncApiServer(object):
                 'rule_perms': [{'role_name':'*', 'role_crud':'CRUD'}]
             },
             {
+                'rule_object':'useragent-kv',
+                'rule_field': '',
+                'rule_perms': [{'role_name':'*', 'role_crud':'CRUD'}]
+            },
+            {
                 'rule_object':'documentation',
                 'rule_field': '',
                 'rule_perms': [{'role_name':'*', 'role_crud':'R'}]
