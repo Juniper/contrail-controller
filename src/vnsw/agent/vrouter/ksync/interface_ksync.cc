3c6293424dcfa4ac73847146ab09ec0697985f90
Prabhjot Singh Sethi <prabhjot@juniper.net>
2016-09-24 23:56:33 +0530

    Fix Vrouter Op error for interface nexthop
    Issue:
    ------
    In absence of parent interface or vlan tag for vlan
    sub-interface results in skipping sending messsage to
    vrouter, however it allows KSync state to complete, which
    further pushes the dependent Nexthop entries without
    programming interface to Vrouter first, causing dataplane
    inconsistency.
    
    Fix:
    ----
    In absence of parent interface or vlan tag for vlan
    sub-interface hold the interface as unresolved, to avoid
    programming dependent objects ahead of interface
    programming
    
    Allow delete of interface to go through without validation.
    we will send a message if interface is seen by vrouter
    
    Closes-Bug: 1625896
    Change-Id: If3b28ddb97cb43ce8f8a83fed4a35e57257c83fb

--- src/vnsw/agent/vrouter/ksync/interface_ksync.cc
+++ src/vnsw/agent/vrouter/ksync/interface_ksync.cc
@@ -457,6 +457,15 @@ KSyncEntry *InterfaceKSyncEntry::UnresolvedReference() {
         return parent_.get();
     }
 
+    if (vmi_device_type_ == VmInterface::VM_VLAN_ON_VMI) {
+        if (!parent_.get() || rx_vlan_id_ == VmInterface::kInvalidVlanId) {
+            // For VM_VLAN_ON_VMI interface hold interface as unresolved, in
+            // absence of parent interface of rx_vlan_id_, to avoid
+            // programming Interface Nexthop with unresolved interface
+            return KSyncObjectManager::default_defer_entry();
+        }
+    }
+
     if (!analyzer_name_.empty()) {
         MirrorKSyncObject *mirror_object =
                          ksync_obj_->ksync()->mirror_ksync_obj();
@@ -496,17 +505,7 @@ int InterfaceKSyncEntry::Encode(sandesh_op::type op, char *buf, int buf_len) {
     vr_interface_req encoder;
     int encode_len;
 
-    // Dont send message if interface index not known
-    if (IsValidOsIndex(os_index_, type_, rx_vlan_id_, vmi_type_, transport_) == false) {
-        return 0;
-    }
-
     if (type_ == Interface::LOGICAL || type_ == Interface::REMOTE_PHYSICAL) {
-        return 0;
-    }
-
-    // No need to add VLAN sub-interface if there is no parent
-    if (vmi_device_type_ == VmInterface::VM_VLAN_ON_VMI && !parent_.get()) {
         return 0;
     }
 
@@ -519,6 +518,16 @@ int InterfaceKSyncEntry::Encode(sandesh_op::type op, char *buf, int buf_len) {
         assert(error == 0);
         assert(encode_len <= buf_len);
         return encode_len;
+    }
+
+    // Dont send message if interface index not known
+    if (IsValidOsIndex(os_index_, type_, rx_vlan_id_, vmi_type_, transport_) == false) {
+        return 0;
+    }
+
+    // No need to add VLAN sub-interface if there is no parent
+    if (vmi_device_type_ == VmInterface::VM_VLAN_ON_VMI && !parent_.get()) {
+        return 0;
     }
 
     switch (type_) {
