Juniper/contrail-controller

commit 811327b8ff33c2ef4452663018982bd96d29c71d

Improve ifmap graph walk performance
Following areas in boost graph implementation for ifmap db needs optimization
1. BFS: Current BFS visits all vertices in DBGraph before starting the BFS.

2. BFS applies EdgeFilter and NodeFilter on all edges and nodes encountered in
the BFS walk. E.g. if there are 1K VMI in a VN, then during the BFS walk of the
VM, VN node is encountered due to VN<->VMI<->VM link. When walking edges of the
VN, all the VMIs belonging to the VN applied with EdgeFilter and NodeFilter.
Since it is not part of whitelist, other VMIs are not visited.

3. During the processing of Vertex during graph walk, JoinVertex, all edges are
notified. This walk of all edges from vertex is expensive in case VN node in the
above example.(1K VMI from a VN). While visiting the VN, all VMI links are
unnecessarily notified.

To solve the above issues, following optimizations are implemented in this commit

1. Store the out edges of the Vertex in multiset sorted by metadata name[i.e.
name of the link]. This helps in iterating the edges of the vertex better.

2. Implement BFS in CN to avoid iterating all vertices in graph to color it.
The color is implemented as genid stored in each vertex.
The BFS implementation also doesn't visit all edges of the graph and apply the
filter. Since the out edges of the vertex is stored in multiset, we can visit
edges only present in the whitelist.  With this, BFS can skip over uninteresting
links in logN time instead of N.

3. The whitelist is stored such that for a given vertex, set of all interested
edges (metadata name is stored). Please note, allowed edge is sorted by edge name
and out edge list is also sorted by edge name. So out edge list walk can be optimized
by using lower_bound()

4. Edges from a visited Vertex is not notified in the JoinVertex(). Instead only
interested edges are notified by calling Notify() when an Edge is visited in graph
walk.

Change-Id: Iadd815d6897dc84c786c7079659023e190e5ef20
Closes-Bug: #1619943

src/db/test/db_graph_test.cc
@@ -124,15 +124,15 @@ TEST_F(DBGraphTest, VertexRemove) {
     int count = 0;
     for (DBGraph::edge_iterator iter = graph_.edge_list_begin();
          iter != graph_.edge_list_end(); ++iter) {
-        const DBGraph::DBVertexPair &tuple = *iter;
+        const DBGraph::DBEdgeInfo &tuple = *iter;
         switch (count) {
             case 0:
-                EXPECT_EQ("vertex:a", tuple.first->ToString());
-                EXPECT_EQ("vertex:c", tuple.second->ToString());
+                EXPECT_EQ("vertex:a", boost::get<0>(tuple)->ToString());
+                EXPECT_EQ("vertex:c", boost::get<1>(tuple)->ToString());
                 break;
             case 1:
-                EXPECT_EQ("vertex:a", tuple.first->ToString());
-                EXPECT_EQ("vertex:d", tuple.second->ToString());
+                EXPECT_EQ("vertex:a", boost::get<0>(tuple)->ToString());
+                EXPECT_EQ("vertex:d", boost::get<1>(tuple)->ToString());
                 break;
             default:
                 EXPECT_TRUE(false);

commit 32c9073e176e80717f87b6ed91722f1364d2c2a2

Fix compilation error in db_graph_test
Fix the compilation error introduced with
https://github.com/Juniper/contrail-controller/commit/811327b8ff33c2ef4452663018982bd96d29c71d

Change-Id: Ic61287b62f5dd31c4e2ebcb16f11a1a49e343336
Related-Bug: #1619943

src/db/test/db_graph_test.cc
@@ -45,7 +45,7 @@ class TestVertex : public DBGraphVertex {
 
 class TestEdge : public DBGraphEdge {
   public:
-    explicit TestEdge(Edge edge_id) : DBGraphEdge(edge_id) { }
+    explicit TestEdge(const string &name) : DBGraphEdge(), name_(name) { }
     virtual string ToString() const {
         ostringstream ss;
         ss << edge_id();
@@ -62,7 +62,12 @@ class TestEdge : public DBGraphEdge {
         return edge_id() < e.edge_id();
     }
 
+    const std::string &name() const {
+        return name_;
+    }
+
   private:
+    const string name_;
     DISALLOW_COPY_AND_ASSIGN(TestEdge);
 };
 
@@ -76,8 +81,10 @@ class DBGraphTest : public ::testing::Test {
     }
 
     void CreateEdge(TestVertex *lhs, TestVertex *rhs) {
-        TestEdge *e = new TestEdge(graph_.Link(lhs, rhs));
-        graph_.SetEdgeProperty(e);
+        ostringstream ss;
+        ss << "TestEdge" << edges_.size();
+        TestEdge *e = new TestEdge(ss.str());
+        graph_.Link(lhs, rhs, e);
         edges_.push_back(e);
     }
 
@@ -177,7 +184,7 @@ TEST_F(DBGraphTest, EdgeRemove) {
 
     EXPECT_TRUE(HasEdge(visitor.edges, "b", "c"));
 
-    graph_.Unlink(vertices_[1], vertices_[2]);
+    graph_.Unlink(edges_[2]);
     delete edges_[2];
     edges_.erase(edges_.begin() + 2);
 
@@ -189,7 +196,7 @@ TEST_F(DBGraphTest, EdgeRemove) {
     EXPECT_EQ(3, visitor.edges.size());
     EXPECT_FALSE(HasEdge(visitor.edges, "b", "c"));
 
-    graph_.Unlink(vertices_[1], vertices_[3]);
+    graph_.Unlink(edges_[2]);
     delete edges_[2];
     edges_.erase(edges_.begin() + 2);
 
@@ -227,6 +234,11 @@ struct TestVisitorFilter : public DBGraph::VisitorFilter {
                             const DBGraphEdge *edge) const {
         return true;
     }
+
+    AllowedEdgeRetVal AllowedEdges(const DBGraphVertex *source) const {
+        return std::make_pair(true, AllowedEdgeSet());
+    }
+
     std::vector<std::string> include_vertex;
     std::vector<std::string> include_edge;
 };

src/db/test/db_test_cmn.h
@@ -5,6 +5,8 @@
 #ifndef db_test_cmn_h
 #define db_test_cmn_h
 
+#include <boost/assign/list_of.hpp>
+
 #include "io/event_manager.h"
 #include "base/task.h"
 #include "base/task_annotations.h"