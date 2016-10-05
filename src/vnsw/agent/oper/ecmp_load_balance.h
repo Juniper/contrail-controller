f612229f0e9af884310b78d92aaef6ba40b1eb9c
jayaramsatya <sajayaram@juniper.net>
2016-10-05 13:24:07 +0530

    Support for Ecmp configured hash in Agent
    Ecmp hash fields are consolidated after going through all the routes
    pointing to ECMP Nexthop.
    The fields are selected based on most common fields pointing to ECMP
    Next hop by all the routes.
    After selecting the fields same will be used for configuring the
    Vrouter.
    
    Change-Id: I47092af68e95dd8491c0f8ac511c85971de945f7
    partial-bug: #1616814

--- src/vnsw/agent/oper/ecmp_load_balance.h
+++ src/vnsw/agent/oper/ecmp_load_balance.h
@@ -4,7 +4,7 @@
 
 #ifndef vnsw_agent_ecmp_load_balance_hpp
 #define vnsw_agent_ecmp_load_balance_hpp
-
+#include <boost/intrusive_ptr.hpp>
 #include <vnc_cfg_types.h>
 
 using namespace boost::uuids;
@@ -219,4 +219,116 @@ private:
     bool use_global_vrouter_;
 };
 
+class EcmpField {
+public:
+    EcmpField() {
+        ref_count_= 0;
+    }
+
+    uint32_t RefCount() const {
+        return ref_count_;
+    }
+private:
+   friend void intrusive_ptr_add_ref(EcmpField* ptr);
+   friend void intrusive_ptr_release(EcmpField* ptr);
+   mutable tbb::atomic<uint32_t> ref_count_;
+};
+
+inline void intrusive_ptr_add_ref(EcmpField* ptr) {
+    ptr->ref_count_.fetch_and_increment();
+}
+
+inline void intrusive_ptr_release(EcmpField* ptr) {
+    uint32_t prev = ptr->ref_count_.fetch_and_decrement();
+    if (prev == 1) {
+      delete ptr;
+    }
+}
+
+class EcmpHashFields {
+public:
+    typedef boost::intrusive_ptr<EcmpField> EcmpFieldPtr;
+
+    EcmpHashFields() {
+    }
+
+    EcmpHashFields(const EcmpHashFields &rhs) {
+       hash_fields_to_use_ = rhs.hash_fields_to_use_;
+    }
+    void  operator = (const EcmpHashFields &rhs) {
+       hash_fields_to_use_ = rhs.hash_fields_to_use_;
+    }
+
+    void AllocateEcmpFields() {
+        sip_ = new EcmpField;
+        dip_ = new EcmpField;
+        proto_ = new EcmpField;
+        sport_ = new EcmpField;
+        dport_ = new EcmpField;
+    }
+
+    uint8_t HashFieldsToUse() const {
+        return hash_fields_to_use_;
+    }
+
+    void SetHashFieldtoUse(EcmpField *ptr, uint8_t key) {
+        if (ptr && ptr->RefCount() == 1) {
+            comp_hash_fields_to_use_ |= 1 << key;
+        }
+    }
+    // If the field is not set create intrusive pointer
+    // else release the pointer.
+    void SetChangeInHashField(bool is_field_set, EcmpFieldPtr& fieldPtr,
+                              EcmpFieldPtr &objFieldPtr) {
+        if (!is_field_set) {
+            if (!objFieldPtr.get()) {
+                objFieldPtr = fieldPtr;
+            }
+        } else {
+            objFieldPtr.reset();
+        }
+    }
+    // This function will be called to ge intersection of ecmp fields
+    uint8_t  CalculateHashFieldsToUse() {
+        comp_hash_fields_to_use_ = 0;
+        SetHashFieldtoUse(sip_.get(), EcmpLoadBalance::SOURCE_IP);
+        SetHashFieldtoUse(dip_.get(), EcmpLoadBalance::DESTINATION_IP);
+        SetHashFieldtoUse(proto_.get(), EcmpLoadBalance::IP_PROTOCOL);
+        SetHashFieldtoUse(sport_.get(), EcmpLoadBalance::SOURCE_PORT);
+        SetHashFieldtoUse(dport_.get(), EcmpLoadBalance::DESTINATION_PORT);
+        return comp_hash_fields_to_use_;
+    }
+    //This function used to calculate the Change in ecmp fields
+    void CalculateChangeInEcmpFields(const EcmpLoadBalance &ecmp_load_balance,
+                                     EcmpHashFields& ecmp_hash_fields) {
+        SetChangeInHashField(ecmp_load_balance.is_source_ip_set(),
+                             ecmp_hash_fields.sip_, sip_);
+        SetChangeInHashField(ecmp_load_balance.is_destination_ip_set(),
+                             ecmp_hash_fields.dip_, dip_);
+        SetChangeInHashField(ecmp_load_balance.is_ip_protocol_set(),
+                             ecmp_hash_fields.proto_, proto_);
+        SetChangeInHashField(ecmp_load_balance.is_source_port_set(),
+                             ecmp_hash_fields.sport_, sport_);
+        SetChangeInHashField(ecmp_load_balance.is_destination_port_set(),
+                             ecmp_hash_fields.dport_, dport_);
+    }
+
+    bool IsFieldsInUseChanged() {
+        return hash_fields_to_use_ != CalculateHashFieldsToUse();
+    }
+
+    void SetHashFieldstoUse() {
+        hash_fields_to_use_ = comp_hash_fields_to_use_;
+    }
+
+private:
+    // This will have latest computed value
+    uint8_t comp_hash_fields_to_use_;
+    uint8_t hash_fields_to_use_;
+    EcmpFieldPtr sip_;
+    EcmpFieldPtr dip_;
+    EcmpFieldPtr proto_;
+    EcmpFieldPtr sport_;
+    EcmpFieldPtr dport_;
+};
 #endif
