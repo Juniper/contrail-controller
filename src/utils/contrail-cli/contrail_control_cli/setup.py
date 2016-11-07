41054a7abe951e18ba84a9cc31f462ccde9f0d5e
Nikhil B <nikhilb@juniper.net>
2016-11-07 14:37:59 +0530

    Adding basic controller backend for CLIs
    This checkin adds basic packages for backend of CLI handling. There will be
    other changes needed to create CLIs. This is first draft to get the feedback.
    
    Change-Id: I9b03513a980f97ed035473176d42bbbb10e4db6d
    Partial-Bug: #1630204

---
+++ src/utils/contrail-cli/contrail_control_cli/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+PROJECT = 'ContrailControlCli'
+
+VERSION = '0.1'
+
+from setuptools import setup, find_packages
+
+from entry_points import entry_points_dict
+
+setup( name=PROJECT,
+        version=VERSION,
+        description='Contrail Control Command Line Interfae',
+        platforms=['Any'],
+        install_requires=['cliff>=2.2.0'],
+        packages=find_packages(),
+        package_data={'': ['*.html', '*.css', '*.xml']},
+        include_package_data=True,
+        entry_points=entry_points_dict,
+        zip_safe=False,
+    )
+
