43c61718e27003d6e1efc76f7b16fdd21dabbe0d
Nikhil B <nikhilb@juniper.net>
2016-11-07 14:37:59 +0530

    Adding basic controller backend for CLIs
    This checkin adds basic packages for backend of CLI handling. There will be
    other changes needed to create CLIs. This is first draft to get the feedback.
    
    Change-Id: I9b03513a980f97ed035473176d42bbbb10e4db6d
    Partial-Bug: #1630204

---
+++ src/utils/contrail-cli/entry_points.py
@@ -0,0 +1,5 @@
+entry_points_dict = dict()
+entry_points_dict['supervisord_analytics_files'] = []
+entry_points_dict['supervisord_control_files'] = []
+entry_points_dict['supervisord_config_files'] = []
+entry_points_dict['supervisord_vrouter_files'] = []
