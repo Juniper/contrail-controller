40d6e90672248e7e8637d6adbbc2c760b46f5bea
Nikhil B <nikhilb@juniper.net>
2016-11-07 14:37:59 +0530

    Adding basic controller backend for CLIs
    This checkin adds basic packages for backend of CLI handling. There will be
    other changes needed to create CLIs. This is first draft to get the feedback.
    
    Change-Id: I9b03513a980f97ed035473176d42bbbb10e4db6d
    Partial-Bug: #1630204

---
+++ src/utils/contrail-cli/contrail_analytics_cli/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+PROJECT = 'ContrailAnalyticsCli'
+
+VERSION = '0.1'
+
+from setuptools import setup, find_packages
+
+import subprocess
+
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
+from entry_points import entry_points_dict
+
+print "setting up contrail analytics cli"
+analytics_entry_points_dict = dict()
+analytics_entry_points_dict['console_scripts'] = []
+
+def _populate_analytics_entry_points_dict():
+    for key,values in entry_points_dict.iteritems():
+        if key == "console_scripts":
+	    if 'supervisord_analytics_files' in values.keys():
+                analytics_entry_points_dict['console_scripts'].append(values['supervisord_analytics_files'])
+
+_populate_analytics_entry_points_dict()
+
+
+
+setup( name=PROJECT,
+        version=VERSION,
+        description='Contrail Command Line Interfae',
+        platforms=['Any'],
+        install_requires=['cliff>=2.2.0'],
+        packages=find_packages(),
+        package_data={'': ['*.html', '*.css', '*.xml']},
+        include_package_data=True,
+        entry_points=analytics_entry_points_dict,
+        zip_safe=False,
+    )
+
---
+++ src/utils/contrail-cli/contrail_cli/help.py
@@ -0,0 +1,92 @@
+import argparse
+import sys
+import traceback
+import os
+
+from cliff.command import Command
+
+
+class HelpAction(argparse.Action):
+    """Provide a custom action so the -h and --help options
+    to the main app will print a list of the commands.
+
+    The commands are determined by checking the CommandManager
+    instance, passed in as the "default" value for the action.
+    """
+    def __call__(self, parser, namespace, values, option_string=None):
+
+	rows, columns = os.popen('stty size', 'r').read().split()
+        app = self.default
+        app.stdout.write('\nCommands:\n')
+        command_manager = app.command_manager
+        for name, ep in sorted(command_manager):
+            try:
+                factory = ep.load()
+            except Exception as err:
+                app.stdout.write('Could not load %r\n' % ep)
+                if namespace.debug:
+                    traceback.print_exc(file=app.stdout)
+                continue
+            try:
+                cmd = factory(app, None)
+                if cmd.deprecated:
+                    continue
+            except Exception as err:
+                app.stdout.write('Could not instantiate %r: %s\n' % (ep, err))
+                if namespace.debug:
+                    traceback.print_exc(file=app.stdout)
+                continue
+            if name == "complete":
+                one_liner = cmd.get_description().split('\n')[0]
+            else:
+                one_liner = cmd.get_description(name)
+	    if len(name) > 35:
+                app.stdout.write('%s\n' % (name))
+                app.stdout.write('                                 %s\n' % (one_liner))
+	    elif 35 + len(one_liner) > columns:
+                app.stdout.write('%s\n' % (name))
+                app.stdout.write('                                 %s\n' % (one_liner))
+	    else:
+                app.stdout.write('  %-33s  %s\n' % (name, one_liner))
+        sys.exit(0)
+
+
+class HelpCommand(Command):
+    """print detailed help for another command
+    """
+
+    def get_parser(self, prog_name):
+        parser = super(HelpCommand, self).get_parser(prog_name)
+        parser.add_argument('cmd',
+                            nargs='*',
+                            help='name of the command',
+                            )
+        return parser
+
+    def take_action(self, parsed_args):
+        if parsed_args.cmd:
+            try:
+                the_cmd = self.app.command_manager.find_command(
+                    parsed_args.cmd,
+                )
+                cmd_factory, cmd_name, search_args = the_cmd
+            except ValueError:
+                # Did not find an exact match
+                cmd = parsed_args.cmd[0]
+                fuzzy_matches = [k[0] for k in self.app.command_manager
+                                 if k[0].startswith(cmd)
+                                 ]
+                if not fuzzy_matches:
+                    raise
+                self.app.stdout.write('Command "%s" matches:\n' % cmd)
+                for fm in sorted(fuzzy_matches):
+                    self.app.stdout.write('  %s\n' % fm)
+                return
+            self.app_args.cmd = search_args
+            cmd = cmd_factory(self.app, self.app_args)
+            full_name = (cmd_name
+                         if self.app.interactive_mode
+                         else ' '.join([self.app.NAME, cmd_name])
+                         )
+            cmd_parser = cmd.get_parser(full_name)
+        return 0
