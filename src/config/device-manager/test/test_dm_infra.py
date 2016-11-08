644dd9b9b5de1033ab0fe8ed8e32c2719045a640
sbalineni <sbalineni@juniper.net>
2016-11-07 16:32:01 -0800

    [DM]: Allocate ip from each subnet of VN for lo0 interface
    - allocate ip from each subnet of VN (non-external L3 VNs) and configure lo0 interface
    - lo0 interface ifl unit number is calculated as 1000 + vn_id
    - for now, do not allocate this ip if vn is type external
    - persist allocated IPs in DB, and on restart use same allocated ips
    - on delete, free up allocated ips
    - Cassandra Key indexing format is changed for supporting both irb & lo0
    - Upgrade issue is taken care
    - Added UT cases
    - Fixed ip_alloc recent regression issue (bad parameter was passed to virtual_network_ip_alloc api)
    
    Generated Config:
    
    root@cmbu-tasman# show groups __contrail__ interfaces lo0 | display set
    set groups __contrail__ interfaces lo0 unit 0 family inet address 172.16.184.200/32 primary
    set groups __contrail__ interfaces lo0 unit 0 family inet address 172.16.184.200/32 preferred
    set groups __contrail__ interfaces lo0 unit 1210 family inet address 40.0.0.6/32
    
    [edit]
    root@cmbu-tasman# show groups __contrail__ interfaces lo0 | display xml
    <rpc-reply xmlns:junos="http://xml.juniper.net/junos/14.2R6/junos">
        <configuration junos:changed-seconds="1471546906" junos:changed-localtime="2016-08-18 12:01:46 PDT">
                <groups>
                    <name>__contrail__</name>
                    <interfaces>
                        <interface>
                            <name>lo0</name>
                            <unit>
                                <name>0</name>
                                <family>
                                    <inet>
                                        <address>
                                            <name>172.16.184.200/32</name>
                                            <primary/>
                                            <preferred/>
                                        </address>
                                    </inet>
                                </family>
                            </unit>
                            <unit>
                                <name>1210</name>
                                <family>
                                    <inet>
                                        <address>
                                            <name>40.0.0.6/32</name>
                                        </address>
                                    </inet>
                                </family>
                            </unit>
                        </interface>
                    </interfaces>
                </groups>
        </configuration>
        <cli>
            <banner>[edit]</banner>
        </cli>
    </rpc-reply>
    
    Change-Id: I2104b0f5a2f72c824e1e14e464271b7bb44106f5
    Closes-Bug: #1491506

---
+++ src/config/device-manager/test/test_dm_infra.py
@@ -0,0 +1,69 @@
+#
+# Copyright (c) 2013 Juniper Infra, Inc. All rights reserved.
+#
+import sys
+import gevent
+from time import sleep
+sys.path.append("../common/tests")
+from test_utils import *
+from vnc_api.vnc_api import *
+from device_api.juniper_common_xsd import *
+from device_manager.dm_utils import *
+from gevent import monkey
+monkey.patch_all()
+from device_manager.db import DMCassandraDB
+from test_common import *
+from test_dm_common import *
+
+#
+# All Infra related DM test cases should go here
+#
+class TestInfraDM(TestCommonDM):
+
+    @retries(5, hook=retry_exc_handler)
+    def check_dm_state(self):
+        try:
+            dm_cs = DMCassandraDB.getInstance()
+            if dm_cs:
+                self.assertTrue(True)
+        except:
+            self.assertTrue(False)
+
+    # test dm private cassandra data
+    def test_dm_cassandra(self):
+        #wait for dm
+        self.check_dm_state()
+        dm_cs = DMCassandraDB.getInstance()
+        ip_addr = '10.0.0.1/32'
+        lo0_ip_addr = '20.0.0.1/32'
+        key = 'pr-uuid' + ':' + 'vn-uuid' + ':' + 'sub-prefix'
+        dm_cs.add(dm_cs._PR_VN_IP_CF, key, {DMUtils.get_ip_cs_column_name('irb'): ip_addr})
+        dm_cs.add(dm_cs._PR_VN_IP_CF, key, {DMUtils.get_ip_cs_column_name('lo0'): lo0_ip_addr})
+        dm_cs.init_pr_map()
+
+        key_value = dm_cs.get_one_col(dm_cs._PR_VN_IP_CF, key, DMUtils.get_ip_cs_column_name('irb'))
+        if key_value != ip_addr:
+            self.assertTrue(False)
+        key_value = dm_cs.get_one_col(dm_cs._PR_VN_IP_CF, key, DMUtils.get_ip_cs_column_name('lo0'))
+        if key_value != lo0_ip_addr:
+            self.assertTrue(False)
+
+        # delete one column
+        dm_cs.delete(dm_cs._PR_VN_IP_CF, key, [DMUtils.get_ip_cs_column_name('lo0')])
+
+        try:
+            key_value = dm_cs.get_one_col(dm_cs._PR_VN_IP_CF, key, DMUtils.get_ip_cs_column_name('lo0'))
+            if key_value is not None:
+                self.assertTrue(False)
+        except NoIdError:
+            pass
+
+        key_value = dm_cs.get_one_col(dm_cs._PR_VN_IP_CF, key, DMUtils.get_ip_cs_column_name('irb'))
+        if key_value != ip_addr:
+            self.assertTrue(False)
+
+        return
+    # end
+
+# end TestInfraDM
+
