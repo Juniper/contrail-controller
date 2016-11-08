53923b64611e9b09be33fbc3e81649a53d365f7b
Ravi BK <ravibk@juniper.net>
2016-11-08 09:22:58 +0530

    Moving TbbKeepAwake task to TaskScheduler
    In the current infra, tbb keepawake task has to be instantiated by
    each client(agent, control etc) which requires it. It is suggested to
    move this part to TaskScheduler so that task is created when it is
    instantiated, given EventManager object to it. This timer task is created
    with default timeout of 1 sec. Client can modify this value according to its
    need.
    
    Change-Id: Idc7b006b8a659a2bf597a37cd91030ccd77d2bfd
    Closes-Bug: #1637293

--- src/control-node/main.cc
+++ src/control-node/main.cc
@@ -54,7 +54,6 @@
 #include "xmpp/xmpp_init.h"
 #include "xmpp/xmpp_sandesh.h"
 #include "xmpp/xmpp_server.h"
-#include "base/task_tbbkeepawake.h"
 
 using namespace std;
 using namespace boost::asio::ip;
@@ -135,8 +134,7 @@ static void ShutdownDiscoveryClient(DiscoveryServiceClient *client) {
 static void ShutdownServers(
     boost::scoped_ptr<BgpXmppChannelManager> *channel_manager,
     DiscoveryServiceClient *dsclient,
-    Timer *node_info_log_timer,
-    TaskTbbKeepAwake *tbb_awake_task) {
+    Timer *node_info_log_timer) {
 
     // Bring down bgp server, xmpp server, etc. in the right order.
     BgpServer *bgp_server = (*channel_manager)->bgp_server();
@@ -172,8 +170,6 @@ static void ShutdownServers(
 
     ConnectionStateManager::
         GetInstance()->Shutdown();
-
-    tbb_awake_task->ShutTbbKeepAwakeTask();
 
     // Do sandesh cleanup.
     Sandesh::Uninit();
@@ -327,7 +323,7 @@ int main(int argc, char *argv[]) {
                         Sandesh::StringToLevel(options.log_level())));
     }
 
-    TaskScheduler::Initialize();
+    TaskScheduler::Initialize(0, &evm);
     TaskScheduler::GetInstance()->SetTrackRunTime(
         options.task_track_run_time());
     BgpServer::Initialize();
@@ -439,12 +435,6 @@ int main(int argc, char *argv[]) {
                     bgp_server.get(), ifmap_manager, _1, _2, _3,
                     expected_connections), "ObjectBgpRouter");
 
-    // Start TbbKeepAwake Task which makes scheduler always active,
-    // for it to not miss any spawn events
-    TaskTbbKeepAwake tbb_awake_task;
-    tbb_awake_task.StartTbbKeepAwakeTask(TaskScheduler::GetInstance(), &evm,
-                                         "bgp::TbbKeepAwake");
-
     // Parse discovery server configuration.
     DiscoveryServiceClient *ds_client = NULL;
     tcp::endpoint dss_ep;
@@ -497,8 +487,7 @@ int main(int argc, char *argv[]) {
                                    &sandesh_context));
             if (!success) {
                 LOG(ERROR, "SANDESH: Initialization FAILED ... exiting");
-                ShutdownServers(&bgp_peer_manager, ds_client, NULL,
-                                &tbb_awake_task);
+                ShutdownServers(&bgp_peer_manager, ds_client, NULL);
                 exit(1);
             }
         }
@@ -576,8 +565,7 @@ int main(int argc, char *argv[]) {
     // Event loop.
     evm.Run();
 
-    ShutdownServers(&bgp_peer_manager, ds_client, node_info_log_timer.get(),
-                    &tbb_awake_task);
+    ShutdownServers(&bgp_peer_manager, ds_client, node_info_log_timer.get());
     BgpServer::Terminate();
     return 0;
 }
