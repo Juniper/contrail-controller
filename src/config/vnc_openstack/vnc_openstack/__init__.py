0553a748d1f29766cfafb92f52a324ca7d6daf48
Ignatious Johnson Christopher <ijohnson@juniper.net>
2016-11-21 15:07:15 -0800

    Certificates needs to be chanined and bundled in the order (certfile, keyfile and cacert).
    1. Chaining in the certificate in correct order
    2. Making certfile/keyfile optional
    
    Closes-Bug: 1639426
    Closes-Bug: 1630513
    
    Conflicts:
    	src/api-lib/vnc_api.py
    
    Change-Id: I599389972824c1cad37962306fac023bf16ce91c

--- src/config/vnc_openstack/vnc_openstack/__init__.py
+++ src/config/vnc_openstack/vnc_openstack/__init__.py
@@ -78,8 +78,10 @@ def fill_keystone_opts(obj, conf_sections):
 
     obj._kscertbundle=''
     obj._use_certs=False
-    if obj._certfile and obj._keyfile and obj._cafile:
-       certs=[obj._certfile,obj._keyfile,obj._cafile]
+    if obj._certfile:
+        certs = [obj._certfile]
+        if obj._keyfile and obj._cafile:
+            certs=[obj._certfile,obj._keyfile,obj._cafile]
        obj._kscertbundle=cfgmutils.getCertKeyCaBundle(_DEFAULT_KS_CERT_BUNDLE,certs)
        obj._use_certs=True
 
