931f1c20fe6e892e23b9f8797e75ed15a039d792
Ananth Suryanarayana <anantha@juniper.net>
2016-11-22 14:19:52 -0800

    Do not use configured timer value for rtarget end-of-rib sends and receives
    VPN routes are exchanged only after end-of-rib is received for rtarget family
    or if the eor-timer for rtarget family expires. When a large eor timer is
    configured in order to accomodate large vpn routes, we should not defer the
    eor send for rtargets. Otherwise vpn routes exchange will not even get to
    start until the large eor timer for route target expires.
    
    Instead always use a sane default of 30 seconds for rtarget eors. This is how
    it was before GR code came in
    
    Closes-Bug: #1644033
    
    Change-Id: Id88054929220f8101cee5ed9b34d97045b538220

--- src/bgp/bgp_peer.cc
+++ src/bgp/bgp_peer.cc
@@ -282,6 +282,16 @@ uint64_t BgpPeer::GetEorSendTimerElapsedTimeUsecs() const {
     return UTCTimestampUsec() - eor_send_timer_start_time_;
 }
 
+uint32_t BgpPeer::GetEndOfRibSendTime(Address::Family family) const {
+    return family == Address::RTARGET ?
+        kRouteTargetEndOfRibTimeSecs : server_->GetEndOfRibSendTime();
+}
+
+uint32_t BgpPeer::GetEndOfRibReceiveTime(Address::Family family) const {
+    return family == Address::RTARGET ?
+        kRouteTargetEndOfRibTimeSecs : server_->GetEndOfRibReceiveTime();
+}
+
 bool BgpPeer::EndOfRibSendTimerExpired(Address::Family family) {
     if (!IsReady())
         return false;
@@ -289,7 +299,7 @@ bool BgpPeer::EndOfRibSendTimerExpired(Address::Family family) {
     // Retry if wait time has not exceeded the max (10 times configured) and
     // the output queue has not been fully drained yet.
     if (GetEorSendTimerElapsedTimeUsecs() <
-            server_->GetEndOfRibSendTime() * 1000000 * 10) {
+            GetEndOfRibSendTime(family) * 1000000 * 10) {
         uint32_t output_depth = GetOutputQueueDepth(family);
         if (output_depth) {
             eor_send_timer_[family]->Reschedule(kEndOfRibSendRetryTimeMsecs);
@@ -308,7 +318,7 @@ bool BgpPeer::EndOfRibSendTimerExpired(Address::Family family) {
 }
 
 void BgpPeer::SendEndOfRIB(Address::Family family) {
-    uint32_t timeout = server_->GetEndOfRibSendTime();
+    uint32_t timeout = GetEndOfRibSendTime(family);
 
     eor_send_timer_start_time_ = UTCTimestampUsec();
     BGP_LOG_PEER(Message, this, SandeshLevel::SYS_INFO,
@@ -1799,7 +1809,7 @@ void BgpPeer::RegisterToVpnTables() {
 }
 
 void BgpPeer::StartEndOfRibReceiveTimer(Address::Family family) {
-    uint32_t timeout = server_->GetEndOfRibReceiveTime();
+    uint32_t timeout = GetEndOfRibReceiveTime(family);
 
     BGP_LOG_PEER(Message, this, SandeshLevel::SYS_INFO,
         BGP_LOG_FLAG_SYSLOG, BGP_PEER_DIR_OUT,
