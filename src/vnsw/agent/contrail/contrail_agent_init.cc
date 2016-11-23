35eff21016c320a06ceefeb74e7ca647eb35169f
ashoksingh <ashoksr@juniper.net>
2016-11-23 20:05:51 +0530

    Open Agent's REST API port after agent initialization is done
    In vrouter-port-control add the following checks for DPDK mode
    --Wait for agent's REST API port to be opened before sending port
      adds with a timeout of 120 seconds
    --Wait for DPDK vrouter to open netlink tcp port before sending port
      adds with a timeout of 120 seconds
    
    Also return different error codes for each failure to identify the
    point of failure in the script
    
    Closes-Bug: #1614616
    (cherry picked from commit 5bcca7956f374c43096d4d255ef12646968deab6)
    
    Change-Id: I647bc42b55b9c7c7c095302447788bc8465bcc49

--- src/vnsw/agent/contrail/contrail_agent_init.cc
+++ src/vnsw/agent/contrail/contrail_agent_init.cc
@@ -130,6 +130,11 @@ void ContrailAgentInit::WaitForIdle() {
 void ContrailAgentInit::InitDone() {
     ContrailInitCommon::InitDone();
 
+    if (agent()->rest_server()) {
+        /* Open REST API port for port add/change/deletes */
+        agent()->rest_server()->InitDone();
+    }
+
     /* Reads and processes port information written by nova-compute */
     PortIpcHandler *pih = agent()->port_ipc_handler();
     if (pih) {
