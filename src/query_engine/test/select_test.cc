2f6d7d2eb28352eae78c95ff5f925171c656b4a9
arvindvis <arvindv@juniper.net>
2016-11-28 17:00:00 -0800

    This fix gets the ObjectId field along with UUID, when ObjectTable is queried. Currently, if ObjectId is part of select term, it is  always null. To overcome this, while where_query processing is  called we get the ObjectId along with UUID in the case of ObjectTable. In the select processing, if ObjectId is also part of  select term, then we look up the ObjectId's corresponding to the  UUID returned by where_ processing. Closes-Bug: #1642088
    Change-Id: I52df5f483333dbec994a5bbd89fe46d666ec2ff7

--- src/query_engine/test/select_test.cc
+++ src/query_engine/test/select_test.cc
@@ -6,6 +6,7 @@
 
 #include "query.h"
 #include "analytics_query_mock.h"
+#include <boost/uuid/uuid.hpp>
 
 using ::testing::Return;
 using ::testing::AnyNumber;
@@ -40,6 +41,17 @@ public:
         EXPECT_CALL(aqmock, direction_ing())
             .Times(AnyNumber())
             .WillRepeatedly(Return(1));
+    }
+
+    bool test_process_object_query_specific_select_params(
+                        const std::string& sel_field,
+                        std::map<std::string, GenDb::DbDataValue>& col_res_map,
+                        std::map<std::string, std::string>& cmap,
+                        const boost::uuids::uuid& uuid,
+                        std::map<boost::uuids::uuid, std::string>&
+                        uuid_to_objid_map, SelectQuery *sq) {
+         return sq->process_object_query_specific_select_params(sel_field,
+             col_res_map, cmap, uuid, uuid_to_objid_map);
     }
 };
 
@@ -102,6 +114,46 @@ TEST_F(SelectTest, InvalidFlowseriesSelectStatsQuery) {
     SelectQuery* select_query = new SelectQuery(&analytics_query_mock,
                                                 json_select);
     EXPECT_TRUE(select_query->status_details == EINVAL);
+}
+
+/* This test checks if process_object_query_specific_select_params
+ * function can work correctly when it is passed a object_id as a
+ * select field
+ */
+
+TEST_F(SelectTest, TestProcessObjectQuerySelectParams) {
+    AnalyticsQueryMock analytics_query_mock;
+    select_fs_query_default_expect_init(analytics_query_mock);
+
+    std::map<std::string, std::string> json_select;
+    json_select.insert(std::pair<std::string, std::string>(
+        "select_fields", "[\"sum(bytes)\", \"packets\", \"ObjecId\"]"));
+    SelectQuery* select_query = new SelectQuery(&analytics_query_mock,
+                                                json_select);
+    std::string select_field("ObjectId");
+    // This represents the columns fetched from the
+    // MessageTable.
+    std::map<std::string, GenDb::DbDataValue> col_res_map;
+    GenDb::DbDataValue sandesh_type;
+    sandesh_type=(uint32_t)7; // corresposnds to SandeshType::Object
+    col_res_map.insert(std::make_pair("Type",sandesh_type));
+    std::map<std::string, std::string> cmap;
+    boost::uuids::random_generator rgen_;
+    boost::uuids::uuid unm(rgen_());
+    // This is the map looked up to get the object id based on uuid
+    std::map<boost::uuids::uuid, std::string> uuid_to_objectid;
+    uuid_to_objectid.insert(std::make_pair(unm, "id1"));
+    if(test_process_object_query_specific_select_params(select_field,
+        col_res_map, cmap, unm, uuid_to_objectid, select_query)) {
+        std::map<std::string, std::string>::iterator cit;
+        cit = cmap.find(select_field);
+        ASSERT_TRUE(cit != cmap.end());
+        std::string actual_val = cit->second;
+        EXPECT_EQ(actual_val, "id1");
+   } else {
+        ASSERT_TRUE(0);
+   }
+
 }
 
 int main(int argc, char **argv) {
