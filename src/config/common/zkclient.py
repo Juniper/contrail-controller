e99fd6a24196c922f6e31b2e0c765cd63425155a
Sahil Sabharwal <ssabharwal@juniper.net>
2016-12-01 14:23:13 -0800

    Added Zookeeper timeout config knob in ST
    Config knob "zk_timeout" for Zookeeper timeout is added in Schema Transformer.
    ZookeeperClient now takes in arguemet for zk_timeout for which default has been
    set to 400.
    
    Change-Id: Ia1a5569292f4957dba4b3f64aaee997d7db9f9da
    Closes-Bug: 1643846

--- src/config/common/zkclient.py
+++ src/config/common/zkclient.py
@@ -220,7 +220,7 @@ class IndexAllocator(object):
 
 class ZookeeperClient(object):
 
-    def __init__(self, module, server_list, logging_fn=None):
+    def __init__(self, module, server_list, logging_fn=None, zk_timeout=400):
         # logging
         logger = logging.getLogger(module)
         logger.setLevel(logging.DEBUG)
@@ -245,7 +245,7 @@ class ZookeeperClient(object):
                                  sleep_func=gevent.sleep)
         self._zk_client = kazoo.client.KazooClient(
                 server_list,
-                timeout=400,
+                timeout=zk_timeout,
                 handler=kazoo.handlers.gevent.SequentialGeventHandler(),
                 logger=logger,
                 connection_retry=self._retry,
