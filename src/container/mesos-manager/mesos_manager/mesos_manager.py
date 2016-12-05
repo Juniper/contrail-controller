435b8ada60ec5aaeb32ca8dde721b17b21dfa738
Praneet Bachheti <praneetb@juniper.net>
2016-12-04 18:42:24 -0800

    Initial code for mesos-manager - defined a mesos-manager server - handle CNI input data
    Change-Id: Idb014897e8479657584bf9770ccb81106a57709d
    Partial-Bug: #1645814

--- src/container/mesos-manager/mesos_manager/mesos_manager.py
+++ src/container/mesos-manager/mesos_manager/mesos_manager.py
@@ -6,36 +6,46 @@
 Mesos network manager
 """
 
-import socket
 import gevent
 from gevent.queue import Queue
 
 import common.args as mesos_args
 import common.logger as logger
-from vnc_api.vnc_api import *
+from cfgm_common import vnc_cgitb
 import vnc.vnc_mesos as vnc_mesos
+import mesos_server as mserver
+
 
 class MesosNetworkManager(object):
     def __init__(self, args=None):
         self.args = args
-        self.logger = logger.MesosManagerLogger(args)
         self.q = Queue()
+
+        self.logger = logger.MesosManagerLogger(args)
+
         self.vnc = vnc_mesos.VncMesos(args=self.args,
             logger=self.logger, q=self.q)
+
+        self.mserver = mserver.MesosServer(args=self.args,
+            logger=self.logger)
+    # end __init__
 
     def start_tasks(self):
         self.logger.info("Starting all tasks.")
 
         gevent.joinall([
             gevent.spawn(self.vnc.vnc_process),
+            gevent.spawn(self.mserver.start_server),
         ])
+    # end start_tasks
 
+# end class MesosNetworkManager
 
 def main():
+    vnc_cgitb.enable(format='text')
     args = mesos_args.parse_args()
     mesos_nw_mgr = MesosNetworkManager(args)
     mesos_nw_mgr.start_tasks()
-
 
 if __name__ == '__main__':
     main()
