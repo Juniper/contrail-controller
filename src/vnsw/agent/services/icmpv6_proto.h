3626f88a2630a0077a8cc8318757a95663fe1e03
Naveen N <naveenn@juniper.net>
2016-12-06 18:55:16 +0530

    * Try ARP request even when EVPN route is in wait for traffic mode
    Currently if only EVPN route is in backup mode, GARP packets are
    not trapped to agent, hence upon mastership switch agent would
    not be able to identify and update EVPN route preference.
    Its easily reproducible by changing the group id of VRRP.
    
    Consider VM 1 was configured with group ID 100, but AAP was configured
    with AAP vmac of group ID 10, now prefernece of IP route would be
    bumped but not EVPN route since mac of GARP packets doesnt match with
    AAP configured mac. Now if group ID is changed to 10, VRRP would still
    send a GARP but it would not be trapped to agent, since l3 route is
    already in high prefernece. Fixing the same by having TRAP flag set even
    if EVPN route is in backup mode, also from flow if EVPN route is in low
    preference.
    Closes-bug:#1644170
    
    Change-Id: Ia849c30e8115463ee7f87d8569444b2997c14959

--- src/vnsw/agent/services/icmpv6_proto.h
+++ src/vnsw/agent/services/icmpv6_proto.h
@@ -21,6 +21,7 @@ do {                                                                         \
 } while (false)                                                              \
 
 class Icmpv6VrfState;
+class Icmpv6PathPreferenceState;
 
 class Icmpv6Proto : public Proto {
 public:
@@ -70,8 +71,12 @@ public:
     const Icmpv6Stats &GetStats() const { return stats_; }
     Icmpv6Stats *VmiToIcmpv6Stats(VmInterface *i);
     void ClearStats() { stats_.Reset(); }
-    void ValidateAndClearVrfState(VrfEntry *vrf);
+    bool ValidateAndClearVrfState(VrfEntry *vrf, Icmpv6VrfState *state);
     Icmpv6VrfState *CreateAndSetVrfState(VrfEntry *vrf);
+
+    DBTableBase::ListenerId vrf_table_listener_id() const {
+        return vrf_table_listener_id_;
+    }
 
 private:
     Timer *timer_;
@@ -87,58 +92,153 @@ private:
 
 class Icmpv6VrfState : public DBState {
 public:
+    typedef std::map<const IpAddress,
+                     Icmpv6PathPreferenceState*> Icmpv6PathPreferenceStateMap;
+    typedef std::pair<const IpAddress,
+                     Icmpv6PathPreferenceState*> Icmpv6PathPreferenceStatePair;
+
     Icmpv6VrfState(Agent *agent, Icmpv6Proto *proto, VrfEntry *vrf,
-                   AgentRouteTable *table);
+                   AgentRouteTable *table, AgentRouteTable *evpn_table);
     ~Icmpv6VrfState();
     Agent *agent() const { return agent_; }
     Icmpv6Proto * icmp_proto() const { return icmp_proto_; }
     void set_route_table_listener_id(const DBTableBase::ListenerId &id) {
         route_table_listener_id_ = id;
     }
+    void set_evpn_route_table_listener_id(const DBTableBase::ListenerId &id) {
+        evpn_route_table_listener_id_ = id;
+    }
     bool default_routes_added() const { return default_routes_added_; }
     void set_default_routes_added(bool value) { default_routes_added_ = value; }
 
     void RouteUpdate(DBTablePartBase *part, DBEntryBase *entry);
+    void EvpnRouteUpdate(DBTablePartBase *part, DBEntryBase *entry);
     void ManagedDelete() { deleted_ = true;}
     void Delete();
     bool DeleteRouteState(DBTablePartBase *part, DBEntryBase *entry);
-    void PreWalkDone(DBTableBase *partition);
+    bool DeleteEvpnRouteState(DBTablePartBase *part, DBEntryBase *entry);
+    bool PreWalkDone(DBTableBase *partition);
     static void WalkDone(DBTableBase *partition, Icmpv6VrfState *state);
     bool deleted() const {return deleted_;}
+
+    Icmpv6PathPreferenceState* Locate(const IpAddress &ip);
+    void Erase(const IpAddress &ip);
+    Icmpv6PathPreferenceState* Get(const IpAddress ip) {
+        return icmpv6_path_preference_map_[ip];
+    }
+
+    const DBTableBase::ListenerId &walk_id() {
+        return walk_id_;
+    }
+
+    const DBTableBase::ListenerId &evpn_walk_id() {
+        return evpn_walk_id_;
+    }
+
+    bool l3_walk_completed() const {
+        return l3_walk_completed_;
+    }
+
+    bool evpn_walk_completed() const {
+        return evpn_walk_completed_;
+    }
 
 private:
     Agent *agent_;
     Icmpv6Proto *icmp_proto_;
     VrfEntry *vrf_;
     AgentRouteTable *rt_table_;
+    AgentRouteTable *evpn_rt_table_;
     DBTableBase::ListenerId route_table_listener_id_;
+    DBTableBase::ListenerId evpn_route_table_listener_id_;
     LifetimeRef<Icmpv6VrfState> table_delete_ref_;
+    LifetimeRef<Icmpv6VrfState> evpn_table_delete_ref_;
     bool deleted_;
     bool default_routes_added_;
     DBTableWalker::WalkId walk_id_;
+    DBTableWalker::WalkId evpn_walk_id_;
+    Icmpv6PathPreferenceStateMap icmpv6_path_preference_map_;
+    bool l3_walk_completed_;
+    bool evpn_walk_completed_;
     DISALLOW_COPY_AND_ASSIGN(Icmpv6VrfState);
 };
 
-class Icmpv6RouteState : public DBState {
+class Icmpv6PathPreferenceState {
 public:
     static const uint32_t kMaxRetry = 30 * 5; //retries upto 5 minutes,
                                               //30 tries/per minutes
     static const uint32_t kTimeout = 2000;
     typedef std::map<uint32_t, uint32_t> WaitForTrafficIntfMap;
+    typedef std::set<uint32_t> NDTransmittedIntfMap;
 
-    Icmpv6RouteState(Icmpv6VrfState *vrf_state, uint32_t vrf_id,
-                     IpAddress vm_ip_addr, uint8_t plen);
-    ~Icmpv6RouteState();
+    Icmpv6PathPreferenceState(Icmpv6VrfState *vrf_state, uint32_t vrf_id,
+                              IpAddress vm_ip_addr, uint8_t plen);
+    ~Icmpv6PathPreferenceState();
     bool SendNeighborSolicit();
-    void SendNeighborSolicitForAllIntf(const InetUnicastRouteEntry *route);
+    bool SendNeighborSolicit(WaitForTrafficIntfMap &wait_for_traffic_map,
+                            NDTransmittedIntfMap &nd_transmitted_map);
+    void SendNeighborSolicitForAllIntf(const AgentRoute *route);
     void StartTimer();
+
+    Icmpv6VrfState* vrf_state() {
+        return vrf_state_;
+    }
+
+    const IpAddress& ip() const {
+        return vm_ip_;
+    }
+
+    bool IntfPresentInIpMap(uint32_t id) {
+        if (l3_wait_for_traffic_map_.find(id) ==
+                l3_wait_for_traffic_map_.end()) {
+            return false;
+        }
+        return true;
+    }
+
+    bool IntfPresentInEvpnMap(uint32_t id) {
+        if (evpn_wait_for_traffic_map_.find(id) ==
+                evpn_wait_for_traffic_map_.end()) {
+            return false;
+        }
+        return true;
+    }
+
+    uint32_t IntfRetryCountInIpMap(uint32_t id) {
+        return l3_wait_for_traffic_map_[id];
+    }
+
+    uint32_t IntfRetryCountInEvpnMap(uint32_t id) {
+        return evpn_wait_for_traffic_map_[id];
+    }
+
 private:
+    friend void intrusive_ptr_add_ref(Icmpv6PathPreferenceState *ps);
+    friend void intrusive_ptr_release(Icmpv6PathPreferenceState *ps);
     Icmpv6VrfState *vrf_state_;
     Timer *ns_req_timer_;
     uint32_t vrf_id_;
     IpAddress vm_ip_;
     uint8_t plen_;
     IpAddress gw_ip_;
-    WaitForTrafficIntfMap wait_for_traffic_map_;
+    WaitForTrafficIntfMap l3_wait_for_traffic_map_;
+    WaitForTrafficIntfMap evpn_wait_for_traffic_map_;
+    tbb::atomic<int> refcount_;
+};
+
+typedef boost::intrusive_ptr<Icmpv6PathPreferenceState>
+            Icmpv6PathPreferenceStatePtr;
+
+void intrusive_ptr_add_ref(Icmpv6PathPreferenceState *ps);
+void intrusive_ptr_release(Icmpv6PathPreferenceState *ps);
+
+class Icmpv6RouteState : public DBState {
+public:
+    Icmpv6RouteState(Icmpv6VrfState *vrf_state, uint32_t vrf_id,
+                     IpAddress vm_ip_addr, uint8_t plen);
+    ~Icmpv6RouteState();
+    void SendNeighborSolicitForAllIntf(const AgentRoute *route);
+private:
+    Icmpv6PathPreferenceStatePtr icmpv6_path_preference_state_;
 };
 #endif // vnsw_agent_icmpv6_proto_h
---
+++ src/vnsw/agent/services/test/icmpv6_path_preference_test.cc
@@ -0,0 +1,164 @@
+/*
+ * Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
+ */
+
+#include "base/os.h"
+#include "testing/gunit.h"
+
+#include <sys/socket.h>
+#include <netinet/if_ether.h>
+#include <base/logging.h>
+
+#include <io/event_manager.h>
+#include <cmn/agent_cmn.h>
+#include <oper/operdb_init.h>
+#include <controller/controller_init.h>
+#include <controller/controller_vrf_export.h>
+#include <pkt/pkt_init.h>
+#include <services/services_init.h>
+#include <vrouter/ksync/ksync_init.h>
+#include <oper/vrf.h>
+#include <oper/mirror_table.h>
+#include <pugixml/pugixml.hpp>
+#include <services/icmpv6_proto.h>
+#include <vr_interface.h>
+#include <test/test_cmn_util.h>
+#include <test/pkt_gen.h>
+#include "vr_types.h"
+#include "xmpp/test/xmpp_test_util.h"
+#include <services/services_sandesh.h>
+#include "oper/path_preference.h"
+
+struct PortInfo input1[] = {
+    {"vnet1", 1, "8.1.1.1", "00:00:00:01:01:01", 1, 1, "fd11::2"}
+};
+
+struct PortInfo input2[] = {
+    {"vnet2", 2, "8.1.1.1", "00:00:00:01:01:01", 1, 1, "fd11::2"}
+};
+
+IpamInfo ipam_info[] = {
+    {"8.1.1.0", 24, "8.1.1.10", true},
+    {"fd11::", 96, "fd11::1"},
+};
+
+class Icmp6PathPreferenceTest : public ::testing::Test {
+public:
+    Icmp6PathPreferenceTest(): agent(Agent::GetInstance()) {}
+    virtual void SetUp() {
+        CreateV6VmportEnv(input1, 1);
+        client->WaitForIdle();
+        EXPECT_TRUE(VmPortActive(1));
+        AddIPAM("vn1", ipam_info, 2);
+        client->WaitForIdle();
+    }
+
+    virtual void TearDown() {
+        DeleteVmportEnv(input1, 1, true, 0, NULL, NULL, true, true);
+        client->WaitForIdle();
+        DelIPAM("vn1");
+        client->WaitForIdle();
+        EXPECT_TRUE(VrfGet("vrf1", true) == NULL);
+    }
+
+    Icmpv6VrfState* GetVrfState(uint32_t vrf_id) {
+        const VrfEntry *vrf = agent->vrf_table()->FindVrfFromId(vrf_id);
+        return static_cast<Icmpv6VrfState *>(vrf->GetState(agent->vrf_table(),
+                                          agent->icmpv6_proto()->
+                                          vrf_table_listener_id()));
+    }
+
+    bool CheckIpMap(uint32_t vrf_id, uint32_t id, const IpAddress &ip) {
+        Icmpv6VrfState *state = GetVrfState(vrf_id);
+        return state->Get(ip)->IntfPresentInIpMap(id);
+    }
+
+    bool CheckEvpnMap(uint32_t vrf_id, uint32_t id, const IpAddress &ip) {
+        Icmpv6VrfState *state = GetVrfState(vrf_id);
+        return state->Get(ip)->IntfPresentInEvpnMap(id);
+    }
+
+    uint32_t GetIpMapRetryCount(uint32_t vrf_id, uint32_t id, const IpAddress &ip) {
+        Icmpv6VrfState *state = GetVrfState(vrf_id);
+        return state->Get(ip)->IntfRetryCountInIpMap(id);
+    }
+
+    uint32_t GetEvpnMapRetryCount(uint32_t vrf_id, uint32_t id, const IpAddress &ip) {
+        Icmpv6VrfState *state = GetVrfState(vrf_id);
+        return state->Get(ip)->IntfRetryCountInEvpnMap(id);
+    }
+protected:
+    Agent *agent;
+};
+
+//Check EVPN route and IP route list has 
+//interface ID
+TEST_F(Icmp6PathPreferenceTest, Test1) {
+    uint32_t vrf_id = VrfGet("vrf1")->vrf_id();
+    uint32_t intf_id = VmPortGetId(1);
+    const IpAddress ip = Ip6Address::from_string("fd11::2");
+    EXPECT_TRUE(CheckIpMap(vrf_id, intf_id, ip));
+    EXPECT_TRUE(CheckEvpnMap(vrf_id, intf_id, ip));
+
+    EXPECT_TRUE(GetIpMapRetryCount(vrf_id, intf_id, ip) >= 1);
+    EXPECT_TRUE(GetEvpnMapRetryCount(vrf_id, intf_id, ip) >= 1);
+}
+
+//Enqueue Traffic seen for IP route, verify
+//EVPN route still has interface ID in map
+TEST_F(Icmp6PathPreferenceTest, Test2) {
+    CreateV6VmportEnv(input2, 1);
+    client->WaitForIdle();
+
+    uint32_t vrf_id = VrfGet("vrf1")->vrf_id();
+    uint32_t intf_id = VmPortGetId(1);
+    uint32_t intf_id1 = VmPortGetId(2);
+    const IpAddress ip = Ip6Address::from_string("fd11::2");
+
+    EXPECT_TRUE(CheckIpMap(vrf_id, intf_id, ip));
+    EXPECT_TRUE(CheckEvpnMap(vrf_id, intf_id, ip));
+    EXPECT_TRUE(GetIpMapRetryCount(vrf_id, intf_id, ip) >= 2);
+    EXPECT_TRUE(GetEvpnMapRetryCount(vrf_id, intf_id, ip) >= 2);
+
+    EXPECT_TRUE(CheckIpMap(vrf_id, intf_id1, ip));
+    EXPECT_TRUE(CheckEvpnMap(vrf_id, intf_id1, ip));
+    EXPECT_TRUE(GetIpMapRetryCount(vrf_id, intf_id1, ip) >= 1);
+    EXPECT_TRUE(GetEvpnMapRetryCount(vrf_id, intf_id1, ip) >= 1);
+
+    DeleteVmportEnv(input2, 1, false, 0, NULL, NULL, true, true);
+    client->WaitForIdle();
+}
+
+TEST_F(Icmp6PathPreferenceTest, Test3) {
+    uint32_t vrf_id = VrfGet("vrf1")->vrf_id();
+    uint32_t intf_id = VmPortGetId(1);
+    const IpAddress ip = Ip6Address::from_string("fd11::2");
+    EXPECT_TRUE(CheckIpMap(vrf_id, intf_id, ip));
+    EXPECT_TRUE(CheckEvpnMap(vrf_id, intf_id, ip));
+
+    EXPECT_TRUE(GetIpMapRetryCount(vrf_id, intf_id, ip) >= 1);
+    EXPECT_TRUE(GetEvpnMapRetryCount(vrf_id, intf_id, ip) >= 1);
+
+    agent->oper_db()->route_preference_module()->
+        EnqueueTrafficSeen(ip, 128, intf_id, vrf_id,
+                           MacAddress::ZeroMac());
+    client->WaitForIdle();
+
+    EXPECT_FALSE(CheckIpMap(vrf_id, intf_id, ip));
+    EXPECT_TRUE(CheckEvpnMap(vrf_id, intf_id, ip));
+    EXPECT_TRUE(GetEvpnMapRetryCount(vrf_id, intf_id, ip) >= 1);
+}
+
+
+int main(int argc, char *argv[]) {
+    GETUSERARGS();
+
+    client = TestInit(init_file, ksync_init, true, true);
+    usleep(100000);
+    client->WaitForIdle();
+    
+    int ret = RUN_ALL_TESTS();
+    TestShutdown();
+    delete client;
+    return ret;
+}
