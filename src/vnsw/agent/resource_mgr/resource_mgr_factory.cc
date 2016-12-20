9a597debca896a7023a6e4fb2278404ac028adcd
jayaramsatya <sajayaram@juniper.net>
2016-12-20 14:58:35 +0530

    To Back up the resource data infra has been added. Sandesh encode & Decode functinality is used, encode the data in to buffers and save in to file and, same will be readed back to buffers from file and decode to feature data.
    Partial-Bug : #1651363
    
    Change-Id: Iee309b18a25e94c9b6acdee9f8402d7a34f1d32c

---
+++ src/vnsw/agent/resource_mgr/resource_mgr_factory.cc
@@ -0,0 +1,29 @@
+#include "resource_mgr_factory.h"
+
+
+ResourceMgrFactory::ResourceMgrFactory(Agent *agent):agent_(agent) {
+
+}
+
+ResourceMgrFactory::~ResourceMgrFactory() {
+}
+
+void ResourceMgrFactory::Shutdown() {
+    mplsrmgr_.reset();
+}
+
+ResourceManager* 
+ResourceMgrFactory::GetResourceMgr(ResourceType type_) {
+    switch(type_) {
+        case MPLS_TYPE:
+        {
+           if(mplsrmgr_.get() == NULL) {
+                mplsrmgr_.reset( new MplsResourceManager(
+                            MplsResourceManager::mpls_tmp_file_, agent_));
+           }
+          return mplsrmgr_.get();
+        }
+        default:
+            return NULL;
+    }
+}
