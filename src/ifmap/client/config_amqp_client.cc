fd2185cb378e1a85f8ebd44a20d6ac5eb2ed30b7
Prakash M Bailkeri <prakashmb@juniper.net>
2017-01-04 18:19:38 +0000

    Config read from cassandra
    Changes in this commit:
    1. Json encoding for refs, list property, map property and parent
    2. Bulk sync code. [Reading from fqname table, Starting multiple task to read in parallel etc]
    3. Thirdparty packages for AMQP client and SConscript for same
    4. Class for reading Rabbit messages
    
    Other dependent review in progress
    https://review.opencontrail.org/#/c/27581/
    https://review.opencontrail.org/#/c/27579/
    
    Change-Id: Idce47fd59fcdfd7d778451c6e1ff220d2b1897a1
    Partial-Bug: #1632470

---
+++ src/ifmap/client/config_amqp_client.cc
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) 2017 Juniper Networks, Inc. All rights reserved.
+ */
+
+#include "config_amqp_client.h"
+
+#include <stdio.h>
+
+#include <SimpleAmqpClient/SimpleAmqpClient.h>
+#include "rapidjson/document.h"
+
+#include "base/task.h"
+#include "ifmap/ifmap_config_options.h"
+#include "config_client_manager.h"
+
+using namespace std;
+using namespace rapidjson;
+
+class ConfigAmqpClient::RabbitMQReader : public Task {
+public:
+    RabbitMQReader(ConfigAmqpClient *amqpclient) 
+        : Task(amqpclient->reader_task_id()), amqpclient_(amqpclient) {
+    }
+
+    virtual bool Run();
+    string Description() const { return "ConfigAmqpClient::RabbitMQReader"; }
+
+private:
+    ConfigAmqpClient *amqpclient_;
+    AmqpClient::Channel::ptr_t channel_;
+    string consumer_tag_;
+    bool ConnectToRabbitMQ();
+};
+
+ConfigAmqpClient::ConfigAmqpClient(ConfigClientManager *mgr, string hostname,
+                                   const IFMapConfigOptions &options) :
+    mgr_(mgr), hostname_(hostname), rabbitmq_ip_(options.rabbitmq_ip),
+    rabbitmq_port_(options.rabbitmq_port),
+    rabbitmq_user_(options.rabbitmq_user),
+    rabbitmq_password_(options.rabbitmq_password),
+    rabbitmq_vhost_(options.rabbitmq_vhost),
+    rabbitmq_use_ssl_(options.rabbitmq_use_ssl),
+    rabbitmq_ssl_version_(options.rabbitmq_ssl_version),
+    rabbitmq_ssl_keyfile_(options.rabbitmq_ssl_keyfile),
+    rabbitmq_ssl_certfile_(options.rabbitmq_ssl_certfile),
+    rabbitmq_ssl_ca_certs_(options.rabbitmq_ssl_ca_certs) {
+
+    TaskScheduler *scheduler = TaskScheduler::GetInstance();
+    reader_task_id_ = scheduler->GetTaskId("amqp::RabbitMQReader");
+    monitor_task_id_ = scheduler->GetTaskId("amqp::RabbitMQMonitor");
+    Task *task = new RabbitMQReader(this);
+    scheduler->Enqueue(task);
+}
+
+ConfigAmqpClient::~ConfigAmqpClient() {
+}
+
+void ConfigAmqpClient::EnqueueUUIDRequest(string uuid_str, string obj_type,
+                                     string oper) {
+    mgr_->EnqueueUUIDRequest(uuid_str, obj_type, oper);
+}
+
+string ConfigAmqpClient::FormAmqpUri() const {
+    return string("amqp://" + rabbitmq_user() + ":" + rabbitmq_password() +
+                  "@" + rabbitmq_ip() + ":" +  rabbitmq_port());
+}
+
+bool ConfigAmqpClient::RabbitMQReader::ConnectToRabbitMQ() {
+    string uri = amqpclient_->FormAmqpUri();
+    try {
+        channel_ = AmqpClient::Channel::CreateFromUri(uri);
+        // passive = false, durable = false
+        channel_->DeclareExchange("vnc_config.object-update",
+                                  AmqpClient::Channel::EXCHANGE_TYPE_FANOUT);
+        string queue_name = string("control-node.") + amqpclient_->hostname();
+        string queue = channel_->DeclareQueue(queue_name);
+        channel_->BindQueue(queue, "vnc_config.object-update");
+        consumer_tag_ = channel_->BasicConsume(queue);
+    } catch (std::exception &e) {
+        static std::string what = e.what();
+        std::cout << "Caught fatal exception while connecting to RabbitMQ: " << what << std::endl;
+        return false;
+    } catch (...) {
+        std::cout << "Caught fatal unknown exception while connecting to RabbitMQ: " << std::endl;
+        assert(0);
+    }
+    return true;
+}
+
+bool ConfigAmqpClient::RabbitMQReader::Run() {
+    while (true) {
+        if (ConnectToRabbitMQ()) {
+            break;
+        }
+    }
+    AmqpClient::Envelope::ptr_t envelope;
+    while (envelope = channel_->BasicConsumeMessage(consumer_tag_)) {
+        std::cout << "Rxed Message : " << envelope->Message()->Body() << std::endl;
+        Document document;
+        document.Parse<0>(envelope->Message()->Body().c_str());
+
+        if (document.HasParseError()) {
+            size_t pos = document.GetErrorOffset();
+            // GetParseError returns const char *
+            std::cout << "Error in parsing JSON message from rabbitMQ at "
+                << pos << "with error description"
+                << document.GetParseError() << std::endl;
+        } else {
+            std::cout << "Success " << std::endl;
+            string oper = "";
+            string uuid_str = "";
+            string obj_type = "";
+            for (Value::ConstMemberIterator itr = document.MemberBegin();
+                 itr != document.MemberEnd(); ++itr) {
+                string key(itr->name.GetString());
+                if (key == "oper") {
+                    oper = itr->value.GetString();
+                } else if (key == "type") {
+                    obj_type = itr->value.GetString();
+                } else if (key == "uuid") {
+                    uuid_str = itr->value.GetString();
+                }
+            }
+            if ((oper == "") || (uuid_str == "") || (obj_type == "")) {
+                assert(0);
+            }
+            amqpclient_->EnqueueUUIDRequest(uuid_str, obj_type, oper);
+        }
+    }
+    return true;
+}
+
+int ConfigAmqpClient::reader_task_id() const {
+    return reader_task_id_;
+}
+
+int ConfigAmqpClient::monitor_task_id() const {
+    return monitor_task_id_;
+}
+
+string ConfigAmqpClient::hostname() const {
+    return hostname_;
+}
