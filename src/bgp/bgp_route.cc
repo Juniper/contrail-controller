2acf323656465b81b4c0fa6c8e764df649b713eb
Albert Khoury Aouad <aouad.albert@gmail.com>
2017-01-09 17:43:04 +0000

    Fixing introspect bug When accessing control-node introspect, bgp standard communities does not show up that are configured as part of static routes. It is seen that extended communities associated with static routes override the bgp standard communities in introspect. Modify method FillRoutePathExtCommunityInfo in /opt/stack/contrail/controller/src/bgp/bgp_route.cc to avoid overwriting bgp standard communities. Closes-Bug: 1655088
    Change-Id: I15e6c627c53db9feb7d0025f07a1ec70c4ac9c99

--- src/bgp/bgp_route.cc
+++ src/bgp/bgp_route.cc
@@ -334,7 +334,7 @@ static void FillRoutePathExtCommunityInfo(const BgpTable *table,
     ShowRoutePath *show_path) {
     const RoutingInstance *ri = table->routing_instance();
     const RoutingInstanceMgr *ri_mgr = ri->manager();
-    vector<string> communities = vector<string>();
+    vector<string> &communities = const_cast<vector<string>&>(show_path->get_communities());
     vector<string> tunnel_encap = vector<string>();
 
     const ExtCommunity::ExtCommunityList &v = extcomm->communities();
@@ -389,7 +389,6 @@ static void FillRoutePathExtCommunityInfo(const BgpTable *table,
             communities.push_back(string(temp));
         }
     }
-    show_path->set_communities(communities);
     show_path->set_tunnel_encap(tunnel_encap);
 }
 
