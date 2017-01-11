5912a8bf417658f53d7878cc800b14ade7800f64
jayaramsatya <sajayaram@juniper.net>
2017-01-11 19:33:37 +0530

    Problem: For vcenter only mode, the agent replies arp with the physical intf mac address for the VN's gateway ip. When vm migrates to a different esxi host as part of vmotion,vcenter plugin binds it to the new contrail-compute-vm,whose physical intf mac is different. But the vm still hold the old mac address for the gateway ip in the arp table. Until the old mac address gets flushed and the new mac is learnt for the gateway ip, the traffic is lost from the vm for a different subnet. Solution:  Whenever VM migrates to new esxi host.  and upon creation of VM interface  Arp module will listen on route update. after getting the route update  send the gratious arp for the Gateway IP.  closes-bug:#1650219
    Change-Id: I6bf4bab98c956bead866d22b7c8e8960419315b4

--- src/vnsw/agent/services/arp_handler.h
+++ src/vnsw/agent/services/arp_handler.h
@@ -20,8 +20,8 @@ public:
 
     bool Run();
     void SendArp(uint16_t op, const MacAddress &smac, in_addr_t sip,
-                 const MacAddress &tmac, in_addr_t tip,
-                 uint32_t itf, uint32_t vrf);
+                 const MacAddress &tmac, const MacAddress &dmac,
+                 in_addr_t tip, uint32_t itf, uint32_t vrf);
     friend void intrusive_ptr_add_ref(const ArpHandler *p);
     friend void intrusive_ptr_release(const ArpHandler *p);
 
