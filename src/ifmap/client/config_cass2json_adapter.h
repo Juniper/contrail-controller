28cad4ccc2acc88779dd8dc8660be8baf394f173
Ananth Suryanarayana <anantha@juniper.net>
2017-01-30 11:52:37 -0800

    Modify config_cass2json_adapter to create a json document natively
    o Instead of creating json string and then parse it, directly
      create json documetn using rapidjson apis
    o Compile and run bgp_stress_test (It does not have features to feed json based
      configurations yet)
    o Add bulk_sync.json with a while bunch of configuration and parse it in
      config_json_parser_test
    o Enable many tests in config_json_parser_test (8 still disabled)
    
    Change-Id: Ifce9a65a0e7cc93b76660558ef95c39250d4dc7a
    Partial-Bug: #1632470

--- src/ifmap/client/config_cass2json_adapter.h
+++ src/ifmap/client/config_cass2json_adapter.h
@@ -24,53 +24,28 @@ public:
     static const std::string prop_prefix;
     static const std::string list_prop_prefix;
     static const std::string map_prop_prefix;
-    static const std::string meta_prefix;
-    static const std::string backref_prefix;
-    static const std::string child_prefix;
     static const std::string ref_prefix;
     static const std::string parent_prefix;
     static const std::string parent_type_prefix;
-    static const std::string comma_str;
 
     static const std::set<std::string> allowed_properties;
+
     ConfigCass2JsonAdapter(ConfigCassandraClient *cassandra_client,
                            const std::string &obj_type,
                            const CassColumnKVVec &cdvec);
-    const std::string &doc_string() { return doc_string_; }
+    const rapidjson::Document &document() { return json_document_; }
 
 private:
-    typedef std::vector<std::string> RefDataList;
-    typedef std::map<std::string, RefDataList> RefTypeMap;
-
-    typedef std::map<int, std::string> PropertyListDataMap;
-    typedef std::map<std::string, PropertyListDataMap> ListPropertyMap;
-
-    typedef std::vector<std::string> PropertyMapDataList;
-    typedef std::map<std::string, PropertyMapDataList> MapPropertyMap;
-
     bool CreateJsonString(const std::string &obj_type,
                           const CassColumnKVVec &cdvec);
-    bool AddOneEntry(const std::string &obj_type,
-                     const CassColumnKVVec &cdvec, int i);
+    void AddOneEntry(rapidjson::Value *jsonObject, const std::string &obj_type,
+                     const JsonAdapterDataType &c,
+                     rapidjson::Document::AllocatorType &a);
+    static std::string GetJsonString(const rapidjson::Value &attr_value);
 
-    std::string GetAttrString(const rapidjson::Value &attr_value);
     ConfigCassandraClient *cassandra_client_;
-    std::string doc_string_;
     std::string type_;
-    int prop_plen_;
-    int propl_plen_;
-    int propm_plen_;
-    int meta_plen_;
-    int backref_plen_;
-    int child_plen_;
-    int ref_plen_;
-    int fq_name_plen_;
-    int parent_type_plen_;
-    int parent_plen_;
-
-    RefTypeMap ref_type_map_;
-    MapPropertyMap map_property_map_;
-    ListPropertyMap list_property_map_;
+    rapidjson::Document json_document_;
 };
 
 #endif // ctrlplane_config_cass2json_adapter_h
