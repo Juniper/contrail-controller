2e673af46782b8efee12936f7cde3c0514062fd9
Manish <manishs@juniper.net>
2017-02-06 07:23:11 +0530

    Different RD calculated for smae VRF from active/backup TA.
    Problem:
    To external BGP peer, active and backup TA will always publish same route.
    However as agent computes different vrf-id(as it is locally
    generated), it may result in different RD/instance-id being published to
    CN for same route behind Tor.
    
    Solution:
    In case of TSN/TA, instead of using locally generated vrf id as instance-id,
    use vn-id(network-id) as instance-id. Currently its assumed that in TSN/TA
    there is always one mapping of VN to VRF.
    
    Change-Id: I432ef791afd88736ac3f5295c7d198f66676ee81
    Closes-bug: #1652162

--- src/vnsw/agent/controller/controller_vrf_export.cc
+++ src/vnsw/agent/controller/controller_vrf_export.cc
@@ -29,8 +29,17 @@ void VrfExport::Notify(const Agent *agent, AgentXmppChannel *bgp_xmpp_peer,
 
     BgpPeer *bgp_peer = static_cast<BgpPeer *>(bgp_xmpp_peer->bgp_peer_id());
     VrfEntry *vrf = static_cast<VrfEntry *>(e);
+    uint32_t instance_id = vrf->RDInstanceId();
+    //Instance ID being zero is possible because of VN unavailability and VRF
+    //ending up with NULL VRF. Reason being config sequence.
+    //So seeing 0 instance_id delete the state so that resubscribe can be done
+    //with new id and then re-export all route.
+    //Note: Assumption is that instance id will never change from non zero to
+    //some other non zero value.
+    //Also Instance ID check is for TSN and TA only.
+    bool deleted = (vrf->IsDeleted()) || (instance_id == VrfEntry::kInvalidIndex);
 
-    if (vrf->IsDeleted()) {
+    if (deleted) {
         agent->controller()->
             DeleteVrfStateOfDecommisionedPeers(partition, e);
         if (!AgentXmppChannel::IsXmppChannelActive(agent, bgp_xmpp_peer)) {
