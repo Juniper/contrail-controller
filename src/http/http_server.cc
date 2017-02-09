821bff88d192ee1e5c43f33e63bb9c859f463091
Nikhil B <nikhilb@juniper.net>
2017-02-09 14:07:24 +0530

    Adding ssl option in http server
    Introspect ports need to be ssl enabled. This requires http servers to be able
    to take ssl config. This checkin enables that. Any application can pass
    corresponding ssl config to enable it.
    
    Change-Id: Ib6aa859d54daac414bbea82d895d0d32fab757c0
    Partial-Bug: #1653079

--- src/http/http_server.cc
+++ src/http/http_server.cc
@@ -9,8 +9,54 @@
 
 using namespace std;
 
-HttpServer::HttpServer(EventManager *evm) : TcpServer(evm) {
+HttpServer::HttpServer(EventManager *evm, const SslConfig &config)
+    : SslServer(evm, boost::asio::ssl::context::tlsv1_server, config.ssl_enabled, false) {
     //ctor
+    if (config.ssl_enabled) {
+
+        // Get SSL context from base class and update
+        boost::asio::ssl::context *ctx = context();
+        boost::system::error_code ec;
+
+        // set mode
+        ctx->set_options(boost::asio::ssl::context::default_workarounds |
+                         boost::asio::ssl::context::no_sslv3 |
+			 boost::asio::ssl::context::no_sslv2, ec);
+        if (ec.value() != 0) {
+            exit(EINVAL);
+        }
+
+        // CA certificate, used to verify if the peer certificate
+        // is signed by a trusted CA
+        std::string ca_cert_filename = config.ca_cert;
+        if (!ca_cert_filename.empty()) {
+
+            // Verify peer has CA signed certificate
+            ctx->set_verify_mode(boost::asio::ssl::verify_peer, ec);
+            if (ec.value() != 0) {
+                exit(EINVAL);
+            }
+
+            ctx->load_verify_file(ca_cert_filename, ec);
+            if (ec.value() != 0) {
+                exit(EINVAL);
+            }
+        }
+
+        // server certificate
+        ctx->use_certificate_file(config.certfile,
+                                  boost::asio::ssl::context::pem, ec);
+        if (ec.value() != 0) {
+            exit(EINVAL);
+        }
+
+        // server private key
+        ctx->use_private_key_file(config.keyfile,
+                                  boost::asio::ssl::context::pem, ec);
+        if (ec.value() != 0) {
+            exit(EINVAL);
+        }
+    }
 }
 
 HttpServer::~HttpServer() {
@@ -22,19 +68,22 @@ void HttpServer::Shutdown() {
     TcpServer::Shutdown();
 }
 
-TcpSession *HttpServer::AllocSession(Socket *socket) {
-    HttpSession *session = new HttpSession(this, socket);
-    boost::system::error_code ec = session->SetSocketOptions();
-    if (ec) {
-        delete session;
-        return NULL;
-    }
+SslSession *HttpServer::AllocSession(SslSocket *socket) {
+    SslSession *session = new HttpSession(this, socket);
+    boost::system::error_code err;
+    HttpSession *https = static_cast<HttpSession *>(session);
+    https->SetSocketOptions();
     return session;
 }
 
 bool HttpServer::AcceptSession(TcpSession *session) {
     HttpSession *h_session = dynamic_cast<HttpSession *>(session);
+    h_session->AcceptSession();
+    return true;
+}
 
+bool HttpServer::AcceptSession(SslSession *session) {
+    HttpSession *h_session = dynamic_cast<HttpSession *>(session);
     h_session->AcceptSession();
     return true;
 }
