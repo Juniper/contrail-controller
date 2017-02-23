4b4f43f0858ae6fe904edfe7ce38a0b67fcd899d
Sundaresan Rajangam <srajanga@juniper.net>
2017-02-23 15:19:53 -0800

    Convert sandesh_ssl_enable and introspect_ssl_enable options to bool
    Presently, sandesh_ssl_enable and introspect_ssl_enable config options
    are read as string and hence always evaluates to True. These options
    should be read as boolean instead.
    
    Change-Id: Ibe6e7f98f669789ce8df53bbef32ab165fa9ca3b
    Partial-Bug: #1654413

--- src/analytics/contrail-broadview/contrail_broadview/config.py
+++ src/analytics/contrail-broadview/contrail_broadview/config.py
@@ -92,6 +92,12 @@ optional arguments:
                 defaults.update(dict(config.items("DEFAULTS")))
             if 'SANDESH' in config.sections():
                 sandesh_opts.update(dict(config.items('SANDESH')))
+                if 'sandesh_ssl_enable' in config.options('SANDESH'):
+                    sandesh_opts['sandesh_ssl_enable'] = config.getboolean(
+                        'SANDESH', 'sandesh_ssl_enable')
+                if 'introspect_ssl_enable' in config.options('SANDESH'):
+                    sandesh_opts['introspect_ssl_enable'] = config.getboolean(
+                        'SANDESH', 'introspect_ssl_enable')
         # Override with CLI options
         # Don't surpress add_help here so it will handle -h
         parser = argparse.ArgumentParser(
