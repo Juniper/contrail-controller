adc24f2fbd0e41364ed9ac8a5631bc5cec017042
ashoksingh <ashoksr@juniper.net>
2017-03-01 14:12:01 +0530

    Fix issue of VM not getting deleted from Agent
    Nova repo is absent in R3.0.2.x/centos71/manifest-juno.xml. This repo has changes to invoke/use Agent’s REST API for port Adds/deletes.
    Until we have this repo in all manifest files of R3.0.2.x branches continue to use Thrift instead of REST.
    
    This reverts the commit https://github.com/Juniper/contrail-controller/commit/bd890daadb06bb8341bfdf217ab2c0c53988311d
    
    Change-Id: Id30bc9d3a5eeab7b2f459a9073e3d56fe0fe9168
    Closes-Bug: #1665309

--- src/vnsw/contrail-vrouter-api/contrail_vrouter_api/vrouter_api.py
+++ src/vnsw/contrail-vrouter-api/contrail_vrouter_api/vrouter_api.py
@@ -1,96 +1,142 @@
-# Copyright (c) 2016 Juniper Networks, Inc
+# Copyright (c) 2014 Juniper Networks, Inc
 
-import subprocess
+import logging
+import uuid
+from thrift.protocol import TBinaryProtocol
+from thrift.transport import TTransport
+from gen_py.instance_service import InstanceService, ttypes
+
 
 class ContrailVRouterApi(object):
 
     def __init__(self, server_port=9090, doconnect=False, semaphore=None):
         """
-        Arguments server_port and doconnect are not used. Retained only
-        for backward compatibility.
-
         local variables:
-        _ports: dictionary of ports keyed by vif uuid.
+        _client: current transport connection
+        _ports: dictionary of active ports keyed by vif uuid.
         """
+        self._server_port = server_port
+        self._client = None
         self._ports = {}
+        self._connect = doconnect
         self._semaphore = semaphore
 
+    def _rpc_client_instance(self):
+        """ Return an RPC client connection """
+        import thrift.transport.TSocket as TSocket
+        socket = TSocket.TSocket('localhost', self._server_port)
+        try:
+            transport = TTransport.TFramedTransport(socket)
+            transport.open()
+        except TTransport.TTransportException:
+            logging.error('Unable to connect to contrail-vrouter-agent '
+                          'localhost:%d' % self._server_port)
+            return None
+        protocol = TBinaryProtocol.TBinaryProtocol(transport)
+        client = InstanceService.Client(protocol)
+        if client and self._connect:
+            client.Connect()
+        return client
+
     def _resynchronize(self):
-        """ Add all ports which we failed to add earlier """
-        if (len(self._ports) == 0):
-            return
-        for key in self._ports.keys():
-            ret_code = subprocess.call(self._ports[key])
-            """ If port is added successfully, remove it from our list """
-            if (ret_code == 0):
-                self._ports.pop(key, None)
+        """ Add all the active ports to the agent """
+        self._client.AddPort([port for port in self._ports.itervalues()])
+
+    def _uuid_from_string(self, idstr):
+        """ Convert an uuid string into an uuid object """
+        if not idstr:
+            return None
+        return uuid.UUID(idstr)
+
+    def _uuid_to_hex(self, id):
+        """ Convert an uuid into an array of integers """
+        hexstr = id.hex
+        return [int(hexstr[i:i + 2], 16) for i in range(32) if i % 2 == 0]
+
+    def _uuid_string_to_hex(self, idstr):
+        return self._uuid_to_hex(self._uuid_from_string(idstr))
+
+    def connect(self):
+        if self._client is None:
+            self._client = self._rpc_client_instance()
+            if self._client is None:
+                return False
+            if self._connect:
+                return True
+
+        return self._client.Connect()
 
     def add_port(self, vm_uuid_str, vif_uuid_str, interface_name, mac_address,
                  **kwargs):
         """
-        Add a port to the agent. Ports which we failed to add are stored in
-        _ports dictionary since the vrouter agent may not be running at the
-        moment or the REST API call may fail.
+        Add a port to the agent. The information is stored in the _ports
+        dictionary since the vrouter agent may not be running at the
+        moment or the RPC may fail.
         """
         try:
             if self._semaphore:
                 self._semaphore.acquire()
-
+            vif_uuid = self._uuid_from_string(vif_uuid_str)
+            # ip_address and network_uuid are optional to this API but must
+            # be present in the message. For instance, when running in
+            # CloudStack/XenServer/XAPI these arguments are not known.
             if 'ip_address' in kwargs:
                 ip_address = kwargs['ip_address']
             else:
                 ip_address = '0.0.0.0'
 
-            network_uuid = ''
             if 'vn_id' in kwargs:
-                network_uuid = kwargs['vn_id']
-            
-            display_name=''
-            if 'display_name' in kwargs:
-                display_name = kwargs['display_name']
-            vm_project_id=''
-
-            if 'vm_project_id' in kwargs:
-                vm_project_id = kwargs['vm_project_id']
-            if ('port_type' in kwargs and
-                kwargs['port_type'] in [0,1]):
-                if (kwargs['port_type'] == 0):
-                    port_type = "NovaVMPort"
-                elif (kwargs['port_type'] == 1):
-                    port_type = "NameSpacePort"
-                elif (kwargs['port_type'] == 'NameSpacePort'):
-                    port_type = "NameSpacePort"
-                else:
-                    port_type = "NovaVMPort"
+                network_uuid = self._uuid_string_to_hex(kwargs['vn_id'])
             else:
-                port_type = "NameSpacePort"
+                network_uuid = [0] * 16
 
-            ip6_address=''
+            # create port with mandatory arguments
+            data = ttypes.Port(
+                self._uuid_to_hex(vif_uuid),
+                self._uuid_string_to_hex(vm_uuid_str),
+                interface_name,
+                ip_address,
+                network_uuid,
+                mac_address)
+
+            if 'display_name' in kwargs:
+                data.display_name = kwargs['display_name']
+            if 'hostname' in kwargs:
+                data.hostname = kwargs['hostname']
+            if 'vm_project_id' in kwargs:
+                data.vm_project_id = self._uuid_string_to_hex(
+                    kwargs['vm_project_id'])
+            if ('port_type' in kwargs and
+                kwargs['port_type'] in ttypes.PortTypes._NAMES_TO_VALUES.keys()):
+                data.port_type = \
+                    ttypes.PortTypes._NAMES_TO_VALUES[kwargs['port_type']]
+            else:
+                data.port_type = ttypes.PortTypes.NameSpacePort
             if 'ip6_address' in kwargs:
-                ip6_address = kwargs['ip6_address']
+                data.ip6_address = kwargs['ip6_address']
 
-            tx_vlan_id = -1
             if 'vlan' in kwargs:
-                tx_vlan_id = kwargs['vlan']
+                data.vlan_id = kwargs['vlan']
 
-            cmd_args = ("vrouter-port-control --oper=\"add\" --uuid=\"%s\" "
-                        "--instance_uuid=\"%s\" --vn_uuid=\"%s\" "
-                        "--vm_project_uuid=\"%s\" --ip_address=\"%s\" "
-                        "--ipv6_address=\"%s\" --vm_name=\"%s\" --mac=\"%s\" "
-                        "--tap_name=\"%s\" --port_type=\"%s\" "
-                        "--vif_type=\"Vrouter\" --tx_vlan_id=\"%d\" "
-                        "--rx_vlan_id=\"%d\"" %(vif_uuid_str, vm_uuid_str,
-                        network_uuid, vm_project_id, ip_address, ip6_address,
-                        display_name, mac_address, interface_name, port_type,
-                        tx_vlan_id, -1))
+            data.validate()
 
-            cmd = cmd_args.split()
-            ret_code = subprocess.call(cmd)
-            if (ret_code != 0):
-                self._ports[vif_uuid_str] = cmd
+            if self._client is None:
+                self._client = self._rpc_client_instance()
+
+            if self._client is None:
+                self._ports[vif_uuid] = data
+                return False
 
             self._resynchronize()
 
+            self._ports[vif_uuid] = data
+
+            try:
+                result = self._client.AddPort([data])
+            except:
+                self._client = None
+                raise
+            return result
         finally:
             if self._semaphore:
                 self._semaphore.release()
@@ -103,31 +149,44 @@ class ContrailVRouterApi(object):
         try:
             if self._semaphore:
                 self._semaphore.acquire()
-            if (vif_uuid_str in self._ports.keys()):
-                self._ports.pop(vif_uuid_str, None)
-                return
+            vif_uuid = self._uuid_from_string(vif_uuid_str)
+            self._ports.pop(vif_uuid, None)
 
-            cmd_args = ("vrouter-port-control --oper=delete --uuid=%s" % vif_uuid_str)
-            cmd = cmd_args.split()
-            ret_code = subprocess.call(cmd)
+            if self._client is None:
+                self._client = self._rpc_client_instance()
+                if self._client is None:
+                    return
+                self._resynchronize()
 
-            self._resynchronize()
-
+            try:
+                self._client.DeletePort(self._uuid_to_hex(vif_uuid))
+            except:
+                self._client = None
         finally:
             if self._semaphore:
                 self._semaphore.release()
 
     def periodic_connection_check(self):
         """
-        Periodicly add ports to agent which we failed to add earlier.
+        Periodicly check if the connection to the agent is valid.
         It is the API client's resposibility to periodically invoke this
         method.
         """
         try:
             if self._semaphore:
                 self._semaphore.acquire()
-            self._resynchronize()
+            if self._client is None:
+                self._client = self._rpc_client_instance()
+                if self._client is None:
+                    return
+                self._resynchronize()
 
+            try:
+                if self._client:
+                    self._client.KeepAliveCheck()
+            except Exception as ex:
+                self._client = None
+                logging.exception(ex)
         finally:
             if self._semaphore:
                 self._semaphore.release()
