b819ca75a56336c681d02051646f44c4f390fc99
sbalineni <sbalineni@juniper.net>
2017-03-07 17:18:07 -0800

    [DM]: iBgp export policy configuration
    Generate JUNOS config for iBGP export policy
    This review depends on https://review.opencontrail.org/#/c/29418/
    
    Change-Id: I0f397a2dcb7ddc4efc103a86eb459ab9eac76a27
    Closes-Bug: #1666199

--- src/config/device-manager/test/test_dm_bgp.py
+++ src/config/device-manager/test/test_dm_bgp.py
@@ -8,6 +8,7 @@ sys.path.append("../common/tests")
 from test_utils import *
 from vnc_api.vnc_api import *
 from device_api.juniper_common_xsd import *
+from device_manager.dm_utils import DMUtils
 from test_common import *
 from test_dm_common import *
 
@@ -29,11 +30,30 @@ class TestBgpDM(TestCommonDM):
 
     # test hold time configuration
     def test_dm_bgp_hold_time_config(self):
-        bgp_router, pr = self.create_router('router1', '1.1.1.1')
+        bgp_router, pr = self.create_router('router' + self.id() , '1.1.1.1')
         self.set_hold_time(bgp_router, 100)
         self._vnc_lib.bgp_router_update(bgp_router)
         gevent.sleep(2)
         self.check_dm_bgp_hold_time_config('internal', 100)
+
+    @retries(5, hook=retry_exc_handler)
+    def check_dm_bgp_export_policy(self):
+        config = FakeDeviceConnect.get_xml_config()
+        bgp_groups = self.get_bgp_groups(config)
+        for gp in bgp_groups or []:
+            if gp.get_type() == 'internal':
+                self.assertEqual(gp.get_export(), DMUtils.make_ibgp_export_policy_name())
+                return
+            if gp.get_type() == 'external':
+                self.assertThat(gp.get_export() != DMUtils.make_ibgp_export_policy_name())
+                return
+        self.assertTrue(False)
+        return
+
+    # test iBgp export policy configuration
+    def test_dm_bgp_export_policy(self):
+        bgp_router, pr = self.create_router('router' + self.id() , '1.1.1.1')
+        self.check_dm_bgp_export_policy()
 
     # Test Auth Confiuration
     @retries(5, hook=retry_exc_handler)
@@ -62,14 +82,14 @@ class TestBgpDM(TestCommonDM):
 
     # test bgp auth configuration
     def test_dm_md5_auth_config(self):
-        bgp_router, _ = self.create_router('router1', '1.1.1.1')
+        bgp_router, _ = self.create_router('router1' + self.id(), '1.1.1.1')
         self.set_auth_data(bgp_router, 0, 'bgppswd', 'md5')
         self._vnc_lib.bgp_router_update(bgp_router)
         gevent.sleep(2)
         self.check_bgp_auth_config('internal', 'bgppswd')
 
         #bgp peering, auth validate
-        bgp_router_peer, _ = self.create_router('router2', '20.2.2.2')
+        bgp_router_peer, _ = self.create_router('router2' + self.id() , '20.2.2.2')
         families = AddressFamilies(['route-target', 'inet-vpn', 'e-vpn'])
         auth = AuthenticationData('md5', [AuthenticationKeyItem(0, 'bgppswd-neigh')])
         bgp_sess_attrs = [BgpSessionAttributes(address_families=families, auth_data=auth)]
