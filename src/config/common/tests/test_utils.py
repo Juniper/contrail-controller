4b9e79ce02dae510a5a3cfaea17cbb7e203b5de4
sbalineni <sbalineni@juniper.net>
2017-03-08 16:27:53 -0800

    [DM]: implementation of DM introspect/trace buffers, enabled CI tests, Fixed ST UT issues, Enhanced UT infra
    - Basic infra and added introspect support for PR, BGP router objects
    - Enabled Unit Tests in CI
    - Fixed unit test issues
    - Fixed ST UT issues (infra related issues + some test cases)
    - Graceful cleanup of Rabbit Queues
    - Added accessor methods for global instances of DM & ST
    
    Change-Id: I5283077989769b5818369a73fe3b70e2e353ca01
    Partial-Bug: #1671283

--- src/config/common/tests/test_utils.py
+++ src/config/common/tests/test_utils.py
@@ -574,10 +574,15 @@ class FakeKombu(object):
 
         def __call__(self, *args):
             class BoundQueue(object):
+
+                def __init__(self, parent):
+                    self.parent_q = parent
+
                 def delete(self):
+                    self.parent_q.clear()
                     pass
                 # end delete
-            return BoundQueue()
+            return BoundQueue(self)
         # end __call__
 
         def put(self, msg_dict, serializer):
@@ -592,6 +597,13 @@ class FakeKombu(object):
             return rv
         # end get
 
+        def clear(self):
+            try:
+                while True:
+                    self._sync_q.get_nowait()
+            except Queue.Empty:
+                pass
+            self._sync_q = gevent.queue.Queue()
     # end class Queue
 
     class FakeChannel(object):
@@ -652,6 +664,8 @@ class FakeKombu(object):
         # end __init__
 
         def consume(self):
+            if not self.queues:
+                return
             while True:
                 msg = self.queues.get()
                 try:
@@ -662,7 +676,9 @@ class FakeKombu(object):
         # end consume
 
         def close(self):
-            pass
+            if self.queues:
+                self.queues.clear()
+                self.queues = None
         # end close
 
     # end class Consumer
@@ -680,15 +696,22 @@ class FakeKombu(object):
         #end publish
 
         def close(self):
-            pass
+            for q in FakeKombu._exchange[self.vhost].values():
+                while True:
+                    try:
+                        q.get_nowait()
+                    except Queue.Empty:
+                        break
         # end close
 
     # end class Producer
     @classmethod
     def reset(cls, vhost):
         _vhost = ''.join(vhost)
+        for name, gevent_q in cls._exchange[_vhost].items():
+            del FakeKombu._exchange[_vhost][name]
         cls._exchange[_vhost].clear()
-        pass
+        cls._exchange = defaultdict(dict)
 # end class FakeKombu
 
 class FakeRedis(object):
@@ -1323,27 +1346,32 @@ def fake_netconf_connect(host, *args, **kwargs):
 class FakeDeviceConnect(object):
     parms = {}
 
-    @staticmethod
-    def get_xml_data(self, config):
+    @classmethod
+    def get_xml_data(cls, config):
         xml_data = StringIO()
-        config.export(xml_data, 1)
+        config.export_xml(xml_data, 1)
         return xml_data.getvalue()
     # end get_xml_data
 
-    @staticmethod
-    def send_netconf(obj, new_config, default_operation="merge", operation="replace"):
-        FakeDeviceConnect.params = {
+    @classmethod
+    def send_netconf(cls, obj, new_config, default_operation="merge", operation="replace"):
+        cls.params = {
                                      "pr_config": obj,
                                      "config": new_config,
                                      "default_operation": default_operation,
                                      "operation": operation
                                    }
-        return len(FakeDeviceConnect.get_xml_data(new_config))
+        return len(cls.get_xml_data(new_config))
     # end send_netconf
 
-    @staticmethod
-    def get_xml_config():
-        return FakeDeviceConnect.params.get('config')
+    @classmethod
+    def get_xml_config(cls):
+        return cls.params.get('config')
+    # end get_xml_config
+
+    @classmethod
+    def reset(cls):
+        cls.params = {}
     # end get_xml_config
 # end
 
