0fd2b9ce7d361a4a23ca52e5f503a5a1ae09f702
ashoksingh <ashoksr@juniper.net>
2017-03-10 13:31:55 +0530

    Ensure Source VN and Destination VN are always sent during FlowExport
    When FlowEntry gets reused, use previously sent Source VN and destination VN
    while exporting flows.
    
    Closes-Bug: #1665821
    (cherry picked from commit fd472cee4e26dca686954427b272b24e253cbfb5)
    
    Change-Id: Ibfda852c3100aaf345944eb1eef67d290f768486

--- src/vnsw/agent/vrouter/flow_stats/test/flow_stats_collector_test.cc
+++ src/vnsw/agent/vrouter/flow_stats/test/flow_stats_collector_test.cc
@@ -13,7 +13,7 @@ FlowStatsCollectorTest::FlowStatsCollectorTest(boost::asio::io_service &io,
                                                FlowStatsManager *aging_module)
     : FlowStatsCollector(io, intvl, flow_cache_timeout, uve,
                          instance_id, key, aging_module),
-      ingress_flow_log_list_() {
+      ingress_flow_log_list_(), dispatch_count_(0) {
 }
 
 FlowStatsCollectorTest::~FlowStatsCollectorTest() {
@@ -24,15 +24,29 @@ void FlowStatsCollectorTest::DispatchFlowMsg
     std::vector<FlowLogData>::const_iterator it = msg_list.begin();
     while (it != msg_list.end()) {
         flow_log_ = *it;
+        flow_log_valid_ = true;
+        if (flow_log_.get_teardown_time()) {
+            flow_del_log_ = flow_log_;
+            flow_del_log_valid_ = true;
+        }
         if (flow_log_.get_direction_ing()) {
             ingress_flow_log_list_.push_back(flow_log_);
         }
+        dispatch_count_++;
         it++;
     }
 }
 
+void FlowStatsCollectorTest::ClearCount() {
+    dispatch_count_ = 0;
+}
+
 FlowLogData FlowStatsCollectorTest::last_sent_flow_log() const {
     return flow_log_;
+}
+
+FlowLogData FlowStatsCollectorTest::last_sent_flow_del_log() const {
+    return flow_del_log_;
 }
 
 void FlowStatsCollectorTest::ClearList() {
@@ -41,3 +55,7 @@ void FlowStatsCollectorTest::ClearList() {
     }
 }
 
+void FlowStatsCollectorTest::ResetLastSentLog() {
+    flow_log_valid_ = false;
+    flow_del_log_valid_ = false;
+}
