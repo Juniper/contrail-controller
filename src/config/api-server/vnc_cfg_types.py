cc9491b6f964f8e1ac3d25ad9c880ce72bd86f98
Deepinder Setia <dsetia@juniper.net>
2017-04-10 12:47:52 -0700

    Schema for New firewall rules
    Partial-bug: #1687773
    Change-Id: I4eb1b099a7eee2f356b8d9f859108d82dee09d10

--- src/config/api-server/vnc_cfg_types.py
+++ src/config/api-server/vnc_cfg_types.py
@@ -1137,6 +1137,38 @@ class BridgeDomainServer(Resource, BridgeDomain):
     # end pre_dbe_create
 # end class BridgeDomainServer
 
+class TagServer(Resource, Tag):
+
+    @classmethod
+    def pre_dbe_create(cls, tenant_name, obj_dict, db_conn):
+
+        tag_type = obj_dict.get('tag_type')
+        tag_value = obj_dict.get('tag_value')
+
+        if tag_type is None or tag_value is None:
+            msg = "Tag must be created with type and value"
+            return (False, (400, msg))
+
+        if obj_dict.get('tag_id'):
+            msg = "Tag id is not setable"
+            return (False, (400, msg))
+
+        return True, ""
+    # end pre_dbe_create
+
+    @classmethod
+    def pre_dbe_update(cls, id, fq_name, obj_dict, db_conn, **kwargs):
+        ok, read_result = cls.dbe_read(db_conn, 'tag', id)
+        if not ok:
+            return ok, read_result
+
+        if obj_dict.get('tag_type') or obj_dict.get('tag_value'):
+            msg = "Tag type or id cannot be updated"
+            return (False, (400, msg))
+
+        return True, ""
+# end class TagServer
+
 class VirtualNetworkServer(Resource, VirtualNetwork):
 
     @classmethod
