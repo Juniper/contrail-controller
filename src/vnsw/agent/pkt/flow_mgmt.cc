dd0edaa36ce1044dec3e6450b9cd98d5ab7cd30e
Praveen K V <kvpraveen@juniper.net>
2017-04-10 10:55:09 +0530

    Do flow recompute on bridge-route add/change
    Add/Change of bridge route can in change to following,
        - NH used by the flow
        - Change in VN/SG-ID for the route
    
    Change in NH can in-turn result in change to RPF-NH. So, set event for
    add/change of bridge route as RECOMPUTE_FLOW.
    
    Change-Id: If3619f38aed10a19e9852fed0e8c226978d630ef
    Closes-Bug: #1680178
    (cherry picked from commit 126032979f4de5d17e1fcb512de047b0c6b810bc)

--- src/vnsw/agent/pkt/flow_mgmt.cc
+++ src/vnsw/agent/pkt/flow_mgmt.cc
@@ -1655,7 +1655,7 @@ void BridgeRouteFlowMgmtTree::ExtractKeys(FlowEntry *flow,
 }
 
 FlowMgmtEntry *BridgeRouteFlowMgmtTree::Allocate(const FlowMgmtKey *key) {
-    return new BgpAsAServiceFlowMgmtEntry();
+    return new BridgeRouteFlowMgmtEntry();
 }
 
 bool BridgeRouteFlowMgmtTree::HasVrfFlows(uint32_t vrf,
