90a6ff9112c36f2dc94ed54af74baac881ba63f7
sbalineni <sbalineni@juniper.net>
2017-04-13 14:50:41 -0700

    [DM]: Plug and Play Architecture: Device Manager
    First cut of Plugin model implementation
    
    DM Current Implementation:
    
        Modules:
           - Device Manager Process
           - Local in-memory Data Base Objects
           - VNC DB to Device Specific XML Generation
           - Device NetConf Mgmt
    
        Issues:
           - XML Generation & Netconf Mgmt tightly coupled with other modules in the process, and hence it only manages MX devices currently.
           - Problem: its tough add/generate/manage configurations for other devices/other than netconf protocol.
    
    New Design:
    
        - De-couple XML Generation/NetConf integration from DM process.
    
        - Plug-and-play architecture
          Any external device mgmt module can be plugged into DM during load time/compile time.
    
        - DM provides generic abstract interface methods, every external plugin has to implement them.
          Example interfaces:
            - Register Plugin (When DM starts up), with vendor/product name; Based on product name, DM will use appropriate plug-in for push.
            - device_connect(), device_disconnect(), is_connected(), device_send(), device_get(), push_conf()
            - DM loads all plugins at start-up time, plugins self register with DM. They supply their product/vendor names.
            - DM implements state machine for connection mgmt, ultimate connections are managed by plugins..
    
        - Plugin is designed as follows:
    
                DeviceConf  (Abstract Base Class with interfaces to be used by DM)
                              |
                    ——————
                     |
             JuniperConf (netconf mgmt goes here)
                   |
            ———————
             |                      |
          MxConf           QfxConf
    
        - With this approach, we could support easily external device mgmt handlers/openconfig easily without modifying Core DM implementation.
    
    Change-Id: I2672d38f6bd9bba529c529e1581c6453a2a66311
    Partial-Bug: #1677895
    Partial-Bug: #1679841

--- src/config/device-manager/device_manager/device_manager.py
+++ src/config/device-manager/device_manager/device_manager.py
@@ -20,6 +20,7 @@ import time
 import hashlib
 import signal
 import random
+import traceback
 from pprint import pformat
 
 from pysandesh.sandesh_base import *
@@ -43,7 +44,8 @@ from db import DBBaseDM, BgpRouterDM, PhysicalRouterDM, PhysicalInterfaceDM,\
     VirtualNetworkDM, RoutingInstanceDM, GlobalSystemConfigDM, \
     GlobalVRouterConfigDM, FloatingIpDM, InstanceIpDM, DMCassandraDB, PortTupleDM
 from dm_amqp import DMAmqpHandle
-from physical_router_config import PushConfigState
+from dm_utils import PushConfigState
+from device_conf import DeviceConf
 from cfgm_common.dependency_tracker import DependencyTracker
 from cfgm_common import vnc_cgitb
 from cfgm_common.utils import cgitb_hook
@@ -159,7 +161,13 @@ class DeviceManager(object):
 
         # Initialize logger
         self.logger = dm_logger or DeviceManagerLogger(args)
-
+        # Register Plugins
+        try:
+            DeviceConf.register_plugins()
+        except Exception as e:
+            # log exception and continue
+            tb = traceback.format_exc()
+            self.logger.error("Exception while registering plugins: " + str(e) + tb)
         # Retry till API server is up
         connected = False
         self.connection_state_update(ConnectionStatus.INIT)
