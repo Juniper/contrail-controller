0138d84cc87f8a4c555bf9256b860ad106e9d70e
Praveen K V <kvpraveen@juniper.net>
2017-04-20 19:53:48 +0530

    Replace tbb::task::spawn() by tbb::task::enqueue() and also support for task monitoring
    Replace tbb::task::spawn() by tbb::task::enqueue(). enqueue() is more
    appropriate call since task library just fires the task without calling
    spawn_root_and_wait()
    
    Also, introduced a TaskMonitor class which monitors number of tasks
    enqueued to scheduler and number of tasks spawned by TBB. If the monitor
    finds tasks are enqueued but not scheduled for a configured time, it
    will exit the program.
    
    Change-Id: Ia68d52f4aba0e79a3fe75f8c4fc248d49ab94faf
    Fixes-Bug: #1684993

--- src/base/task_sandesh.cc
+++ src/base/task_sandesh.cc
@@ -3,9 +3,11 @@
  */
 
 #include <base/task.h>
+#include <base/task_monitor.h>
 #include <sandesh/sandesh.h>
 #include <sandesh/sandesh_types.h>
 #include <base/sandesh/task_types.h>
+#include <base/task_tbbkeepawake.h>
 
 using std::string;
 
@@ -24,4 +26,29 @@ void SandeshTaskRequest::HandleRequest() const {
 
 void SandeshTaskSummaryRequest::HandleRequest() const {
     HandleRequestCommon(context(), true);
+}
+
+void SandeshTaskMonitorRequest::HandleRequest() const {
+    TaskScheduler *scheduler = TaskScheduler::GetInstance();
+
+    SandeshMonitorResponse *resp = new SandeshMonitorResponse;
+    resp->set_context(context());
+    resp->set_more(false);
+
+    const TaskMonitor *monitor = scheduler->task_monitor();
+    if (monitor && monitor->inactivity_time_usec() &&
+        monitor->poll_interval_msec()) {
+        resp->set_running(true);
+        resp->set_inactivity_time_msec(monitor->inactivity_time_msec());
+        resp->set_poll_interval_msec(monitor->poll_interval_msec());
+        resp->set_poll_count(monitor->poll_count());
+        resp->set_last_activity(monitor->last_activity());
+        resp->set_last_enqueue_count(monitor->last_enqueue_count());
+        resp->set_last_done_count(monitor->last_done_count());
+        resp->set_tbb_keepawake_time(monitor->tbb_keepawake_time_msec());
+    } else {
+        resp->set_running(false);
+    }
+
+    resp->Response();
 }
