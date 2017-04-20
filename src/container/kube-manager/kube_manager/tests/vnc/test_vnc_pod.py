ee1b30177a3df144ee6dd69e8481bb0fb08a28e4
Sachchidanand Vaidya <vaidyasd@juniper.net>
2017-04-20 16:19:43 -0700

    Few changes to system-less test infra     - Run svc-monitor & schema-transformer as part of ClassSetup       instead of setup/teardown per test.     - Keep kube-manager setup/teardown per test. It can also be       changed to per ClassSetup if required.     - Enabled additional tests and class to verify the infra.
    Change-Id: I13cde9345d7b9d88702d561c882a2798f915b29c
    Partial-Bug: #1639062

--- src/container/kube-manager/kube_manager/tests/vnc/test_vnc_pod.py
+++ src/container/kube-manager/kube_manager/tests/vnc/test_vnc_pod.py
@@ -6,15 +6,25 @@ import mock
 import unittest
 from mock import patch
 
+from gevent.queue import Queue
+from test_case import KMTestCase
+from kube_manager.vnc.config_db import *
+from kube_manager.vnc.loadbalancer import *
+from kube_manager.vnc.config_db import *
+from kube_manager.kube_manager import *
+from kube_manager.vnc import db
 from kube_manager import kube_manager
+from kube_manager.vnc import vnc_namespace
 from kube_manager.vnc import vnc_pod
 
-class VncPodTest(unittest.TestCase):
+class VncPodTest(KMTestCase):
     def setUp(self):
-        pass
+        super(VncPodTest, self).setUp()
+    #end setUp
 
     def tearDown(self):
-        pass
+        super(VncPodTest, self).tearDown()
+    #end tearDown
 
     def test_vnc_pod_add(self):
         pass
