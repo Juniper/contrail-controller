c1f3ccaa2d35453b30135718bb306fa0534ac4ae
sbalineni <sbalineni@juniper.net>
2017-04-27 13:29:45 -0700

    [DM]: Configure BGP on QFX devices using netconf
    - Push BGP configuration to QFX devices
    - Following BGP config push is implemented (exactly similar to what we push to MX)
       - BGP Groups , internal & external
       - bgp hold time/auth config
       - lo0 interfaces config
       - router-id config
       - Dynamic Tunnels
    - Rerefactored code to re-use the existing MX implementaion
    - Added UTs for QFX devices
    
    Change-Id: Ia6c57ed78be7aa875e2889aafdd40994c72f365b
    Closes-Bug: #1686829

--- src/config/device-manager/device_manager/plugins/juniper/qfx/qfx_conf.py
+++ src/config/device-manager/device_manager/plugins/juniper/qfx/qfx_conf.py
@@ -31,12 +31,41 @@ class QfxConf(JuniperConf):
         return super(QfxConf, cls).register(qconf)
     # end register
 
-    def has_conf(self):
-        return True
-    # end has_conf
-
     def push_conf(self, is_delete=False):
-        return self.send_conf(is_delete)
+        if not self.physical_router:
+            return 0
+        if is_delete:
+            return self.send_conf(is_delete=True)
+        bgp_router = BgpRouterDM.get(self.physical_router.bgp_router)
+        if bgp_router:
+            for peer_uuid, attr in bgp_router.bgp_routers.items():
+                peer = BgpRouterDM.get(peer_uuid)
+                if peer is None:
+                    continue
+                local_as = (bgp_router.params.get('local_autonomous_system') or
+                               bgp_router.params.get('autonomous_system'))
+                peer_as = (peer.params.get('local_autonomous_system') or
+                               peer.params.get('autonomous_system'))
+                external = (local_as != peer_as)
+                self.add_bgp_peer(peer.params['address'],
+                                                 peer.params, attr, external, peer)
+            self.set_bgp_config(bgp_router.params, bgp_router)
+            self.set_global_routing_options(bgp_router.params)
+            bgp_router_ips = bgp_router.get_all_bgp_router_ips()
+            tunnel_ip = self.physical_router.dataplane_ip
+            if not tunnel_ip and bgp_router.params:
+                tunnel_ip = bgp_router.params.get('address')
+            if (tunnel_ip and self.physical_router.is_valid_ip(tunnel_ip)):
+                self.add_dynamic_tunnels(
+                    tunnel_ip,
+                    GlobalSystemConfigDM.ip_fabric_subnets,
+                    bgp_router_ips)
+
+        if self.physical_router.loopback_ip:
+            self.add_lo0_unit_0_interface(self.physical_router.loopback_ip)
+        self.set_as_config()
+        self.set_bgp_group_config()
+        return self.send_conf()
     # end push_conf
 
 # end QfxConf
