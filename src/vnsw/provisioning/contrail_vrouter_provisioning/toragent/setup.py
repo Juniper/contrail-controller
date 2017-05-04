1ddc233e67e3ec66bb036bdad77ec473861df5e6
jayaramsatya <sajayaram@juniper.net>
2017-05-04 23:30:19 +0530

    python scripts are added for adding and deletting the toragent for Ansible support partial-bug:#1686068
    Change-Id: I53a61cebd7a629baeb5e47ef98a2781bc7e09c36

--- src/vnsw/provisioning/contrail_vrouter_provisioning/toragent/setup.py
+++ src/vnsw/provisioning/contrail_vrouter_provisioning/toragent/setup.py
@@ -16,17 +16,16 @@ class TorAgentBaseSetup(ContrailSetup):
         super(TorAgentBaseSetup, self).__init__()
         self._args = tor_agent_args
 
-    def fixup_tor_agent(self):
+    def create_ssl_certs(self):
         ssl_cert = ''
         ssl_privkey = ''
         ssl_cacert = ''
+        self.ssl_cert = '/etc/contrail/ssl/certs/server.pem'
+        self.ssl_privkey = '/etc/contrail/ssl/private/server-privkey.pem'
+        
+    def fixup_tor_agent(self):
         if self._args.tor_ovs_protocol.lower() == 'pssl':
-            ssl_cert = ('/etc/contrail/ssl/certs/tor.' + self._args.tor_id +
-                        '.cert.pem')
-            ssl_privkey = ('/etc/contrail/ssl/private/tor.' +
-                           self._args.tor_id + '.privkey.pem')
-            ssl_cacert = '/etc/contrail/ssl/certs/cacert.pem'
-
+            self.ssl_cacert = '/etc/contrail/ssl/certs/ca-cert.pem'
         template_vals = {
                 '__contrail_control_ip__': self._args.self_ip,
                 '__contrail_agent_name__': self._args.agent_name,
@@ -67,16 +66,50 @@ class TorAgentBaseSetup(ContrailSetup):
         dst = "/etc/contrail/supervisord_vrouter_files/%s" %\
               self.tor_ini_file_name
         local("sudo mv %s %s" % (src, dst))
-
     def create_init_file(self):
-        local("sudo cp /etc/init.d/contrail-vrouter-agent /etc/init.d/%s" %
-              (self.tor_process_name))
+        local("sudo cp /etc/init.d/contrail-vrouter-agent /etc/init.d/%s" %(self.tor_process_name))
+
+    def add_vnc_config(self):
+        cmd = "sudo python /opt/contrail/utils/provision_vrouter.py"
+        cmd += " --host_name %s" % self.agent_name
+        cmd += " --host_ip %s" % self._args.self_ip
+        cmd += " --api_server_ip %s" % self._args.cfgm_ip
+        cmd += " --admin_user %s" % self._args.admin_user
+        cmd += " --admin_password %s"  % self._args.admin_password
+        cmd += " --admin_tenant_name %s" % self._args.admin_tenant
+        cmd += " --openstack_ip %s" % self._args.authserver_ip
+        cmd += " --router_type tor-agent"
+        cmd += " --oper add "
+        with settings(warn_only=True):
+            local(cmd)
+
+    def add_tor_vendor(self):
+        cmd = "sudo python /opt/contrail/utils/provision_physical_device.py"
+        cmd += " --device_name %s" % self.args_.agent_name
+        cmd += " --vendor_name %s" % self.args_.tor_vendor_name
+        cmd += " --device_mgmt_ip %s" % self.args_.tor_ip
+        cmd += " --device_tunnel_ip %s" % self.args_.tor_tunnel_ip
+        cmd += " --device_tor_agent %s" % self._args.agent_name
+        cmd += " --device_tsn %s" % self.args_.tsn_name
+        cmd += " --api_server_ip %s" % self._args.cfgm_ip
+        cmd += " --admin_user %s"  % self._args.admin_user
+        cmd += " --admin_password %s" % self._args.admin_password
+        cmd += " --admin_tenant_name %s" % self._args.admin_tenant
+        cmd += " --openstack_ip %s" % self._args.authserver_ip
+        cmd += " --oper add"
+        local(cmd)
+
+    def run_services(self):
+        if self._args.restart:
+            local("sudo supervisorctl -c /etc/contrail/supervisord_vrouter.conf update")
 
     def setup(self):
         self.fixup_tor_agent()
         self.fixup_tor_ini()
         self.create_init_file()
-
+        self.add_vnc_config()
+        self.add_tor_vendor()
+        self.run_services()
 
 class TorAgentSetup(ContrailSetup):
     def __init__(self, args_str=None):
@@ -86,6 +119,12 @@ class TorAgentSetup(ContrailSetup):
             args_str = ' '.join(sys.argv[1:])
 
         self.global_defaults = {
+            'cfgm_ip': '127.0.0.1',
+            'authserver_ip': '127.0.0.1',
+            'admin_user':None,
+            'admin_passwd':None,
+            'admin_tenant_name':'admin',
+            'restart':False
         }
 
         self.parse_args(args_str)
@@ -99,20 +138,22 @@ class TorAgentSetup(ContrailSetup):
             --tor_agent_ovs_ka 10000
         '''
         parser = self._parse_args(args_str)
-
-        parser.add_argument(
-                "--self_ip", help="IP Address of this(compute) node")
+        parser.add_argument( "--self_ip", help="IP Address of this(compute) node")
+        parser.add_argument("--cfgm_ip", help = "IP Address of the config node")
+        parser.add_argument("--discovery_server_ip", help="IP Address of the config node")
+        parser.add_argument("--authserver_ip", help = "IP Address of the authserver(keystone) node")
+        parser.add_argument("--admin_user", help = "Authserver admin tenants user name")
+        parser.add_argument("--admin_password", help = "AuthServer admin user's password")
+        parser.add_argument("--admin_tenant_name", help = "AuthServer admin tenant name")
+        
         parser.add_argument("--agent_name", help="Name of the TOR agent")
         parser.add_argument(
                 "--http_server_port", help="Port number for the HTTP server.")
-        parser.add_argument(
-                "--discovery_server_ip", help="IP Address of the config node")
         parser.add_argument("--tor_ip", help="TOR Switch IP")
         parser.add_argument("--tor_id", help="Unique ID for the TOR")
         parser.add_argument("--tor_ovs_port", help="OVS Port Number")
-        parser.add_argument("--tsn_ip", help="TSN Node IP")
-        parser.add_argument(
-                "--tor_ovs_protocol",
+        parser.add_argument("--tor_tunnel_ip", help="Tor tunnel  Node IP")
+        parser.add_argument("--tor_ovs_protocol",
                 help="TOR OVS Protocol. Currently Only TCP supported")
         parser.add_argument(
                 "--tor_agent_ovs_ka",
