a6caa4127cc9f9fb66bf94018834156bd65ec21e
Ananth Suryanarayana <anantha@juniper.net>
2017-05-04 17:04:19 -0700

    Provide Llgr only support
    o Send GR capability even when configured restart-time is 0
    o Enable GR helper even if received restart-time is 0
    
    Fix a few issues in LLGR
    
    o Process received notification code-subcode correctly and trigger
      GR/LLGR Helper mode accordingly
    o Add UTs to cover most of the scenarios described above
    
    Change-Id: Ibcef9c059408a61e8513b6a9f6e1d015128e6515
    Closes-Bug: 1688133

--- src/bgp/bgp_table.cc
+++ src/bgp/bgp_table.cc
@@ -165,16 +165,13 @@ void BgpTable::ProcessRemovePrivate(const RibOut *ribout, BgpAttr *attr) const {
 // attach NO_EXPORT community instead.
 //
 void BgpTable::ProcessLlgrState(const RibOut *ribout, const BgpPath *path,
-                                BgpAttr *attr) {
+                                BgpAttr *attr, bool llgr_stale_comm) {
     if (!server() || !server()->comm_db())
         return;
 
     // Skip LLGR specific attributes manipulation for rtarget routes.
     if (family() == Address::RTARGET)
         return;
-
-    bool llgr_stale_comm = attr->community() &&
-        attr->community()->ContainsValue(CommunityType::LlgrStale);
 
     // If the path is not marked as llgr_stale or if it does not have the
     // LLGR_STALE community, then no action is necessary.
@@ -210,8 +207,6 @@ void BgpTable::ProcessLlgrState(const RibOut *ribout, const BgpPath *path,
                                 attr->community(), CommunityType::NoExport);
         attr->set_community(comm);
     }
-
-    return;
 }
 
 UpdateInfo *BgpTable::GetUpdateInfo(RibOut *ribout, BgpRoute *route,
@@ -257,6 +252,8 @@ UpdateInfo *BgpTable::GetUpdateInfo(RibOut *ribout, BgpRoute *route,
 
         const IPeer *peer = path->GetPeer();
         BgpAttr *clone = NULL;
+        bool llgr_stale_comm = attr->community() &&
+            attr->community()->ContainsValue(CommunityType::LlgrStale);
         if (ribout->peer_type() == BgpProto::IBGP) {
             // Split horizon check.
             if (peer && peer->PeerType() == BgpProto::IBGP)
@@ -371,7 +368,7 @@ UpdateInfo *BgpTable::GetUpdateInfo(RibOut *ribout, BgpRoute *route,
         }
 
         assert(clone);
-        ProcessLlgrState(ribout, path, clone);
+        ProcessLlgrState(ribout, path, clone, llgr_stale_comm);
 
         // Locate the new BgpAttrPtr.
         attr_ptr = clone->attr_db()->Locate(clone);
