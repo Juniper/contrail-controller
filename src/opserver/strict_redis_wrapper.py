92d83d20b726ce5914ea241e91c0ad130d9ae9b7
arvindvis <arvindv@juniper.net>
2017-05-15 11:37:43 -0700

    We are passing TCP timeout options to all StrictRedis calls. The newer python-redis package has API options to support these . Earlier the library had TIMEOUT changes in it and was maintained by us. Introducing a wrapper class for StrictRedis which calls the StrictRedis with TCP timeout values Closes-Bug:#1674803
    Change-Id: I48556394b7dd84a8df77704eabc7f0ecf8850b18

---
+++ src/opserver/strict_redis_wrapper.py
@@ -0,0 +1,11 @@
+import socket
+import redis
+class StrictRedisWrapper(redis.StrictRedis):
+
+    def __init__(self, *args, **kwargs):
+        tcp_keepalive_opts = {socket.TCP_KEEPIDLE:3,socket.TCP_KEEPINTVL:1,
+                      socket.TCP_KEEPCNT:3}
+        super(StrictRedisWrapper, self).__init__(*args,
+                          socket_keepalive=True,
+                          socket_keepalive_options=tcp_keepalive_opts,
+                          **kwargs)
