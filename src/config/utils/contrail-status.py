6c4d30edf0af489d17d364ac0b149150a100be57
Megh Bhatt <meghb@juniper.net>
2017-05-19 16:10:27 -0700

    contrail-status does not show correct status for contrail-database
    Seperate out initd_svc and check_return_code variables in
    check_svc(). Cassandra is run as initd service even when
    the init system used is upstart (ub14.04 bare metal),
    supervisor (ub14.04 docker) or sysv and the service status
    command always returns the correct status and so we need to
    check the return code in all cases for cassandra.
    
    Change-Id: Ibdca3f009ed46c687a949c73884c2222107629ae
    Closes-Bug: #1692069

--- src/config/utils/contrail-status.py
+++ src/config/utils/contrail-status.py
@@ -252,7 +252,7 @@ class IntrospectUtil(object):
 #end class IntrospectUtil
 
 def service_installed(svc, initd_svc):
-    si_init = init
+    si_init = init_sys_used
     if initd_svc:
         si_init = 'sysv'
     if distribution == 'redhat':
@@ -269,7 +269,7 @@ def service_installed(svc, initd_svc):
 # end service_installed
 
 def service_bootstatus(svc, initd_svc):
-    sb_init = init
+    sb_init = init_sys_used
     if initd_svc:
         sb_init = 'sysv'
     if distribution == 'redhat':
@@ -315,11 +315,11 @@ def service_status(svc, check_return_code):
         return 'inactive'
 # end service_status
 
-def check_svc(svc, initd_svc=False):
+def check_svc(svc, initd_svc=False, check_return_code=False):
     psvc = svc + ':'
     if service_installed(svc, initd_svc):
         bootstatus = service_bootstatus(svc, initd_svc)
-        status = service_status(svc, initd_svc)
+        status = service_status(svc, check_return_code)
     else:
         bootstatus = ' (disabled on boot)'
         status='inactive'
@@ -554,8 +554,10 @@ def contrail_service_status(nodetype, options):
             check_status(svc_name, options)
     elif nodetype == 'database':
         print "== Contrail Database =="
-        initd_svc = init_sys_used == 'sysv' or init_sys_used == 'upstart'
-        check_svc('contrail-database', initd_svc=initd_svc)
+        initd_svc = init_sys_used != 'systemd'
+        check_return_code = True
+        check_svc('contrail-database', initd_svc=initd_svc,
+            check_return_code=check_return_code)
         print ""
         for svc_name in CONTRAIL_SERVICES[nodetype][init_sys_used]:
             check_status(svc_name, options)
