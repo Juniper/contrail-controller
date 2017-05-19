4e80de6b103ba7ed26b0eb0b6ec531b3c9da5d0c
Hari Prasad Killi <haripk@juniper.net>
2017-05-19 14:55:49 +0530

    Create & Delete Tor Agent service files for ubuntu 16.04
    Also, check for files to be present before calling remove.
    
    Change-Id: Id737984d2e76bc6daf5432c2d5a534f54c71a3fe
    closes-bug: #1691970
    (cherry picked from commit a50d1c072e4e25bed582e5eb424b6f4eba86e5f7)

--- src/vnsw/provisioning/contrail_vrouter_provisioning/toragent/cleanup.py
+++ src/vnsw/provisioning/contrail_vrouter_provisioning/toragent/cleanup.py
@@ -7,10 +7,15 @@ import os
 import sys
 import argparse
 import ConfigParser
+import platform
 import re
+
 from contrail_vrouter_provisioning import local
 from setup import TorAgentSetup
 from contrail_vrouter_provisioning.base import ContrailSetup
+from distutils.version import LooseVersion
+
+(PLATFORM, VERSION, EXTRA) = platform.linux_distribution()
 
 class TorAgentBaseCleanup(ContrailSetup):
     def __init__(self, tor_agent_args, args_str=None):
@@ -23,6 +28,15 @@ class TorAgentBaseCleanup(ContrailSetup):
         # if the non_mgmt_ip is set use this as control ip
         if self._args.non_mgmt_ip:
             self._args.self_ip = self._args.non_mgmt_ip
+        if (('ubuntu' in PLATFORM.lower()) and
+            (LooseVersion(VERSION) >= LooseVersion('16.04'))):
+            self.systemd_setup = True
+            self.dirname = '/lib/systemd/system/'
+            self.suffix = '.service'
+        else:
+            self.systemd_setup = False
+            self.dirname = '/etc/contrail/supervisord_vrouter_files/'
+            self.suffix = '.ini'
 
     def remove_tor_agent_conf_files(self, tor_id):
         ssl_cert = '/etc/contrail/ssl/certs/tor.' + tor_id + '.cert.pem'
@@ -32,13 +46,19 @@ class TorAgentBaseCleanup(ContrailSetup):
             os.remove(ssl_cert)
         if os.path.exists(ssl_privkey):
             os.remove(ssl_privkey)
-        self.tor_file_name = 'contrail-tor-agent-' + tor_id + '.conf'
-        os.remove("/etc/contrail/%s" % self.tor_file_name)
+        self.tor_file_name = '/etc/contrail/contrail-tor-agent-' + tor_id + '.conf'
+        if os.path.exists(self.tor_file_name):
+            os.remove(self.tor_file_name)
         self.tor_process_name = 'contrail-tor-agent-' + tor_id
         local("sudo service %s stop" % self.tor_process_name)
-        self.tor_ini_file_name = self.tor_process_name + '.ini'
-        os.remove("/etc/contrail/supervisord_vrouter_files/%s" % self.tor_ini_file_name)
-        os.remove("/etc/init.d/%s" % self.tor_process_name)
+
+    def remove_tor_agent_ini_files(self, tor_id):
+        self.tor_ini_file_name = self.dirname + self.tor_process_name + self.suffix
+        if os.path.exists(self.tor_ini_file_name):
+            os.remove(self.tor_ini_file_name)
+        initd_file = '/etc/init.d/' + self.tor_process_name
+        if os.path.exists(initd_file):
+            os.remove(initd_file)
 
     def del_vnc_config(self):
         cmd = "sudo python /opt/contrail/utils/provision_vrouter.py"
@@ -66,15 +86,17 @@ class TorAgentBaseCleanup(ContrailSetup):
 
     def stop_services(self):
         if self._args.restart:
-            local("sudo supervisorctl -c /etc/contrail/supervisord_vrouter.conf update")
+            if not self.systemd_setup:
+                local("sudo supervisorctl -c /etc/contrail/supervisord_vrouter.conf update")
 
     def cleanup(self, tor_id):
         self.remove_tor_agent_conf_files(tor_id)
+        self.remove_tor_agent_ini_files(tor_id)
         self.del_physical_device()
         self.del_vnc_config()
 
     def get_tor_id_list_old(self):
-        for file in os.listdir("/etc/contrail/supervisord_vrouter_files"):
+        for file in os.listdir(self.dirname):
             if file.startswith('contrail-tor-agent'):
                 tor_id_old = re.findall(r'\d+', file)
                 self.tor_id_list_old.append(tor_id_old[0])
@@ -92,13 +114,10 @@ class TorAgentBaseCleanup(ContrailSetup):
         flag = False
         self.get_tor_id_list_old()
         for tor_id_old in self.tor_id_list_old:
-            if self._args.tor_id_list is None:
+            if self._args.tor_id_list is None or \
+               not (tor_id_old in self._args.tor_id_list):
                 self.delete_torid_config(tor_id_old)
                 flag = True
-            else:
-                if not (tor_id_old in self._args.tor_id_list):
-                    self.delete_torid_config(tor_id_old)
-                    flag = True
         if flag:
             self.stop_services()
 
