74ee24048d9f94d552aa8b133ae4ffc5c5bde2a8
Sachin Bansal <sbansal@juniper.net>
2017-05-19 12:00:15 -0700

    Handle errors in delete callback
    Even when an error is enountered during a delete callback for an object,
    we should still purge it from local cache so that it does not interfere
    with future creations of the object with the same name.
    
    Change-Id: I37e478fca688155a917948ae18eb6c970617a44b
    Closes-Bug: 1681844

--- src/config/common/vnc_db.py
+++ src/config/common/vnc_db.py
@@ -91,11 +91,14 @@ class DBBase(object):
 
     @classmethod
     def delete(cls, key):
-        obj = cls.get(key)
-        if obj is None:
-            return
-        obj.delete_obj()
-        del cls._dict[key]
+        try:
+            obj = cls.get(key)
+            if obj is None:
+                return
+            obj.delete_obj()
+        finally:
+            # Even if an error is encountered, delete the object from cache
+            del cls._dict[key]
     # end delete
 
     def get_ref_uuid_from_dict(self, obj_dict, ref_name):
@@ -422,4 +425,3 @@ class DBBase(object):
             return None
 
 # end class DBBase
-
