1d9d377e39475d5ae4530f27b0668b17319bf8fb
Hari Prasad Killi <haripk@juniper.net>
2017-05-27 10:08:12 +0530

    Initialize all variables in Agent Param.
    As log file count is not initialized, it takes random value and is
    causing Agent hogging CPU whenever log file rotation is required
    (thanks to Praveen for identifying this).
    
    Also initialized other missing variables.
    
    Change-Id: I53600f333e1c42b74d2566ef52ae04dc9810818c
    closes-bug: #1693748

--- src/vnsw/agent/init/agent_param.cc
+++ src/vnsw/agent/init/agent_param.cc
@@ -1221,10 +1221,12 @@ AgentParam::AgentParam(bool enable_flow_options,
         flow_ksync_tokens_(Agent::kFlowKSyncTokens),
         flow_del_tokens_(Agent::kFlowDelTokens),
         flow_update_tokens_(Agent::kFlowUpdateTokens),
+        flow_netlink_pin_cpuid_(0),
         stale_interface_cleanup_timeout_
         (Agent::kDefaultStaleInterfaceCleanupTimeout),
         config_file_(), program_name_(),
-        log_file_(), log_local_(false), log_flow_(false), log_level_(),
+        log_file_(), log_files_count_(10), log_file_size_(1024 * 1024),
+        log_local_(false), log_flow_(false), log_level_(),
         log_category_(), use_syslog_(false),
         http_server_port_(), host_name_(),
         agent_stats_interval_(kAgentStatsInterval),
@@ -1268,7 +1270,11 @@ AgentParam::AgentParam(bool enable_flow_options,
         task_monitor_timeout_msec_(Agent::kDefaultTaskMonitorTimeout),
         qos_priority_tagging_(true),
         default_nic_queue_(Agent::kInvalidQueueId),
-        llgr_params_() {
+        llgr_params_(),
+        mac_learning_thread_count_(Agent::kDefaultFlowThreadCount),
+        mac_learning_add_tokens_(Agent::kMacLearningDefaultTokens),
+        mac_learning_update_tokens_(Agent::kMacLearningDefaultTokens),
+        mac_learning_delete_tokens_(Agent::kMacLearningDefaultTokens) {
 
     uint32_t default_pkt0_tx_buffers = Agent::kPkt0TxBufferCount;
     uint32_t default_stale_interface_cleanup_timeout = Agent::kDefaultStaleInterfaceCleanupTimeout;
