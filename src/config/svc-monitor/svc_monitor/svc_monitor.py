4dc34965352373cd20c905112c69ae215dbd342b
Nipa Kumar <nipak@juniper.net>
2017-06-06 09:52:05 -0700

    Load re-balance of collector node after fail-over
    Auto re-balance for load distribution is not supported. To achieve fair
    load-balance when a node goes DOWN/UP on SIGHUP we will reconnect to
    original even if the list is not modified ensuring if nodes as UP, the
    subscribers will connect to original and hence achieve a fair load-balance.
    (Thnaks to Sundar to suggest the solution)
    
    Change-Id: I91e4db5712726bb027d38674a0370671cd66b552
    Closes-Bug:1694793

--- src/config/svc-monitor/svc_monitor/svc_monitor.py
+++ src/config/svc-monitor/svc_monitor/svc_monitor.py
@@ -517,7 +517,8 @@ class SvcMonitor(object):
                         if new_chksum != self._chksum:
                             self._chksum = new_chksum
                             config.random_collectors = random.sample(collectors, len(collectors))
-                            self.logger.sandesh_reconfig_collectors(config)
+                        # Reconnect to achieve load-balance irrespective of list
+                        self.logger.sandesh_reconfig_collectors(config)
                 except ConfigParser.NoOptionError as e:
                      pass
     # end sighup_handler
