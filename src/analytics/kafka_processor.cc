69cc6128e04792ab666aff3421d94b97d9eec0b5
zcui <zcui@juniper.net>
2017-06-13 08:49:11 -0700

    Problem description:Colector cores in KafkaRebalanceCb::rebalance_cb
    Root cause: When problem happen, contrail-collector met assert, so shutdown
    flow is triggered, at this point, analytics(k_re_cb->collector_) has been deleted.
    
    (gdb) p analytics
    $1 = (VizCollector *) 0x0
    (gdb)
    
    Because stop kafka has a time delay, so callback function use the memory which has
    been delated.
    
    Solution: Add Shutdown fucntion for KafkaRebalanceCb to set collector to NULL when
    shutdown happen
    
    Closes bug: 1695816
    
    Change-Id: Ide87627a29b696d2b713a38121e03287ec24bdff

--- src/analytics/kafka_processor.cc
+++ src/analytics/kafka_processor.cc
@@ -111,12 +111,15 @@ class KafkaProcessor::KafkaWorker {
 };
 
 class KafkaRebalanceCb : public RdKafka::RebalanceCb {
-  public:
+  public: 
     void rebalance_cb (RdKafka::KafkaConsumer *consumer,
              RdKafka::ErrorCode err,
                 std::vector<RdKafka::TopicPartition*> &pts) {
        map<string, map<int32_t,uint64_t> > topics;
-   
+
+       if (NULL == collector_) {
+           return;
+       }
 
        // Load all topic-partitions into a
        // map (key topic) of sets of ints (partitions)
@@ -223,6 +226,10 @@ class KafkaRebalanceCb : public RdKafka::RebalanceCb {
         collector_ = collector;
         topics_ = topics;
         aggs_ = aggs;
+    }
+
+    void Shutdown(){
+        collector_ = NULL;
     }
     KafkaRebalanceCb() : collector_(NULL) {}
   private:
@@ -559,6 +566,7 @@ void
 KafkaProcessor::Shutdown() {
     TimerManager::DeleteTimer(kafka_timer_);
     kafka_timer_ = NULL;
+    k_re_cb.Shutdown();
     StopKafka();
 }
 
