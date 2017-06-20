0b5eb737a9bec79c84fa57e8a81d938772f330ad
sbalineni <sbalineni@juniper.net>
2017-06-20 12:52:06 -0700

    [DM]: QFX Netconf Configuration : Config generation
    - This is the basic implementaion, covers the push of QFX configuration.
    - Added dependency tracker for LR, Project, VN, PR associations
    - Added needed support for persisting LR, Project DB objects, established links among VN, LR, PR, Project objects
    - Added code to push QFx config (Unit Tests TBD) : RI, Policies, switch otpions, interfaces, vlans
    - UT: To be added more in this review
    - API Server: Schema Changes for ESI, fix for serialzing vn obj to json
    
    Change-Id: I1fef50fb7b3e5aa5477dd69f6c5e049e20b53652
    Partial-Bug: #1686829

--- src/config/device-manager/device_manager/plugins/juniper/qfx/series5K/qfx_5k.py
+++ src/config/device-manager/device_manager/plugins/juniper/qfx/series5K/qfx_5k.py
@@ -28,12 +28,18 @@ class Qfx5kConf(QfxConf):
         return super(Qfx5kConf, cls).register(qconf)
     # end register
 
+    def set_product_specific_config(self):
+        if self.global_switch_options_config is None:
+            self.global_switch_options_config = SwitchOptions(comment=DMUtils.switch_options_comment())
+        self.global_switch_options_config.set_route_distinguisher(self.bgp_params['identifier'])
+    # end set_product_specific_config
+
     def push_conf(self, is_delete=False):
         if not self.physical_router:
             return 0
         if is_delete:
             return self.send_conf(is_delete=True)
-        self.build_bgp_config()
+        self.set_qfx_common_config()
         return self.send_conf()
     # end push_conf
 
