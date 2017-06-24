fb9cd718d17d0428d20623aabaf383501ba0dd49
Pramodh D'Souza <pdsouza@juniper.net>
2017-06-24 14:35:03 -0700

    Fixes: IfMap  Introspect and tests Ifmap show test cases added/modiied. uses page limit. Also refactored some show code. (NodeToUuidMapping, UuidToNodeMapping,PendingVmreg, UuidCache,FqNameCache Modified the json file to indicate children correctly. Disabled older ifmap_uuid_mpper_tests and moved out of flaky tests. Partial-bug: 1698247 Change-Id: I247ad47f10e7b3b2008b5a925098a1fb21302c15

--- src/ifmap/ifmap_sandesh_context.h
+++ src/ifmap/ifmap_sandesh_context.h
@@ -12,13 +12,19 @@ class IFMapServer;
 class IFMapSandeshContext : public SandeshContext {
 public:
     IFMapSandeshContext(IFMapServer *ifmap_server)
-        : ifmap_server_(ifmap_server) {
+        : ifmap_server_(ifmap_server), page_limit_(0), iter_limit_(0) {
     }
+    uint32_t page_limit() const { return page_limit_; }
+    void set_page_limit(uint32_t page_limit) { page_limit_ = page_limit; }
+    uint32_t iter_limit() const { return iter_limit_; }
+    void set_iter_limit(uint32_t iter_limit) { iter_limit_ = iter_limit; }
 
     IFMapServer *ifmap_server() { return ifmap_server_; }
 
 private:
     IFMapServer *ifmap_server_;
+    uint32_t page_limit_;
+    uint32_t iter_limit_;
 };
 
 #endif  // IFMAP__IFMAP_SANDESH_CONTEXT_H__
