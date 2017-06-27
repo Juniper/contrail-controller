e47d4b28f3a221620388bfbe291e52641ce7ad27
Artur Dębski <artur.debski@codilime.com>
2017-06-27 12:26:32 +0200

    Pod add/delete tests for isolated namespace
    Pod resource is tested for ADD and DELETE operations in a scenario
    where pod namespace is isolated.
    After creating designated namespace with "isolation" annotation, tests
    issue a pod add/delete event. After kube-manager processes the event,
    check existance of corresponding Contrail resources:
    - VM (virtual machine)
    - VMI (virtual machine interface)
    - IIP (instance ip)
    - SG (security group).
    
    Change-Id: Ib2a175e37b15efd11a59220f2314a5978730fc7a
    Closes-Bug: #1700738

--- src/container/kube-manager/kube_manager/tests/vnc/test_vnc_pod.py
+++ src/container/kube-manager/kube_manager/tests/vnc/test_vnc_pod.py
@@ -33,22 +33,28 @@ class VncPodTest(KMTestCase):
         super(VncPodTest, self).tearDown()
     #end tearDown
 
-    def _create_namespace(self, ns_name, ns_eval_vn_dict):
+    def _create_namespace(self, ns_name, ns_eval_vn_dict, is_isolated=False):
         ns_uuid = str(uuid.uuid4())
         ns_add_event = self.create_add_namespace_event(ns_name, ns_uuid)
+        ns_object = ns_add_event['object']
+        ns_object['spec'] = {}
+        ns_meta = ns_object['metadata']
+        ns_meta['annotations'] = {}
+
+        ns_meta['name'] = ns_name
+        ns_meta['uid'] = ns_uuid
+        ns_meta['namespace'] = ns_name
+        ns_meta['labels'] = []
+
+        if ns_eval_vn_dict:
+            ns_meta['annotations']['opencontrail.org/network'] = ns_eval_vn_dict
+        if is_isolated:
+            ns_meta['annotations']['opencontrail.org/isolation'] = 'true'
+
+        NamespaceKM.locate(ns_name, ns_object)
+
         self.enqueue_event(ns_add_event)
         self.wait_for_all_tasks_done()
-
-        # Add Namespace to DB
-        ns_labels = []
-        ns_meta = {'name': ns_name, 'uid': ns_uuid,
-                   'namespace': ns_name, 'labels': []}
-        ns_meta['annotations'] = {'opencontrail.org/network': ns_eval_vn_dict}
-        ns_obj = {}
-        ns_obj['kind'] = 'Namespace'
-        ns_obj['spec'] = {}
-        ns_obj['metadata'] = ns_meta
-        NamespaceKM.locate(ns_name, ns_obj)
 
         return ns_uuid
 
@@ -77,14 +83,11 @@ class VncPodTest(KMTestCase):
         ns_name = 'guestbook'
         pod_name = 'test-pod'
         pod_status = {'hostIP': '192.168.0.1', 'phase': 'created'}
-        eval_vn_dict = '{"domain":"default-domain",\
-                         "project":"%s",\
-                         "name":"%s"}' % (cluster_project, vn_name)
 
         kube_config.VncKubernetesConfig.args().cluster_project = \
             "{'project':'" + cluster_project + "'}"
 
-        ns_uuid = self._create_namespace(ns_name, eval_vn_dict)
+        ns_uuid = self._create_namespace(ns_name, None)
 
         proj_fq_name = ['default-domain', cluster_project]
         proj_obj = self._vnc_lib.project_read(fq_name=proj_fq_name)
@@ -93,7 +96,7 @@ class VncPodTest(KMTestCase):
         pod_uuid, pod_meta, pod_spec = self._create_pod(pod_name,
                                                         ns_name,
                                                         pod_status,
-                                                        eval_vn_dict)
+                                                        None)
 
         vn_obj = self._vnc_lib.virtual_network_read(id=vn_obj_uuid)
         self.assertIsNotNone(vn_obj)
@@ -145,13 +148,10 @@ class VncPodTest(KMTestCase):
         cluster_project = ns_name
         pod_name = 'test-pod'
         pod_status = {'hostIP': '192.168.0.1', 'phase': 'created'}
-        eval_vn_dict = '{"domain":"default-domain",\
-                         "project":"%s",\
-                         "name":"%s"}' % (cluster_project, vn_name)
 
         kube_config.VncKubernetesConfig.args().cluster_project = None
 
-        ns_uuid = self._create_namespace(ns_name, eval_vn_dict)
+        ns_uuid = self._create_namespace(ns_name, None)
 
         proj_fq_name = ['default-domain', cluster_project]
         proj_obj = self._vnc_lib.project_read(fq_name=proj_fq_name)
@@ -167,9 +167,145 @@ class VncPodTest(KMTestCase):
         pod_uuid, pod_meta, pod_spec = self._create_pod(pod_name,
                                                         ns_name,
                                                         pod_status,
-                                                        eval_vn_dict)
+                                                        None)
 
         vn_obj = self._vnc_lib.virtual_network_read(id=vn_obj_uuid)
+        self.assertIsNotNone(vn_obj)
+        vn_obj = VirtualNetworkKM.locate(vn_obj.uuid)
+        self.assertIsNotNone(vn_obj)
+
+        tmp_fq_name = ['default-domain', ns_name, pod_name]
+        vm = self._vnc_lib.virtual_machine_read(id=pod_uuid)
+        self.assertIsNotNone(vm)
+        vm = VirtualMachineKM.locate(vm.uuid)
+        self.assertIsNotNone(vm)
+        self.assertTrue(len(vm.virtual_machine_interfaces) > 0)
+
+        for vmi_id in list(vm.virtual_machine_interfaces):
+            vmi = self._vnc_lib.virtual_machine_interface_read(id=vmi_id)
+            self.assertIsNotNone(vmi)
+            self.assertEqual(vmi.parent_name, ns_name)
+            self.assertEqual(vmi.parent_uuid, proj_obj.uuid)
+            vmi = VirtualMachineInterfaceKM.locate(vmi_id)
+            self.assertTrue(len(vmi.security_groups) > 1)
+            for sg_uuid in list(vmi.security_groups):
+                sg = self._vnc_lib.security_group_read(id=sg_uuid)
+                self.assertIsNotNone(sg)
+            self.assertTrue(len(vmi.instance_ips) > 0)
+            for iip_uuid in list(vmi.instance_ips):
+                iip = self._vnc_lib.instance_ip_read(id=iip_uuid)
+                self.assertIsNotNone(iip)
+
+        pod_delete_event = self.create_event('Pod', pod_spec,
+                                             pod_meta, 'DELETED')
+        self.enqueue_event(pod_delete_event)
+        self.wait_for_all_tasks_done()
+
+        vn_obj = self._vnc_lib.virtual_network_read(id=vn_obj.uuid)
+        self.assertIsNotNone(vn_obj)
+        vn_obj = VirtualNetworkKM.locate(vn_obj.uuid)
+        self.assertIsNotNone(vn_obj)
+
+        tmp_fq_name = ['default-domain', ns_name, pod_name]
+        self.assertRaises(
+            NoIdError,
+            self._vnc_lib.virtual_machine_read,
+            fq_name=tmp_fq_name
+        )
+        self.assertTrue(len(vn_obj.instance_ips) == 0)
+
+
+    def test_vnc_pod_add_delete_isolated_cluster_project_undefined(self):
+        kube_config.VncKubernetesConfig.args().cluster_project = None
+
+        ns_name = 'guestbook-isolated'
+        cluster_project = ns_name
+        pod_name = 'test-pod'
+        pod_status = {'hostIP': '192.168.0.1', 'phase': 'created'}
+        eval_vn_dict = '{"domain":"default-domain",\
+            "project":"%s",\
+            "name":"%s"}' % (cluster_project, ns_name+'-vn')
+
+        self._create_namespace(ns_name, None, True)
+        vn_obj = self._vnc_lib.virtual_network_read(fq_name=['default-domain', cluster_project, ns_name+'-vn'])
+
+        proj_fq_name = ['default-domain', cluster_project]
+        proj_obj = self._vnc_lib.project_read(fq_name=proj_fq_name)
+
+        pod_uuid, pod_meta, pod_spec = self._create_pod(pod_name,
+                                                        ns_name,
+                                                        pod_status,
+                                                        None)
+        vn_obj = self._vnc_lib.virtual_network_read(id=vn_obj.uuid)
+        self.assertIsNotNone(vn_obj)
+        vn_obj = VirtualNetworkKM.locate(vn_obj.uuid)
+        self.assertIsNotNone(vn_obj)
+
+        tmp_fq_name = ['default-domain', ns_name, pod_name]
+        vm = self._vnc_lib.virtual_machine_read(id=pod_uuid)
+        self.assertIsNotNone(vm)
+        vm = VirtualMachineKM.locate(vm.uuid)
+        self.assertIsNotNone(vm)
+        self.assertTrue(len(vm.virtual_machine_interfaces) > 0)
+
+        for vmi_id in list(vm.virtual_machine_interfaces):
+            vmi = self._vnc_lib.virtual_machine_interface_read(id=vmi_id)
+            self.assertIsNotNone(vmi)
+            self.assertEqual(vmi.parent_name, ns_name)
+            self.assertEqual(vmi.parent_uuid, proj_obj.uuid)
+            vmi = VirtualMachineInterfaceKM.locate(vmi_id)
+            self.assertTrue(len(vmi.security_groups) > 1)
+            for sg_uuid in list(vmi.security_groups):
+                sg = self._vnc_lib.security_group_read(id=sg_uuid)
+                self.assertIsNotNone(sg)
+            self.assertTrue(len(vmi.instance_ips) > 0)
+            for iip_uuid in list(vmi.instance_ips):
+                iip = self._vnc_lib.instance_ip_read(id=iip_uuid)
+                self.assertIsNotNone(iip)
+
+        pod_delete_event = self.create_event('Pod', pod_spec,
+                                             pod_meta, 'DELETED')
+        self.enqueue_event(pod_delete_event)
+        self.wait_for_all_tasks_done()
+
+        vn_obj = self._vnc_lib.virtual_network_read(id=vn_obj.uuid)
+        self.assertIsNotNone(vn_obj)
+        vn_obj = VirtualNetworkKM.locate(vn_obj.uuid)
+        self.assertIsNotNone(vn_obj)
+
+        tmp_fq_name = ['default-domain', ns_name, pod_name]
+        self.assertRaises(
+            NoIdError,
+            self._vnc_lib.virtual_machine_read,
+            fq_name=tmp_fq_name
+        )
+        self.assertTrue(len(vn_obj.instance_ips) == 0)
+
+    def test_vnc_pod_add_delete_isolated_cluster_project_defined(self):
+        ns_name = 'guestbook-isolated'
+        cluster_project = ns_name
+        kube_config.VncKubernetesConfig.args().cluster_project = \
+            "{'project':'" + cluster_project + "'}"
+        ns_name = 'guestbook-isolated'
+        cluster_project = ns_name
+        pod_name = 'test-pod'
+        pod_status = {'hostIP': '192.168.0.1', 'phase': 'created'}
+        eval_vn_dict = '{"domain":"default-domain",\
+            "project":"%s",\
+            "name":"%s"}' % (cluster_project, ns_name + '-vn')
+
+        self._create_namespace(ns_name, None, True)
+        vn_obj = self._vnc_lib.virtual_network_read(
+            fq_name=['default-domain', cluster_project, ns_name + '-vn'])
+
+        proj_fq_name = ['default-domain', cluster_project]
+        proj_obj = self._vnc_lib.project_read(fq_name=proj_fq_name)
+
+        pod_uuid, pod_meta, pod_spec = self._create_pod(pod_name,
+                                                        ns_name,
+                                                        pod_status,
+                                                        None)
+        vn_obj = self._vnc_lib.virtual_network_read(id=vn_obj.uuid)
         self.assertIsNotNone(vn_obj)
         vn_obj = VirtualNetworkKM.locate(vn_obj.uuid)
         self.assertIsNotNone(vn_obj)
