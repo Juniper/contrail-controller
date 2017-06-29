93d0c13f3ede525076a3a4370e485dbc4e65f08a
Ignatious Johnson Christopher <ijohnson@juniper.net>
2017-06-28 21:39:43 -0700

    get by fq_name to avoid lookup in fq_name map
    by uuid.
    
    Change-Id: Ie948900eafefd46eaac14f12d55c5c1b38e673f6
    Closes-Bug: 1701160

--- src/config/schema-transformer/config_db.py
+++ src/config/schema-transformer/config_db.py
@@ -200,7 +200,9 @@ class GlobalSystemConfigST(DBBaseST):
             old_rtgt_obj = RouteTarget(old_rtgt_name)
 
             for ri_ref in route_tgt.obj.get_routing_instance_back_refs() or []:
-                ri = RoutingInstanceST.get_by_uuid(ri_ref['uuid']).obj
+                ri = RoutingInstanceST.get(':'.join(ri_ref['to'])).obj
+                if not ri:
+                    continue
                 inst_tgt_data = InstanceTargetType()
                 ri.del_route_target(old_rtgt_obj)
                 ri.add_route_target(new_rtgt_obj.obj, inst_tgt_data)
@@ -220,7 +222,9 @@ class GlobalSystemConfigST(DBBaseST):
             # Updating the logical router referred by the route target with
             # new route target.
             for router_ref in route_tgt.obj.get_logical_router_back_refs() or []:
-                logical_router = LogicalRouterST.get_by_uuid(router_ref['uuid']).obj
+                logical_router = LogicalRouterST.get(':'.join(router_ref['to'])).obj
+                if not logical_router:
+                    continue
                 logical_router.del_route_target(old_rtgt_obj)
                 logical_router.add_route_target(new_rtgt_obj.obj) 
                 cls._vnc_lib.logical_router_update(logical_router)
