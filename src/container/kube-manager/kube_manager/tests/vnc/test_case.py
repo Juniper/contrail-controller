5d487989f47d22824859ab0a23d038fc7254ac66
Adam Wierzbicki <adam.wierzbicki@codilime.com>
2017-07-11 17:48:51 +0200

    Automated monitoring logs for tracebacks
    ErrorInterceptingSandeshLogger is a custom SandeshLogger class which
    allows to intercept calls containing tracebacks. They are stored in
    a class variable and can be used in a test to assert that no traceback
    appeared during test execution. Base class for kube-manager tests was
    modified to use this logger.
    
    Related-Bug: #1703837
    Change-Id: I78a970b1cdf18aa9b368b869207f3413018dc952

--- src/container/kube-manager/kube_manager/tests/vnc/test_case.py
+++ src/container/kube-manager/kube_manager/tests/vnc/test_case.py
@@ -1,20 +1,85 @@
 import sys
 import time
 import uuid
-import os
+import tempfile
 
+import gevent
+from cfgm_common import vnc_cgitb
+from kube_manager.common import args as kube_args
+from pysandesh import sandesh_logger
 sys.path.append("../../config/common/tests")
-from test_utils import *
 import test_common
 sys.path.insert(0, '../../../../build/production/container/kube-manager')
+from vnc_api.vnc_api import (
+    Project, AddressType, SubnetType, PolicyRuleType, RefsExistError, PortType, PolicyEntriesType, IdPermsType,
+    SecurityGroup, VirtualNetwork, VirtualNetworkType, NoIdError, VirtualMachine, VirtualMachineInterface, InstanceIp)
 
-import tempfile
-from vnc_api.vnc_api import *
-from cfgm_common import vnc_cgitb
-from kube_manager.kube_manager import *
-from kube_manager.common import args as kube_args
+
+class ErrorInterceptingSandeshLogger(sandesh_logger.SandeshLogger):
+
+    _exceptions = []
+    _other_errors = []
+
+    @classmethod
+    def register_exception(cls, msg, *args, **kwargs):
+        if 'traceback' in msg.lower():
+            cls._exceptions.append((msg, args, kwargs))
+            return True
+        return False
+
+    @classmethod
+    def register_error(cls, msg, *args, **kwargs):
+        if not cls.register_exception(msg, *args, **kwargs):
+            cls._other_errors.append((msg, args, kwargs))
+
+    @classmethod
+    def get_exceptions(cls):
+        return list(cls._exceptions)
+
+    @classmethod
+    def get_other_errors(cls):
+        return list(cls._other_errors)
+
+    @classmethod
+    def reset(cls):
+        cls._exceptions, cls._other_errors = [], []
+
+    @classmethod
+    def get_qualified_name(cls):
+        return '{module_name}.{class_name}'.format(module_name=cls.__module__, class_name=cls.__name__)
+
+    class LoggerWrapper(object):
+
+        def __init__(self, logger):
+            self._logger = logger
+
+        def __getattr__(self, item):
+            return getattr(self._logger, item)
+
+        def error(self, msg, *args, **kwargs):
+            ErrorInterceptingSandeshLogger.register_error(msg, *args, **kwargs)
+            return self._logger.error(msg, *args, **kwargs)
+
+        def critical(self, msg, *args, **kwargs):
+            ErrorInterceptingSandeshLogger.register_error(msg, *args, **kwargs)
+            return self._logger.critical(msg, *args, **kwargs)
+
+        def exception(self, msg, *args, **kwargs):
+            ErrorInterceptingSandeshLogger.register_error(msg, *args, **kwargs)
+            return self._logger.exception(msg, *args, **kwargs)
+
+        def log(self, lvl, msg, *args, **kwargs):
+            ErrorInterceptingSandeshLogger.register_exception(msg, *args, **kwargs)
+            return self._logger.log(lvl, msg, *args, **kwargs)
+
+    def __init__(self, *args, **kwargs):
+        super(ErrorInterceptingSandeshLogger, self).__init__(*args, **kwargs)
+        self._logger = ErrorInterceptingSandeshLogger.LoggerWrapper(self._logger)
+
 
 class KMTestCase(test_common.TestCase):
+
+    DEFAULT_SECGROUP_DESCRIPTION = "Default security group"
 
     @classmethod
     def setUpClass(cls, extra_config_knobs=None):
@@ -25,19 +90,27 @@ class KMTestCase(test_common.TestCase):
         if extra_config_knobs:
             extra_config.append(extra_config_knobs)
         super(KMTestCase, cls).setUpClass(extra_config_knobs=extra_config)
-        cls._svc_mon_greenlet = gevent.spawn(test_common.launch_svc_monitor,
-            cls._cluster_id, cls.__name__, cls._api_server_ip, cls._api_server_port)
-        cls._st_greenlet = gevent.spawn(test_common.launch_schema_transformer,
-            cls._cluster_id, cls.__name__, cls._api_server_ip, cls._api_server_port)
+
+        ErrorInterceptingSandeshLogger.reset()
+
+        cls._svc_mon_greenlet = gevent.spawn(
+            test_common.launch_svc_monitor,
+            cls._cluster_id, cls.__name__, cls._api_server_ip, cls._api_server_port,
+            logger_class=ErrorInterceptingSandeshLogger.get_qualified_name())
+        cls._st_greenlet = gevent.spawn(
+            test_common.launch_schema_transformer,
+            cls._cluster_id, cls.__name__, cls._api_server_ip, cls._api_server_port,
+            extra_args="--logger_class {} ".format(ErrorInterceptingSandeshLogger.get_qualified_name()))
         test_common.wait_for_schema_transformer_up()
 
-        cls.event_queue = Queue()
+        cls.event_queue = gevent.queue.Queue()
         cls.spawn_kube_manager()
 
     @classmethod
     def spawn_kube_manager(cls, extra_args=()):
         kube_config = [
             ('DEFAULTS', 'log_file', 'contrail-kube-manager.log'),
+            ('DEFAULTS', 'logger_class', ErrorInterceptingSandeshLogger.get_qualified_name()),
             ('VNC', 'vnc_endpoint_ip', cls._api_server_ip),
             ('VNC', 'vnc_endpoint_port', cls._api_server_port),
             ('VNC', 'cassandra_server_list', "0.0.0.0:9160"),
@@ -57,6 +130,11 @@ class KMTestCase(test_common.TestCase):
         test_common.kill_schema_transformer(cls._st_greenlet)
         cls.kill_kube_manager()
         super(KMTestCase, cls).tearDownClass()
+
+        exceptions = ErrorInterceptingSandeshLogger.get_exceptions()
+        if exceptions:
+            raise AssertionError(
+                "Tracebacks found in logs:\n\n{}".format("\n\n".join(msg for msg, _, __ in exceptions)))
 
     @classmethod
     def kill_kube_manager(cls):
@@ -85,7 +163,6 @@ class KMTestCase(test_common.TestCase):
         self.event_queue.put(idle_event)
 
     def generate_kube_args(self):
-        args_str = ""
         kube_config = [
             ('DEFAULTS', 'log_file', 'contrail-kube-manager.log'),
             ('VNC', 'vnc_endpoint_ip', self._api_server_ip),
@@ -105,60 +182,52 @@ class KMTestCase(test_common.TestCase):
             cfg_parser.write(logconf)
             logconf.flush()
 
-            args_str = ["-c", conf.name]
-            args = kube_args.parse_args(args_str)
+            args = kube_args.parse_args(["-c", conf.name])
             return args
 
-    def create_add_namespace_event(self, name, uuid):
-        event = {}
-        object = {}
-        object['kind'] = 'Namespace'
-        object['spec'] = {}
-        object['metadata'] = {}
-        object['metadata']['name'] = name
-        object['metadata']['uid'] = uuid
-        event['type'] = 'ADDED'
-        event['object'] = object
-        return event
+    @staticmethod
+    def create_add_namespace_event(name, uid):
+        return KMTestCase.create_namespace_event(name, uid, type='ADDED')
 
-    def create_delete_namespace_event(self, name, uuid):
-        event = {}
-        object = {}
-        object['kind'] = 'Namespace'
-        object['spec'] = {}
-        object['metadata'] = {}
-        object['metadata']['name'] = name
-        object['metadata']['uid'] = uuid
-        event['type'] = 'DELETED'
-        event['object'] = object
-        return event
+    @staticmethod
+    def create_delete_namespace_event(name, uid):
+        return KMTestCase.create_namespace_event(name, uid, type='DELETED')
 
-    def create_event(self, kind, spec, meta, type):
-        # type: (object, object, object, object) -> object
-        event = {}
-        object = {}
-        object['kind'] = kind
-        object['spec'] = spec
-        object['metadata'] = meta
-        event['type'] = type
-        event['object'] = object
-        return event
+    @staticmethod
+    def create_namespace_event(name, uid, type):
+        return KMTestCase.create_event(
+            kind='Namespace',
+            spec={},
+            meta={
+                'name': name,
+                'uid': uid},
+            type=type)
+
+    @staticmethod
+    def create_event(kind, spec, meta, type):
+        return {
+            'type': type,
+            'object': {
+                'kind': kind,
+                'spec': spec,
+                'metadata': meta
+            }
+        }
 
     def create_project(self, name):
         proj_fq_name = ['default-domain', name]
         proj_obj = Project(name=name, fq_name=proj_fq_name)
 
         try:
-            uuid = self._vnc_lib.project_create(proj_obj)
-            if uuid:
-                proj_obj = self._vnc_lib.project_read(id=uuid)
+            uid = self._vnc_lib.project_create(proj_obj)
+            if uid:
+                proj_obj = self._vnc_lib.project_read(id=uid)
         except RefsExistError:
             proj_obj = self._vnc_lib.project_read(fq_name=proj_fq_name)
 
         return proj_obj
 
     def create_security_group(self, proj_obj):
-        DEFAULT_SECGROUP_DESCRIPTION = "Default security group"
         def _get_rule(ingress, sg, prefix, ethertype):
             sgr_uuid = str(uuid.uuid4())
             if sg:
@@ -189,7 +258,7 @@ class KMTestCase(test_common.TestCase):
 
         # create security group
         id_perms = IdPermsType(enable=True,
-                               description=DEFAULT_SECGROUP_DESCRIPTION)
+                               description=KMTestCase.DEFAULT_SECGROUP_DESCRIPTION)
         sg_obj = SecurityGroup(name='default', parent_obj=proj_obj,
                                id_perms=id_perms,
                                security_group_entries=sg_rules)
@@ -199,7 +268,9 @@ class KMTestCase(test_common.TestCase):
         return sg_obj
 
     def create_network(self, name, proj_obj):
-        vn = VirtualNetwork(name=name, parent_obj=proj_obj,
+        vn = VirtualNetwork(
+            name=name,
+            parent_obj=proj_obj,
             virtual_network_properties=VirtualNetworkType(forwarding_mode='l3'),
             address_allocation_mode='flat-subnet-only')
         try:
@@ -208,39 +279,38 @@ class KMTestCase(test_common.TestCase):
 
         except NoIdError:
             # Virtual network does not exist. Create one.
-            uuid = self._vnc_lib.virtual_network_create(vn)
-            vn_obj = self._vnc_lib.virtual_network_read(id=uuid)
+            uid = self._vnc_lib.virtual_network_create(vn)
+            vn_obj = self._vnc_lib.virtual_network_read(id=uid)
         return vn_obj
 
     def create_virtual_machine(self, name, vn, ipaddress):
         vm_instance = VirtualMachine(name)
         self._vnc_lib.virtual_machine_create(vm_instance)
-        fq_name = [name]
-        fq_name.append('0')
-        vmi = VirtualMachineInterface(parent_type = 'virtual-machine', fq_name = fq_name)
+        fq_name = [name, '0']
+        vmi = VirtualMachineInterface(parent_type='virtual-machine', fq_name=fq_name)
         vmi.set_virtual_network(vn)
         self._vnc_lib.virtual_machine_interface_create(vmi)
         ip = InstanceIp(vm_instance.name + '.0')
         ip.set_virtual_machine_interface(vmi)
         ip.set_virtual_network(vn)
         ip.set_instance_ip_address(ipaddress)
-        uuid = self._vnc_lib.instance_ip_create(ip)
+        self._vnc_lib.instance_ip_create(ip)
         return vm_instance
 
     def vmi_clean(self, vm_instance):
         fq_name = vm_instance.fq_name
         fq_name.append('0')
         try:
-            vmi = self._vnc_lib.virtual_machine_interface_read(fq_name = fq_name)
+            vmi = self._vnc_lib.virtual_machine_interface_read(fq_name=fq_name)
         except NoIdError:
             return
 
         ips = vmi.get_instance_ip_back_refs()
         for ref in ips:
-            self._vnc_lib.instance_ip_delete(id = ref['uuid'])
+            self._vnc_lib.instance_ip_delete(id=ref['uuid'])
 
-        self._vnc_lib.virtual_machine_interface_delete(id = vmi.uuid)
+        self._vnc_lib.virtual_machine_interface_delete(id=vmi.uuid)
 
     def delete_virtual_machine(self, vm_instance):
         self.vmi_clean(vm_instance)
-        self._vnc_lib.virtual_machine_delete(id = vm_instance.uuid)
+        self._vnc_lib.virtual_machine_delete(id=vm_instance.uuid)
