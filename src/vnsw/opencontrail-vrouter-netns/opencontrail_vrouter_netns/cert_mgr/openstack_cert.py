cd473b9d3ec181c5df9859d860f0820df18ca4b7
Yuvaraja Mariappan <ymariappan@juniper.net>
2017-07-12 08:01:30 -0700

    Kubernetes ingress https support in contrail
    Added following changes
    1. vnc_ingress is modified to understand the tls info
       from kubernetes and create the loadbalancer with https
       support in contrail
    2. svc_monior is modified to understand the k8s based https
       along with openstack https
    3. opencontrail_vrouter_netns is modifed to understand k8s
       https and openstack https
    
    Change-Id: I0be95b3e8ab1e896b841c4a7af7a6a6e4475c452
    Partial-bug: #1703846

---
+++ src/vnsw/opencontrail-vrouter-netns/opencontrail_vrouter_netns/cert_mgr/openstack_cert.py
@@ -0,0 +1,60 @@
+import logging
+import exceptions
+
+class OSCert:
+
+    def __init__(self, cert_container):
+        self._cert_container = cert_container
+
+    def get_certificate(self):
+        if self._cert_container.certificate:
+            try:
+                payload = self._cert_container.certificate.payload
+                return True, payload
+            except exceptions.Exception as e:
+                logging.exception('')
+                logging.error(e.__class__)
+                logging.error(e.__doc__)
+                logging.error(e.message)
+                return False, None
+        return True, None
+
+    def get_intermediates(self):
+        if self._cert_container.intermediates:
+            try:
+                payload = self._cert_container.intermediates.payload
+                return True, payload
+            except exceptions.Exception as e:
+                logging.exception('')
+                logging.error(e.__class__)
+                logging.error(e.__doc__)
+                logging.error(e.message)
+                return False, None
+        return True, None
+
+    def get_private_key(self):
+        if self._cert_container.private_key:
+            try:
+                payload = self._cert_container.private_key.payload
+                return True, payload
+            except exceptions.Exception as e:
+                logging.exception('')
+                logging.error(e.__class__)
+                logging.error(e.__doc__)
+                logging.error(e.message)
+                return False, None
+        return True, None
+
+    def get_private_key_passphrase(self):
+        if self._cert_container.private_key_passphrase:
+            try:
+                payload = self._cert_container.private_key_passphrase.payload
+                return True, payload
+            except exceptions.Exception as e:
+                msg = "Error in getting Private-Key-PassPhrase from Container"
+                logging.exception('')
+                logging.error(e.__class__)
+                logging.error(e.__doc__)
+                logging.error(e.message)
+                return False, None
+        return True, None
