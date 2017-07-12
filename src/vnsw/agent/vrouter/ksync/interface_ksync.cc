e214f425c5708d35b1fd5e300aa265a10439d070
jayaramsatya <sajayaram@juniper.net>
2017-07-12 11:11:15 +0530

    Added code for Updating  the v6 & v4 address for vm interface to Vrouter partial-bug: #1687720
    Change-Id: Ia33ff87f2ab25daf867f8e7ca90fdf334d6ac209

--- src/vnsw/agent/vrouter/ksync/interface_ksync.cc
+++ src/vnsw/agent/vrouter/ksync/interface_ksync.cc
@@ -48,7 +48,8 @@ InterfaceKSyncEntry::InterfaceKSyncEntry(InterfaceKSyncObject *obj,
     has_service_vlan_(entry->has_service_vlan_),
     interface_id_(entry->interface_id_),
     interface_name_(entry->interface_name_),
-    ip_(entry->ip_), hc_active_(false), ipv4_active_(false),
+    ip_(entry->ip_), primary_ip6_(entry->primary_ip6_),
+    hc_active_(false), ipv4_active_(false),
     layer3_forwarding_(entry->layer3_forwarding_),
     ksync_obj_(obj), l2_active_(false),
     metadata_l2_active_(entry->metadata_l2_active_),
@@ -127,9 +128,8 @@ InterfaceKSyncEntry::InterfaceKSyncEntry(InterfaceKSyncObject *obj,
     if (type_ == Interface::VM_INTERFACE) {
         const VmInterface *vmitf =
             static_cast<const VmInterface *>(intf);
-        if (vmitf->do_dhcp_relay()) {
-            ip_ = vmitf->primary_ip_addr().to_ulong();
-        }
+        ip_ = vmitf->primary_ip_addr().to_ulong();
+        primary_ip6_ = vmitf->primary_ip6_addr();
         network_id_ = vmitf->vxlan_id();
         rx_vlan_id_ = vmitf->rx_vlan_id();
         tx_vlan_id_ = vmitf->tx_vlan_id();
@@ -232,16 +232,13 @@ bool InterfaceKSyncEntry::Sync(DBEntry *e) {
             ret = true;
         }
 
-        if (vm_port->do_dhcp_relay()) {
-            if (ip_ != vm_port->primary_ip_addr().to_ulong()) {
-                ip_ = vm_port->primary_ip_addr().to_ulong();
-                ret = true;
-            }
-        } else {
-            if (ip_) {
-                ip_ = 0;
-                ret = true;
-            }
+        if (ip_ != vm_port->primary_ip_addr().to_ulong()) {
+            ip_ = vm_port->primary_ip_addr().to_ulong();
+            ret = true;
+        }
+        if (primary_ip6_ != vm_port->primary_ip6_addr()) {
+            primary_ip6_ = vm_port->primary_ip6_addr();
+            ret = true;
         }
         if (layer3_forwarding_ != vm_port->layer3_forwarding()) {
             layer3_forwarding_ = vm_port->layer3_forwarding();
@@ -624,6 +621,12 @@ int InterfaceKSyncEntry::Encode(sandesh_op::type op, char *buf, int buf_len) {
                 fat_flow_list.push_back(it->protocol << 16 | it->port);
             }
             encoder.set_vifr_fat_flow_protocol_port(fat_flow_list);
+        }
+        if (!primary_ip6_.is_unspecified()) {
+            uint64_t data[2];
+            Ip6AddressToU64Array(primary_ip6_, data, 2);
+            encoder.set_vifr_ip6_u(data[0]);
+            encoder.set_vifr_ip6_l(data[1]);
         }
         break;
     }
