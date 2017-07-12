42c6dd16c021415be653e4799138bbb204af2f83
Yuvaraja Mariappan <ymariappan@juniper.net>
2017-07-12 08:01:30 -0700

    Kubernetes ingress https support in contrail
    Added following changes
    1. vnc_ingress is modified to understand the tls info
       from kubernetes and create the loadbalancer with https
       support in contrail
    2. svc_monior is modified to understand the k8s based https
       along with openstack https
    3. opencontrail_vrouter_netns is modifed to understand k8s
       https and openstack https
    
    Change-Id: I0be95b3e8ab1e896b841c4a7af7a6a6e4475c452
    Partial-bug: #1703846

---
+++ src/vnsw/opencontrail-vrouter-netns/opencontrail_vrouter_netns/cert_mgr/cert_manager.py
@@ -0,0 +1,29 @@
+import logging
+import exceptions
+
+try:
+    from barbican_cert_manager import BarbicanCertManager
+except ImportError:
+    pass
+
+try:
+    from kubernetes_cert import KubernetesCert
+except ImportError:
+    pass
+
+class CertManager:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def update_ssl_config(orchestrator, provider, \
+        haproxy_config, auth_conf, dest_dir):
+        if orchestrator == 'openstack':
+            if provider == 'barbican':
+                updated_config = (BarbicanCertManager(auth_conf)). \
+                    update_ssl_config(haproxy_config, dest_dir)
+        elif orchestrator == 'kubernetes':
+            updated_config = (KubernetesCert(auth_conf)). \
+                update_ssl_config(haproxy_config, dest_dir)
+        return updated_config
