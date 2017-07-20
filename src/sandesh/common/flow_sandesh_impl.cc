559007663f77b4b28b999f915554f7448b745e11
mkheni <mkheni@juniper.net>
2017-07-20 12:07:28 -0700

    Adding a new flowlog sandesh of type SessionEndpoint Closes-Bug:#1703055 Implements: blueprint session_collection
    Change-Id: I455e275176ad0ce3907ea648903b3ee45beee115

---
+++ src/sandesh/common/flow_sandesh_impl.cc
@@ -0,0 +1,28 @@
+#include <sandesh/common/flow_types.h>
+
+bool SessionIpPortProtocol::operator < (const SessionIpPortProtocol &rhs) const {
+    if (port < rhs.port) {
+        return true;
+    }
+    else if (port == rhs.port && protocol < rhs.protocol) {
+        return true;
+    }
+    else if (port == rhs.port && protocol == rhs.protocol && ip < rhs.ip) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool SessionIpPort::operator < (const SessionIpPort &rhs) const {
+    if (port < rhs.port) {
+        return true;
+    }
+    else if (port == rhs.port && ip < rhs.ip) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
