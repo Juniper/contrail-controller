cd874a927b9b846484bec8d70fd3d5462ea10750
Suresh Venkata <sureshk@juniper.net>
2017-07-20 13:50:18 -0700

    VNC Cassandra library to accept connections with optional column families
    Description: VNC Cassandra library to accept connections
    with optional column families. Application like ISSU uses
    this library to connect to older version of config node,
    where some column families are not present.
    Also, updated the pool_size for faster recovery.
    
    Change-Id: I3a1405d5b08ecff1522575fd33f9c18667d4cc52
    Closes-Bug: #1704891

--- src/config/api-server/vnc_db.py
+++ src/config/api-server/vnc_db.py
@@ -89,7 +89,7 @@ class VncServerCassandraClient(VncCassandraClient):
             generate_url=db_client_mgr.generate_url, reset_config=reset_config,
             credential=cassandra_credential, walk=walk,
             obj_cache_entries=obj_cache_entries,
-            obj_cache_exclude_types=obj_cache_exclude_types)
+            obj_cache_exclude_types=obj_cache_exclude_types, pool_size=20)
     # end __init__
 
     def config_log(self, msg, level):
