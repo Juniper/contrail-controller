e59911a0b69e53c146addc7d7bba9d8d31be2798
Sachin Bansal <sbansal@juniper.net>
2017-07-22 21:34:44 -0700

    Remove legacy multi-tenancy code
    This config option has been replaced with aaa_mode. Keeping
    it around causes confusion. Remove the references to multi
    tenancy in the code except reading the config option. That is
    left for backward compatibility reason.
    
    Change-Id: I7132ec56c5be07a126b10a7712183b7a3ac4312f
    Closes-Bug: 1705846

--- src/config/api-server/vnc_auth_keystone.py
+++ src/config/api-server/vnc_auth_keystone.py
@@ -88,10 +88,6 @@ class AuthPreKeystone(object):
         self.conf = conf
         self.server_mgr = server_mgr
 
-    @property
-    def mt(self):
-        return self.server_mgr.is_multi_tenancy_set()
-
     def path_in_white_list(self, path):
         for pattern in self.server_mgr.white_list:
             if re.search(pattern, path):
@@ -104,8 +100,10 @@ class AuthPreKeystone(object):
             # permit access to white list without requiring a token
             env['HTTP_X_ROLE'] = ''
             app = self.server_mgr.api_bottle
+        elif self.self.server_mgr.is_auth_needed():
+            app = self.app
         else:
-            app = self.app if self.mt else self.server_mgr.api_bottle
+            app = self.server_mgr.api_bottle
 
         get_context().set_proc_time('PRE_KEYSTONE_REQ')
         return app(env, start_response)
@@ -160,7 +158,7 @@ class AuthServiceKeystone(object):
             'admin_port': args.admin_port,
             'max_requests': args.max_requests,
             'region_name': args.region_name,
-            'insecure':args.insecure,
+            'insecure': args.insecure,
             'identity_uri': identity_uri,
         }
         try:
@@ -170,12 +168,12 @@ class AuthServiceKeystone(object):
         except AttributeError:
             pass
         if _kscertbundle:
-           self._conf_info['cafile'] = _kscertbundle
+            self._conf_info['cafile'] = _kscertbundle
         self._server_mgr = server_mgr
         self._auth_method = args.auth
         self._auth_middleware = None
         self._mt_rbac = server_mgr.is_rbac_enabled()
-        self._multi_tenancy = server_mgr.is_multi_tenancy_set()
+        self._auth_needed = server_mgr.is_auth_needed()
         if not self._auth_method:
             return
         if self._auth_method != 'keystone':
@@ -187,7 +185,7 @@ class AuthServiceKeystone(object):
         self._ks_users = {}
 
         # configure memcache if enabled
-        if self._multi_tenancy and 'memcache_servers' in args:
+        if self._auth_needed and 'memcache_servers' in args:
             self._conf_info[
                 'memcached_servers'] = args.memcache_servers.split(',')
             if 'token_cache_time' in args:
@@ -198,7 +196,7 @@ class AuthServiceKeystone(object):
         if not self._auth_method:
             return None
 
-        if not self._multi_tenancy:
+        if not self._auth_needed:
             return None
 
         # keystone middleware is needed for fetching objects
@@ -210,17 +208,12 @@ class AuthServiceKeystone(object):
 
         # open access for troubleshooting
         admin_port = self._conf_info['admin_port']
-        self._local_auth_app = LocalAuth(self._server_mgr.api_bottle, self._conf_info)
-        vnc_greenlets.VncGreenlet("VNC Auth Keystone", self._local_auth_app.start_http_server)
+        self._local_auth_app = LocalAuth(self._server_mgr.api_bottle,
+                                         self._conf_info)
+        vnc_greenlets.VncGreenlet("VNC Auth Keystone",
+                                  self._local_auth_app.start_http_server)
 
-        app = auth_middleware
-
-        # allow multi tenancy to be updated dynamically
-        app = AuthPreKeystone(
-            auth_middleware,
-            None,
-            self._server_mgr)
-
+        app = AuthPreKeystone(auth_middleware, None, self._server_mgr)
         return app
     # end get_middleware_app
 
