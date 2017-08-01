5445f3d500e50604e5774ff898cbadd6f83e1dfb
Adam Wierzbicki <adam.wierzbicki@codilime.com>
2017-08-01 13:27:04 +0200

    Tests for vnc_endpoints
    1) Endpoints add received after service add was received
    2) Endpoints modify -- pod added to service
    3) Endpoints modify -- pod removed from service
    
    Fixed reference to undefined variable in vnc_pod
    
    Change-Id: I42cebcd0d3491407bcd445dd01facb16c71922c3
    Partial-Bug: #1707892

---
+++ src/container/kube-manager/kube_manager/tests/vnc/test_vnc_endpoints.py
@@ -0,0 +1,283 @@
+import uuid
+
+from gevent import monkey
+from mock import MagicMock, patch
+monkey.patch_all()
+
+from kube_manager.common.kube_config_db import NamespaceKM, PodKM
+from kube_manager.tests.vnc import test_case
+from test_common import launch_kube_manager
+from vnc_api.vnc_api import KeyValuePair, KeyValuePairs
+
+
+TEST_NAMESPACE = 'test-namespace'
+TEST_SERVICE_NAME = 'test-service'
+
+
+class VncEndpointsTest(test_case.KMTestCase):
+
+    @classmethod
+    def setUpClass(cls, extra_config_knobs=None):
+        cls.kube_mock = MagicMock()
+        super(VncEndpointsTest, cls).setUpClass(
+            extra_config_knobs=extra_config_knobs)
+
+    @classmethod
+    def launch_kube_manager(cls, *args, **kwargs):
+        with patch('kube_manager.vnc.vnc_endpoints.vnc_kube_config.kube',
+                   return_value=cls.kube_mock):
+            launch_kube_manager(*args, **kwargs)
+
+    @classmethod
+    def spawn_kube_manager(cls, *args, **kwargs):
+        with patch('test_common.launch_kube_manager',
+                   side_effect=cls.launch_kube_manager):
+            super(VncEndpointsTest, cls).spawn_kube_manager(*args, **kwargs)
+
+    def setUp(self, *args, **kwargs):
+        super(VncEndpointsTest, self).setUp(*args, **kwargs)
+        self._add_namespace(namespace_name=TEST_NAMESPACE)
+        self.service_uid = self._add_service(
+            namespace=TEST_NAMESPACE,
+            srv_name=TEST_SERVICE_NAME)
+        self.wait_for_all_tasks_done()
+        self._check_service()
+
+    def _add_namespace(self, namespace_name, isolated=False):
+        ns_uid = str(uuid.uuid4())
+        ns_add_event = self.create_add_namespace_event(namespace_name, ns_uid)
+        if isolated:
+            ns_add_event['object']['metadata']['annotations'] = {
+                'opencontrail.org/isolation': 'true'}
+        NamespaceKM.locate(ns_uid, ns_add_event['object'])
+        self.enqueue_event(ns_add_event)
+        return namespace_name, ns_uid
+
+    def _add_service(self, namespace, srv_name, srv_type='ClusterIP'):
+        srv_meta = {
+            'name': srv_name,
+            'uid': str(uuid.uuid4()),
+            'namespace': namespace
+        }
+        srv_spec = {
+            'type': srv_type,
+            'ports': [{
+                'name': 'http',
+                'protocol': 'TCP',
+                'port': 80,
+            }]
+        }
+        srv_add_event = self.create_event(
+            'Service', srv_spec, srv_meta, 'ADDED')
+        self.enqueue_event(srv_add_event)
+        self.kube_mock.get_resource.return_value = {'metadata': srv_meta}
+        return srv_meta['uid']
+
+    def _check_service(self):
+        # Assert proper creation of loadbalancer, listener, and pool
+
+        lb = self._vnc_lib.loadbalancer_read(
+            id=self.service_uid, fields=('loadbalancer_listener_back_refs',))
+        self.assertEqual(len(lb.loadbalancer_listener_back_refs), 1)
+        self.listener_uid = lb.loadbalancer_listener_back_refs[0]['uuid']
+
+        lb_listener = self._vnc_lib.loadbalancer_listener_read(
+            id=self.listener_uid, fields=('loadbalancer_pool_back_refs',))
+        self.assertEqual(len(lb_listener.loadbalancer_pool_back_refs), 1)
+        self.pool_uid = lb_listener.loadbalancer_pool_back_refs[0]['uuid']
+
+        lb_pool = self._vnc_lib.loadbalancer_pool_read(
+            id=self.pool_uid, fields=('loadbalancer-pool-loadbalancer-member',))
+        self.assertIsNone(lb_pool.get_loadbalancer_members())
+
+    def _add_pod(self, pod_name, pod_namespace, pod_status):
+        pod_uid = str(uuid.uuid4())
+        pod_spec = {'nodeName': 'test-node'}
+        pod_meta = {
+            'name': pod_name,
+            'uid': pod_uid,
+            'namespace': pod_namespace,
+            'labels': {}
+        }
+        pod_add_event = self.create_event('Pod', pod_spec, pod_meta, 'ADDED')
+        pod_add_event['object']['status'] = pod_status
+        PodKM.locate(pod_uid, pod_add_event['object'])
+        self.enqueue_event(pod_add_event)
+        return pod_uid
+
+    def _add_endpoints(self, name, namespace, pod_uids):
+        endpoint_uid = str(uuid.uuid4())
+        event = self.create_event(
+            kind='Endpoints',
+            spec={},
+            meta={
+                'name': name,
+                'namespace': namespace,
+                'uid': endpoint_uid
+            },
+            type='ADDED',
+
+        )
+        event['object']['subsets'] = [{
+            'ports': [{
+                'name': 'http',
+                'port': 80,
+                'protocol': 'TCP'
+            }],
+            'addresses': [{
+                'targetRef': {
+                    'kind': 'Pod',
+                    'name': 'test-pod',
+                    'namespace': namespace,
+                    'uid': pod_uid
+                }
+            } for pod_uid in pod_uids]
+        }]
+        self.enqueue_event(event)
+        return event['object']
+
+    def _add_pod_to_endpoints(self, endpoints, namespace, pod_uid):
+        event = {
+            'object': endpoints,
+            'type': 'MODIFIED'
+        }
+        event['object']['subsets'][0]['addresses'].append({
+            'targetRef': {
+                'kind': 'Pod',
+                'name': 'test-pod',
+                'namespace': namespace,
+                'uid': pod_uid
+            }
+        })
+        self.enqueue_event(event)
+        return event['object']
+
+    def _delete_pod_from_endpoints(self, endpoints, pod_uid):
+        event = {
+            'object': endpoints,
+            'type': 'MODIFIED'
+        }
+        event['object']['subsets'][0]['addresses'] = [
+            address for address in endpoints['subsets'][0]['addresses']
+            if address['targetRef']['uid'] != pod_uid]
+
+        self.enqueue_event(event)
+        return event['object']
+
+    def _get_vmi_uid(self, pod_uid):
+        # Assert proper creation of pod and return id of vm interface
+        vm = self._vnc_lib.virtual_machine_read(
+            id=pod_uid, fields=('virtual_machine_interface_back_refs',))
+        self.assertEqual(len(vm.virtual_machine_interface_back_refs), 1)
+        vm_interface = self._vnc_lib.virtual_machine_interface_read(
+            id=vm.virtual_machine_interface_back_refs[0]['uuid'])
+        return vm_interface.uuid
+
+    def _check_lb_members(self, *members):
+        lb_pool = self._vnc_lib.loadbalancer_pool_read(
+            id=self.pool_uid, fields=('loadbalancer-pool-loadbalancer-member',))
+        self.assertEqual(len(lb_pool.get_loadbalancer_members()), len(members))
+        lb_members = [
+            self._vnc_lib.loadbalancer_member_read(id=member['uuid'])
+            for member in lb_pool.loadbalancer_members]
+        member_annotations = [member.annotations for member in lb_members]
+
+        for vm_uid, vmi_uid in members:
+            self.assertIn(
+                KeyValuePairs([
+                    KeyValuePair('vm', vm_uid),
+                    KeyValuePair('vmi', vmi_uid)]),
+                member_annotations)
+
+    def test_endpoints_add(self):
+        pod_uid = self._add_pod(
+            pod_name='test-pod',
+            pod_namespace=TEST_NAMESPACE,
+            pod_status={
+                'hostIP': '192.168.0.1',
+                'phase': 'created'
+            })
+        self.wait_for_all_tasks_done()
+        vmi_uid = self._get_vmi_uid(pod_uid)
+
+        self._add_endpoints(
+            name=TEST_SERVICE_NAME,
+            namespace=TEST_NAMESPACE,
+            pod_uids=[pod_uid])
+        self.wait_for_all_tasks_done()
+
+        self._check_lb_members((pod_uid, vmi_uid))
+
+    def test_endpoints_modify_pod_added_to_service(self):
+        pod1_uid = self._add_pod(
+            pod_name='test-pod',
+            pod_namespace=TEST_NAMESPACE,
+            pod_status={
+                'hostIP': '192.168.0.1',
+                'phase': 'created'
+            })
+        pod2_uid = self._add_pod(
+            pod_name='test-pod2',
+            pod_namespace=TEST_NAMESPACE,
+            pod_status={
+                'hostIP': '192.168.0.2',
+                'phase': 'created'
+            })
+        self.wait_for_all_tasks_done()
+        vmi1_uid = self._get_vmi_uid(pod1_uid)
+        vmi2_uid = self._get_vmi_uid(pod2_uid)
+
+        endpoints = self._add_endpoints(
+            name=TEST_SERVICE_NAME,
+            namespace=TEST_NAMESPACE,
+            pod_uids=[pod1_uid])
+        self.wait_for_all_tasks_done()
+
+        self._check_lb_members((pod1_uid, vmi1_uid))
+
+        self._add_pod_to_endpoints(
+            endpoints=endpoints,
+            namespace=TEST_NAMESPACE,
+            pod_uid=pod2_uid)
+        self.wait_for_all_tasks_done()
+
+        self._check_lb_members(
+            (pod1_uid, vmi1_uid),
+            (pod2_uid, vmi2_uid))
+
+    def test_endpoints_modify_pod_deleted_from_service(self):
+        pod1_uid = self._add_pod(
+            pod_name='test-pod',
+            pod_namespace=TEST_NAMESPACE,
+            pod_status={
+                'hostIP': '192.168.0.1',
+                'phase': 'created'
+            })
+        pod2_uid = self._add_pod(
+            pod_name='test-pod2',
+            pod_namespace=TEST_NAMESPACE,
+            pod_status={
+                'hostIP': '192.168.0.2',
+                'phase': 'created'
+            })
+        self.wait_for_all_tasks_done()
+        vmi1_uid = self._get_vmi_uid(pod1_uid)
+        vmi2_uid = self._get_vmi_uid(pod2_uid)
+
+        endpoints = self._add_endpoints(
+            name=TEST_SERVICE_NAME,
+            namespace=TEST_NAMESPACE,
+            pod_uids=(pod1_uid, pod2_uid))
+        self.wait_for_all_tasks_done()
+
+        self._check_lb_members(
+            (pod1_uid, vmi1_uid),
+            (pod2_uid, vmi2_uid))
+
+        self._delete_pod_from_endpoints(
+            endpoints=endpoints,
+            pod_uid=pod2_uid)
+        self.wait_for_all_tasks_done()
+
+        self._check_lb_members((pod1_uid, vmi1_uid))
+
