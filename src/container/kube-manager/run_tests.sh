94d4bf1a6e2a1ce60175332a3a82a472a5958e76
Maciej Jagiello <maciej.jagiello@codilime.com>
2017-08-09 16:22:46 +0200

    Set concurrency to 1 in kube-manager tests
    Change-Id: Idbc91c4818f2830563049381b7a18fba355b1aa5
    Closes-Bug: 1709637

--- src/container/kube-manager/run_tests.sh
+++ src/container/kube-manager/run_tests.sh
@@ -5,4 +5,4 @@ build_top=${build_top:-$(pwd)/../../../../build/debug}
 tools_path=${tools_path:-$(pwd)/../../../debug/config/common/tests/}
 root_path=${root_path:-$(pwd)}
 
-source ${tools_path}/tools/run_tests.sh
+source ${tools_path}/tools/run_tests.sh --concurrency 1
