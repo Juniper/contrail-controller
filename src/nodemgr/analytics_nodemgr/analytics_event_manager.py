391acc9f84993bc898ff27fbe6dfa0fcebc74640
Sundaresan Rajangam <srajanga@juniper.net>
2017-08-12 21:02:09 -0700

    Add log file for nodemgr
    Presently, log file is not specified in the nodemgr conf file. Hence the
    nodemgr logs are redirected to contrail-xxx-nodemgr-stderr.log. In
    ubuntu 16.04, contrail-xxx-nodemgr-stderr.log doesn't get created and
    nodemgr logs need to be read using journalctl. nodemgr logs should be
    written in contrail-xxx-nodemgr.log like any other contrail service.
    
    Change-Id: Iaadb789fc8e78376c5aa570a1506e280e95693a8
    Partial-Bug: #1708534

--- src/nodemgr/analytics_nodemgr/analytics_event_manager.py
+++ src/nodemgr/analytics_nodemgr/analytics_event_manager.py
@@ -11,7 +11,7 @@ from nodemgr.common.event_manager import EventManager, EventManagerTypeInfo
 from sandesh_common.vns.ttypes import Module
 
 class AnalyticsEventManager(EventManager):
-    def __init__(self, rule_file, unit_names, collector_addr, sandesh_config):
+    def __init__(self, config, rule_file, unit_names):
 
         if os.path.exists('/tmp/supervisord_analytics.sock'):
             supervisor_serverurl = "unix:///tmp/supervisord_analytics.sock"
@@ -22,9 +22,8 @@ class AnalyticsEventManager(EventManager):
             object_table = 'ObjectCollectorInfo',
             supervisor_serverurl = supervisor_serverurl,
             unit_names = unit_names)
-        EventManager.__init__(
-            self, type_info, rule_file, collector_addr, sandesh_global, 
-            sandesh_config)
+        super(AnalyticsEventManager, self).__init__(config, type_info,
+                rule_file, sandesh_global)
     # end __init__
 
 # end class AnaltyicsEventManager
