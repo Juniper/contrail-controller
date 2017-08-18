c650b956b73ab2ded02ed1202cbd3d621d4d931d
Keyur Golani <keyurrgolani@gmail.com>
2017-08-18 10:52:55 -0700

    Making VNC API pip installable
    VNC API is much dependent on the config common code right now. Also we are
    hard coding the version of the generated package. Which makes the package
    static and dependent on other code. Hence, to upload to pip as a package,
    we need to make it independent of the other modules and also automate the
    versioning of the package.
    
    Fixed all pep8 errors in vnc_api module
    
    Change-Id: Idc914c445d9ef18d4c2e2313298bf2c4075375fc
    Partial-Bug: #1711447

--- src/config/common/__init__.py
+++ src/config/common/__init__.py
@@ -4,6 +4,7 @@
 
 import re
 import sys
+from vn_api import utils as vncutils
 
 IP_FABRIC_VN_FQ_NAME = ['default-domain', 'default-project', 'ip-fabric']
 IP_FABRIC_RI_FQ_NAME = IP_FABRIC_VN_FQ_NAME + ['__default__']
@@ -26,30 +27,33 @@ PERMS_RW = 6
 PERMS_RWX = 7
 
 AAA_MODE_DEFAULT_VALUE = 'cloud-admin'
-AAA_MODE_VALID_VALUES = ['no-auth', 'cloud-admin', 'rbac']
+AAA_MODE_VALID_VALUES = vncutils.AAA_MODE_VALID_VALUES
 CLOUD_ADMIN_ROLE = 'admin'
 GLOBAL_READ_ONLY_ROLE = None
 PERMS2_VALID_SHARE_TYPES = ['tenant', 'domain']
 DOMAIN_SHARING_PERMS = PERMS_RW
 
 proto_dict = {
-    'any'    : 0,
-    'icmp'   : 1,
-    'tcp'    : 6,
-    'udp'    : 17,
+    'any': 0,
+    'icmp': 1,
+    'tcp': 6,
+    'udp': 17,
 }
 
 
 RULE_IMPLICIT_ALLOW_UUID = "00000000-0000-0000-0000-100000000001"
 RULE_IMPLICIT_DENY_UUID = "00000000-0000-0000-0000-100000000002"
 
+
 def obj_to_json(obj):
     return dict((k, v) for k, v in obj.__dict__.iteritems())
-#end obj_to_json
+# end obj_to_json
+
 
 def json_to_obj(obj):
     pass
-#end json_to_obj
+# end json_to_obj
+
 
 def ignore_exceptions(func):
     def wrapper(*args, **kwargs):
@@ -60,18 +64,19 @@ def ignore_exceptions(func):
     return wrapper
 # end ignore_exceptions
 
+
 _illegal_unichrs = [(0x00, 0x08), (0x0B, 0x0C), (0x0E, 0x1F),
-                        (0x7F, 0x84), (0x86, 0x9F),
-                        (0xFDD0, 0xFDDF), (0xFFFE, 0xFFFF)]
+                    (0x7F, 0x84), (0x86, 0x9F),
+                    (0xFDD0, 0xFDDF), (0xFFFE, 0xFFFF)]
 if sys.maxunicode >= 0x10000:  # not narrow build
-        _illegal_unichrs.extend([(0x1FFFE, 0x1FFFF), (0x2FFFE, 0x2FFFF),
-                                 (0x3FFFE, 0x3FFFF), (0x4FFFE, 0x4FFFF),
-                                 (0x5FFFE, 0x5FFFF), (0x6FFFE, 0x6FFFF),
-                                 (0x7FFFE, 0x7FFFF), (0x8FFFE, 0x8FFFF),
-                                 (0x9FFFE, 0x9FFFF), (0xAFFFE, 0xAFFFF),
-                                 (0xBFFFE, 0xBFFFF), (0xCFFFE, 0xCFFFF),
-                                 (0xDFFFE, 0xDFFFF), (0xEFFFE, 0xEFFFF),
-                                 (0xFFFFE, 0xFFFFF), (0x10FFFE, 0x10FFFF)])
+    _illegal_unichrs.extend([(0x1FFFE, 0x1FFFF), (0x2FFFE, 0x2FFFF),
+                             (0x3FFFE, 0x3FFFF), (0x4FFFE, 0x4FFFF),
+                             (0x5FFFE, 0x5FFFF), (0x6FFFE, 0x6FFFF),
+                             (0x7FFFE, 0x7FFFF), (0x8FFFE, 0x8FFFF),
+                             (0x9FFFE, 0x9FFFF), (0xAFFFE, 0xAFFFF),
+                             (0xBFFFE, 0xBFFFF), (0xCFFFE, 0xCFFFF),
+                             (0xDFFFE, 0xDFFFF), (0xEFFFE, 0xEFFFF),
+                             (0xFFFFE, 0xFFFFF), (0x10FFFE, 0x10FFFF)])
 
 _illegal_ranges = ["%s-%s" % (unichr(low), unichr(high))
                    for (low, high) in _illegal_unichrs]
@@ -81,6 +86,7 @@ HEX_ELEM = '[0-9A-Fa-f]'
 UUID_PATTERN = '-'.join([HEX_ELEM + '{8}', HEX_ELEM + '{4}',
                          HEX_ELEM + '{4}', HEX_ELEM + '{4}',
                          HEX_ELEM + '{12}'])
+
 
 def has_role(role, roles):
     """ Check if the a role is contained in a role list
@@ -92,8 +98,10 @@ def has_role(role, roles):
         return False
     return role.lower() in [r.lower() for r in roles]
 
+
 def get_lr_internal_vn_name(uuid):
     return '__contrail_lr_internal_vn_' + uuid + '__'
+
 
 def _obj_serializer_all(obj):
     if hasattr(obj, 'serialize_to_json'):
