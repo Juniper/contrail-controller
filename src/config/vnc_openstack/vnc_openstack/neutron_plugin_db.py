f7e7d208c337be591d4c9512f1711376f420ac5d
Sahil <ssabharwal@juniper.net>
2017-08-24 18:14:24 -0700

    [vnc_openstack]: Fix to raise correct err msg
    Tempest test 'test_network_quota_exceeding' expects Confict exception
    with ['network'] instead of u'virtual_network'. This patch fixes this
    error message.
    
    Change-Id: I41137290ee851746a7ec1aa041fe54cf29ccec21
    Closes-Bug: #1712773

--- src/config/vnc_openstack/vnc_openstack/neutron_plugin_db.py
+++ src/config/vnc_openstack/vnc_openstack/neutron_plugin_db.py
@@ -42,6 +42,10 @@ _IFACE_ROUTE_TABLE_NAME_PREFIX = 'NEUTRON_IFACE_RT'
 _IFACE_ROUTE_TABLE_NAME_PREFIX_REGEX = re.compile(
     '%s_%s_%s' % (_IFACE_ROUTE_TABLE_NAME_PREFIX, UUID_PATTERN, UUID_PATTERN))
 
+# VNC to neutron resource type
+_RESOURCE_VNC_TO_NEUTRON = {'virtual_network':
+                            'network'}
+
 class LocalVncApi(VncApi):
     def __init__(self, api_server_obj, *args, **kwargs):
         if api_server_obj:
@@ -426,6 +430,8 @@ class DBInterface(object):
             self._raise_contrail_exception('BadRequest',
                 resource=resource_type, msg=str(e))
         except OverQuota as e:
+            if resource_type in _RESOURCE_VNC_TO_NEUTRON:
+                resource_type = _RESOURCE_VNC_TO_NEUTRON[resource_type]
             self._raise_contrail_exception('OverQuota',
                 overs=[resource_type], msg=str(e))
         return obj_uuid
