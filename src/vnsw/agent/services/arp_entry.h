d509adb229124fc3145c39b2b4a799582a124815
jayaramsatya <sajayaram@juniper.net>
2017-08-28 20:45:42 +0530

    Make interface as intrusive pointer in the ArpEntry class As the interface could get deleted before grat ARP send message in the Timer expiry case. closes-bug: #1713415 Change-Id: I236d67081fcb59747b06bc96a2e6f8e732545a93

--- src/vnsw/agent/services/arp_entry.h
+++ src/vnsw/agent/services/arp_entry.h
@@ -30,13 +30,13 @@ public:
 
     ArpEntry(boost::asio::io_service &io, ArpHandler *handler,
              ArpKey &key, const VrfEntry *vrf, State state,
-             const Interface *itf);
+              const Interface *itf);
     virtual ~ArpEntry();
 
     const ArpKey &key() const { return key_; }
     State state() const { return state_; }
     const MacAddress &mac_address() const { return mac_address_; }
-    const Interface *interface() const { return interface_; }
+    const Interface *interface() const { return interface_.get(); }
 
     bool HandleArpRequest();
     void HandleArpReply(const MacAddress &);
@@ -64,7 +64,7 @@ private:
     int retry_count_;
     boost::intrusive_ptr<ArpHandler> handler_;
     Timer *arp_timer_;
-    const Interface *interface_;
+    InterfaceConstRef interface_;
     DISALLOW_COPY_AND_ASSIGN(ArpEntry);
 };
 
