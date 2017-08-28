b1415ed6297337512adf365e4f53bfefb9000783
Suresh Venkata <sureshk@juniper.net>
2017-08-28 11:21:56 -0700

    Operations on objects created pre RBAC fail, on enabling RBAC.
    Description: Certain objects created pre RBAC has owner ID not
    same as the id of the project in which those objects are created.
    This is because the project information passed was the service
    tenant. This is causing failure of operations on those objects
    after enbaling RBAC.
    This change will set appropriate tenant ids as the owner.
    
    Change-Id: I7ce9a8080b76b0ce15bd2fecc51b3a8da3903c3d
    Closes-Bug: #1706218

--- src/config/api-server/vnc_perms.py
+++ src/config/api-server/vnc_perms.py
@@ -89,19 +89,15 @@ class VncPermissions(object):
 
     def validate_perms_rbac(self, request, obj_uuid, mode=PERMS_R, obj_owner_for_delete=None, perms2=None):
         err_msg = (403, 'Permission Denied')
-        parent_type = None
 
         # retrieve object and permissions
-        config = self._server_mgr._db_conn.uuid_to_obj_dict(obj_uuid)
         if not perms2:
             try:
+                config = self._server_mgr._db_conn.uuid_to_obj_dict(obj_uuid)
                 perms2 = config.get('prop:perms2', config.get("perms2"))
             except NoIdError:
                 return (True, '')
 
-        if 'parent_type' in config:
-            parent_type = config['parent_type']
-        obj_type = config['type']
         user, roles = self.get_user_roles(request)
         is_admin = has_role(self.cloud_admin_role, roles)
         if is_admin:
@@ -136,8 +132,7 @@ class VncPermissions(object):
 
         # build perms
         mask = 07
-        #For non project paren type being domain objects, don't check for tenant==owner
-        if ((obj_type != 'project' and parent_type == 'domain') or (tenant == owner)):
+        if ((tenant == owner)):
             mask |= 0700
 
         share = perms2['share']
@@ -154,8 +149,7 @@ class VncPermissions(object):
 
         mode_mask = mode | mode << 3 | mode << 6
         ok = (mask & perms & mode_mask)
-        if (ok and obj_owner_for_delete and ((parent_type != 'domain')
-            or (parent_type == 'domain' and obj_type == 'project'))):
+        if (ok and obj_owner_for_delete):
             obj_owner_for_delete = obj_owner_for_delete.replace('-','')
             ok = (tenant == obj_owner_for_delete)
 
@@ -248,10 +242,10 @@ class VncPermissions(object):
             (ok, obj_dict) = self._server_mgr._db_conn.dbe_read(obj_type,
                              obj_uuid, obj_fields=['perms2'])
             obj_owner=obj_dict['perms2']['owner']
-            return self.validate_perms_rbac(request, obj_uuid, PERMS_W,
+            return self.validate_perms_rbac(request, parent_uuid, PERMS_W,
                                             obj_owner_for_delete = obj_owner)
         elif self._auth_needed:
-            return self.validate_perms(request, obj_uuid, PERMS_W)
+            return self.validate_perms(request, parent_uuid, PERMS_W)
         else:
             return (True, '')
     # end check_perms_write
