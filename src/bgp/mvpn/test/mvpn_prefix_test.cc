b7ebe013e31797d3b603895862d7b4c020224c94
Nikhil B <nikhilb@juniper.net>
2017-09-01 14:58:23 +0530

    Adding some more test cases for mvpn prefix
    Earlier only type 3 test cases were added. So, adding test cases for
    other route types
    
    Change-Id: Idb3edd319c5db67caf45114912fe54fd477d2026
    Partial-Bug: #1708403

--- src/bgp/mvpn/test/mvpn_prefix_test.cc
+++ src/bgp/mvpn/test/mvpn_prefix_test.cc
@@ -14,7 +14,121 @@ using std::string;
 class MvpnPrefixTest : public ::testing::Test {
 };
 
-TEST_F(MvpnPrefixTest, BuildPrefix) {
+// Invalid prefix type.
+TEST_F(MvpnPrefixTest, PrefixTypeError) {
+    boost::system::error_code ec;
+    string prefix_str("9-10.1.1.1:65535,9.8.7.6,224.1.2.3,192.168.1.1");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+TEST_F(MvpnPrefixTest, Type1BuildPrefix) {
+    boost::system::error_code ec;
+    RouteDistinguisher rd(RouteDistinguisher::FromString("10.1.1.1:65535"));
+    Ip4Address originator(Ip4Address::from_string("9.8.7.6", ec));
+    MvpnPrefix prefix(MvpnPrefix::IntraASPMSIADRoute, rd, originator);
+    EXPECT_TRUE(prefix.IsValid(prefix.type()));
+    EXPECT_EQ("1-10.1.1.1:65535,9.8.7.6", prefix.ToString());
+    EXPECT_EQ(MvpnPrefix::IntraASPMSIADRoute, prefix.type());
+    EXPECT_EQ("10.1.1.1:65535", prefix.route_distinguisher().ToString());
+    EXPECT_EQ("9.8.7.6", prefix.originator().to_string());
+}
+
+TEST_F(MvpnPrefixTest, Type1ParsePrefix) {
+    string prefix_str("1-10.1.1.1:65535,192.168.1.1");
+    MvpnPrefix prefix(MvpnPrefix::FromString(prefix_str));
+    EXPECT_TRUE(prefix.IsValid(prefix.type()));
+    EXPECT_EQ(MvpnPrefix::IntraASPMSIADRoute, prefix.type());
+    EXPECT_EQ("10.1.1.1:65535", prefix.route_distinguisher().ToString());
+    EXPECT_EQ("192.168.1.1", prefix.originator().to_string());
+}
+
+// No "-" to delineate the prefix type.
+TEST_F(MvpnPrefixTest, Type1Error1) {
+    boost::system::error_code ec;
+    string prefix_str("1:10.1.1.1:65535,192.168.1.1");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// No "," to delineate the rd.
+TEST_F(MvpnPrefixTest, Type1Error2) {
+    boost::system::error_code ec;
+    string prefix_str("1-10.1.1.1:65535:192.168.1.1");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// Bad rd.
+TEST_F(MvpnPrefixTest, Type1Error3) {
+    boost::system::error_code ec;
+    string prefix_str("1-10.1.1.1:65536,192.168.1.1");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// Bad originator address.
+TEST_F(MvpnPrefixTest, Type1Error4) {
+    boost::system::error_code ec;
+    string prefix_str("1-10.1.1.1:65535,192.168.1");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+TEST_F(MvpnPrefixTest, Type2BuildPrefix) {
+    boost::system::error_code ec;
+    RouteDistinguisher rd(RouteDistinguisher::FromString("10.1.1.1:65535"));
+    uint32_t asn = 12345;
+    MvpnPrefix prefix(MvpnPrefix::InterASPMSIADRoute, rd, asn);
+    EXPECT_TRUE(prefix.IsValid(prefix.type()));
+    EXPECT_EQ("2-10.1.1.1:65535,12345", prefix.ToString());
+    EXPECT_EQ(MvpnPrefix::InterASPMSIADRoute, prefix.type());
+    EXPECT_EQ("10.1.1.1:65535", prefix.route_distinguisher().ToString());
+    EXPECT_EQ(12345, prefix.asn());
+}
+
+TEST_F(MvpnPrefixTest, Type2ParsePrefix) {
+    string prefix_str("2-10.1.1.1:65535,23456");
+    MvpnPrefix prefix(MvpnPrefix::FromString(prefix_str));
+    EXPECT_TRUE(prefix.IsValid(prefix.type()));
+    EXPECT_EQ(MvpnPrefix::InterASPMSIADRoute, prefix.type());
+    EXPECT_EQ("10.1.1.1:65535", prefix.route_distinguisher().ToString());
+    EXPECT_EQ(23456, prefix.asn());
+}
+
+// No "-" to delineate the prefix type.
+TEST_F(MvpnPrefixTest, Type2Error1) {
+    boost::system::error_code ec;
+    string prefix_str("2:10.1.1.1:65535,23456");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// No "," to delineate the rd.
+TEST_F(MvpnPrefixTest, Type2Error2) {
+    boost::system::error_code ec;
+    string prefix_str("2-10.1.1.1:65535:23456");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// Bad rd.
+TEST_F(MvpnPrefixTest, Type2Error3) {
+    boost::system::error_code ec;
+    string prefix_str("2-10.1.1.1:65536:23456");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// Bad asn.
+TEST_F(MvpnPrefixTest, Type2Error4) {
+    boost::system::error_code ec;
+    string prefix_str("2-10.1.1.1:65535:12334455623456");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+TEST_F(MvpnPrefixTest, Type3BuildPrefix) {
     boost::system::error_code ec;
     RouteDistinguisher rd(RouteDistinguisher::FromString("10.1.1.1:65535"));
     Ip4Address originator(Ip4Address::from_string("9.8.7.6", ec));
@@ -30,7 +144,7 @@ TEST_F(MvpnPrefixTest, BuildPrefix) {
     EXPECT_EQ("192.168.1.1", prefix.source().to_string());
 }
 
-TEST_F(MvpnPrefixTest, ParsePrefix) {
+TEST_F(MvpnPrefixTest, Type3ParsePrefix) {
     string prefix_str("3-10.1.1.1:65535,9.8.7.6,224.1.2.3,192.168.1.1");
     MvpnPrefix prefix(MvpnPrefix::FromString(prefix_str));
     EXPECT_TRUE(prefix.IsValid(prefix.type()));
@@ -41,23 +155,15 @@ TEST_F(MvpnPrefixTest, ParsePrefix) {
 }
 
 // No "-" to delineate the prefix type.
-TEST_F(MvpnPrefixTest, Error1) {
+TEST_F(MvpnPrefixTest, Type3Error1) {
     boost::system::error_code ec;
-    string prefix_str("2:10.1.1.1:65535:9.8.7.6,224.1.2.3,192.168.1.1");
-    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
-    EXPECT_NE(0, ec.value());
-}
-
-// Invalid prefix type.
-TEST_F(MvpnPrefixTest, Error2) {
-    boost::system::error_code ec;
-    string prefix_str("9-10.1.1.1:65535,9.8.7.6,224.1.2.3,192.168.1.1");
+    string prefix_str("3:10.1.1.1:65535,9.8.7.6,224.1.2.3,192.168.1.1");
     MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
     EXPECT_NE(0, ec.value());
 }
 
 // No "-" to delineate the rd.
-TEST_F(MvpnPrefixTest, Error3) {
+TEST_F(MvpnPrefixTest, Type3Error2) {
     boost::system::error_code ec;
     string prefix_str("3-10.1.1.1:65535:9.8.7.6,224.1.2.3,192.168.1.1");
     MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
@@ -65,31 +171,15 @@ TEST_F(MvpnPrefixTest, Error3) {
 }
 
 // Bad rd.
-TEST_F(MvpnPrefixTest, Error4) {
+TEST_F(MvpnPrefixTest, Type3Error3) {
     boost::system::error_code ec;
-    string prefix_str("3-10.1.1.1:65536-9.8.7.6,224.1.2.3,192.168.1.1");
-    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
-    EXPECT_NE(0, ec.value());
-}
-
-// No "," to delineate the router-id.
-TEST_F(MvpnPrefixTest, Error5) {
-    boost::system::error_code ec;
-    string prefix_str("3-10.1.1.1:65535-9.8.7.6:224.1.2.3:192.168.1.1");
-    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
-    EXPECT_NE(0, ec.value());
-}
-
-// Bad router-id.
-TEST_F(MvpnPrefixTest, Error6) {
-    boost::system::error_code ec;
-    string prefix_str("3-10.1.1.1:65535,9.8.7,224.1.2.3,192.168.1.1");
+    string prefix_str("3-10.1.1.1:65536,9.8.7.6,224.1.2.3,192.168.1.1");
     MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
     EXPECT_NE(0, ec.value());
 }
 
 // No "," to delineate the group.
-TEST_F(MvpnPrefixTest, Error7) {
+TEST_F(MvpnPrefixTest, Type3Error4) {
     boost::system::error_code ec;
     string prefix_str("3-10.1.1.1:65535,9.8.7.6,224.1.2.3:192.168.1.1");
     MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
@@ -97,17 +187,244 @@ TEST_F(MvpnPrefixTest, Error7) {
 }
 
 // Bad group address.
-TEST_F(MvpnPrefixTest, Error8) {
+TEST_F(MvpnPrefixTest, Type3Error5) {
     boost::system::error_code ec;
     string prefix_str("3-10.1.1.1:65535,9.8.7.6,224.1.2,192.168.1.1");
     MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
     EXPECT_NE(0, ec.value());
 }
 
-// Bad source address.
-TEST_F(MvpnPrefixTest, Error9) {
+// Bad originator address.
+TEST_F(MvpnPrefixTest, Type3Error6) {
     boost::system::error_code ec;
     string prefix_str("3-10.1.1.1:65535,9.8.7.6,224.1.2.3,192.168.1");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+TEST_F(MvpnPrefixTest, Type4BuildPrefix) {
+    boost::system::error_code ec;
+    RouteDistinguisher rd(RouteDistinguisher::FromString("10.1.1.1:65535"));
+    Ip4Address originator(Ip4Address::from_string("9.8.7.6", ec));
+    Ip4Address group(Ip4Address::from_string("224.1.2.3", ec));
+    Ip4Address source(Ip4Address::from_string("192.168.1.1", ec));
+    MvpnPrefix prefix(MvpnPrefix::SPMSIADRoute, rd, originator, group, source);
+    EXPECT_TRUE(prefix.IsValid(prefix.type()));
+    EXPECT_EQ("3-10.1.1.1:65535,192.168.1.1,224.1.2.3,9.8.7.6", prefix.ToString());
+    EXPECT_EQ(MvpnPrefix::SPMSIADRoute, prefix.type());
+    EXPECT_EQ("10.1.1.1:65535", prefix.route_distinguisher().ToString());
+    EXPECT_EQ("9.8.7.6", prefix.originator().to_string());
+    EXPECT_EQ("224.1.2.3", prefix.group().to_string());
+    EXPECT_EQ("192.168.1.1", prefix.source().to_string());
+    Ip4Address type4_originator(Ip4Address::from_string("20.1.1.1"));
+    MvpnPrefix type4_prefix(MvpnPrefix::LeafADRoute, type4_originator);
+    type4_prefix.SetRtKeyFromSPMSIADRoute(prefix);
+    EXPECT_EQ("4-10.1.1.1:65535,192.168.1.1,224.1.2.3,9.8.7.6,20.1.1.1",
+	    type4_prefix.ToString());
+}
+
+TEST_F(MvpnPrefixTest, Type4ParsePrefix) {
+    string prefix_str("4-10.1.1.1:65,192.168.1.1,224.1.2.3,9.8.7.6,20.1.1.1");
+    MvpnPrefix prefix(MvpnPrefix::FromString(prefix_str));
+    EXPECT_TRUE(prefix.IsValid(prefix.type()));
+    EXPECT_EQ(MvpnPrefix::LeafADRoute, prefix.type());
+    EXPECT_EQ("10.1.1.1:65", prefix.route_distinguisher().ToString());
+    EXPECT_EQ("224.1.2.3", prefix.group().to_string());
+    EXPECT_EQ("192.168.1.1", prefix.source().to_string());
+    EXPECT_EQ("20.1.1.1", prefix.originator().to_string());
+}
+
+// No "-" to delineate the prefix type.
+TEST_F(MvpnPrefixTest, Type4Error1) {
+    boost::system::error_code ec;
+    string prefix_str("4:10.1.1.1:65,192.168.1.1,224.1.2.3,9.8.7.6,20.1.1.1");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// No "-" to delineate the rd.
+TEST_F(MvpnPrefixTest, Type4Error3) {
+    boost::system::error_code ec;
+    string prefix_str("4:10.1.1.1:65:192.168.1.1,224.1.2.3,9.8.7.6,20.1.1.1");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// Bad rd.
+TEST_F(MvpnPrefixTest, Type4Error4) {
+    boost::system::error_code ec;
+    string prefix_str("4-10.1.1.1:65536,192.168.1.1,24.1.2.3,9.8.7.6,20.1.1.1");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// No "," to delineate the group.
+TEST_F(MvpnPrefixTest, Type4Error5) {
+    boost::system::error_code ec;
+    string prefix_str("4-10.1.1.1:65535,192.168.1.1:24.1.2.3,9.8.7.6,20.1.1.1");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// Bad group address.
+TEST_F(MvpnPrefixTest, Type4Error6) {
+    boost::system::error_code ec;
+    string prefix_str("4-10.1.1.1:65535,192.168.1.1,24.2.3,9.8.7.6,20.1.1.1");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// Bad source address.
+TEST_F(MvpnPrefixTest, Type4Error7) {
+    boost::system::error_code ec;
+    string prefix_str("4-10.1.1.1:65535,192.1.1,24.1.2.3,9.8.7.6,20.1.1.1");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+TEST_F(MvpnPrefixTest, Type5BuildPrefix) {
+    boost::system::error_code ec;
+    RouteDistinguisher rd(RouteDistinguisher::FromString("10.1.1.1:65535"));
+    Ip4Address group(Ip4Address::from_string("224.1.2.3", ec));
+    Ip4Address source(Ip4Address::from_string("192.168.1.1", ec));
+    MvpnPrefix prefix(MvpnPrefix::SourceActiveADRoute, rd, group, source);
+    EXPECT_TRUE(prefix.IsValid(prefix.type()));
+    EXPECT_EQ("5-10.1.1.1:65535,192.168.1.1,224.1.2.3", prefix.ToString());
+    EXPECT_EQ(MvpnPrefix::SourceActiveADRoute, prefix.type());
+    EXPECT_EQ("10.1.1.1:65535", prefix.route_distinguisher().ToString());
+    EXPECT_EQ("224.1.2.3", prefix.group().to_string());
+    EXPECT_EQ("192.168.1.1", prefix.source().to_string());
+}
+
+TEST_F(MvpnPrefixTest, Type5ParsePrefix) {
+    string prefix_str("5-10.1.1.1:65535,9.8.7.6,224.1.2.3");
+    MvpnPrefix prefix(MvpnPrefix::FromString(prefix_str));
+    EXPECT_TRUE(prefix.IsValid(prefix.type()));
+    EXPECT_EQ(MvpnPrefix::SourceActiveADRoute, prefix.type());
+    EXPECT_EQ("10.1.1.1:65535", prefix.route_distinguisher().ToString());
+    EXPECT_EQ("224.1.2.3", prefix.group().to_string());
+    EXPECT_EQ("9.8.7.6", prefix.source().to_string());
+}
+
+// No "-" to delineate the prefix type.
+TEST_F(MvpnPrefixTest, Type5Error1) {
+    boost::system::error_code ec;
+    string prefix_str("5:10.1.1.1:65535,9.8.7.6,224.1.2.3");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// No "-" to delineate the rd.
+TEST_F(MvpnPrefixTest, Type5Error2) {
+    boost::system::error_code ec;
+    string prefix_str("5:10.1.1.1:65535:9.8.7.6,224.1.2.3");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// Bad rd.
+TEST_F(MvpnPrefixTest, Type5Error3) {
+    boost::system::error_code ec;
+    string prefix_str("5:10.1.1.1:65536,9.8.7.6,224.1.2.3");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// No "," to delineate the group.
+TEST_F(MvpnPrefixTest, Type5Error4) {
+    boost::system::error_code ec;
+    string prefix_str("5:10.1.1.1:65535,9.8.7.6:224.1.2.3");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// Bad group address.
+TEST_F(MvpnPrefixTest, Type5Error5) {
+    boost::system::error_code ec;
+    string prefix_str("5:10.1.1.1:65535,9.8.7.6,224.2.3");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// Bad source address.
+TEST_F(MvpnPrefixTest, Type5Error6) {
+    boost::system::error_code ec;
+    string prefix_str("5:10.1.1.1:65535,9.8.7,224.1.2.3");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// Type6 and Type7 are exactly same formats, so no need of separate tests
+TEST_F(MvpnPrefixTest, Type7BuildPrefix) {
+    boost::system::error_code ec;
+    RouteDistinguisher rd(RouteDistinguisher::FromString("10.1.1.1:65535"));
+    Ip4Address group(Ip4Address::from_string("224.1.2.3", ec));
+    Ip4Address source(Ip4Address::from_string("192.168.1.1", ec));
+    MvpnPrefix prefix(MvpnPrefix::SourceTreeJoinRoute, rd, 1234, group, source);
+    EXPECT_TRUE(prefix.IsValid(prefix.type()));
+    EXPECT_EQ("7-10.1.1.1:65535,1234,192.168.1.1,224.1.2.3", prefix.ToString());
+    EXPECT_EQ(MvpnPrefix::SourceTreeJoinRoute, prefix.type());
+    EXPECT_EQ("10.1.1.1:65535", prefix.route_distinguisher().ToString());
+    EXPECT_EQ("224.1.2.3", prefix.group().to_string());
+    EXPECT_EQ("192.168.1.1", prefix.source().to_string());
+    EXPECT_EQ(1234, prefix.asn());
+}
+
+TEST_F(MvpnPrefixTest, Type7ParsePrefix) {
+    string prefix_str("7-10.1.1.1:65535,12345,9.8.7.6,224.1.2.3");
+    MvpnPrefix prefix(MvpnPrefix::FromString(prefix_str));
+    EXPECT_TRUE(prefix.IsValid(prefix.type()));
+    EXPECT_EQ(MvpnPrefix::SourceTreeJoinRoute, prefix.type());
+    EXPECT_EQ("10.1.1.1:65535", prefix.route_distinguisher().ToString());
+    EXPECT_EQ("224.1.2.3", prefix.group().to_string());
+    EXPECT_EQ("9.8.7.6", prefix.source().to_string());
+    EXPECT_EQ(12345, prefix.asn());
+}
+
+// No "-" to delineate the prefix type.
+TEST_F(MvpnPrefixTest, Type7Error1) {
+    boost::system::error_code ec;
+    string prefix_str("7:10.1.1.1:65535,12345,9.8.7.6,224.1.2.3");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// No "-" to delineate the rd.
+TEST_F(MvpnPrefixTest, Type7Error2) {
+    boost::system::error_code ec;
+    string prefix_str("7:10.1.1.1:65535:12345,9.8.7.6,224.1.2.3");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// Bad rd.
+TEST_F(MvpnPrefixTest, Type7Error3) {
+    boost::system::error_code ec;
+    string prefix_str("7:10.1.1.1:65536,12345,9.8.7.6,224.1.2.3");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// No "," to delineate the group.
+TEST_F(MvpnPrefixTest, Type7Error4) {
+    boost::system::error_code ec;
+    string prefix_str("7:10.1.1.1:65535,12345,9.8.7.6:224.1.2.3");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// Bad group address.
+TEST_F(MvpnPrefixTest, Type7Error5) {
+    boost::system::error_code ec;
+    string prefix_str("7:10.1.1.1:65535,12345,9.8.7.6,224.1.3");
+    MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
+    EXPECT_NE(0, ec.value());
+}
+
+// Bad source address.
+TEST_F(MvpnPrefixTest, Type7Error6) {
+    boost::system::error_code ec;
+    string prefix_str("7:10.1.1.1:65535,12345,9.7.6,224.1.2.3");
     MvpnPrefix prefix = MvpnPrefix::FromString(prefix_str, &ec);
     EXPECT_NE(0, ec.value());
 }
