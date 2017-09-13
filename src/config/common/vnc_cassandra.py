9b9a146d24e664917fdeb5dbb80f8c685f0ca4bd
Suresh Venkata <sureshk@juniper.net>
2017-09-13 15:52:30 -0700

    Sanity bug fixes in cassandra cache and keystone authentication
    Description: This commit has two fixes.
    
    1) For calls made through api-lib, exclude_back_refs is true,
    id_perms timestamp is used to cassandra cache has to be refresshed.
    update_latest_col_ts doesn't update id_perms timestamp, when
    symmetric refs are updated. Changed to call update_last_modified
    does update id_perms timestamp as well as latest_col_ts.
    
    2) keystone authentication when failed in neutron plugin code,
    will return empty auth_hdrs. Because empty auth_hdrs are returned,
    HTTP_X_ROLES were missing when the execution comes to RBAC.
    When authentiation fails, auth headers are set to None, and this
    will raise AuthFailed exception in plugin code, which client may
    use to retry with a valid token.
    
    Change-Id: I46827370bba90bbad1b36843474f636e3cf72f77
    Closes-Bug: #1716311
    Closes-Bug: #1716573

--- src/config/common/vnc_cassandra.py
+++ src/config/common/vnc_cassandra.py
@@ -358,7 +358,7 @@ class VncCassandraClient(object):
         if obj_type == ref_obj_type:
             bch.insert(ref_uuid, {'ref:%s:%s' %
                                   (obj_type, obj_uuid): json.dumps(ref_data)})
-            self.update_latest_col_ts(bch, ref_uuid)
+            self.update_last_modified(bch, obj_type, ref_uuid)
             symmetric_ref_updates = [ref_uuid]
         else:
             bch.insert(ref_uuid, {'backref:%s:%s' %
@@ -389,7 +389,7 @@ class VncCassandraClient(object):
             if obj_type == ref_obj_type:
                 bch.remove(old_ref_uuid, columns=[
                            'ref:%s:%s' % (obj_type, obj_uuid)])
-                self.update_latest_col_ts(bch, old_ref_uuid)
+                self.update_last_modified(bch, obj_type, old_ref_uuid)
                 symmetric_ref_updates = [old_ref_uuid]
             else:
                 bch.remove(old_ref_uuid, columns=[
@@ -402,7 +402,7 @@ class VncCassandraClient(object):
             if obj_type == ref_obj_type:
                 bch.insert(old_ref_uuid, {'ref:%s:%s' % (obj_type, obj_uuid):
                                           json.dumps(new_ref_data)})
-                self.update_latest_col_ts(bch, old_ref_uuid)
+                self.update_last_modified(bch, obj_type, old_ref_uuid)
                 symmetric_ref_updates = [old_ref_uuid]
             else:
                 bch.insert(old_ref_uuid, {'backref:%s:%s' %
@@ -431,7 +431,7 @@ class VncCassandraClient(object):
         if obj_type == ref_obj_type:
             bch.remove(ref_uuid, columns=[
                        'ref:%s:%s' % (obj_type, obj_uuid)])
-            self.update_latest_col_ts(bch, ref_uuid)
+            self.update_last_modified(bch, obj_type, ref_uuid)
             symmetric_ref_updates = [ref_uuid]
         else:
             bch.remove(ref_uuid, columns=[
