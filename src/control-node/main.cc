03e56815604d098756f1c6a47bd15e0ed47ca324
Nipa Kumar <nipak@juniper.net>
2017-09-20 00:38:08 -0700

    Add support to listen on an ip-address for tcp.
    o Support in tcp library to bind on an ip-address
    o BgpSpeaker to bind to configured ip.
    o Relevant tests added.
    
    Change-Id: I946d0addd9968452adeb460ae32e0a9f62909e09
    Closes-Bug: #1714411

--- src/control-node/main.cc
+++ src/control-node/main.cc
@@ -63,6 +63,7 @@ using process::ProcessState;
 using process::ConnectionType;
 using process::ConnectionTypeName;
 using process::g_process_info_constants;
+using boost::system::error_code;
 
 static EventManager evm;
 
@@ -385,12 +386,23 @@ int main(int argc, char *argv[]) {
     BgpConfigParser parser(&config_db);
     parser.Parse(FileRead(options.bgp_config_file().c_str()));
 
+    error_code ec;
+    IpAddress BgpIpAddress = address::from_string(options.host_ip(), ec);
+    if (ec) {
+        LOG(ERROR, "Bgp IP Address:" <<  options.host_ip() <<
+                   " conversion error:" << ec.message());
+        exit(1);
+    }
+
     // TODO:  Initialize throws an exception (via boost) in case the
     // user does not have permissions to bind to the port.
     bgp_server->rtarget_group_mgr()->Initialize();
-    LOG(DEBUG, "Starting Bgp Server at port " << options.bgp_port());
-    if (!bgp_server->session_manager()->Initialize(options.bgp_port()))
+    LOG(DEBUG, "Starting Bgp Server at " <<
+                options.host_ip() << ":" << options.bgp_port());
+    if (!bgp_server->session_manager()->Initialize(options.bgp_port(),
+                                                   BgpIpAddress)) {
         exit(1);
+    }
 
     // Create Xmpp Server.
     XmppChannelConfig xmpp_cfg(false);
