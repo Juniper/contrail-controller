f3276594f35e7e185db82e00bd43b148ecf0a277
Manish <manishs@juniper.net>
2017-09-20 12:25:18 +0530

    Agent crash @VrfExport::Notify
    Issue here is that notification is coming for deleted peer.
    There was a check to handle route notify, same has to be present for vrf
    notification as well.
    
    Change-Id: Ib81ffb58ed8c3e0e1fa77dd7d6047aa0c9993c8f
    Closes-bug: #1715061

--- src/vnsw/agent/test/test_util.cc
+++ src/vnsw/agent/test/test_util.cc
@@ -3849,8 +3849,9 @@ void DeleteBgpPeer(Peer *peer) {
 
     if (bgp_peer) {
         channel = bgp_peer->GetAgentXmppChannel();
-        AgentXmppChannel::HandleAgentXmppClientChannelEvent(channel,
-                                                            xmps::NOT_READY);
+        if (channel)
+            AgentXmppChannel::HandleAgentXmppClientChannelEvent(channel,
+                                                                xmps::NOT_READY);
     }
     if (channel) {
         xmpp_channel = static_cast<XmppChannelMock *>
