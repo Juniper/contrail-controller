655b7312e3d1ba40977ac8e3bbfef99009532972
sbalineni <sbalineni@juniper.net>
2017-09-22 15:36:45 -0700

    [DM]:  QFX L2 Leaf - Configure only VN's route target under evpn instance
    - Should not import connected RI's route targets
    - This issue should have been fixed with bug: 1714843
      But, for some reason, did not check condition properly in this change:
      https://review.opencontrail.org/#/c/35287/
    
    Change-Id: Ie957d562c3a2be15727b53b3919ec82cf08f1563
    Closes-Bug: #1719007

--- src/config/device-manager/device_manager/plugins/juniper/qfx/qfx_conf.py
+++ src/config/device-manager/device_manager/plugins/juniper/qfx/qfx_conf.py
@@ -558,7 +558,7 @@ class QfxConf(JuniperConf):
                                                    vn_obj.vn_network_id, 'l3')
                     export_set = copy.copy(ri_obj.export_targets)
                     import_set = copy.copy(ri_obj.import_targets)
-                    if not self.is_spine():
+                    if self.is_spine():
                         for ri2_id in ri_obj.routing_instances:
                             ri2 = RoutingInstanceDM.get(ri2_id)
                             if ri2 is None:
