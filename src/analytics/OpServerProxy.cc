7914565d4c7fdb49f02a0f811ecfbd8034611499
arvindvis <arvindv@juniper.net>
2017-09-22 15:25:23 -0700

    This disables version negotiation check to be issued by the driver to the kafka. As the kafka(0.9.0.1) does not respond to this request. Closes-Bug: 1719036
    Change-Id: If17d5614823583810ad57e97a8ffde87fb877adb

--- src/analytics/OpServerProxy.cc
+++ src/analytics/OpServerProxy.cc
@@ -599,6 +599,7 @@ class OpServerProxy::OpServerImpl {
             conf->set("metadata.broker.list", brokers_, errstr);
             conf->set("event_cb", &k_event_cb, errstr);
             conf->set("dr_cb", &k_dr_cb, errstr);
+            conf->set("api.version.request", "false", errstr);
             producer_.reset(RdKafka::Producer::create(conf, errstr));
             LOG(ERROR, "Kafka new Prod " << errstr);
             if (!producer_) 
