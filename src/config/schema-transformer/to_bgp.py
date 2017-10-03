cb6b0800adb2b81ff7efe6cbc5f5181cc5207fff
Sahil <ssabharwal@juniper.net>
2017-10-03 05:37:02 -0700

    [ST]: Modified to add rp and default RI link
    1. Modified vnc_cfg schema to support linkage between a VN and routing
       policy.
    2. ST reads the routing policy from a VN and link it to VN's default RI.
    
    Change-Id: Ic478d0aa5e430135b35e9776c6a1657c04992270
    Closes-Bug: #1708045

--- src/config/schema-transformer/to_bgp.py
+++ src/config/schema-transformer/to_bgp.py
@@ -75,6 +75,7 @@ class SchemaTransformer(object):
             'virtual_machine_interface': [],
             'route_table': [],
             'bgpvpn': [],
+            'routing_policy': [],
         },
         'virtual_machine': {
             'self': ['service_instance'],
@@ -143,7 +144,7 @@ class SchemaTransformer(object):
             'self': [],
         },
         'routing_policy': {
-            'self': ['service_instance'],
+            'self': ['service_instance', 'virtual_network'],
         },
         'route_aggregate': {
             'self': ['service_instance'],
