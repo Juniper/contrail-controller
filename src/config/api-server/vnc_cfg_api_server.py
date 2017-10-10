adc4a43391b0dbf04ec3c1afd85ba72ceed3da7e
Ignatious Johnson Christopher <ijohnson@juniper.net>
2017-10-10 00:46:36 -0700

    security_group_rule object is modified
    to count quota count using zookeeper counter object.
    
    Change-Id: I491b9f864ac8af6de0da3513227bca94dee42918
    Closes-Bug: 1712775

--- src/config/api-server/vnc_cfg_api_server.py
+++ src/config/api-server/vnc_cfg_api_server.py
@@ -39,6 +39,7 @@ from cStringIO import StringIO
 from cfgm_common import vnc_cgitb
 from cfgm_common import has_role
 from cfgm_common import _obj_serializer_all
+from cfgm_common.utils import _DEFAULT_ZK_COUNTER_PATH_PREFIX
 
 from cfgm_common.uve.vnc_api.ttypes import VncApiLatencyStats, VncApiLatencyStatsLog
 logger = logging.getLogger(__name__)
@@ -556,8 +557,8 @@ class VncApiServer(object):
 
             get_context().set_state('PRE_DBE_CREATE')
             # type-specific hook
-            (ok, result) = r_class.pre_dbe_create(tenant_name, obj_dict,
-                                                  db_conn)
+            (ok, result) = r_class.pre_dbe_create(
+                    tenant_name, obj_dict, db_conn)
             if not ok:
                 return (ok, result)
 
@@ -925,7 +926,8 @@ class VncApiServer(object):
 
             proj_id = r_class.get_project_id_for_resource(read_result, db_conn)
 
-            (ok, del_result) = r_class.pre_dbe_delete(id, read_result, db_conn)
+            (ok, del_result) = r_class.pre_dbe_delete(
+                    id, read_result, db_conn)
             if not ok:
                 return (ok, del_result)
             # Delete default children first
@@ -1293,7 +1295,7 @@ class VncApiServer(object):
         self._db_conn = None
         self._resource_classes = {}
         self._args = None
-        self._path_prefix = "/vnc_api_server_obj_create/"
+        self._path_prefix = _DEFAULT_ZK_COUNTER_PATH_PREFIX
         self.quota_counter = {}
         if not args_str:
             args_str = ' '.join(sys.argv[1:])
