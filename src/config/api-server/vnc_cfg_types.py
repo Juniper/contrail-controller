b143e2aaed06912ae8e3bffa59097e7e71472e2d
Nagendra Maynattamai <npchandran@juniper.net>
2017-11-08 23:04:30 -0800

    Gateway_less_Fwd: ACL should have rule to deny traffic to ip-fabric Closes-Bug: 1724891
    Change-Id: I8025cfee261474582c1d12d362d80f98dbfa5801

--- src/config/api-server/vnc_cfg_types.py
+++ src/config/api-server/vnc_cfg_types.py
@@ -2133,6 +2133,79 @@ class VirtualRouterServer(Resource, VirtualRouter):
 class VirtualNetworkServer(Resource, VirtualNetwork):
 
     @classmethod
+    def _check_is_provider_network_property(cls, obj_dict, db_conn,
+        vn_ref=None, create=False):
+        # no further checks if is_provider_network is not set
+        if 'is_provider_network' not in obj_dict:
+            return (True, '')
+        if create:
+            # must be set as False for non provider VN
+            if obj_dict.get('is_provider_network'):
+                return (False,
+                        'Non provider VN (%s) can not be '
+                        'configured with is_provider_network = True' % (
+                        obj_dict.get('uuid')))
+        else:
+            # retrieve this VN
+            if not vn_ref:
+                ok, vn_ref = cls.dbe_read(db_conn,
+                    'virtual_network',
+                    obj_dict['uuid'],
+                    obj_fields=['is_provider_network'])
+                if not ok:
+                    return (ok, 'Error reading VN (%s)' % obj_dict['uuid'])
+            # compare obj_dict with db and fail
+            # if not same as this is a read-only property
+            if obj_dict.get('is_provider_network') != \
+                vn_ref.get('is_provider_network', False):
+                return (False,
+                    'Update is_provider_network property of VN (%s) '
+                    'is not allowed' % obj_dict.get('uuid'))
+        return (True, '')
+    # end _check_is_provider_network_property
+
+    @classmethod
+    def _check_provider_network(cls, obj_dict, db_conn, vn_ref=None):
+        # no further checks if not linked
+        # to a provider network
+        if not obj_dict.get('virtual_network_refs'):
+            return (True, '')
+        # retrieve this VN
+        if not vn_ref:
+            ok, vn_ref = cls.dbe_read(db_conn,
+                'virtual_network',
+                obj_dict['uuid'],
+                obj_fields=['is_provider_network'])
+            if not ok:
+                return (ok, 'Error reading VN (%s)' % obj_dict['uuid'])
+
+        # retrieve vn_refs of linked VNs
+        uuids = [vn['uuid'] for vn in obj_dict.get('virtual_network_refs')]
+        (ok, provider_vns, _) = db_conn.dbe_list('virtual_network',
+            obj_uuids=uuids,
+            field_names=['is_provider_network'],
+            filters={'is_provider_network': [True]})
+        if not ok:
+            return (ok, 'Error reading VN refs '
+                        '(%s)' % uuids)
+        linked_vns_uuid = ', '.join([vn['uuid'] for vn in provider_vns])
+
+        if vn_ref.get('is_provider_network'):
+            # this is a provider-VN, no linked provider_vns is expected
+            if provider_vns:
+                return (False,
+                    'Provider VN (%s) can not connect to another '
+                    'provider VN (%s)' % (vn_ref.get('uuid'), linked_vns_uuid))
+        else:
+            # this is a non-provider VN, only one provider vn is expected
+            if not (len(provider_vns) == 1 and
+                len(obj_dict.get('virtual_network_refs')) == 1):
+                return (False,
+                    'Non Provider VN (%s) can connect only to one provider VN '
+                    'but (%s)' % (vn_ref.get('uuid'), linked_vns_uuid))
+        return (True, '')
+
+    @classmethod
     def _check_route_targets(cls, obj_dict, db_conn):
         if 'route_target_list' not in obj_dict:
             return (True, '')
@@ -2402,6 +2475,15 @@ class VirtualNetworkServer(Resource, VirtualNetwork):
         if not ok:
             return (False, (400, error))
 
+        (ok, error) = cls._check_is_provider_network_property(obj_dict,
+            db_conn, create=True)
+        if not ok:
+            return (False, (409, error))
+
+        (ok, error) = cls._check_provider_network(obj_dict, db_conn)
+        if not ok:
+            return (False, (409, error))
+
         # Check if network forwarding mode support BGP VPN types
         ok, result = BgpvpnServer.check_network_supports_vpn_type(
             db_conn, obj_dict)
@@ -2467,9 +2549,7 @@ class VirtualNetworkServer(Resource, VirtualNetwork):
 
     @classmethod
     def pre_dbe_update(cls, id, fq_name, obj_dict, db_conn, **kwargs):
-        if ((fq_name == cfgm_common.IP_FABRIC_VN_FQ_NAME) or
-                (fq_name == cfgm_common.LINK_LOCAL_VN_FQ_NAME)):
-            # Ignore ip-fabric subnet updates
+        if ((fq_name == cfgm_common.LINK_LOCAL_VN_FQ_NAME)):
             return True,  ""
 
         # neutron <-> vnc sharing
@@ -2503,6 +2583,16 @@ class VirtualNetworkServer(Resource, VirtualNetwork):
         ok, read_result = cls.dbe_read(db_conn, 'virtual_network', id)
         if not ok:
             return ok, read_result
+
+        (ok, error) = cls._check_is_provider_network_property(obj_dict,
+            db_conn, vn_ref=read_result)
+        if not ok:
+            return (False, (409, error))
+
+        (ok, error) = cls._check_provider_network(obj_dict,
+            db_conn, vn_ref=read_result)
+        if not ok:
+            return (False, (409, error))
 
         # TODO(ethuleau): As we keep the virtual network ID allocation in
         #                 schema and in the vnc API for one release overlap to
