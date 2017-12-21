b87583c705ca367611b84f44fa0e29363d9f36b3
Ashok Singh R <ashoksr@juniper.net>
2017-12-21 12:20:02 +0530

    Support fat-flow at VN level
    vrouter-agent changes to support
    1. Fat-flow at VN level. Parse VN level fat-flow config and apply to its VMIs.
    2. 0 as port number for fat-flow. This will result in ignoring of
       both source and destination ports.
    3. Enhance introspect to display ignore-remote-port configuration
    4. Update UT
    
    Change-Id: I32f6ef5e7ca257e93aff977896e060e7ebdf0784
    Partial-Bug: #1739544

--- src/vnsw/agent/oper/vm_interface_config.cc
+++ src/vnsw/agent/oper/vm_interface_config.cc
@@ -517,8 +517,9 @@ static void BuildFatFlowTable(Agent *agent, VmInterfaceConfigData *data,
     for (FatFlowProtocols::const_iterator it = cfg->fat_flow_protocols().begin();
             it != cfg->fat_flow_protocols().end(); it++) {
         uint16_t protocol = Agent::ProtocolStringToInt(it->protocol);
-        data->fat_flow_list_.list_.insert(VmInterface::FatFlowEntry(protocol,
-                                          it->port));
+        VmInterface::FatFlowEntry entry(protocol, it->port,
+                                        it->ignore_remote_address);
+        data->fat_flow_list_.Insert(&entry);
     }
 }
 
@@ -842,6 +843,14 @@ static void BuildVn(VmInterfaceConfigData *data,
         data->proxy_arp_mode_ = VmInterface::PROXY_ARP_UNRESTRICTED;
     }
 
+    /* Copy fat-flow configured at VN level */
+    for (FatFlowProtocols::const_iterator it = vn->fat_flow_protocols().begin();
+            it != vn->fat_flow_protocols().end(); it++) {
+        uint16_t protocol = Agent::ProtocolStringToInt(it->protocol);
+        VmInterface::FatFlowEntry fentry(protocol, it->port,
+                                         it->ignore_remote_address);
+        data->fat_flow_list_.Insert(&fentry);
+    }
     IFMapAgentTable *table = static_cast<IFMapAgentTable *>(node->table());
     for (DBGraphVertex::adjacency_iterator iter =
             node->begin(table->GetGraph());
