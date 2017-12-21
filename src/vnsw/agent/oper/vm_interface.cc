b9426d590d6714bb98ba311a65e018edc881d871
Ashok Singh R <ashoksr@juniper.net>
2017-12-21 12:20:02 +0530

    Support fat-flow at VN level
    vrouter-agent changes to support
    1. Fat-flow at VN level. Parse VN level fat-flow config and apply to its VMIs.
    2. 0 as port number for fat-flow. This will result in ignoring of
       both source and destination ports.
    3. Enhance introspect to display ignore-remote-port configuration
    4. Update UT
    
    Partial-Bug: #1739544
    (cherry picked from commit 02ad1f51257c8cd07097ff6beda5bdb9e3c54e66)
    
    Change-Id: Ida692d69d1dca7383dd6afae31b819fd4578eb3d

--- src/vnsw/agent/oper/vm_interface.cc
+++ src/vnsw/agent/oper/vm_interface.cc
@@ -692,11 +692,18 @@ bool VmInterface::SecurityGroupEntry::DeleteL3(const Agent *agent,
 // any new states
 /////////////////////////////////////////////////////////////////////////////
 void VmInterface::FatFlowList::Insert(const FatFlowEntry *rhs) {
-    list_.insert(*rhs);
+    std::pair<FatFlowEntrySet::iterator, bool> ret = list_.insert(*rhs);
+    /* Insertion fails when the entry already exists. In this case update
+     * non-key fields
+     */
+    if (ret.second == false) {
+        ret.first->ignore_remote_address = rhs->ignore_remote_address;
+    }
 }
 
 void VmInterface::FatFlowList::Update(const FatFlowEntry *lhs,
                                       const FatFlowEntry *rhs) {
+    lhs->ignore_remote_address = rhs->ignore_remote_address;
 }
 
 void VmInterface::FatFlowList::Remove(FatFlowEntrySet::iterator &it) {
