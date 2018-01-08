cd9356fd4feba554abef07b0a387377a704ccdbd
nitishkrishna <nitishk@juniper.net>
2018-01-08 11:26:18 -0800

    Partial-Bug: #1736197 - Sandesh changes for Ironic Notification Manager
    This code will generate the sandesh ttypes files which are needed for importing and using in Ironic Notification Manager Daemon
    
    Change-Id: I5cf777ebfec8646fb7516aa8c676b32af6b6e837

---
+++ src/config/ironic-notification-manager/setup.py
@@ -0,0 +1,14 @@
+#
+# Copyright (c) 2017 Juniper Networks, Inc. All rights reserved.
+#
+
+from setuptools import setup
+import setuptools
+
+setup(
+    name='ironic-notification-manager',
+    version='0.1dev',
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    long_description="Ironic Node Notifications",
+)
