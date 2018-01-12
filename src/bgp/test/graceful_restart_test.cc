51812f9b917f62b82ce2dc21c4b3ca4c6a5f4046
Ananth Suryanarayana <anantha@juniper.net>
2018-01-12 14:49:41 -0800

    Do not bring up peers when GR process is under progress
    Peer sessions are not allowed to come up when GR is in progress. Once the state
    enters GR_TIMER_WAIT or LLGR_TIMER_WAIT, then the sessions are allowed to come
    up. However, it can so happen that when the peers are about to come up and
    become established, the GR timers fire.. In this case, we do not want the peers
    to come up to keep the design simple.
    
    In such a rare scenario (which was exposed in some of the GR unit tests), flip
    the peer gracefully.
    
    It is to be noted in the normal GR state machine, most of the time is spent
    in GR_TIMER_WAIT/LLGR_TIMER_WAIT states, which in the order of several minutes.
    Hence it is not a common occurance for a peer session to come up at exactly the
    same time as when the GR timers expire.
    
    Tests marked as flaky still need to be monitored before unmarking some of them
    if not all..
    
    Change-Id: I497a3e84ed6222ae5cfc1cb80679c0a962e0ead9
    Partial-Bug: 1733446

--- src/bgp/test/graceful_restart_test.cc
+++ src/bgp/test/graceful_restart_test.cc
@@ -462,7 +462,7 @@ protected:
     string GetEnetPrefix(string inet_prefix) const;
     void ProcessFlippingPeers(int &total_routes, int remaining_instances,
         vector<GRTestParams> &n_flipping_peers);
-    void SetXmppGRHelperMode(string flag);
+    void SetGRHelperMode(string flag);
 
     std::vector<GRTestParams> n_flipped_agents_;
     std::vector<GRTestParams> n_flipped_peers_;
@@ -704,7 +704,7 @@ void GracefulRestartTest::VerifyRoutes(int count) {
     }
 }
 
-void GracefulRestartTest::SetXmppGRHelperMode(string flag) {
+void GracefulRestartTest::SetGRHelperMode(string flag) {
     ostringstream os;
     os << "<config><global-system-config><graceful-restart-parameters>";
     os << "<xmpp-helper-enable>" << flag << "</xmpp-helper-enable>";
@@ -1819,7 +1819,7 @@ void GracefulRestartTest::GracefulRestartTestRun () {
 
     // Disable XMPP GR Helper mode in configuration.
     if (disable_gr_config_in_between_) {
-        SetXmppGRHelperMode("false");
+        SetGRHelperMode("false");
         total_xmpp_routes = 0;
     }
 
