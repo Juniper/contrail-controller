eba4975f1fba96526e2f9da2d0e38f1bec920fe1
Michał Cłapiński <michal.clapinski@codilime.com>
2018-01-15 13:41:18 +0100

    Fix agent build on Windows
    1. Simplified some SConscripts for Windows
    2. GCC only __PRETTY_FUNCTION__ to standard __FUNCTION__
    3. POSIX only random() to standard rand()
    4. u_int8_t to standard uint8_t
    5. Deprecated bzero to standard memset
    6. POSIX only sysconf to platform independent boost solution
    7. and more less significant fixes (mostly for C++11)
    
    Change-Id: I5b6a9849aca56cc997ef850263e9c17162ff103b
    Partial-Bug: #1737177

--- src/bfd/bfd_server.cc
+++ src/bfd/bfd_server.cc
@@ -11,6 +11,7 @@
 #include "bfd/bfd_common.h"
 
 #include <boost/foreach.hpp>
+#include <boost/random/mersenne_twister.hpp>
 
 #include "base/logging.h"
 #include "io/event_manager.h"
@@ -234,8 +235,7 @@ ResultCode Server::SessionManager::RemoveSessionReference(
         const SessionKey &key) {
     Session *session = SessionByKey(key);
     if (session == NULL) {
-        LOG(DEBUG,
-            __PRETTY_FUNCTION__ << " No such session: " << key.to_string());
+        LOG(DEBUG, __FUNCTION__ << " No such session: " << key.to_string());
         return kResultCode_UnknownSession;
     }
 
@@ -280,7 +280,7 @@ Discriminator Server::SessionManager::GenerateUniqueDiscriminator() {
     class DiscriminatorGenerator {
      public:
         DiscriminatorGenerator() {
-            next_ = random()%0x1000000 + 1;
+            next_ = gen()%0x1000000 + 1;
         }
 
         Discriminator Next() {
@@ -289,6 +289,7 @@ Discriminator Server::SessionManager::GenerateUniqueDiscriminator() {
 
      private:
         tbb::atomic<Discriminator> next_;
+        boost::random::mt19937 gen;
     };
 
     static DiscriminatorGenerator generator;
