57a0e8787a27fb7415619eed79ad75a96751973c
Karl Klashinsky <klash@juniper.net>
2018-01-14 20:27:01 -0800

    Use new SetupPyTestSuite*() methods
    Replace copy/paste of "python setup.py run_tests" with calls to new
    methods added to our scons rules.
    
    Main objective is to recover ability to set BUILD_ONLY, but this also
    consolidates our (mis)use of "python setup.py run_tests"
    
    Note that there are still some direct Command() calls that use "python
    setup.py run_tests" that need to be revisited
    
    Change-Id: I8fadd9c896422ed5e107aa7abad7af68353e7620
    Partial-Bug: #1685303

--- src/config/api-server/setup.py
+++ src/config/api-server/setup.py
@@ -32,7 +32,7 @@ setup(
     author='OpenContrail',
     author_email='dev@lists.opencontrail.org',
     url='http://www.opencontrail.org/documentation/api/r4.0/',
-    version='0.1dev0',
+    version='0.1dev',
     classifiers=[
         'Environment :: OpenContrail',
         'Intended Audience :: Information Technology',
