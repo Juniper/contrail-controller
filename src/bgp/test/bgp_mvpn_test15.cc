41275ce286e84b906fec3894e557f81cc17ded73
Ananth Suryanarayana <anantha@juniper.net>
2018-01-16 12:18:15 -0800

    Extend bgp_mvpn_test with multiple paths
    When type-3, type-5 and type-7 routes are added to the table, use different
    BgpMockPeers as source so that multiple paths get generated instead of just one.
    
    Also split bgp_mvpn_test tests into different tests so that they can be run in
    parallel and also to identify flakyness among different tests.
    
    Change-Id: I8efce44af8c03950408541b928f3347e584775dc
    Partia-Bug: 1708403

---
+++ src/bgp/test/bgp_mvpn_test15.cc
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2017 Juniper Networks, Inc. All rights reserved.
+ */
+
+#include "bgp_mvpn_test.cc"
+
+// Receive Type-7 remote join, but no type-5 source-active is received at all.
+TEST_P(BgpMvpnTest, Type3_SPMSI_5) {
+    VerifyInitialState(preconfigure_pm_);
+    // Inject type-7 receiver route with red1 RI vit. There is no source-active
+    // route yet, hence no type-3 s-pmsi should be generated.
+    for (size_t i = 1; i <= instances_set_count_; i++) {
+        for (size_t j = 1; j <= groups_count_; j++) {
+            AddMvpnRoute(master_, prefix7(i,j), "target:127.0.0.1:" +
+                integerToString(red_[i-1]->routing_instance()->index()));
+        }
+    }
+
+    if (!preconfigure_pm_) {
+        VerifyInitialState(false, groups_count_, 0, 1,
+                           instances_set_count_*groups_count_,
+                           groups_count_, 0, 0,
+                           instances_set_count_*groups_count_);
+        VerifyInitialState(true, groups_count_, 0, 0,
+                           instances_set_count_*groups_count_,
+                           groups_count_, 0, 0,
+                           instances_set_count_*groups_count_);
+    } else {
+        // 4 local-ad + 1 remote-join
+        TASK_UTIL_EXPECT_EQ((4+groups_count_)*instances_set_count_ + 1,
+                            master_->Size());
+        for (size_t i = 1; i <= instances_set_count_; i++) {
+            // 1 local-ad + 1 remote-sa(red1)
+            TASK_UTIL_EXPECT_EQ(1+groups_count_, red_[i-1]->Size());
+            TASK_UTIL_EXPECT_EQ(1, blue_[i-1]->Size()); // 1 local
+        }
+    }
+
+    for (size_t i = 1; i <= instances_set_count_; i++) {
+        // 1 local-ad + 1 remote-ad(red1) + 1 remote-ad(blue1)
+        TASK_UTIL_EXPECT_EQ(3, green_[i-1]->Size());
+    }
+
+    for (size_t i = 1; i <= instances_set_count_; i++)
+        for (size_t j = 1; j <= groups_count_; j++)
+            DeleteMvpnRoute(master_, prefix7(i, j));
+
+    // 4 local-ad
+    TASK_UTIL_EXPECT_EQ(4*instances_set_count_ + 1, master_->Size());
+    for (size_t i = 1; i <= instances_set_count_; i++) {
+        TASK_UTIL_EXPECT_EQ(1, red_[i-1]->Size()); // 1 local+ 1 join
+        TASK_UTIL_EXPECT_EQ(1, blue_[i-1]->Size()); // 1 local
+        // 1 local + 1 remote(red1) + 1 remote(blue1)
+        TASK_UTIL_EXPECT_EQ(3, green_[i-1]->Size());
+    }
+}
---
+++ src/bgp/test/bgp_mvpn_test3.cc
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2017 Juniper Networks, Inc. All rights reserved.
+ */
+
+#include "bgp_mvpn_test.cc"
+
+// Reset BGP Identifier and ensure that Type1 route is no longer generated.
+TEST_P(BgpMvpnTest, Type1ADLocalWithIdentifierRemoved) {
+    if (!preconfigure_pm_) {
+        for (size_t i = 1; i <= instances_set_count_; i++) {
+            TASK_UTIL_EXPECT_EQ(static_cast<MvpnRoute *>(NULL),
+                                red_[i-1]->FindType1ADRoute());
+            TASK_UTIL_EXPECT_EQ(static_cast<MvpnRoute *>(NULL),
+                                blue_[i-1]->FindType1ADRoute());
+            TASK_UTIL_EXPECT_EQ(static_cast<MvpnRoute *>(NULL),
+                                green_[i-1]->FindType1ADRoute());
+        }
+    }
+    VerifyInitialState();
+    error_code err;
+    UpdateBgpIdentifier("0.0.0.0");
+    TASK_UTIL_EXPECT_EQ(0, master_->Size());
+
+    for (size_t i = 1; i <= instances_set_count_; i++) {
+        TASK_UTIL_EXPECT_EQ(0, red_[i-1]->Size());
+        TASK_UTIL_EXPECT_EQ(static_cast<MvpnRoute *>(NULL),
+                            red_[i-1]->FindType1ADRoute());
+
+        TASK_UTIL_EXPECT_EQ(0, blue_[i-1]->Size());
+        TASK_UTIL_EXPECT_EQ(static_cast<MvpnRoute *>(NULL),
+                            blue_[i-1]->FindType1ADRoute());
+
+        TASK_UTIL_EXPECT_EQ(0, green_[i-1]->Size()); // 1 green1+1 red1+1 blue1
+        TASK_UTIL_EXPECT_EQ(static_cast<MvpnRoute *>(NULL),
+                            green_[i-1]->FindType1ADRoute());
+
+        // Verify that only green1 has discovered a neighbor from red1.
+        TASK_UTIL_EXPECT_EQ(0, red_[i-1]->manager()->neighbors_count());
+        TASK_UTIL_EXPECT_EQ(0, blue_[i-1]->manager()->neighbors_count());
+        TASK_UTIL_EXPECT_EQ(0, green_[i-1]->manager()->neighbors_count());
+    }
+}
