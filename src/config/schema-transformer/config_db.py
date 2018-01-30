6279317fc9e3b1d8966e370f585ba07fdbf2b9de
Nagendra Maynattamai <npchandran@juniper.net>
2018-01-29 17:29:07 -0800

    Remove references to acl_direction_comp variable Closes-Bug: 1746132
    acl_direction_comp is obsolete. Updating schema-controller
    to not to use the variable
    
    Change-Id: Id32b63f3fd41edfe4bc9af44ea43dfe406edf0d4

--- src/config/schema-transformer/config_db.py
+++ src/config/schema-transformer/config_db.py
@@ -1188,10 +1188,8 @@ class VirtualNetworkST(DBBaseST):
                         sa, sp, da, dp, arule_proto, rule_uuid,
                         prule.action_list, prule.direction, service_ri)
                     result_acl_rule_list.append(acl)
-                    acl_direction_comp = self._manager._args.acl_direction_comp
                     if ((prule.direction == "<>") and
-                            (sa != da or sp != dp) and
-                            (not acl_direction_comp)):
+                            (sa != da or sp != dp)):
                         acl = self.add_acl_rule(
                             da, dp, sa, sp, arule_proto, rule_uuid,
                             prule.action_list, prule.direction, service_ri)
@@ -1208,11 +1206,7 @@ class VirtualNetworkST(DBBaseST):
         action_list = copy.deepcopy(action)
         action_list.set_assign_routing_instance(service_ri)
         match = MatchConditionType(proto, sa, sp, da, dp)
-        acl_direction_comp = self._manager._args.acl_direction_comp
-        if acl_direction_comp:
-            acl = AclRuleType(match, action_list, rule_uuid, direction)
-        else:
-            acl = AclRuleType(match, action_list, rule_uuid)
+        acl = AclRuleType(match, action_list, rule_uuid, direction)
         return acl
 
     def update_pnf_presence(self):
@@ -1343,10 +1337,8 @@ class VirtualNetworkST(DBBaseST):
 
                     acl_list.append(acl)
 
-                    acl_direction_comp = self._manager._args.acl_direction_comp
                     if ((rule.direction == "<>") and
-                            (src_address != dst_address) and
-                            (not acl_direction_comp)):
+                            (src_address != dst_address)):
                         acl = self.add_acl_rule(
                             src_address, PortType(), dst_address, PortType(),
                             "any", rule.get_rule_uuid(),
--- src/config/schema-transformer/to_bgp.py
+++ src/config/schema-transformer/to_bgp.py
@@ -460,7 +460,6 @@ def parse_args(args_str):
         'kombu_ssl_ca_certs': '',
         'zk_timeout': 400,
         'logical_routers_enabled': True,
-        'acl_direction_comp': False,
     }
     defaults.update(SandeshConfig.get_default_options(['DEFAULTS']))
     secopts = {
@@ -584,8 +583,6 @@ def parse_args(args_str):
                         help="Timeout for ZookeeperClient")
     parser.add_argument("--logical_routers_enabled", type=_bool,
                         help="Enabled logical routers")
-    parser.add_argument("--acl_direction_comp", type=_bool,
-                        help="Acl direction compression")
     SandeshConfig.add_parser_arguments(parser)
 
     args = parser.parse_args(remaining_argv)
