d77be582c8eb334d3bd3c09146f328df8e643ea5
nitishkrishna <nitishk@juniper.net>
2018-02-08 09:33:56 -0800

    Partial-Bug: #1736197 - SConsript changes for Ironic Notification Manager
    These are required for RPM building
    
    Change-Id: I95356d92f79e5c958954661f8bb743485610bc79

--- src/config/ironic-notification-manager/setup.py
+++ src/config/ironic-notification-manager/setup.py
@@ -4,11 +4,24 @@
 
 from setuptools import setup
 import setuptools
+import re
+
+def requirements(filename):
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    c = re.compile(r'\s*#.*')
+    return filter(bool, map(lambda y: c.sub('', y).strip(), lines))
 
 setup(
     name='ironic-notification-manager',
     version='0.1dev',
     packages=setuptools.find_packages(),
     zip_safe=False,
-    long_description="Ironic Node Notifications",
+    long_description="Ironic Node Notification Management Daemon",
+    install_requires=requirements('requirements.txt'),
+    entry_points = {
+         'console_scripts' : [
+             'ironic-notification-manager = ironic_notification_manager:server_main',
+         ],
+    },
 )
