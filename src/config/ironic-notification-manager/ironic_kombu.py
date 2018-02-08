2e629c18dc6fc04b582e67b3c11f2ae82a147de1
nitishkrishna <nitishk@juniper.net>
2018-02-08 09:33:56 -0800

    Partial-Bug: #1736197 - SConsript changes for Ironic Notification Manager
    These are required for RPM building
    
    Patch:
    requirements.txt changes
    Console Script changes
    Ironic Authentication changes - Keystone V3 support, using IronicClient
    Service uses contrail-keystone-auth.conf
    PEP8 changes
    Introduced cgitb for detailed Traceback
    Removed INITD Scons code
    Changed Keystone auth processing
    
    Change-Id: I95356d92f79e5c958954661f8bb743485610bc79

--- src/config/ironic-notification-manager/ironic_kombu.py
+++ src/config/ironic-notification-manager/ironic_kombu.py
@@ -26,9 +26,11 @@ from cfgm_common import vnc_greenlets
 '''
 import ssl
 
+
 class IronicKombuClient(object):
 
-    def __init__(self, rabbit_server, rabbit_port, rabbit_user, rabbit_password,
+    def __init__(self, rabbit_server, rabbit_port,
+                 rabbit_user, rabbit_password,
                  notification_level, ironic_notif_mgr_obj, **kwargs):
         self._rabbit_port = rabbit_port
         self._rabbit_user = rabbit_user
@@ -45,10 +47,12 @@ class IronicKombuClient(object):
         # then we will have to modify this function to perhaps take an argument
         # gevent.signal(signal.SIGTERM, self.sigterm_handler)
 
-        self._url = "amqp://%s:%s@%s:%s/" % (self._rabbit_user, self._rabbit_password, 
-                                             self._rabbit_ip, self._rabbit_port)
+        self._url = "amqp://%s:%s@%s:%s/" % (self._rabbit_user,
+                                             self._rabbit_password,
+                                             self._rabbit_ip,
+                                             self._rabbit_port)
         msg = "Initializing RabbitMQ connection, urls %s" % self._url
-        #self._conn_state = ConnectionStatus.INIT
+        # self._conn_state = ConnectionStatus.INIT
         self._conn = kombu.Connection(self._url)
         self._exchange = self._set_up_exchange()
         self._queues = []
@@ -64,7 +68,9 @@ class IronicKombuClient(object):
         ret = []
         rabbit_hosts = re.compile('[,\s]+').split(rabbit_servers)
         for s in rabbit_hosts:
-            match = re.match("(?:(?P<user>.*?)(?::(?P<password>.*?))*@)*(?P<host>.*?)(?::(?P<port>\d+))*$", s)
+            match = re.match("(?:(?P<user>.*?)"
+                             "(?::(?P<password>.*?))"
+                             "*@)*(?P<host>.*?)(?::(?P<port>\d+))*$", s)
             if match:
                 mdict = match.groupdict().copy()
                 for key in ['user', 'password', 'port']:
@@ -77,14 +83,17 @@ class IronicKombuClient(object):
 
     def _set_up_exchange(self, exchange_name=None):
         if exchange_name:
-            exchange = kombu.Exchange(str(exchange_name), type="topic", durable=False)
+            exchange = kombu.Exchange(str(exchange_name),
+                                      type="topic", durable=False)
         else:
             exchange = kombu.Exchange("ironic", type="topic", durable=False)
 
     def _set_up_queues(self, notification_level):
         if notification_level not in ['info', 'debug', 'warning', 'error']:
-            msg = "Unrecongized notification level: " + str(notification_level) + \
-                  "\nPlease enter a valid notification level from: 'info', 'debug', 'warning', 'error'"
+            msg = "Unrecongized notification level: " + \
+                  str(notification_level) + \
+                  "\nPlease enter a valid notification level from: " \
+                  "'info', 'debug', 'warning', 'error'"
             return 0
         sub_queue_names = []
         sub_queues = []
@@ -99,10 +108,13 @@ class IronicKombuClient(object):
             log_levels = ['error']
 
         for level in log_levels:
-            sub_queue_names.append('ironic_versioned_notifications.'+str(level))
+            sub_queue_names.append('ironic_versioned_notifications.' +
+                                   str(level))
 
         for sub_queue_name in sub_queue_names:
-            sub_queues.append(kombu.Queue(str(sub_queue_name), durable=False, exchange=self._exchange, routing_key=str(sub_queue_name)))
+            sub_queues.append(kombu.Queue(str(sub_queue_name),
+                              durable=False, exchange=self._exchange,
+                              routing_key=str(sub_queue_name)))
 
         return sub_queues
 
@@ -113,30 +125,30 @@ class IronicKombuClient(object):
             # the connection and releases the lock, so the other one can
             # just wait on the lock, till it gets released
             self._conn_lock.wait()
-            #if self._conn_state == ConnectionStatus.UP:
+            # if self._conn_state == ConnectionStatus.UP:
             #    return
 
         with self._conn_lock:
             msg = "RabbitMQ connection down"
-            #self._logger(msg, level=SandeshLevel.SYS_NOTICE)
-            #self._update_sandesh_status(ConnectionStatus.DOWN)
-            #self._conn_state = ConnectionStatus.DOWN
+            # self._logger(msg, level=SandeshLevel.SYS_NOTICE)
+            # self._update_sandesh_status(ConnectionStatus.DOWN)
+            # self._conn_state = ConnectionStatus.DOWN
 
             self._conn.close()
 
             self._conn.ensure_connection()
             self._conn.connect()
 
-            #self._update_sandesh_status(ConnectionStatus.UP)
-            #self._conn_state = ConnectionStatus.UP
+            # self._update_sandesh_status(ConnectionStatus.UP)
+            # self._conn_state = ConnectionStatus.UP
             msg = 'RabbitMQ connection ESTABLISHED %s' % repr(self._conn)
-            #self._logger(msg, level=SandeshLevel.SYS_NOTICE)
+            # self._logger(msg, level=SandeshLevel.SYS_NOTICE)
 
             self._channel = self._conn.channel()
             self._consumer = kombu.Consumer(self._conn,
-                                           queues=self._queues,
-                                           callbacks=[self._subscriber],
-                                           accept=["application/json"])
+                                            queues=self._queues,
+                                            callbacks=[self._subscriber],
+                                            accept=["application/json"])
     # end _reconnect
 
     def _connection_watch(self, connected, timeout=10000):
@@ -147,7 +159,8 @@ class IronicKombuClient(object):
             try:
                 self._consumer.consume()
                 self._conn.drain_events()
-            except self._conn.connection_errors + self._conn.channel_errors as e:
+            except self._conn.connection_errors +
+            self._conn.channel_errors as e:
                 self._reconnect()
     # end _connection_watch
 
@@ -157,8 +170,8 @@ class IronicKombuClient(object):
             try:
                 self._connection_watch(connected, timeout)
             except Exception as e:
-                msg = 'Error in rabbitmq drainer greenlet: %s' %(str(e))
-                print(msg) 
+                msg = 'Error in rabbitmq drainer greenlet: %s' % (str(e))
+                print(msg)
                 # avoid 'reconnect()' here as that itself might cause exception
                 connected = False
     # end _connection_watch_forever
@@ -167,17 +180,17 @@ class IronicKombuClient(object):
         return message_dict["event_type"]
 
     def _subscribe_cb(self, body):
-        #print("The body is {}".format(body))
+        # print("The body is {}".format(body))
         message_dict = json.loads(str(body["oslo.message"]))
-        #print("Event recorded: " + str(self._process_message_dict(message_dict)))
-        #print("Message: \n" + str(message_dict))
+        # print("Message: \n" + str(message_dict))
         message_dict_payload = message_dict.pop("payload")
         ironic_object_data = message_dict_payload["ironic_object.data"]
         for k in message_dict:
             ironic_object_data[k] = message_dict[k]
         ironic_node_list = []
         ironic_node_list.append(ironic_object_data)
-        self._ironic_notification_manager.process_ironic_node_info(ironic_node_list)
+        self._ironic_notification_manager.process_ironic_node_info(
+            ironic_node_list)
 
     def _subscriber(self, body, message):
         try:
@@ -192,3 +205,4 @@ class IronicKombuClient(object):
 
     def shutdown(self):
         self._conn.close()
+
