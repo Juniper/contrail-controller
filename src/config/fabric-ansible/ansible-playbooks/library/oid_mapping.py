955d46b4847819c22aa65a948b496f07913ffd71
Tong Jiang <tjiang@juniper.net>
2018-02-08 16:50:24 -0800

    [DM] Migrate the ems playbook related source code from contrail-fabric-ansible repo to contrail-controller/src/config/fabric-ansible/ansible-playbooks. Need to package these files in a tgz to be packaged in debian package. Partial-Bug: #1739812
    Change-Id: I81e718e35606e87652402dec89fe7284a6f76c19

---
+++ src/config/fabric-ansible/ansible-playbooks/library/oid_mapping.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+oid_mapping = {
+    "1.3.6.1.4.1.2636.1.1.1.4.82.8": {"vendor": "juniper", "family": "juniper-qfx", "product": "qfx5100"},
+    "1.3.6.1.4.1.2636.1.1.1.2.29": {"vendor": "juniper", "family": "juniper-mx", "product": "mx240"},
+}
+
