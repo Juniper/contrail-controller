885497e0e44e6718cf3ae6c878e02bb7628b1d92
Tong Jiang <tjiang@juniper.net>
2018-02-12 11:57:59 -0800

    [DM] initial check in for fabric ansible playbooks and related ansible modules/plugins
    Change-Id: I9c6f92092a79971012cf3c1589373dc7fa306e73
    Closes-Bug: #1748975

---
+++ src/config/fabric-ansible/ansible-playbooks/library/job_objectlog.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+#
+# Copyright (c) 2018 Juniper Networks, Inc. All rights reserved.
+#
+
+"""
+This file contains implementation of passing messages and results to callbacks
+configuration manager
+"""
+
+DOCUMENTATION = '''
+---
+
+module: output_adapter
+author: Juniper Networks
+short_description: Private module to pass messages and results to callbacks
+description:
+    - Pass message and results to callbacks
+requirements:
+    -
+options:
+    message:
+        description:
+            - String to send in job objectlog 'message' field
+        required: true
+    status:
+        description:
+            - String to send in job objectlog 'status' field
+        required: true
+    result:
+        description:
+            - JSON formatted string to send in job objectlog 'result' field
+        required: true
+'''
+
+EXAMPLES = '''
+'''
+
+from ansible.module_utils.basic import *
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            message=dict(required=True),
+            status=dict(required=False),
+            result=dict(required=False)
+            ),
+        supports_check_mode=False)
+
+    results = {}
+    results['job_log_message'] = module.params.get('message')
+    results['job_log_status'] = module.params.get('status')
+    results['job_log_result'] = module.params.get('result')
+
+    module.exit_json(**results)
+
+if __name__ == '__main__':
+    main()
