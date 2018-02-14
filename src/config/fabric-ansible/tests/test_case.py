7a03753ea1d6181d15e15220d8e99c80ca6f7739
Tong Jiang <tjiang@juniper.net>
2018-02-14 13:53:59 -0800

    [DM] Initial check-in for the job manager
    Change-Id: I35434465d0e11a7c8ced73348267570eb3998bd8
    Partial-Bug: #1739812

---
+++ src/config/fabric-ansible/tests/test_case.py
@@ -0,0 +1,36 @@
+#
+# Copyright (c) 2018 Juniper Networks, Inc. All rights reserved.
+#
+import sys
+sys.path.append("../common/tests")
+from test_utils import *
+import test_common
+sys.path.insert(0, '../../../../build/production/config/fabric-ansible/')
+sys.path.insert(0, '../../../../build/debug/config/fabric-ansible/job_manager')
+
+from vnc_api.vnc_api import *
+
+
+class JobTestCase(test_common.TestCase):
+
+    @classmethod
+    def setUpClass(cls, extra_config_knobs=None):
+        extra_config = [
+            ('DEFAULTS', 'multi_tenancy', 'False'),
+            ('DEFAULTS', 'aaa_mode', 'no-auth'),
+        ]
+        if extra_config_knobs:
+            extra_config.append(extra_config_knobs)
+        super(JobTestCase, cls).setUpClass(extra_config_knobs=extra_config)
+
+    @classmethod
+    def tearDownClass(cls):
+        super(JobTestCase, cls).tearDownClass()
+
+    def setUp(self, extra_config_knobs=None):
+        super(JobTestCase, self).setUp(extra_config_knobs=extra_config_knobs)
+        return
+
+    def tearDown(self):
+        super(JobTestCase, self).tearDown()
+
