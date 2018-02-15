52b2d63df2e8eef9d0d87ec605bdaebd95388ff2
Nikhil B <nikhilb@juniper.net>
2018-02-15 08:58:35 +0530

    Control changes for remote compute feature
    1. Read sub-cluster link to find the mapping of bgp-router to subcluster
    2. Make changes in xmpp code to use subcluster name to validate any incoming
       xmpp_open message. Close the sessions if values don't match
    3. As of now, not sending any UVE in case of rejection of connection due to
       mismatch of subcluster_name.
    
    Change-Id: I53afd377eae4f07aaad0c92c55b1e395e78fb1b5
    Partial-Bug: #1747826

--- src/bgp/bgp_config_parser.h
+++ src/bgp/bgp_config_parser.h
@@ -36,6 +36,8 @@ private:
                               RequestList *requests) const;
     bool ParseVirtualNetwork(const pugi::xml_node &parent, bool add_change,
                               RequestList *requests) const;
+    bool ParseSubCluster(const pugi::xml_node &parent, bool add_change,
+                         RequestList *requests) const;
     bool ParseRoutingPolicy(const pugi::xml_node &parent, bool add_change,
                               RequestList *requests) const;
     bool ParseRouteAggregate(const pugi::xml_node &parent, bool add_change,
