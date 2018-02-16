33e349afdd4a569da6ccf34c51ef42e51a96ad98
Prasad Miriyala <pmiriyala@juniper.net>
2018-02-17 03:54:42 +0530

    Vrouter to Vrouter encryption functionality. The following are agent changes to support it. https://blueprints.launchpad.net/juniperopenstack/+spec/vrouter-datapath-encryption
    Agent crypt tunnel configuration/processing
    Agent receives configuration for tunnel setup via config global vrouter configuration.
    list of remote ips and encryption mode.
    Agent creates IPsec tunnels as per above configuration remote endpoints.
    Agent processes routes and create NH. While creating Tunnel NH and Fabric Resolv NH,
    agent sets crypt_path_avaiable flag to say tunnel to the NH destination is available.
    Another flag to indicate, VRouter based encryption.
    If VRouter based encryption is enabled and NH crypt flag is also enabled,
    packets will be send to encryption path.
    
    Fixed review comments and send another patch for unittests
    
    Partial Bug: #1749575
    
    Change-Id: I878564754f0af4ebe74cd6592ef6c7708c6dc142

--- src/vnsw/agent/init/agent_param.h
+++ src/vnsw/agent/init/agent_param.h
@@ -188,6 +188,9 @@ public:
     const bool &eth_port_no_arp() const { return eth_port_no_arp_; }
     const std::string &eth_port_encap_type() const { return eth_port_encap_type_; }
 
+    const std::string &crypt_port() const { return crypt_port_; }
+    const bool &crypt_port_no_arp() const { return crypt_port_no_arp_; }
+    const std::string &crypt_port_encap_type() const { return crypt_port_encap_type_; }
 
     const std::vector<std::string> controller_server_list() const {
         return controller_server_list_;
@@ -620,6 +623,8 @@ private:
         (const boost::program_options::variables_map &v);
     void ParseMacLearning
         (const boost::program_options::variables_map &v);
+    void ParseCryptArguments
+        (const boost::program_options::variables_map &v);
 
     boost::program_options::variables_map var_map_;
     boost::program_options::options_description options_;
@@ -641,6 +646,9 @@ private:
     std::string eth_port_;
     bool eth_port_no_arp_;
     std::string eth_port_encap_type_;
+    std::string crypt_port_;
+    bool crypt_port_no_arp_;
+    std::string crypt_port_encap_type_;
     std::vector<std::string> controller_server_list_;
     std::string sub_cluster_id_;
     std::vector<std::string> dns_server_list_;
