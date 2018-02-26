8b59f49fd98479acafdd78f6606086cdbfd5207b
jnpr-supriyas <supriyas@juniper.net>
2018-02-26 22:23:16 +0000

    [DM] Fixing the unit test. Remove sandesh code
    Change-Id: I7bb2a4280b275161a0fa02a73a831ba295632993
    Closes-Bug: #1751925

--- src/config/fabric-ansible/job_manager/job_mgr.py
+++ src/config/fabric-ansible/job_manager/job_mgr.py
@@ -16,9 +16,6 @@ import argparse
 import traceback
 
 from vnc_api.vnc_api import *
-from pysandesh.sandesh_base import *
-from pysandesh.sandesh_logger import *
-from pysandesh.gen_py.sandesh.ttypes import SandeshLevel
 
 from job_handler import JobHandler
 from job_exception import JobException
@@ -32,18 +29,16 @@ POOL_SIZE = 20
 
 
 class JobManager(object):
-    # sandesh instance is maintained as class variable for access from within
-    #  ansible callbacks
-    sandesh = None
 
     def __init__(self, logger, vnc_api, job_input):
-        sandesh = logger._sandesh
         self._logger = logger
         self._vnc_api = vnc_api
         self.job_template_id = None
         self.job_execution_id = None
         self.job_data = None
         self.job_params = dict()
+        self.device_json = None
+        self.auth_token = None        
         self.parse_job_input(job_input)
         self.job_utils = JobUtils(self.job_execution_id, self.job_template_id,
                                   self._logger, self._vnc_api)
@@ -58,7 +53,7 @@ class JobManager(object):
             raise Exception("job_execution_id is missing in the job input")
 
         self.job_template_id = job_input_json['job_template_id']
-        self.job_execution_id = job_input_json['execution_id']
+        self.job_execution_id = job_input_json['job_execution_id']
 
         self.job_data = job_input_json.get('input')
         if self.job_data is None:
@@ -68,16 +63,20 @@ class JobManager(object):
         if self.job_params is None:
             self._logger.debug("Job extra params is not present.")
 
+        self.device_json = job_input_json.get('device_json')
+        if self.device_json is None:
+            self._logger.debug("Device data is not passed from api server.")
+
+        self.auth_token = job_input_json['auth_token']
+
+
     def start_job(self):
         try:
             # create job UVE and log
             msg = "Starting execution for job with template id %s" \
                   " and execution id %s"
             self._logger.debug(msg)
-            timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-            self.job_utils.send_job_execution_uve(timestamp, 0)
-            self.job_utils.send_job_log(msg, JobStatus.STARTING,
-                                        timestamp=timestamp)
+            
             # read the job template object
             job_template = self.job_utils.read_job_template()
 
@@ -85,7 +84,8 @@ class JobManager(object):
             job_handler = JobHandler(self._logger, self._vnc_api,
                                      job_template, self.job_execution_id,
                                      self.job_data, self.job_params,
-                                     self.job_utils)
+                                     self.job_utils, self.device_json,
+                                     self.auth_token)
             result_handler = JobResultHandler(job_template,
                                               self.job_execution_id,
                                               self._logger, self.job_utils)
@@ -94,25 +94,14 @@ class JobManager(object):
             else:
                 self.handle_single_job(job_handler, result_handler)
 
-            # update job uve
             timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-            self.job_utils.send_job_execution_uve(timestamp, 100)
             result_handler.create_job_summary_log(timestamp)
+
         except JobException as e:
-            self.mark_job_failure(e.message, self.job_execution_id)
+            self._logger.error("Job Exception recieved: %s" % e.msg)
+            self._logger.error("%s" % traceback.print_stack())        
         except Exception as e:
             self._logger.error("Error while executing job %s " % repr(e))
-            self._logger.error(traceback.print_stack())
-            self.mark_job_failure(e.message)
-
-    def mark_job_failure(self, msg):
-        try:
-            timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-            self.job_utils.send_job_execution_uve(timestamp, 100)
-            self.job_utils.send_job_log(msg, JobStatus.FAILURE,
-                                        timestamp=timestamp)
-        except Exception as e:
-            self._logger.error("Exception while marking the job as failed.")
             self._logger.error("%s" % traceback.print_stack())
 
     def handle_multi_device_job(self, job_handler, result_handler):
@@ -133,144 +122,6 @@ def parse_args():
     return parser.parse_args()
 
 
-def parse_logger_args(args_str=None):
-    parser = argparse.ArgumentParser(description='Job manager parameters')
-    parser.add_argument('-i', '--job_input', nargs=1,
-                        help='Job manager input json')
-
-    args_str = ''
-    conf_parser = argparse.ArgumentParser(add_help=False)
-
-    conf_parser.add_argument("-c", "--conf_file", action='append',
-                             help="Specify config file", metavar="FILE")
-    parser = argparse.ArgumentParser()
-    args, remaining_argv = conf_parser.parse_known_args(args_str.split())
-
-    defaults = {
-        'cassandra_server_list': '127.0.0.1:9160',
-        'api_server_ip': '127.0.0.1',
-        'api_server_port': '8082',
-        'api_server_use_ssl': False,
-        'collectors': None,
-        'http_server_port': '8111',
-        'log_local': False,
-        'log_level': SandeshLevel.SYS_DEBUG,
-        'log_category': '',
-        'log_file': Sandesh._DEFAULT_LOG_FILE,
-        'use_syslog': False,
-        'syslog_facility': Sandesh._DEFAULT_SYSLOG_FACILITY,
-        'cluster_id': '',
-        'logging_conf': '',
-        'logger_class': None,
-    }
-    defaults.update(SandeshConfig.get_default_options(['DEFAULTS']))
-    secopts = {
-        'use_certs': False,
-        'keyfile': '',
-        'certfile': '',
-        'ca_certs': '',
-    }
-    ksopts = {}
-    cassandraopts = {}
-    sandeshopts = SandeshConfig.get_default_options()
-
-    saved_conf_file = args.conf_file
-    if args.conf_file:
-        config = ConfigParser.SafeConfigParser()
-        config.read(args.conf_file)
-        defaults.update(dict(config.items("DEFAULTS")))
-        if ('SECURITY' in config.sections() and
-                'use_certs' in config.options('SECURITY')):
-            if config.getboolean('SECURITY', 'use_certs'):
-                secopts.update(dict(config.items("SECURITY")))
-        if 'KEYSTONE' in config.sections():
-            ksopts.update(dict(config.items("KEYSTONE")))
-        if 'CASSANDRA' in config.sections():
-            cassandraopts.update(dict(config.items('CASSANDRA')))
-        SandeshConfig.update_options(sandeshopts, config)
-
-    defaults.update(secopts)
-    defaults.update(ksopts)
-    defaults.update(cassandraopts)
-    defaults.update(sandeshopts)
-    parser.set_defaults(**defaults)
-
-    parser.add_argument(
-        "--cassandra_server_list",
-        help="List of cassandra servers in IP Address:Port format",
-        nargs='+')
-    parser.add_argument(
-        "--reset_config", action="store_true",
-        help="Warning! Destroy previous configuration and start clean")
-    parser.add_argument("--api_server_ip",
-                        help="IP address of API server")
-    parser.add_argument("--api_server_port",
-                        help="Port of API server")
-    parser.add_argument("--api_server_use_ssl",
-                        help="Use SSL to connect with API server")
-    parser.add_argument("--collectors",
-                        help="List of VNC collectors in ip:port format",
-                        nargs="+")
-    parser.add_argument("--http_server_port",
-                        help="Port of local HTTP server")
-    parser.add_argument("--log_local", action="store_true",
-                        help="Enable local logging of sandesh messages")
-    parser.add_argument(
-        "--log_level",
-        help="Severity level for local logging of sandesh messages")
-    parser.add_argument(
-        "--log_category",
-        help="Category filter for local logging of sandesh messages")
-    parser.add_argument("--log_file",
-                        help="Filename for the logs to be written to")
-    parser.add_argument("--use_syslog", action="store_true",
-                        help="Use syslog for logging")
-    parser.add_argument("--syslog_facility",
-                        help="Syslog facility to receive log lines")
-    parser.add_argument("--admin_user",
-                        help="Name of keystone admin user")
-    parser.add_argument("--admin_password",
-                        help="Password of keystone admin user")
-    parser.add_argument("--admin_tenant_name",
-                        help="Tenant name for keystone admin user")
-    parser.add_argument("--cluster_id",
-                        help="Used for database keyspace separation")
-    parser.add_argument(
-        "--logging_conf",
-        help=("Optional logging configuration file, default: None"))
-    parser.add_argument(
-        "--logger_class",
-        help=("Optional external logger class, default: None"))
-    parser.add_argument("--cassandra_user",
-                        help="Cassandra user name")
-    parser.add_argument("--cassandra_password",
-                        help="Cassandra password")
-    SandeshConfig.add_parser_arguments(parser)
-    args = parser.parse_args(remaining_argv)
-    if isinstance(args.cassandra_server_list, str):
-        args.cassandra_server_list = args.cassandra_server_list.split()
-    if isinstance(args.collectors, str):
-        args.collectors = args.collectors.split()
-    args.sandesh_config = SandeshConfig.from_parser_arguments(args)
-
-    args.conf_file = saved_conf_file
-    return args
-
-
-def initialize_sandesh_logger():
-    # parse the logger args
-    args = parse_logger_args()
-    args.random_collectors = args.collectors
-    if args.collectors:
-        args.random_collectors = random.sample(args.collectors,
-                                               len(args.collectors))
-
-    # initialize logger
-    logger = JobLogger(args)
-    logger.info("Job Manager process is starting. Sandesh is initialized.")
-    return logger
-
-
 if __name__ == "__main__":
 
     # TODO the logs before the sandesh is initalized should go to some log file
@@ -283,13 +134,13 @@ if __name__ == "__main__":
             sys.exit("Job input data is not passed to job mgr. "
                      "Aborting job ...")
 
-        # TODO need to get the config from api-server
-        logger = initialize_sandesh_logger()
+        # TODO integrate with Sandesh logger
+        logger = JobLogger()
     except Exception as e:
-        print >> sys.stderr, "Failed to initialize sandesh logger due "\
+        print >> sys.stderr, "Failed to initialize logger due "\
                              "to Exception: %s" % traceback.print_stack()
         sys.exit(
-            "Exiting due to sandesh logger initialization error: %s" % repr(e))
+            "Exiting due to logger initialization error: %s" % repr(e))
 
     # initialize _vnc_api instance
     vnc_api = None
@@ -311,4 +162,3 @@ if __name__ == "__main__":
                      "%s" % traceback.print_stack())
         sys.exit("Existing job due to error: %s " % repr(e))
 
-    # TODO disconnect from sandesh
--- src/config/fabric-ansible/job_manager/job_utils.py
+++ src/config/fabric-ansible/job_manager/job_utils.py
@@ -10,8 +10,6 @@ from enum import Enum
 from time import gmtime, strftime
 import traceback
 
-from sandesh.job.ttypes import *
-
 from job_exception import JobException
 
 
@@ -30,46 +28,6 @@ class JobUtils(object):
         self._job_template_id = job_template_id
         self._logger = logger
         self._vnc_api = vnc_api
-
-    def send_job_log(self, message, status, result=None, timestamp=None):
-        try:
-            if timestamp is None:
-                timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-            job_log_entry = JobLogEntry(name=self._job_template_id,
-                                        execution_id=self._job_execution_id,
-                                        timestamp=timestamp, message=message,
-                                        status=status, result=result)
-            job_log = JobLog(log_entry=job_log_entry)
-            job_log.send(sandesh=self._logger._sandesh)
-            self._logger.debug("Created job log for job template: %s, "
-                               "execution id: %s,  status: %s, result: %s,"
-                               " message: %s" % (self._job_template_id,
-                                                 self._job_execution_id,
-                                                 status, result, message))
-        except Exception as e:
-            msg = "Error while creating the job log for job template " \
-                  "%s and execution id %s : %s" % (self._job_template_id,
-                                                   self._job_execution_id,
-                                                   repr(e))
-            raise JobException(msg, self._job_execution_id)
-
-    def send_job_execution_uve(self, timestamp=None,
-                               percentage_completed=None):
-        try:
-            if timestamp is None:
-                timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-            job_exe_data = JobExecution(
-                name=self._job_template_id,
-                execution_id=self._job_execution_id,
-                job_start_ts=timestamp,
-                percentage_completed=percentage_completed)
-            job_uve = UveJobExecution(data=job_exe_data)
-            job_uve.send(sandesh=self._logger._sandesh)
-        except Exception as e:
-            msg = "Error while sending the job execution UVE for job " \
-                  "template %s and execution id %s : %s" % \
-                  (self._job_template_id, self._job_execution_id, repr(e))
-            raise JobException(msg, self._job_execution_id)
 
     def read_job_template(self):
         try:
