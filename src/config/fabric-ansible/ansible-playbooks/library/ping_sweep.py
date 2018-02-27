da4127ae4bcb0f14a4f837d24eedae8477db521a
sjeevaraj <sjeevaraj@juniper.net>
2018-02-27 00:31:30 +0000

    [DM] discover device workflow related files
    Change-Id: I291449b36dd2187fee0c2bde406885fa0d4cba37
    Closes-Bug: #1751952

--- src/config/fabric-ansible/ansible-playbooks/library/ping_sweep.py
+++ src/config/fabric-ansible/ansible-playbooks/library/ping_sweep.py
@@ -9,12 +9,12 @@ This file contains implementation of sending ping requests to the list of
 IP addresses defined by subnet
 """
 
+__metaclass__ = type
+
 from ansible.module_utils.basic import AnsibleModule
 import subprocess
 import ipaddress
 import socket
-__metaclass__ = type
-
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
@@ -22,10 +22,15 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 
 DOCUMENTATION = '''
 ---
+For a given subnet or a list of IPs/hosts, check if they are reachable through ICMP ping.
 '''
 EXAMPLES = '''
+ping_sweep:
+        subnets: List of IP Prefixes
+        hosts: List of Ip addresses / hostnames        
 '''
 RETURN = '''
+Return two lists, rechable hosts and unrechable hosts.
 '''
 
 result = {}
@@ -82,7 +87,7 @@ def main():
     result['reachable_hosts'] = reachable
     result['unreachable_hosts'] = unreachable
     result['job_log_message'] = "Task: PING SWEEP: ping sweep completed." + \
-                                "Reachable hosts are : " + ','.join(reachable)
+        "Reachable hosts are : " + ','.join(reachable)
 
     module.exit_json(**result)
 
