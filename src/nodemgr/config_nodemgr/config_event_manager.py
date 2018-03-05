1249076606674c52e82b0f91c6d8ee298f072568
Andrey Pavlov <andrey.mp@gmail.com>
2018-03-05 18:31:18 +0300

    do refactoring of nodemgr to be able to fix cassandra issues for microservices setup
    issues will be fixed:
    - allow to monitor config DB
    - check cassandra status inside its container
    
    Change-Id: I10b0f5921d4b023fb81bf6a5756201b63d553e0e
    Partial-Bug: #1738520

--- src/nodemgr/config_nodemgr/config_event_manager.py
+++ src/nodemgr/config_nodemgr/config_event_manager.py
@@ -18,16 +18,10 @@ from nodemgr.common.utils import package_installed
 
 class ConfigEventManager(EventManager):
     def __init__(self, config, rule_file, unit_names):
-
         if os.path.exists('/tmp/supervisord_config.sock'):
             supervisor_serverurl = "unix:///tmp/supervisord_config.sock"
         else:
             supervisor_serverurl = "unix:///var/run/supervisord_config.sock"
-
-        self.db = package_installed('contrail-openstack-database')
-        self.config_db = package_installed('contrail-database-common')
-        if not self.db and self.config_db:
-            unit_names.append('contrail-database.service')
 
         type_info = EventManagerTypeInfo(package_name = 'contrail-config',
             module_type = Module.CONFIG_NODE_MGR,
@@ -37,20 +31,20 @@ class ConfigEventManager(EventManager):
                 "cassandra" : "Dcassandra-pidfile=.*cassandra\.pid",
                 "zookeeper" : "org.apache.zookeeper.server.quorum.QuorumPeerMain"
             },
-            sandesh_packages = ['database.sandesh'],
-            unit_names = unit_names)
+            sandesh_packages = ['database.sandesh'])
         super(ConfigEventManager, self).__init__(config, type_info, rule_file,
-                sandesh_global)
+            sandesh_global, unit_names)
         self.hostip = config.hostip
         self.db_port = config.db_port
         self.minimum_diskgb = config.minimum_diskgb
         self.contrail_databases = config.contrail_databases
         self.cassandra_repair_interval = config.cassandra_repair_interval
-        self.cassandra_repair_logdir = config.cassandra_repair_logdir
-        self.cassandra_mgr = CassandraManager(self.cassandra_repair_logdir,
+        self.cassandra_mgr = CassandraManager(config.cassandra_repair_logdir,
                                               'configDb', self.contrail_databases,
                                               self.hostip, self.minimum_diskgb,
-                                              self.db_port)
+                                              self.db_port, self.process_info_manager)
+        if self.cassandra_mgr.can_serve():
+            self.add_unit_name('contrail-database.service')
     # end __init__
 
     def get_failbits_nodespecific_desc(self, fail_status_bits):
@@ -67,7 +61,7 @@ class ConfigEventManager(EventManager):
         return description
 
     def do_periodic_events(self):
-        if not self.db and self.config_db:
+        if self.cassandra_mgr.can_serve():
             self.cassandra_mgr.database_periodic(self)
             # Perform nodetool repair every cassandra_repair_interval hours
             if self.tick_count % (60 * self.cassandra_repair_interval) == 0:
@@ -76,7 +70,7 @@ class ConfigEventManager(EventManager):
     # end do_periodic_events
 
     def process(self):
-        if not self.db and self.config_db:
+        if self.cassandra_mgr.can_serve():
             self.cassandra_mgr.process(self)
     # end process
 # end class ConfigEventManager
