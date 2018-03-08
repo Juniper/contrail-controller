a2825966e3a97354a8b438ed3ca495c14cda263c
Sahil <ssabharwal@juniper.net>
2018-03-08 13:11:53 -0800

    Adding TLS support for config services to cassandra
    This change adds support for TLS encrypted communication between config
    services and Cassandra. Following arguments are added to enable TLS
    support -
    
    'cassandra_use_ssl' - when set to true, enables tls communication
    'cassandra_ca_certs' - path to ca certs to use to perform ssl validation
    
    Both of the above parameters are optional, service connects to Cassandra
    using an uncrypted connection when the above parameters are not
    specified.
    
    TLS for cassandra is enabled for the following config services -
    
    schema-transformer
    api-server
    svc-monitor
    device-manager
    
    Change-Id: I427660a096a3e58e5e74af6ec00f48302e146ce7
    Closes-Bug: #1727500

--- src/config/api-server/vnc_db.py
+++ src/config/api-server/vnc_db.py
@@ -81,7 +81,8 @@ class VncServerCassandraClient(VncCassandraClient):
 
     def __init__(self, db_client_mgr, cass_srv_list, reset_config, db_prefix,
                       cassandra_credential, walk, obj_cache_entries,
-                      obj_cache_exclude_types):
+                      obj_cache_exclude_types, ssl_enabled=False,
+                      ca_certs=None):
         self._db_client_mgr = db_client_mgr
         keyspaces = self._UUID_KEYSPACE.copy()
         keyspaces[self._USERAGENT_KEYSPACE_NAME] = {
@@ -91,7 +92,8 @@ class VncServerCassandraClient(VncCassandraClient):
             generate_url=db_client_mgr.generate_url, reset_config=reset_config,
             credential=cassandra_credential, walk=walk,
             obj_cache_entries=obj_cache_entries,
-            obj_cache_exclude_types=obj_cache_exclude_types, pool_size=20)
+            obj_cache_exclude_types=obj_cache_exclude_types,
+            ssl_enabled=ssl_enabled, ca_certs=ca_certs, pool_size=20)
     # end __init__
 
     def config_log(self, msg, level):
@@ -573,7 +575,8 @@ class VncDbClient(object):
                  reset_config=False, zk_server_ip=None, db_prefix='',
                  db_credential=None, obj_cache_entries=0,
                  obj_cache_exclude_types=None, db_engine='cassandra',
-                 connection=None, **kwargs):
+                 connection=None, cassandra_use_ssl=False,
+                 cassandra_ca_certs=None, **kwargs):
         self._db_engine = db_engine
         self._api_svr_mgr = api_svr_mgr
         self._sandesh = api_svr_mgr._sandesh
@@ -612,7 +615,8 @@ class VncDbClient(object):
                 self._object_db = VncServerCassandraClient(
                     self, db_srv_list, reset_config, db_prefix,
                     db_credential, walk, obj_cache_entries,
-                    obj_cache_exclude_types)
+                    obj_cache_exclude_types, ssl_enabled=cassandra_use_ssl,
+                    ca_certs=cassandra_ca_certs)
 
             self._zk_db.master_election("/api-server-election", db_client_init)
         elif db_engine == 'rdbms':
