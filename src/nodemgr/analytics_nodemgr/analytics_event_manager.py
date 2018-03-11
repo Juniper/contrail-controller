bfc19187f4142bca3fd4f9ffa16972d82a6ff9e9
Andrey Pavlov <andrey.mp@gmail.com>
2018-03-11 18:16:17 +0300

    rework nodemgr to be able to monitor both cassandra-s
    also old unused stuff is removed. and some refactoring
    was made for better logic.
    
    Partial-Bug: #1738520
    
    Change-Id: Ia568fca7797d53f6bd80f38fd4b6a3ea92c95239

--- src/nodemgr/analytics_nodemgr/analytics_event_manager.py
+++ src/nodemgr/analytics_nodemgr/analytics_event_manager.py
@@ -5,8 +5,6 @@
 from gevent import monkey
 monkey.patch_all()
 
-import os
-
 from pysandesh.sandesh_base import sandesh_global
 from sandesh_common.vns.ttypes import Module
 
@@ -14,19 +12,13 @@ from nodemgr.common.event_manager import EventManager, EventManagerTypeInfo
 
 
 class AnalyticsEventManager(EventManager):
-    def __init__(self, config, rule_file, unit_names):
+    def __init__(self, config, unit_names):
 
-        if os.path.exists('/tmp/supervisord_analytics.sock'):
-            supervisor_serverurl = "unix:///tmp/supervisord_analytics.sock"
-        else:
-            supervisor_serverurl = "unix:///var/run/supervisord_analytics.sock"
         type_info = EventManagerTypeInfo(
-            package_name='contrail-analytics',
             module_type=Module.ANALYTICS_NODE_MGR,
-            object_table='ObjectCollectorInfo',
-            supervisor_serverurl=supervisor_serverurl)
-        super(AnalyticsEventManager, self).__init__(config, type_info, rule_file,
-            sandesh_global, unit_names)
+            object_table='ObjectCollectorInfo')
+        super(AnalyticsEventManager, self).__init__(
+            config, type_info, sandesh_global, unit_names)
     # end __init__
 
 # end class AnaltyicsEventManager
--- src/nodemgr/common/utils.py
+++ src/nodemgr/common/utils.py
@@ -3,29 +3,18 @@
 #
 
 import os
-import platform
-import subprocess
 
 
-class NodeMgrUtils(object):
-    @staticmethod
-    def get_package_version(pkg):
-        #retrieve current installed version of pkg
-        cmd = "contrail-version %s | grep %s" % (pkg, pkg)
-        version_line = os.popen(cmd).read()
-        try:
-            version = version_line.split()[1]
-        except IndexError:
-            return None
-        else:
-            return version
-
-
-def is_systemd_based():
-    (pdist, version, name) = platform.dist()
-    if pdist == 'Ubuntu' and version == '16.04':
-        return True
-    return False
+def get_package_version(pkg):
+    #retrieve current installed version of pkg
+    cmd = "contrail-version %s | grep %s" % (pkg, pkg)
+    version_line = os.popen(cmd).read()
+    try:
+        version = version_line.split()[1]
+    except IndexError:
+        return None
+    else:
+        return version
 
 
 def is_running_in_docker():
@@ -36,13 +25,3 @@ def is_running_in_docker():
 def is_running_in_kubepod():
     with open('/proc/1/cgroup', 'rt') as ifh:
         return 'kubepods' in ifh.read()
-
-
-def package_installed(pkg):
-    (pdist, _, _) = platform.dist()
-    if pdist == 'Ubuntu':
-        cmd = "dpkg -l " + pkg
-    else:
-        cmd = "rpm -q " + pkg
-    with open(os.devnull, "w") as fnull:
-        return not subprocess.call(cmd.split(), stdout=fnull, stderr=fnull)
