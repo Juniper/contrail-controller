47ba8c75c363605815c1fc1925e3f028df9d9f3f
Andrey Pavlov <andrey.mp@gmail.com>
2018-03-11 18:16:17 +0300

    rework nodemgr to be able to monitor both cassandra-s
    also old unused stuff is removed. and some refactoring
    was made for better logic.
    
    Partial-Bug: #1738520
    
    Change-Id: Ia568fca7797d53f6bd80f38fd4b6a3ea92c95239

--- src/nodemgr/control_nodemgr/control_event_manager.py
+++ src/nodemgr/control_nodemgr/control_event_manager.py
@@ -2,7 +2,6 @@
 # Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
 #
 
-import os
 from gevent import monkey
 monkey.patch_all()
 
@@ -10,18 +9,14 @@ from pysandesh.sandesh_base import sandesh_global
 from nodemgr.common.event_manager import EventManager, EventManagerTypeInfo
 from sandesh_common.vns.ttypes import Module
 
-class ControlEventManager(EventManager):
-    def __init__(self, config, rule_file, unit_names):
 
-        if os.path.exists('/tmp/supervisord_control.sock'):
-            supervisor_serverurl = "unix:///tmp/supervisord_control.sock"
-        else:
-            supervisor_serverurl = "unix:///var/run/supervisord_control.sock"
-        type_info = EventManagerTypeInfo(package_name = 'contrail-control',
-            module_type = Module.CONTROL_NODE_MGR,
-            object_table = 'ObjectBgpRouter',
-            supervisor_serverurl = supervisor_serverurl)
-        super(ControlEventManager, self).__init__(config, type_info, rule_file,
+class ControlEventManager(EventManager):
+    def __init__(self, config, unit_names):
+
+        type_info = EventManagerTypeInfo(
+            module_type=Module.CONTROL_NODE_MGR,
+            object_table='ObjectBgpRouter')
+        super(ControlEventManager, self).__init__(config, type_info,
             sandesh_global, unit_names)
     # end __init__
 
