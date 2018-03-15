5e3b5339f68309576a0eb8d3eb66e1cb7837f237
jnpr-supriyas <supriyas@juniper.net>
2018-03-15 22:08:43 +0000

    [DM] Sandesh integration initial code
    Change-Id: Ib627fedccfc3eb911bedf9967ce7c5cb335eac47
    Closes-Bug: #1756193

--- src/config/fabric-ansible/job_manager/logger.py
+++ src/config/fabric-ansible/job_manager/logger.py
@@ -5,21 +5,51 @@
 """
 Job manager logger.
 """
+from sandesh_common.vns.ttypes import Module
+from cfgm_common.vnc_logger import ConfigServiceLogger
+from cfgm_common.vnc_greenlets import VncGreenlet
+from pysandesh.sandesh_base import Sandesh
 
-class JobLogger():
 
-    def __init__(self):
-        self.job_log_file = open("/tmp/job_logs.txt", "w+")
-        self._sandesh = None
+class JobLogger(ConfigServiceLogger):
 
-    def close_logger(self):
-        self.job_log_file.close()
+    def __init__(self, args=None, http_server_port=None,
+                 sandesh_instance_id=None):
+        self.sandesh_instance_id = sandesh_instance_id
+        module = Module.FABRIC_ANSIBLE
+        module_pkg = "job_manager"
+        self.context = "job_manager"
+        super(JobLogger, self).__init__(
+            module, module_pkg, args, http_server_port)
 
-    def error(self, message):
-        self.job_log_file.write(message + "\n")
+    def sandesh_init(self, http_server_port=None):
+        """ Init sandesh """
+        self._sandesh = Sandesh()
+        self.redefine_sandesh_handles()
+        if not http_server_port:
+            http_server_port = self._args.http_server_port
 
-    def debug(self, message):
-        self.job_log_file.write(message + "\n")
+        self._instance_id = self.sandesh_instance_id
 
-    def info(self, message):
-        self.job_log_file.write(message + "\n")
+        self._sandesh.init_generator(
+            self._module_name, self._hostname, self._node_type_name,
+            self.sandesh_instance_id, self._args.random_collectors,
+            '%s_context' % self.context, int(http_server_port),
+            ['cfgm_common', '%s.sandesh' % self.module_pkg],
+            logger_class=self._args.logger_class,
+            logger_config_file=self._args.logging_conf,
+            config=self._args.sandesh_config)
+
+        self._sandesh.set_logging_params(
+            enable_local_log=self._args.log_local,
+            category=self._args.log_category,
+            level=self._args.log_level,
+            file=self._args.log_file,
+            enable_syslog=self._args.use_syslog,
+            syslog_facility=self._args.syslog_facility)
+
+        VncGreenlet.register_sandesh_handler()
+
+        self._sandesh.trace_buffer_create(name="MessageBusNotifyTraceBuf",
+                                          size=1000)
+
