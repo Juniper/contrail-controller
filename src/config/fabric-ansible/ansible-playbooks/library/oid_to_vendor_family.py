05d64c1c2b64693e7108c2a5f00ea4f8f171f6a5
sjeevaraj <sjeevaraj@juniper.net>
2018-03-15 16:01:01 +0000

    [DM] change python lib from ipaddress to netaddr
    Change-Id: Ie1e7fcb9e8cca5ee9008484cfad242c73cc90b81
    Closes-Bug: #1755940

--- src/config/fabric-ansible/ansible-playbooks/library/oid_to_vendor_family.py
+++ src/config/fabric-ansible/ansible-playbooks/library/oid_to_vendor_family.py
@@ -42,12 +42,18 @@ oid_mapping = {
     "1.3.6.1.4.1.2636.1.1.1.4.82.8": {"vendor": "juniper",
                                       "family": "juniper-qfx",
                                       "product": "qfx5100"},
+    "1.3.6.1.4.1.2636.1.1.1.4.82.5": {"vendor": "juniper",
+                                      "family": "juniper-qfx",
+                                      "product": "qfx5100"},
     "1.3.6.1.4.1.2636.1.1.1.2.29": {"vendor": "juniper",
                                     "family": "juniper-mx",
                                     "product": "mx240"},
     "1.3.6.1.4.1.2636.1.1.1.2.11": {"vendor": "juniper",
                                     "family": "juniper-mx",
                                     "product": "m10i"},
+    "1.3.6.1.4.1.2636.1.1.1.2.57": {"vendor": "juniper",
+                                    "family": "juniper-mx",
+                                    "product": "mx80"}
 }
 _output = {'job_log_message': '', 'oid_mapping': {}}
 
