31c13e0d4db923fdcd684507a3d9a4310ce94c35
Shivayogi Ugaji <yogi@juniper.net>
2018-03-21 22:11:59 -0700

    move generateds from tools repo to contrail-api-client repo Partial-Bug: #1757504
    Change-Id: I5bee3c8bf229b4332b350bd0d04e52e65dafb260

--- src/api-lib/java/generate-source.sh
+++ src/api-lib/java/generate-source.sh
@@ -7,5 +7,5 @@
 TARGET=build/debug/api-lib/java/src/net/juniper/contrail/api/types
 
 mkdir -p ${TARGET}
-tools/generateds/generateDS.py -f -o ${TARGET} -g java-api src/contrail-api-client/schema/vnc_cfg.xsd
-tools/generateds/generateDS.py -f -o ${TARGET} -g golang-api src/contrail-api-client/schema/vnc_cfg.xsd
+src/contrail-api-client/generateds/generateDS.py -f -o ${TARGET} -g java-api src/contrail-api-client/schema/vnc_cfg.xsd
+src/contrail-api-client/generateds/generateDS.py -f -o ${TARGET} -g golang-api src/contrail-api-client/schema/vnc_cfg.xsd
