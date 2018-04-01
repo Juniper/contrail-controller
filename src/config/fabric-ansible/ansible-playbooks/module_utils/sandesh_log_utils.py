6a8615f6cd4c45560d9725c10c808bf7ead8f9b9
atandon <atandon@juniper.net>
2018-04-01 02:22:32 +0000

    [DM] Ansible modules for prouter and job object log
    Added prouter object log for image and undelay playbook
    
    Change-Id: I04cb705e4dd4ca82ce2ad6e9950ff3a8c0219669
    Closes-Bug: #1759964

---
+++ src/config/fabric-ansible/ansible-playbooks/module_utils/sandesh_log_utils.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+
+
+# Copyright (c) 2018 Juniper Networks, Inc. All rights reserved.
+#
+
+"""
+This file contains utility functions for sending prouter and job objectlogs
+via sandesh
+"""
+
+import uuid
+import time
+import json
+
+from job_manager.job_log_utils import JobLogUtils
+
+
+def send_prouter_object_log(prouter_fqname,
+                            config_args,
+                            job_execution_id,
+                            job_template_fqname,
+                            job_input,
+                            os_version,
+                            serial_num,
+                            onboarding_state):
+    results = {}
+    results['failed'] = False
+
+    try:
+        job_log_util = JobLogUtils(sandesh_instance_id=str(uuid.uuid4()),
+                                   config_args=json.dumps(config_args))
+        job_log_util.send_prouter_object_log(
+            prouter_fqname,
+            job_execution_id,
+            json.dumps(job_input),
+            job_template_fqname,
+            os_version,
+            serial_num,
+            onboarding_state)
+        time.sleep(10)
+    except Exception as ex:
+        results['msg']=str(ex)
+        results['failed']=True
+
+    return results
+
+def send_job_object_log(config_args,
+                        job_execution_id,
+                        job_template_fqname,
+                        message,
+                        status,
+                        result):
+    results={}
+    results['failed']=False
+
+    try:
+        job_log_util=JobLogUtils(sandesh_instance_id = str(uuid.uuid4()),
+                                   config_args = json.dumps(config_args))
+        job_log_util.send_job_log(
+            job_template_fqname,
+            job_execution_id,
+            message,
+            status,
+            result)
+        time.sleep(10)
+    except Exception as ex:
+        results['msg']=str(ex)
+        results['failed']=True
+
+    return results
