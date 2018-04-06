2052e501148f36e88924ae1cf47b5e9cdd3a8d32
Joe Williams <josephw@juniper.net>
2018-04-05 23:25:10 -0700

    [DM] "PEP8 changes"
    Change-Id: I8b6f3fd950a062aad43b57df2f77402a218df507
    Partial-Bug: #1761667

---
+++ src/config/fabric-ansible/ansible-playbooks/module_utils/fabric_utils.py
@@ -0,0 +1,14 @@
+from ansible.module_utils.basic import AnsibleModule
+from job_manager.fabric_display import fabric_ansible_logger
+
+
+class FabricAnsibleModule(AnsibleModule):
+    def __init__(self, argument_spec={}, **kwargs):
+        super(
+            FabricAnsibleModule,
+            self).__init__(
+            argument_spec=argument_spec,
+            **kwargs)
+        self.module_name = self._name
+        self.ctx = self.params.get('job_ctx')
+        self.logger = fabric_ansible_logger(self.module_name)
