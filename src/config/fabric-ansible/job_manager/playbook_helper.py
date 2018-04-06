285653fedea8f96d8d3af9cef77d1ed9097f3bdc
Joe Williams <josephw@juniper.net>
2018-04-05 23:25:10 -0700

    [DM] Fabric ansible debug logging enhancements
    Change-Id: I8b6f3fd950a062aad43b57df2f77402a218df507
    Partial-Bug: #1761667

--- src/config/fabric-ansible/job_manager/playbook_helper.py
+++ src/config/fabric-ansible/job_manager/playbook_helper.py
@@ -11,6 +11,24 @@ import json
 from collections import namedtuple
 import traceback
 import argparse
+import os
+from ansible import constants as C
+
+if C.DEFAULT_VERBOSITY:
+    verbosity = C.DEFAULT_VERBOSITY
+else:
+    verbosity = 0
+
+module_utils_path = os.path.normpath(os.path.dirname(__file__))
+
+if module_utils_path is not None:
+    sys.path.insert(0, module_utils_path)
+    import fabric_display
+    del sys.path[0]
+
+from fabric_display import Display as FabricDisplay
+
+display = FabricDisplay(verbosity)
 
 from ansible.parsing.dataloader import DataLoader
 from ansible.vars.manager import VariableManager
@@ -91,4 +109,3 @@ if __name__ == "__main__":
             "Exiting due playbook input parsing error: %s" % repr(e))
     playbook_helper = PlaybookHelper()
     playbook_helper.execute_playbook(playbook_input_json)
-
