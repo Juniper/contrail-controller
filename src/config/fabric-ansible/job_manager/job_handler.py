f0c565df8ae27dfb969dc2b305c263a48978fbaa
jnpr-akshayam <akshayam@juniper.net>
2018-04-13 17:21:51 +0000

    [DM] Job_manager_changes
    Consolidated Changes including the following:
    1. Input / Output Schema json validation
    2. Job_Manager input validation (eg. credentials, mgmt.ip)
    3. Error test cases for job_manager
    4. Consolidating job_messages with locale support(en)
    5. JsonSchema dependent review link:
       https://review.opencontrail.org/#/c/41764/
    
    Change-Id: I02bdf66dc014b44aa6f0c45e5b77fb1a4db631c3
    Closes-Bug: #1760736
    Closes-Bug: #1753001
    Closes-Bug: #1753002
    Closes-Bug: #1755287

--- src/config/fabric-ansible/job_manager/job_handler.py
+++ src/config/fabric-ansible/job_manager/job_handler.py
@@ -13,6 +13,7 @@ import traceback
 
 from job_exception import JobException
 from job_utils import JobStatus
+from job_messages import MsgBundle
 
 
 class JobHandler(object):
@@ -46,9 +47,10 @@ class JobHandler(object):
             # run the playbook
             self.run_playbook(playbook_info)
 
-            msg = "Completed playbook execution for job template %s with " \
-                  "execution id %s" % (self._job_template.get_uuid(),
-                                       self._execution_id)
+            msg = MsgBundle.getMessage(
+                      MsgBundle.PLAYBOOK_EXECUTION_COMPLETE,
+                      job_template_id=self._job_template.get_uuid(),
+                      job_execution_id=self._execution_id)
             self._logger.debug(msg)
             result_handler.update_job_status(JobStatus.SUCCESS, msg, device_id)
         except JobException as e:
@@ -62,20 +64,7 @@ class JobHandler(object):
             result_handler.update_job_status(JobStatus.FAILURE, e.message,
                                              device_id)
 
-    def find_playbook_info(self, device_id, playbook_list):
-
-        device_details = self._device_json.get(device_id)
-        if not device_details:
-            msg = "Device details for the device %s not found" \
-                  % device_id
-            raise JobException(msg, self._execution_id)
-        device_vendor = device_details.get('device_vendor')
-        device_family = device_details.get('device_family')
-
-        if not device_vendor or not device_family:
-            raise JobException(
-                "device_vendor or device_family not found for %s"
-                % device_id, self._execution_id)
+    def find_playbook_info(self, device_family, device_vendor, playbook_list):
 
         for playbook_info in playbook_list:
             pb_vendor_name = playbook_info.get_vendor()
@@ -90,48 +79,84 @@ class JobHandler(object):
                 else:
                     # device_family agnostic
                     return playbook_info
-        msg = "Playbook info not found in the job template for " \
-              "%s and %s" % (device_vendor, device_family)
+        msg = MsgBundle.getMessage(MsgBundle.
+                                   PLAYBOOK_INFO_DEVICE_MISMATCH,
+                                   device_vendor=device_vendor,
+                                   device_family=device_family)
         raise JobException(msg, self._execution_id)
 
     def get_playbook_info(self, device_id=None):
         try:
-            # get the playbook uri from the job template
-            if device_id:
-                playbooks = self._job_template.get_job_template_playbooks()
-                play_info = self.find_playbook_info(device_id,
-                                                    playbooks.playbook_info)
-            else:
-                playbooks = self._job_template.get_job_template_playbooks()
-                play_info = playbooks.playbook_info[0]
-
             # create the cmd line param for the playbook
             extra_vars = {'input': self._job_input,
                           'params': self._job_params,
                           'job_template_id': self._job_template.get_uuid(),
                           'job_template_fqname': self._job_template.fq_name,
-                          'device_id': device_id,
                           'auth_token': self._auth_token,
                           'job_execution_id': self._execution_id,
-                          'vendor': play_info.vendor,
                           'args': self._sandesh_args
                           }
-            if self._device_json is not None:
+            playbooks = self._job_template.get_job_template_playbooks()
+
+            if device_id:
+                if not self._device_json:
+                    msg = MsgBundle.getMessage(MsgBundle.DEVICE_JSON_NOT_FOUND)
+                    raise JobException(msg,
+                                       self._execution_id)
+
                 device_data = self._device_json.get(device_id)
-                if device_data is not None:
-                    extra_vars['device_management_ip'] = \
-                        device_data.get('device_management_ip')
-                    extra_vars['device_username'] = \
-                        device_data.get('device_username')
-                    extra_vars['device_password'] = \
-                        device_data.get('device_password')
-                    extra_vars['device_fqname'] = \
-                        device_data.get('device_fqname')
-                    extra_vars['device_family'] = \
-                        device_data.get('device_family')
-                    self._logger.debug("Passing the following device "
-                                       "ip to playbook %s " % device_data.get(
-                                        'device_management_ip'))
+                if not device_data:
+                    msg = MsgBundle.getMessage(MsgBundle.NO_DEVICE_DATA_FOUND,
+                                               device_id=device_id)
+                    raise JobException(msg, self._execution_id)
+
+                device_family = device_data.get('device_family')
+                device_vendor = device_data.get('device_vendor')
+
+                if not device_vendor or not device_family:
+                    msg = MsgBundle.getMessage(MsgBundle.
+                                               DEVICE_VENDOR_FAMILY_MISSING,
+                                               device_id=device_id)
+                    raise JobException(msg, self._execution_id)
+
+                # check for credentials,required param; else playbooks
+                # will fail
+                device_username = device_data.get('device_username')
+                device_password = device_data.get('device_password')
+
+                if not device_username or not device_password:
+                    msg = MsgBundle.getMessage(MsgBundle.
+                                               NO_CREDENTIALS_FOUND,
+                                               device_id=device_id)
+                    raise JobException(msg, self._execution_id)
+
+                # update extra-vars to reflect device-related params
+                device_fqname = device_data.get('device_fqname')
+                device_management_ip = device_data.get('device_management_ip')
+                extra_vars.update(
+                                  {
+                                   'device_id': device_id,
+                                   'device_fqname': device_fqname,
+                                   'device_management_ip':
+                                   device_management_ip,
+                                   'vendor': device_vendor,
+                                   'device_family': device_family,
+                                   'device_username': device_username,
+                                   'device_password': device_password
+                                  }
+                                 )
+
+                self._logger.debug("Passing the following device "
+                                   "ip to playbook %s " % device_management_ip)
+
+                # get the playbook uri from the job template
+                play_info = self.find_playbook_info(device_family,
+                                                    device_vendor,
+                                                    playbooks.playbook_info)
+            else:
+                # get the playbook uri from the job template
+                play_info = playbooks.playbook_info[0]
+
             playbook_input = {'playbook_input': extra_vars}
 
             playbook_info = dict()
@@ -142,9 +167,10 @@ class JobHandler(object):
         except JobException:
             raise
         except Exception as e:
-            msg = "Error while getting the playbook information from the " \
-                  "job template %s : %s" % (self._job_template.get_uuid(),
-                                            repr(e))
+            msg = MsgBundle.getMessage(
+                      MsgBundle.GET_PLAYBOOK_INFO_ERROR,
+                      job_template_id=self._job_template.get_uuid(),
+                      exc_msg=repr(e))
             raise JobException(msg, self._execution_id)
 
     def run_playbook_process(self, playbook_info):
@@ -159,41 +185,49 @@ class JobHandler(object):
         except subprocess32.TimeoutExpired as e:
             if p is not None:
                 os.kill(p.pid, 9)
-            msg = "Timeout while executing the playbook " \
-                  "for %s : %s. Playbook process is aborted."\
-                  % (playbook_info['uri'], repr(e))
+            msg = MsgBundle.getMessage(
+                      MsgBundle.RUN_PLAYBOOK_PROCESS_TIMEOUT,
+                      playbook_uri=playbook_info['uri'],
+                      exc_msg=repr(e))
             raise JobException(msg, self._execution_id)
         except Exception as e:
-            msg = "Exception in executing the playbook " \
-                  "for %s : %s" % (playbook_info['uri'], repr(e))
+            msg = MsgBundle.getMessage(MsgBundle.
+                                       RUN_PLAYBOOK_PROCESS_ERROR,
+                                       playbook_uri=playbook_info['uri'],
+                                       exc_msg=repr(e))
             raise JobException(msg, self._execution_id)
 
         if p.returncode != 0:
-            msg = "Playbook exited with error."
+            msg = MsgBundle.getMessage(MsgBundle.
+                                       PLAYBOOK_EXIT_WITH_ERROR)
             raise JobException(msg, self._execution_id)
 
     def run_playbook(self, playbook_info):
         try:
             # create job log to capture the start of the playbook
             device_id = \
-                playbook_info['extra_vars']['playbook_input']['device_id']
-            if device_id is None:
-                device_id = ""
-            msg = "Starting to execute the playbook %s for device " \
-                  "%s with input %s and params %s " % \
-                  (playbook_info['uri'], device_id,
-                   json.dumps(playbook_info['extra_vars']['playbook_input']
-                              ['input']),
-                   json.dumps(playbook_info['extra_vars']['playbook_input']
-                              ['params']))
+                playbook_info['extra_vars']['playbook_input'].get(
+                    'device_id', "")
+            msg = MsgBundle.getMessage(MsgBundle.START_EXECUTE_PLAYBOOK_MSG,
+                                       playbook_uri=playbook_info['uri'],
+                                       device_id=device_id,
+                                       input_params=json.dumps(
+                                                    playbook_info['extra_vars']
+                                                    ['playbook_input']
+                                                    ['input']),
+                                       extra_params=json.dumps(
+                                                    playbook_info['extra_vars']
+                                                    ['playbook_input']
+                                                    ['params']))
             self._logger.debug(msg)
             self._job_log_utils.send_job_log(self._job_template.fq_name,
                                              self._execution_id,
                                              msg, JobStatus.IN_PROGRESS.value)
 
             if not os.path.exists(playbook_info['uri']):
-                raise JobException("Playbook %s does not "
-                                   "exist" % playbook_info['uri'],
+                msg = MsgBundle.getMessage(MsgBundle.PLAYBOOK_NOT_FOUND,
+                                           playbook_uri=playbook_info['uri'])
+                raise JobException(msg,
                                    self._execution_id)
 
             # Run playbook in a separate process. This is needed since
@@ -201,19 +235,17 @@ class JobHandler(object):
             self.run_playbook_process(playbook_info)
 
             # create job log to capture completion of the playbook execution
-            msg = "Completed to execute the playbook %s for" \
-                  " device %s" % (playbook_info['uri'],
-                                  playbook_info['extra_vars']['playbook_input']
-                                  ['device_id'])
+            msg = MsgBundle.getMessage(MsgBundle.PB_EXEC_COMPLETE_WITH_INFO,
+                                       playbook_uri=playbook_info['uri'],
+                                       device_id=device_id)
             self._logger.debug(msg)
             self._job_log_utils.send_job_log(self._job_template.fq_name,
                                              self._execution_id,
                                              msg, JobStatus.IN_PROGRESS.value)
-            return
         except JobException:
             raise
         except Exception as e:
-            msg = "Error while executing the playbook %s : %s" % \
-                  (playbook_info['uri'], repr(e))
+            msg = MsgBundle.getMessage(MsgBundle.RUN_PLAYBOOK_ERROR,
+                                       playbook_uri=playbook_info['uri'],
+                                       exc_msg=repr(e))
             raise JobException(msg, self._execution_id)
-
