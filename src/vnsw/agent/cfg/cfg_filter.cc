2f3f538d7f9eb159494ce91c1b0773f968780428
Hari Prasad Killi <haripk@juniper.net>
2018-04-17 23:46:47 +0530

    Filter out tag nodes without uuid
    Change-Id: I9b14bb38ff54016e04c523160a482f0b9ae6e88a
    closes-bug: #1764821

--- src/vnsw/agent/cfg/cfg_filter.cc
+++ src/vnsw/agent/cfg/cfg_filter.cc
@@ -89,6 +89,8 @@ int CfgFilter::GetIdPermsPropertyId(DBTable *table) const {
         return autogen::PortTuple::ID_PERMS;
     if (table == agent_cfg_->cfg_firewall_rule_table())
         return autogen::PortTuple::ID_PERMS;
+    if (table == agent_cfg_->cfg_tag_table())
+        return autogen::PortTuple::ID_PERMS;
     return -1;
 }
 
@@ -187,6 +189,15 @@ void CfgFilter::Init() {
 
     agent_cfg_->cfg_port_tuple_table()->RegisterPreFilter
         (boost::bind(&CfgFilter::CheckProperty, this, _1, _2, _3));
+
+    agent_cfg_->cfg_firewall_policy_table()->RegisterPreFilter
+        (boost::bind(&CfgFilter::CheckProperty, this, _1, _2, _3));
+
+    agent_cfg_->cfg_firewall_rule_table()->RegisterPreFilter
+        (boost::bind(&CfgFilter::CheckProperty, this, _1, _2, _3));
+
+    agent_cfg_->cfg_tag_table()->RegisterPreFilter
+        (boost::bind(&CfgFilter::CheckProperty, this, _1, _2, _3));
 }
 
 void CfgFilter::Shutdown() {
@@ -206,4 +217,7 @@ void CfgFilter::Shutdown() {
     agent_cfg_->cfg_bridge_domain_table()->RegisterPreFilter(NULL);
     agent_cfg_->cfg_slo_table()->RegisterPreFilter(NULL);
     agent_cfg_->cfg_port_tuple_table()->RegisterPreFilter(NULL);
+    agent_cfg_->cfg_firewall_policy_table()->RegisterPreFilter(NULL);
+    agent_cfg_->cfg_firewall_rule_table()->RegisterPreFilter(NULL);
+    agent_cfg_->cfg_tag_table()->RegisterPreFilter(NULL);
 }
