2a872e619a13ddcd030c6866aa2ef9971a7b8ab4
sjeevaraj <sjeevaraj@juniper.net>
2018-04-29 20:54:44 +0000

    [DM] device discovery changes with greenlets ti improve perf
    1. Discover devices in multiple threads in parallel. Reduced time to discover
       /25 networks from 40 minutes to 40 seconds
    2. Added ssh probe so that device can be discoveded with snmp enabled on the device
    3. Added job logs for incremental job progress
    
    Change-Id: I51af4343dceb20557a5766e10c95c264b391b118
    Closes-Bug: #1749315
    Closes-Bug: #1749042

--- src/config/fabric-ansible/ansible-playbooks/test/units/test_vnc_db_mod.py
+++ src/config/fabric-ansible/ansible-playbooks/test/units/test_vnc_db_mod.py
@@ -62,7 +62,7 @@ class TestFabricVncDbModule(TestFabricModule):
                        "physical_router_management_ip": "172.10.68.1",
                        "physical_router_vendor_name": "Juni",
                        "physical_router_product_name": "MX",
-                       "physical_router_device_family": "juniper-mx"}
+                       "physical_router_device_family": "junos"}
         fake_vnc_lib = flexmock()
         flexmock(VncApi, __new__=fake_vnc_lib)
         fake_vnc_lib.should_receive('__init__')
@@ -90,7 +90,74 @@ class TestFabricVncDbModule(TestFabricModule):
             'physical_router_update').and_return(fake_router_obj)
         flexmock(fake_vnc_lib).should_receive(
             'id_to_fq_name').and_return('MX-240')
-        set_module_args(dict(object_type='physical_router', object_op='update',
-                             object_dict=object_dict, auth_token="1234"))
+        set_module_args(
+            dict(enable_job_ctx=False, object_type='physical_router',
+                 object_op='update', object_dict=object_dict,
+                 job_ctx={"auth_token": "1234"}))
+        result = self.execute_module()
+        self.assertEqual(result.get('failed'), None)
+
+    def test_vnc_read_with_job_params(self):
+        fake_vnc_lib = flexmock()
+        flexmock(VncApi, __new__=fake_vnc_lib)
+        fake_vnc_lib.should_receive('__init__')
+        fake_router = PhysicalRouter(name='test-tenant')
+        fake_router_obj = {"physical-router": {"name": "Router-1"}}
+        flexmock(fake_vnc_lib).should_receive(
+            'physical_router_read').and_return(fake_router)
+        flexmock(fake_vnc_lib).should_receive(
+            'obj_to_dict').and_return(fake_router_obj)
+        set_module_args(
+            dict(object_type='physical_router',
+                 object_op='read', object_dict={"uuid": "1234"},
+                 job_ctx={"auth_token": "1234",
+                          "job_template_fqname": [
+                              "default-global-system-config",
+                              "device_import_template"],
+                          "job_execution_id": "123",
+                          "config_args": "546", "job_input": ""}))
+        result = self.execute_module()
+        self.assertEqual(result.get('failed'), None)
+
+    def test_vnc_bulk_create(self):
+        object_list = [{"parent_type": "global-system-config",
+                        "fq_name": ["default-global-system-config", "mx-240"],
+                        "physical_router_management_ip": "172.10.68.1",
+                        "physical_router_vendor_name": "Juni",
+                        "physical_router_product_name": "MX",
+                        "physical_router_device_family": "junos"}]
+        fake_vnc_lib = flexmock()
+        flexmock(VncApi, __new__=fake_vnc_lib)
+        fake_vnc_lib.should_receive('__init__')
+        fake_router_uuid = "1ef6cf9d-c2e2-4004-810a-43d471c94dc5"
+        flexmock(fake_vnc_lib).should_receive(
+            'physical_router_create').and_return(fake_router_uuid)
+        set_module_args(
+            dict(enable_job_ctx=False, object_type='physical_router',
+                 object_op='bulk_create', object_list=object_list,
+                 job_ctx={"auth_token": "1234"}))
+        result = self.execute_module()
+        self.assertEqual(result.get('failed'), None)
+
+    def test_vnc_bulk_update(self):
+        object_list = [{
+            "uuid": "1ef6cf9d-c2e2-4004-810a-43d471c94dc5",
+            "physical_router_user_credentials": {
+                "username": "root",
+                "password": "*****"
+            }
+        }]
+        fake_vnc_lib = flexmock()
+        flexmock(VncApi, __new__=fake_vnc_lib)
+        fake_vnc_lib.should_receive('__init__')
+        fake_router_obj = "{'physical-router' : {'uuid': '1ef6cf9d-c2e2-4004-810a-43d471c94dc5'}}"
+        flexmock(fake_vnc_lib).should_receive(
+            'physical_router_update').and_return(fake_router_obj)
+        flexmock(fake_vnc_lib).should_receive(
+            'id_to_fq_name').and_return('MX-240')
+        set_module_args(
+            dict(enable_job_ctx=False, object_type='physical_router',
+                 object_op='bulk_update', object_list=object_list,
+                 job_ctx={"auth_token": "1234"}))
         result = self.execute_module()
         self.assertEqual(result['failed'], False)
