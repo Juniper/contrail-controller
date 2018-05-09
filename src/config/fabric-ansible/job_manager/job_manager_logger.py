7514310372ff50db82ab8286dec51ff12cccc068
jnpr-akshayam <akshayam@juniper.net>
2018-05-09 23:02:51 +0000

    [DM] Capture Playbook execute exception msg from subprocess
    Logging the exception message from subprocess
    playbook_helper.py into job_manager logs for troubleshooting
    
    Change-Id: Id4c66c633c0a2e0efe6f48a4dc5cc4d2bd0ef2f8
    Closes-Bug: #1764587

---
+++ src/config/fabric-ansible/job_manager/job_manager_logger.py
@@ -0,0 +1,26 @@
+#
+# Copyright (c) 2018 Juniper Networks, Inc. All rights reserved.
+#
+
+"""
+Contains common logger initialization to be used in job_manager internals
+that are to be captured from playbook_helper.py sub-process
+"""
+
+import logging
+
+DEFAULT_JOB_MGR_LOG_PATH = '/var/log/contrail/contrail-fabric-ansible.log'
+DATE_FORMAT = "%m/%d/%Y %I:%M:%S %p"
+LOGGING_FORMAT = '%(asctime)s [%(name)s] [%(levelname)s]:  %(message)s'
+
+def job_mgr_logger(name, ctx=None):
+    name = name
+    ctx = ctx
+
+    logger = logging.getLogger(name)
+    handler = logging.FileHandler(DEFAULT_JOB_MGR_LOG_PATH)
+    formatter = logging.Formatter(fmt=LOGGING_FORMAT, datefmt=DATE_FORMAT)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
