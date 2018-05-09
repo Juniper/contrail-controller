8f85aab47a5a5f16b0562575813503a4b0640f58
Atul Moghe <amoghe@juniper.net>
2018-05-08 22:52:02 -0700

    changes in fip_test to use DM db instead of using hardcoded config
    currently only routing instances have been validated, othervalidation
    will be added later
    Partial-Bug: #1770080
    
    Change-Id: I257cec03b3a87d2a5547dc7e0c99b116ecd87f53

--- src/config/device-manager/test/test_dm_networks.py
+++ src/config/device-manager/test/test_dm_networks.py
@@ -254,6 +254,71 @@ class TestNetworkDM(TestCommonDM):
 
         return
 
+    @retries(5, hook=retry_exc_handler)
+    def check_fip_config(self, vn1, vn2, pr, fip_obj):
+        config = FakeDeviceConnect.get_xml_config()
+        ris = self.get_routing_instances(config, ri_name='')
+        self.assertEqual(len(ris), 4)
+        for ri in ris:
+            instance_type = ri.get_instance_type()
+            instance_name = ri.get_name()
+            if instance_type:
+                vrf_import=instance_name + "-import"
+                vrf_export=instance_name + "-export"
+                self.assertEqual(ri.get_vrf_import(), vrf_import)
+                self.assertEqual(ri.get_vrf_export(), vrf_export)
+                if instance_type == 'evpn':
+                    self.assertEqual(instance_name, "_contrail_vn-private-l2-4")
+                    intf_name = "irb." + instance_name[-1]
+                    ri_intf=ri.get_routing_interface()
+                    self.assertEqual(ri_intf, intf_name)
+                if instance_type == 'vrf':
+                    ri_intf=ri.get_interface()
+                    self.assertEqual(len(ri_intf), 1)
+                    ri_intf_name = ri_intf[0].get_name()
+                    ri_opts = ri.get_routing_options()
+                    route=ri_opts.get_static().get_route()
+                    self.assertEqual(len(route), 1)
+                    route_name = route[0].get_name()
+                    if 'nat' in instance_name:
+                        self.assertEqual(instance_name,
+                            "_contrail_vn-private-l3-4-nat")
+                        ri_intf_name = ri_intf_name[:-2]
+                        self.assertEqual(ri_intf_name, "ge-0/0/0")
+                        next_hop = route[0].get_next_hop()
+                        self.assertEqual(route_name, "0.0.0.0/0")
+                        self.assertEqual(next_hop, "ge-0/0/0.7")
+
+                    else:
+                        self.assertEqual(instance_name,
+                            "_contrail_vn-private-l3-4")
+                        intf_name = "irb." + instance_name[-1]
+                        self.assertEqual(ri_intf_name, intf_name)
+                        network_ipam=vn1.get_network_ipam_refs()[0]
+                        ipam_subnet = network_ipam['attr'].get_ipam_subnets()[0]
+                        subnet = ipam_subnet.get_subnet()
+                        ip_prefix = subnet.get_ip_prefix()
+                        prefix_len = subnet.get_ip_prefix_len()
+                        subnet_name = ip_prefix +'/' + str(prefix_len)
+                        self.assertEqual(route_name, subnet_name)
+
+            else:
+                self.assertEqual(instance_name, "_contrail_vn-public-l3-5")
+                ri_opts = ri.get_routing_options()
+                route=ri_opts.get_static().get_route()
+                self.assertEqual(len(route), 1)
+                name = route[0].get_name()
+                next_hop = route[0].get_next_hop()
+                fip_addr = fip_obj.get_floating_ip_address()
+                fip_addr = fip_addr + "/32"
+                self.assertEqual(fip_addr, name)
+                junos_service_port=pr.get_physical_router_junos_service_ports()
+                service_ports = junos_service_port.get_service_port()
+                self.assertEqual(len(service_ports), 1)
+                ser_port = service_ports[0]
+                self.assertEqual(ser_port, "ge-0/0/0")
+        return
+
     def set_global_vrouter_config(self, encap_priority_list = []):
         create = False
         try:
@@ -341,5 +406,71 @@ class TestNetworkDM(TestCommonDM):
         self.delete_routers(bgp_router, pr)
         self.wait_for_routers_delete(bgp_router_fq, pr_fq)
 
+
+    def test_fip(self):
+        vn1_name = 'vn-private'
+        vn1_obj = VirtualNetwork(vn1_name)
+        ipam_obj = NetworkIpam('ipam1')
+        self._vnc_lib.network_ipam_create(ipam_obj)
+        vn1_obj.add_network_ipam(ipam_obj, VnSubnetsType(
+            [IpamSubnetType(SubnetType("10.0.0.0", 24))]))
+        vn1_uuid = self._vnc_lib.virtual_network_create(vn1_obj)
+
+        #MX router
+        bgp_router1, pr_mx = self.create_router('mx_router', '1.1.1.1')
+        pr_mx.set_virtual_network(vn1_obj)
+        junos_service_ports = JunosServicePorts()
+        junos_service_ports.service_port.append("ge-0/0/0")
+        pr_mx.set_physical_router_junos_service_ports(junos_service_ports)
+        self._vnc_lib.physical_router_update(pr_mx)
+
+        pi_mx = PhysicalInterface('pi-mx', parent_obj = pr_mx)
+        pi_mx_id = self._vnc_lib.physical_interface_create(pi_mx)
+
+        #TOR
+        bgp_router2, pr_tor = self.create_router('tor_router', '2.2.2.2')
+        pr_tor.set_virtual_network(vn1_obj)
+        pr_tor.vnc_managed = False
+        self._vnc_lib.physical_router_update(pr_tor)
+        pi_tor = PhysicalInterface('pi-tor', parent_obj = pr_tor)
+        pi_tor_id = self._vnc_lib.physical_interface_create(pi_tor)
+
+        fq_name = ['default-domain', 'default-project', 'vmi1']
+        vmi = VirtualMachineInterface(fq_name=fq_name, parent_type = 'project')
+        vmi.set_virtual_network(vn1_obj)
+        vmi.virtual_machine_interface_device_owner = 'physical-router'
+        self._vnc_lib.virtual_machine_interface_create(vmi)
+
+        li_tor = LogicalInterface('li_tor', parent_obj = pi_tor)
+        li_tor.set_virtual_machine_interface(vmi)
+        li_tor_id = self._vnc_lib.logical_interface_create(li_tor)
+        vmi = self._vnc_lib.virtual_machine_interface_read(vmi.get_fq_name())
+
+        ip_obj1 = InstanceIp(name='inst-ip-1')
+        ip_obj1.set_virtual_machine_interface(vmi)
+        ip_obj1.set_virtual_network(vn1_obj)
+        ip_id1 = self._vnc_lib.instance_ip_create(ip_obj1)
+        ip_obj1 = self._vnc_lib.instance_ip_read(id=ip_id1)
+        ip_addr1 = ip_obj1.get_instance_ip_address()
+        vn2_name = 'vn-public'
+        vn2_obj = VirtualNetwork(vn2_name)
+        vn2_obj.set_router_external(True)
+        ipam2_obj = NetworkIpam('ipam2')
+        self._vnc_lib.network_ipam_create(ipam2_obj)
+        vn2_obj.add_network_ipam(ipam2_obj, VnSubnetsType(
+                [IpamSubnetType(SubnetType("192.168.7.0", 24))]))
+        vn2_uuid = self._vnc_lib.virtual_network_create(vn2_obj)
+        fip_pool_name = 'vn_public_fip_pool'
+        fip_pool = FloatingIpPool(fip_pool_name, vn2_obj)
+        self._vnc_lib.floating_ip_pool_create(fip_pool)
+        fip_obj = FloatingIp("fip1", fip_pool)
+        fip_obj.set_virtual_machine_interface(vmi)
+        self._vnc_lib.physical_router_update(pr_mx)
+
+        default_project = self._vnc_lib.project_read(fq_name=[u'default-domain', u'default-project'])
+        fip_obj.set_project(default_project)
+        fip_uuid = self._vnc_lib.floating_ip_create(fip_obj)
+        fip_obj1 = self._vnc_lib.floating_ip_read(id=fip_uuid)
+        self.check_fip_config(vn1_obj, vn2_obj, pr_mx, fip_obj1)
 # end TestNetworkDM
 
