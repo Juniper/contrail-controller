cf5a27ed182f54e3f82c574df62b5a3474aa2572
Mithun Mistry <mmithun@juniper.net>
2018-05-16 02:09:20 -0800

    check changes to disable centos jobs
    Change-Id: If3d37340973746858b8a8189499da8a25c385da6

--- src/analytics/structured_syslog_config.h
+++ src/analytics/structured_syslog_config.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Juniper Networks, Inc. All rights reserved.
+ * Copyright (c) 2017 Juniper Networks, Inc. All rights reserved. 34
  */
 #ifndef ANALYTICS_STRUCTURED_SYSLOG_CONFIG_H_
 #define ANALYTICS_STRUCTURED_SYSLOG_CONFIG_H_
