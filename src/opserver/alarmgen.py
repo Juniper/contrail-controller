288cbcd3e6ae4d97dcdb72dcf71fae3727818d43
zcui <zcui@juniper.net>
2018-05-22 07:58:05 -0700

    Vrouter UVE is imcomplete in DB7, but complete in DB1
    Root cause:
    When Alarmgen receives UVE metedata, a pendingset will
    be generated. At same time, a gevent task is assinged to deal with
    pendingset.New gevent task will
         1. get uve content from DB1
         2. update ptab_info with uve content
         3. Identify changing/deleting list of ptab_info
         4. Do alarm update if UVEAlarm block changes
         5. return changing list for aggragating
    When any step fails, Alarmgen will restart a gevent to deal with
    same pendingset again to ensure data consistency between DB7 and
    DB1.
    
    The problem is if first gevent failed, ptab_info has been updated
    with latest one in DB1, and when retry gevent to do step 3, it
    wrongly considered the content of UVE has no change and do not
    generate changing list. This let retry gevent missing uve blocks.
    
    Solution:
    Change ptab_info design:
      (1) Add pending set for content updating
      (2) Add commit API
      (3) legecy current set will be updated when commit is called
      (3) Values was retrieved from pending set
      (4) changing/removed list obtained with comparing between
          input and current set.
      (5) Add counter to record times diff which peding set was
          updated but commit was not called.
    
    unittest case:
      test_00_update_by_types_add
      test_01_update_by_types_remove
      test_02_update_by_types_change
      test_03_update_by_types_no_change
      test_04_update_by_types_commit_failed_once
      test_05_update_by_types_diff_type_seperate_commit
      test_06_update_by_types_diff_type_same_commit
      test_07_update_by_types_diff_type_seperate_commit_one_failed
    
    Closes-bug: 1719365
    
    Conflicts:
    	src/opserver/alarmgen.py
    
    Change-Id: I477b3be4dc1570859f5814cd0f1fc0026e4a6661

--- src/opserver/alarmgen.py
+++ src/opserver/alarmgen.py
@@ -125,37 +125,56 @@ class AGKeyInfo(object):
         # key of struct name, value of content dict
 
         self.current_dict = {}
+        self.pending_dict = {}
+        self.is_commit_pending = False
+        self.commit_failure = 0
         self.update({})
-        
-    def update_single(self, typ, val):
+ 
+    def update_by_types(self, types, val_dict):
         # A single UVE struct has changed
         # If the UVE has gone away, the val is passed in as None
-
+        if self.is_commit_pending:
+            self.is_commit_pending = False
+            self.commit_failure += 1
+            self.pending_dict = copy.deepcopy(self.current_dict)
         self.set_removed = set()
         self.set_added = set()
         self.set_changed = set()
-        self.set_unchanged = self.current_dict.keys()
+        self.set_unchanged = set(self.current_dict.keys())
 
-        if typ in self.current_dict:
-            # the "added" set must stay empty in this case
-            if val is None:
-                self.set_unchanged.remove(typ)
-                self.set_removed.add(typ)
-                del self.current_dict[typ]
-            else:
-                # both "added" and "removed" will be empty
-                if val != self.current_dict[typ]:
+        for typ in types:
+            val = None
+            if typ in val_dict:
+                val = val_dict[typ]
+            if typ in self.current_dict:
+                # the "added" set must stay empty in this case
+                if val is None:
                     self.set_unchanged.remove(typ)
-                    self.set_changed.add(typ)
-                    self.current_dict[typ] = val
-        else:
-            if val != None:
-                self.set_added.add(typ)
-                self.current_dict[typ] = val
+                    self.set_removed.add(typ)
+                    del self.pending_dict[typ]
+                else:
+                    # both "added" and "removed" will be empty
+                    if val != self.current_dict[typ]:
+                        self.set_unchanged.remove(typ)
+                        self.set_changed.add(typ)
+                        self.pending_dict[typ] = val
+            else:
+                if val != None:
+                    self.set_added.add(typ)
+                    self.pending_dict[typ] = val
+
+        if len(self.set_removed) or \
+            len(self.set_added) or \
+            len(self.set_changed):
+            self.is_commit_pending = True
 
     def update(self, new_dict):
-        # A UVE has changed, and we have the entire new 
+        # A UVE has changed, and we have the entire new
         # content of the UVE available in new_dict
+        if self.is_commit_pending:
+            self.is_commit_pending = False
+            self.commit_failure += 1
+            self.pending_dict = {}
         set_current = set(new_dict.keys())
         set_past = set(self.current_dict.keys())
         set_intersect = set_current.intersection(set_past)
@@ -169,10 +188,29 @@ class AGKeyInfo(object):
                 self.set_changed.add(o)
             else:
                 self.set_unchanged.add(o)
-        self.current_dict = new_dict
+        self.pending_dict = new_dict
+        if len(self.set_changed) or \
+            len(self.set_added) or \
+            len(self.set_removed):
+            self.is_commit_pending = True
+
+    def commit(self):
+        self.is_commit_pending = False
+        self.current_dict = copy.deepcopy(self.pending_dict)
 
     def values(self):
         return self.current_dict
+
+    def latest_values(self):
+        return self.pending_dict
+
+    def values_with_pending_data(self):
+        values = copy.deepcopy(self.current_dict)
+        if self.is_commit_pending:
+            values['pending_data'] = str(self.pending_dict)
+        if self.commit_failure:
+            values['commit_failure'] = self.commit_failure
+        return values
 
     def added(self):
         return self.set_added
@@ -1768,6 +1806,7 @@ class Controller(object):
 		sandesh=self._sandesh)
 
         erruves = []
+        uves_uuid = UTCTimestampUsec()
         for uv,types in uves.iteritems():
             tab = uv.split(':',1)[0]
             if tab not in self.tab_perf:
@@ -1802,79 +1841,45 @@ class Controller(object):
             touched = False
             if not types:
                 self.ptab_info[part][tab][uve_name].update(uve_data)
-                if len(self.ptab_info[part][tab][uve_name].removed()):
-                    touched = True
-                    rset = self.ptab_info[part][tab][uve_name].removed()
-                    self._logger.info("UVE %s removed structs %s" % (uv, rset))
-		    uveq_trace = UVEQTrace()
-		    uveq_trace.uves = [uv + "-" + str(rset)]
-		    uveq_trace.part = part
-		    uveq_trace.oper = "remove"
-		    uveq_trace.trace_msg(name="UVEQTrace",\
-			    sandesh=self._sandesh)
-                    for rems in self.ptab_info[part][tab][uve_name].removed():
-                        output[uv][rems] = None
-                        self.increment_uve_notif_count(part, tab, rems, "remove")
-                if len(self.ptab_info[part][tab][uve_name].changed()):
-                    touched = True
-                    self._logger.debug("UVE %s changed structs %s" % (uv, \
-                            self.ptab_info[part][tab][uve_name].changed()))
-                    for chgs in self.ptab_info[part][tab][uve_name].changed():
-                        output[uv][chgs] = \
-                                self.ptab_info[part][tab][uve_name].values()[chgs]
-                        self.increment_uve_notif_count(part, tab, chgs, "change")
-                if len(self.ptab_info[part][tab][uve_name].added()):
-                    touched = True
-                    self._logger.debug("UVE %s added structs %s" % (uv, \
-                            self.ptab_info[part][tab][uve_name].added()))
-                    for adds in self.ptab_info[part][tab][uve_name].added():
-                        output[uv][adds] = \
-                                self.ptab_info[part][tab][uve_name].values()[adds]
-                        self.increment_uve_notif_count(part, tab, adds, "add")
             else:
-                for typ in types:
-                    val = None
-                    if typ in uve_data:
-                        val = uve_data[typ]
-                    self.ptab_info[part][tab][uve_name].update_single(typ, val)
-                    if len(self.ptab_info[part][tab][uve_name].removed()):
-                        touched = True
-                        rset = self.ptab_info[part][tab][uve_name].removed()
-			self._logger.info("UVE %s removed structs %s" % (uv, rset))
-			uveq_trace = UVEQTrace()
-			uveq_trace.uves = [uv + "-" + str(rset)]
-			uveq_trace.part = part
-			uveq_trace.oper = "remove"
-			uveq_trace.trace_msg(name="UVEQTrace",\
-				sandesh=self._sandesh)
-                        for rems in self.ptab_info[part][tab][uve_name].removed():
-                            output[uv][rems] = None
-                            self.increment_uve_notif_count(part, tab, rems, "remove")
-                    if len(self.ptab_info[part][tab][uve_name].changed()):
-                        touched = True
-                        self._logger.debug("UVE %s changed structs %s" % (uve_name, \
-                                self.ptab_info[part][tab][uve_name].changed()))
-                        for chgs in self.ptab_info[part][tab][uve_name].changed():
-                            output[uv][chgs] = \
-                                    self.ptab_info[part][tab][uve_name].values()[chgs]
-                            self.increment_uve_notif_count(part, tab, chgs, "change")
-                    if len(self.ptab_info[part][tab][uve_name].added()):
-                        touched = True
-                        self._logger.debug("UVE %s added structs %s" % (uve_name, \
-                                self.ptab_info[part][tab][uve_name].added()))
-                        for adds in self.ptab_info[part][tab][uve_name].added():
-                            output[uv][adds] = \
-                                    self.ptab_info[part][tab][uve_name].values()[adds]
-                            self.increment_uve_notif_count(part, tab, adds, "add")
+                self.ptab_info[part][tab][uve_name].update_by_types(types, uve_data)
+            if len(self.ptab_info[part][tab][uve_name].removed()):
+                touched = True
+                rset = self.ptab_info[part][tab][uve_name].removed()
+                self._logger.info("UVE %s removed structs %s" % (uv, rset))
+		uveq_trace = UVEQTrace()
+		uveq_trace.uves = [uv + "-" + str(rset)]
+		uveq_trace.part = part
+		uveq_trace.oper = "remove"
+		uveq_trace.trace_msg(name="UVEQTrace",\
+		    sandesh=self._sandesh)
+                for rems in self.ptab_info[part][tab][uve_name].removed():
+                    output[uv][rems] = None
+                    self.increment_uve_notif_count(part, tab, rems, "remove")
+            if len(self.ptab_info[part][tab][uve_name].changed()):
+                touched = True
+                self._logger.debug("UVE %s changed structs %s" % (uv, \
+                        self.ptab_info[part][tab][uve_name].changed()))
+                for chgs in self.ptab_info[part][tab][uve_name].changed():
+                    output[uv][chgs] = \
+                            self.ptab_info[part][tab][uve_name].latest_values()[chgs]
+                    self.increment_uve_notif_count(part, tab, chgs, "change")
+            if len(self.ptab_info[part][tab][uve_name].added()):
+                touched = True
+                self._logger.debug("UVE %s added structs %s" % (uv, \
+                        self.ptab_info[part][tab][uve_name].added()))
+                for adds in self.ptab_info[part][tab][uve_name].added():
+                    output[uv][adds] = \
+                            self.ptab_info[part][tab][uve_name].latest_values()[adds]
+                    self.increment_uve_notif_count(part, tab, adds, "add")
             if not touched:
                 del output[uv]
-            local_uve = self.ptab_info[part][tab][uve_name].values()
+            local_uve = self.ptab_info[part][tab][uve_name].latest_values()
             
             self.tab_perf[tab].record_pub(UTCTimestampUsec() - prevt)
 
             if len(local_uve.keys()) == 0:
                 self._logger.info("UVE %s deleted in proc" % (uv))
-                del self.ptab_info[part][tab][uve_name]
                 output[uv] = None
                 
                 if tab in self.tab_alarms:
@@ -1914,6 +1919,12 @@ class Controller(object):
             uveq_trace = UVEQTrace()
             uveq_trace.uves = []
             for k,v in output.iteritems():
+                tab_co = k.split(':',1)[0]
+                uve_co = k.split(':',1)[1]
+                self.ptab_info[part][tab_co][uve_co].commit()
+                lo_uve = self.ptab_info[part][tab_co][uve_co].latest_values()
+                if len(lo_uve.keys()) == 0:
+                        del self.ptab_info[part][tab_co][uve_co]
                 if isinstance(v,dict):
                     uveq_trace.uves.append(str((k,v.keys())))
                     for ut in v:
@@ -1953,7 +1964,7 @@ class Controller(object):
                 uvel = []
                 for uk,uv in self.ptab_info[part][tab].iteritems():
                     types = []
-                    for tk,tv in uv.values().iteritems():
+                    for tk,tv in uv.values_with_pending_data().iteritems():
                         types.append(UVEStructInfo(type = tk,
                                 content = json.dumps(tv)))
                     uvel.append(UVEObjectInfo(
