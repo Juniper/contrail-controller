f59a78a018c48f1255b120274a913d4e653788b8
zcui <zcui@juniper.net>
2018-05-22 07:58:05 -0700

    Vrouter UVE is imcomplete in DB7, but complete in DB1
    Root cause:
    When Alarmgen receives UVE metedata, a pendingset will
    be generated. At same time, a gevent task is assinged to deal with
    pendingset.New gevent task will
         1. get uve content from DB1
         2. update ptab_info with uve content
         3. Identify changing/deleting list of ptab_info
         4. Do alarm update if UVEAlarm block changes
         5. return changing list for aggragating
    When any step fails, Alarmgen will restart a gevent to deal with
    same pendingset again to ensure data consistency between DB7 and
    DB1.
    
    The problem is if first gevent failed, ptab_info has been updated
    with latest one in DB1, and when retry gevent to do step 3, it
    wrongly considered the content of UVE has no change and do not
    generate changing list. This let retry gevent missing uve blocks.
    
    Solution:
    We need not check content of uve to determine if uve change.
    Any metedata coming from Kafka we should identify it as 'change'
    even the content is same to previous one.
    
    Change-Id: I83f9e8844a955b465a4d16c5d3c4473cc1cdfb7c
    Closes-bug: 1719365

--- src/opserver/alarmgen.py
+++ src/opserver/alarmgen.py
@@ -125,6 +125,7 @@ class AGKeyInfo(object):
         # key of struct name, value of content dict
 
         self.current_dict = {}
+        self.pending_dict = {}
         self.update({})
         
     def update_single(self, typ, val):
@@ -141,17 +142,19 @@ class AGKeyInfo(object):
             if val is None:
                 self.set_unchanged.remove(typ)
                 self.set_removed.add(typ)
-                del self.current_dict[typ]
+                del self.pending_dict[typ]
             else:
                 # both "added" and "removed" will be empty
                 if val != self.current_dict[typ]:
                     self.set_unchanged.remove(typ)
                     self.set_changed.add(typ)
-                    self.current_dict[typ] = val
+                self.pending_dict[typ] = val
         else:
             if val != None:
                 self.set_added.add(typ)
-                self.current_dict[typ] = val
+                self.pending_dict[typ] = val
+            else:
+                del self.pending_dict[typ]
 
     def update(self, new_dict):
         # A UVE has changed, and we have the entire new 
@@ -169,10 +172,13 @@ class AGKeyInfo(object):
                 self.set_changed.add(o)
             else:
                 self.set_unchanged.add(o)
-        self.current_dict = new_dict
+        self.pending_dict = new_dict
+
+    def commit(self):
+        self.current_dict = self.pending_dict
 
     def values(self):
-        return self.current_dict
+        return self.pending_dict
 
     def added(self):
         return self.set_added
@@ -1914,6 +1920,9 @@ class Controller(object):
             uveq_trace = UVEQTrace()
             uveq_trace.uves = []
             for k,v in output.iteritems():
+                tab_commit = k.split(':',1)[0]
+                uve_commit = k.split(':',1)[1]
+                self.ptab_info[part][tab_commit][uve_commit].commit()
                 if isinstance(v,dict):
                     uveq_trace.uves.append(str((k,v.keys())))
                     for ut in v:
