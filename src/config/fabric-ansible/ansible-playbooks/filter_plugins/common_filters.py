4c19f3069a2d8f81457955f5df88e2e3f132e85e
josephw <josephw@juniper.net>
2018-06-05 21:20:48 +0000

    [DM] Merged dhcp and tftp playbooks
    Change-Id: I7672a74ec8cd8dfe2cba3a6457b4f3a0c9a44597
    Partial-Bug: #1772768

---
+++ src/config/fabric-ansible/ansible-playbooks/filter_plugins/common_filters.py
@@ -0,0 +1,27 @@
+import random
+import crypt
+
+
+class FilterModule(object):
+    CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    def filters(self):
+        return {
+            'encrypt': self.crypt_password,
+            'shell_escape': self.shell_escape
+        }
+
+    @classmethod
+    def _generate_salt(cls, length):
+        chars = []
+        for _ in range(length):
+            chars.append(random.choice(cls.CHARS))
+        return "".join(chars)
+
+    @classmethod
+    def crypt_password(cls, strg):
+        return crypt.crypt(strg, "$6$%s" % cls._generate_salt(8))
+
+    @classmethod
+    def shell_escape(cls, strg):
+        return strg.replace("\\", "\\\\").replace("$", "\\$")
