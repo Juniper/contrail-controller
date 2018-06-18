535f4fba1b611fdafe8f6b2dd48aae16904385bf
Ananth Suryanarayana <anantha@juniper.net>
2018-06-18 10:26:05 -0700

    Verify MvpnNeigbor correctly after BGP RouterID change
    MVPN Neighbor information is not verified correctly, after bgp router
    id changes. It can so happen that after router-id is changed, before
    the router-id change is fully processed, part of the verification over
    old data is complete. Later the test fails when verifying against new
    data, especially the new RouterId information from the neighbor.
    
    Fix by ensuring that MvpnNeighbor info is checked correctly with retry
    over entire type1 routes lookup
    
    Change-Id: I836451c7e4f2487f19850fe94cfdcefe1c3b1aca
    Closes-Bug: 1777488

--- src/bgp/test/bgp_mvpn_test2.cc
+++ src/bgp/test/bgp_mvpn_test2.cc
@@ -17,41 +17,11 @@ TEST_P(BgpMvpnTest, Type1ADLocalWithIdentifierChanged) {
         }
     }
     VerifyInitialState();
-    error_code err;
     UpdateBgpIdentifier("127.0.0.2");
     TASK_UTIL_EXPECT_EQ(4*instances_set_count_ + 1, master_->Size());
 
-    for (size_t i = 1; i <= instances_set_count_; i++) {
-        TASK_UTIL_EXPECT_EQ(1, red_[i-1]->Size());
-        TASK_UTIL_EXPECT_NE(static_cast<MvpnRoute *>(NULL),
-                            red_[i-1]->FindType1ADRoute());
-
-        TASK_UTIL_EXPECT_EQ(1, blue_[i-1]->Size());
-        TASK_UTIL_EXPECT_NE(static_cast<MvpnRoute *>(NULL),
-                            blue_[i-1]->FindType1ADRoute());
-
-        TASK_UTIL_EXPECT_EQ(3, green_[i-1]->Size()); // 1 green1+1 red1+1 blue1
-        TASK_UTIL_EXPECT_NE(static_cast<MvpnRoute *>(NULL),
-                            green_[i-1]->FindType1ADRoute());
-
-        // Verify that only green1 has discovered a neighbor from red1.
-        TASK_UTIL_EXPECT_EQ(0, red_[i-1]->manager()->neighbors_count());
-        TASK_UTIL_EXPECT_EQ(0, blue_[i-1]->manager()->neighbors_count());
-        TASK_UTIL_EXPECT_EQ(2, green_[i-1]->manager()->neighbors_count());
-
-        MvpnNeighbor neighbor;
-        EXPECT_TRUE(green_[i-1]->manager()->FindNeighbor(
-                        *(red_[i-1]->routing_instance()->GetRD()), &neighbor));
-        EXPECT_EQ(*(red_[i-1]->routing_instance()->GetRD()), neighbor.rd());
-        EXPECT_EQ(0, neighbor.source_as());
-        EXPECT_EQ(IpAddress::from_string("127.0.0.2", err),
-                  neighbor.originator());
-
-        EXPECT_TRUE(green_[i-1]->manager()->FindNeighbor(
-                        *(blue_[i-1]->routing_instance()->GetRD()), &neighbor));
-        EXPECT_EQ(*(blue_[i-1]->routing_instance()->GetRD()), neighbor.rd());
-        EXPECT_EQ(0, neighbor.source_as());
-        EXPECT_EQ(IpAddress::from_string("127.0.0.2", err),
-                  neighbor.originator());
-    }
+    // Retry Type1 route checks as router-id update processing is executed
+    // completely asynchronously.
+    for (size_t i = 1; i <= instances_set_count_; i++)
+        TASK_UTIL_EXPECT_TRUE(CheckMvpnNeighborRoute(i));
 }
