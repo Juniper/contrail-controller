2f1d8733ba82587325056c882de683344a33514d
zcui <zcui@juniper.net>
2018-07-04 21:10:43 -0700

    Add contrail-collector to zookeeper
    Problem description:
      To check redis down or not, current uve server implement is like:
      (1) Try to connect configured redis server, if failed return false
      (2) else read contrail-collector from redis db, if not found, return
          false
      But one scenario is redis/contrail-collector pair down, we should
      not consider this as fault since no any useful date in DB.
      To correct this, we redesign it. We will add contrail-collecor to
      zookeeper.
      The znode is like
           /contrail-collector/{collector_ip1}
           /contrail-collector/{collector_ip2}
           ......
      /contrail-collector is PERSISTENT znode, {collector_ip1} is EPHEMERAL
      znode, and znode value is
           {hosename}:Analytics:contrail-collecor:0
    
    Test case:
        3 nodes cluster: n1, n2, n3 run contrail-analytics
        (1) n1 stop redis-server, n2 show contrail-analytics-api state init
        (2) n1 start redis-server, n2 show contrail-analytics-api state active
        (3) n1 stop contrail-collector, n2 show contrail-analytics-api state init
        (4) n1 start redis-server, n2 show contrail-analytics-api state active
        (5) n1 stop redis-server and contrail-collector, n2 show
            contrail-analytics-api state active
    
    Closes bug: 1733027
    Change-Id: I120d165891d97ef6d8bb9446077d02a10daed1dc

--- src/opserver/alarmgen.py
+++ src/opserver/alarmgen.py
@@ -1004,6 +1004,8 @@ class Controller(object):
             self._libpart = self.start_libpart(self._conf.alarmgen_list())
             self._ad = None
 
+        self._us.set_ad_handle(self._ad)
+
         self._workers = {}
         self._uvestats = {}
         self._alarmstats = {}
