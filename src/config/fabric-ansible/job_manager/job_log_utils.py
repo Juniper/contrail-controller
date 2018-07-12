2c8e38581ffe70388d386da6fa10eefb43d85c6c
sjeevaraj <sjeevaraj@juniper.net>
2018-07-12 20:48:15 +0000

    [DM] Fabric Job uve and Prouter Job uve
    1. Fabric Job uve creation moved to api server
    2. Percentage update is from job mgmr to Fabric Job uve
    3. Prouter Job uve creation for ztp job, is from job mgmr
    4. Prouter Job uve creation for non-ztp jobs, moved to api-server
    5. job status for each prouter and prouter state gets updated in Prouter Job uve
    
    Change-Id: Id42cb8e8983c7c092d16f06be9b47bd90431044a

--- src/config/fabric-ansible/job_manager/job_log_utils.py
+++ src/config/fabric-ansible/job_manager/job_log_utils.py
@@ -21,8 +21,8 @@ from pysandesh.sandesh_base import Sandesh
 from pysandesh.gen_py.sandesh.ttypes import SandeshLevel
 from sandesh.job.ttypes import JobLogEntry
 from sandesh.job.ttypes import JobLog
-from sandesh.job.ttypes import JobExecution
-from sandesh.job.ttypes import UveJobExecution
+from cfgm_common.uve.vnc_api.ttypes import FabricJobExecution, FabricJobUve, \
+    PhysicalRouterJobExecution, PhysicalRouterJobUve
 from sandesh.job.ttypes import PRouterOnboardingLogEntry
 from sandesh.job.ttypes import PRouterOnboardingLog
 
@@ -199,19 +199,49 @@ class JobLogUtils(object):
                                        exc_msg=repr(e))
             raise JobException(msg, job_execution_id)
 
-    def send_job_execution_uve(self, job_template_fqname, job_execution_id,
+
+
+
+    def send_job_execution_uve(self, fabric_fq_name, job_template_fqname, job_execution_id,
                                timestamp=None, percentage_completed=None):
         try:
-            job_template_fqname = self.get_fq_name_log_str(job_template_fqname)
-            if timestamp is None:
-                timestamp = int(round(time.time() * 1000))
-            job_exe_data = JobExecution(
-                name=job_template_fqname,
-                execution_id=job_execution_id,
-                job_start_ts=timestamp,
+            fabric_job_name = list(job_template_fqname)
+            fabric_job_name.insert(0, fabric_fq_name)
+            fabric_job_uve_name = ':'.join(map(str, fabric_job_name))
+
+            job_exe_data = FabricJobExecution(
+                name=fabric_job_uve_name,
                 percentage_completed=percentage_completed)
-            job_uve = UveJobExecution(data=job_exe_data)
+            job_uve = FabricJobUve(data=job_exe_data, sandesh=self.config_logger._sandesh)
             job_uve.send(sandesh=self.config_logger._sandesh)
+        except Exception as exp:
+            job_template_fqname = self.get_fq_name_log_str(job_template_fqname)
+            msg = MsgBundle.getMessage(MsgBundle.SEND_JOB_EXC_UVE_ERROR,
+                                       job_template_fqname=job_template_fqname,
+                                       job_execution_id=job_execution_id,
+                                       exc_msg=repr(exp))
+            raise JobException(msg, job_execution_id)
+
+    def send_prouter_job_uve(self, job_template_fqname, fq_names, job_execution_id, prouter_state=None, 
+                             job_status=None): 
+        try:
+            job_template_fqname = self.get_fq_name_log_str(job_template_fqname)
+            if prouter_state == None:
+                prouter_job_data = PhysicalRouterJobExecution(
+                    name=fq_names,
+                    execution_id=job_execution_id,
+                    job_status=job_status)
+
+            prouter_job_data = PhysicalRouterJobExecution(
+                name=fq_names,
+                execution_id=job_execution_id,
+                prouter_state=prouter_state,
+                job_status=job_status)
+
+            prouter_job_uve = PhysicalRouterJobUve(data=prouter_job_data,
+                                                   sandesh=self.config_logger._sandesh)
+            prouter_job_uve.send(sandesh=self.config_logger._sandesh)
+
         except Exception as exp:
             msg = MsgBundle.getMessage(MsgBundle.SEND_JOB_EXC_UVE_ERROR,
                                        job_template_fqname=job_template_fqname,
