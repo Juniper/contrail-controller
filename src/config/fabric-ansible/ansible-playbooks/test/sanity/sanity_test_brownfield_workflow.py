e20326ac2289900827b2aa99d321fd5d3dd00665
josephw <josephw@juniper.net>
2018-07-19 00:55:01 +0000

    [Fabric] Display job logs and percentages during sanity tests
    Change-Id: Ie0c09721348c42209178abde67afaed56ec9047b
    Closes-bug: 1782462

--- src/config/fabric-ansible/ansible-playbooks/test/sanity/sanity_test_brownfield_workflow.py
+++ src/config/fabric-ansible/ansible-playbooks/test/sanity/sanity_test_brownfield_workflow.py
@@ -33,16 +33,20 @@ class SanityTestZtpWorkflow(SanityBase):
     def onboard_fabric(self, fabric_info):
         """Onboard fabric"""
         self._logger.info("Onboard fabric ...")
+        job_template_fq_name = [
+            'default-global-system-config', 'existing_fabric_onboard_template']
+        fabric_fq_name = fabric_info['fabric_fq_name']
+
         job_execution_info = self._api.execute_job(
-            job_template_fq_name=[
-                'default-global-system-config', 'existing_fabric_onboard_template'],
+            job_template_fq_name=job_template_fq_name,
             job_input=fabric_info
         )
-
         job_execution_id = job_execution_info.get('job_execution_id')
         self._logger.debug(
             "Onboard fabric job started with execution id: %s", job_execution_id)
-        self._wait_for_job_to_finish('Onboard fabric', job_execution_id)
+        self._wait_and_display_job_progress('Onboard fabric', job_execution_id,
+                                            fabric_fq_name, job_template_fq_name)
+        self._logger.info("... Brownfield workflow complete")
     # end onboard_fabric
 
     def test(self):
