1c9fdba572244958f0c1fa889131ea1afd0f6b76
Michal Clapinski <michal.clapinski@codilime.com>
2018-08-02 17:20:01 +0200

    Implement proper status reporting in nodemgr by events on Windows
    Reporting by events was missing so I had to add some new code to
    windows_process_manager.py. Also I wanted to reuse some code from
    docker_process_manager.py so I moved it to common_process_manager.py
    
    Change-Id: If191e8cfd6ca18fd8b4899c7bcaa259d6950d628
    Partial-Bug: #1783539

---
+++ src/nodemgr/common/common_process_manager.py
@@ -0,0 +1,38 @@
+#
+# Copyright (c) 2018 Juniper Networks, Inc. All rights reserved.
+#
+
+def dummy_process_info(name):
+    info = dict()
+    info['name'] = name
+    info['group'] = name
+    info['pid'] = 0
+    info['statename'] = 'PROCESS_STATE_EXITED'
+    info['expected'] = -1
+    return info
+
+
+def convert_to_pi_event(info):
+    pi_event = info.copy()
+    pi_event['state'] = pi_event.pop('statename')
+    if 'start' in pi_event:
+        del pi_event['start']
+    return pi_event
+
+class ProcessInfoCache(object):
+    def __init__(self):
+        self._cached_process_infos = {}
+
+    def update_cache(self, info):
+        name = info['name']
+        cached_info = self._cached_process_infos.get(name)
+        if cached_info is None:
+            self._cached_process_infos[name] = info
+            return True
+        if info['name'] != cached_info['name'] or \
+                info['group'] != cached_info['group'] or \
+                info['pid'] != cached_info['pid'] or \
+                info['statename'] != cached_info['statename']:
+            self._cached_process_infos[name] = info
+            return True
+        return False
