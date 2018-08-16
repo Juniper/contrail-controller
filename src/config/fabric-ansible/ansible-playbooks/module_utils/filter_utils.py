49d740aaa4acc36fb147102009af30ffbae965c6
jnpr-akshayam <akshayam@juniper.net>
2018-08-16 23:53:20 +0000

    [DM] Vendor agnostic changes - initial checkin
    1. Initial checkin for framework testing
    2. Device import tested for vendor agnostic device querying / command execution
    3. Device import tested for filter plugin invoking
    4. Device import tested for functionality with refactoring
    5. Device import including debug logging functionality
    6. Topology discovery tested for vendor agnostic device querying / command execution
    7. Topology discovery tested for filter plugin invoking
    8. Topology discovery tested for functionality with refactoring
    9. Topology discovery including debug logging functionality
    10.Unified logging across all modules into a single instance
    11.Included device name in debug logs also
    12.Included validators at the beginning of parsing
    13. Included schema checks.
    14. To Modify parsing methods and include some annotations
    
    Change-Id: I89d6bcb2bd31ddbcef3e2dec6b4d740edd1d5a13
    Closes-Bug: #1785127
    Closes-Bug: #1784968
    Closes-Bug: #1786319

---
+++ src/config/fabric-ansible/ansible-playbooks/module_utils/filter_utils.py
@@ -0,0 +1,125 @@
+import logging
+import os
+from functools import wraps
+
+class FilterLog(object):
+    _instance = None
+
+    @staticmethod
+    def instance(loggername=None, device_name=None):
+        if not FilterLog._instance:
+            FilterLog._instance = FilterLog(loggername, device_name)
+        return FilterLog._instance
+    # end instance
+
+    @staticmethod
+    def cleanup_filterlog_instance():
+        if FilterLog._instance:
+            FilterLog._instance = None
+    # end cleanup_filterlog_instance
+
+    @staticmethod
+    def _init_logging(loggername):
+        """
+        :return: type=<logging.Logger>
+        """
+        logger = logging.getLogger(loggername)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+
+        formatter = logging.Formatter(
+            '%(asctime)s %(levelname)-8s %(message)s',
+            datefmt='%Y/%m/%d %H:%M:%S'
+        )
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+        return logger
+      # end _init_logging
+
+    def __init__(self, loggername, device_name):
+        self._msg = None
+        self._logs = []
+        if device_name != None:
+            loggername = loggername + " : " + device_name
+        self._logger = FilterLog._init_logging(loggername)
+    # end __init__
+
+    def logger(self):
+        return self._logger
+    # end logger
+
+    def msg_append(self, msg):
+        if msg:
+            if not self._msg:
+                self._msg = msg + ' ... '
+            else:
+                self._msg += msg + ' ... '
+    # end log
+
+    def msg_end(self):
+        if self._msg:
+            self._msg += 'done'
+            self._logs.append(self._msg)
+            self._logger.info(self._msg)
+            self._msg = None
+    # end msg_end
+
+    def msg_error(self, msg):
+        self._logger.error(msg)
+    # end msg_error
+
+    def msg_debug(self, msg):
+        self._logger.debug(msg)
+    # end msg_debug
+
+    def msg_warn(self, msg):
+        self._logger.warn(msg)
+    # end msg_warn
+
+    def dump(self):
+        retval = ""
+        for msg in self._logs:
+            retval += msg + '\n'
+        return retval
+    # end dump
+# end FilterLog
+
+
+def _task_log(msg):
+    FilterLog.instance().msg_append(msg)
+# end _task_log
+
+
+def _task_done(msg=None):
+    if msg:
+        _task_log(msg)
+    FilterLog.instance().msg_end()
+# end _task_done
+
+
+def _task_error_log(msg):
+    FilterLog.instance().msg_error(msg)
+# end _task_error_log
+
+
+def _task_debug_log(msg):
+    FilterLog.instance().msg_debug(msg)
+# end _task_debug_log
+
+
+def _task_warn_log(msg):
+    FilterLog.instance().msg_warn(msg)
+# end _task_warn_log
+
+def validate_payload(func):
+    @wraps(func)
+    def decorated(self, *args, **kwargs):
+        # pre function call validations
+        validator_method = args[-1]
+        validator_method(args[1])
+        _task_log("Validated parsed payload")
+        # post function call validations
+        return func(self, *args, **kwargs)
+    return decorated
+
+
