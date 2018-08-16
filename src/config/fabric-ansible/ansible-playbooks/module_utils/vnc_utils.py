d00b42ac4a916623b0f66d31da1e81dbb44fdf75
jnpr-akshayam <akshayam@juniper.net>
2018-08-16 23:53:20 +0000

    [DM] Vendor agnostic changes - initial checkin
    1. Initial checkin for framework testing
    2. Device import tested for vendor agnostic device querying / command execution
    3. Device import tested for filter plugin invoking
    4. Device import tested for functionality with refactoring
    5. Device import including debug logging functionality
    6. Topology discovery tested for vendor agnostic device querying / command execution
    7. Topology discovery tested for filter plugin invoking
    8. Topology discovery tested for functionality with refactoring
    9. Topology discovery including debug logging functionality
    10.Unified logging across all modules into a single instance
    11.To include device name in debug logs also
    12.Included validators at the beginning of parsing
    13. To include schema checks
    14. To modify parsing methods
    
    Change-Id: I89d6bcb2bd31ddbcef3e2dec6b4d740edd1d5a13
    Closes-Bug: #1785127
    Closes-Bug: #1784968
    Closes-Bug: #1786319

---
+++ src/config/fabric-ansible/ansible-playbooks/module_utils/vnc_utils.py
@@ -0,0 +1,14 @@
+from vnc_api.vnc_api import VncApi
+import vnc_api
+from inflection import camelize
+
+class VncUtils(object):
+
+    @staticmethod
+    def _init_vnc_api(auth_token):
+        return VncApi(auth_type=VncApi._KEYSTONE_AUTHN_STRATEGY,
+                      auth_token=auth_token)
+    @staticmethod
+    def _get_vnc_cls(object_type):
+        cls_name = camelize(object_type)
+        return getattr(vnc_api.gen.resource_client, cls_name, None)
