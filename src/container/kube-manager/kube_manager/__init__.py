5f33a24769a238d0ba44adfdca748f9f4ab2df07
Pawel Zadrozny <pawel.zny@gmail.com>
2018-09-12 08:47:05 +0200

    etcd support for kube-manager
    vnc_etcd with integration to existing code base.
    Using etcd instead of cassandra and rabiitmq can be done by
    changing configuration file. Example file has
    been attached to this review.
    
    Change-Id: I69319c63080e705fa13e4f6d5dc2c523ea295c56
    Partial-Bug: #1792379
    Depends-on: Ia3b58d2df5a2984adc3c0e5049b43d542eeb77a7

--- src/container/kube-manager/kube_manager/__init__.py
+++ src/container/kube-manager/kube_manager/__init__.py
@@ -1,3 +1,8 @@
 #
 # Copyright (c) 2017 Juniper Networks, Inc. All rights reserved.
 #
+from gevent import monkey
+monkey.patch_all()
+
+import grpc.experimental.gevent as grpc_gevent
+grpc_gevent.init_gevent()
