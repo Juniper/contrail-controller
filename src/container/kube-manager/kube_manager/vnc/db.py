c74dc4212c9502399e9977977b793ea15b1435d7
Pawel Zadrozny <pawel.zny@gmail.com>
2018-09-12 08:47:05 +0200

    etcd support for kube-manager
    vnc_etcd with integration to existing code base.
    Using etcd instead of cassandra and rabiitmq can be done by
    changing configuration file. Example file has
    been attached to this review.
    
    Change-Id: I69319c63080e705fa13e4f6d5dc2c523ea295c56
    Partial-Bug: #1792379
    Depends-on: Ia3b58d2df5a2984adc3c0e5049b43d542eeb77a7

--- src/container/kube-manager/kube_manager/vnc/db.py
+++ src/container/kube-manager/kube_manager/vnc/db.py
@@ -7,18 +7,46 @@
 Kube network manager DB
 """
 
-from cfgm_common.vnc_object_db import VncObjectDBClient
+from cfgm_common.vnc_object_db import VncObjectDBClient, VncObjectEtcdClient
+from cfgm_common.vnc_etcd import etcd_args
 
-class KubeNetworkManagerDB(VncObjectDBClient):
+DRIVER_CASS = 'cassandra'
+DRIVER_ETCD = 'etcd'
 
+
+class KubeNetworkManagerDB(object):
     def __init__(self, args, logger):
         self._db_logger = logger
+        self._db_logger.log("KubeNetworkManagerDB arguments: {}".format(args))
 
-        cred = None
+        if not hasattr(args, 'db_driver') or not args.db_driver:
+            msg = str("Contrail API server supports cassandra and etcd "
+                      "backends, but neither of them have been configured.")
+            raise ValueError(msg)
+
+        if args.db_driver == DRIVER_ETCD:
+            self._object_db = self._etcd_driver(args)
+        else:
+            self._object_db = self._cass_driver(args)
+
+    def __getattr__(self, name):
+        return getattr(self._object_db, name)
+
+    def _cass_driver(self, args):
+        vnc_db = {
+            'server_list': args.cassandra_server_list,
+            'db_prefix': args.cluster_id,
+            'logger': self._db_logger.log,
+            'reset_config': False,
+            'credential': None,
+        }
         if args.cassandra_user and args.cassandra_password:
-            cred={'username':args.cassandra_user,
-                  'password':args.cassandra_password}
+            vnc_db['credential'] = {'username': args.cassandra_user,
+                                    'password': args.cassandra_password}
+        self._db_logger.log("VncObjectDBClient arguments: {}".format(vnc_db))
+        return VncObjectDBClient(**vnc_db)
 
-        super(KubeNetworkManagerDB, self).__init__(args.cassandra_server_list,
-            args.cluster_id, None, None, self._db_logger.log,
-            reset_config=False, credential=cred)
+    def _etcd_driver(self, args):
+        vnc_db = etcd_args(args)
+        self._db_logger.log("VncObjectEtcdClient arguments: {}".format(vnc_db))
+        return VncObjectEtcdClient(logger=self._db_logger.log, **vnc_db)
