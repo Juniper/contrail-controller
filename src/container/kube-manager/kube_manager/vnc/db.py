11e5a4cf80200c6dd6cde68743c934032d0f5bc4
Pawel Zadrozny <pawel.zny@gmail.com>
2018-09-12 13:29:38 +0200

    config args and KubeNetworkManagerDB update for etcd
    Change-Id: I47d25fbf14a4ce60a4be4957b333c8e50eb0d5b5

--- src/container/kube-manager/kube_manager/vnc/db.py
+++ src/container/kube-manager/kube_manager/vnc/db.py
@@ -7,18 +7,44 @@
 Kube network manager DB
 """
 
-from cfgm_common.vnc_object_db import VncObjectDBClient
+from cfgm_common.vnc_object_db import VncObjectDBClient, VncObjectEtcdClient
 
 
-class KubeNetworkManagerDB(VncObjectDBClient):
+class KubeNetworkManagerDB(object):
     def __init__(self, args, logger):
         self._db_logger = logger
+        if args.cassandra_server_list and not args.etcd_server:
+            self._cass_backend(args)
+        elif args.etcd_server and not args.cassandra_server_list:
+            self._etcd_backend(args)
+        else:
+            msg = str("Contrail API server supports cassandra and etcd "
+                      "backends, but neither of them have been configured.")
+            raise NotImplementedError(msg)
 
-        cred = None
+    def __getattr__(self, name):
+        return getattr(self._object_db, name)
+
+    def _cass_backend(self, args):
+        vnc_db = {
+            'server_list': args.cassandra_server_list,
+            'db_prefix': args.cluster_id,
+            'logger': self._db_logger.log,
+            'reset_config': False,
+        }
         if args.cassandra_user and args.cassandra_password:
-            cred={'username': args.cassandra_user,
-                  'password': args.cassandra_password}
+            vnc_db['credential'] = {'username': args.cassandra_user,
+                                    'password': args.cassandra_password}
+        self._object_db = VncObjectDBClient(**vnc_db)
 
-        super(KubeNetworkManagerDB, self).__init__(
-            args.cassandra_server_list, args.cluster_id, None, None,
-            self._db_logger.log, reset_config=False, credential=cred)
+    def _etcd_backend(self, args):
+        server = args.etcd_server.split(':')
+        vnc_db = {
+            'host': server[0],
+            'port': server[1],
+            'logger': self._db_logger.log,
+        }
+        if args.etcd_user and args.etcd_password:
+            vnc_db['credential'] = {'user': args.etcd_user,
+                                    'password': args.etcd_password}
+        self._object_db = VncObjectEtcdClient(**vnc_db)
