e129292dca2f10327f1ef1df9b67bd74cacdc112
Michal Clapinski <michal.clapinski@codilime.com>
2018-09-28 19:09:39 +0200

    Fix default paths on Windows
    There are hardcoded paths to /var and /etc in agent.
    This PR fixes them for Windows.
    
    Change-Id: If9b152c83fd97765c15fd016c2ec9f26b306417e
    Partial-Bug: #1737177

---
+++ src/vnsw/agent/cmn/nix/agent_paths.cc
@@ -0,0 +1,4 @@
+#include <cmn/agent.h>
+
+const std::string Agent::config_file_ = "/etc/contrail/contrail-vrouter-agent.conf";
+const std::string Agent::log_file_ = "/var/log/contrail/vrouter.log";
---
+++ src/vnsw/agent/cmn/windows/agent_paths.cc
@@ -0,0 +1,4 @@
+#include <cmn/agent.h>
+
+const std::string Agent::config_file_ = "C:\\ProgramData\\Contrail\\etc\\contrail\\contrail-vrouter-agent.conf";
+const std::string Agent::log_file_ = "C:\\ProgramData\\Contrail\\var\\log\\contrail\\vrouter.log";
