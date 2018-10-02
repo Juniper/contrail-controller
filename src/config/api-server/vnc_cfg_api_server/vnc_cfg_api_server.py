5f0016108b3b1d5fdd680caddcba7e3d85b95967
Édouard Thuleau <ethuleau@juniper.net>
2018-10-02 14:46:35 +0200

    [config] Fix existing fq_name error for draft security resources
    When draft mode is enabled on a scope, API server must prevent to use
    an enforced and exiting fq_names.
    
    Change-Id: I844c27e51b88d8de68980fefe55a7b7cf6b2e43b
    Closes-Bug: #1795636

--- src/config/api-server/vnc_cfg_api_server/vnc_cfg_api_server.py
+++ src/config/api-server/vnc_cfg_api_server/vnc_cfg_api_server.py
@@ -987,6 +987,15 @@ class VncApiServer(object):
             result = 'Bad reference in create: ' + result
             raise cfgm_common.exceptions.HttpError(400, result)
 
+        # common handling for all resource create
+        (ok, result) = self._post_common(obj_type, obj_dict)
+        if not ok:
+            (code, msg) = result
+            fq_name_str = ':'.join(obj_dict.get('fq_name', []))
+            self.config_object_error(None, fq_name_str, obj_type, 'http_post', msg)
+            raise cfgm_common.exceptions.HttpError(code, msg)
+        uuid_in_req = result
+
         # Can abort resource creation and retrun 202 status code
         get_context().set_state('PENDING_DBE_CREATE')
         ok, result = r_class.pending_dbe_create(obj_dict)
@@ -1017,15 +1026,6 @@ class VncApiServer(object):
             code, msg = result
             raise cfgm_common.exceptions.HttpError(code, msg)
 
-        # common handling for all resource create
-        (ok, result) = self._post_common(obj_type, obj_dict)
-        if not ok:
-            (code, msg) = result
-            fq_name_str = ':'.join(obj_dict.get('fq_name', []))
-            self.config_object_error(None, fq_name_str, obj_type, 'http_post', msg)
-            raise cfgm_common.exceptions.HttpError(code, msg)
-
-        uuid_in_req = result
         name = obj_dict['fq_name'][-1]
         fq_name = obj_dict['fq_name']
 
@@ -4914,7 +4914,7 @@ class VncApiServer(object):
                                        r_class.object_type, fq_name, draft)
                 # Purge pending resource as we re-use the same UUID
                 self.internal_request_delete(r_class.object_type,
-                                            child['uuid'])
+                                             child['uuid'])
                 if uuid and draft['draft_mode_state'] == 'deleted':
                     # The resource is removed, we can purge original resource
                     deletes.append((r_class.object_type, uuid))
