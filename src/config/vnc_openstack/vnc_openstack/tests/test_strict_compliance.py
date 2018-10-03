eab550a0da9cee8ca50dbb3c1340211f6f26bc12
Édouard Thuleau <ethuleau@juniper.net>
2018-10-04 00:09:38 +0200

    [vnc openstack] Add support of Neutron FWaaS v2 API
    Add support to the Neutron FWaaS v2 API extension [1] as described in
    the spec [2].
    
    TODO:
      - firewall rule REJECT action not supported
    
    [1] https://developer.openstack.org/api-ref/network/v2/?expanded=id341-detail,id346-detail#fwaas-v2-0-current-fwaas-firewall-groups-firewall-policies-firewall-rules
    [2] https://github.com/Juniper/contrail-specs/blob/master/neutron_FWaaSv2.md
    
    Depends-On: I7a120078e9c18b35773428b3be44cc8ea3d76f3e
    Depends-On: I41ef58c96489b7275cbd43f9669142dd7d4bccbe
    Change-Id: I39758a3d3aefe7c8e6cc0d590bf6f6ea75fadc38
    Closes-bug: #1664193

--- src/config/vnc_openstack/vnc_openstack/tests/test_strict_compliance.py
+++ src/config/vnc_openstack/vnc_openstack/tests/test_strict_compliance.py
@@ -24,215 +24,80 @@ class TestStrictCompOn(test_case.NeutronBackendTestCase):
             extra_config_knobs=[('NEUTRON', 'strict_compliance', True)])
     #end setUpClass
 
-    def _create_resource(self, res_type, proj_id, name=None, extra_res_fields=None):
-        context = {'operation': 'CREATE',
-                   'user_id': '',
-                   'is_admin': False,
-                   'roles': '',
-                   'tenant_id': proj_id}
-        if name:
-            res_name = name
-        else:
-            res_name = '%s-%s' %(res_type, str(uuid.uuid4()))
-        data = {'resource': {'name': res_name,
-                             'tenant_id': proj_id}}
-        if extra_res_fields:
-            data['resource'].update(extra_res_fields)
-
-        body = {'context': context, 'data': data}
-        resp = self._api_svr_app.post_json('/neutron/%s' %(res_type), body)
-        res_q = json.loads(resp.text)
-        return res_name, res_q
-    # end _create_resource
-
-    def _create_floatingip_and_associate_port_without_ext_gw(self, proj_id, name=None):
+    def _create_floatingip_and_associate_port_without_ext_gw(self, proj_id):
         #external network
-        net_name, net_q = self._create_resource('network', proj_id, extra_res_fields={'router:external':True})
-        subnet_name, subnet_q = self._create_resource('subnet', proj_id, name, extra_res_fields={'network_id': net_q['id'], 'cidr': '10.2.0.0/24', 'ip_version': 4})
-        
-        #private network
-        pvt_net_name, pvt_net_q = self._create_resource('network', proj_id)
-        pvt_subnet_name, pvt_subnet_q = self._create_resource('subnet', proj_id, name, extra_res_fields={'network_id': pvt_net_q['id'], 'cidr': '10.1.0.0/24', 'ip_version': 4})
-        
-        port_name, port_q = self._create_resource('port', proj_id, name, extra_res_fields={'network_id': pvt_subnet_q['network_id']}) 
-        
-        return self._create_resource('floatingip', proj_id, name, extra_res_fields={'floating_network_id': net_q['id'], 'port_id':port_q['id']})
+        net_q = self.create_resource('network', proj_id, extra_res_fields={'router:external':True})
+        subnet_q = self.create_resource('subnet', proj_id, extra_res_fields={'network_id': net_q['id'], 'cidr': '10.2.0.0/24', 'ip_version': 4})
 
-    def _create_floatingip_and_associate_port_with_ext_gw(self, proj_id, name=None):
+        #private network
+        pvt_net_q = self.create_resource('network', proj_id)
+        pvt_subnet_q = self.create_resource('subnet', proj_id, extra_res_fields={'network_id': pvt_net_q['id'], 'cidr': '10.1.0.0/24', 'ip_version': 4})
+
+        port_q = self.create_resource('port', proj_id, extra_res_fields={'network_id': pvt_subnet_q['network_id']})
+
+        return self.create_resource('floatingip', proj_id, extra_res_fields={'floating_network_id': net_q['id'], 'port_id':port_q['id']})
+
+    def _create_floatingip_and_associate_port_with_ext_gw(self, proj_id):
         #external network
-        net_name, net_q = self._create_resource('network', proj_id, extra_res_fields={'router:external':True})
-        subnet_name, subnet_q = self._create_resource('subnet', proj_id, name, extra_res_fields={'network_id': net_q['id'], 'cidr': '10.2.0.0/24', 'ip_version': 4})
-        router_name, router_q = self._create_resource('router',proj_id, name)
-        
+        net_q = self.create_resource('network', proj_id, extra_res_fields={'router:external':True})
+        subnet_q = self.create_resource('subnet', proj_id, extra_res_fields={'network_id': net_q['id'], 'cidr': '10.2.0.0/24', 'ip_version': 4})
+        router_q = self.create_resource('router',proj_id)
+
         #private network
-        pvt_net_name, pvt_net_q = self._create_resource('network', proj_id)
-        pvt_subnet_name, pvt_subnet_q = self._create_resource('subnet', proj_id, name, extra_res_fields={'network_id': pvt_net_q['id'], 'cidr': '10.1.0.0/24', 'ip_version': 4})
-        
-        port_name, port_q = self._create_resource('port', proj_id, name, extra_res_fields={'network_id': pvt_subnet_q['network_id']}) 
-        port2_name, port2_q = self._create_resource('port', proj_id, name, extra_res_fields={'network_id': pvt_subnet_q['network_id']}) 
-       
-        #External gateway  
-        router_name, router_q = self._update_resource('router', router_q['id'], proj_id, name, extra_res_fields={'external_gateway_info': {'network_id':net_q['id']}})
-        router_name, router_q = self._add_router_interface('router', router_q['id'], proj_id, name, extra_res_fields={'port_id':port2_q['id']})
-        return self._create_resource('floatingip', proj_id, name, extra_res_fields={'floating_network_id': net_q['id'], 'port_id':port_q['id']})
+        pvt_net_q = self.create_resource('network', proj_id)
+        pvt_subnet_q = self.create_resource('subnet', proj_id, extra_res_fields={'network_id': pvt_net_q['id'], 'cidr': '10.1.0.0/24', 'ip_version': 4})
 
+        port_q = self.create_resource('port', proj_id, extra_res_fields={'network_id': pvt_subnet_q['network_id']})
+        port2_q = self.create_resource('port', proj_id, extra_res_fields={'network_id': pvt_subnet_q['network_id']})
 
-    def _update_resource(self, res_type, res_id, proj_id, name=None, extra_res_fields=None):
-        context = {'operation': 'UPDATE',
-                   'user_id': '',
-                   'is_admin': False,
-                   'roles': '',
-                   'tenant_id': proj_id}
-        if name:
-            res_name = name
-        else:
-            res_name = '%s-%s' %(res_type, str(uuid.uuid4()))
-
-        data = {'resource': {'name': res_name,
-                             'tenant_id': proj_id},
-                'id': res_id}
-        if extra_res_fields:
-            data['resource'].update(extra_res_fields)
-
-        body = {'context': context, 'data': data}
-        resp = self._api_svr_app.post_json('/neutron/%s' %(res_type), body)
-        res_q = json.loads(resp.text)
-        return res_name, res_q
-    # end _update_resource
-
-    def _add_router_interface(self, res_type, res_id, proj_id, name=None, extra_res_fields=None):
-        context = {'operation': 'ADDINTERFACE',
-                   'user_id': '',
-                   'is_admin': False,
-                   'roles': '',
-                   'tenant_id': proj_id}
-        if name:
-            res_name = name
-        else:
-            res_name = '%s-%s' %(res_type, str(uuid.uuid4()))
-
-        data = {'resource': {'name': res_name,
-                             'tenant_id': proj_id},
-                'id': res_id}
-        if extra_res_fields:
-            data['resource'].update(extra_res_fields)
-
-        body = {'context': context, 'data': data}
-        resp = self._api_svr_app.post_json('/neutron/%s' %(res_type), body)
-        res_q = json.loads(resp.text)
-        return res_name, res_q
-    # end _update_resource
+        #External gateway
+        router_q = self.update_resource('router', router_q['id'], proj_id, extra_res_fields={'external_gateway_info': {'network_id':net_q['id']}})
+        router_q = self.add_router_interface(router_q['id'], proj_id, extra_res_fields={'port_id': port2_q['id']})
+        return self.create_resource('floatingip', proj_id, extra_res_fields={'floating_network_id': net_q['id'], 'port_id':port_q['id']})
 
     #test when strict_compliance is ON
     def test_create_fip_and_associate_port_without_ext_gw(self):
         proj_obj = self._vnc_lib.project_read(fq_name=['default-domain', 'default-project'])
-        for res_type in ['security_group']:
-            res_name, res_q = getattr(self, '_create_' + res_type, lambda x:self._create_resource(res_type, x))(proj_obj.uuid)
-            res_list = self._list_resources(res_type, tenant_id=proj_obj.uuid, name=res_name)
+        res_q = self.create_resource('security_group', proj_obj.uuid)
+        self.list_resource('security_group', proj_uuid=proj_obj.uuid, req_filters={'name': res_q['name']})
 
         with ExpectedException(webtest.app.AppError):
             self._create_floatingip_and_associate_port_without_ext_gw(proj_obj.uuid)
- 
+
     #test when strict_compliance is ON
     def test_create_fip_and_associate_port_with_ext_gw(self):
         proj_obj = self._vnc_lib.project_read(fq_name=['default-domain', 'default-project'])
-        for res_type in ['security_group']:
-            res_name, res_q = getattr(self, '_create_' + res_type, lambda x:self._create_resource(res_type, x))(proj_obj.uuid)
-            res_list = self._list_resources(res_type, tenant_id=proj_obj.uuid, name=res_name)
+        res_q = self.create_resource('security_group', proj_obj.uuid)
+        self.list_resource('security_group', proj_uuid=proj_obj.uuid, req_filters={'name': res_q['name']})
 
         self._create_floatingip_and_associate_port_with_ext_gw(proj_obj.uuid)
-
-    def _list_resources(self, res_type, fields=None, tenant_id=None, name=None):
-        context = {'operation': 'READALL',
-                   'userid': '',
-                   'roles': '',
-                   'is_admin': False,
-                   'tenant': tenant_id,
-                   'tenant_id': tenant_id}
-
-        data = {'filters':{}, 'fields': None}
-        if name:
-            data.update({'filters': {'name': [name]}})
-        if fields:
-            data.update({'fields': fields})
-
-        body = {'context': context, 'data': data}
-        resp = self._api_svr_app.post_json('/neutron/%s' %(res_type), body)
-        res_q = json.loads(resp.text)
-        return res_q
-    # end _list_resources
-
 # end class TestStrictCompON
 
 
 class TestStrictCompOff(test_case.NeutronBackendTestCase):
-    @classmethod  
+    @classmethod
     def setUpClass(cls):
         super(TestStrictCompOff, cls).setUpClass(
             extra_config_knobs=[('NEUTRON', 'strict_compliance', False)])
     #end setUpClass
 
-    def _create_resource(self, res_type, proj_id, name=None, extra_res_fields=None):
-        context = {'operation': 'CREATE',
-                   'user_id': '',
-                   'is_admin': False,
-                   'roles': '',
-                   'tenant_id': proj_id}
-        if name:
-            res_name = name
-        else:
-            res_name = '%s-%s' %(res_type, str(uuid.uuid4()))
-        data = {'resource': {'name': res_name,
-                             'tenant_id': proj_id}}
-        if extra_res_fields:
-            data['resource'].update(extra_res_fields)
-
-        body = {'context': context, 'data': data}
-        resp = self._api_svr_app.post_json('/neutron/%s' %(res_type), body)
-        res_q = json.loads(resp.text)
-        return res_name, res_q
-    # end _create_resource
-    
-    def _create_floatingip_and_associate_port_without_ext_gw(self, proj_id, name=None):
+    def _create_floatingip_and_associate_port_without_ext_gw(self, proj_id):
         #external network
-        net_name, net_q = self._create_resource('network', proj_id, extra_res_fields={'router:external':True})
-        subnet_name, subnet_q = self._create_resource('subnet', proj_id, name, extra_res_fields={'network_id': net_q['id'], 'cidr': '10.2.0.0/24', 'ip_version': 4})
+        net_q = self.create_resource('network', proj_id, extra_res_fields={'router:external':True})
+        self.create_resource('subnet', proj_id, extra_res_fields={'network_id': net_q['id'], 'cidr': '10.2.0.0/24', 'ip_version': 4})
 
         #private network
-        pvt_net_name, pvt_net_q = self._create_resource('network', proj_id)
-        pvt_subnet_name, pvt_subnet_q = self._create_resource('subnet', proj_id, name, extra_res_fields={'network_id': pvt_net_q['id'], 'cidr': '10.1.0.0/24', 'ip_version': 4})
+        pvt_net_q = self.create_resource('network', proj_id)
+        pvt_subnet_q = self.create_resource('subnet', proj_id, extra_res_fields={'network_id': pvt_net_q['id'], 'cidr': '10.1.0.0/24', 'ip_version': 4})
 
-        port_name, port_q = self._create_resource('port', proj_id, name, extra_res_fields={'network_id': pvt_subnet_q['network_id']})
+        port_q = self.create_resource('port', proj_id, extra_res_fields={'network_id': pvt_subnet_q['network_id']})
 
-        return self._create_resource('floatingip', proj_id, name, extra_res_fields={'floating_network_id': net_q['id'], 'port_id':port_q['id']})
-    
+        return self.create_resource('floatingip', proj_id, extra_res_fields={'floating_network_id': net_q['id'], 'port_id':port_q['id']})
+
     #test when strict_compliance is OFF
     def test_create_fip_and_associate_port_without_ext_gw(self):
         proj_obj = self._vnc_lib.project_read(fq_name=['default-domain', 'default-project'])
-        for res_type in ['security_group']:
-            res_name, res_q = getattr(self, '_create_' + res_type, lambda x:self._create_resource(res_type, x))(proj_obj.uuid)
-            res_list = self._list_resources(res_type, tenant_id=proj_obj.uuid, name=res_name)
-
+        res_q = self.create_resource('security_group', proj_obj.uuid)
+        self.list_resource('security_group', proj_uuid=proj_obj.uuid, req_filters={'name': res_q['name']})
         self._create_floatingip_and_associate_port_without_ext_gw(proj_obj.uuid)
-
-    def _list_resources(self, res_type, fields=None, tenant_id=None, name=None):
-        context = {'operation': 'READALL',
-                   'userid': '',
-                   'roles': '',
-                   'is_admin': False,
-                   'tenant': tenant_id,
-                   'tenant_id': tenant_id}
-
-        data = {'filters':{}, 'fields': None}
-        if name:
-            data.update({'filters': {'name': [name]}})
-        if fields:
-            data.update({'fields': fields})
-
-        body = {'context': context, 'data': data}
-        resp = self._api_svr_app.post_json('/neutron/%s' %(res_type), body)
-        res_q = json.loads(resp.text)
-        return res_q
-    # end _list_resources
-
 # end class TestStrictCompOFF
