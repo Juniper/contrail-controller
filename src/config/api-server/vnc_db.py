57095ae13333ce24c9b5552e4e00d02f185453b0
Yijie Xiao <yixiao@juniper.net>
2018-10-09 14:38:51 -0700

    [config] Log VNC API indirect calls from VNC OpenStack
    Since patch Ieaa4881d5dd314cb6b722a0569949a4c7d42e3c7 all API method
    calls from VNC OpenStack code to VNC API are not logged as we shortcut
    VNC API HTTP code.
    
    Change-Id: I1ad8dc41f76725a190259a934e971fa57606f26f
    Closes-Bug: #1789467

--- src/config/api-server/vnc_db.py
+++ src/config/api-server/vnc_db.py
@@ -49,11 +49,11 @@ from pysandesh.gen_py.sandesh.ttypes import SandeshLevel
 from sandesh_common.vns.constants import USERAGENT_KEYSPACE_NAME
 from sandesh.traces.ttypes import DBRequestTrace, MessageBusNotifyTrace
 
-@ignore_exceptions
 def get_trace_id():
     try:
-        req_id = gevent.getcurrent().trace_request_id
-    except Exception:
+        req_id = get_request().headers.get(
+            'X-Request-Id', gevent.getcurrent().trace_request_id)
+    except AttributeError:
         req_id = 'req-%s' %(str(uuid.uuid4()))
         gevent.getcurrent().trace_request_id = req_id
 
