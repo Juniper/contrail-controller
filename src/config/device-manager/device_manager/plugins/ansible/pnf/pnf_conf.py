5862cfac134d1347ea3042fc1dcd9128d16b99cb
atandon <atandon@juniper.net>
2018-10-30 21:55:09 +0000

    [DM] - Updating local cache for new VNC objects - Defining new PNF plugin for PNF physical role
    Change-Id: I345af68d4a6c9feed599b64fb6bef9863a696fe7
    Partial-Bug: #1800701

---
+++ src/config/device-manager/device_manager/plugins/ansible/pnf/pnf_conf.py
@@ -0,0 +1,35 @@
+#
+# Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+#
+
+"""
+This file contains implementation of abstract config generation for PNFs
+"""
+
+from ansible_role_common import AnsibleRoleCommon
+from abstract_device_api.abstract_device_xsd import *
+
+class PnfConf(AnsibleRoleCommon):
+    _roles = ['pnf']
+
+    def __init__(self, logger, params={}):
+        super(PnfConf, self).__init__(logger, params)
+    # end __init__
+
+    @classmethod
+    def register(cls):
+        qconf = {
+              "roles": cls._roles,
+              "class": cls
+            }
+        return super(PnfConf, cls).register(qconf)
+    # end register
+
+    def push_conf(self, is_delete=False):
+        if not self.physical_router:
+            return 0
+        if is_delete:
+            return self.send_conf(is_delete=True)
+        self.set_common_config()
+        return self.send_conf()
+    # end push_conf
