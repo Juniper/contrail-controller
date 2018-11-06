71de0db89470548e4b0319c1241051ba232adbae
Andrey Pavlov <andrey.mp@gmail.com>
2018-11-06 15:37:18 +0300

    get pkg version via rpm instead of yum
    yum doesn't work even in cacheonly mode inside containers.
    repositories in built containers are not accessible in most cases.
    So 'rpm' utility should be used instead.
    
    Change-Id: I6380ce83e29ff6966fcb685acae7ed12d7f90369
    Closes-Bug: #1800729

--- src/nodemgr/common/event_manager.py
+++ src/nodemgr/common/event_manager.py
@@ -88,7 +88,6 @@ class EventManager(object):
         self.new_build_info = None
         self.last_cpu = None
         self.last_time = 0
-        self.own_version = None
         self.hostname = socket.gethostname()
         event_handlers = {}
         event_handlers['PROCESS_STATE'] = self.event_process_state
@@ -230,19 +229,13 @@ class EventManager(object):
         if self.curr_build_info is not None:
             return self.curr_build_info
 
-        command = "contrail-version contrail-nodemgr | grep contrail-nodemgr"
-        version = os.popen(command).read()
-        version_partials = version.split()
-        if len(version_partials) < 3:
-            self.msg_log('Not enough values to parse package version %s'
-                             % version,
-                         SandeshLevel.SYS_ERR)
-            return ""
-        else:
-            _, rpm_version, build_num = version_partials
+        pkg_version = self._get_package_version()
+        pkg_version_parts = pkg_version.split('-')
+        build_id = pkg_version_parts[0]
+        build_number = pkg_version_parts[1] if len(pkg_version_parts) > 1 else "unknown"
         self.new_build_info = build_info + '"build-id" : "' + \
-            rpm_version + '", "build-number" : "' + \
-            build_num + '"}]}'
+            build_id + '", "build-number" : "' + \
+            build_number + '"}]}'
         if (self.new_build_info != self.curr_build_info):
             self.curr_build_info = self.new_build_info
         return self.curr_build_info
@@ -478,12 +471,12 @@ class EventManager(object):
     # end send_nodemgr_process_status
 
     def _get_package_version(self):
-        own_version = utils.get_package_version('contrail-nodemgr')
-        if own_version is None:
+        pkg_version = utils.get_package_version('contrail-nodemgr')
+        if pkg_version is None:
             self.msg_log('Error getting %s package version' % (
                 'contrail-nodemgr'), SandeshLevel.SYS_ERR)
-            own_version = "package-version-unknown"
-        return own_version
+            pkg_version = "unknown"
+        return pkg_version
 
     def send_init_info(self, group_name):
         key = next(key for key in self.process_state_db[group_name])
@@ -501,10 +494,9 @@ class EventManager(object):
             build_info=self.get_build_info())
 
         # installed/running package version
-        own_version = self._get_package_version()
-        self.own_version = own_version
-        node_status.installed_package_version = own_version
-        node_status.running_package_version = own_version
+        pkg_version = self._get_package_version()
+        node_status.installed_package_version = pkg_version
+        node_status.running_package_version = pkg_version
 
         node_status_uve = NodeStatusUVE(table=self.type_info._object_table,
                                         data=node_status)
@@ -592,7 +584,6 @@ class EventManager(object):
 
     def event_tick_60(self):
         self.tick_count += 1
-        own_version = None
         for group in self.process_state_db:
             key = next(key for key in self.process_state_db[group])
             # get disk usage info periodically
@@ -630,16 +621,12 @@ class EventManager(object):
             if self.update_all_core_file():
                 node_status.all_core_file_list = self.all_core_file_list
 
-            own_version = self._get_package_version()
-            if (own_version != self.own_version):
-                node_status.own_version = own_version
             node_status_uve = NodeStatusUVE(table=self.type_info._object_table,
                                             data=node_status)
 
             self.msg_log('DBG: event_tick_60: node_status=%s' % node_status,
                          SandeshLevel.SYS_DEBUG)
             node_status_uve.send()
-        self.own_version = own_version
 
     def do_periodic_events(self):
         self.event_tick_60()
