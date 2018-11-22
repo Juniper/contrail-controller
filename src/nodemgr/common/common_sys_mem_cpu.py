ca4e714e9f14c3053dd8a36780c5b190b465a3d6
Michal Clapinski <michal.clapinski@codilime.com>
2018-11-22 17:17:02 +0100

    Implement pulling CPU and RAM info in nodemgr for Windows
    This feature was missing from Windows nodes.
    
    Change-Id: Iaa673574e0e8ba0c04157b218fed7b12bf3ff627
    Closes-Bug: #1804676

---
+++ src/nodemgr/common/common_sys_mem_cpu.py
@@ -0,0 +1,38 @@
+#
+# Copyright (c) 2016 Juniper Networks, Inc. All rights reserved.
+#
+
+import psutil
+
+class SysCpuShare(object):
+    def __init__(self, num_cpu):
+        self.last_cpu = 0
+        self.last_time = 0
+        self.num_cpu = num_cpu
+
+    def get(self):
+        last_cpu = self.last_cpu
+        last_time = self.last_time
+
+        current_cpu = psutil.cpu_times()
+        current_time = 0.00
+        for i in range(0, len(current_cpu) - 1):
+            current_time += current_cpu[i]
+
+        # tracking system/user time only
+        interval_time = 0
+        if last_cpu and (last_time != 0):
+            sys_time = current_cpu.system - last_cpu.system
+            usr_time = current_cpu.user - last_cpu.user
+            interval_time = current_time - last_time
+
+        self.last_cpu = current_cpu
+        self.last_time = current_time
+
+        if interval_time == 0:
+            return 0
+
+        sys_percent = 100 * sys_time / interval_time
+        usr_percent = 100 * usr_time / interval_time
+        cpu_share = round((sys_percent + usr_percent) / self.num_cpu, 2)
+        return cpu_share
--- src/nodemgr/common/windows_process_manager.py
+++ src/nodemgr/common/windows_process_manager.py
@@ -9,47 +9,28 @@ import common_process_manager as cpm
 from windows_process_mem_cpu import WindowsProcessMemCpuUsageData
 
 
-def _service_status_to_state(status):
-    if status == 'running':
-        return 'PROCESS_STATE_RUNNING'
-    else:
-        return 'PROCESS_STATE_STOPPED'
-
-def _get_service_by_name(name):
-    service = None
-    try:
-        service = psutil.win_service_get(name)
-    except:
-        pass
-    return service
-
-def _get_process_by_pid(pid):
-    process = None
-    try:
-        process = psutil.Process(pid)
-    except:
-        pass
-    return process
+def _get_process_by_name(name):
+    return next((proc for proc in psutil.process_iter() if proc.name() == name), None)
 
 class WindowsProcessInfoManager(object):
     def __init__(self, event_handlers):
         self._event_handlers = event_handlers
         self._process_info_cache = cpm.ProcessInfoCache()
+        self.pid = 0
 
     def get_mem_cpu_usage_data(self, pid, last_cpu, last_time):
         return WindowsProcessMemCpuUsageData(pid, last_cpu, last_time)
 
     def _poll_processes(self):
         agent_name = 'contrail-vrouter-agent'
-        agent_service = _get_service_by_name(agent_name)
+        agent_process = _get_process_by_name(agent_name + '.exe')
         info = cpm.dummy_process_info(agent_name)
-        if agent_service != None:
-            info['statename'] = _service_status_to_state(agent_service.status())
-            if info['statename'] == 'PROCESS_STATE_RUNNING':
-                info['pid'] = agent_service.pid()
-                agent_process = _get_process_by_pid(info['pid'])
-                if agent_process != None:
-                    info['start'] = str(int(agent_process.create_time() * 1000000))
+        if agent_process == None:
+            info['statename'] = 'PROCESS_STATE_STOPPED'
+        else:
+            info['statename'] = 'PROCESS_STATE_RUNNING'
+            info['pid'] = agent_process.pid
+            info['start'] = str(int(agent_process.create_time() * 1000000))
         return [info]
 
     def get_all_processes(self):
