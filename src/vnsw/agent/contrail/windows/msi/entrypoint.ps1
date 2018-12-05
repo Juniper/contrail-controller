e6a567b1474c16594743d4ef69181237180410aa
Michal Clapinski <michal.clapinski@codilime.com>
2018-12-05 13:51:01 +0100

    Make agent's entrypoint update agent's config on Windows
    In future we will generate the whole config from scratch
    as on Linux, but for now we only update the vhost's ifname.
    It's the only field that can change upon restart.
    
    Change-Id: Iec93bf6985295f9791b5a718c7b76cccaee7dacc
    Partial-Bug: #1806677

--- src/vnsw/agent/contrail/windows/msi/entrypoint.ps1
+++ src/vnsw/agent/contrail/windows/msi/entrypoint.ps1
@@ -1,1 +1,27 @@
+#
+# Copyright (c) 2018 Juniper Networks, Inc. All rights reserved.
+#
+
+$ErrorActionPreference = "Stop"
+
+$ConfPath = 'C:\ProgramData\Contrail\etc\contrail\contrail-vrouter-agent.conf'
+$VhostName = 'vEthernet (HNSTransparent)'
+
+$Conf = Get-Content $ConfPath
+$VhostIfname = (Get-NetAdapter -Name $VhostName).ifName
+
+for ($i = 0; $i -lt $Conf.Count; $i++) {
+    if ($Conf[$i] -match '^name=') {
+        break
+    }
+}
+
+if ($i -eq $Conf.Count) {
+    Write-Output 'Failed to update config'
+    Exit 1
+}
+
+$Conf[$i] = "name=$VhostIfname"
+Set-Content -Path $ConfPath -Value $Conf
+
 .\contrail-vrouter-agent.exe
