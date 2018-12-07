a773ceb7b0d7d05a4574f688aad3eb2db81927cf
Dheeraj Gautam <dgautam@juniper.net>
2018-12-07 12:39:52 -0800

    Add Support for: Server Discovery, Server Import and Node Profile Import
    Servers and Node Profiles can be imported from YAML or JSON Files
    
    Patches:
    Fixed Job Ctx to use Single Auth token end to end
    Fixed Try Catch Blocks
    Removed whitespaces
    Removed auth vars from group_vars/all.yml -> Should be in test code
    
    Remaining:
    IPMI Port Range format (Min Max)
    
    Partial-Bug: #1807762
    
    Change-Id: Ib487247d9e59a009bece95e36587762307c86f54

---
+++ src/config/fabric-ansible/ansible-playbooks/common/contrail_command.py
@@ -0,0 +1,159 @@
+#!/usr/bin/python
+
+import requests
+from urlparse import urlparse
+from keystoneauth1.identity import v3
+from keystoneauth1 import session
+from keystoneclient.v3 import client
+import json
+import pprint
+
+
+class CreateCCResource(object):
+
+    auth_token = None
+    auth_url = None
+    auth_uri = None
+
+    keystone_auth = None
+    keystone_session = None
+    keystone_client = None
+
+    # Default Auth Args
+    auth_args = {
+        'auth_url': None,
+        'auth_host': None,
+        'auth_protocol': 'https',
+        'username': "",
+        'password': "",
+        'auth_port': '9091',
+        'user_domain_name': 'default',
+        'project_domain_name': 'default',
+        'project_name': 'admin',
+        'auth_path': "/keystone/v3"
+    }
+
+    auth_headers = {
+        'Content-Type': 'application/json'
+    }
+
+    def __init__(self, auth_host, auth_token):
+        if not auth_host:
+            return
+        self.auth_args['auth_host'] = auth_host
+
+        if not self.auth_args['auth_url']:
+            self.auth_url = '%s://%s:%s%s' % (self.auth_args['auth_protocol'],
+                                              self.auth_args['auth_host'],
+                                              self.auth_args['auth_port'],
+                                              self.auth_args[
+                                                  'auth_path'])
+            self.auth_uri = '%s://%s:%s' % (self.auth_args['auth_protocol'],
+                                              self.auth_args['auth_host'],
+                                              self.auth_args['auth_port'])
+        else:
+            self.auth_url = self.auth_args['auth_url']
+            parsed_auth_url = urlparse(self.auth_url)
+            self.auth_args['auth_protocol'] = parsed_auth_url.scheme
+            self.auth_args['auth_host'] = parsed_auth_url.hostname
+            self.auth_args['auth_port'] = parsed_auth_url.port
+            self.auth_args['auth_url_version'] = parsed_auth_url.path
+            self.auth_uri = '%s://%s:%s' % (self.auth_args['auth_protocol'],
+                                              self.auth_args['auth_host'],
+                                              self.auth_args['auth_port'])
+
+        if not auth_token and self.auth_args['username'] and \
+                self.auth_args['password']:
+            self.set_ks_auth_sess()
+            self.auth_token = self.keystone_session.get_token()
+        elif auth_token:
+            self.auth_token = auth_token
+        self.auth_headers['X-Auth-Token'] = self.auth_token
+        import pdb; pdb.set_trace()
+
+    def set_ks_auth_sess(self):
+        self.keystone_auth = v3.Password(
+            auth_url=self.auth_url,
+            username=self.auth_args['username'],
+            password=self.auth_args['password'],
+            project_name=self.auth_args['project_name'],
+            user_domain_name=self.auth_args['user_domain_name'],
+            project_domain_name=self.auth_args['project_domain_name']
+        )
+        self.keystone_session = session.Session(
+            auth=self.keystone_auth,
+            verify=False)
+        self.keystone_client = client.Client(
+            session=self.keystone_session)
+
+    def get_rest_api_response(self, url, headers, data=None, request_type=None):
+        response = None
+        print data
+        if request_type == "post":
+            response = requests.post(url, headers=headers, data=data,
+                                     verify=False)
+        elif request_type == "get":
+            response = requests.get(url, headers=headers, data=data,
+                                        verify=False)
+        response.raise_for_status()
+        return response
+
+    def create_cc_resource(self, resource_payload):
+        cc_url = '%s%s' % (self.auth_uri, '/sync')
+        response = self.get_rest_api_response(cc_url,
+                                              headers=self.auth_headers,
+                                              data=json.dumps(resource_payload),
+                                              request_type="post")
+        return response.content
+
+
+class CreateCCNode(CreateCCResource):
+
+    def __init__(self, auth_host, auth_token):
+        super(CreateCCNode, self).__init__(auth_host, auth_token)
+
+    def create_cc_node(self, node_payload):
+
+        response = self.create_cc_resource(node_payload)
+        return response
+
+    def get_cc_nodes(self ):
+        cc_url = '%s%s' % (self.auth_uri, '/nodes?detail=true')
+        response = self.get_rest_api_response(cc_url,
+                                              headers=self.auth_headers,
+                                              request_type="get")
+        pprint.pprint(json.loads(response.content))
+        return json.loads(response.content)
+
+
+class CreateCCNodeProfile(CreateCCResource):
+
+    def __init__(self, auth_host, auth_token):
+        super(CreateCCNodeProfile, self).__init__(auth_host, auth_token)
+
+    def create_cc_node_profile(self, node_profile_payload):
+        response = self.create_cc_resource(node_profile_payload)
+        return response
+
+    def get_cc_node_profiles(self):
+        cc_url = '%s%s' % (self.auth_uri, '/node_profiles?detail=true')
+        response = self.get_rest_api_response(cc_url,
+                                              headers=self.auth_headers,
+                                              request_type="get")
+        pprint.pprint(json.loads(response.content))
+        return json.loads(response.content)
+
+
+def main(cc_auth_args=None):
+    return
+
+if __name__ == '__main__':
+    my_auth_args = {
+        'auth_host': "1.1.1.1",
+        'username': "admin",
+        'password': "password"
+    }
+    try:
+        main(cc_auth_args=my_auth_args)
+    except Exception as e:
+        print e.message
