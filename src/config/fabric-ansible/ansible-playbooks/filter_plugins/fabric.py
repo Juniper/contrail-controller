298f3939b81b8d947e06ba637e70cd5d9c22d150
Joe Williams <josephw@juniper.net>
2019-01-09 14:17:15 -0800

    [DM] Populate default annotations on fabric object
    - Example annotations listed in bug CF-44 comments
    - Currently annotations only used for hitless upgrade and fabric creation
    
    closes-jira-bug: CF-44
    
    Change-Id: Ic0fbffe0699a581e0f8e255b2dd124a1aa6eb5fa

--- src/config/fabric-ansible/ansible-playbooks/filter_plugins/fabric.py
+++ src/config/fabric-ansible/ansible-playbooks/filter_plugins/fabric.py
@@ -15,7 +15,7 @@ import uuid
 from netaddr import IPNetwork
 import jsonschema
 
-from job_manager.job_utils import JobVncApi
+from job_manager.job_utils import JobVncApi, JobAnnotations
 
 from cfgm_common.exceptions import (
     RefsExistError,
@@ -219,7 +219,7 @@ class FilterModule(object):
                 }
             ]
 
-        return job_input
+        return job_input, job_template_fqname
     # end _validate_job_ctx
 
     @staticmethod
@@ -353,10 +353,11 @@ class FilterModule(object):
         try:
             FilterLog.instance("FabricOnboardFilter")
             vnc_api = JobVncApi.vnc_init(job_ctx)
-            fabric_info = FilterModule._validate_job_ctx(
+            fabric_info, job_template_fqname = FilterModule._validate_job_ctx(
                 vnc_api, job_ctx, False
             )
-            fabric_obj = self._onboard_fabric(vnc_api, fabric_info, True)
+            fabric_obj = self._onboard_fabric(vnc_api, fabric_info, True,
+                                              job_template_fqname)
             return {
                 'status': 'success',
                 'fabric_uuid': fabric_obj.uuid,
@@ -421,10 +422,11 @@ class FilterModule(object):
         try:
             FilterLog.instance("FabricOnboardBrownfieldFilter")
             vnc_api = JobVncApi.vnc_init(job_ctx)
-            fabric_info = FilterModule._validate_job_ctx(
-                vnc_api, job_ctx, True
+            fabric_info, job_template_fqname = FilterModule._validate_job_ctx(
+                vnc_api, job_ctx, False
             )
-            fabric_obj = self._onboard_fabric(vnc_api, fabric_info, False)
+            fabric_obj = self._onboard_fabric(vnc_api, fabric_info, False,
+                                              job_template_fqname)
             return {
                 'status': 'success',
                 'fabric_uuid': fabric_obj.uuid,
@@ -443,14 +445,23 @@ class FilterModule(object):
             }
     # end onboard_existing_fabric
 
-    def _onboard_fabric(self, vnc_api, fabric_info, ztp):
+    def _onboard_fabric(self, vnc_api, fabric_info, ztp, job_template_fqname):
         """
         :param vnc_api: <vnc_api.VncApi>
         :param fabric_info: Dictionary
         :param ztp: set to True if fabric is to be ZTPed
+        :param job_template_fqname: job template fqname
         :return: <vnc_api.gen.resource_client.Fabric>
         """
         fabric_obj = self._create_fabric(vnc_api, fabric_info, ztp)
+
+        # add fabric annotations
+        self._add_fabric_annotations(
+            vnc_api,
+            fabric_obj,
+            job_template_fqname,
+            fabric_info
+        )
 
         # management network
         mgmt_subnets = [
@@ -911,6 +922,28 @@ class FilterModule(object):
         _task_done()
         return network
     # end _add_fabric_vn
+
+    def _add_fabric_annotations(self, vnc_api, fabric_obj,
+                                onboard_job_template_fqname, job_input):
+        """
+        :param vnc_api: <vnc_api.VncApi>
+        :param fabric_obj: <vnc_api.gen.resource_client.Fabric>
+        :param onboard_job_template_fqname: string
+        :param job_input: dict
+        """
+        # First add default annotations for all job templates
+        jt_refs = vnc_api.job_templates_list().get('job-templates')
+        ja = JobAnnotations(vnc_api)
+        for jt_ref in jt_refs:
+            job_template_fqname = jt_ref.get('fq_name')
+            def_job_input = ja.generate_default_json(job_template_fqname)
+            ja.cache_job_input(fabric_obj.uuid, job_template_fqname[-1],
+                               def_job_input)
+
+        # Now update the fabric onboarding annotation with the current
+        # job input
+        ja.cache_job_input(fabric_obj.uuid, onboard_job_template_fqname[-1],
+                           job_input)
 
     # ***************** delete_fabric filter **********************************
     def delete_fabric(self, job_ctx):
