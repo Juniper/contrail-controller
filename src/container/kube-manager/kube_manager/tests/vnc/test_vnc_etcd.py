f9f399368ae40b294bdcb76ac6a4c435aec52cfb
Pawel Marchewka <pawel.marchewka@codilime.com>
2019-01-15 09:24:24 +0100

    Use APIServer for route target number allocation
    Use APIServer for route target number allocation
    Store routing instance route target number association in ETCD
    
    Depends-on: I1238b5f21e1500fa276ddd3a342864be03be95d7
    Change-Id: I4dd8d47b1ad3a65f927a66cda33b91ed974d5955
    closes-jira-bug: JBE-788

--- src/container/kube-manager/kube_manager/tests/vnc/test_vnc_etcd.py
+++ src/container/kube-manager/kube_manager/tests/vnc/test_vnc_etcd.py
@@ -12,10 +12,10 @@ from cfgm_common.vnc_etcd import EtcdCache, VncEtcd, _handle_conn_error
 
 
 def _vnc_etcd_factory(host='etcd-host-01', port='2379', prefix='/contrail',
-                      logger=None, obj_cache_exclude_types=None,
+                      kv_store='/vnc', logger=None, obj_cache_exclude_types=None,
                       log_response_time=None, credentials=None):
     """VncEtcd factory function for testing only."""
-    return VncEtcd(host=host, port=port, prefix=prefix, logger=logger,
+    return VncEtcd(host=host, port=port, prefix=prefix, kv_store=kv_store, logger=logger,
                    obj_cache_exclude_types=obj_cache_exclude_types,
                    log_response_time=log_response_time, credentials=credentials)
 
@@ -27,6 +27,7 @@ class VncEtcdTest(unittest.TestCase):
         self.assertEqual(vnc_etcd._host, 'etcd-host-01')
         self.assertEqual(vnc_etcd._port, '2379')
         self.assertEqual(vnc_etcd._prefix, '/contrail')
+        self.assertEqual(vnc_etcd._kv_store, '/vnc')
         self.assertIsNone(vnc_etcd._logger)
         self.assertIsNone(vnc_etcd._credentials)
         self.assertIsInstance(vnc_etcd._client, etcd3.Etcd3Client)
