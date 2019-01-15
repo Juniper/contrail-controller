13f08e5203eee399389338afc6d9aa001bc3816e
Mateusz Neumann <mateusz.neumann@codilime.com>
2019-01-15 09:55:58 +0100

    Use etcd for service chain ip storage
    Due to migration of Schema Transformer to etcd, three more methods:
    'get_service_chain_ip', 'add_service_chain_ip' and
    'remove_service_chain_ip' are implemented with etcd client.
    
    Change-Id: Ie9e4d38602dfac0998063dac1c2c31a9b229b35d
    closes-jira-bug: JBE-791

--- src/config/schema-transformer/etcd.py
+++ src/config/schema-transformer/etcd.py
@@ -7,6 +7,8 @@
 Schema transformer DB for etcd to allocate ids and store internal data
 """
 
+import re
+
 from cfgm_common import vnc_object_db, vnc_etcd
 from vnc_api.exceptions import RefsExistError
 
@@ -19,8 +21,10 @@ class RTHandler(object):
 
 class SchemaTransformerEtcd(vnc_object_db.VncObjectEtcdClient):
 
-    _ETCD_SERVICE_CHAIN_PATH = "schema_transformer/service_chain"
+    _ETCD_SCHEMA_TRANSFORMER_PREFIX = "schema_transformer"
     _BGPAAS_PORT_ALLOC_POOL = "bgpaas_port"
+    _ETCD_SERVICE_CHAIN_UUID_PATH = "service_chain_uuid"
+    _ETCD_SERVICE_CHAIN_IP_PATH = "service_chain_ip"
 
     def __init__(self, args, vnc_lib, logger):
         self._args = args
@@ -28,12 +32,14 @@ class SchemaTransformerEtcd(vnc_object_db.VncObjectEtcdClient):
         self._logger = logger
         self._vnc_lib = vnc_lib
         self._rt_cf = RTHandler()
-        self._logger.log(
-            "VncObjectEtcdClient arguments: {}".format(self._etcd_args))
+        logger_log = None
+        if self._logger:
+            self._logger.log(
+                "VncObjectEtcdClient arguments: {}".format(self._etcd_args))
+            logger_log = self._logger.log
         self._init_bgpaas_ports_index_allocator()
-
         super(SchemaTransformerEtcd, self).__init__(
-            logger=self._logger.log, **self._etcd_args)
+            logger=logger_log, **self._etcd_args)
 
     def _init_bgpaas_ports_index_allocator(self):
         bgpaas_port_start = self._args.bgpaas_port_start
@@ -61,13 +67,21 @@ class SchemaTransformerEtcd(vnc_object_db.VncObjectEtcdClient):
         """
         return {'value': obj}
 
-    def _path_key(self, key):
+    def _strip_sc_name(self, path, sc_name):
+        """Strip prefix from etcd key."""
+        pattern = r"^.*\/" + \
+            re.escape(self._ETCD_SCHEMA_TRANSFORMER_PREFIX) + \
+            r"\/[^/]+\/" + re.escape(sc_name) + r"\/"
+        return re.sub(pattern, '', path)
+
+    def _path_key(self, resource, key):
         """Combine and return self._prefix with given key.
 
         :param (str) key: key of an
-        :return: (str) full key ie: "/contrail/virtual_network/aaa-bbb-ccc"
+        :param (str) resource: resourece prefix
+        :return: (str) full key ie: "schema_transformer/service_chain_uuid/13eb9327-f40e-4ef1-8020-1c36af1b4b70"
         """
-        return "%s/%s" % (self._ETCD_SERVICE_CHAIN_PATH, key)
+        return "%s/%s/%s" % (self._ETCD_SCHEMA_TRANSFORMER_PREFIX, resource, key)
 
     def allocate_service_chain_vlan(self, service_vm, service_chain):
         # Since vlan tag 0 is not valid, return 1
@@ -90,43 +104,64 @@ class SchemaTransformerEtcd(vnc_object_db.VncObjectEtcdClient):
         pass
 
     def get_service_chain_ip(self, sc_name):
-        # TODO
-        return 0
+        """List all service chain ip objects for sc_name.
+
+        :param sc_name (str): ip service chain name
+        :return: (dict) dictionary of IP data stored for sc_name
+        """
+        response = self._object_db.list_kv(
+            self._path_key(self._ETCD_SERVICE_CHAIN_IP_PATH, sc_name))
+        return {self._strip_sc_name(key, sc_name): value for (key, value) in response}
 
     def add_service_chain_ip(self, sc_name, ip_dict):
-        # TODO
-        pass
+        """Store multiple values from ip_dict in etcd.
+
+        For each ip_dict entry create a single entry in etcd.  Each etc
+        key is created by combining prefix, _ETC_SERVICE_CHAIN_IP_PATH,
+        sc_name and ip_dict key.
+
+        :param sc_name (str): ip service chain name
+        :param ip_dict (dict): dictionary to be stored
+        """
+        key_path = self._path_key(self._ETCD_SERVICE_CHAIN_IP_PATH, sc_name)
+        for k, v in ip_dict.items():
+            self._object_db.put_kv("%s/%s" % (key_path, k), v)
 
     def remove_service_chain_ip(self, sc_name):
-        # TODO
-        pass
+        """Remove all data for sc_name in etcd.
+
+        :param sc_name (str): ip service chain name
+        """
+        self._object_db.delete_path(self._path_key(
+            self._ETCD_SERVICE_CHAIN_IP_PATH, sc_name))
 
     def list_service_chain_uuid(self):
         """List all objects with 'service_chain' object type.
 
         :return: (gen) Generator of fetched objects
         """
-        response = self._object_db.list_kv(self._ETCD_SERVICE_CHAIN_PATH)
-        return (self._patch_encapsulate_value(value) for key, value in response)
+        response = self._object_db.list_kv(
+            "%s/%s" % (self._ETCD_SCHEMA_TRANSFORMER_PREFIX, self._ETCD_SERVICE_CHAIN_UUID_PATH))
+        return (self._patch_encapsulate_value(value) for _, value in response)
 
     def add_service_chain_uuid(self, key, value):
         """Store a value with a key created from combining prefix,
-        _ETCD_SERVICE_CHAIN_PATH and key in etcd.
+        _ETCD_SERVICE_CHAIN_UUID_PATH and key in etcd.
 
         :param key (str): identifier (UUID)
         :param value (Any): data to store, typically a string (object
                             serialized with jsonpickle.encode)
         """
-        key_path = self._path_key(key)
+        key_path = self._path_key(self._ETCD_SERVICE_CHAIN_UUID_PATH, key)
         self._object_db.put_kv(key_path, value)
 
     def remove_service_chain_uuid(self, key):
         """Remove a etcd entry with key created from combining prefix,
-        _ETCD_SERVICE_CHAIN_PATH and key.
+        _ETCD_SERVICE_CHAIN_UUID_PATH and key.
 
         :param key (str): identifier (UUID)
         """
-        key_path = self._path_key(key)
+        key_path = self._path_key(self._ETCD_SERVICE_CHAIN_UUID_PATH, key)
         self._object_db.delete_kv(key_path)
 
     def alloc_bgpaas_port(self, *args, **kwargs):
