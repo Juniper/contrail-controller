8c2d9067e855b43474eed860edf9196a8759f936
aadarshk <aadarshk@juniper.net>
2019-01-16 16:10:01 +0530

    Merging changes from 5.x to 3.1
    Node manager fixes to work with systemd
    Changes to make node manager work with both systemd
    and supervisord. Abstracted the process information
    manager portion of node manager so that it can work
    with both systems. Refactor EventManager so that the
    node type specific code is abstracted out. Change
    supervisord event TICK_60 to a periodic instead so
    that common code can be used between supervisord and
    systemd.
    
    The systemd process info manager code uses pydbus
    which will be installed as part of another commit.
    
    DocImpact
    Partial-Bug: #1651777
    Implements: blueprint libvirt-xml-cpu-model
    
    Change-Id: Iad3afb2ecc0340ff8ec5eb19fd64590f088e6153

--- src/nodemgr/config_nodemgr/config_event_manager.py
+++ src/nodemgr/config_nodemgr/config_event_manager.py
@@ -4,87 +4,44 @@
 
 from gevent import monkey
 monkey.patch_all()
-import os
-import sys
-import socket
-import subprocess
-import json
-import time
-import datetime
-import platform
-import select
-import gevent
-import ConfigParser
 
-from nodemgr.common.event_manager import EventManager, package_installed
+from nodemgr.common.event_manager import EventManager, EventManagerTypeInfo, \
+    package_installed
 from nodemgr.common.cassandra_manager import CassandraManager
-
-from ConfigParser import NoOptionError
-
-from supervisor import childutils
-
-from pysandesh.sandesh_base import *
-from pysandesh.sandesh_session import SandeshWriter
 from pysandesh.sandesh_logger import SandeshLogger
-from pysandesh.gen_py.sandesh_trace.ttypes import SandeshTraceRequest
-from sandesh_common.vns.ttypes import Module, NodeType
-from sandesh_common.vns.constants import ModuleNames, NodeTypeNames,\
-    Module2NodeType, UVENodeTypeNames
 
-from nodemgr.common.sandesh.nodeinfo.ttypes import *
-from nodemgr.common.sandesh.nodeinfo.cpuinfo.ttypes import *
-from nodemgr.common.sandesh.nodeinfo.process_info.ttypes import *
-from nodemgr.common.sandesh.nodeinfo.process_info.constants import *
-from pysandesh.connection_info import ConnectionState
+
+from pysandesh.sandesh_base import sandesh_global
+from sandesh_common.vns.ttypes import Module
 
 class ConfigEventManager(EventManager):
-    def __init__(self, rule_file, discovery_server,
+    def __init__(self, rule_file,unit_names,discovery_server,
                  discovery_port, collector_addr,
                  hostip, db_port, minimum_diskgb, contrail_databases,
                  cassandra_repair_interval,
                  cassandra_repair_logdir):
-        self.node_type = "contrail-config"
-        self.uve_node_type = UVENodeTypeNames[NodeType.CONFIG]
-        self.table = "ObjectConfigNode"
-        self.module = Module.CONFIG_NODE_MGR
-        self.module_id = ModuleNames[self.module]
+        type_info = EventManagerTypeInfo(package_name = 'contrail-config',
+            module_type = Module.CONFIG_NODE_MGR,
+            object_table = 'ObjectConfigNode',
+            supervisor_serverurl = 'unix:///var/run/supervisord_config.sock',
+            unit_names = unit_names)
+        EventManager.__init__(
+            self, type_info, rule_file, discovery_server,
+            discovery_port, collector_addr, sandesh_global)
         self.cassandra_repair_interval = cassandra_repair_interval
         self.cassandra_repair_logdir = cassandra_repair_logdir
         self.hostip = hostip
         self.db_port = db_port
         self.minimum_diskgb = minimum_diskgb
+        self.sandesh_global = sandesh_global #AddedJan24
         self.contrail_databases = contrail_databases
         self.cassandra_mgr = CassandraManager(self.cassandra_repair_logdir,
-                                              'configDb', self.table,
-                                              self.contrail_databases,
+                                              'configDb',self.contrail_databases,
                                               self.hostip, self.minimum_diskgb,
                                               self.db_port)
-        if os.path.exists('/tmp/supervisord_config.sock'):
-            self.supervisor_serverurl = "unix:///tmp/supervisord_config.sock"
-        else:
-            self.supervisor_serverurl = "unix:///var/run/supervisord_config.sock"
 
         self.db = package_installed('contrail-openstack-database')
         self.config_db = package_installed('contrail-database-common')
-        self.add_current_process()
-        node_type = Module2NodeType[self.module]
-        node_type_name = NodeTypeNames[node_type]
-        self.sandesh_global = sandesh_global
-        EventManager.__init__(
-            self, rule_file, discovery_server,
-            discovery_port, collector_addr, sandesh_global)
-        _disc = self.get_discovery_client()
-        sandesh_global.init_generator(
-            self.module_id, socket.gethostname(),
-            node_type_name, self.instance_id, self.collector_addr,
-            self.module_id, 8100, ['database.sandesh', 'nodemgr.common.sandesh'], _disc)
-        sandesh_global.set_logging_params(enable_local_log=True)
-        ConnectionState.init(sandesh_global, socket.gethostname(),
-		self.module_id, self.instance_id,
-		staticmethod(ConnectionState.get_process_state_cb),
-                NodeStatusUVE, NodeStatus, self.table)
-        self.send_system_cpu_info()
-        self.third_party_process_dict = {}
         self.third_party_process_dict["cassandra"] = "Dcassandra-pidfile=.*cassandra\.pid"
         self.third_party_process_dict["zookeeper"] = "org.apache.zookeeper.server.quorum.QuorumPeerMain"
     # end __init__
@@ -93,30 +50,6 @@ class ConfigEventManager(EventManager):
         self.sandesh_global.logger().log(SandeshLogger.get_py_logger_level(
                             level), msg)
 
-    def process(self):
-        if self.rule_file is '':
-            self.rule_file = "/etc/contrail/" + \
-                "supervisord_config_files/contrail-config.rules"
-        json_file = open(self.rule_file)
-        self.rules_data = json.load(json_file)
-        if not self.db and self.config_db:
-            self.cassandra_mgr.process(self)
-
-    def send_process_state_db(self, group_names):
-        self.send_process_state_db_base(
-            group_names, ProcessInfo)
-
-    def send_nodemgr_process_status(self):
-        self.send_nodemgr_process_status_base(
-            ProcessStateNames, ProcessState, ProcessStatus)
-
-    def get_node_third_party_process_dict(self):
-        return self.third_party_process_dict 
-
-    def get_process_state(self, fail_status_bits):
-        return self.get_process_state_base(
-            fail_status_bits, ProcessStateNames, ProcessState)
-
     def do_periodic_events(self):
         if not self.db and self.config_db:
             self.cassandra_mgr.database_periodic(self)
@@ -124,6 +57,7 @@ class ConfigEventManager(EventManager):
             if self.tick_count % (60 * self.cassandra_repair_interval) == 0:
                 self.cassandra_mgr.repair()
         self.event_tick_60()
+     # end do_periodic_events
 
     def get_failbits_nodespecific_desc(self, fail_status_bits):
         description = ""
