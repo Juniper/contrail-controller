488681d6bb6cf0c6112049d9acd4f3ada19edd22
aadarshk <aadarshk@juniper.net>
2019-01-16 16:10:01 +0530

    Merging changes from 5.x to 3.1
    Node manager fixes to work with systemd
    Changes to make node manager work with both systemd
    and supervisord. Abstracted the process information
    manager portion of node manager so that it can work
    with both systems. Refactor EventManager so that the
    node type specific code is abstracted out. Change
    supervisord event TICK_60 to a periodic instead so
    that common code can be used between supervisord and
    systemd.
    
    The systemd process info manager code uses pydbus
    which will be installed as part of another commit.
    
    DocImpact
    Partial-Bug: #1651777
    Implements: blueprint libvirt-xml-cpu-model
    
    Change-Id: Iad3afb2ecc0340ff8ec5eb19fd64590f088e6153

--- src/nodemgr/vrouter_nodemgr/vrouter_event_manager.py
+++ src/nodemgr/vrouter_nodemgr/vrouter_event_manager.py
@@ -4,147 +4,48 @@
 
 from gevent import monkey
 monkey.patch_all()
-import os
-import sys
-import socket
-import subprocess
-import json
-import time
-import datetime
-import platform
-import select
-import gevent
-import ConfigParser
 
-from nodemgr.common.event_manager import EventManager
+from nodemgr.common.event_manager import EventManager, EventManagerTypeInfo
 from nodemgr.vrouter_nodemgr.vrouter_process_stat import VrouterProcessStat
 
-from ConfigParser import NoOptionError
 
-from supervisor import childutils
 
-from pysandesh.sandesh_base import *
-from pysandesh.gen_py.sandesh.ttypes import SandeshLevel
-from pysandesh.sandesh_session import SandeshWriter
-from pysandesh.sandesh_logger import SandeshLogger
-from pysandesh.gen_py.sandesh_trace.ttypes import SandeshTraceRequest
-from sandesh_common.vns.ttypes import Module, NodeType
-from sandesh_common.vns.constants import ModuleNames, NodeTypeNames,\
-    Module2NodeType, INSTANCE_ID_DEFAULT, UVENodeTypeNames
-from subprocess import Popen, PIPE
-from StringIO import StringIO
 
-from nodemgr.common.sandesh.nodeinfo.ttypes import *
-from nodemgr.common.sandesh.nodeinfo.cpuinfo.ttypes import *
-from nodemgr.common.sandesh.nodeinfo.process_info.ttypes import *
-from nodemgr.common.sandesh.nodeinfo.process_info.constants import *
-from pysandesh.connection_info import ConnectionState
-
+from pysandesh.sandesh_base import sandesh_global
+from sandesh_common.vns.ttypes import Module
 from loadbalancer_stats import LoadbalancerStatsUVE
 
 
 class VrouterEventManager(EventManager):
-    def __init__(self, rule_file, discovery_server,
+    def __init__(self, rule_file,unit_names, discovery_server,
                  discovery_port, collector_addr):
-        self.module = Module.COMPUTE_NODE_MGR
-        self.module_id = ModuleNames[self.module]
 
-        node_type = Module2NodeType[self.module]
-        node_type_name = NodeTypeNames[node_type]
-        self.sandesh_global = sandesh_global
-        EventManager.__init__(self, rule_file, discovery_server,
-                              discovery_port, collector_addr, sandesh_global)
-        self.node_type = "contrail-vrouter"
-        self.uve_node_type = UVENodeTypeNames[NodeType.COMPUTE]
-        self.table = "ObjectVRouter"
-        _disc = self.get_discovery_client()
-        sandesh_global.init_generator(
-            self.module_id, socket.gethostname(),
-            node_type_name, self.instance_id, self.collector_addr,
-            self.module_id, 8102, ['vrouter.loadbalancer',
-                'nodemgr.common.sandesh'], _disc)
-        sandesh_global.set_logging_params(enable_local_log=True)
-        if os.path.exists('/tmp/supervisord_vrouter.sock'):
-            self.supervisor_serverurl = "unix:///tmp/supervisord_vrouter.sock"
-        else:
-            self.supervisor_serverurl = "unix:///var/run/supervisord_vrouter.sock"
-        self.add_current_process()
-        ConnectionState.init(sandesh_global, socket.gethostname(), self.module_id,
-            self.instance_id,
-            staticmethod(ConnectionState.get_process_state_cb),
-            NodeStatusUVE, NodeStatus, self.table)
 
+        type_info = EventManagerTypeInfo(
+            package_name = 'contrail-vrouter-common',
+            module_type = Module.COMPUTE_NODE_MGR,
+            object_table = 'ObjectVRouter',
+            supervisor_serverurl = "unix:///var/run/supervisord_vrouter.sock",
+            unit_names = unit_names)
+        EventManager.__init__(self, type_info, rule_file, discovery_server,
+                              discovery_port, collector_addr, sandesh_global,
+                              sandesh_config, update_process_list = True)
         self.lb_stats = LoadbalancerStatsUVE()
         self.send_system_cpu_info()
-        self.third_party_process_dict = {}
     # end __init__
 
-    def msg_log(self, msg, level):
-        self.sandesh_global.logger().log(SandeshLogger.get_py_logger_level(
-                            level), msg)
 
-    def process(self):
-        if self.rule_file is '':
-            self.rule_file = \
-                "/etc/contrail/supervisord_vrouter_files/" + \
-                "contrail-vrouter.rules"
-        json_file = open(self.rule_file)
-        self.rules_data = json.load(json_file)
 
-    def send_process_state_db(self, group_names):
-        self.send_process_state_db_base(
-            group_names, ProcessInfo)
-
-    def send_nodemgr_process_status(self):
-        self.send_nodemgr_process_status_base(
-            ProcessStateNames, ProcessState, ProcessStatus)
-
-    def get_node_third_party_process_dict(self):
-        return self.third_party_process_dict 
-
-    def get_process_state(self, fail_status_bits):
-        return self.get_process_state_base(
-            fail_status_bits, ProcessStateNames, ProcessState)
 
     def get_process_stat_object(self, pname):
         return VrouterProcessStat(pname)
+    # end get_process_stat_object
+    
+    def do_periodic_events(self):
+        self.event_tick_60()
+        # loadbalancer processing
+        self.lb_stats.send_loadbalancer_stats()
+    # end do_periodic_events
 
-    # overridden delete_process_handler -
-    def delete_process_handler(self, deleted_process):
-        super(VrouterEventManager,
-              self).delete_process_handler(deleted_process)
-    # end delete_process_handler
 
-    def runforever(self, test=False):
-        self.prev_current_time = int(time.time())
-        while 1:
-            # we explicitly use self.stdin, self.stdout, and self.stderr
-            # instead of sys.* so we can unit test this code
-            headers, payload = \
-                self.listener_nodemgr.wait(self.stdin, self.stdout)
-
-            # self.stderr.write("headers:\n" + str(headers) + '\n')
-            # self.stderr.write("payload:\n" + str(payload) + '\n')
-
-            pheaders, pdata = childutils.eventdata(payload + '\n')
-            # self.stderr.write("pheaders:\n" + str(pheaders)+'\n')
-            # self.stderr.write("pdata:\n" + str(pdata))
-
-            # check for process state change events
-            if headers['eventname'].startswith("PROCESS_STATE"):
-                self.event_process_state(pheaders, headers)
-                # check for addition / deletion of processes in the node.
-                # Tor Agent process can get added / deleted based on need.
-                self.update_current_process()
-
-            # check for flag value change events
-            if headers['eventname'].startswith("PROCESS_COMMUNICATION"):
-                self.event_process_communication(pdata)
-            # do periodic events
-            if headers['eventname'].startswith("TICK_60"):
-                self.event_tick_60()
-
-                # loadbalancer processing
-                self.lb_stats.send_loadbalancer_stats()
-
-            self.listener_nodemgr.ok(self.stdout)
+# end class VrouterEventManager 
