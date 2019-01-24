cfd109f21540b47c857f77b3ca738b885e1de046
Maciej Łastawiecki <maciej.lastawiecki@codilime.com>
2019-01-24 18:39:23 +0100

    [WIP] SVC_monitor locate refactor
    Change-Id: Ifebaf47cf4b071104b173e3bbc9c8693e29c294c
    closes-jira-bug: JBE-871

--- src/config/common/utils.py
+++ src/config/common/utils.py
@@ -22,6 +22,7 @@
 
 
 import urllib
+import types
 from collections import OrderedDict
 import sys
 import cStringIO
@@ -199,3 +200,18 @@ def get_domain_scope_kwargs(args):
         scope_kwargs.update(**domain_id)
     return scope_kwargs
 # end get_domain_scope_kwargs
+
+def dictify(v):
+    if (isinstance(v, types.InstanceType) or 'class' in str(type(v))) and  not 'mock' in str(type(v)):
+        return { k:dictify(_v) for k, _v in v.__dict__.iteritems() }
+    elif isinstance(v, dict):
+        return { k:dictify(_v) for k, _v in v.iteritems() }
+    elif isinstance(v, set):
+        try:
+            return { dictify(i) for i in v }
+        except TypeError as e:
+            raise
+    elif isinstance(v, list):
+        return [ dictify(i) for i in v ]
+    else:
+        return v
