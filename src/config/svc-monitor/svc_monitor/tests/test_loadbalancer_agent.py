43b550868e41a66944ca7246d934056b24ecb8a0
Maciej Łastawiecki <maciej.lastawiecki@codilime.com>
2019-01-24 18:39:23 +0100

    svc_monitor locate refactor
    Change-Id: Ifebaf47cf4b071104b173e3bbc9c8693e29c294c
    closes-jira-bug: JBE-871

--- src/config/svc-monitor/svc_monitor/tests/test_loadbalancer_agent.py
+++ src/config/svc-monitor/svc_monitor/tests/test_loadbalancer_agent.py
@@ -7,6 +7,7 @@ from svc_monitor import loadbalancer_agent
 from vnc_api.vnc_api import *
 import argparse
 import ConfigParser
+import test_common_utils as test_utils
 
 class LoadbalancerAgentTest(unittest.TestCase):
     def setUp(self):
@@ -63,13 +64,15 @@ class LoadbalancerAgentTest(unittest.TestCase):
         self.cassandra.pool_config_insert = mock.Mock(side_effect=put_db_config)
         self.cassandra.health_monitor_list = mock.Mock(side_effect=list_health_monitors)
 
+        self.vnc_lib.obj_to_dict = test_utils.vnc_obj_to_dict
+
         mocked_gsc = mock.MagicMock()
         mocked_gsc.uuid = 'fake-gsc-uuid'
         self.vnc_lib.service_appliance_set_create.return_value = "opencontrail"
         self.vnc_lib.global_system_config_read.return_value = mocked_gsc
         def no_id_side_effect(fq_name):
             raise NoIdError("xxx")
-        self.vnc_lib.service_appliance_set_read = mock.Mock(side_effect=no_id_side_effect)
+        self.vnc_lib.service_appliance_set_read = test_utils.sas_vnc_read
         conf_parser = argparse.ArgumentParser(add_help=False)
         config = ConfigParser.SafeConfigParser({'admin_token': None})
         self._args, remaining_argv = conf_parser.parse_known_args()
