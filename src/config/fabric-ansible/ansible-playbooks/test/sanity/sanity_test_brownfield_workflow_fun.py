ba6c47453b468d9b41ced3cb82cca0a796fc07ab
Eddie Ruan <eddie.ruan@gmail.com>
2019-02-05 09:45:46 -0800

    [fabric] fungible underlay fabric integration     Ansible changes needed for letting contrail manage fungible devices in DC underlay fabric.     Test is done via adding / removing brownfield work flow and assign roles.     partial-jira-bug: CEMDA-141
    Change-Id: I469973048a988e5825e5fadc2d4659309ad4e311

---
+++ src/config/fabric-ansible/ansible-playbooks/test/sanity/sanity_test_brownfield_workflow_fun.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+
+#
+# Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
+#
+
+"""
+This file contains sanity test for all major workflows supported by
+fabric ansible
+"""
+
+from sanity_base import SanityBase
+import config
+
+
+# pylint: disable=E1101
+class SanityTestZtpWorkflow(SanityBase):
+    """
+    Sanity test on full brown field fabric onborad workflow:
+     - fabric onboard
+     - device discovery
+     - role discovery
+     - device import
+     - topology discovery
+    """
+
+    def __init__(self, cfg):
+        SanityBase.__init__(self, cfg, 'sanity_test_brownfield_workflow')
+        self._namespaces = cfg['namespaces']
+        self._prouter = cfg['prouter']
+    # end __init__
+
+    def onboard_fabric(self, fabric_info):
+        """Onboard fabric"""
+        self._logger.info("Onboard fabric ...")
+        job_template_fq_name = [
+            'default-global-system-config', 'existing_fabric_onboard_template']
+        fabric_fq_name = fabric_info['fabric_fq_name']
+
+        job_execution_info = self._api.execute_job(
+            job_template_fq_name=job_template_fq_name,
+            job_input=fabric_info
+        )
+        job_execution_id = job_execution_info.get('job_execution_id')
+        self._logger.debug(
+            "Onboard fabric job started with execution id: %s", job_execution_id)
+        self._wait_and_display_job_progress('Onboard fabric', job_execution_id,
+                                            fabric_fq_name, job_template_fq_name)
+        self._logger.info("... Brownfield workflow complete")
+    # end onboard_fabric
+
+    def test(self):
+        try:
+            self.onboard_fabric(
+                fabric_info={
+                    "fabric_fq_name": ["default-global-system-config", "fab03"],
+                    "device_auth": [
+                        {
+                            "username": "fungible",
+                            "password": "test123"
+                        }
+                    ],
+                    "fabric_asn_pool": [
+                        {
+                            "asn_max": 65000,
+                            "asn_min": 64000
+                        },
+                        {
+                            "asn_max": 65100,
+                            "asn_min": 65000
+                        }
+                    ],
+                    "fabric_subnets": [
+                        "30.1.1.1/24"
+                    ],
+                    "loopback_subnets": [
+                        "20.1.1.1/24"
+                    ],
+                    "management_subnets": [
+                        { "cidr": "10.1.20.84/28", "gateway": "10.1.20.85" }
+                    ],
+                    "node_profiles": [
+                        {
+                            "node_profile_name": "fungible-fs1600"
+                        }
+                    ]
+                }
+            )
+        except Exception as ex:
+            self._exit_with_error(
+                "Test failed due to unexpected error: %s" % str(ex))
+    # end test
+
+
+if __name__ == "__main__":
+    SanityTestZtpWorkflow(config.load('config/test_config.yml')).test()
+# end __main__
---
+++ src/config/fabric-ansible/ansible-playbooks/test/sanity/sanity_test_fabric_deletion_fun.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+#
+# Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
+#
+
+"""
+This file contains sanity test for all major workflows supported by
+fabric ansible
+"""
+
+from sanity_base import SanityBase
+import config
+
+
+# pylint: disable=E1101
+class SanityTestFabricDeletion(SanityBase):
+    """
+    Sanity test on fabric deletion
+    """
+
+    def __init__(self, cfg):
+        SanityBase.__init__(self, cfg, 'sanity_test_fabric_deletion')
+    # end __init__
+
+    def delete_fabric(self, fabric_name):
+        """delete fabric and all objects in it"""
+        self._logger.info("Delete fabric ...")
+        job_template_fq_name = [
+            'default-global-system-config', 'fabric_deletion_template']
+        fabric_fq_name = [
+            'default-global-system-config', fabric_name]
+
+        job_execution_info = self._api.execute_job(
+            job_template_fq_name=job_template_fq_name, 
+            job_input={
+                "fabric_fq_name": fabric_fq_name
+            }
+        )
+        job_execution_id = job_execution_info.get('job_execution_id')
+        self._logger.debug(
+            "fabric deletion job started with execution id: %s", job_execution_id)
+        self._wait_and_display_job_progress('fabric deletion',
+                                            job_execution_id, fabric_fq_name,
+                                            job_template_fq_name)
+        self._logger.info("... Fabric deletion complete")
+    # end delete_fabric
+
+    def test(self):
+        try:
+            self.delete_fabric('fab03')
+        except Exception as ex:
+            self._exit_with_error(
+                "Test failed due to unexpected error: %s" % str(ex))
+    # end test
+
+
+if __name__ == "__main__":
+    SanityTestFabricDeletion(config.load('config/test_config.yml')).test()
+# end __main__
