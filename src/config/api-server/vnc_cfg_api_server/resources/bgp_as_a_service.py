e8d7711ffb68569c65b052869da68c172d751720
Yuvaraja Mariappan <ymariappan@juniper.net>
2019-02-04 23:28:37 -0800

    Added Validation Check in BGPaaS object
    BGPaaS object should not have more than one primary or
    secondary control-node-zone refs. Added validation check
    in pre_dbe_create and pre_dbe_update to handle it.
    
    Change-Id: I7aff6a2c8992695a90d4aefc0165787c685ae8e3
    Closes-jira-bug: JCB-219071

--- src/config/api-server/vnc_cfg_api_server/resources/bgp_as_a_service.py
+++ src/config/api-server/vnc_cfg_api_server/resources/bgp_as_a_service.py
@@ -9,7 +9,31 @@ from vnc_cfg_api_server.resources._resource_base import ResourceMixin
 
 class BgpAsAServiceServer(ResourceMixin, BgpAsAService):
     @classmethod
+    def _validate_control_node_zone_dep(cls, obj_dict, db_dict={}):
+        cnz_refs = []
+        if 'control_node_zone_refs' in db_dict:
+            cnz_refs.extend(db_dict['control_node_zone_refs'])
+        if 'control_node_zone_refs' in obj_dict:
+            cnz_refs.extend(obj_dict['control_node_zone_refs'])
+        cnz_db = {}
+        for ref in cnz_refs:
+            if ('to' in ref and
+                ref['to'][0] == 'default-global-system-config'):
+                attr_type = ref['attr']['bgpaas_control_node_zone_type']
+                if (attr_type in cnz_db and cnz_db[attr_type] != ref['uuid']):
+                    msg = ("BGPaaS has configured with more than one %s "
+                           "control-node-zones" % attr_type)
+                    return False, (400, msg)
+                cnz_db[attr_type] = ref['uuid']
+        return True, ""
+
+    @classmethod
     def pre_dbe_create(cls, tenant_name, obj_dict, db_conn):
+        if 'control_node_zone_refs' in obj_dict:
+            (ok, msg) = cls._validate_control_node_zone_dep(obj_dict)
+            if not ok:
+                return ok, msg
+
         if (not obj_dict.get('bgpaas_shared') is True or
                 obj_dict.get('bgpaas_ip_address') is not None):
             return True, ''
@@ -18,11 +42,21 @@ class BgpAsAServiceServer(ResourceMixin, BgpAsAService):
 
     @classmethod
     def pre_dbe_update(cls, id, fq_name, obj_dict, db_conn, **kwargs):
-        if 'bgpaas_shared' in obj_dict:
+        result = None
+        if 'control_node_zone_refs' in obj_dict:
             ok, result = cls.dbe_read(db_conn, 'bgp_as_a_service', id)
-
             if not ok:
                 return ok, result
+            (ok, msg) = cls._validate_control_node_zone_dep(obj_dict, result)
+            if not ok:
+                return ok, msg
+
+        if 'bgpaas_shared' in obj_dict:
+            if not result:
+                ok, result = cls.dbe_read(db_conn, 'bgp_as_a_service', id)
+                if not ok:
+                    return ok, result
             if result.get('bgpaas_shared', False) != obj_dict['bgpaas_shared']:
                 return (False, (400, 'BGPaaS sharing cannot be modified'))
+
         return True, ""
