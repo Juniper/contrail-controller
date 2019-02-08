046d06d4653c9c2d4aa4833eb3b7a21c540df8a7
jnpr-akshayam <akshayam@juniper.net>
2019-02-07 17:12:29 -0800

    [DM] Import other details such as os_version while onboarding in device import.
    This is to update the physical router with additional details such as
    os_version and some annotations
    
    Change-Id: I147eb428390f8acc84ab4991d1258e786558875f
    closes-jira-bug: JCB-219116

---
+++ src/config/fabric-ansible/ansible-playbooks/filter_plugins/update_physical_router.py
@@ -0,0 +1,193 @@
+#!/usr/bin/python
+
+import traceback
+import sys
+import json
+
+from job_manager.job_utils import JobVncApi
+from vnc_api.exceptions import NoIdError
+
+
+sys.path.append("/opt/contrail/fabric_ansible_playbooks/module_utils")
+
+from filter_utils import FilterLog, _task_log, _task_done, \
+    _task_error_log
+
+
+class FilterModule(object):
+
+    def filters(self):
+        return {
+            'update_physical_router': self.update_physical_router,
+        }
+    # end filters
+
+    @staticmethod
+    def _parse_additional_prop_and_upd_payload(obj_dict_payload, prouter_vendor):
+        if obj_dict_payload.get("additional_properties"):
+            annotations = {
+                "key_value_pair": [
+                    {
+                        "key": prouter_vendor,
+                        "value": json.dumps(
+                            obj_dict_payload.get("additional_properties"))
+                    }
+                ]
+            }
+            obj_dict_payload.update({"annotations": annotations})
+        obj_dict_payload.pop("additional_properties")
+
+
+    # end _parse_additional_prop_and_upd_payload
+
+    def update_physical_router(self, job_ctx, prouter_name,
+                               obj_dict_payload, prouter_vendor):
+        """
+        :param job_ctx: Dictionary
+            example:
+            {
+                "auth_token": "EB9ABC546F98",
+                "job_input": {
+                    "fabric_fq_name": [
+                        "default-global-system-config",
+                        "fab01"
+                    ],
+                    "device_auth": [{
+                        "username": "root",
+                        "password": "Embe1mpls"
+                    }],
+                    "management_subnets": [
+                        {
+                            "cidr": "10.87.69.0/25",
+                            "gateway": "10.87.69.1"
+                        }
+                    ],
+                    "overlay_ibgp_asn": 64512,
+                    "node_profiles": [
+                        {
+                            "node_profile_name": "juniper-qfx5k"
+                        }
+                    ]
+                }
+            }
+        :param prouter_name: String
+            example: "5c3-qfx8"
+        :param obj_dict_payload: Dictionary
+            example:
+            {
+              "physical_router_dataplane_ip": "10.0.0.2",
+              "physical_router_loopback_ip": "10.0.0.2",
+              "fq_name": ["default-global-system-config","5c3-qfx8"],
+              "physical_router_os_version": "srx-20.65",
+              "additional_properties": {
+                  'juniper': 'Some random annotations'
+              }
+            }
+        :param prouter_vendor: String
+            example: "juniper"
+
+        :return: Dictionary
+        if success, returns
+            {
+              'status': 'success',
+              'upd_pr_log': <String: upd_pr_log>,
+              'physical_router_upd_resp': <Dictionary: physical_router_upd_resp>
+            }
+        if failure, returns
+            {
+              'status': 'failure',
+              'error_msg': <String: exception message>,
+              'upd_pr_log': <String: upd_pr_log>
+
+            }
+            :param physical_router_upd_resp: Dictionary
+                example:
+                {
+                  "job_log_msg": <String: job log message>,
+                  "warning_info": <Dictionary: warning_info>
+                }
+        """
+        FilterLog.instance("UpdatePhysicalRouterFilter", prouter_name)
+        _task_log("Starting Device Update")
+
+        try:
+
+            _task_log("Creating vnc handle")
+
+            self.vnc_lib = JobVncApi.vnc_init(job_ctx)
+
+            _task_log("Parsing additional physical router properties")
+
+            FilterModule._parse_additional_prop_and_upd_payload(
+                obj_dict_payload, prouter_vendor)
+
+            _task_log("Updating the physical router")
+
+            physical_router_upd_resp = \
+                self._update_physical_router_object(
+                    obj_dict_payload,
+                    prouter_name
+                )
+            _task_done()
+
+            return {
+                'status': 'success',
+                'upd_pr_log': FilterLog.instance().dump(),
+                'physical_router_upd_resp': physical_router_upd_resp
+            }
+        except Exception as ex:
+            _task_error_log(str(ex))
+            _task_error_log(traceback.format_exc())
+            return {'status': 'failure',
+                    'error_msg': str(ex),
+                    'upd_pr_log': FilterLog.instance().dump()
+                   }
+    # end update_physical_router
+
+    def _update_physical_router_object(self, obj_dict_payload, prouter_name):
+
+        warning_info = {}
+
+        object_type = "physical_router"
+
+        try:
+            cls = JobVncApi.get_vnc_cls(object_type)
+            physical_router_obj = cls.from_dict(**obj_dict_payload)
+            self.vnc_lib.physical_router_update(physical_router_obj)
+            job_log_msg = "- loopback_ip: %s\n   - OS Version: %s\n" \
+                          % (
+                              physical_router_obj.
+                                  get_physical_router_loopback_ip(),
+                              physical_router_obj.
+                                  get_physical_router_os_version()
+                          )
+
+        except NoIdError as exc:
+            _task_error_log(str(exc))
+            _task_error_log(traceback.format_exc())
+            job_log_msg = "The device being updated was not found" \
+                          " in the vnc database"
+            warning_info = {
+                "device_name": prouter_name,
+                "obj_dict_payload": obj_dict_payload,
+                "warning_message": str(exc)
+            }
+
+        except Exception as ex:
+            _task_error_log(str(ex))
+            _task_error_log(traceback.format_exc())
+            job_log_msg = "There was a problem while updating the" \
+                       " device"
+            warning_info = {
+                "device_name": prouter_name,
+                "obj_dict_payload": obj_dict_payload,
+                "warning_message": str(ex)
+            }
+
+        return {
+            "warning_info": warning_info,
+            "job_log_msg": job_log_msg
+        }
+
+    # end _update_physical_router_object
+
