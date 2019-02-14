1dec2367253dd30f537d2951d40eb4658f002d99
jnpr-rtulsian <rtulsian@juniper.net>
2019-02-14 09:33:44 -0800

    Assign Static IP to device during ZTP
    Change-Id: I4f153770609b76e8057b3b182f8568cee0876cab
    Depends-On: I6c09858e29c6447ac6353a6e4aa76e499a562ea2
    closes-jira-bug: CEM-2735

---
+++ src/config/fabric-ansible/ansible-playbooks/roles/read_mgmt_intf_info/filter_plugins/read_mgmt_intf_info_filters.py
@@ -0,0 +1,36 @@
+class FilterModule(object):
+    def filters(self):
+        return {
+            'find_dhcp_assigned_mgmt_interface': self.find_dhcp_assigned_mgmt_interface
+        }
+    # end filters
+
+    def find_dhcp_assigned_mgmt_interface(self, interface_configuration, mgmt_ip):
+        mgmt_interface = {}
+        if not interface_configuration or \
+                'runtime_interfaces' not in interface_configuration:
+            return mgmt_interface
+
+        runtime_interfaces = interface_configuration.get('runtime_interfaces')
+
+        if not runtime_interfaces:
+            return mgmt_interface
+
+        found_interface = None
+        for interface in runtime_interfaces:
+            if interface['logical_interface_address'] == mgmt_ip:
+                found_interface = interface
+                break
+
+        if not found_interface:
+            return mgmt_interface
+
+        mgmt_interface['ip_address'] = mgmt_ip
+        mgmt_interface['prefix'] = found_interface['logical_interface_network'].split('/')[-1]
+        mgmt_interface['mac_address'] = found_interface['physical_interface_mac_address']
+        mgmt_interface['name'] = found_interface['physical_interface_name']
+        mgmt_interface['unit'] = found_interface['logical_interface_name'].split('.')[-1]
+
+        return mgmt_interface
+    # end find_dhcp_assigned_mgmt_interface
+# end FilterModule
