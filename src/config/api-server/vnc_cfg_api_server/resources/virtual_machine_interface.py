368ddabba1cc19d166f567986fce8aa772d573d1
Nagendra Maynattamai <npchandran@juniper.net>
2019-02-20 11:53:54 -0800

    Use old_vlan instead of 0 as default for new_vlan closes-jira-bug: JCB-219307
    as the if condt for virtual_machine_interface_properties in obj_dict
    in pre_dbe_update is removed, new_vlan should be set to old_vlan
    in case of first creation
    
    Change-Id: Ie108f0e33be38804434696db8ad1c9d9e4795496

--- src/config/api-server/vnc_cfg_api_server/resources/virtual_machine_interface.py
+++ src/config/api-server/vnc_cfg_api_server/resources/virtual_machine_interface.py
@@ -640,10 +640,10 @@ class VirtualMachineInterfaceServer(ResourceMixin, VirtualMachineInterface):
         if old_vnic_type == cls.portbindings['VNIC_TYPE_DIRECT']:
             cls._check_vrouter_link(read_result, kvp_dict, obj_dict, db_conn)
 
-        new_vlan = (obj_dict.get('virtual_machine_interface_properties') or
-                    {}).get('sub_interface_vlan_tag') or 0
         old_vlan = (read_result.get('virtual_machine_interface_properties') or
                     {}).get('sub_interface_vlan_tag') or 0
+        new_vlan = (obj_dict.get('virtual_machine_interface_properties') or
+                    {}).get('sub_interface_vlan_tag') or old_vlan
         if new_vlan != old_vlan:
             return False, (400, "Cannot change sub-interface VLAN tag ID")
 
