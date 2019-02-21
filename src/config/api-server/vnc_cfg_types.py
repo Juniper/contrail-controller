61e6cc6752180e956d141d463666b2ea8c30bf81
Édouard Thuleau <ethuleau@juniper.net>
2019-02-21 16:09:54 +0100

    [config] Refuse user to create, update or delete default RI
    Only system is allowed to CUD API operations on default routing instance.
    
    Change-Id: Iafe3d02e313d99c8c01dca119522d5b6542c8c96
    Closes-Jira-Bug: JCB-219193
    (cherry picked from commit 52da54a05245af001b881a6bcd3594057fc1b49f)
    (cherry picked from commit b7b06390756578c8272e15beff0d2f00e09a7c39)

--- src/config/api-server/vnc_cfg_types.py
+++ src/config/api-server/vnc_cfg_types.py
@@ -4907,3 +4907,53 @@ class RouteTargetServer(Resource, RouteTarget):
     @classmethod
     def pre_dbe_create(cls, tenant_name, obj_dict, db_conn):
         return cls.parse_route_target_name(obj_dict['fq_name'][-1])
+
+
+class RoutingInstanceServer(Resource, RoutingInstance):
+    @staticmethod
+    def _check_default_routing_instance(obj_dict, db_obj_dict=None):
+        """Prevent to update/delete default routing instance.
+
+        Forbidden an external call to delete a default routing instance and
+        also prevent an external call to change the default flag.
+        """
+        if is_internal_request():
+            return True, ''
+        if 'routing_instance_is_default' not in obj_dict:
+            return True, ''
+        if not db_obj_dict and not obj_dict['routing_instance_is_default']:
+            # create and delete allowed if not default RI
+            return True, ''
+        elif (db_obj_dict and obj_dict['routing_instance_is_default'] ==
+                db_obj_dict.get('routing_instance_is_default', False)):
+            # update allowed if same as before
+            return True, ''
+
+        if not db_obj_dict:
+            db_obj_dict = obj_dict
+        msg = ("Routing instance %s(%s) cannot modify as it the default "
+               "routing instance of the %s %s(%s)" % (
+                   ':'.join(db_obj_dict['fq_name']),
+                   db_obj_dict['uuid'],
+                   db_obj_dict['parent_type'].replace('_', ' '),
+                   ':'.join(db_obj_dict['fq_name'][:-1]),
+                   db_obj_dict['parent_uuid']))
+        return False, (400, msg)
+
+    @classmethod
+    def pre_dbe_create(cls, tenant_name, obj_dict, db_conn):
+        return cls._check_default_routing_instance(obj_dict)
+
+    @classmethod
+    def pre_dbe_update(cls, id, fq_name, obj_dict, db_conn,
+                       prop_collection_updates=None, ref_update=None):
+        ok, result = cls.locate(fq_name, uuid=id, create_it=False, fields=[
+            'parent_type', 'parent_uuid', 'routing_instance_is_default'])
+        if not ok:
+            return False, result
+        db_obj_dict = result
+        return cls._check_default_routing_instance(obj_dict, db_obj_dict)
+
+    @classmethod
+    def pre_dbe_delete(cls, id, obj_dict, db_conn):
+        return cls._check_default_routing_instance(obj_dict) + (None,)
