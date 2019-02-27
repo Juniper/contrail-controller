c826b683a68274ce0bb87d9b7880d5e930601d98
jnpr-akshayam <akshayam@juniper.net>
2019-02-26 19:18:28 -0800

    [DM] Reverting back fabric changes and moving fabric config as part of delete as multidevice job
    Fixing the following bugs:
    
    1. fabric uuid resetting was happening before device delete which caused it to be not cached.
    2. Sending device ids in the job_params from DM in case of is_delete workflow.
    3. Making the is_delete workflow a multidevice job so that it can acquire the zk lock and
       prevent two jobs of same concurrency level (usually device level) from running simultaneously.
    
    Change-Id: I3e4939e7af5c2caa4adf9f0f0d173a781f99ef34
    closes-jira-bug: JCB-219436

--- src/config/device-manager/device_manager/plugins/ansible/ansible_conf.py
+++ src/config/device-manager/device_manager/plugins/ansible/ansible_conf.py
@@ -271,7 +271,6 @@ class AnsibleConf(AnsibleBase):
                                               sort_keys=True))
                 device_manager = DeviceManager.get_instance()
                 job_handler = JobHandler(job_template, job_input,
-                                         None if is_delete else
                                          [self.physical_router.uuid],
                                          device_manager.get_api_server_config(),
                                          self._logger,
@@ -395,9 +394,7 @@ class AnsibleConf(AnsibleBase):
         config = self.prepare_conf(is_delete=is_delete)
         feature_configs, job_template = self.read_node_profile_info()
         job_input = {
-            'fabric_uuid':
-                self.physical_router.fabric
-                if self.physical_router.fabric else '',
+            'fabric_uuid': self.physical_router.fabric,
             'device_management_ip': self.physical_router.management_ip,
             'additional_feature_params': feature_configs,
             'device_abstract_config': self.export_dict(config),
