e186299a872ec6e873c3cdfacc67dcf4be2a1463
Arkadiusz Kudan <arkadiusz.kudan@codilime.com>
2019-02-28 14:38:51 +0100

    Make agent's ksync unit tests compile on Windows
    This is required change to fix agent's ksync unit tests.
    The change affects Linux code, because:
      It fixes bug with map values comparision in port_subscribe_table.h
      Moves logic of VnswifInterfaceListenerBase native to nix systems
        to VnswifInterfaceListenerBaseNix
    
    Change-Id: I8f9cc305b41d3e73a37a6b2cb10033db183e7ad8
    Closes-Jira-Bug: WIND-195
    Depends-On: I37f5f7a6f8d00de12d9c8d275db7f912e7261941

--- src/vnsw/agent/vrouter/ksync/windows/vnswif_listener.h
+++ src/vnsw/agent/vrouter/ksync/windows/vnswif_listener.h
@@ -6,13 +6,21 @@
 #define windows_vnsw_agent_router_id_h
 
 #include <cmn/agent.h>
+#include "vrouter/ksync/vnswif_listener_base.h"
+#include <boost/asio.hpp>
 
-class VnswInterfaceListenerWindows {
+class VnswInterfaceListenerWindows : public VnswInterfaceListenerBase {
 public:
     VnswInterfaceListenerWindows(Agent *agent);
-    void Init();
-    void Shutdown();
+
+    virtual int CreateSocket();
+    virtual void SyncCurrentState();
+    virtual void RegisterAsyncReadHandler();
+    void ReadHandler(const boost::system::error_code &, std::size_t length);
+    void UpdateLinkLocalRoute(const Ip4Address &addr, uint8_t plen, bool del_rt);
 private:
+    friend class TestVnswIf;
+
     DISALLOW_COPY_AND_ASSIGN(VnswInterfaceListenerWindows);
 };
 
