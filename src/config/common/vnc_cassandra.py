4347a87688975702d75f86523cd39ea01ece6a72
Sathish Holla <saholla@juniper.net>
2019-02-28 14:58:26 -0800

    [Config] Handle KeyErrors in uuid_to_fq_name
    The SYS_ERR log and KeyError were added as part of
    https://bugs.launchpad.net/juniperopenstack/+bug/1771879
    
    However, there is a change to modify the log level as it leads to excessive logs.
    As part of this review, instead of raising SYS_ERR for all the KeyErrors,
    we will instead raise NoIdError in case fq_name or type is not present in
    uuid_to_fq_name function.
    
    This fix is based on comments made by Sachin in https://review.opencontrail.org/#/c/43148/
    
    Closes-Jira-Bug: JCB-206854
    Closes-Jira-Bug: JCB-206859
    Change-Id: Ic864c4f2d91f6ab804e2ffe9517ce05667cf711b
    (cherry picked from commit c9ddd6d645c6b194bfdbfa735479fe11278e787c)

--- src/config/common/vnc_cassandra.py
+++ src/config/common/vnc_cassandra.py
@@ -1279,6 +1279,8 @@ class VncCassandraClient(object):
                            columns=['fq_name', 'type'])
             if not obj:
                 raise NoIdError(id)
+            if 'type' not in obj or 'fq_name' not in obj:
+                raise NoIdError(id)
             fq_name = obj['fq_name']
             obj_type = obj['type']
             self.cache_uuid_to_fq_name_add(id, fq_name, obj_type)
@@ -1292,6 +1294,8 @@ class VncCassandraClient(object):
             obj = self.get(self._OBJ_UUID_CF_NAME, id,
                            columns=['fq_name', 'type'])
             if not obj:
+                raise NoIdError(id)
+            if 'type' not in obj or 'fq_name' not in obj:
                 raise NoIdError(id)
             fq_name = obj['fq_name']
             obj_type = obj['type']
