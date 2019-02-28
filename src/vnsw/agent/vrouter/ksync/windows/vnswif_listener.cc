69c419763ff9c4fb5510da1e02257b802391fbac
Arkadiusz Kudan <arkadiusz.kudan@codilime.com>
2019-02-28 14:38:51 +0100

    Make agent's ksync unit tests compile on Windows
    This is required change to fix agent's ksync unit tests.
    The change affects Linux code, because:
      It fixes bug with map values comparision in port_subscribe_table.h
      Moves logic of VnswifInterfaceListenerBase native to nix systems
        to VnswifInterfaceListenerBaseNix
    
    Change-Id: I8f9cc305b41d3e73a37a6b2cb10033db183e7ad8
    Closes-Jira-Bug: WIND-195
    Depends-On: I37f5f7a6f8d00de12d9c8d275db7f912e7261941

--- src/vnsw/agent/vrouter/ksync/windows/vnswif_listener.cc
+++ src/vnsw/agent/vrouter/ksync/windows/vnswif_listener.cc
@@ -4,11 +4,22 @@
 
 #include "vnswif_listener.h"
 
-VnswInterfaceListenerWindows::VnswInterfaceListenerWindows(Agent *agent) {
+VnswInterfaceListenerWindows::VnswInterfaceListenerWindows(Agent *agent) : VnswInterfaceListenerBase(agent) {
 }
 
-void VnswInterfaceListenerWindows::Init() {
+int VnswInterfaceListenerWindows::CreateSocket() {
+    VNSWIF_TRACE("VnswInterfaceListenerWindows::CreateSocket -- NOOP");
+    return -EACCES;
 }
-
-void VnswInterfaceListenerWindows::Shutdown() {
+void VnswInterfaceListenerWindows::SyncCurrentState() {
+    VNSWIF_TRACE("VnswInterfaceListenerWindows::SyncCurrentState -- NOOP");
+}
+void VnswInterfaceListenerWindows::RegisterAsyncReadHandler() {
+    VNSWIF_TRACE("VnswInterfaceListenerWindows::RegisterAsyncReadHandler -- NOOP");
+}
+void VnswInterfaceListenerWindows::ReadHandler(const boost::system::error_code &, std::size_t length) {
+    VNSWIF_TRACE("VnswInterfaceListenerWindows::ReadHandler -- NOOP");
+}
+void VnswInterfaceListenerWindows::UpdateLinkLocalRoute(const Ip4Address &addr, uint8_t plen, bool del_rt) {
+    VNSWIF_TRACE("VnswInterfaceListenerWindows::UpdateLinkLocalRoute -- NOOP");
 }
