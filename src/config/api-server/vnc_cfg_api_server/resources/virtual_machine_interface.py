3ef08aba43835ea73b63d95927abea1ee130a963
Michael Henkel <mhenkel@juniper.net>
2019-03-08 17:53:38 +0100

    Check for short name in dpdk_enable
    Change-Id: Iab0a9cf7f4fd3f3f9a590b359a2cb94ba7c35f77
    closes-jira-bug: CEM-3066

--- src/config/api-server/vnc_cfg_api_server/resources/virtual_machine_interface.py
+++ src/config/api-server/vnc_cfg_api_server/resources/virtual_machine_interface.py
@@ -261,10 +261,19 @@ class VirtualMachineInterfaceServer(ResourceMixin, VirtualMachineInterface):
                     # case, hack for vcenter plugin
                     return True, False
             else:
-                # dropping the NoIdError as its equivalent to
-                # VirtualRouterNotFound its treated as False, non dpdk case,
-                # hack for vcenter plugin
-                return (True, False)
+                ok, vrList, _ = db_conn.dbe_list('virtual_router',
+                                                 field_names=[
+                                                     'uuid',
+                                                     'fq_name'])
+                if not ok:
+                    return False, vrList
+                vrouter_id = ""
+                for vr in vrList:
+                    if vr['fq_name'][-1].partition('.')[0] == host_id:
+                        vrouter_id = vr['uuid']
+                        break
+                if vrouter_id == "":
+                    return True, False
 
         ok, result = cls.dbe_read(db_conn, 'virtual_router', vrouter_id)
         if not ok:
