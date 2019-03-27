85e7e49e3432180dc1b64754920c7bdd4347bbc8
Nagendra E S <esnagendra@juniper.net>
2019-03-27 13:55:02 +0530

    Fix forwarding issue with L2 only mode and vxlan routing cfged.
    Forwarding issue seen when contrail configured in L2 only mode
    or when vxlan routing using logical routing is used for evpn
    multicast.
    
    Failure to find out VMI caused the incomplete NH programming
    for local VMs. Corrected that by modifying logic to find
    VM IP to VmInterface object.
    
    Change-Id: I68db9586924723c2401bedf9f406293ac28eee26
    closes-jira-bug: CEM-3835

--- src/vnsw/agent/services/test/igmp_test.cc
+++ src/vnsw/agent/services/test/igmp_test.cc
@@ -292,24 +292,24 @@ TEST_F(IgmpTest, SendV2ReportsWithDelVnFirst) {
     usleep(4*USECS_PER_SEC);
 
     idx = 1;
-    local_g_del_count++;
     leave_count++;
     SendIgmp(GetItfId(idx), input[idx].addr, IgmpTypeV2Leave, igmp_gs, 1);
     ret = WaitForRxOkCount(idx, IGMP_GROUP_LEAVE, leave_count);
     EXPECT_EQ(ret, true);
-    ret = WaitForGCount(false, local_g_del_count+2);
+    ret = WaitForGCount(false, local_g_del_count);
+    EXPECT_EQ(ret, true);
 
     // 4 second delay to trigger timer as is case with
     // evpn test-framework script.
     usleep(4*USECS_PER_SEC);
 
     idx = 2;
-    local_g_del_count++;
     leave_count++;
     SendIgmp(GetItfId(idx), input[idx].addr, IgmpTypeV2Leave, igmp_gs, 1);
     ret = WaitForRxOkCount(idx, IGMP_GROUP_LEAVE, leave_count);
     EXPECT_EQ(ret, true);
-    ret = WaitForGCount(false, local_g_del_count+1);
+    ret = WaitForGCount(false, local_g_del_count);
+    EXPECT_EQ(ret, true);
 
     nh = MCRouteToNextHop(agent->local_vm_peer(), vrf_name, group, source);
     EXPECT_EQ((nh == NULL), true);
