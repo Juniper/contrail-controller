181ef4bc6e8671c08068a02b7baf3ad47a0d48c6
Édouard Thuleau <ethuleau@juniper.net>
2019-04-25 17:01:41 +0200

    [config] Fix address pairs not allowed with no port security
    Allows to disable port security if address pair is set to an empty
    list.
    
    Change-Id: I7cdd598cf1e011cb36e9135ee2d31259c1cd922d
    Closes-Jira-Bug: CEM-5155

--- src/config/api-server/vnc_cfg_api_server/tests/resources/test_virtual_machine_interface.py
+++ src/config/api-server/vnc_cfg_api_server/tests/resources/test_virtual_machine_interface.py
@@ -4,7 +4,10 @@
 import logging
 
 from cfgm_common.exceptions import BadRequest
+from vnc_api.vnc_api import AllowedAddressPair
+from vnc_api.vnc_api import AllowedAddressPairs
 from vnc_api.vnc_api import Project
+from vnc_api.vnc_api import SubnetType
 from vnc_api.vnc_api import VirtualMachineInterface
 from vnc_api.vnc_api import VirtualMachineInterfacePropertiesType
 from vnc_api.vnc_api import VirtualNetwork
@@ -112,3 +115,69 @@ class TestVirtualMachineInterface(test_case.ApiServerTestCase):
         vmi42.set_virtual_machine_interface_properties(
             VMIPT(sub_interface_vlan_tag=42))
         self.api.virtual_machine_interface_update(vmi42)
+
+    def test_port_security_and_allowed_address_pairs(self):
+        project = Project('%s-project' % self.id())
+        self.api.project_create(project)
+        vn = VirtualNetwork('vn-%s' % self.id(), parent_obj=project)
+        self._vnc_lib.virtual_network_create(vn)
+        addr_pair = AllowedAddressPairs(
+            allowed_address_pair=[
+                AllowedAddressPair(ip=SubnetType('1.1.1.0', 24),
+                                   mac='02:ce:1b:d7:a6:e7')])
+        msg = (r"^Allowed address pairs are not allowed when port security is "
+               "disabled$")
+
+        vmi = VirtualMachineInterface(
+            'vmi-%s' % self.id(),
+            parent_obj=project,
+            port_security_enabled=False,
+            virtual_machine_interface_allowed_address_pairs=addr_pair)
+        vmi.set_virtual_network(vn)
+        with self.assertRaisesRegexp(BadRequest, msg):
+            self._vnc_lib.virtual_machine_interface_create(vmi)
+
+        vmi = VirtualMachineInterface('vmi-%s' % self.id(), parent_obj=project,
+                                      port_security_enabled=False)
+        vmi.set_virtual_network(vn)
+        self._vnc_lib.virtual_machine_interface_create(vmi)
+
+        # updating a port with allowed address pair should throw an exception
+        # when port security enabled is set to false
+        vmi.virtual_machine_interface_allowed_address_pairs = addr_pair
+        with self.assertRaisesRegexp(BadRequest, msg):
+            self._vnc_lib.virtual_machine_interface_update(vmi)
+
+    def test_disable_port_security_with_empty_allowed_address_pair_list(self):
+        project = Project('%s-project' % self.id())
+        self.api.project_create(project)
+        vn = VirtualNetwork('vn-%s' % self.id(), parent_obj=project)
+        self._vnc_lib.virtual_network_create(vn)
+        addr_pair = AllowedAddressPairs(
+            allowed_address_pair=[
+                AllowedAddressPair(ip=SubnetType('1.1.1.0', 24),
+                                   mac='02:ce:1b:d7:a6:e7')])
+
+        vmi1 = VirtualMachineInterface(
+            'vmi1-%s' % self.id(),
+            parent_obj=project,
+            port_security_enabled=False,
+            virtual_machine_interface_allowed_address_pairs=\
+                AllowedAddressPairs())
+        vmi1.set_virtual_network(vn)
+        self._vnc_lib.virtual_machine_interface_create(vmi1)
+
+        vmi2 = VirtualMachineInterface(
+            'vmi2-%s' % self.id(),
+            parent_obj=project,
+            port_security_enabled=True,
+            virtual_machine_interface_allowed_address_pairs=addr_pair)
+        vmi2.set_virtual_network(vn)
+        self._vnc_lib.virtual_machine_interface_create(vmi2)
+
+        vmi2.set_virtual_machine_interface_allowed_address_pairs(
+            AllowedAddressPairs())
+        self._vnc_lib.virtual_machine_interface_update(vmi2)
+
+        vmi2.set_port_security_enabled(False)
+        self._vnc_lib.virtual_machine_interface_update(vmi2)
