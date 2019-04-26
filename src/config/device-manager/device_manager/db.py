cc2b13f50ea25e00fb086a8cdf6d42aad869aa5f
sjeevaraj <sjeevaraj@juniper.net>
2019-04-26 00:24:39 +0000

    [DM] Security Group change on VPG doesnot push config
    Change-Id: I750709e6f2ca926c7c1906a37a0486d5f1fa32ba
    Closes-Jira-Bug: CEM-5204

--- src/config/device-manager/device_manager/db.py
+++ src/config/device-manager/device_manager/db.py
@@ -1177,6 +1177,7 @@ class VirtualMachineInterfaceDM(DBBaseDM):
         self.security_groups = set()
         self.service_instance = None
         self.service_endpoint = None
+        self.virtual_port_group = None
         self.update(obj_dict)
     # end __init__
 
@@ -1212,6 +1213,7 @@ class VirtualMachineInterfaceDM(DBBaseDM):
             if pt:
                 self.service_instance = pt.svc_instance
         self.update_single_ref('service_endpoint', obj)
+        self.update_single_ref('virtual_port_group', obj)
     # end update
 
     def is_device_owner_bms(self):
@@ -1240,6 +1242,7 @@ class VirtualMachineInterfaceDM(DBBaseDM):
         obj.update_multiple_refs('security_group', {})
         obj.update_single_ref('port_tuple', {})
         obj.update_single_ref('service_endpoint', {})
+        obj.update_single_ref('virtual_port_group', {})
         del cls._dict[uuid]
     # end delete
 
@@ -2415,7 +2418,7 @@ class VirtualPortGroupDM(DBBaseDM):
             vmi_obj = VirtualMachineInterfaceDM.get(vmi_uuid)
             if not vmi_obj:
                 return sg_list
-            if vmi_obj.vlan_tag == int(vlan_tag):
+            if vmi_obj.vlan_tag == int(vlan_tag) or (not vmi_obj.vlan_tag and int(vlan_tag) == 4094):
                 for sg in vmi_obj.security_groups or []:
                     sg = SecurityGroupDM.get(sg)
                     if sg and sg not in sg_list:
