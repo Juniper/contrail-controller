ee3c4ece67956335ef6b647b57c989b7d21245ab
Sathish Holla <saholla@juniper.net>
2019-04-26 13:38:21 -0700

    [Config] Enabled TCP Keep alive for API Server
    Currently, we don't enable TCP keepalive options for any of the
    ports created by Gevent servers. This change is to enable TCP
    Keepalive options for API Server on port 9100.
    
    Change-Id: Ib0c1482a9b5e97479b1172bc81feae71ccf191b3
    Closes-Jira-Bug: CEM-3316

--- src/config/api-server/vnc_bottle.py
+++ src/config/api-server/vnc_bottle.py
@@ -2,7 +2,11 @@
 # Copyright (c) 2015 Juniper Networks, Inc. All rights reserved.
 #
 
-def get_bottle_server(pool_size):
+def get_bottle_server(pool_size,
+                      tcp_keepalive_enable=True,
+                      tcp_keepalive_idle_time=7200,
+                      tcp_keepalive_interval=75,
+                      tcp_keepalive_probes=9):
     """
     Custom gevent pool server for bottle
     """
@@ -15,5 +19,45 @@ def get_bottle_server(pool_size):
         def __init__(self, host='127.0.0.1', port=8080, **options):
             super(GeventPoolServer, self ).__init__(host, port,
                 spawn=Pool(size=pool_size), **options)
+
+            self.tcp_keepalive_enable = tcp_keepalive_enable
+            self.tcp_keepalive_idle_time = tcp_keepalive_idle_time
+            self.tcp_keepalive_interval = tcp_keepalive_interval
+            self.tcp_keepalive_probes = tcp_keepalive_probes
+
+        def run(self, handler):
+            import threading, os, _socket
+            import cfgm_common
+            from cfgm_common import vnc_wsgi
+            from gevent import wsgi, pywsgi, local
+            if not isinstance(threading.local(), local.local):
+                msg = "Bottle requires gevent.monkey.patch_all() (before import)"
+                raise RuntimeError(msg)
+            if not self.options.pop('fast', None): wsgi = pywsgi
+            self.options['log'] = None if self.quiet else 'default'
+            address = (self.host, self.port)
+            server = wsgi.WSGIServer(address, handler, **self.options)
+            server.init_socket()
+
+            if hasattr(_socket,'SO_KEEPALIVE'):
+                server.socket.setsockopt(_socket.SOL_SOCKET,
+                                           _socket.SO_KEEPALIVE,
+                                           self.tcp_keepalive_enable)
+            if hasattr(_socket, 'TCP_KEEPIDLE'):
+                server.socket.setsockopt(_socket.IPPROTO_TCP,
+                                           _socket.TCP_KEEPIDLE,
+                                           self.tcp_keepalive_idle_time)
+            if hasattr(_socket, 'TCP_KEEPINTVL'):
+                server.socket.setsockopt(_socket.IPPROTO_TCP,
+                                           _socket.TCP_KEEPINTVL,
+                                           self.tcp_keepalive_interval)
+            if hasattr(_socket, 'TCP_KEEPCNT'):
+                server.socket.setsockopt(_socket.IPPROTO_TCP,
+                                           _socket.TCP_KEEPCNT,
+                                           self.tcp_keepalive_probes)
+            if 'BOTTLE_CHILD' in os.environ:
+                import signal
+                signal.signal(signal.SIGINT, lambda s, f: server.stop())
+            server.serve_forever()
 
     return GeventPoolServer
