da53135d6ff9135a4339282cab868562d2faffc3
sjeevaraj <sjeevaraj@juniper.net>
2019-05-13 23:54:17 +0000

    [DM] Assisted Replicator
    1. Role support for AR-Client and AR-Replicator
    2. Business Logic in DM
    3. Unit tests in DM
    4. Jinja template support for rendering config
    
    Closes-Jira-Bug: CEM-4627
    Closes-Jira-Bug: CEM-5454
    
    Change-Id: Idb4166ba44efe316af78790113f2c064b7147589
    Depends-On: Ieafd2e48b8e087c0b974246076a24d4cabd58953

---
+++ src/config/device-manager/device_manager/plugins/feature/assisted_replicator_feature.py
@@ -0,0 +1,37 @@
+#
+# Copyright (c) 2019 Juniper Networks, Inc. All rights reserved.
+#
+
+"""
+This file contains implementation of abstract config generation for overlay bgp feature
+"""
+
+import db
+from abstract_device_api.abstract_device_xsd import *
+from collections import OrderedDict
+from dm_utils import DMUtils
+from feature_base import FeatureBase
+
+class AssistedReplicatorFeature(FeatureBase):
+
+    @classmethod
+    def feature_name(cls):
+        return 'assisted-replicator'
+    # end feature_name
+
+    def __init__(self, logger, physical_router, configs):
+        super(AssistedReplicatorFeature, self).__init__(logger, physical_router, configs)
+    # end __init__
+
+    def _build_ar_config(self, feature_config):
+        ar = AssistedReplicator(ar_loopback_ip=self._physical_router.replicator_ip,
+                                replicator_activation_delay=None)
+        feature_config.set_assisted_replicator(ar)
+
+    def feature_config(self, **kwargs):
+        feature_config = Feature(name=self.feature_name())
+        self._build_ar_config(feature_config)
+        return feature_config
+    # end push_conf
+
+# end AssistedReplicatorFeature
