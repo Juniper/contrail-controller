9768b40a4a69c7108b2df89fb5c1e193aaaef243
Joe Williams <josephw@juniper.net>
2019-05-23 05:29:33 +0000

    [Fabric] RMA workflow bug fixes
    Closes-jira-bug: CEM-5718
    
    Change-Id: I9749b8fe2f9ad98d4a62f750f8252f39811348df

--- src/config/fabric-ansible/ansible-playbooks/test/sanity/sanity_test_rma_activate.py
+++ src/config/fabric-ansible/ansible-playbooks/test/sanity/sanity_test_rma_activate.py
@@ -28,37 +28,102 @@ class SanityTestRmaActivate(SanityBase):
         self.api_server_host = cfg['api_server']['host']
         rma = cfg['rma']
         self.fabric = rma['fabric']
-        self.device_name = rma['device']
-        self.serial_number = rma['serial_number']
+        self.device_list = rma['device_list']
         self.zeroize_timeout = rma.get('timeout', DEFAULT_ZEROIZE_TIMEOUT)
-        self.device_password = rma.get('password', DEFAULT_DEVICE_PASSWORD)
-    # end __init__
+        self.job_input = {'rma_devices': []}
+        self.job_device_list = []
+        # end __init__
 
     def rma_activate(self):
         self._logger.info("RMA activate ...")
-        fabric_fq_name = ['default-global-system-config', self.fabric]
-        fabric_obj = self._api.fabric_read(fq_name=fabric_fq_name)
+
+        # Read fabric object
+        self.fabric_fq_name = ['default-global-system-config', self.fabric]
+        fabric_obj = self._api.fabric_read(fq_name=self.fabric_fq_name)
         self.fabric_uuid = fabric_obj.uuid
-        device_fq_name = ['default-global-system-config', self.device_name]
+
+        # Setup each device to activate
+        for device in self.device_list:
+            self.prepare_device(device['name'], device['serial_number'],
+                                 device.get('password',
+                                            DEFAULT_DEVICE_PASSWORD))
+
+        # Execute rma_activate job for all devices at once
+        job_template_fq_name = [
+            'default-global-system-config', 'rma_activate_template']
+
+        job_execution_info = self._api.execute_job(
+            job_template_fq_name=job_template_fq_name,
+            job_input=self.job_input,
+            device_list = self.job_device_list
+        )
+        job_execution_id = job_execution_info.get('job_execution_id')
+        self._logger.info(
+            "RMA activate job started with execution id: %s", job_execution_id)
+        try:
+            self._wait_and_display_job_progress('RMA activate',
+                                                job_execution_id,
+                                                self.fabric_fq_name,
+                                                job_template_fq_name)
+        except Exception as ex:
+            self._exit_with_error(
+                "Activate maintenance mode failed due to unexpected error: %s"
+                % str(ex))
+
+        self._logger.info("... RMA activate complete")
+    # end rma_activate
+
+    def prepare_device(self, device_name, serial_number, password):
+        device_fq_name = ['default-global-system-config', device_name]
         device_obj = self._api.physical_router_read(fq_name=device_fq_name)
-        self.device_uuid = device_obj.uuid
-        self.managed_state = device_obj.get_physical_router_managed_state()
-        self.underlay_config = device_obj.get_physical_router_underlay_config()
-        self.mac = device_obj.get_physical_router_management_mac()
+        device_uuid = device_obj.uuid
+        managed_state = device_obj.get_physical_router_managed_state()
+        underlay_managed = device_obj.get_physical_router_underlay_managed()
+        mac = device_obj.get_physical_router_management_mac()
         self._logger.info("Device {}: serial_number={}, mac={}, managed_state={}".\
-                           format(self.device_name, self.serial_number,
-                                  self.mac, self.managed_state))
+                           format(device_name, serial_number,
+                                  mac, managed_state))
 
-        # Use underlay config if already found, otherwise provide default
-        if self.underlay_config:
-            self._logger.info("Underlay config found: \'{}\'".\
-                               format(self.underlay_config))
+        if underlay_managed:
+            self.process_greenfield(device_obj, password)
         else:
-            self.underlay_config = test_underlay_config
-            self._logger.info("Underlay config added: \'{}\'".\
-                               format(self.underlay_config))
-            device_obj.set_physical_router_underlay_config(self.underlay_config)
+            self.process_brownfield(device_obj)
 
+        # Change to rma state and verify
+        device_obj = self._api.physical_router_read(fq_name=device_fq_name)
+        device_obj.set_physical_router_managed_state('rma')
+        self._api.physical_router_update(device_obj)
+        device_obj = self._api.physical_router_read(fq_name=device_fq_name)
+        self.managed_state = device_obj.get_physical_router_managed_state()
+        if self.managed_state == 'rma':
+            self._logger.info("State set to rma")
+        else:
+            self._logger.info("State is {} instead of rma. Exiting...".\
+                               format(self.managed_state))
+            return
+
+        self.job_input['rma_devices'].append({
+                'device_uuid': device_uuid,
+                'serial_number': serial_number
+            })
+        self.job_device_list.append(device_uuid)
+
+    def process_brownfield(self, device_obj):
+        # Use underlay config if already found, otherwise provide default
+        underlay_config = device_obj.get_physical_router_underlay_config()
+
+        if underlay_config:
+            self._logger.info("Underlay config found: \'{}\'".\
+                               format(underlay_config))
+        else:
+            underlay_config = test_underlay_config
+            self._logger.info("Underlay config added: \'{}\'".\
+                               format(underlay_config))
+            device_obj.set_physical_router_underlay_config(underlay_config)
+            self._api.physical_router_update(device_obj)
+
+
+    def process_greenfield(self, device_obj, password):
         # Change mac and serial number to bogus values to pretend like this
         # is another device. Then run update_dhcp_config playbook to install
         # mac into dnsmasq config file
@@ -69,8 +134,8 @@ class SanityTestRmaActivate(SanityBase):
         playbook_input = {
             "device_management_ip": device_obj.physical_router_management_ip,
             "device_username": device_obj.physical_router_user_credentials.username,
-            "device_password": self.device_password,
-            "fabric_fq_name": fabric_fq_name,
+            "device_password": password,
+            "fabric_fq_name": self.fabric_fq_name,
             "fabric_uuid": self.fabric_uuid,
             "input": {"fabric_uuid": self.fabric_uuid},
             "api_server_host": [
@@ -103,55 +168,6 @@ class SanityTestRmaActivate(SanityBase):
         self.execute_playbook('test_zeroize.yml', playbook_input)
         self._logger.info("Wait for {} minutes...".format(self.zeroize_timeout))
         time.sleep(self.zeroize_timeout*60)
-
-        # Change to rma state and verify
-        device_obj = self._api.physical_router_read(fq_name=device_fq_name)
-        device_obj.set_physical_router_managed_state('rma')
-        self._api.physical_router_update(device_obj)
-        device_obj = self._api.physical_router_read(fq_name=device_fq_name)
-        self.managed_state = device_obj.get_physical_router_managed_state()
-        if self.managed_state == 'rma':
-            self._logger.info("State set to rma")
-        else:
-            self._logger.info("State is {} instead of rma. Exiting...".\
-                               format(self.managed_state))
-            return
-
-        # Execute rma_activate job
-        job_template_fq_name = [
-            'default-global-system-config', 'rma_activate_template']
-        job_input = {}
-        if self.serial_number:
-            job_input['serial_number'] = self.serial_number
-
-        job_execution_info = self._api.execute_job(
-            job_template_fq_name=job_template_fq_name,
-            job_input=job_input,
-            device_list = [self.device_uuid]
-        )
-        job_execution_id = job_execution_info.get('job_execution_id')
-        self._logger.info(
-            "RMA activate job started with execution id: %s", job_execution_id)
-        try:
-            self._wait_and_display_job_progress('RMA activate',
-                                            job_execution_id, fabric_fq_name,
-                                            job_template_fq_name)
-        except Exception as ex:
-            self._exit_with_error(
-                "Activate maintenance mode failed due to unexpected error: %s"
-                % str(ex))
-        # Verify state change to active
-        device_obj = self._api.physical_router_read(fq_name=device_fq_name)
-        self.managed_state = device_obj.get_physical_router_managed_state()
-        if self.managed_state == 'active':
-            self._logger.info("State set to active")
-        else:
-            self._logger.info("State is {} instead of active. Exiting...".\
-                               format(self.managed_state))
-            return
-
-        self._logger.info("... RMA activate complete")
-    # end rma_activate
 
     def execute_playbook(self, playbook_name, playbook_input):
         try:
