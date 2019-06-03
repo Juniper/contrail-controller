b6213694a023e4378166ef4819a7997d9676e6e5
Yuvaraja Mariappan <ymariappan@juniper.net>
2019-06-02 19:12:58 -0700

    Added validation check for CRD dict in network_monitor
    When CustomResourceDefination(CRD) is not present, k8s-api
    returns empty dict. Added validation check for CRD dict
    before it is getting used.
    
    Change-Id: Ica7baca2f79634f499eb28eac5d7b7f30ce96a53
    closes-jira-bug: CEM-6138

--- src/container/kube-manager/kube_manager/kube/network_monitor.py
+++ src/container/kube-manager/kube_manager/kube/network_monitor.py
@@ -23,10 +23,12 @@ class NetworkMonitor(KubeMonitor):
                   %(self.name, crd_info))
             return
         else:
-            current_crds = [x['metadata']['name'].lower() \
+            current_crds = []
+            if 'items' in crd_info:
+                current_crds = [x['metadata']['name'].lower() \
                                             for x in crd_info['items']]
-            self.logger.debug("%s - Retrieved following CRD list = %s"
-                  %(self.name, current_crds))
+                self.logger.debug("%s - Retrieved following CRD list = %s"
+                    %(self.name, current_crds))
             if 'network-attachment-definitions.k8s.cni.cncf.io' \
                                                     not in current_crds:
                 # Create Networks CRD - networks.kubernetes.cni.cncf.io
