7fd2c5e04094f19c2aea124eedf830a63b536757
jnpr-akshayam <akshayam@juniper.net>
2019-06-10 17:28:58 -0700

    [Fabric] Import chassis mac id during device import and use in topology discovery
    Supporting chassis mac-id based LLDP topology discovery requires
    importing chassis information during device import which can later
    be used during topology discovery
    
    Fixed merge conflicts
    
    target-release: R1908
    Change-Id: I2d9e226ddeba0e617cc9e3c2f46b06ba051b6b8f
    closes-jira-bug: CEM-61

--- src/config/fabric-ansible/ansible-playbooks/filter_plugins/discover_role.py
+++ src/config/fabric-ansible/ansible-playbooks/filter_plugins/discover_role.py
@@ -5,12 +5,10 @@ import sys
 import traceback
 
 from cfgm_common.exceptions import NoIdError
+sys.path.append("/opt/contrail/fabric_ansible_playbooks/module_utils")
 from filter_utils import _task_done, _task_error_log, _task_log, FilterLog
 
 from job_manager.job_utils import JobVncApi
-
-
-sys.path.append("/opt/contrail/fabric_ansible_playbooks/module_utils")
 
 
 class FilterModule(object):
