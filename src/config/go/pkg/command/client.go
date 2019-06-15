825d958af9ed331eb7c2dcfaa1f8a3d67ce609ca
Dheeraj Gautam <dgautam@juniper.net>
2019-06-14 17:23:38 -0700

    add vcenter_import binary to device-manager
    partial-jira-bug: CEM-6635
    
    patch-1:
    1. changed the directory structure of the go vcenter binary
    2. updated the job-manager code to call executable
    
    patch-2:
    1. move logs to error to info.
    2. added job_messages for executable.
    3. updated job_mgr.py with latest files.
    
    patch-3:
    1. read input params from command line
    
    patch-4:
    1. update hirarchy structure again.
    
    patch-5:
    1. remove some dead code
    2. make use of json.Marshal() instead of raw json strings
    3. make use of flag module to accept input params.
    
    patch-6:
    1. read nodes into struct instead of bad parsing logic.
    
    Change-Id: Ie61314b158328a697e976ccf4afdf1bb296015f0

---
+++ src/config/go/pkg/command/client.go
@@ -0,0 +1,288 @@
+package client
+
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	vc "github.com/Juniper/contrail-controller/src/config/go/pkg/vcenter"
+	"io/ioutil"
+	"net/http"
+)
+
+type ContrailCommandNodes struct {
+	Nodes []struct {
+		Node struct {
+			UUID     string   `json:"uuid"`
+			Name     string   `json:"name"`
+			FqName   []string `json:"fq_name"`
+			Hostname string   `json:"hostname"`
+			NodeType string   `json:"node_type"`
+			Ports    []struct {
+				UUID       string   `json:"uuid"`
+				Name       string   `json:"name"`
+				ParentUUID string   `json:"parent_uuid"`
+				ParentType string   `json:"parent_type"`
+				FqName     []string `json:"fq_name"`
+			} `json:"ports"`
+		} `json:"node"`
+	} `json:"nodes"`
+}
+
+type KeyStoneAuthViaUsername struct {
+	Auth UserAuth `json:"auth"`
+}
+type Domain struct {
+	ID string `json:"id"`
+}
+type User struct {
+	Name     string `json:"name"`
+	Password string `json:"password"`
+	Domain   Domain `json:"domain"`
+}
+type Password struct {
+	User User `json:"user"`
+}
+type UserIdentity struct {
+	Methods  []string `json:"methods"`
+	Password Password `json:"password"`
+}
+type Project struct {
+	Name   string `json:"name"`
+	Domain Domain `json:"domain"`
+}
+type Scope struct {
+	Project Project `json:"project"`
+}
+type UserAuth struct {
+	Identity UserIdentity `json:"identity"`
+	Scope    Scope        `json:"scope"`
+}
+
+type KeyStoneAuthViaClusterToken struct {
+	Auth Auth `json:"auth"`
+}
+type Token struct {
+	ID string `json:"id"`
+}
+type Cluster struct {
+	ID    string `json:"id"`
+	Token Token  `json:"token"`
+}
+type Identity struct {
+	Methods []string `json:"methods"`
+	Cluster Cluster  `json:"cluster"`
+}
+type Auth struct {
+	Identity Identity `json:"identity"`
+}
+
+type ContrailCommand struct {
+	AuthHost      string
+	AuthPort      string
+	AuthPath      string
+	Username      string
+	Password      string
+	UserDomain    string
+	ProjectDomain string
+	ProjectName   string
+	ClusterId     string
+	ClusterToken  string
+	AuthToken     string
+}
+
+type CCPort struct {
+	Name string
+	Uuid string
+}
+
+type CCNode struct {
+	Name  string
+	Uuid  string
+	Ports map[string]CCPort
+}
+
+func GetDefaultValues(CCHost string, username string, password string, clusterID string, clusterToken string) ContrailCommand {
+	CCData := ContrailCommand{}
+	CCData.AuthHost = CCHost
+	CCData.Username = username
+	CCData.Password = password
+	CCData.ClusterId = clusterID
+	CCData.ClusterToken = clusterToken
+	CCData.AuthPath = "/keystone/v3/auth/tokens"
+	CCData.UserDomain = "default"
+	CCData.ProjectName = "admin"
+	CCData.ProjectDomain = "default"
+	return CCData
+}
+
+func (ccData ContrailCommand) GetTokenViaClusterToken() (string, error) {
+	fmt.Println("CCData", ccData.ClusterId, ccData.ClusterToken)
+	authData := KeyStoneAuthViaClusterToken{
+		Auth: Auth{
+			Identity: Identity{
+				Methods: []string{"cluster-token"},
+				Cluster: Cluster{
+					ID: ccData.ClusterId,
+					Token: Token{
+						ID: ccData.ClusterToken,
+					},
+				},
+			},
+		},
+	}
+
+	jsonData, err := json.Marshal(authData)
+	fmt.Println(string(jsonData))
+	resp, _, err := ccData.SendRequest(ccData.AuthPath, string(jsonData), "POST")
+	if err != nil {
+		return "", err
+	}
+	fmt.Println("response Status:", resp.StatusCode)
+	fmt.Println("response Headers:", resp.Header["X-Subject-Token"])
+	//fmt.Println("response Body:", string(body))
+	if resp.StatusCode != 200 && resp.StatusCode != 201 {
+		return "", nil
+	}
+	token := resp.Header["X-Subject-Token"][0]
+	ccData.AuthToken = token
+	return token, nil
+}
+func (ccData *ContrailCommand) GetTokenViaUsername() (string, error) {
+	authData := KeyStoneAuthViaUsername{
+		Auth: UserAuth{
+			Identity: UserIdentity{
+				Methods: []string{"password"},
+				Password: Password{
+					User: User{
+						Name:     ccData.Username,
+						Password: ccData.Password,
+						Domain: Domain{
+							ID: ccData.UserDomain,
+						},
+					},
+				},
+			},
+			Scope: Scope{
+				Project: Project{
+					Name: ccData.ProjectName,
+					Domain: Domain{
+						ID: ccData.ProjectDomain,
+					},
+				},
+			},
+		},
+	}
+
+	jsonData, err := json.Marshal(authData)
+	fmt.Println(string(jsonData))
+	resp, _, err := ccData.SendRequest(ccData.AuthPath, string(jsonData), "POST")
+	if err != nil {
+		return "", err
+	}
+	fmt.Println("response0 Status:", resp.StatusCode)
+	if resp.StatusCode != 200 && resp.StatusCode != 201 {
+		fmt.Println("returning due to non-200")
+		//TODO: send error back
+		return "", nil
+	}
+	fmt.Println("response Headers:", resp.Header["X-Subject-Token"])
+	//fmt.Println("response Body:", string(body))
+	token := resp.Header["X-Subject-Token"][0]
+	ccData.AuthToken = token
+	return token, nil
+}
+
+// Caller should call resp.Body.Close()
+func (cc ContrailCommand) SendRequest(resourcePath string, resourceData string, requestMethod string) (*http.Response, []byte, error) {
+	url := fmt.Sprintf("https://%s%s", cc.AuthHost, resourcePath)
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	req, err := http.NewRequest(requestMethod, url, bytes.NewBufferString(resourceData))
+	if err != nil {
+		return nil, nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	if cc.AuthToken != "" {
+		req.Header.Set("X-Auth-Token", cc.AuthToken)
+	}
+	client := &http.Client{Transport: tr}
+	fmt.Println("REQ", req)
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		return nil, nil, err
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	return resp, body, nil
+}
+
+func (cc ContrailCommand) GetNodes() (map[string]CCNode, error) {
+	resp, body, err := cc.SendRequest("/nodes?detail=true", "", "GET")
+	if err != nil {
+		fmt.Println("GetNodes-Returning", err)
+		return nil, err
+	}
+
+	fmt.Println("GetNodes Status:", resp.StatusCode)
+	ccNodes := make(map[string]CCNode)
+
+	var nodeJSON ContrailCommandNodes
+	err = json.Unmarshal(body, &nodeJSON)
+	if err != nil {
+		panic(err)
+	}
+	for num, node := range nodeJSON.Nodes {
+		fmt.Println("NODE ", node.Node.UUID, node.Node.FqName)
+		fmt.Println(num)
+		ccPorts := make(map[string]CCPort)
+		for _, port := range node.Node.Ports {
+			ccport := CCPort{port.Name, port.UUID}
+			ccPorts[port.Name] = ccport
+		}
+		ccNode := CCNode{node.Node.Name, node.Node.UUID, ccPorts}
+		ccNodes[node.Node.Name] = ccNode
+	}
+	fmt.Println("NODES", ccNodes)
+	return ccNodes, nil
+}
+
+func (cc ContrailCommand) CreateNode(host vc.EsxiHost) error {
+	//fmt.Println(host)
+	jsonStr := fmt.Sprintf(`{"resources": [{"kind": "node","data":{ 
+                                 "node_type": "esxi",
+				 "uuid": "%s",
+                                 "hostname": "%s",
+                                 "fq_name": ["default-global-system-config", "%s"]
+			       }}]}`, host.Uuid, host.Hostname, host.Hostname)
+	resp, _, err := cc.SendRequest("/sync", jsonStr, "POST")
+	fmt.Println("CreateNode:", resp.StatusCode, err)
+	return nil
+}
+
+func (cc ContrailCommand) CreatePort(port vc.EsxiPort, host vc.EsxiHost) error {
+	fmt.Println(port, host.Hostname)
+	jsonStr := fmt.Sprintf(`{"resources": [{"kind": "port","data":{ 
+                                 "name": "%s",
+				 "uuid": "%s",
+                                 "fq_name": ["default-global-system-config", "%s", "%s"],
+				 "bms_port_info": {
+				   "address": "%s",
+				   "local_link_connection": {
+				     "port_id": "%s",
+				     "switch_info": "%s",
+				     "switch_id": "%s",
+				     "port_index": "%s"
+				 }},
+				 "esxi_port_info": {
+				   "dvs_name": "%s"
+				}}}]}`, port.Name, port.Uuid, host.Hostname, port.Name, port.MacAddress,
+		port.PortId, port.SwitchName, port.ChassisId, port.PortIndex, port.DvsName)
+	_, _, err := cc.SendRequest("/sync", jsonStr, "POST")
+	if err != nil {
+		return err
+	}
+	//fmt.Println("CreatePort:", resp.StatusCode, err)
+	return nil
+}
---
+++ src/config/go/pkg/vcenter/client.go
@@ -0,0 +1,141 @@
+package readvcenter
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/vmware/govmomi"
+	"github.com/vmware/govmomi/view"
+	"github.com/vmware/govmomi/vim25/methods"
+	"github.com/vmware/govmomi/vim25/mo"
+	"github.com/vmware/govmomi/vim25/soap"
+	"github.com/vmware/govmomi/vim25/types"
+)
+
+type EsxiPort struct {
+	Name       string
+	MacAddress string
+	SwitchName string
+	PortIndex  string
+	ChassisId  string
+	PortId     string
+	DvsName    string
+	Uuid       string
+}
+
+type EsxiHost struct {
+	Hostname string
+	Uuid     string
+	Ports    map[string]EsxiPort
+}
+
+func GetDataCenterHostSystems(ctx context.Context,
+	datacenter string,
+	sdk_url string,
+	insecureFlag bool) ([]EsxiHost, error) {
+
+	u, err := soap.ParseURL(sdk_url)
+	if err != nil {
+		fmt.Printf("ParseURL error\n")
+		return nil, err
+	}
+
+	c, err := govmomi.NewClient(ctx, u, insecureFlag)
+	if err != nil {
+		fmt.Printf("New Client error : %s\n", err)
+		return nil, err
+	}
+
+	defer c.Logout(ctx)
+
+	m := view.NewManager(c.Client)
+	d, err := m.CreateContainerView(ctx, c.ServiceContent.RootFolder, []string{"Datacenter"}, true)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	defer d.Destroy(ctx)
+	var dcs []mo.Datacenter
+	err = d.Retrieve(ctx, []string{"Datacenter"}, nil, &dcs)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	var esxi_hosts []EsxiHost
+	for _, dc := range dcs {
+		fmt.Println("DataCenter", dc.Name)
+		if dc.Name == datacenter {
+			fmt.Println("DataCenter1", dc.Name)
+			v, err := m.CreateContainerView(ctx, dc.Reference(), []string{"HostSystem"}, true)
+			if err != nil {
+				log.Fatal(err)
+				return nil, err
+			}
+
+			defer v.Destroy(ctx)
+
+			var hss []mo.HostSystem
+			err = v.Retrieve(ctx, []string{"HostSystem"}, nil, &hss)
+			if err != nil {
+				log.Fatal(err)
+				return nil, err
+			}
+
+			fmt.Println("HOSTS: ", hss)
+			for _, hs := range hss {
+				fmt.Println("HOSTS: ", hs.Name)
+				var esxi_host EsxiHost
+				ns := hs.ConfigManager.NetworkSystem
+				req := types.QueryNetworkHint{
+					This: ns.Reference(),
+				}
+				val, err := methods.QueryNetworkHint(ctx, c.Client, &req)
+				if err != nil {
+					return nil, err
+				}
+				var esxi_ports map[string]EsxiPort
+				esxi_ports = make(map[string]EsxiPort)
+				for _, pnic := range hs.Config.Network.Pnic {
+					var esxi_port EsxiPort
+					esxi_port.MacAddress = pnic.Mac
+					esxi_port.Name = pnic.Device
+					for _, proxySw := range hs.Config.Network.ProxySwitch {
+						for _, key := range proxySw.Pnic {
+							if key == pnic.Key {
+								esxi_port.DvsName = proxySw.DvsName
+							}
+						}
+					}
+					esxi_ports[pnic.Device] = esxi_port
+				}
+				for _, lldpNic := range val.Returnval {
+					if lldpNic.LldpInfo != nil {
+						switchName := ""
+						portId := ""
+						for _, param := range lldpNic.LldpInfo.Parameter {
+							if param.Key == "Port Description" {
+								portId = param.Value.(string)
+							} else if param.Key == "System Name" {
+								switchName = param.Value.(string)
+							}
+						}
+						if _, ok := esxi_ports[lldpNic.Device]; ok {
+							var esxi_port EsxiPort
+							esxi_port = esxi_ports[lldpNic.Device]
+							esxi_port.PortId = portId
+							esxi_port.SwitchName = switchName
+							esxi_port.PortIndex = lldpNic.LldpInfo.PortId
+							esxi_port.ChassisId = lldpNic.LldpInfo.ChassisId
+							esxi_ports[lldpNic.Device] = esxi_port
+						}
+					}
+				}
+				esxi_host = EsxiHost{hs.Name, "", esxi_ports}
+				esxi_hosts = append(esxi_hosts, esxi_host)
+			}
+		}
+	}
+	fmt.Println("ESXI_HOSTS", esxi_hosts)
+	return esxi_hosts, nil
+}
