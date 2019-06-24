62aee607edbce9712c6840faf0dc550431b5f39d
Édouard Thuleau <ethuleau@juniper.net>
2019-06-24 15:58:06 +0200

    [config] Fix db_manage.py script
    The heal_fq_name_index does not properly extract resource UUID from the
    FQ name index table.
    
    Change-Id: Ic67ec94e44b6848cedd34c26cf96ffe37368bd0b
    Closes-Jira-Bug: CEM-6692

--- src/config/api-server/vnc_cfg_api_server/db_manage.py
+++ src/config/api-server/vnc_cfg_api_server/db_manage.py
@@ -38,12 +38,15 @@ except ImportError:
     from vnc_cfg_ifmap import VncServerCassandraClient
 import schema_transformer.db
 
-__version__ = "1.15"
+__version__ = "1.16"
 """
 NOTE: As that script is not self contained in a python package and as it
 supports multiple Contrail releases, it brings its own version that needs to be
 manually updated each time it is modified. We also maintain a change log list
 in that header:
+* 1.16
+  - The heal_fq_name_index does not properly extract resource UUID from the
+    FQ name index table
 * 1.15
   - Fix CEM-6463, handle cassandra_use_ssl properly
 * 1.14
@@ -2503,7 +2506,7 @@ class DatabaseHealer(DatabaseManager):
                         fq_name_table.insert(type, cols)
                     continue
                 # FQ name already there, check if it's a stale entry
-                uuid = fq_name_uuid_str.rpartition(':')[-1]
+                uuid = fq_name_uuid_str.popitem()[0].rpartition(':')[-1]
                 try:
                     uuid_table.get(uuid, columns=['type'])
                     # FQ name already use by an object, remove stale object
