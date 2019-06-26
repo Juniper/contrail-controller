6aff8c86d87cbb491b5d558e0441f916ac0118ec
Joe Williams <josephw@juniper.net>
2019-06-26 22:59:11 +0000

    [Fabric] Enterprise-style configuration support for CRB-access@leaf
    Change-Id: I38737c1df2f5b2407ed37cb1d7cf65bfaf8104af
    Target-release: R1908
    Partial-jira-bug: CEM-6253
    Depends-on: Idef6d453e051711ab91ed58dd21b75a6206e4b12

--- src/config/fabric-ansible/ansible-playbooks/filter_plugins/fabric_config_filters.py
+++ src/config/fabric-ansible/ansible-playbooks/filter_plugins/fabric_config_filters.py
@@ -75,6 +75,7 @@ class FilterModule(object):
         self.additional_feature_params = job_input['additional_feature_params']
         self.is_delete = job_input['is_delete']
         self.manage_underlay = job_input['manage_underlay']
+        self.enterprise_style = job_input['enterprise_style']
 
         self.conf_dir = './config/' + self.device_mgmt_ip
         self._load_abstract_config()
@@ -163,6 +164,7 @@ class FilterModule(object):
             additional_feature_params=self.additional_feature_params,
             is_delete=self.is_delete,
             manage_underlay=self.manage_underlay,
+            enterprise_style = self.enterprise_style,
             cfg_group='__contrail_' + feature + model + '__',
             device_model=self.device_model,
             feature_empty=is_empty
