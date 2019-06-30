38078fd970dba3465fd3984f55fe5e5afac04e34
Andrey Pavlov <andrey.mp@gmail.com>
2019-06-30 18:56:40 +0300

    rework UT infra for projects without tox
    CentOS UT infra assumes that xml file with results will be
    created during tests run. This ability was introduced for
    projects that was moved to tox infra.
    
    Change-Id: Ie516e81ae993cd71d75e1f36baa7ba8a1b15376d

--- src/config/schema-transformer/setup.py
+++ src/config/schema-transformer/setup.py
@@ -2,32 +2,6 @@
 # Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
 #
 from setuptools import setup, find_packages, Command
-import os, sys, re
-
-class RunTestsCommand(Command):
-    description = "Test command to run testr in virtualenv"
-    user_options = [
-        ('coverage', 'c',
-         "Generate code coverage report"),
-        ]
-    boolean_options = ['coverage']
-    def initialize_options(self):
-        self.cwd = None
-        self.coverage = False
-    def finalize_options(self):
-        self.cwd = os.getcwd()
-    def run(self):
-        logfname = 'test.log'
-        args = '-V'
-        if self.coverage:
-            logfname = 'coveragetest.log'
-            args += ' -c'
-        rc_sig = os.system('./run_tests.sh %s' % args)
-        if rc_sig >> 8:
-            os._exit(rc_sig>>8)
-        with open(logfname) as f:
-            if not re.search('\nOK', ''.join(f.readlines())):
-                os._exit(1)
 
 setup(
     name='schema_transformer',
@@ -45,7 +19,4 @@ setup(
     install_requires=[
         'jsonpickle'
     ],
-    cmdclass={
-       'run_tests': RunTestsCommand,
-    },
 )
--- src/config/vnc_openstack/setup.py
+++ src/config/vnc_openstack/setup.py
@@ -1,37 +1,8 @@
 #
 # Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
 #
-
-import os
-import os, sys, re
-
 from setuptools import setup, find_packages, Command
 
-
-class RunTestsCommand(Command):
-    description = "Test command to run testr in virtualenv"
-    user_options = [
-        ('coverage', 'c',
-         "Generate code coverage report"),
-        ]
-    boolean_options = ['coverage']
-    def initialize_options(self):
-        self.cwd = None
-        self.coverage = False
-    def finalize_options(self):
-        self.cwd = os.getcwd()
-    def run(self):
-        logfname = 'test.log'
-        args = '-V'
-        if self.coverage:
-            logfname = 'coveragetest.log'
-            args += ' -c'
-        rc_sig = os.system('./run_tests.sh %s' % args)
-        if rc_sig >> 8:
-            os._exit(rc_sig>>8)
-        with open(logfname) as f:
-            if not re.search('\nOK', ''.join(f.readlines())):
-                os._exit(1)
 
 setup(
     name='vnc_openstack',
@@ -53,7 +24,4 @@ setup(
     },
     install_requires=[
     ],
-    cmdclass={
-       'run_tests': RunTestsCommand,
-    },
 )
