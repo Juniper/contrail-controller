5ddaaf40c5657c83ef990fdb45303cbae6ef7513
Andrey Pavlov <andrey.mp@gmail.com>
2019-06-30 18:56:40 +0300

    rework UT infra for projects without tox
    CentOS UT infra assumes that xml file with results will be
    created during tests run. This ability was introduced for
    projects that was moved to tox infra.
    
    Change-Id: Ie516e81ae993cd71d75e1f36baa7ba8a1b15376d

--- src/config/svc-monitor/setup.py
+++ src/config/svc-monitor/setup.py
@@ -3,38 +3,14 @@
 #
 
 import setuptools, re
-import os
 
-class RunTestsCommand(setuptools.Command):
-    description = "Test command to run testr in virtualenv"
-    user_options = [
-        ('coverage', 'c',
-         "Generate code coverage report"),
-        ]
-    boolean_options = ['coverage']
-    def initialize_options(self):
-        self.cwd = None
-        self.coverage = False
-    def finalize_options(self):
-        self.cwd = os.getcwd()
-    def run(self):
-        logfname = 'test.log'
-        args = '-V'
-        if self.coverage:
-            logfname = 'coveragetest.log'
-            args += ' -c'
-        rc_sig = os.system('./run_tests.sh %s' % args)
-        if rc_sig >> 8:
-            os._exit(rc_sig>>8)
-        with open(logfname) as f:
-            if not re.search('\nOK', ''.join(f.readlines())):
-                os._exit(1)
 
 def requirements(filename):
     with open(filename) as f:
         lines = f.read().splitlines()
     c = re.compile(r'\s*#.*')
     return filter(bool, map(lambda y: c.sub('', y).strip(), lines))
+
 
 setuptools.setup(
     name='svc_monitor',
@@ -59,8 +35,5 @@ setuptools.setup(
         'console_scripts' : [
             'contrail-svc-monitor = svc_monitor.svc_monitor:server_main',
         ],
-    },
-    cmdclass={
-       'run_tests': RunTestsCommand,
     },
 )
