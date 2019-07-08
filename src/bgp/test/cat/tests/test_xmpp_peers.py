9f8327b823103f543ee40bab761e92fdad8bfc31
Ananth Suryanarayana <anantha@juniper.net>
2019-07-08 12:34:09 -0700

    Integrate CAT framework based tests with CI
    Run CAT based tests through a new unit test bgp_cat_test
    
    Test is disabled due to additional changes required in agent and cat
    framework.
    
    Change-Id: Ic6de81ceb8278db6b70e1cd8a80187aca7af5766
    Depends-On: I2433740da521e1a9b095ec2db27fe9ead2c58cd0
    partial-jira-bug: CEM-6984

--- src/bgp/test/cat/tests/test_xmpp_peers.py
+++ src/bgp/test/cat/tests/test_xmpp_peers.py
@@ -2,13 +2,14 @@
 ## Copyright (c) 2019 Juniper Networks, Inc. All rights reserved.
 ##
 
+#!/usr/bin/env python
+
 from pyunitreport import HTMLTestRunner
 import os
 import sys
 import unittest
 
-sys.path.append(os.path.abspath(os.path.dirname(__file__))+'/../lib')
-
+sys.path.append('controller/src/bgp/test/cat/lib')
 from cat import *
 
 class TestCAT(unittest.TestCase):
@@ -43,10 +44,13 @@ class TestCAT(unittest.TestCase):
         a3 = CAT.add_agent(self.test, "agent3", [c1])
         ret, ms = CAT.check_connection([c1], [a1, a2, a3], 2, 3)
         self.assertTrue(ret, msg=ms)
+        c1.restart_control_node()
+        ret, ms = CAT.check_connection([c1], [a1, a2, a3], 2, 3)
+        self.assertTrue(ret, msg=ms)
 
     def test_multi_controlnodes_multi_agents(self):
         c1 = CAT.add_control_node(self.test, "con1", 5976)
-        c2 = CAT.add_control_node(self.test, "con2", 5986)
+        c2 = CAT.add_control_node(self.test, "con2", 5979)
         c3 = CAT.add_control_node(self.test, "con3", 5996)
         a1 = CAT.add_agent(self.test, "agent1", [c1, c2])
         a2 = CAT.add_agent(self.test, "agent2", [c1, c3])
