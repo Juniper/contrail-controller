7d7f99e8bf50c979644274254d036fe299ea7cc3
Ananth Suryanarayana <anantha@juniper.net>
2019-07-08 12:34:09 -0700

    Integrate CAT framework based tests with CI
    Run CAT based tests through a new unit test bgp_cat_test
    
    Change-Id: Ic6de81ceb8278db6b70e1cd8a80187aca7af5766
    Depends-On: I2433740da521e1a9b095ec2db27fe9ead2c58cd0
    Depends-On: I7d7379eb71763369d8c7181a69c5cdd99e698044
    partial-jira-bug: CEM-6984

---
+++ src/bgp/test/bgp_cat_test.cc
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2019 Juniper Networks, Inc. All rights reserved.
+ */
+
+#include <unistd.h>
+#include "testing/gunit.h"
+
+class BgpCatTest : public ::testing::Test {
+};
+
+TEST_F(BgpCatTest, Basic) {
+    system("sudo pip install PyUnitReport");
+    system("sudo apt-get -y install liblog4cplus-dev");
+    pid_t child = 0;
+    char *const argv[3] = {
+        (char *) "python",
+        (char *) "controller/src/bgp/test/cat/tests/test_xmpp_peers.py",
+        NULL
+    };
+    if (!(child = vfork()))
+        execv("/usr/bin/python", argv);
+    int status = 0;
+    waitpid(child, &status, 0);
+    if (WEXITSTATUS(status))
+        std::cout << "CAT TESTS FAILED" << std::endl;
+    EXPECT_EQ(0, WEXITSTATUS(status));
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    int result = RUN_ALL_TESTS();
+    return result;
+}
