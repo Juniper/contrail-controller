309800c680059504b698e59f379e36655858c22c
Ananth Suryanarayana <anantha@juniper.net>
2019-07-08 12:34:09 -0700

    Integrate CAT framework based tests with CI
    Run CAT based tests through a new unit test bgp_cat_test
    
    Change-Id: Ic6de81ceb8278db6b70e1cd8a80187aca7af5766
    partial-jira-bug: CEM-6984
    Depends-On: I4368c12a6d1df2d4e3e33c111f55c7ff74c4dc83

--- src/bgp/test/cat/tests/test_xmpp_peers.py
+++ src/bgp/test/cat/tests/test_xmpp_peers.py
@@ -2,13 +2,15 @@
 ## Copyright (c) 2019 Juniper Networks, Inc. All rights reserved.
 ##
 
+#!/usr/bin/env python
+
+import argparse
 from pyunitreport import HTMLTestRunner
 import os
 import sys
 import unittest
 
-sys.path.append(os.path.abspath(os.path.dirname(__file__))+'/../lib')
-
+sys.path.append('controller/src/bgp/test/cat/lib')
 from cat import *
 
 class TestCAT(unittest.TestCase):
@@ -29,25 +31,34 @@ class TestCAT(unittest.TestCase):
     def tearDown(self):
         CAT.clean_up()
 
-    def test_multi_controlnodes_single_agent(self):
-        c1 = CAT.add_control_node(self.test, "con1", 5946)
-        c2 = CAT.add_control_node(self.test, "con2", 5956)
+    def test_1_single_controlnode_single_agent(self):
+        c1 = CAT.add_control_node(self.test, "con1")
+        a1 = CAT.add_agent(self.test, "agent1", [c1])
+        ret, ms = CAT.check_connection([c1], [a1], 2, 3)
+        self.assertTrue(ret, msg=ms)
+
+    def test_2_multi_controlnodes_single_agent(self):
+        c1 = CAT.add_control_node(self.test, "con1")
+        c2 = CAT.add_control_node(self.test, "con2")
         a1 = CAT.add_agent(self.test, "agent1", [c1, c2])
         ret, ms = CAT.check_connection([c1, c2], [a1], 2, 3)
         self.assertTrue(ret, msg=ms)
 
-    def test_single_controlnode_multi_agents(self):
-        c1 = CAT.add_control_node(self.test, "con1", 5966)
+    def test_3_single_controlnode_multi_agents(self):
+        c1 = CAT.add_control_node(self.test, "con1")
         a1 = CAT.add_agent(self.test, "agent1", [c1])
         a2 = CAT.add_agent(self.test, "agent2", [c1])
         a3 = CAT.add_agent(self.test, "agent3", [c1])
         ret, ms = CAT.check_connection([c1], [a1, a2, a3], 2, 3)
         self.assertTrue(ret, msg=ms)
+        c1.restart_control_node()
+        ret, ms = CAT.check_connection([c1], [a1, a2, a3], 2, 3)
+        self.assertTrue(ret, msg=ms)
 
-    def test_multi_controlnodes_multi_agents(self):
-        c1 = CAT.add_control_node(self.test, "con1", 5976)
-        c2 = CAT.add_control_node(self.test, "con2", 5986)
-        c3 = CAT.add_control_node(self.test, "con3", 5996)
+    def test_4_multi_controlnodes_multi_agents(self):
+        c1 = CAT.add_control_node(self.test, "con1")
+        c2 = CAT.add_control_node(self.test, "con2")
+        c3 = CAT.add_control_node(self.test, "con3")
         a1 = CAT.add_agent(self.test, "agent1", [c1, c2])
         a2 = CAT.add_agent(self.test, "agent2", [c1, c3])
         ret, ms = CAT.check_connection([c1, c2], [a1], 2, 3)
@@ -56,7 +67,10 @@ class TestCAT(unittest.TestCase):
         self.assertFalse(ret, msg=ms)
 
 def main():
+
+    CAT.parse_arguments()
     unittest.main(testRunner=HTMLTestRunner(output=CAT.get_report_dir()))
 
 if __name__ == "__main__":
+
     main()
