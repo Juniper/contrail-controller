aa7b729b53f7cd0ed5fe94b71ace9876a67a7902
aadarshk <aadarshk@juniper.net>
2019-07-10 19:19:29 +0530

    Contrail Service Monitor changes for TLS1.2 implementation
    Change-Id: I629851dfc7803d811f6d44d1b2e03995bc26caca
    closes-jira-bug: CEM-7139

--- src/config/svc-monitor/svc_monitor/scheduler/vrouter_scheduler.py
+++ src/config/svc-monitor/svc_monitor/scheduler/vrouter_scheduler.py
@@ -51,7 +51,11 @@ class VRouterScheduler(object):
         analytics_server_list_in_str = ' '.join(analytics_server_list)
         analytics_port = int(server_port_list[1])
 
-        return analytics_client.Client(analytics_server_list_in_str, analytics_port)
+        if (self._args.analytics_api_ssl_enable):
+            return analytics_client.Client(analytics_server_list_in_str, analytics_port,
+                    self._args.analytics_api_ssl_params)
+        else:
+            return analytics_client.Client(analytics_server_list_in_str, analytics_port)
 
     @abc.abstractmethod
     def schedule(self, plugin, context, router_id, candidates=None):
