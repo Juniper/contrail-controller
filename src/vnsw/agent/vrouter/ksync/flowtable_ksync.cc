29b23bc041f32deb66323f6e8ede0ba08a659444
Sergey Kreys <skreys@mirantis.com>
2019-07-11 15:57:18 -0500

    Shrink FreeList container in case of overflow
    In high-load scenarios with a huge flow rate the FreeList container may grow
    rapidly, using too much RAM.
    We actually do not need to store a lot of dummy flow entries in this list, so
    it is safe to just shrink it whenever it reaches some threshold, kMaxThreshold.
    
    Here we implement a new method FlowEntryFreeList::Shrink() that will be
    triggered when the table FreeList size is exceeding kMaxThreshold (100 * 1000).
    We reduce the container size by removing kShrinkSize (4 * 1000) elements, and
    we also free the memory allocated for those entries.
    Also, removed unused variable grow_count_.
    
    Related-Bug: #1571163
    Change-Id: Idee56534ecf6362646c96d9e8d439abe08094cef

--- src/vnsw/agent/vrouter/ksync/flowtable_ksync.cc
+++ src/vnsw/agent/vrouter/ksync/flowtable_ksync.cc
@@ -46,7 +46,9 @@
 const uint32_t KSyncFlowEntryFreeList::kInitCount;
 const uint32_t KSyncFlowEntryFreeList::kTestInitCount;
 const uint32_t KSyncFlowEntryFreeList::kGrowSize;
+const uint32_t KSyncFlowEntryFreeList::kShrinkSize;
 const uint32_t KSyncFlowEntryFreeList::kMinThreshold;
+const uint32_t KSyncFlowEntryFreeList::kMaxThreshold;
 
 using namespace boost::asio::ip;
 
@@ -711,8 +713,8 @@ void FlowTableKSyncObject::Init() {
 // KSyncFlowEntryFreeList implementation
 /////////////////////////////////////////////////////////////////////////////
 KSyncFlowEntryFreeList::KSyncFlowEntryFreeList(FlowTableKSyncObject *object) :
-    object_(object), max_count_(0), grow_pending_(false), total_alloc_(0),
-    total_free_(0), free_list_() {
+    object_(object), max_count_(0), grow_pending_(false),
+    shrink_pending_(false), total_alloc_(0), total_free_(0), free_list_() {
 
     uint32_t count = kInitCount;
     if (object->ksync()->agent()->test_mode()) {
@@ -745,6 +747,20 @@ void KSyncFlowEntryFreeList::Grow() {
     }
 }
 
+// Dispose a chunk of FlowEntries
+void KSyncFlowEntryFreeList::Shrink() {
+    if (free_list_.size() <= kMaxThreshold)
+        return;
+
+    for (uint32_t i = 0; i < kShrinkSize; ++i) {
+        FlowTableKSyncEntry *fe = &free_list_.front();
+        free_list_.pop_front();
+        delete fe;
+        --max_count_;
+    }
+    shrink_pending_ = false;
+}
+
 FlowTableKSyncEntry *KSyncFlowEntryFreeList::Allocate(const KSyncEntry *key) {
     const FlowTableKSyncEntry *flow_key  =
         static_cast<const FlowTableKSyncEntry *>(key);
@@ -772,14 +788,23 @@ FlowTableKSyncEntry *KSyncFlowEntryFreeList::Allocate(const KSyncEntry *key) {
 }
 
 void KSyncFlowEntryFreeList::Free(FlowTableKSyncEntry *flow) {
+    FlowTable *ft = flow->flow_entry()->flow_table();
     total_free_++;
     flow->Reset();
     free_list_.push_back(*flow);
-    // TODO : Free entry if beyond threshold
+    if (!shrink_pending_ && free_list_.size() > kMaxThreshold) {
+        shrink_pending_ = true;
+        FlowProto *proto = object_->ksync()->agent()->pkt()->get_flow_proto();
+        proto->ShrinkFreeListRequest(ft);
+    }
 }
 
 void FlowTableKSyncObject::GrowFreeList() {
     free_list_.Grow();
+}
+
+void FlowTableKSyncObject::ShrinkFreeList() {
+    free_list_.Shrink();
 }
 
 // We want to handle KSync transitions for flow from Flow task context.
