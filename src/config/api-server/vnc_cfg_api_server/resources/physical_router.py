27a23968f477cb1fb2cada61eeabeb9144fd995b
supriyas <supriyas@juniper.net>
2019-07-23 15:38:15 -0700

    Encrypt PR pwd while writing it into DB
    Closes-Jira-Bug: CEM-7083
    Change-Id: Iaa62843ea2d9c4dfe28259720e6f3cd0d1364de8

--- src/config/api-server/vnc_cfg_api_server/resources/physical_router.py
+++ src/config/api-server/vnc_cfg_api_server/resources/physical_router.py
@@ -11,11 +11,30 @@ from vnc_cfg_api_server.resources._resource_base import ResourceMixin
 
 
 class PhysicalRouterServer(ResourceMixin, PhysicalRouter):
+
     @classmethod
-    def post_dbe_read(cls, obj_dict, db_conn):
+    def post_dbe_list(cls, obj_result_list, db_conn):
+        for obj_result in obj_result_list:
+            if obj_result.get('physical-router'):
+                ok, err_msg = cls.post_dbe_read(obj_result['physical-router'],
+                                                db_conn)
+                if not ok:
+                    return ok, err_msg
+        return True, ''
+
+    @classmethod
+    def pre_dbe_create(cls, tenant_name, obj_dict, db_conn):
+        if obj_dict.get('physical_router_managed_state'):
+            state = obj_dict.get('physical_router_managed_state')
+            if state == 'rma' or state == 'activating':
+                msg = "Managed state cannot be %s for router %s" % (
+                    state, obj_dict.get('fq_name'))
+                return False, (400, msg)
+
+        # encrypt password before writing to DB
         if obj_dict.get('physical_router_user_credentials') and \
-           obj_dict.get('physical_router_user_credentials', {}).get(
-                'password'):
+                obj_dict.get('physical_router_user_credentials', {}).get(
+                    'password'):
 
             if cls.server._args.auth == 'keystone':
                 admin_password = cls.server._auth_svc.args.admin_password
@@ -43,26 +62,5 @@ class PhysicalRouterServer(ResourceMixin, PhysicalRouter):
 
             password = base64.b64encode(cipher.encrypt(padded_text))
             obj_dict['physical_router_user_credentials']['password'] = password
-
-        return True, ''
-
-    @classmethod
-    def post_dbe_list(cls, obj_result_list, db_conn):
-        for obj_result in obj_result_list:
-            if obj_result.get('physical-router'):
-                ok, err_msg = cls.post_dbe_read(obj_result['physical-router'],
-                                                db_conn)
-                if not ok:
-                    return ok, err_msg
-        return True, ''
-
-    @classmethod
-    def pre_dbe_create(cls, tenant_name, obj_dict, db_conn):
-        if obj_dict.get('physical_router_managed_state'):
-            state = obj_dict.get('physical_router_managed_state')
-            if state == 'rma' or state == 'activating':
-                msg = "Managed state cannot be %s for router %s" % (
-                    state, obj_dict.get('fq_name'))
-                return False, (400, msg)
 
         return True, ''
