a16886498aa292df10316c5c18f1a6b8155f359c
Sathish Holla <saholla@juniper.net>
2019-07-24 22:05:46 -0700

    [Config] Add range checks for user created route targets
    Route targets will be in the format of target:<ASN>:<target_value>
    This patch takes care of following.
    1. For user created Route Targets(RT), verify the ASN to be within range.
       Based on enable_4byte_asn flag, the range can be either 1-0xFFFF or
       1-0xFFFFFFFF
    2. For user created RTs, verify the <target_value> is not within contrail reserved values
       For a 2 byte ASN, this reserved range is 8000000 to (1 << 24)
       For a 4 byte ASN, this reserved range is 8000 to (1 << 15)
    3. Cache the enable_4byte_as_flag inside the api server.
       As part of this change, the patch also takes care ofupdating the internal
       global_autonomous_number and enable_4_byte_as flag as part of an update_notification
    
    Depends-On: If8b2fa05e9a3c93121df7ef92b2f47daded2a3b5
    Closes-Jira-Bug: CEM-6701
    Change-Id: I904a7ae0c84f2352f9a5d9a23773aee760c3a3f8

--- src/config/api-server/vnc_cfg_api_server/resources/bgp_as_a_service.py
+++ src/config/api-server/vnc_cfg_api_server/resources/bgp_as_a_service.py
@@ -28,6 +28,19 @@ class BgpAsAServiceServer(ResourceMixin, BgpAsAService):
         return True, ""
 
     @classmethod
+    def _check_asn(cls, obj_dict):
+        local_asn = obj_dict.get('autonomous_system')
+        if not local_asn:
+            return True, ''
+
+        ok, result = cls.server.get_resource_class(
+            'global_system_config').check_asn_range(local_asn)
+        if not ok:
+            return ok, result
+
+        return True, ''
+
+    @classmethod
     def pre_dbe_create(cls, tenant_name, obj_dict, db_conn):
         if 'control_node_zone_refs' in obj_dict:
             (ok, msg) = cls._validate_control_node_zone_dep(obj_dict)
