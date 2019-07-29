98c6af7f3cd9774effe9be130fb2f1819ad8df4e
supriyas <supriyas@juniper.net>
2019-07-29 16:43:16 -0700

    Encrypt the PR password during DB create
    closes-jira-bug: CEM-7083
    Change-Id: Ie9fefea5c74cb284da5aabefb24b16d341c17d04

--- src/config/api-server/vnc_cfg_api_server/resources/physical_router.py
+++ src/config/api-server/vnc_cfg_api_server/resources/physical_router.py
@@ -11,27 +11,6 @@ from vnc_cfg_api_server.resources._resource_base import ResourceMixin
 
 
 class PhysicalRouterServer(ResourceMixin, PhysicalRouter):
-    @classmethod
-    def post_dbe_read(cls, obj_dict, db_conn):
-        if obj_dict.get('physical_router_user_credentials') and \
-           obj_dict.get('physical_router_user_credentials', {}).get(
-                'password'):
-
-            if cls.server._args.auth == 'keystone':
-                admin_password = cls.server._auth_svc.args.admin_password
-            else:
-                admin_password = cls.server._auth_svc._conf_info.get(
-                    'admin_password')
-
-            key = admin_password.rjust(16)
-            cipher = AES.new(key, AES.MODE_ECB)
-
-            padded_text = (obj_dict.get('physical_router_user_credentials',
-                                        {}).get('password')).rjust(32)
-            password = base64.b64encode(cipher.encrypt(padded_text))
-            obj_dict['physical_router_user_credentials']['password'] = password
-
-        return True, ''
 
     @classmethod
     def post_dbe_list(cls, obj_result_list, db_conn):
@@ -51,5 +30,24 @@ class PhysicalRouterServer(ResourceMixin, PhysicalRouter):
                 msg = "Managed state cannot be %s for router %s" % (
                     state, obj_dict.get('fq_name'))
                 return False, (400, msg)
+
+        # encrypt password before writing to DB
+        if obj_dict.get('physical_router_user_credentials') and \
+                obj_dict.get('physical_router_user_credentials', {}).get(
+                    'password'):
+
+            if cls.server._args.auth == 'keystone':
+                admin_password = cls.server._auth_svc.args.admin_password
+            else:
+                admin_password = cls.server._auth_svc._conf_info.get(
+                    'admin_password')
+
+            key = admin_password.rjust(16)
+            cipher = AES.new(key, AES.MODE_ECB)
+
+            padded_text = (obj_dict.get('physical_router_user_credentials',
+                                        {}).get('password')).rjust(32)
+            password = base64.b64encode(cipher.encrypt(padded_text))
+            obj_dict['physical_router_user_credentials']['password'] = password
 
         return True, ''
