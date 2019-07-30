d014e205a41ad2cc4dae50add60b4cc7308bd114
Ananth Suryanarayana <anantha@juniper.net>
2019-07-30 13:17:24 -0700

    Fix flakiness in bgp_cat_test
    Some times scons launches bgp_cat_test too soon, even though the
    dependency to contrail-vrouter-agent and bgp_ifmap_xmpp_integration_test
    has been clearly specificed.
    
    Wait in bgp_cat_test until all dependents are ready for execution
    
    Change-Id: I35c330e01b0559b62dfac979830be1e0a740106b
    Partial-Jira-Bug: CEM-6984

--- src/bgp/test/bgp_cat_test.cc
+++ src/bgp/test/bgp_cat_test.cc
@@ -3,15 +3,35 @@
  */
 
 #include <unistd.h>
+#include "base/test/task_test_util.h"
+#include "bgp/bgp_log.h"
 #include "testing/gunit.h"
+
+using std::cout;
+using std::endl;
+using std::string;
 
 class BgpCatTest : public ::testing::Test {
 };
 
 TEST_F(BgpCatTest, Basic) {
-    system("sudo pip install PyUnitReport");
-    system("sudo apt-get -y install liblog4cplus-dev");
-    system("sudo yum -y install liblog4cplus-dev");
+    system("sudo pip install PyUnitReport 2>/dev/null");
+    system("sudo apt-get -y install liblog4cplus-dev 2>/dev/null");
+    system("sudo yum -y install liblog4cplus-dev 2>/dev/null");
+
+    EXPECT_EQ(0, access("/usr/bin/python", X_OK));
+
+    // Wait for the mock agent and control-node binaries availability.
+    string build_dir = getenv("CONTRAIL_BUILD_DIR") ?: "build/debug";
+    TASK_UTIL_WAIT_EQ(0, access((build_dir +
+        "/bgp/test/bgp_ifmap_xmpp_integration_test").c_str(), X_OK), 5000000,
+            120, "Waiting for bgp_ifmap_xmpp_integration_test to get built");
+    TASK_UTIL_WAIT_EQ(0, access((build_dir +
+        "/vnsw/agent/contrail/contrail-vrouter-agent").c_str(), X_OK), 5000000,
+            120, "Waiting for contrail-vrouter-agent to get built");
+
+    // Wait for some time to ensure that linked excutables are ready.
+    sleep(30);
     pid_t child = 0;
     char *const argv[3] = {
         (char *) "python",
@@ -23,7 +43,7 @@ TEST_F(BgpCatTest, Basic) {
     int status = 0;
     waitpid(child, &status, 0);
     if (WEXITSTATUS(status))
-        std::cout << "CAT TESTS FAILED" << std::endl;
+        cout << "CAT TESTS FAILED" << endl;
     EXPECT_EQ(0, WEXITSTATUS(status));
 }
 
