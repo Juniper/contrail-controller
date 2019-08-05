7fcb5a8b7171bdd2fd909615dbde64af08ea89e7
Manish Sahu <msahu@juniper.net>
2019-08-05 22:20:11 +0530

    parse new extended community subcluster:<as>:<number>
    1. Implemented necessary FromString() and ToString() methods.
    2. Provided support for both 2 byte and 4 byte as formats
    
    Change-Id: I3268e427e1801bf51c9bf38a33b9e71780729a06
    closes-jira-bug: CEM-6741

---
+++ src/bgp/extended-community/sub_cluster.cc
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2019 Juniper Networks, Inc. All rights reserved.
+ */
+
+#include "bgp/extended-community/sub_cluster.h"
+
+#include <algorithm>
+
+#include "base/address.h"
+
+using std::copy;
+using std::string;
+
+SubCluster SubCluster::null_sub_cluster;
+
+SubCluster::SubCluster() {
+    data_.fill(0);
+}
+
+SubCluster::SubCluster(const bytes_type &data) {
+    copy(data.begin(), data.end(), data_.begin());
+}
+
+SubCluster::SubCluster(as_t asn, const uint16_t sub_cluster_asn) {
+    data_[0] = BgpExtendedCommunityType::Experimental4ByteAs;
+    data_[1] = BgpExtendedCommunitySubType::SubCluster;
+    put_value(&data_[2], 4, asn);
+    put_value(&data_[6], SubCluster::kSize - 6, sub_cluster_asn);
+}
+
+uint16_t SubCluster::SubClusterAsn() const {
+    return get_value(&data_[6], 2);
+}
+
+string SubCluster::ToString() const {
+    uint8_t data[SubCluster::kSize];
+    copy(data_.begin(), data_.end(), &data[0]);
+    char temp[50];
+    as_t asn = get_value(data + 2, 4);
+    uint16_t sub_cluster_asn = get_value(data + 6, 2);
+    snprintf(temp, sizeof(temp), "subcluster:%u:%u", asn, sub_cluster_asn);
+    return string(temp);
+}
+
+SubCluster SubCluster::FromString(const string &str,
+    boost::system::error_code *errorp) {
+    SubCluster sc;
+    uint8_t data[SubCluster::kSize];
+
+    // subcluster:1:2
+    size_t pos = str.find(':');
+    if (pos == string::npos) {
+        if (errorp != NULL) {
+            *errorp = make_error_code(boost::system::errc::invalid_argument);
+        }
+        return SubCluster::null_sub_cluster;
+    }
+
+    string first(str.substr(0, pos));
+    if (first != "subcluster") {
+        if (errorp != NULL) {
+            *errorp = make_error_code(boost::system::errc::invalid_argument);
+        }
+        return SubCluster::null_sub_cluster;
+    }
+
+    string rest(str.substr(pos+1));
+
+    pos = rest.find(':');
+    if (pos == string::npos) {
+        if (errorp != NULL) {
+            *errorp = make_error_code(boost::system::errc::invalid_argument);
+        }
+        return SubCluster::null_sub_cluster;
+    }
+
+    boost::system::error_code ec;
+    string second(rest.substr(0, pos));
+    int offset = 6;
+    char *endptr;
+    int64_t asn = strtol(second.c_str(), &endptr, 10);
+    if (asn == 0 || asn >= 0xFFFFFFFF || *endptr != '\0') {
+        if (errorp != NULL) {
+            *errorp = make_error_code(boost::system::errc::invalid_argument);
+        }
+        return SubCluster::null_sub_cluster;
+    }
+    data[0] = BgpExtendedCommunityType::Experimental4ByteAs;
+    data[1] = BgpExtendedCommunitySubType::SubCluster;
+    put_value(&data[2], 4, asn);
+
+    string third(rest.substr(pos+1));
+    uint64_t sub_cluster_asn = strtol(third.c_str(), &endptr, 10);
+    // Check sub-cluster-asn for type 0x82.
+    if (sub_cluster_asn == 0 || sub_cluster_asn > 0xFFFF || *endptr != '\0') {
+        if (errorp != NULL) {
+            *errorp = make_error_code(boost::system::errc::invalid_argument);
+        }
+        return SubCluster::null_sub_cluster;
+    }
+
+    put_value(&data[offset], SubCluster::kSize - offset, sub_cluster_asn);
+    copy(&data[0], &data[SubCluster::kSize], sc.data_.begin());
+    return sc;
+}
---
+++ src/bgp/extended-community/test/sub_cluster_test.cc
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2019 Juniper Networks, Inc. All rights reserved.
+ */
+
+#include "bgp/extended-community/sub_cluster.h"
+#include "testing/gunit.h"
+
+using namespace std;
+
+class SubClusterTest : public ::testing::Test {
+};
+
+TEST_F(SubClusterTest, ByteArrayTypeExperimental4ByteAs_1) {
+    SubCluster::bytes_type data =
+    { { BgpExtendedCommunityType::Experimental4ByteAs,
+        BgpExtendedCommunitySubType::SubCluster,
+        0, 0, 0xFF, 0x84, 0x00, 0x01 } };
+    SubCluster sub_cluster(data);
+    EXPECT_FALSE(sub_cluster.IsNull());
+    EXPECT_EQ(0x82, sub_cluster.Type());
+    EXPECT_EQ(0x11, sub_cluster.Subtype());
+    EXPECT_EQ("subcluster:65412:1", sub_cluster.ToString());
+}
+
+TEST_F(SubClusterTest, ByteArrayTypeExperimental4ByteAs_2) {
+    SubCluster::bytes_type data = { {
+        BgpExtendedCommunityType::Experimental4ByteAs,
+        BgpExtendedCommunitySubType::SubCluster,
+        0 ,0 ,0xFF, 0x84, 0xFF, 0xFE
+    } };
+    SubCluster sub_cluster(data);
+    EXPECT_FALSE(sub_cluster.IsNull());
+    EXPECT_EQ(0x82, sub_cluster.Type());
+    EXPECT_EQ(0x11, sub_cluster.Subtype());
+    EXPECT_EQ("subcluster:65412:65534", sub_cluster.ToString());
+}
+
+TEST_F(SubClusterTest, ByteArrayTypeExperimental4ByteAs_3) {
+    SubCluster::bytes_type data = { {
+        BgpExtendedCommunityType::Experimental4ByteAs,
+        BgpExtendedCommunitySubType::SubCluster,
+        0xFF ,0xFF ,0xFF, 0xFE, 0xFF, 0xFE
+    } };
+    SubCluster sub_cluster(data);
+    EXPECT_FALSE(sub_cluster.IsNull());
+    EXPECT_EQ(0x82, sub_cluster.Type());
+    EXPECT_EQ(0x11, sub_cluster.Subtype());
+    EXPECT_EQ("subcluster:4294967294:65534", sub_cluster.ToString());
+}
+
+TEST_F(SubClusterTest, FromStringTypeExperimental4ByteAs_1) {
+    boost::system::error_code ec;
+    SubCluster sub_cluster =
+        SubCluster::FromString("subcluster:65412:1", &ec);
+    EXPECT_EQ(0, ec.value());
+    EXPECT_EQ(0x82, sub_cluster.Type());
+    EXPECT_EQ(0x11, sub_cluster.Subtype());
+    EXPECT_EQ("subcluster:65412:1", sub_cluster.ToString());
+}
+
+TEST_F(SubClusterTest, FromStringTypeExperimental4ByteAs_2) {
+    boost::system::error_code ec;
+    SubCluster sub_cluster =
+        SubCluster::FromString("subcluster:65412:65534", &ec);
+    EXPECT_EQ(0, ec.value());
+    EXPECT_EQ(0x82, sub_cluster.Type());
+    EXPECT_EQ(0x11, sub_cluster.Subtype());
+    EXPECT_EQ("subcluster:65412:65534", sub_cluster.ToString());
+}
+
+// Does not contain the second colon.
+TEST_F(SubClusterTest, ErrorExperimental4ByteAs_1) {
+    boost::system::error_code ec;
+    SubCluster sub_cluster =
+        SubCluster::FromString("subcluster:65412-65535", &ec);
+    EXPECT_NE(0, ec.value());
+    EXPECT_TRUE(sub_cluster.IsNull());
+}
+
+// AS number is 0.
+TEST_F(SubClusterTest, ErrorExperimental4ByteAs_2) {
+    boost::system::error_code ec;
+    SubCluster sub_cluster =
+        SubCluster::FromString("subcluster:0:100", &ec);
+    EXPECT_NE(0, ec.value());
+    EXPECT_TRUE(sub_cluster.IsNull());
+}
+
+// AS number is 0xFFFFFFFF.
+TEST_F(SubClusterTest, ErrorExperimental4ByteAs_3) {
+    boost::system::error_code ec;
+    SubCluster sub_cluster =
+        SubCluster::FromString("subcluster:4294967295:100", &ec);
+    EXPECT_NE(0, ec.value());
+    EXPECT_TRUE(sub_cluster.IsNull());
+}
+
+// AS number is greater than 0xFFFFFFFF.
+TEST_F(SubClusterTest, ErrorExperimental4ByteAs_4) {
+    boost::system::error_code ec;
+    SubCluster sub_cluster =
+        SubCluster::FromString("subcluster:4294967296:100", &ec);
+    EXPECT_NE(0, ec.value());
+    EXPECT_TRUE(sub_cluster.IsNull());
+}
+
+// sub-cluster-asn equal to 0.
+TEST_F(SubClusterTest, ErrorExperimental4ByteAs_5) {
+    boost::system::error_code ec;
+    SubCluster sub_cluster =
+        SubCluster::FromString("subcluster:65412:0", &ec);
+    EXPECT_NE(0, ec.value());
+    EXPECT_TRUE(sub_cluster.IsNull());
+}
+
+// sub-cluster-asn greater than 0xFFFF
+TEST_F(SubClusterTest, ErrorExperimental4ByteAs_6) {
+    boost::system::error_code ec;
+    SubCluster sub_cluster =
+        SubCluster::FromString("subcluster:65412:65536", &ec);
+    EXPECT_NE(0, ec.value());
+    EXPECT_TRUE(sub_cluster.IsNull());
+}
+
+// sub-cluster-asn field is bad.
+TEST_F(SubClusterTest, ErrorExperimental4ByteAs_7) {
+    boost::system::error_code ec;
+    SubCluster sub_cluster =
+        SubCluster::FromString("subcluster:65412:10.1.1.1", &ec);
+    EXPECT_NE(0, ec.value());
+    EXPECT_TRUE(sub_cluster.IsNull());
+}
+
+
+// Get sub_cluster_asn from subcluster
+TEST_F(SubClusterTest, GetSubClusterAsn) {
+    SubCluster::bytes_type data =
+    { { BgpExtendedCommunityType::Experimental4ByteAs,
+        BgpExtendedCommunitySubType::SubCluster,
+        0, 0, 0xff, 0x84, 0x01, 0x02 } };
+    SubCluster sub_cluster(data);
+    EXPECT_FALSE(sub_cluster.IsNull());
+    EXPECT_EQ(0x82, sub_cluster.Type());
+    EXPECT_EQ(0x11, sub_cluster.Subtype());
+    EXPECT_EQ("subcluster:65412:258", sub_cluster.ToString());
+    EXPECT_EQ(0x102, sub_cluster.SubClusterAsn());
+}
+
+// Set sub_cluster_asn
+TEST_F(SubClusterTest, SetSubClusterAsn) {
+    as_t asn = 65412345;
+    uint16_t sub_cluster_asn = 100;
+    SubCluster sub_cluster(asn, sub_cluster_asn);
+    EXPECT_FALSE(sub_cluster.IsNull());
+    EXPECT_EQ(0x82, sub_cluster.Type());
+    EXPECT_EQ(0x11, sub_cluster.Subtype());
+    EXPECT_EQ("subcluster:65412345:100", sub_cluster.ToString());
+    EXPECT_EQ(100, sub_cluster.SubClusterAsn());
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    int result = RUN_ALL_TESTS();
+    return result;
+}
