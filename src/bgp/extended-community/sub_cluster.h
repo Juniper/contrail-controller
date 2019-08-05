dc71d1814c065f3f5bd774116137febcf295493e
Manish Sahu <msahu@juniper.net>
2019-08-05 22:20:11 +0530

    parse new extended community subcluster:<as>:<number>
    1. Implemented necessary FromString() and ToString() methods.
    2. Provided support for both 2 byte and 4 byte as formats
    
    Change-Id: I3268e427e1801bf51c9bf38a33b9e71780729a06
    closes-jira-bug: CEM-6741

---
+++ src/bgp/extended-community/sub_cluster.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2017 Juniper Networks, Inc. All rights reserved.
+ */
+
+#ifndef SRC_BGP_EXTENDED_COMMUNITY_SUB_CLUSTER_H_
+#define SRC_BGP_EXTENDED_COMMUNITY_SUB_CLUSTER_H_
+
+#include <boost/array.hpp>
+#include <boost/system/error_code.hpp>
+
+#include <string>
+
+#include "base/parse_object.h"
+#include "bgp/extended-community/types.h"
+
+class SubCluster {
+public:
+    static const int kSize = 8;
+    static SubCluster null_sub_cluster;
+    typedef boost::array<uint8_t, kSize> bytes_type;
+
+    SubCluster();
+    explicit SubCluster(const bytes_type &data);
+    SubCluster(const uint32_t asn, const uint32_t sub_cluster_id);
+
+    uint32_t GetAsn() const;
+    bool IsNull() const { return operator==(SubCluster::null_sub_cluster); }
+    uint8_t Type() const { return data_[0]; }
+    uint8_t Subtype() const { return data_[1]; }
+
+    bool operator<(const SubCluster &rhs) const {
+        return data_ < rhs.data_;
+    }
+    bool operator>(const SubCluster &rhs) const {
+        return data_ > rhs.data_;
+    }
+    bool operator==(const SubCluster &rhs) const {
+        return data_ == rhs.data_;
+    }
+    bool operator!=(const SubCluster &rhs) const {
+        return data_ != rhs.data_;
+    }
+
+    const bytes_type &GetExtCommunity() const { return data_; }
+    const uint64_t GetExtCommunityValue() const {
+        return get_value(data_.begin(), 8);
+    }
+
+    std::string ToString() const;
+    static SubCluster FromString(const std::string &str,
+        boost::system::error_code *error = NULL);
+
+private:
+    bytes_type data_;
+};
+
+#endif  // SRC_BGP_EXTENDED_COMMUNITY_SUB_CLUSTER_H_
