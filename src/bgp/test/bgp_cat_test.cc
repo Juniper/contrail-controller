853c411ca300b3057d4107bf19b381ec82f8f61c
Ananth Suryanarayana <anantha@juniper.net>
2019-08-19 18:53:18 -0700

    Rewrite CAT Tests framework in GoLang
    Add basic test to verify connectivity among multiple mock control-nodes and
    mock agents.
    
    For now, run both python based and golang based tests in CI
    
    Change-Id: I66b498e4994cfd7b0fa73882da9115766a297eed
    Partial-Jira-Bug: CEM-7956

--- src/bgp/test/bgp_cat_test.cc
+++ src/bgp/test/bgp_cat_test.cc
@@ -14,7 +14,7 @@ using std::string;
 class BgpCatTest : public ::testing::Test {
 };
 
-TEST_F(BgpCatTest, Basic) {
+TEST_F(BgpCatTest, BasicPython) {
     int e;
     e = system("sudo pip install PyUnitReport 2>/dev/null");
     e = system("sudo pip install colorama 2>/dev/null");
@@ -33,7 +33,27 @@ TEST_F(BgpCatTest, Basic) {
     int status = 0;
     waitpid(child, &status, 0);
     if (WEXITSTATUS(status))
-        cout << "CAT TESTS FAILED" << endl;
+        cout << "CAT PYTHON TESTS FAILED" << endl;
+    EXPECT_EQ(0, WEXITSTATUS(status));
+}
+
+TEST_F(BgpCatTest, BasicGoLang) {
+    int e = system("sudo apt-get -y install golang-go 2>/dev/null");
+    EXPECT_EQ(true, e == 0 || e != 0);
+    pid_t child = 0;
+    char *const argv[3] = {
+        (char *) "go",
+        (char *) "test",
+        NULL
+    };
+    if (!(child = vfork())) {
+        chdir("controller/src/bgp/test/cat/lib");
+        execv("/usr/bin/go", argv);
+    }
+    int status = 0;
+    waitpid(child, &status, 0);
+    if (WEXITSTATUS(status))
+        cout << "CAT GOLANG TESTS FAILED" << endl;
     EXPECT_EQ(0, WEXITSTATUS(status));
 }
 
