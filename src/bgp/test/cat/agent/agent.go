8683634341811835bc8a8ba9bde20e0597253701
Marcus Hines <mhines@juniper.net>
2019-08-21 14:14:29 -0700

    Rewrite CAT Tests framework in GoLang
        Add basic test to verify connectivity among multiple mock control-nodes and
        mock agents.
    
        For now, run both python based and golang based tests in CI
    
        Partial-Jira-Bug: CEM-7956
    
    Change-Id: I02db391087e1aa4a4f88e8c315167379269a981c

---
+++ src/bgp/test/cat/agent/agent.go
@@ -0,0 +1,120 @@
+package agent
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"syscall"
+
+	"cat/sut"
+)
+
+const agentName = "control-vrouter-agent"
+
+// Agent is a SUT component for VRouter Agent.
+type Agent struct {
+	Component sut.Component
+	XMPPPorts []int
+}
+
+func New(m sut.Manager, name, test string) (*Agent, error) {
+	a := &Agent {
+		Component: sut.Component{
+			Name: name,
+		Manager: m,
+		LogDir: filepath.Join(m.LogDir, test, agentName, name, "log"),
+		ConfDir: filepath.Join(m.LogDir, test, agentName, name, "conf"),
+		Config: sut.Config{
+			PID: 0,
+		  HTTPPort: 0,
+			XMPPPort: 0,
+		},
+	 ConfFile: "",
+	},
+}
+	if err := os.MkdirAll(a.Component.ConfDir, 0755); err != nil {
+		return nil, fmt.Errorf("failed to make conf directory: %v", err)
+	}
+	if err := os.MkdirAll(a.Component.LogDir, 0755); err != nil {
+		return nil, fmt.Errorf("failed to make log directory: %v", err)
+	}
+	return a, nil
+}
+
+// Stop will stop the component sending a signal to the process.
+func (a *Agent) Stop(signal syscall.Signal) {
+	syscall.Kill(c.Component.Config.PID, signal) // syscall.SIGHUP
+  os.Remove(strconv.Itoa(item.Pid) + ".json")
+}
+
+func (a *Agent) CreateAgent(control_nodes []*ControlNode) {
+	var ports []int
+	for _, i := range control_nodes {
+		ports = append(ports, i.Ports.XmppPort)
+	}
+	a.XmppPorts = ports
+	a.CreateConfiguration()
+	a.Pid = a.Start()
+}
+
+func (a *Agent) Start() int {
+	c1 :=
+		"../../../../../../build/debug/vnsw/agent/contrail/contrail-vrouter-agent"
+	if _, err := os.Stat(c1); os.IsNotExist(err) {
+		log.Fatal(err)
+	}
+	env := map[string]string{
+		"LD_LIBRARY_PATH": "../../../../../../build/lib",
+		"LOGNAME":         os.Getenv("USER"),
+	}
+	var enva []string
+	for k, v := range env {
+		enva = append(enva, k+"="+v)
+	}
+	cmd := exec.Command(c1, "--config_file="+a.ConfFile)
+	cmd.Env = enva
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return cmd.Process.Pid
+}
+
+func (a *Agent) CreateConfiguration() {
+	sample_conf := "contrail-vrouter-agent.conf"
+	file, err := os.Open(sample_conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	var new_conf []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, "xmpp_port") {
+			updated_line := "servers="
+			for _, p := range a.XmppPorts {
+				updated_line = updated_line + "127.0.0.1:" + strconv.Itoa(p) + " "
+			}
+			new_conf = append(new_conf, updated_line)
+		} else if strings.Contains(line, "agent_name") {
+			new_conf = append(new_conf, "agent_name="+a.Name)
+		} else if strings.Contains(line, "log_file") {
+			new_conf = append(new_conf, "log_file="+a.LogDir+"/"+
+				a.Name+".log")
+		} else {
+			new_conf = append(new_conf, line)
+		}
+	}
+
+	a.ConfFile = a.ConfDir + "/" + a.Name + ".conf"
+	file, err = os.Create(a.ConfFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	for _, line := range new_conf {
+		file.WriteString(line + "\n")
+	}
+}
---
+++ src/bgp/test/cat/cat.go
@@ -0,0 +1,131 @@
+package cat
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+
+	"cat/agent"
+	"cat/controlnode"
+	"cat/sut"
+)
+
+// CAT is a contrail automated test.
+type CAT struct {
+	sut           component.SUT
+	PauseAfterRun bool
+
+	ControlNodes []*controlNode.ControlNode
+	Agents       []*agent.Agent
+}
+
+// Timestamp format for logfiles.
+const timestamp = "20060102_150405"
+
+// New creates an initialized CAT instance.
+func New() (*CAT, error) {
+	c := &CAT{}
+	now := time.Now()
+
+	c.sut.LogDir = filepath.Join("/tmp/CAT", os.Getenv("USER"), now.Format(timestamp))
+	if err := os.MkdirsAll(c.LogDir); err != nil {
+		return nil, fmt.Errorf("failed to create logdir %q :%v", c.LogDir, err)
+	}
+	fmt.Println("Logs are in " + c.LogDir)
+	c.setHostIP()
+
+	if err := os.MkdirAll(c.sut.ReportDir); err != nil {
+		return nil, fmt.Errorf("failed to make report directory: %v", err)
+	}
+	return c
+}
+
+
+func (c *CAT) RedirectStdOuterr() {
+	/*
+	   old := os.dup(1)
+	   os.close(1)
+	   f = self.log_files + "/" + self.Name + ".log"
+	   open(f, 'w')
+	   os.open(f, os.O_WRONLY)
+
+	   old = os.dup(2)
+	   os.close(2)
+	   f = self.log_files + "/" + self.Name + ".log"
+	   open(f, 'w')
+	   os.open(f, os.O_WRONLY)
+	*/
+}
+
+// Teardown stops all components and closes down the CAT instance.
+func (c *CAT) Teardown() {
+	if c.PauseAfterRun {
+		c.Pause()
+	}
+	for _, cn := range c.ControlNodes {
+		cn.Stop()
+	}
+	for _, a := range c.Agents {
+		a.Stop()
+	}
+	os.Remove(c.LogDir)
+}
+
+func (c *CAT) Pause() {
+	cmd := exec.Command("/usr/bin/python")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	time.Sleep(2600 * time.Second)
+}
+
+func (c *CAT) hostIP() error {
+	cmd := exec.Command("hostname", "-i")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal("Cannot find host ip address")
+		return "", err
+	}
+
+	ips := strings.Split(string(out), " ")
+	for _, ip := range ips {
+		if !strings.HasPrefix(ip, "127.") {
+			c.sut.IP = strings.Trim(ip, "\n")
+			return nil
+		}
+	}
+	return "", errors.New("Cannot retrieve host ip address")
+}
+
+func (c *CAT) AddAgent(test string, name string, nodes []*ControlNode) (*Agent, err) {
+	agent, err := agent.New(test, name, c.SUT, nodes)
+	if err != nil {
+		return nil, fmt.Errorf("failed create agent: %v", err)
+	}
+	c.Agents = append(self.Agents, agent)
+	return agent, nil
+}
+
+func (c *CAT) AddControlNode(test string, name string, port int) *ControlNode {
+	cn := controlnode.New(test, name, self)
+	con.Verbose = self.Verbose
+	con.HttpPort = port
+	con.Pid = con.Start()
+	con.ReadPortNumbers()
+	self.ControlNodes = append(self.ControlNodes, con)
+	return con
+}
