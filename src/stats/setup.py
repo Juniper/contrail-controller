e3654deb233b41d1eb2262d405c462b28eaea6c8
Dmitry_Eremeev <deremeev@juniper.net>
2019-08-27 18:01:41 +0300

    TF usage reporting client.
    Link to server source code - https://github.com/progmaticlab/tf_stats_server
    Effort to massively offer Tungsten Fabric as an open-source SDN standard is quite new and require measuring the progress of “conquering” the market - growth rate, community “health”.
    The service collects statistics information and sends it to statistics server.
    By default the service doesn't collect and send statistics.
    Sending settings are checked every hour.
    To enable(edit) sending:
    - go to contrail web UI (https://ip_address:8143) --> Configure tab --> Tags --> Global Tags
    ( https://ip_address:8143/#p=config_tags_globaltags )
    - add tag with type = Label
    valid values are:
    1. stats_monthly
    2. stats_weekly
    3. stats_daily
    In order to disable sending just remove tags.
    In case there are few tags (misconfiguration) the service picks a tag with the least frequency
    "stats_monthly" is the least frequency tag.
    
    closes-jira-bug: CEM-4219
    Change-Id: I10f6341a468e5d04e1208638a172eff16b59d214

---
+++ src/stats/setup.py
@@ -0,0 +1,30 @@
+import re
+from setuptools import setup, find_packages
+
+
+def requirements(filename):
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    c = re.compile(r'\s*#.*')
+    return filter(bool, map(lambda y: c.sub('', y).strip(), lines))
+
+
+setup(
+    name="contrail_stats_client",
+    version="0.1dev",
+    description="contrail statistics package.",
+    packages=find_packages(),
+    zip_safe=False,
+    author="OpenContrail",
+    author_email="dev@lists.opencontrail.org",
+    license="Apache Software License",
+    url="http://www.opencontrail.org/",
+    install_requires=requirements('requirements.txt'),
+    tests_require=requirements('test-requirements.txt'),
+    test_suite='stats.tests',
+    entry_points={
+      "console_scripts": [
+        "contrail-stats-client = contrail_stats_client.main:main"
+        ]
+    }
+)
