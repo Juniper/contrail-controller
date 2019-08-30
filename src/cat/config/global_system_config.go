cf7358a331536fe82c5d10adb47cfc4e09f463f1
Ananth Suryanarayana <anantha@juniper.net>
2019-08-30 15:46:43 -0700

    Rewrite CAT Tests framework in GoLang
    Add basic test to verify connectivity among multiple mock control-nodes and
    mock agents. Also inject basic configuration with following objects
    
    o virtual-network
    o virtual-machine
    o virtual-machine-interface
    etc.
    
    XMPP and BGP connections among control-nodes and agents come up to ESTABLISHED
    state before and after restart of each mock control-node.
    
    Partial-Jira-Bug: CEM-7956
    Change-Id: I3c6a9dcbddf2652aed719af6994ef76883d68a7d

---
+++ src/cat/config/global_system_config.go
@@ -0,0 +1,42 @@
+/*
+ * copyright (c) 2019 juniper networks, inc. all rights reserved.
+ */
+
+package config
+
+
+import (
+    "encoding/json"
+)
+
+// GlobalSystemsConfig represents global-systems-config configuration construct.
+type GlobalSystemsConfig struct {
+    *ContrailConfig
+    AutonomousSystem string `json:"prop:autonomous_system"`
+}
+
+func NewGlobalSystemsConfig(fqNameTable *FQNameTableType, uuidTable *UUIDTableType, as string) (*GlobalSystemsConfig, error) {
+    co, err := createContrailConfig(fqNameTable, "global_system_config", "default-global-system-config", "", []string{"default-global-system-config"})
+    if err != nil {
+        return nil, err
+    }
+    g := &GlobalSystemsConfig{
+        ContrailConfig: co,
+        AutonomousSystem: as,
+    }
+
+    err = g.UpdateDB(uuidTable)
+    if err != nil {
+        return nil, err
+    }
+    return g, nil
+}
+
+func (g *GlobalSystemsConfig) UpdateDB(uuidTable *UUIDTableType) error {
+    b, err := json.Marshal(g)
+    if err != nil {
+        return err
+    }
+    (*uuidTable)[g.UUID], err = g.Map(b)
+    return err
+}
