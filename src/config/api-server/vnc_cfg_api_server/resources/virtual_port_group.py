8f1ae29a4bb5d3d8af2fdb860ee9ec3876dc6484
atandon <atandon@juniper.net>
2019-09-17 22:31:17 -0400

    [DM][API] - Handled the case for 'access' interfaces
    - Mark interfacetype as access during VPG creation/update workflow.
    - Delete 'access' interface type during VPG delete workflow
    - Handle the case to enable sflow on access interfaces
    - Fixed pep8 issue
    - Added test cases
    
    Change-Id: I43d14d0e7f1fdb3124d29773be95fed0e95ad763
    target-release: R1910
    closes-jira-bug: CEM-8388
    closes-jira-bug: CEM-8389

--- src/config/api-server/vnc_cfg_api_server/resources/virtual_port_group.py
+++ src/config/api-server/vnc_cfg_api_server/resources/virtual_port_group.py
@@ -2,6 +2,9 @@
 # Copyright (c) 2018 Juniper Networks, Inc. All rights reserved.
 #
 
+from cfgm_common.exceptions import HttpError
+from cfgm_common.exceptions import NoIdError
+from pysandesh.gen_py.sandesh.ttypes import SandeshLevel
 from vnc_api.gen.resource_common import VirtualPortGroup
 
 from vnc_cfg_api_server.resources._resource_base import ResourceMixin
@@ -10,9 +13,49 @@ from vnc_cfg_api_server.resources._resource_base import ResourceMixin
 class VirtualPortGroupServer(ResourceMixin, VirtualPortGroup):
 
     @classmethod
+    def update_physical_intf_type(cls, obj_dict, op):
+        api_server = cls.server
+        db_conn = cls.db_conn
+        for phys_intf_ref in obj_dict.get('physical_interface_refs') or []:
+            if phys_intf_ref.get('uuid') is None:
+                try:
+                    pi_uuid = db_conn.fq_name_to_uuid(
+                        'physical_interface', phys_intf_ref.get('to'))
+                except NoIdError as e:
+                    return False, (400, str(e))
+            else:
+                pi_uuid = phys_intf_ref['uuid']
+
+            if op == 'ADD':
+                try:
+                    api_server.internal_request_update(
+                        'physical_interface',
+                        pi_uuid,
+                        {'physical_interface_type': 'access'},
+                    )
+                except HttpError as e:
+                    db_conn.config_log("PI (%s) add update failed (%s)" %
+                                       (pi_uuid, str(e)),
+                                       level=SandeshLevel.SYS_WARN)
+            elif op == 'DELETE':
+                try:
+                    api_server.internal_request_update(
+                        'physical_interface',
+                        pi_uuid,
+                        {'physical_interface_type': None},
+                    )
+                except HttpError as e:
+                    db_conn.config_log("PI (%s) delete update failed (%s)" %
+                                       (pi_uuid, str(e)),
+                                       level=SandeshLevel.SYS_WARN)
+
+        return True, ''
+    # end update_physical_intf_type
+
+    @classmethod
     def pre_dbe_create(cls, tenant_name, obj_dict, db_conn):
         if ('vpg-internal' in obj_dict['fq_name'][2] and
-           obj_dict.get('virtual_port_group_user_created', True)):
+                obj_dict.get('virtual_port_group_user_created', True)):
             msg = "Virtual port group(%s) with name vpg-internal as prefix "\
                   "can only be created internally"\
                   % (obj_dict['uuid'])
@@ -36,7 +79,16 @@ class VirtualPortGroupServer(ResourceMixin, VirtualPortGroup):
                             (back_ref['uuid'], ':'.join(back_ref['to'])))
                 return (False, (409, msg))
 
+        ok, result = cls.update_physical_intf_type(obj_dict, 'ADD')
+        if not ok:
+            return ok, result
+
         return True, ''
+
+    @classmethod
+    def pre_dbe_update(cls, id, fq_name, obj_dict, db_conn, **kwargs):
+        return cls.update_physical_intf_type(obj_dict, 'ADD')
+    # end pre_dbe_update
 
     @classmethod
     def pre_dbe_delete(cls, id, obj_dict, db_conn):
@@ -47,6 +99,10 @@ class VirtualPortGroupServer(ResourceMixin, VirtualPortGroup):
                   "to VMI and BMS instance association still exists."\
                   % (obj_dict['uuid'])
             return (False, (400, msg), None)
+
+        ok, result = cls.update_physical_intf_type(obj_dict, 'DELETE')
+        if not ok:
+            return (False, result, None)
 
         return True, '', None
 
