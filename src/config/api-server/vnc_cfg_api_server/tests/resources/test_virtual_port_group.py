d8d09c25c1245a579f9f7766c6e438537b74414b
atandon <atandon@juniper.net>
2019-09-17 22:31:17 -0400

    [DM][API] - Handled the case for 'access' interfaces
    - Mark interfacetype as access during VPG creation/update workflow.
    - Delete 'access' interface type during VPG delete workflow
    - Handle the case to enable sflow on access interfaces
    - Fixed pep8 issue
    
    Change-Id: I43d14d0e7f1fdb3124d29773be95fed0e95ad763
    target-release: R1910
    closes-jira-bug: CEM-8388
    closes-jira-bug: CEM-8389

--- src/config/api-server/vnc_cfg_api_server/tests/resources/test_virtual_port_group.py
+++ src/config/api-server/vnc_cfg_api_server/tests/resources/test_virtual_port_group.py
@@ -800,6 +800,13 @@ class TestVirtualPortGroup(TestVirtualPortGroupBase):
         vmi_obj.set_virtual_machine_interface_properties(
             VirtualMachineInterfacePropertiesType(sub_interface_vlan_tag=42))
         vmi_uuid_1 = self.api.virtual_machine_interface_create(vmi_obj)
+        # Read physical interface type, it should be set to access
+        pi_obj_1 = self._vnc_lib.physical_interface_read(id=pi_uuid_1)
+        pi_obj_2 = self._vnc_lib.physical_interface_read(id=pi_uuid_2)
+        intf_type_pi_1 = pi_obj_1.get_physical_interface_type()
+        self.assertEqual(intf_type_pi_1, 'access')
+        intf_type_pi_2 = pi_obj_2.get_physical_interface_type()
+        self.assertEqual(intf_type_pi_2, 'access')
         vpg_obj.add_virtual_machine_interface(vmi_obj)
         self.api.virtual_port_group_update(vpg_obj)
 
