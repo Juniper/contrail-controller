2a526a87d6051fd1e2f7e9fd26bf251af94e8514
Sridevi <sjeevaraj@juniper.net>
2019-09-25 20:38:21 +0000

    [DM] Support for standalone AR
    We had a dependency of extending LRs on Spines to assocaite VNs,
    which caused Gateway role dependency for Assited-Replicator.
    Fix is to populate all vnis (in the fabric) if AR alone is configured on a device (leaf/spine)
    
    Change-Id: Iddd1f06d1dc9df5d5e9bd909c99c878ccdb2c28b
    Target-Release: R1910
    Closes-Jira-Bug: CEM-8590

--- src/config/device-manager/device_manager/plugins/feature/assisted_replicator_feature.py
+++ src/config/device-manager/device_manager/plugins/feature/assisted_replicator_feature.py
@@ -5,6 +5,8 @@
 """Assisted Replicator Feature Implementation."""
 
 from abstract_device_api.abstract_device_xsd import *
+import db
+from dm_utils import DMUtils
 from feature_base import FeatureBase
 
 
@@ -20,6 +22,34 @@ class AssistedReplicatorFeature(FeatureBase):
             logger, physical_router, configs)
     # end __init__
 
+    def _build_vlan_config(self,feature_config, vns):
+        for vn in vns:
+            vn_obj = db.VirtualNetworkDM.get(vn)
+            ri_obj = self._get_primary_ri(vn_obj)
+            if ri_obj is None:
+                continue
+            #build route targets
+            ri_name = DMUtils.make_vrf_name(vn_obj.fq_name[-1],
+                                            vn_obj.vn_network_id, 'l2')
+            export_targets, import_targets = self._get_export_import_targets(
+                vn_obj, ri_obj)
+            network_id = vn_obj.vn_network_id
+            vxlan_id = vn_obj.get_vxlan_vni()
+
+            ri = RoutingInstance(
+                name=ri_name, virtual_network_mode='l2',
+                export_targets=export_targets, import_targets=import_targets,
+                virtual_network_id=str(network_id), vxlan_id=str(vxlan_id))
+            feature_config.add_routing_instances(ri)
+
+            #build vlan config
+            vlan = Vlan(name=DMUtils.make_bridge_name(vn_obj.get_vxlan_vni()),
+                        vxlan_id=vn_obj.get_vxlan_vni())
+            vlan.set_comment(DMUtils.vn_bd_comment(vn_obj, 'VXLAN'))
+            desc = "Virtual Network - %s" % vn_obj.name
+            vlan.set_description(desc)
+            feature_config.add_vlans(vlan)
+
     def _build_ar_config(self, feature_config):
         rep_act_delay = None
         for c in self._configs:
@@ -30,13 +60,22 @@ class AssistedReplicatorFeature(FeatureBase):
         ar = AssistedReplicator(
             ar_loopback_ip=self._physical_router.replicator_ip,
             replicator_activation_delay=rep_act_delay)
-
-        feature_config.set_assisted_replicator(ar)
+        feature_config.add_assisted_replicator(ar)
+    # end _build_ar_config
 
     def feature_config(self, **kwargs):
         feature_config = Feature(name=self.feature_name())
+
+        if not self._physical_router.is_gateway():
+            vns = self._get_all_vns_in_fabric(
+                self._physical_router.fabric_obj)
+
+            self._build_vlan_config(feature_config, vns)
+
         self._build_ar_config(feature_config)
+
+
         return feature_config
-    # end push_conf
+    # end feature_config
 
 # end AssistedReplicatorFeature
