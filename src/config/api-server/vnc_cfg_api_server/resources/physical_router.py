0d6119a9f70d404ff462bce391d71531a96df00b
Joe Williams <josephw@juniper.net>
2019-09-26 17:30:23 +0000

    Move encrypt_password from cfgm_common to api_server
    Change-Id: Id94a97660201a5bf9b58944910f2889f38e66349
    Closes-jira-bug: CEM-9203
    Target-release: R1910

--- src/config/api-server/vnc_cfg_api_server/resources/physical_router.py
+++ src/config/api-server/vnc_cfg_api_server/resources/physical_router.py
@@ -2,10 +2,10 @@
 # Copyright (c) 2018 Juniper Networks, Inc. All rights reserved.
 #
 
-from cfgm_common import utils
 from vnc_api.gen.resource_common import PhysicalRouter
 
 from vnc_cfg_api_server.resources._resource_base import ResourceMixin
+from vnc_cfg_api_server import utils
 
 
 class PhysicalRouterServer(ResourceMixin, PhysicalRouter):
