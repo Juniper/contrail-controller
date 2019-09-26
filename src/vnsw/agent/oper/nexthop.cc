c4ca88a845edd6117ad391870e5ecf5488c13bee
Kumar Harsh <hkumar@juniper.net>
2019-09-27 00:55:07 +0530

    Read resource max limits from vrouter.
    Enforce resource limits on next hop and MPLS  tables.
    Add a map containing resource name and usage status,
    evaluate map on add and delete of nexthop and labels.
    
    Add vr_limits_exceeded attribute to VrouterAgent Uve
    for sending list of vrouter objects for which resource usage
    is exceeded, also add flags res_limit and res_table_limit.
    
    flags res_limit and res_table_limit are sent to analytics,
    as part of VrouterAgent uve when set alarm is raised,
    updated contrail_alarms for raising alarms.
    
    Add UT.
    
    Change-Id: Ibfad305fecc5da16111d1575f310f0a367183f98
    closes-jira-bug: CEM-8550
    closes-jira-bug: CEM-8989

--- src/vnsw/agent/oper/nexthop.cc
+++ src/vnsw/agent/oper/nexthop.cc
@@ -306,6 +306,7 @@ DBEntry *NextHopTable::Add(const DBRequest *req) {
         delete nh;
         return NULL;
     }
+    CheckVrNexthopLimit();
     nh->set_id(index_table_.Insert(nh));
     nh->Add(agent(), req);
     nh->SendObjectLog(this, AgentLogEvent::ADD);
@@ -339,6 +340,7 @@ bool NextHopTable::Delete(DBEntry *entry, const DBRequest *req) {
     NextHop *nh = static_cast<NextHop *>(entry);
     nh->Delete(req);
     nh->SendObjectLog(this, AgentLogEvent::DEL);
+    CheckVrNexthopLimit();
     return true;
 }
 
@@ -382,6 +384,33 @@ void NextHopTable::OnZeroRefcount(AgentDBEntry *e) {
     req.key = key;
     req.data.reset(NULL);
     Process(req);
+}
+
+void NextHopTable::CheckVrNexthopLimit() {
+    VrLimitExceeded &vr_limits = agent()->get_vr_limits_exceeded_map();
+    VrLimitExceeded::iterator vr_limit_itr = vr_limits.find("vr_nexthops");
+    if (vr_limit_itr->second == "Normal") {
+        if (index_table_.InUseIndexCount() >= ((agent()->vr_limit_high_watermark() *
+            agent()->vrouter_max_nexthops())/100) ) {
+            vr_limit_itr->second.assign(std::string("Exceeded"));
+            LOG(ERROR, "Vrouter Nexthop Index Exceeded.");
+        }
+    } else if ( vr_limit_itr->second == "Exceeded") {
+        if (index_table_.InUseIndexCount() >= agent()->vrouter_max_nexthops()) {
+            vr_limit_itr->second.assign(std::string("TableLimit"));
+            LOG(ERROR, "Vrouter Nexthop Table Limit Reached. Skip NH Add.");
+        } else if ( index_table_.InUseIndexCount() < ((agent()->vr_limit_low_watermark() *
+            agent()->vrouter_max_nexthops())/100) ) {
+            vr_limit_itr->second.assign(std::string("Normal"));
+        }
+    } else if ( vr_limit_itr->second == "TableLimit" ) {
+        if (index_table_.InUseIndexCount() <
+            ((agent()->vrouter_max_nexthops()*95)/100) ) {
+            vr_limit_itr->second.assign(std::string("Exceeded"));
+            LOG(ERROR, "Vrouter Nexthop Index Exceeded.");
+        }
+    }
+    agent()->set_vr_limits_exceeded_map(vr_limits);
 }
 
 /////////////////////////////////////////////////////////////////////////////
