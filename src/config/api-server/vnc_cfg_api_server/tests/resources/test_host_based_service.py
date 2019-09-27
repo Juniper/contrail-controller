9bb8f8f0a89fb16ecdc95bc5509c41237b5e5349
Sajeesh Mathew <sajeeshm@juniper.net>
2019-09-27 14:05:48 -0700

    [Config] host based service  development
    Added following changes hbs code
    -Creation of default left and right VN's for HBS object.
    -Added checks not to allow update default left and right VN's .
    -API to return HBS configuration template.
    -Updated unit test cases.
    
    Closes-Jira-Bug: CEM-9347
    Change-Id: I77fd717ff89de0d64885fa1a70f6d33c3a72a4ba

--- src/config/api-server/vnc_cfg_api_server/tests/resources/test_host_based_service.py
+++ src/config/api-server/vnc_cfg_api_server/tests/resources/test_host_based_service.py
@@ -1,6 +1,7 @@
 #
 # Copyright (c) 2019 Juniper Networks, Inc. All rights reserved.
 #
+import json
 import logging
 
 from vnc_api.exceptions import BadRequest
@@ -8,6 +9,8 @@ from vnc_api.exceptions import OverQuota
 from vnc_api.gen.resource_client import HostBasedService
 from vnc_api.gen.resource_client import Project
 from vnc_api.gen.resource_client import VirtualNetwork
+from vnc_api.gen.resource_xsd import KeyValuePair
+from vnc_api.gen.resource_xsd import KeyValuePairs
 from vnc_api.gen.resource_xsd import QuotaType
 from vnc_api.gen.resource_xsd import ServiceVirtualNetworkType
 
@@ -57,7 +60,7 @@ class TestHostBasedService(test_case.ApiServerTestCase):
         hbs.set_host_based_service_type('foo')
         self.assertRaises(BadRequest, self.api.host_based_service_update, hbs)
 
-    def test_hbs_unique_vn_ref_per_type(self):
+    def test_hbs_mgmnt_vn_ref_muliple_add(self):
         hbs = HostBasedService('hbs-%s' % self.id(), parent_obj=self.project)
         self.api.host_based_service_create(hbs)
         vn1 = VirtualNetwork('vn1-%s' % self.id(), parent_obj=self.project)
@@ -65,7 +68,7 @@ class TestHostBasedService(test_case.ApiServerTestCase):
         vn2 = VirtualNetwork('vn2-%s' % self.id(), parent_obj=self.project)
         self.api.virtual_network_create(vn2)
 
-        for vn_type in ['management', 'left', 'right', 'other']:
+        for vn_type in ['management']:
             self.api.ref_update(
                 hbs.resource_type,
                 hbs.uuid,
@@ -80,15 +83,78 @@ class TestHostBasedService(test_case.ApiServerTestCase):
                 self.api.ref_update,
                 hbs.resource_type,
                 hbs.uuid,
-                vn1.resource_type,
+                vn2.resource_type,
                 vn2.uuid,
                 None,
                 'ADD',
                 ServiceVirtualNetworkType(vn_type),
             )
-            hbs = self.api.host_based_service_read(id=hbs.uuid)
-            hbs.set_virtual_network_list([])
-            self.api.host_based_service_update(hbs)
+
+    def test_hbs_mgmnt_vn_ref_add_del_add(self):
+        hbs = HostBasedService('hbs-%s' % self.id(), parent_obj=self.project)
+        self.api.host_based_service_create(hbs)
+        vn1 = VirtualNetwork('vn1-%s' % self.id(), parent_obj=self.project)
+        self.api.virtual_network_create(vn1)
+        vn2 = VirtualNetwork('vn2-%s' % self.id(), parent_obj=self.project)
+        self.api.virtual_network_create(vn2)
+
+        for vn_type in ['management']:
+            self.api.ref_update(
+                hbs.resource_type,
+                hbs.uuid,
+                vn1.resource_type,
+                vn1.uuid,
+                None,
+                'ADD',
+                ServiceVirtualNetworkType(vn_type),
+            )
+            self.api.ref_update(
+                hbs.resource_type,
+                hbs.uuid,
+                vn1.resource_type,
+                vn1.uuid,
+                None,
+                'DELETE',
+                ServiceVirtualNetworkType(vn_type),
+            )
+            self.api.ref_update(
+                hbs.resource_type,
+                hbs.uuid,
+                vn2.resource_type,
+                vn2.uuid,
+                None,
+                'ADD',
+                ServiceVirtualNetworkType(vn_type),
+            )
+
+    def test_hbs_default_vn_ref_add(self):
+        hbs = HostBasedService('hbs-%s' % self.id(), parent_obj=self.project)
+        self.api.host_based_service_create(hbs)
+        vn1 = VirtualNetwork('vn1-%s' % self.id(), parent_obj=self.project)
+        self.api.virtual_network_create(vn1)
+
+        for vn_type in ['left', 'right']:
+            self.assertRaises(
+                BadRequest,
+                self.api.ref_update,
+                hbs.resource_type,
+                hbs.uuid,
+                vn1.resource_type,
+                vn1.uuid,
+                None,
+                'ADD',
+                ServiceVirtualNetworkType(vn_type),
+            )
+
+    def test_hbs_default_vn_ref_del(self):
+        hbs = HostBasedService('hbs-%s' % self.id(), parent_obj=self.project)
+        self.api.host_based_service_create(hbs)
+        hbs = self.api.host_based_service_read(id=hbs.uuid)
+        hbs.set_virtual_network_list([])
+        self.assertRaises(
+            BadRequest,
+            self.api.host_based_service_update,
+            hbs)
 
     def test_host_based_resource_limited_to_one_per_project(self):
         project2 = Project('project2-%s' % self.id())
@@ -115,3 +181,97 @@ class TestHostBasedService(test_case.ApiServerTestCase):
         project2.set_quota(QuotaType(host_based_service=None))
         self._vnc_lib.project_update(project2)
         self.assertRaises(OverQuota, self.api.host_based_service_create, hbs2)
+
+    def get_hbs_template(self):
+        project2 = Project('project2-%s' % self.id())
+        project2.set_quota(QuotaType(host_based_service=1))
+
+        kvp_array = []
+        kvp = KeyValuePair("namespace", "k8test")
+        kvp_array.append(kvp)
+        kvp = KeyValuePair("cluster", "c1")
+        kvp_array.append(kvp)
+        kvps = KeyValuePairs()
+        kvps.set_key_value_pair(kvp_array)
+
+        project2.set_annotations(kvps)
+
+        self.api.project_create(project2)
+        hbs = HostBasedService('hbs-%s' % self.id(), parent_obj=project2)
+        self.api.host_based_service_create(hbs)
+
+        (code, msg) = self._http_post('/hbs-get',
+                                      json.dumps({'hbs_fq_name': hbs.fq_name}))
+        self.assertEquals(code, 200)
+        return json.loads(msg)
+
+    def test_get_hbs_template_with_uuid(self):
+        project2 = Project('project2-%s' % self.id())
+        project2.set_quota(QuotaType(host_based_service=1))
+
+        kvp_array = []
+        kvp = KeyValuePair("namespace", "k8test")
+        kvp_array.append(kvp)
+        kvp = KeyValuePair("cluster", "c1")
+        kvp_array.append(kvp)
+        kvps = KeyValuePairs()
+        kvps.set_key_value_pair(kvp_array)
+
+        project2.set_annotations(kvps)
+
+        self.api.project_create(project2)
+        hbs = HostBasedService('hbs-%s' % self.id(), parent_obj=project2)
+        self.api.host_based_service_create(hbs)
+
+        (code, msg) = self._http_post('/hbs-get',
+                                      json.dumps({'hbs_uuid': hbs.uuid}))
+        self.assertEquals(code, 200)
+
+        return json.loads(msg)
+
+    def test_get_hbs_template_no_json(self):
+        project2 = Project('project2-%s' % self.id())
+        project2.set_quota(QuotaType(host_based_service=1))
+
+        kvp_array = []
+        kvp = KeyValuePair("namespace", "k8test")
+        kvp_array.append(kvp)
+        kvp = KeyValuePair("cluster", "c1")
+        kvp_array.append(kvp)
+        kvps = KeyValuePairs()
+        kvps.set_key_value_pair(kvp_array)
+
+        project2.set_annotations(kvps)
+
+        self.api.project_create(project2)
+        hbs = HostBasedService('hbs-%s' % self.id(), parent_obj=project2)
+        self.api.host_based_service_create(hbs)
+
+        (code, msg) = self._http_post('/hbs-get', None)
+        self.assertEquals(code, 400)
+
+    def test_host_based_resource_http_post(self):
+        self.get_hbs_template()
+
+    def test_hbs_template_types(self):
+        hbs = self.get_hbs_template()
+        left = hbs['hbs'][0]
+        right = hbs['hbs'][1]
+        ds = hbs['hbs'][2]
+
+        self.assertEqual(left['kind'], 'NetworkAttachmentDefinition')
+        self.assertEqual(right['kind'], 'NetworkAttachmentDefinition')
+        self.assertEqual(ds['kind'], 'DaemonSet')
+
+    def test_hbs_create_with_vn_ref(self):
+        project2 = Project('project2-%s' % self.id())
+        self.api.project_create(project2)
+        vn1 = VirtualNetwork('vn1-%s' % self.id(), parent_obj=project2)
+        self.api.virtual_network_create(vn1)
+        hbs1 = HostBasedService('hbs1-%s' % self.id(), parent_obj=project2)
+        hbs1.add_virtual_network(vn1, ServiceVirtualNetworkType('left'))
+        self.assertRaises(
+            BadRequest,
+            self.api.host_based_service_create,
+            hbs1
+        )
