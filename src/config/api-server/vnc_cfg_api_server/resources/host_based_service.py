2c875f9d609a719a3e14c4550786ef9707973883
Sajeesh Mathew <sajeeshm@juniper.net>
2019-09-27 14:05:48 -0700

    [WIP][Config] host based service  development Added following changes hbs code -Creation of default left and right VN's for HBS object. -Added checks not to allow update default left and right VN's. -API to return HBS configuration template. -Updated unit test cases.
    Closes-Jira-Bug: CEM-9347
    Change-Id: I77fd717ff89de0d64885fa1a70f6d33c3a72a4ba

--- src/config/api-server/vnc_cfg_api_server/resources/host_based_service.py
+++ src/config/api-server/vnc_cfg_api_server/resources/host_based_service.py
@@ -2,12 +2,34 @@
 # Copyright (c) 2019 Juniper Networks, Inc. All rights reserved.
 #
 
+from cfgm_common import _obj_serializer_all
+from cfgm_common import jsonutils as json
+from cfgm_common.exceptions import HttpError
 from vnc_api.gen.resource_common import HostBasedService
-
+from vnc_api.gen.resource_xsd import VnSubnetsType
+from vnc_api.gen.resource_xsd import IpamSubnetType
+from vnc_api.gen.resource_xsd import SubnetType
+from vnc_api.gen.resource_xsd import ServiceVirtualNetworkType
 from vnc_cfg_api_server.resources._resource_base import ResourceMixin
+from vnc_cfg_api_server.context import is_internal_request
 
 
 class HostBasedServiceServer(ResourceMixin, HostBasedService):
+    LEFT = 'left'
+    RIGHT = 'right'
+    hbf = {
+        LEFT: {
+            'IP': '1.1.1.0',
+            'PREFIX_LEN': '24',
+            'NAME': 'hbf-left'
+        },
+        RIGHT: {
+            'IP': '2.2.2.0',
+            'PREFIX_LEN': '24',
+            'NAME': 'hbf-right'
+        }
+    }
+
     @staticmethod
     def _check_only_one_vn_per_type(req_dict, db_dict=None):
         vn_type_map = {}
@@ -33,6 +55,20 @@ class HostBasedServiceServer(ResourceMixin, HostBasedService):
                        "than one virtual network at a time" % vn_type)
                 return False, (400, msg)
 
+        return True, ''
+
+    @staticmethod
+    def _check_for_allowed_configure_vn_types(fq_name, req_dict):
+        # interal request, return True
+        if is_internal_request():
+            return True, ''
+        vn_types = ['management']
+        for ref in req_dict.get('virtual_network_refs', []):
+            if ref['attr'].get('virtual_network_type') not in vn_types:
+                msg = ("Host based service (%s) allows only (%s) "
+                       "network types" % (':'.join(fq_name),
+                                          ','.join(map(str, vn_types))))
+                return False, (400, msg)
         return True, ''
 
     @staticmethod
@@ -66,8 +102,8 @@ class HostBasedServiceServer(ResourceMixin, HostBasedService):
 
         ok, result = cls.server.get_resource_class(
             'host_based_service').locate(
-                uuid=hbss[0]['uuid'], create_it=False,
-                fields=['virtual_network_refs'])
+            uuid=hbss[0]['uuid'], create_it=False,
+            fields=['virtual_network_refs'])
         if not ok:
             return False, result
         hbs = result
@@ -84,8 +120,82 @@ class HostBasedServiceServer(ResourceMixin, HostBasedService):
         return True, (True, '')
 
     @classmethod
+    def _create_vn(cls, vn_fq_name, svn_type, parent_uuid, db_conn):
+        api_server = db_conn.get_api_server()
+        # Create a VN
+        ok, result = cls.server.get_resource_class(
+            'virtual_network').locate(fq_name=vn_fq_name)
+        if not ok:
+            return False, result
+        vn_obj = result
+        vn_uuid = vn_obj['uuid']
+
+        attr = ServiceVirtualNetworkType(svn_type)
+        attr_as_dict = attr.__dict__
+        try:
+            api_server.internal_request_ref_update(
+                'host-based-service', parent_uuid, 'ADD',
+                'virtual-network', vn_uuid,
+                ref_fq_name=vn_fq_name,
+                attr=attr_as_dict)
+        except HttpError as e:
+            return False, (e.status_code, e.content)
+
+        # Create a subnet and link it to default ipam
+        # ref update to VN
+        ipam_obj_type = 'network_ipam'
+        ipam_fq_name = ['default-domain',
+                        'default-project', 'default-network-ipam']
+        ipam_uuid = db_conn.fq_name_to_uuid(ipam_obj_type, ipam_fq_name)
+        subnet = SubnetType(cls.hbf[svn_type]['IP'],
+                            cls.hbf[svn_type]['PREFIX_LEN'])
+        attr = VnSubnetsType([IpamSubnetType(subnet=subnet)])
+        attr_as_dict = json.loads(json.dumps(
+            attr, default=_obj_serializer_all))
+        try:
+            api_server.internal_request_ref_update(
+                'virtual-network', vn_uuid, 'ADD',
+                'network-ipam', ipam_uuid,
+                attr=attr_as_dict)
+        except HttpError as e:
+            return False, (e.status_code, e.content)
+        return ok, vn_obj
+
+    @classmethod
+    def post_dbe_create(cls, tenant_name, obj_dict, db_conn):
+        ok, result = cls.server.get_resource_class('project').locate(
+            uuid=obj_dict['parent_uuid'],
+            create_it=False,
+            fields=['annotations'])
+        if not ok:
+            return False, result
+        left = cls.hbf[cls.LEFT]['NAME']
+
+        # Create Left VN
+        proj_name = obj_dict['fq_name'][:-1]
+        vn_left_fq_name = proj_name + [left]
+        ok, resp = cls._create_vn(vn_left_fq_name, cls.LEFT,
+                                  obj_dict['uuid'], db_conn)
+        if not ok:
+            return ok, resp
+
+        # Create Right VN
+        right = cls.hbf[cls.RIGHT]['NAME']
+        vn_right_fq_name = proj_name + [right]
+        ok, resp = cls._create_vn(vn_right_fq_name, cls.RIGHT,
+                                  obj_dict['uuid'], db_conn)
+        if not ok:
+            return ok, resp
+
+        return True, ''
+
+    @classmethod
     def pre_dbe_create(cls, tenant_name, obj_dict, db_conn):
         obj_dict.setdefault('host_based_service_type', 'firewall')
+        ok, result = cls._check_for_allowed_configure_vn_types(
+            tenant_name, obj_dict)
+        if not ok:
+            return False, result
         return cls._check_only_one_vn_per_type(obj_dict)
 
     @classmethod
@@ -101,4 +211,41 @@ class HostBasedServiceServer(ResourceMixin, HostBasedService):
         if not ok:
             return False, result
 
+        ok, result = cls._check_for_allowed_configure_vn_types(
+            fq_name, obj_dict)
+        if not ok:
+            return False, result
+
         return cls._check_only_one_vn_per_type(obj_dict, db_obj_dict)
+
+    @classmethod
+    def pre_dbe_delete(cls, id, obj_dict, db_conn):
+        ok, result = cls.locate(uuid=id, create_it=False,
+                                fields=['virtual_network_refs',
+                                        'host_based_service_type'])
+        if not ok:
+            return False, result
+        db_obj_dict = result
+        for ref in db_obj_dict.get('virtual_network_refs', []):
+            ok, result = cls.server.get_resource_class(
+                'virtual_network').locate(
+                uuid=ref['uuid'],
+                fields=['virtual_machine_interface_back_refs', 'fq_name'],
+                create_it=False)
+            if ok:
+                if result.get('virtual_machine_interface_back_refs', []):
+                    msg = ("HBS object virtual network (%s) got interfaces"
+                           " cannot be removed" % ':'.join(result['fq_name']))
+                    return False, (400, msg)
+        return True, "", None
+
+    @classmethod
+    def post_dbe_delete(cls, id, obj_dict, db_conn):
+        api_server = db_conn.get_api_server()
+        vn_types = ['left', 'right']
+        for ref in obj_dict.get('virtual_network_refs', []):
+            if ref['attr'].get('virtual_network_type') in vn_types:
+                api_server.internal_request_delete('virtual-network',
+                                                   ref['uuid'])
+
+        return True, ''
