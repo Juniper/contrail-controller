c847ad3eaa0124cb417a846b00b9664c559c38e7
Sajeesh Mathew <sajeeshm@juniper.net>
2019-09-27 14:05:48 -0700

    [Config] host based service  development
    Added following changes hbs code
    -Creation of default left and right VN's for HBS object.
    -Added checks not to allow update default left and right VN's .
    -API to return HBS configuration template.
    -Updated unit test cases.
    
    Closes-Jira-Bug: CEM-9347
    Change-Id: I77fd717ff89de0d64885fa1a70f6d33c3a72a4ba

--- src/config/api-server/vnc_cfg_api_server/vnc_cfg_api_server.py
+++ src/config/api-server/vnc_cfg_api_server/vnc_cfg_api_server.py
@@ -174,6 +174,8 @@ _ACTION_RESOURCES = [
      'method': 'POST', 'method_name': 'amqp_publish_http_post'},
     {'uri': '/amqp-request', 'link_name': 'amqp-request',
      'method': 'POST', 'method_name': 'amqp_request_http_post'},
+    {'uri': '/hbs-get', 'link_name': 'hbs-get',
+     'method': 'POST', 'method_name': 'hbs_get'},
 ]
 
 _MANDATORY_PROPS = [
@@ -1253,11 +1255,11 @@ class VncApiServer(object):
                              resource_type, ref_uuids,fields)}
 
         for link_field in obj_links:
-            links = obj_dict[link_field]
+            links = obj_dict[link_field][:]
 
             # build new links in returned dict based on permissions on linked
             # object
-            for link in obj_dict[link_field]:
+            for link in links:
                 if (link['uuid'] not in ref_perms or
                         not self._permissions.check_perms_read(
                             get_request(),
@@ -3194,6 +3196,205 @@ class VncApiServer(object):
                                      include_shared, exclude_hrefs,
                                      pagination)
     # end list_bulk_collection_http_post
+
+
+
+    # hbs network template
+    def _get_hbs_network(self, vn_uuid, vn_k8s_str, namespace):
+        hbs_nw = {
+            'apiVersion': 'k8s.cni.cncf.io/v1',
+            'kind': 'NetworkAttachmentDefinition',
+            'metadata': {'annotations': {'opencontrail.org/network': ''},
+                         'name': 'hbf-left',
+                         'namespace': 'hp101'},
+            'spec': {'config': '{"cniVersion":"0.3.0", "type": "contrail-k8s-cni" }'}
+        }
+        vn_class = self.get_resource_class('virtual_network')
+        ok, result = vn_class.locate(uuid=vn_uuid,
+                                     fields=['network_ipam_refs'],
+                                     create_it=False)
+        if not ok:
+            raise cfgm_common.exceptions.HttpError(result[0], result[1])
+        network_ipam_refs = result.get('network_ipam_refs', [])
+        hbs_class = self.get_resource_class('host-based-service')
+        vn_fq_name = result['fq_name']
+        hbs_nw['metadata']['annotations']['opencontrail.org/network'] = hbs_class._get_network(vn_fq_name)
+        hbs_nw['metadata']['name'] = vn_k8s_str
+        hbs_nw['metadata']['namespace'] = namespace
+        return hbs_nw
+
+    # end _get_hbs_network
+
+    # Get hbs daemonset
+    def _get_hbs_ds(self, hbs_info):
+        hbs_ds_template = {
+            'apiVersion': 'apps/v1',
+            'kind': 'DaemonSet',
+            'metadata': {'labels': {'type': 'hbf'},
+                         'name': 'csrx',
+                         'namespace': '<namespace>'},
+            'spec': {'selector': {'matchLabels': {'name': 'hbf'}},
+                     'template':
+                         {'metadata': {'annotations': {'k8s.v1.cni.cncf.io/networks':
+                                                           '[{"name":"left"}, {"name":"right"}]'},
+                                       'labels': {'name': 'hbf'}},
+                          'spec': {'containers': [{'env': [{'name': 'CSRX_FORWARD_MODE',
+                                                            'value': 'wire'}],
+                                                   'image': 'hub.juniper.net/security/csrx:19.2R1.8',
+                                                   'imagePullPolicy': 'IfNotPresent',
+                                                   'name': 'csrx',
+                                                   'securityContext': {'privileged': True},
+                                                   'stdin': True,
+                                                   'tty': False}],
+                                   'imagePullSecrets': [{'name': 'psd'}],
+                                   'nodeSelector': {'type': 'hbf'},
+                                   'restartPolicy': 'Always'}}}
+        }
+        hbs_ds_template['metadata']['namespace'] = hbs_info['namespace']
+        ds_template = hbs_ds_template['spec']['template']
+        hbs_class = self.get_resource_class('host-based-service')
+        if hbs_info['vnmgmt']['fq_name']:
+            ds_template['metadata']['annotations']['opencontrail.org/networks'] = \
+                        hbs_class._get_network(hbs_info['vnmgmt']['fq_name'])
+        ds_template_spec = ds_template['spec']
+        if hbs_info['service']['image']: 
+            ds_template_spec['containers'][0]['image'] = hbs_info['service']['image']
+        if hbs_info['service']['imagePullSecrets']:
+            ds_template_spec['imagePullSecrets'][0]['name'] = \
+                 hbs_info['service']['imagePullSecrets']
+        return hbs_ds_template
+
+    # end _get_hbs_ds
+
+    # hbs object daemon set generation 
+    def _get_hbs_info(self, hbs_fq_name, hbs_uuid):
+        hbs_info = {
+            'cluster': None, 'namespace': None,
+            'vnleft': {'fq_name': None, 'uuid': None, 'k8s_str': None},
+            'vnright': {'fq_name': None, 'uuid': None, 'k8s_str': None},
+            'vnmgmt': {'fq_name': None, 'uuid': None, 'k8s_str': None},
+            'service': {'image': None, 'imagePullSecrets': None}
+        }
+        # get hbs object
+        hbs_class = self.get_resource_class('host_based_service')
+        ok, result = hbs_class.locate(fq_name=hbs_fq_name,
+                                      uuid=hbs_uuid,
+                                      create_it=False)
+        if not ok:
+            raise cfgm_common.exceptions.HttpError(result[0], result[1])
+        hbs_dict = result
+
+        # get project and check for k8s, if not return an error
+        project_uuid = hbs_dict['parent_uuid']
+        project_class = self.get_resource_class('project')
+        ok, result = project_class.locate(uuid=project_uuid,
+                                          fields=['annotations'],
+                                          create_it=False)
+        if not ok:
+            raise cfgm_common.exceptions.HttpError(result[0], result[1])
+        project_dict = result
+        project_fq_name = project_dict['fq_name']
+
+        # Get namespace information from project annotations
+        kvs = project_dict.get('annotations', {}).get('key_value_pair', [])
+        namespace = None
+        cluster = None
+        for kv in kvs:
+            if kv['key'] == 'namespace':
+                namespace = kv['value']
+            if kv['key'] == 'cluster':
+                cluster = kv['value']
+        if not namespace:
+            raise cfgm_common.exceptions.HttpError(
+                404, 'Project ' + pformat(project_fq_name) + \
+                     ' is not k8s project, namespace is not present')
+        if not cluster:
+            raise cfgm_common.exceptions.HttpError(
+                404, 'Project ' + pformat(project_fq_name) + \
+                     ' is not k8s project, clsuter is not present')
+        hbs_info['namespace'] = namespace
+        hbs_info['cluster'] = cluster
+
+        # Get vn refs, get both left and right VNs
+        vn_refs = hbs_dict.get('virtual_network_refs', [])
+        vn_left = vn_right = vn_mgmt = None
+        for vn in vn_refs:
+            attr = vn.get('attr', {})
+            l_type = attr.get('virtual_network_type', None)
+            if l_type == 'left':
+                hbs_info['vnleft']['fq_name'] = vn['to']
+                hbs_info['vnleft']['uuid'] = vn['uuid']
+                vn_left = vn['to'][-1:][0]
+                hbs_info['vnleft']['k8s_str'] = vn_left
+            if l_type == 'right':
+                hbs_info['vnright']['fq_name'] = vn['to']
+                hbs_info['vnright']['uuid'] = vn['uuid']
+                vn_right = vn['to'][-1:][0]
+                hbs_info['vnright']['k8s_str'] = vn_right
+            if l_type == 'management':
+                hbs_info['vnmgmt']['fq_name'] = vn['to']
+                hbs_info['vnmgmt']['uuid'] = vn['uuid']
+                vn_mgmt = vn['to'][-1:][0]
+                hbs_info['vnmgmt']['k8s_str'] = vn_mgmt
+                # both left and right vn should be present
+        if vn_left is None or vn_right is None:
+            raise cfgm_common.exceptions.HttpError(
+                404, 'Left and right HBF networks should be present')
+
+        # get the map and look for image name and secrect
+        kvs = hbs_dict.get('annotations', {}).get('key_value_pair', [])
+        for kv in kvs:
+            if kv['key'] == 'spec.template.spec.containers[].image' or \
+                    kv['key'] == 'image':
+                hbs_info['service']['image'] = kv['value']
+            if kv['key'] == 'spec.template.spec.imagePullSecrets[].name' or \
+                    kv['key'] == 'imagePullSecrets':
+                hbs_info['service']['imagePullSecrets'] = kv['value']
+        return hbs_info
+
+    # end _get_hbs_info
+
+    # Get hbs 
+    def hbs_get(self):
+        self._post_common(None, {})
+        # Get hbs fq_name from request
+        req_json = get_request().json
+        hbs_fq_name = req_json.get('hbs_fq_name', '')
+        hbs_uuid = req_json.get('hbs_uuid', '')
+
+        # hbs fq_name or uuid required
+        if not hbs_fq_name and not hbs_uuid:
+            raise cfgm_common.exceptions.HttpError(
+                400, 'HBS fqname or uuid (hbs_fqname/hbs_uuid) required for hbs daemonset get')
+
+        # get hbs uuid, if not given for rbac checks
+        if not hbs_uuid:
+            try:
+                hbs_uuid = self._db_conn.fq_name_to_uuid('host_based_service', hbs_fq_name)
+            except NoIdError:
+                raise cfgm_common.exceptions.HttpError(
+                    404, 'Name ' + pformat(hbs_fq_name) + ' not found')
+
+        # rbac check for hbs object operations
+        ok, result = self._permissions.check_perms_read(get_request(), hbs_uuid)
+        if not ok:
+            raise cfgm_common.exceptions.HttpError(result[0], result[1])
+
+        hbs_class = self.get_resource_class('host-based-service')
+        # get hbs info
+        hbs_info = self._get_hbs_info(hbs_fq_name, hbs_uuid)
+        namespace_template = hbs_class._get_hbs_namespace(hbs_info['namespace'])
+        ds_template = self._get_hbs_ds(hbs_info)
+        left_vn_template = self._get_hbs_network(hbs_info['vnleft']['uuid'],
+                                                 'left',
+                                                 hbs_info['namespace'])
+        right_vn_template = self._get_hbs_network(hbs_info['vnright']['uuid'],
+                                                  'right',
+                                                  hbs_info['namespace'])
+        hbs = {'hbs': [left_vn_template, right_vn_template, ds_template]}
+        return hbs
+
+    # end hbs_get
 
     # Private Methods
     def _parse_args(self, args_str):
