415fa6814a9567a7df7ddf6d812ccb83eada4afa
Andrey Pavlov <andrey.mp@gmail.com>
2019-10-11 12:23:53 +0300

    revert and fix pep8 for schema_transformer
    Change-Id: I5cd115fae8f41ad82f1ee93d9562878f3e433096
    closes-jira-bug: CE-1912

--- src/config/schema-transformer/setup.py
+++ src/config/schema-transformer/setup.py
@@ -3,7 +3,9 @@
 #
 
 import re
-from setuptools import setup, find_packages, Command
+
+import setuptools
+
 
 def requirements(filename):
     with open(filename) as f:
@@ -12,18 +14,19 @@ def requirements(filename):
     return filter(bool, map(lambda y: c.sub('', y).strip(), lines))
 
 
-setup(
+setuptools.setup(
     name='schema_transformer',
     version='0.1dev',
-    packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test"]),
+    packages=setuptools.find_packages(
+        exclude=["*.test", "*.test.*", "test.*", "test"]),
     package_data={'': ['*.html', '*.css', '*.xml']},
     zip_safe=False,
     long_description="VNC Configuration Schema Transformer",
-    entry_points = {
-         # Please update sandesh/common/vns.sandesh on process name change
-         'console_scripts' : [
-             'contrail-schema = schema_transformer.to_bgp:server_main',
-         ],
+    entry_points={
+        # Please update sandesh/common/vns.sandesh on process name change
+        'console_scripts': [
+            'contrail-schema = schema_transformer.to_bgp:server_main',
+        ],
     },
     install_requires=requirements('requirements.txt'),
     tests_require=requirements('test-requirements.txt'),
