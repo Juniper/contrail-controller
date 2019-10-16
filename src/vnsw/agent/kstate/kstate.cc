b6f060e9917b7b2fe2d711eddc7022a778934c7f
sagarkchitnis <sagarc@juniper.net>
2019-10-16 14:00:40 -0700

    fix for crash in agent running in mock mode
    closes-jira-bug: CEM-9385
    Change-Id: I0f8808b56dea50541cdcb02ad0ee767ccb836534

--- src/vnsw/agent/kstate/kstate.cc
+++ src/vnsw/agent/kstate/kstate.cc
@@ -62,8 +62,11 @@ int KState::VrResponseMsgHandler(vr_response *r) {
 void KState::EncodeAndSend(Sandesh &encoder) {
 
     int encode_len, error;
-    uint8_t *buf = (uint8_t *)malloc(KSYNC_DEFAULT_MSG_SIZE);
     KSyncSock   *sock = KSyncSock::Get(0);
+
+    if (sock == NULL)//due to mock mode we have to handle cases when this value is NULL
+        return;
+    uint8_t *buf = (uint8_t *)malloc(KSYNC_DEFAULT_MSG_SIZE);
 
     encode_len = encoder.WriteBinary(buf, KSYNC_DEFAULT_MSG_SIZE, &error);
 
