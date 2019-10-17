a460607dab5fadeba69e89fb4227e1cd45ce1c75
Joe Williams <josephw@juniper.net>
2019-10-17 23:38:27 +0000

    Add transaction information to physical-router object
    Change-Id: I68884ce3baa1d4e4701391fd7903bf55b32f0643
    Closes-jira-bug: CEM-9660
    Target-release: R2002

--- src/config/api-server/vnc_cfg_api_server/resources/security_group.py
+++ src/config/api-server/vnc_cfg_api_server/resources/security_group.py
@@ -2,17 +2,21 @@
 # Copyright (c) 2018 Juniper Networks, Inc. All rights reserved.
 #
 
+import json
+
 from cfgm_common.utils import _DEFAULT_ZK_COUNTER_PATH_PREFIX
 from vnc_api.gen.resource_common import SecurityGroup
 
 from vnc_cfg_api_server.context import get_context
 from vnc_cfg_api_server.resources._policy_base import check_policy_rules
-from vnc_cfg_api_server.resources._resource_base import ResourceMixin
+from vnc_cfg_api_server.resources._transaction_base import \
+    TransactionResourceBase
+from vnc_cfg_api_server.utils import kvp_to_dict
 from vnc_cfg_api_server.vnc_quota import QUOTA_OVER_ERROR_CODE
 from vnc_cfg_api_server.vnc_quota import QuotaHelper
 
 
-class SecurityGroupServer(ResourceMixin, SecurityGroup):
+class SecurityGroupServer(TransactionResourceBase, SecurityGroup):
     get_nested_key_as_list = classmethod(lambda cls, x, y, z: (
         x.get(y).get(z)
         if (type(x) is dict and x.get(y) and x.get(y).get(z)) else []))
@@ -168,6 +172,30 @@ class SecurityGroupServer(ResourceMixin, SecurityGroup):
                 proj_dict, db_conn, rule_count)
             if not ok:
                 return ok, result
+
+        # Find affected physical-router objects and insert transaction info
+        pr_name_list = []
+        for vmi_ref in sg_dict.get('virtual_machine_interface_back_refs', []):
+            vmi_id = vmi_ref['uuid']
+            ok, vmi_dict = cls.dbe_read(
+                db_conn, 'virtual_machine_interface', vmi_id,
+                obj_fields=['virtual_machine_interface_bindings'])
+            if not ok:
+                return ok, vmi_dict
+
+            bindings = vmi_dict.get('virtual_machine_interface_bindings')
+            if bindings:
+                kvps = bindings['key_value_pair']
+                kvp_dict = kvp_to_dict(kvps)
+                prof_str = kvp_dict.get('profile')
+                if prof_str:
+                    profile = json.loads(prof_str)
+                    link_info = profile.get('local_link_information', [])
+                    for link in link_info:
+                        pr_name_list.append(link['switch_info'])
+        cls.create_job_transaction(
+            "Security Group '{}' Update".format(fq_name[-1]),
+            pr_name_list=pr_name_list)
 
         return True, {
             'deallocated_security_group_id': deallocated_security_group_id,
