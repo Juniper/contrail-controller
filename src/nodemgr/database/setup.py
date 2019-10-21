97f1edcca124b6ede34fb398a4915733e26504b5
Andrey Pavlov <andrey.mp@gmail.com>
2019-10-21 11:48:13 +0300

    remove outdated files. move database info schema to nodemgr
    Change-Id: I16214d8ec316e3313e3eeeb3cf61926af8596253
    related-jira-bug: CEM-8399

---
+++ src/nodemgr/database/setup.py
@@ -0,0 +1,22 @@
+#
+# Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
+#
+
+from setuptools import setup
+
+setup(
+    name='database',
+    version='0.1dev',
+    install_requires=[
+        'future',
+        ],
+    packages=['database',
+              'database.sandesh',
+              'database.sandesh.database',
+              'database.sandesh.database.process_info',
+              'database.sandesh.database.cpuinfo'
+              ],
+    package_data={'': ['*.html', '*.css', '*.xml']},
+    zip_safe=False,
+    long_description="database package",
+)
