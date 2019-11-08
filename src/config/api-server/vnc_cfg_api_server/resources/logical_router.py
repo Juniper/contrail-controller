38698e7b429dba999eca178fcd0ca1032a126e71
Sathish Holla <saholla@juniper.net>
2019-11-08 11:16:09 -0800

    [Config] Set the id_perms "enable" field to True for internal VN
    This is needed for EVPN-VXLAN service chaining feature.
    We need to enable the internal VN for service VMs to work properly.
    
    Closes-Jira-Bug: CEM-10401
    Change-Id: Iebf48af05f430e81ca5966d326230cb1ef927841

--- src/config/api-server/vnc_cfg_api_server/resources/logical_router.py
+++ src/config/api-server/vnc_cfg_api_server/resources/logical_router.py
@@ -475,7 +475,7 @@ class LogicalRouterServer(ResourceMixin, LogicalRouter):
     def create_intvn_and_ref(cls, obj_dict):
         vn_fq_name = (obj_dict['fq_name'][:-1] +
                       [get_lr_internal_vn_name(obj_dict['uuid'])])
-        kwargs = {'id_perms': IdPermsType(user_visible=False)}
+        kwargs = {'id_perms': IdPermsType(user_visible=False,enable=True)}
         kwargs['display_name'] = 'LR::%s' % obj_dict['fq_name'][-1]
         vn_property = VirtualNetworkType(forwarding_mode='l3')
         if 'vxlan_network_identifier' in obj_dict:
