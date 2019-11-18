73e5cc45ac7a7544038bbf521f3157a99c7b1e46
pandita-juniper <pandita@juniper.net>
2019-11-17 21:20:05 -0800

    [DM] In case of brownfield use ASN number if pre-configured by user otherwise use default fabric ASN number. Target-Release: R1912 Closes-Jira-Bug: CEM-10310
    Change-Id: I5c1064349c5ffe9777a1367f8662509df7cdb346

--- src/config/device-manager/device_manager/db.py
+++ src/config/device-manager/device_manager/db.py
@@ -380,6 +380,10 @@ class PhysicalRouterDM(DBBaseDM):
             self.forced_cfg_push = False
         self.product = obj.get('physical_router_product_name') or ''
         self.device_family = obj.get('physical_router_device_family')
+
+        # In case of brownfield deployment if user has already configured
+        # ASN number use that, otherwise default to system configured
+        # ASN number
         self.brownfield_global_asn = ''
         brownfield_global_asn = obj.get(
             'physical_router_autonomous_system') or {}
@@ -389,6 +393,9 @@ class PhysicalRouterDM(DBBaseDM):
             if brownfield_global_asn_list:
                 self.brownfield_global_asn = str(
                     brownfield_global_asn_list[-1])
+        else:
+            self.brownfield_global_asn = \
+                str(GlobalSystemConfigDM.get_global_asn())
         self.vnc_managed = obj.get('physical_router_vnc_managed')
         self.underlay_managed = obj.get('physical_router_underlay_managed')
         self.physical_router_role = obj.get('physical_router_role')
