1f175139d813d9299c1bfdb50788f1700aa0b377
yijie <yixiao@juniper.net>
2019-11-19 22:17:48 -0800

    [Config] ST stage 2 Futurization
    Config schema transformer code is modernized using stage2 futurize option.
    
    Partial-Jira-Bug: CEM-7483
    Change-Id: Ie6ca05fb34be3c9b217d8e0b5191593d402f8a7a

--- src/config/schema-transformer/schema_transformer/resources/service_chain.py
+++ src/config/schema-transformer/schema_transformer/resources/service_chain.py
@@ -4,7 +4,9 @@
 
 import copy
 import json
+from builtins import str
 import uuid
+
 
 from cfgm_common.uve.virtual_network.ttypes import UveServiceChain
 from cfgm_common.uve.virtual_network.ttypes import UveServiceChainData
@@ -99,7 +101,7 @@ class ServiceChain(ResourceBaseST):
     @classmethod
     def find(cls, left_vn, right_vn, direction, sp_list, dp_list, protocol,
              service_list):
-        for sc in ServiceChain.values():
+        for sc in list(ServiceChain.values()):
             if (left_vn == sc.left_vn and
                     right_vn == sc.right_vn and
                     sp_list == sc.sp_list and
