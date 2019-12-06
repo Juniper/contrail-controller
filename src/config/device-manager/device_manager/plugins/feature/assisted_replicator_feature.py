345ee3926be03ca752f75aee2985bab8f6f7a888
Sridevi <sjeevaraj@juniper.net>
2019-12-06 19:13:14 +0000

    [DM] Implement intent based config push in DM (to resolve AR issue)
    1. New object intent-map created
    2. Associate AR related relevant objects to AR intent-map obj
    3. Reaction map will trigger config on AR Spine
    
    Change-Id: I274c44a32a1336233f7e6b0358721ac7b107e04a
    Target-Release: R1912
    Closes-Jira-Bug: CEM-8590

--- src/config/device-manager/device_manager/plugins/feature/assisted_replicator_feature.py
+++ src/config/device-manager/device_manager/plugins/feature/assisted_replicator_feature.py
@@ -5,6 +5,8 @@
 """Assisted Replicator Feature Implementation."""
 
 from abstract_device_api.abstract_device_xsd import *
+import db
+from dm_utils import DMUtils
 from feature_base import FeatureBase
 
 
@@ -20,6 +22,42 @@ class AssistedReplicatorFeature(FeatureBase):
             logger, physical_router, configs)
     # end __init__
 
+    def _build_vn_config(self, feature_config):
+        if self._physical_router.intent_maps:
+            for intent_uuid in self._physical_router.intent_maps or ():
+                imp = db.IntentMapDM.get(intent_uuid)
+                if imp.intent_type == 'assisted-replicator':
+                    for vn_uuid in imp.virtual_networks or []:
+                        vn_obj = db.VirtualNetworkDM.get(vn_uuid)
+                        ri_obj = self._get_primary_ri(vn_obj)
+                        if ri_obj is None:
+                            continue
+                        # build route targets
+                        ri_name = DMUtils.make_vrf_name(vn_obj.fq_name[-1],
+                                                        vn_obj.vn_network_id,
+                                                        'l2')
+                        export_targets, import_targets = \
+                            self._get_export_import_targets(vn_obj, ri_obj)
+                        network_id = vn_obj.vn_network_id
+                        vxlan_id = vn_obj.get_vxlan_vni()
+                        ri = RoutingInstance(
+                            name=ri_name, virtual_network_mode='l2',
+                            export_targets=export_targets,
+                            import_targets=import_targets,
+                            virtual_network_id=str(network_id),
+                            vxlan_id=str(vxlan_id))
+                        feature_config.add_routing_instances(ri)
+                        # build vlan config
+                        vlan = Vlan(
+                            name=DMUtils.make_bridge_name(
+                                vn_obj.get_vxlan_vni()),
+                            vxlan_id=vn_obj.get_vxlan_vni())
+                        vlan.set_comment(DMUtils.vn_bd_comment(vn_obj,
+                                                               'VXLAN'))
+                        desc = "Virtual Network - %s" % vn_obj.name
+                        vlan.set_description(desc)
+                        feature_config.add_vlans(vlan)
+
     def _build_ar_config(self, feature_config):
         rep_act_delay = None
         for c in self._configs:
@@ -32,6 +70,8 @@ class AssistedReplicatorFeature(FeatureBase):
             replicator_activation_delay=rep_act_delay)
 
         feature_config.set_assisted_replicator(ar)
+
+        self._build_vn_config(feature_config)
 
     def feature_config(self, **kwargs):
         feature_config = Feature(name=self.feature_name())
