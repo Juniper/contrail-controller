f93b5f6d415d2a39ce87cafc3c43f94b9c29e0e9
Sridevi <sjeevaraj@juniper.net>
2019-12-06 19:13:14 +0000

    [DM] Implement intent based config push in DM (to resolve AR issue)
    1. New object intent-map created
    2. Associate AR related relevant objects to AR intent-map obj
    3. Reaction map will trigger config on AR Spine
    
    Change-Id: I274c44a32a1336233f7e6b0358721ac7b107e04a
    Target-Release: R1912
    Closes-Jira-Bug: CEM-8590

--- src/config/device-manager/device_manager/db.py
+++ src/config/device-manager/device_manager/db.py
@@ -290,6 +290,7 @@ class PhysicalRouterDM(DBBaseDM):
         self.plugins = None
         self.nc_handler_gl = None
         self.telemetry_profile = None
+        self.intent_maps = set()
         self.update(obj_dict)
 
         self.set_conf_sent_state(False)
@@ -437,6 +438,9 @@ class PhysicalRouterDM(DBBaseDM):
                     brownfield_global_asn_list[-1])
         else:
             self.get_overlay_ibgp_asn()
+
+        self.update_multiple_refs('intent_map', obj)
+
         self.reinit_device_plugin()
         self.allocate_asn()
     # end update
@@ -652,6 +656,7 @@ class PhysicalRouterDM(DBBaseDM):
         self.update_single_ref('node_profile', {})
         self.update_single_ref('telemetry_profile', {})
         self.update_single_ref('fabric', {})
+        self.update_multiple_refs('intent_map', {})
         self.fabric_obj = None
     # end delete_handler
 
@@ -1750,6 +1755,43 @@ class NetworkIpamDM(DBBaseDM):
 # end NetworkIpamDM
 
 
+class IntentMapDM(DBBaseDM):
+    _dict = {}
+    obj_type = 'intent_map'
+
+    def __init__(self, uuid, obj_dict=None):
+        self.uuid = uuid
+        self.name = None
+        self.physical_routers = set()
+        self.virtual_networks = set()
+        self.fabrics = set()
+        self.intent_type = None
+        self.update(obj_dict)
+    # end __init__
+
+    def update(self, obj=None):
+        if obj is None:
+            obj = self.read_obj(self.uuid)
+        self.update_multiple_refs('physical_router', obj)
+        self.update_multiple_refs('virtual_network', obj)
+        self.update_multiple_refs('fabric', obj)
+        self.fq_name = obj['fq_name']
+        self.name = self.fq_name[-1]
+        self.intent_type = obj.get('intent_map_intent_type')
+    # end update
+
+    @classmethod
+    def delete(cls, uuid):
+        if uuid not in cls._dict:
+            return
+        obj = cls._dict[uuid]
+        obj.update_multiple_refs('physical_router', {})
+        obj.update_multiple_refs('virtual_network', {})
+        obj.update_multiple_refs('fabric', {})
+        del cls._dict[uuid]
+    # end delete
+
+
 class VirtualNetworkDM(DBBaseDM):
     _dict = {}
     obj_type = 'virtual_network'
@@ -1769,6 +1811,7 @@ class VirtualNetworkDM(DBBaseDM):
         self.route_targets = None
         self.has_ipv6_subnet = False
         self.ipv6_ll_vn_id = None
+        self.intent_maps = set()
         self.update(obj_dict)
     # end __init__
 
@@ -1885,6 +1928,7 @@ class VirtualNetworkDM(DBBaseDM):
             route_targets = route_target_list.get('route_target')
             if route_targets:
                 self.route_targets = set(route_targets)
+        self.update_multiple_refs('intent_map', obj)
     # end update
 
     def get_prefixes(self, pr_uuid=None):
@@ -1996,6 +2040,7 @@ class VirtualNetworkDM(DBBaseDM):
         obj.update_multiple_refs('physical_router', {})
         obj.update_multiple_refs('tag', {})
         obj.update_multiple_refs('network_ipam', {})
+        obj.update_multiple_refs('intent_map', {})
         del cls._dict[uuid]
     # end delete
 # end VirtualNetworkDM
