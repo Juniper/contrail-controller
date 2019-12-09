a7c4254d928c640ace30f51c6b5f0e16736c21e1
Yijie Xiao <yixiao@juniper.net>
2019-12-09 13:20:30 -0800

    [config] change import statement of db_manage.py
    Make import statement complatible for python2.
    
    Partial-Jira-Bug: CEM-7484
    Change-Id: I7b3f6dd95c3dd6569848ff46e5c73740523d786d

--- src/config/api-server/vnc_cfg_api_server/db_manage.py
+++ src/config/api-server/vnc_cfg_api_server/db_manage.py
@@ -42,15 +42,15 @@ import pycassa
 import pycassa.connection
 from thrift.transport import TSSLSocket
 import ssl
-from . import utils
+from vnc_cfg_api_server import utils
 from cfgm_common.zkclient import IndexAllocator
 from cfgm_common.zkclient import ZookeeperClient
 from cfgm_common.svc_info import _VN_SNAT_PREFIX_NAME
 
 try:
-    from .vnc_db import VncServerCassandraClient
+    from vnc_cfg_api_server.vnc_db import VncServerCassandraClient
 except ImportError:
-    from vnc_cfg_ifmap import VncServerCassandraClient
+    from vnc_cfg_api_server.vnc_cfg_ifmap import VncServerCassandraClient
 import schema_transformer.db
 
 __version__ = "1.23"
