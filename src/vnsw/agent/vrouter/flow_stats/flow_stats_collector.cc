1882b8fd10ec74e1da08cd773e5d3ae563b904b7
Pranavadatta D N <pranavad@juniper.net>
2019-12-12 21:21:36 +0530

    Add a new field to route path to store path VN information for flows
    Route’s dest_vn_list is being used to populate flow’s source and destination VNs
    in Agent. This field was not being populated in some cases for which we made
    changes to populate this field (CEM-10553). However, this change has been found
    to impact Service Chaining which in some cases applies policies of the network
    in dest_vn_list. This change adds a new field to route's path to carry VN info
    and uses this field to populate flow's vn info where it is not availabe in
    route's dest_vn_list.
    
    Closes-jira-bug: CEM-11164
    Change-Id: I1afc1a55ebe48fbc2faf743e4527046456eed911

--- src/vnsw/agent/vrouter/flow_stats/flow_stats_collector.cc
+++ src/vnsw/agent/vrouter/flow_stats/flow_stats_collector.cc
@@ -1101,8 +1101,15 @@ static void FlowExportInfoToSandesh(const FlowExportInfo &value,
     if (rflow) {
         info.set_rev_flow_uuid(to_string(rflow->uuid()));
     }
-    info.set_source_vn(flow->data().source_vn_match);
-    info.set_dest_vn(flow->data().dest_vn_match);
+    LOG(ERROR, "FlowExportInfoToSandesh for  flow " << flow->flow_handle() << " s " << flow->data().source_vn_match << " es " << flow->data().evpn_source_vn_match << " d " << flow->data().dest_vn_match << " ed " << flow->data().evpn_dest_vn_match);
+    if (!flow->data().source_vn_match.empty())
+        info.set_source_vn(flow->data().source_vn_match);
+    else
+        info.set_source_vn(flow->data().evpn_source_vn_match);
+    if (!flow->data().dest_vn_match.empty())
+        info.set_dest_vn(flow->data().dest_vn_match);
+    else
+        info.set_dest_vn(flow->data().evpn_dest_vn_match);
     info.set_sg_rule_uuid(flow->sg_rule_uuid());
     info.set_nw_ace_uuid(flow->nw_ace_uuid());
     info.set_teardown_time(value.teardown_time());
