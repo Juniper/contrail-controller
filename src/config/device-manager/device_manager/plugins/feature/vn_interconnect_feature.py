7a36268fd1506d5946fcb5fbca41c7bea00d1f2b
Joe Williams <josephw@juniper.net>
2019-12-18 06:25:20 +0000

    [DM Fabric] Python3 stage3 changes
    Closes-jira-bug: CEM-7757
    Closes-jira-bug: CEM-7758
    Closes-jira-bug: CEM-7759
    
    Change-Id: I3e8f6744e60929bfde52ece1ced363f1a9992a98
    Target-release: R2002

--- src/config/device-manager/device_manager/plugins/feature/vn_interconnect_feature.py
+++ src/config/device-manager/device_manager/plugins/feature/vn_interconnect_feature.py
@@ -8,10 +8,10 @@ from builtins import str
 from collections import OrderedDict
 
 from abstract_device_api.abstract_device_xsd import *
-import db
-from dm_utils import DMUtils
-from feature_base import FeatureBase
 from netaddr import IPAddress, IPNetwork
+
+from .db import *
+from .feature_base import FeatureBase
 
 
 class VnInterconnectFeature(FeatureBase):
