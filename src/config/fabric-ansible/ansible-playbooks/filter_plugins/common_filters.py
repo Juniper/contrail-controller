d46f95802d80f02136c255cff914b37517a76cfe
Jacek Iżykowski <jacek.izykowski@codilime.com>
2019-12-27 15:52:26 +0100

    GDO - check incoming multicast traffic
    Task of this GDO is to gather multicast statistics for the
    traffic incoming from the outside of the fabric and going
    to devices attached to server leaves (for source address
    and multicast group provided as parameters).
    More details are provided in juniper.yml.
    
    Change-Id: Idd7f80a1c85dd97bba3d64a0a16ea3312b43300b

--- src/config/fabric-ansible/ansible-playbooks/filter_plugins/common_filters.py
+++ src/config/fabric-ansible/ansible-playbooks/filter_plugins/common_filters.py
@@ -3,6 +3,8 @@ from builtins import range
 import crypt
 import random
 
+from job_manager.job_utils import JobVncApi
+
 
 class FilterModule(object):
     CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -10,7 +12,8 @@ class FilterModule(object):
     def filters(self):
         return {
             'encrypt': self.crypt_password,
-            'shell_escape': self.shell_escape
+            'shell_escape': self.shell_escape,
+            'decrypt_device_password': self.decrypt_device_password
         }
 
     @classmethod
@@ -27,3 +30,9 @@ class FilterModule(object):
     @classmethod
     def shell_escape(cls, strg):
         return strg.replace("\\", "\\\\").replace("$", "\\$")
+
+    @classmethod
+    def decrypt_device_password(cls, encrypted_password, secret_key):
+        return JobVncApi.decrypt_password(
+                    encrypted_password=encrypted_password,
+                    pwd_key=secret_key)
