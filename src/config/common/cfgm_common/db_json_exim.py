dd4befac758e2b66be34fef848d20f209ee9aa37
Radosław Cymer <radoslaw.cymer@codilime.com>
2020-01-08 15:24:04 +0100

    Mock DB JSON dumps from unit tests
    1. Introduced a slight twist into how DatabaseExim works.
    It will now accept an optional "overwrite" flag on import,
     that will conditionally merge imported data with existing one.
    Test case creates a minimal dump that's accepted by Cassandra and ZK,
     loads data in, dumps it and compares file sizes with state prior to loading.
    
    2. Mock DB load-before and dump-after tests.
    We now depend on another Pawel's commit... [WIP]
    
    closes-jira-bug: CEM-10878
    closes-jira-bug: CEM-9953
    Change-Id: Ib85f3cee0e880a39068fff353acb560d91c3b144

--- src/config/common/cfgm_common/db_json_exim.py
+++ src/config/common/cfgm_common/db_json_exim.py
@@ -34,6 +34,7 @@ from pycassa import ColumnFamily
 from pycassa.system_manager import SystemManager
 import kazoo.client
 import kazoo.handlers.gevent
+import kazoo.exceptions
 from thrift.transport import TSSLSocket
 import ssl
 
@@ -119,7 +120,7 @@ class DatabaseExim(object):
         pass
     # end _parse_args
 
-    def db_import(self):
+    def db_import(self, merge=False):
         if self._args.import_from.endswith('.gz'):
             try:
                 f = gzip.open(self._args.import_from, 'rb')
@@ -134,31 +135,32 @@ class DatabaseExim(object):
             for ks,cf in list(self.import_data['cassandra'].items()))
         self.init_cassandra(ks_cf_info)
 
-        # refuse import if db already has data
-        non_empty_errors = []
-        for ks in list(self.import_data['cassandra'].keys()):
-            for cf in list(self.import_data['cassandra'][ks].keys()):
-                if len(list(self._cassandra.get_cf(cf).get_range(
-                    column_count=0))) > 0:
-                    non_empty_errors.append(
-                        'Keyspace %s CF %s already has entries.' %(ks, cf))
+        # conditionally refuse import if db already has data
+        if not merge:
+            non_empty_errors = []
+            for ks in list(self.import_data['cassandra'].keys()):
+                for cf in list(self.import_data['cassandra'][ks].keys()):
+                    if len(list(self._cassandra.get_cf(cf).get_range(
+                        column_count=0))) > 0:
+                        non_empty_errors.append(
+                            'Keyspace %s CF %s already has entries.' %(ks, cf))
 
-        if non_empty_errors:
-            raise CassandraNotEmptyError('\n'.join(non_empty_errors))
+            if non_empty_errors:
+                raise CassandraNotEmptyError('\n'.join(non_empty_errors))
 
-        non_empty_errors = []
-        existing_zk_dirs = set(
-            self._zookeeper.get_children(self._api_args.cluster_id+'/'))
-        import_zk_dirs = set([p_v_ts[0].split('/')[1]
-            for p_v_ts in json.loads(self.import_data['zookeeper'] or "[]")])
+            non_empty_errors = []
+            existing_zk_dirs = set(
+                self._zookeeper.get_children(self._api_args.cluster_id+'/'))
+            import_zk_dirs = set([p_v_ts[0].split('/')[1]
+                for p_v_ts in json.loads(self.import_data['zookeeper'] or "[]")])
 
-        for non_empty in ((existing_zk_dirs & import_zk_dirs) -
-                          set(['zookeeper'])):
-            non_empty_errors.append(
-                'Zookeeper has entries at /%s.' %(non_empty))
+            for non_empty in ((existing_zk_dirs & import_zk_dirs) -
+                              set(['zookeeper'])):
+                non_empty_errors.append(
+                    'Zookeeper has entries at /%s.' %(non_empty))
 
-        if non_empty_errors:
-            raise ZookeeperNotEmptyError('\n'.join(non_empty_errors))
+            if non_empty_errors:
+                raise ZookeeperNotEmptyError('\n'.join(non_empty_errors))
 
         # seed cassandra
         for ks_name in list(self.import_data['cassandra'].keys()):
@@ -184,7 +186,10 @@ class DatabaseExim(object):
             if path.split('/')[1] in zk_ignore_list:
                 continue
             value = path_value_ts[1][0]
-            self._zookeeper.create(path, str(value), makepath=True)
+            try:
+                self._zookeeper.get(path)
+            except kazoo.exceptions.NoNodeError:
+                self._zookeeper.create(path, str(value), makepath=True)
     # end db_import
 
     def _make_ssl_socket_factory(self, ca_certs, validate=True):
@@ -225,7 +230,13 @@ class DatabaseExim(object):
                 socket_factory=socket_factory, credentials=creds)
             sys_mgr = SystemManager(self._api_args.cassandra_server_list[0],
                 credentials=creds, socket_factory=socket_factory)
-            for cf_name in sys_mgr.get_keyspace_column_families(full_ks_name):
+
+            try:
+                column_family_names = sys_mgr.get_keyspace_column_families(full_ks_name)
+            except KeyError:
+                continue
+
+            for cf_name in column_family_names:
                 cassandra_contents[ks_name][cf_name] = {}
                 cf = ColumnFamily(pool, cf_name,
                                   buffer_size=self._args.buffer_size)
@@ -233,8 +244,12 @@ class DatabaseExim(object):
                     cassandra_contents[ks_name][cf_name][r] = c
 
         def get_nodes(path):
-            if not zk.get_children(path):
-                return [(path, zk.get(path))]
+            try:
+                if not zk.get_children(path):
+                    return [(path, zk.get(path))]
+            except kazoo.exceptions.NoNodeError:
+                # when mock data is flaky then zk.get(path) may not exist and throw an error
+                return []
 
             nodes = []
             for child in zk.get_children(path):
--- src/config/common/cfgm_common/tests/test_common.py
+++ src/config/common/cfgm_common/tests/test_common.py
@@ -37,7 +37,7 @@ from vnc_api.vnc_api import *
 import kombu
 import cfgm_common.zkclient
 from cfgm_common.uve.vnc_api.ttypes import VncApiConfigLog
-from cfgm_common import vnc_cgitb
+from cfgm_common import db_json_exim, vnc_cgitb
 from cfgm_common.vnc_cassandra import VncCassandraClient
 from cfgm_common.utils import cgitb_hook
 
@@ -52,6 +52,7 @@ import novaclient.client
 import gevent.pywsgi
 import uuid
 from pysandesh import sandesh_logger
+
 
 def lineno():
     """Returns the current line number in our program."""
@@ -1056,6 +1057,49 @@ class TestCase(testtools.TestCase, fixtures.TestWithFixtures):
             raise Exception('SecurityGroupRuleNotExists %s' % sg_rule.rule_uuid)
     #end _security_group_rule_append
 
+    @classmethod
+    def _get_golden_json(cls):
+        test_root = os.path.normpath(os.getcwd())
+        dirpath = os.path.join(test_root, 'vnc_cfg_api_server', 'tests')
+        dirpath = os.path.abspath(dirpath)
+        if not os.path.exists(dirpath):
+            os.makedirs(dirpath)
+        filename_origin = os.path.join(dirpath, 'db-dump.json')
+
+        # it would be great if every gevent coroutine
+        # would get it's own file to dump db into
+        # maybe something like threading.current_thread()
+
+        # for now it's going to be separate for all tests cases being run
+        fingerprint = id(cls)
+        filename_coproc = os.path.join(dirpath, ('db-dump-%s.json' % fingerprint) )
+        try:
+            os.stat(filename_coproc)
+            return filename_coproc
+        except OSError:
+            # this is the first time we are being run
+            open(filename_coproc, 'a').close()
+            return filename_origin
+    # end get_golden_json
+
+    @classmethod
+    def dump_db_contents(cls):
+        dump_path = cls._get_golden_json()
+        db_json_exim.DatabaseExim(
+            '--export-to %s --cluster_id %s ' % (dump_path, cls._cluster_id)
+        ).db_export()
+    # end dump_db_contents
+
+    @classmethod
+    def load_db_contents(cls):
+        dump_path = cls._get_golden_json()
+        if not os.path.exists(dump_path):
+            return
+        db_json_exim.DatabaseExim(
+            '--import-from %s --cluster_id %s' % (dump_path, cls._cluster_id)
+        ).db_import(merge=True)
+    # end load_db_contents
+
 # end TestCase
 
 
@@ -1122,3 +1166,6 @@ class ErrorInterceptingLogger(sandesh_logger.SandeshLogger):
         super(ErrorInterceptingLogger, self).__init__(*args, **kwargs)
         self._logger = ErrorInterceptingLogger.LoggerWrapper(
             self._logger)
+# end class ErrorInterceptingLogger
+
+# test_common.py
