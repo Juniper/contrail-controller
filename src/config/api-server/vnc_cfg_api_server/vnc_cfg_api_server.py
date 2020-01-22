5a4309e9ee67ac839e614a6cfdbf2683330aaef9
Sajeesh Mathew <sajeeshm@juniper.net>
2020-01-22 14:09:31 -0800

    [Config] Config Misc python 3 conversion
    Fixed python3 list expressions scope issues
    
    Change-Id: I7c49ccc03801def8309c99352166283470747faa
    Partial-Jira-Bug: CEM-11675

--- src/config/api-server/vnc_cfg_api_server/vnc_cfg_api_server.py
+++ src/config/api-server/vnc_cfg_api_server/vnc_cfg_api_server.py
@@ -2966,15 +2966,15 @@ class VncApiServer(object):
 
         if operation == 'ADD':
             if ref_obj_type+'_refs' not in obj_dict:
-                obj_dict[ref_obj_type+'_refs'] = []
-            existing_ref = [ref for ref in obj_dict[ref_obj_type+'_refs']
-                            if ref['uuid'] == ref_uuid]
-            if existing_ref:
-                ref['attr'] = attr
+                obj_dict[ref_obj_type + '_refs'] = []
+            for ref in obj_dict.get(ref_obj_type + '_refs', []):
+                if ref['uuid'] == ref_uuid:
+                    ref['attr'] = attr
+                    break
             else:
                 obj_dict[ref_obj_type+'_refs'].append(
-                    {'to':ref_fq_name, 'uuid': ref_uuid, 'attr':attr})
-        elif operation == 'DELETE':
+                    {'to':ref_fq_name, 'uuid': ref_uuid, 'attr':attr}) 
+       elif operation == 'DELETE':
             for old_ref in obj_dict.get(ref_obj_type+'_refs', []):
                 if old_ref['to'] == ref_fq_name or old_ref['uuid'] == ref_uuid:
                     obj_dict[ref_obj_type+'_refs'].remove(old_ref)
