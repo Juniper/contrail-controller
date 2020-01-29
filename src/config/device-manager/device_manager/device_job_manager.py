21400688ebc1e6546b5c11b9dccfdc3b9b111f06
Joe Williams <josephw@juniper.net>
2020-01-29 22:48:14 +0000

    [DM] Device-manager keeps restarting during scale testing
    Closes-jira-bug: CEM-10926
    
    Change-Id: I2a6685a1a5becc46ad987c32d4c4f5971b2a4341
    Target-release: R2002
    
    [DM] Device-manager keeps restarting during scale testing
    
    Closes-jira-bug: CEM-10926
    
    Change-Id: I2a6685a1a5becc46ad987c32d4c4f5971b2a4341
    Target-release: R2002

--- src/config/device-manager/device_manager/device_job_manager.py
+++ src/config/device-manager/device_manager/device_job_manager.py
@@ -174,6 +174,8 @@ class DeviceJobManager(object):
             # requeue the message if the max threshold is reached, to be picked
             # by another job manager or wait until the current job mgr is free
             message.reject(requeue=True)
+            gevent.idle()
+            return
 
         update_uve_on_failure = False
         device_list = None
