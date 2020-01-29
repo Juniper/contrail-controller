d12ca2179bf6ddb5ea99626ff58915aa76459e0e
Joe Williams <josephw@juniper.net>
2020-01-29 22:48:14 +0000

    [DM] Device-manager keeps restarting during scale testing
    Closes-jira-bug: CEM-10926
    
    Change-Id: I2a6685a1a5becc46ad987c32d4c4f5971b2a4341
    Target-release: R2002
    
    [DM] Device-manager keeps restarting during scale testing
    
    Closes-jira-bug: CEM-10926
    
    Change-Id: I2a6685a1a5becc46ad987c32d4c4f5971b2a4341
    Target-release: R2002

--- src/config/device-manager/device_manager/plugins/feature/l2_gateway_feature.py
+++ src/config/device-manager/device_manager/plugins/feature/l2_gateway_feature.py
@@ -13,6 +13,8 @@ import db
 from dm_utils import DMUtils
 from feature_base import FeatureBase
 
+import gevent # noqa
+
 
 class L2GatewayFeature(FeatureBase):
 
@@ -29,6 +31,7 @@ class L2GatewayFeature(FeatureBase):
 
     def _build_ri_config(self, vn, ri_name, ri_obj, interfaces, export_targets,
                          import_targets, feature_config):
+        gevent.idle()
         encapsulation_priorities = self._get_encapsulation_priorities()
         highest_encapsulation = encapsulation_priorities[0]
         network_id = vn.vn_network_id
--- src/config/device-manager/test/test_dm_qfx_bgp.py
+++ src/config/device-manager/test/test_dm_qfx_bgp.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
 #
 from __future__ import absolute_import
+import mock
 from .test_dm_bgp import TestBgpDM
 
 #
@@ -12,6 +13,15 @@ class TestQfxBgpDM(TestBgpDM):
     def __init__(self, *args, **kwargs):
         self.product = "qfx5110"
         super(TestQfxBgpDM, self).__init__(*args, **kwargs)
+
+    def setUp(self, extra_config_knobs=None):
+        super(TestQfxBgpDM, self).setUp(extra_config_knobs=extra_config_knobs)
+        self.idle_patch = mock.patch('gevent.idle')
+        self.idle_mock = self.idle_patch.start()
+
+    def tearDown(self):
+        self.idle_patch.stop()
+        super(TestQfxBgpDM, self).tearDown()
 
     # test hold time configuration
     def test_dm_bgp_hold_time_config(self):
