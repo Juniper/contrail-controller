d7ba8eb12a57c301275d24eb62ceb6ee62d27252
Joe Williams <josephw@juniper.net>
2020-01-29 22:48:14 +0000

    [DM] Device-manager keeps restarting during scale testing
    Closes-jira-bug: CEM-10926
    
    Change-Id: I2a6685a1a5becc46ad987c32d4c4f5971b2a4341
    Target-release: R2002

--- src/config/device-manager/device_manager/plugins/feature/l2_gateway_feature.py
+++ src/config/device-manager/device_manager/plugins/feature/l2_gateway_feature.py
@@ -9,6 +9,8 @@ from collections import OrderedDict
 
 from abstract_device_api.abstract_device_xsd import *
 from attrdict import AttrDict
+import gevent
+
 import db
 from dm_utils import DMUtils
 from feature_base import FeatureBase
@@ -29,6 +31,7 @@ class L2GatewayFeature(FeatureBase):
 
     def _build_ri_config(self, vn, ri_name, ri_obj, interfaces, export_targets,
                          import_targets, feature_config):
+        gevent.idle()
         encapsulation_priorities = self._get_encapsulation_priorities()
         highest_encapsulation = encapsulation_priorities[0]
         network_id = vn.vn_network_id
