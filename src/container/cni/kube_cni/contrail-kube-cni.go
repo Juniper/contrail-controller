f51bc4e4b24c52577af989fd5bab71d14cc4d5aa
Yuvaraja Mariappan <ymariappan@juniper.net>
2020-03-06 12:27:25 -0800

    Fixed Alignment issues
    Fixed Alignment issues
    
    Closes-jira-bug: CEM-13374
    Change-Id: Id248a5b50193f5c627af7a3f6fad70eb2a8fd829

--- src/container/cni/kube_cni/contrail-kube-cni.go
+++ src/container/cni/kube_cni/contrail-kube-cni.go
@@ -8,101 +8,101 @@
 package main
 
 import (
-	"context"
-	"os"
+    "context"
+    "os"
 
-	"../contrail"
-	log "../logging"
-	"github.com/containernetworking/cni/pkg/skel"
-	cniSpecVersion "github.com/containernetworking/cni/pkg/version"
-	"github.com/docker/docker/client"
+    "../contrail"
+    log "../logging"
+    "github.com/containernetworking/cni/pkg/skel"
+    cniSpecVersion "github.com/containernetworking/cni/pkg/version"
+    "github.com/docker/docker/client"
 )
 
 // Use "docker inspect" equivalent API to get UUID and Name for container
 func getPodInfo(skelArgs *skel.CmdArgs) (string, string, error) {
-	os.Setenv("DOCKER_API_VERSION", "1.22")
-	cli, err := client.NewEnvClient()
-	if err != nil {
-		log.Errorf("Error creating docker client. %+v", err)
-		return "", "", err
-	}
+    os.Setenv("DOCKER_API_VERSION", "1.22")
+    cli, err := client.NewEnvClient()
+    if err != nil {
+        log.Errorf("Error creating docker client. %+v", err)
+        return "", "", err
+    }
 
-	data, err := cli.ContainerInspect(context.Background(),
-		skelArgs.ContainerID)
-	if err != nil {
-		log.Errorf("Error querying for container %s. %+v",
-			skelArgs.ContainerID, err)
-		return "", "", err
-	}
+    data, err := cli.ContainerInspect(context.Background(),
+        skelArgs.ContainerID)
+    if err != nil {
+        log.Errorf("Error querying for container %s. %+v",
+            skelArgs.ContainerID, err)
+        return "", "", err
+    }
 
-	uuid := data.Config.Labels["io.kubernetes.pod.uid"]
-	name := data.Config.Hostname
-	log.Infof("getPodInfo success. container-id %s uuid %s name %s",
-		skelArgs.ContainerID, uuid, name)
-	return uuid, name, nil
+    uuid := data.Config.Labels["io.kubernetes.pod.uid"]
+    name := data.Config.Hostname
+    log.Infof("getPodInfo success. container-id %s uuid %s name %s",
+        skelArgs.ContainerID, uuid, name)
+    return uuid, name, nil
 }
 
 // Add command
 func CmdAdd(skelArgs *skel.CmdArgs) error {
-	// Initialize ContrailCni module
-	cni, err := contrailCni.Init(skelArgs)
-	if err != nil {
-		return err
-	}
+    // Initialize ContrailCni module
+    cni, err := contrailCni.Init(skelArgs)
+    if err != nil {
+        return err
+    }
 
-	log.Infof("Came in Add for container %s", skelArgs.ContainerID)
-	// Get UUID and Name for container
-	containerUuid, containerName, err := getPodInfo(skelArgs)
-	if err != nil {
-		log.Errorf("Error getting UUID/Name for Container")
-		return err
-	}
+    log.Infof("Came in Add for container %s", skelArgs.ContainerID)
+    // Get UUID and Name for container
+    containerUuid, containerName, err := getPodInfo(skelArgs)
+    if err != nil {
+        log.Errorf("Error getting UUID/Name for Container")
+        return err
+    }
 
-	// Update UUID and Name for container
-	cni.Update(containerName, containerUuid, "")
-	cni.Log()
+    // Update UUID and Name for container
+    cni.Update(containerName, containerUuid, "")
+    cni.Log()
 
-	// Handle Add command
-	err = cni.CmdAdd()
-	if err != nil {
-		log.Errorf("Failed processing Add command.")
-		return err
-	}
+    // Handle Add command
+    err = cni.CmdAdd()
+    if err != nil {
+        log.Errorf("Failed processing Add command.")
+        return err
+    }
 
-	return nil
+    return nil
 }
 
 // Del command
 func CmdDel(skelArgs *skel.CmdArgs) error {
-	// Initialize ContrailCni module
-	cni, err := contrailCni.Init(skelArgs)
-	if err != nil {
-		return err
-	}
+    // Initialize ContrailCni module
+    cni, err := contrailCni.Init(skelArgs)
+    if err != nil {
+        return err
+    }
 
-	log.Infof("Came in Del for container %s", skelArgs.ContainerID)
-	// Get UUID and Name for container
-	containerUuid, containerName, err := getPodInfo(skelArgs)
-	if err != nil {
-		log.Errorf("Error getting UUID/Name for Container")
-		return err
-	}
+    log.Infof("Came in Del for container %s", skelArgs.ContainerID)
+    // Get UUID and Name for container
+    containerUuid, containerName, err := getPodInfo(skelArgs)
+    if err != nil {
+        log.Errorf("Error getting UUID/Name for Container")
+        return err
+    }
 
-	// Update UUID and Name for container
-	cni.Update(containerName, containerUuid, "")
-	cni.Log()
+    // Update UUID and Name for container
+    cni.Update(containerName, containerUuid, "")
+    cni.Log()
 
-	// Handle Del command
-	err = cni.CmdDel()
-	if err != nil {
-		log.Errorf("Failed processing Del command.")
-		return err
-	}
+    // Handle Del command
+    err = cni.CmdDel()
+    if err != nil {
+        log.Errorf("Failed processing Del command.")
+        return err
+    }
 
-	return nil
+    return nil
 }
 
 func main() {
-	// Let CNI skeletal code handle demux based on env variables
-	skel.PluginMain(CmdAdd, CmdDel, cniSpecVersion.All)
+    // Let CNI skeletal code handle demux based on env variables
+    skel.PluginMain(CmdAdd, CmdDel, cniSpecVersion.All)
 }
--- src/container/cni/mesos_cni/contrail-mesos-cni.go
+++ src/container/cni/mesos_cni/contrail-mesos-cni.go
@@ -8,78 +8,78 @@
 package main
 
 import (
-	"../contrail"
-	log "../logging"
-	"github.com/containernetworking/cni/pkg/skel"
-	cniSpecVersion "github.com/containernetworking/cni/pkg/version"
+    "../contrail"
+    log "../logging"
+    "github.com/containernetworking/cni/pkg/skel"
+    cniSpecVersion "github.com/containernetworking/cni/pkg/version"
 )
 
 // Get UUID and Name for container. UUID is same as container-id
 func getPodInfo(skelArgs *skel.CmdArgs) (string, string, error) {
-	return skelArgs.ContainerID, skelArgs.ContainerID, nil
+    return skelArgs.ContainerID, skelArgs.ContainerID, nil
 }
 
 // Add command
 func CmdAdd(skelArgs *skel.CmdArgs) error {
-	// Initialize ContrailCni module
-	cni, err := contrailCni.Init(skelArgs)
-	if err != nil {
-		return err
-	}
+    // Initialize ContrailCni module
+    cni, err := contrailCni.Init(skelArgs)
+    if err != nil {
+        return err
+    }
 
-	log.Infof("Came in Add for container %s", skelArgs.ContainerID)
-	// Get UUID and Name for container
-	containerUuid, containerName, err := getPodInfo(skelArgs)
-	if err != nil {
-		log.Errorf("Error getting UUID/Name for Container")
-		return err
-	}
+    log.Infof("Came in Add for container %s", skelArgs.ContainerID)
+    // Get UUID and Name for container
+    containerUuid, containerName, err := getPodInfo(skelArgs)
+    if err != nil {
+        log.Errorf("Error getting UUID/Name for Container")
+        return err
+    }
 
-	// Update UUID and Name for container
-	cni.Update(containerName, containerUuid, "")
-	cni.Log()
+    // Update UUID and Name for container
+    cni.Update(containerName, containerUuid, "")
+    cni.Log()
 
-	// Handle Add command
-	err = cni.CmdAdd()
-	if err != nil {
-		log.Errorf("Failed processing Add command.")
-		return err
-	}
+    // Handle Add command
+    err = cni.CmdAdd()
+    if err != nil {
+        log.Errorf("Failed processing Add command.")
+        return err
+    }
 
-	return nil
+    return nil
 }
 
 // Del command
 func CmdDel(skelArgs *skel.CmdArgs) error {
-	// Initialize ContrailCni module
-	cni, err := contrailCni.Init(skelArgs)
-	if err != nil {
-		return err
-	}
+    // Initialize ContrailCni module
+    cni, err := contrailCni.Init(skelArgs)
+    if err != nil {
+        return err
+    }
 
-	log.Infof("Came in Del for container %s", skelArgs.ContainerID)
-	// Get UUID and Name for container
-	containerUuid, containerName, err := getPodInfo(skelArgs)
-	if err != nil {
-		log.Errorf("Error getting UUID/Name for Container")
-		return err
-	}
+    log.Infof("Came in Del for container %s", skelArgs.ContainerID)
+    // Get UUID and Name for container
+    containerUuid, containerName, err := getPodInfo(skelArgs)
+    if err != nil {
+        log.Errorf("Error getting UUID/Name for Container")
+        return err
+    }
 
-	// Update UUID and Name for container
-	cni.Update(containerName, containerUuid, "")
-	cni.Log()
+    // Update UUID and Name for container
+    cni.Update(containerName, containerUuid, "")
+    cni.Log()
 
-	// Handle Del command
-	err = cni.CmdDel()
-	if err != nil {
-		log.Errorf("Failed processing Add command.")
-		return err
-	}
+    // Handle Del command
+    err = cni.CmdDel()
+    if err != nil {
+        log.Errorf("Failed processing Add command.")
+        return err
+    }
 
-	return nil
+    return nil
 }
 
 func main() {
-	// Let CNI skeletal code handle demux based on env variables
-	skel.PluginMain(CmdAdd, CmdDel, cniSpecVersion.All)
+    // Let CNI skeletal code handle demux based on env variables
+    skel.PluginMain(CmdAdd, CmdDel, cniSpecVersion.All)
 }
