f7e063fcda56bc3042bfa3318e5bd1c5586b179f
Sahid Orentino Ferdjaoui <sferdjaoui@juniper.net>
2020-03-24 18:58:01 +0100

    config/api-server: fix requirements for vnc_auth_keystone
    The module is needing keystoneauth1 and keystonemiddleware.
    NOTE: This would fix the undefined variable auth_token error.
    
    Signed-off-by: Sahid Orentino Ferdjaoui <sferdjaoui@juniper.net>
    Change-Id: If0c94ad118b84efbb48a4562cf89a6a46157c890

--- src/config/api-server/vnc_cfg_api_server/vnc_auth_keystone.py
+++ src/config/api-server/vnc_cfg_api_server/vnc_auth_keystone.py
@@ -21,13 +21,7 @@ import base64
 import re
 import uuid
 
-try:
-    from keystoneclient.middleware import auth_token
-except ImportError:
-    try:
-        from keystonemiddleware import auth_token
-    except ImportError:
-        pass
+from keystonemiddleware import auth_token
 from keystoneauth1 import exceptions as k_exc
 
 from pysandesh.gen_py.sandesh.ttypes import SandeshLevel
