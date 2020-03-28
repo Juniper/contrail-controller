d58e5820834177fb420abb6ed6f6a314f3910dbb
Parag Sanghvi <parags@juniper.net>
2020-03-27 18:26:46 -0700

    [API]: For Routing Policy and data-center-interconnect object added pre_dbe_check() and update validation for schema change to handle. - Added master LR and Public LR validation - updated code to use term_type outside of policyEntries in RP. - Added validation not to allow same source LR in more than one intra_fabric DCI. - added function doc header to all new api needed places - added UT test case for data_center_interconnect and routing_policy - added multiple functions to split logic.
    Closes-Jira-Bug: CEM-13387
    Change-Id: I86679181df6af886b8e481f2a7c8a2aa1e3a2f75

--- src/config/api-server/vnc_cfg_api_server/resources/routing_policy.py
+++ src/config/api-server/vnc_cfg_api_server/resources/routing_policy.py
@@ -3,6 +3,7 @@
 #
 
 from builtins import str
+import re
 
 from cfgm_common.exceptions import VncError
 from vnc_api.gen.resource_common import RoutingPolicy
@@ -11,13 +12,207 @@ from vnc_cfg_api_server.resources._resource_base import ResourceMixin
 
 
 class RoutingPolicyServer(ResourceMixin, RoutingPolicy):
-    @classmethod
-    def pre_dbe_create(cls, tenant_name, obj_dict, db_conn):
-        asn_list = None
+
+    @staticmethod
+    def _check_routefilter_range_string(rfvalue):
+        """Validate route_type_value of route_filter.
+
+        Value must be in the format of "/n-/n" where n must be the number
+        from 1 to 32.
+        : Args:
+        : rfvalue: route_type_value of route_filter property
+        : return:
+        : returns True if value is in expected proper format else returns
+        : False with proper error message
+        :
+        """
+        res = None
+        if rfvalue:
+            res = re.match(
+                r'^\/([1-9]|[1-2]\d|3[0-2])-\/([1-9]|[1-2]\d|3[0-2])$',
+                rfvalue)
+        if not res:
+            msg = ("route-filter value %s invalid. prefix-length-range "
+                   "should be in format '/minlength-/maxlength'" % rfvalue)
+            return False, (400, msg)
+        return True, ''
+
+    @staticmethod
+    def _check_aspath_string(aspath, aspath_type):
+        """Validate as_path_expand and as_path_prepend value.
+
+        value must be in the format of "n" or "n n" where n is integer number
+        represents ASN number.
+        : Args:
+        : aspath: value of as_path_expand or as_path_prepend
+        : aspath_type: either as_path_expand or as_path_prepend
+        : return:
+        : returns True if value is in expected proper format else returns
+        : False with proper error message
+        :
+        """
+        if aspath:
+            res = re.match(r'^\d+( \d+)*$', aspath)
+            if not res:
+                msg = ("%s has invalid value %s. valid format is "
+                       "AS number(s) seperated by spaces" %
+                       (aspath_type, aspath))
+                return False, (400, msg)
+        return True, ''
+
+    @staticmethod
+    def _check_rp_term_route_filter(cls, route_filter):
+        """Validate network-device type term's route_filter.
+
+        It validates route_filter defined in term_match_condition.
+        : Args:
+        : route_filter: route_filter from term_match_condition
+        : return:
+        : returns True if value is in expected proper format else returns
+        : False with proper error message
+        :
+        """
+        if not route_filter:
+            return True, ""
+        for route_filter in route_filter.get(
+                'route_filter_properties') or []:
+            if route_filter.get('route') is None:
+                msg = ('route_filter must have non-empty route value '
+                       'for network-device term')
+                return False, (400, msg)
+            route_type = route_filter.get('route_type')
+            if route_type is None:
+                msg = ('route_filter must have valid route_type '
+                       'for network-device term')
+                return False, (400, msg)
+            route_tvalue = route_filter.get('route_type_value')
+            if route_type == 'prefix-length-range':
+                ok, result = cls._check_routefilter_range_string(
+                    route_tvalue)
+                if not ok:
+                    return ok, result
+            elif route_type == 'through' or route_type == 'upto':
+                res = None
+                if route_tvalue:
+                    if route_type == 'upto':
+                        # upto value should be in format of "/number"
+                        # where allowed number is from 1 to 32
+                        res = re.match(
+                            r'^\/([1-9]|[1-2]\d|3[0-2])$',
+                            route_tvalue)
+                    else:
+                        if len(route_tvalue) > 0:
+                            res = True
+                if not res:
+                    if route_type == 'upto':
+                        msg = ("route-filter type value '%s' is"
+                               " invalid. '%s' should be in "
+                               "format '/number'" % (route_tvalue,
+                                                     route_type))
+                    else:
+                        msg = ("route-filter type value for "
+                               "'%s' should be non-empty "
+                               "string." % route_type)
+                    return False, (400, msg)
+        return True, ""
+
+    @staticmethod
+    def _check_rp_term_match_condition(cls, match_condition, db_conn):
+        """Validate network-device type term's term_match_condition.
+
+        It validates protocols, nlri_route_type, prefix_list and
+        route_filter defined in term_match_condition.
+        : Args:
+        : match_condition: term's term_match_condition
+        : db_conn: db connection handler
+        : return:
+        : returns True if value is in expected proper format else returns
+        : False with proper error message
+        :
+        """
+        if not match_condition:
+            return True, ""
+        protocol = match_condition.get('protocol')
+        if protocol:
+            allowed_protocol = set(['bgp', 'static', 'aggregate', 'pim',
+                                    'direct', 'evpn', 'ospf', 'ospf3'])
+            for p in protocol:
+                if p not in allowed_protocol:
+                    msg = ('%s protocol value is not valid value for '
+                           'network-device term' % p)
+                    return False, (400, msg)
+        nlri_route_type = match_condition.get('nlri_route_type')
+        for nlri in nlri_route_type or []:
+            if nlri < 1 or nlri > 10:
+                msg = ('%s nlri-route-type is not valid value, '
+                       'value should be in range of 1 to 10' %
+                       nlri)
+                return False, (400, msg)
+        prefix_list = match_condition.get('prefix_list')
+        for prefix in prefix_list or []:
+            irt_list = prefix.get('interface_route_table_uuid')
+            for irt in irt_list or []:
+                ok, read_result = cls.dbe_read(
+                    db_conn, 'interface_route_table', irt)
+                if not ok:
+                    return ok, read_result
+
+        route_filter_type = match_condition.get('route_filter')
+        return cls._check_rp_term_route_filter(route_filter_type)
+
+    @staticmethod
+    def _check_network_device_routing_policy_term(cls, term, term_type,
+                                                  db_conn):
+        """Validate network-device type term.
+
+        It validates term's various properties and its values should be in
+        expected format as per junos device.
+        : Args:
+        : term: term to be validated
+        : term_type: either network-device or vrouter
+        : db_conn: db connection handler
+        : return:
+        : returns True if value is in expected proper format else returns
+        : False with proper error message
+        :
+        """
+        if not term or not term_type or term_type != 'network-device':
+            return True, ''
+        match_condition = term.get('term_match_condition')
+        ok, result = cls._check_rp_term_match_condition(match_condition,
+                                                        db_conn)
+        if not ok:
+            return ok, result
+
+        action_list = term.get('term_action_list')
+        if action_list:
+            aspath_e = action_list.get('as_path_expand')
+            ok, result = cls._check_aspath_string(aspath_e, 'as_path_expand')
+            if not ok:
+                return ok, result
+            aspath_e = action_list.get('as_path_prepend')
+            ok, result = cls._check_aspath_string(aspath_e, 'as_path_prepend')
+            if not ok:
+                return ok, result
+
+        return True, ''
+
+    @staticmethod
+    def _validate_term_and_get_asnlist(cls, obj_dict, db_conn):
+        asn_list = []
         rp_entries = obj_dict.get('routing_policy_entries')
+        term_type = obj_dict.get('term_type', 'vrouter')
         if rp_entries:
-            term = rp_entries.get('term')[0]
-            if term:
+            termlist = rp_entries.get('term') or []
+            if term_type != 'network-device':
+                if len(termlist) > 1:
+                    termlist = [termlist[0]]
+            for term in termlist:
+                # validate network-device type term,
+                ok, result = cls._check_network_device_routing_policy_term(
+                    cls, term, term_type, db_conn)
+                if not ok:
+                    return False, asn_list, result
                 action_list = term.get('term_action_list')
                 if action_list:
                     action = action_list.get('update')
@@ -26,7 +221,21 @@ class RoutingPolicyServer(ResourceMixin, RoutingPolicy):
                         if as_path:
                             expand = as_path.get('expand')
                             if expand:
-                                asn_list = expand.get('asn_list')
+                                tasn_list = expand.get('asn_list')
+                                if tasn_list:
+                                    asn_list.extend(tasn_list)
+        return True, asn_list, ''
+
+    @classmethod
+    def pre_dbe_create(cls, tenant_name, obj_dict, db_conn):
+        asn_list = None
+        try:
+            ok, asn_list, result = cls._validate_term_and_get_asnlist(
+                cls, obj_dict, db_conn)
+            if not ok:
+                return False, result
+        except Exception as e:
+            return False, (400, str(e))
 
         try:
             global_asn = cls.server.global_autonomous_system
@@ -36,4 +245,29 @@ class RoutingPolicyServer(ResourceMixin, RoutingPolicy):
         if asn_list and global_asn in asn_list:
             msg = ("ASN can't be same as global system config asn")
             return False, (400, msg)
+        return True, ""
+
+    @classmethod
+    def pre_dbe_update(cls, id, fq_name, obj_dict, db_conn, **kwargs):
+        ok, read_result = cls.dbe_read(db_conn, 'routing_policy', id,
+                                       obj_fields=['routing_policy_entries',
+                                                   'term_type'])
+        if not ok:
+            return ok, read_result
+        # do not allow change in term type
+        old_term_type = read_result.get('term_type')
+        new_term_type = obj_dict.get('term_type')
+        if old_term_type and new_term_type:
+            if old_term_type != new_term_type:
+                return False, (
+                    403, "Cannot change term_type of routing policy! "
+                         "Please specify '%s' as a term_type in "
+                         "routing_policy_entries." % old_term_type)
+        try:
+            ok, _, result = cls._validate_term_and_get_asnlist(
+                cls, obj_dict, db_conn)
+            if not ok:
+                return False, result
+        except Exception as e:
+            return False, (400, str(e))
         return True, ""
