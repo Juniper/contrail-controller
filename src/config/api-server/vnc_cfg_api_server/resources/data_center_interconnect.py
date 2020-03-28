907a8316933c4c578d23c56794503ca83f264346
Parag Sanghvi <parags@juniper.net>
2020-03-27 18:26:46 -0700

    [API]: For Routing Policy and data-center-interconnect object added pre_dbe_check() and update validation for schema change to handle. - Added master LR and Public LR validation
    Closes-Jira-Bug: CEM-13387
    Change-Id: I86679181df6af886b8e481f2a7c8a2aa1e3a2f75

--- src/config/api-server/vnc_cfg_api_server/resources/data_center_interconnect.py
+++ src/config/api-server/vnc_cfg_api_server/resources/data_center_interconnect.py
@@ -9,11 +9,186 @@ from vnc_cfg_api_server.resources._resource_base import ResourceMixin
 
 class DataCenterInterconnectServer(ResourceMixin, DataCenterInterconnect):
 
+    @staticmethod
+    def _is_this_src_lr(lr_ref):
+        return True if lr_ref.get('attr') is not None else False
+
     @classmethod
-    def pre_dbe_create(cls, tenant_name, obj_dict, db_conn):
-        # make sure referenced LRs belongs to different fabrics
-        return cls._make_sure_lrs_belongs_to_different_fabrics(
-            db_conn, obj_dict)
+    def _validate_dci_lrs_fabrics_based_on_dcitype(cls, db_conn, dci):
+        """Validate for intra_fabric type dci properties.
+
+           - at least one destination LR must present
+           - at least one valid entry exist in VN_refs/or RP_Refs exist.
+           - at least one PR must have selected per destination LR
+           - LR ref count must be destination LR + source LR
+           - ALL destination LR's and their PR list must exist.
+           - If Src LR is master LR then
+                - every selected DST PR must exist in master SRC PR AND
+                - DO NOT ALLOW any DST Public LR
+           - if src LR is Public LR then DST LR must not be master LR
+           - if master LR is in DST LR then master LR selected DST PR must
+           exist in Src LR PR
+        """
+        dci_type = dci.get('data_center_interconnect_type')
+        if dci_type is None:
+            dci_type = 'inter_fabric'
+        if dci_type != 'intra_fabric':
+            return cls._make_sure_lrs_belongs_to_different_fabrics(
+                db_conn, dci)
+        # for all intra_fabric type dci do following validation
+        # get all lr uuid from lr_refs
+        dst_lrs_uuid = []
+        src_lr_uuid = None
+        for lr_ref in dci.get('logical_router_refs') or []:
+            lruuid = lr_ref.get('uuid')
+            if cls._is_this_src_lr(lr_ref):
+                if src_lr_uuid is not None:
+                    return False, (
+                        400, "More than one Source LR not allowed for "
+                             "intra_fabric type data_center_interconnect.")
+                src_lr_uuid = lruuid
+            else:
+                if lruuid in dst_lrs_uuid:
+                    return False, (
+                        400, "%s logical router entry exists more than one "
+                             "in logical_router_refs." % lruuid)
+                dst_lrs_uuid.append(lruuid)
+
+        if src_lr_uuid is None:
+            return False, (
+                400, "No Source LR specified in "
+                     "logical_router_refs for intra_fabric type "
+                     "data_center_interconnect.")
+        if len(dst_lrs_uuid) < 1:
+            return False, (
+                400, "No Destination LR specified in "
+                     "logical_router_refs for intra_fabric type "
+                     "data_center_interconnect.")
+
+        # get all dst lr and their pr list into dictionary
+        lr_to_pr = {}
+        dci_dpr_list = dci.get('destination_physical_router_list')
+        dci_lr_list = []
+        if dci_dpr_list:
+            dci_lr_list = dci_dpr_list.get('logical_router_list') or []
+        dst_lr_uuid_set = set(dst_lrs_uuid)
+        for dstlr in dci_lr_list:
+            uuid = dstlr.get('logical_router_uuid') or None
+            if not uuid:
+                return False, (
+                    400, "Invalid logical_router_uuid in "
+                         "destination_physical_router_list.")
+            if uuid == src_lr_uuid:
+                return False, (
+                    400, "%s logical_router_uuid exist in "
+                         "destination_physical_router_list is marked as a "
+                         " source LR." % uuid)
+            if uuid not in dst_lr_uuid_set:
+                return False, (
+                    400, "%s logical_router_uuid in "
+                         "destination_physical_router_list does not exist in "
+                         "logical_router_refs" % uuid)
+            dci_prlist = dstlr.get('physical_router_uuid_list') or []
+            if len(dci_prlist) < 1:
+                return False, (
+                    400, "%s logical_router_uuid in "
+                         "destination_physical_router_list does not have "
+                         " any physical_router" % uuid)
+            if uuid in lr_to_pr:
+                return False, (
+                    400, "%s logical_router_uuid in "
+                         "destination_physical_router_list specified more "
+                         "than once." % uuid)
+            lr_to_pr[uuid] = dci_prlist
+        if len(lr_to_pr) < 1:
+            return False, (
+                400, "No destination LR exist in "
+                     "destination_physical_router_list")
+        if len(lr_to_pr) != len(dst_lrs_uuid):
+            return False, (
+                400, "destination LR list count %s in "
+                     "destination_physical_router_list does not match "
+                     "logical_router_Refs expected %s count" %
+                (len(lr_to_pr), len(dst_lrs_uuid)))
+
+        # Validate routing policies if supplied.
+        dci_rps = dci.get('routing_policy_refs') or []
+        dci_vns = dci.get('virtual_network_refs') or []
+        if len(dci_rps) > 0 and len(dci_vns) > 0:
+            return False, (
+                400, "Provide either routing_policy_refs or "
+                     "virtual_network_refs. Both has been provided for "
+                     "intra_fabric type data_center_interconnect")
+        if len(dci_rps) == 0 and len(dci_vns) == 0:
+            return False, (
+                400, "Provide either routing_policy_refs or "
+                     "virtual_network_refs. None of them provided for "
+                     "intra_fabric type data_center_interconnect")
+        # validate src lr and related properties
+        ok, lrsrc = cls.dbe_read(
+            db_conn, 'logical_router', src_lr_uuid,
+            obj_fields=['physical_router_refs',
+                        'logical_router_gateway_external',
+                        'display_name'])
+        if not ok:
+            return False, lrsrc
+        srclr_public = lrsrc.get('logical_router_gateway_external', False)
+        srclr_master = True if lrsrc.get('display_name', '') == 'master-LR'\
+            else False
+        srclr_prs = []
+        for pr_ref in lrsrc.get('physical_router_refs') or []:
+            pr_uuid = pr_ref.get('uuid')
+            if pr_uuid:
+                srclr_prs.append(pr_uuid)
+        # validate all lr uuids in DB and their pr against dst lr pr list
+        for lr_uuid, prlist in lr_to_pr.items():
+            ok, dstlr = cls.dbe_read(
+                db_conn, 'logical_router', lr_uuid,
+                obj_fields=['physical_router_refs',
+                            'logical_router_gateway_external',
+                            'display_name'])
+            if not ok:
+                return False, dstlr
+            dstlr_public = dstlr.get('logical_router_gateway_external', False)
+            dstlr_master = True if dstlr.get('display_name', '') == \
+                'master-LR' else False
+            if srclr_master is True and dstlr_public is True:
+                return False, (
+                    400, "master-LR as source logical router, destination LR "
+                         "%s must not have logical_router_gateway_external"
+                         " set to be True." % dstlr.get('display_name', ''))
+            if srclr_public is True and dstlr_master is True:
+                return False, (
+                    400, "source logical router %s has "
+                         "logical_router_gateway_external True, Destination "
+                         "LR %s must not be a master-LR" %
+                    (lrsrc.get('display_name', src_lr_uuid),
+                     dstlr.get('display_name', lr_uuid)))
+            dstlr_pr_list = []
+            for pr_ref in dstlr.get('physical_router_refs') or []:
+                pr_uuid = pr_ref.get('uuid')
+                if pr_uuid:
+                    dstlr_pr_list.append(pr_uuid)
+            dstlr_pr_list_set = set(dstlr_pr_list)
+            for pr in prlist:
+                if pr not in dstlr_pr_list_set:
+                    return False, (
+                        400, "physical_router uuid %s does not exist in "
+                             "destination LR %s" %
+                        (pr, dstlr.get('display_name', '')))
+                if srclr_master is True and pr not in srclr_prs:
+                    return False, (
+                        400, "master-LR as source logical router, destination"
+                             " LR %s selected physical router uuid %s must "
+                             "exists in master-LR's physical routers list." %
+                        (dstlr.get('display_name', lr_uuid), pr))
+                if dstlr_master is True and pr not in srclr_prs:
+                    return False, (
+                        400, "destination logical router as a master-LR, its "
+                             "selected physical router uuid %s must exist in "
+                             "source LR %s physical router list." %
+                        (pr, lrsrc.get('display_name', src_lr_uuid)))
+        return True, ''
 
     @classmethod
     def _make_sure_lrs_belongs_to_different_fabrics(cls, db_conn, dci):
@@ -22,7 +197,6 @@ class DataCenterInterconnectServer(ResourceMixin, DataCenterInterconnect):
             lr_uuid = lr_ref.get('uuid')
             if lr_uuid:
                 lr_list.append(lr_uuid)
-
         if not lr_list:
             return True, ''
 
@@ -31,24 +205,32 @@ class DataCenterInterconnectServer(ResourceMixin, DataCenterInterconnect):
                 db_conn, 'logical_router', lr_uuid, obj_fields=[
                     'physical_router_refs',
                     'data_center_interconnect_back_refs'])
-
             if not ok:
                 return False, read_result
-
             lr = read_result
+            # use only dci back ref with dci marked as interfabric
+            lr_dcireflist = []
+            for dciref in lr.get('data_center_interconnect_back_refs') or []:
+                ok, dci = cls.dbe_read(
+                    db_conn, 'data_center_interconnect', dciref.get('uuid'),
+                    obj_fields=['data_center_interconnect_type'])
+                if ok and dci:
+                    dcitype = dci.get('data_center_interconnect_type')
+                    if not dcitype or dcitype == 'inter_fabric':
+                        lr_dcireflist.append(dciref)
             # check there are no more than one DCI back ref for this LR
             # it is acceptable that dci object can associate with a lr,
             # but lr not associated with any PRs yet
             # make sure LR update should check for this association
-            if len(lr.get('data_center_interconnect_back_refs') or []) > 1:
-                msg = ("Logical router can not associate with  more than one "
-                       "DCI: %s" % lr_uuid)
+            if len(lr_dcireflist) > 1:
+                msg = ("Logical router can not associate with more than one "
+                       "inter_fabric type DCI: %s" % lr_uuid)
                 return False, (400, msg)
-            if len(lr.get('data_center_interconnect_back_refs') or []) == 1:
-                dci_ref = lr.get('data_center_interconnect_back_refs')[0]
+            if len(lr.lr_dcireflist) == 1:
+                dci_ref = lr_dcireflist[0]
                 if dci.get('fq_name') != dci_ref.get('to'):
                     msg = ("Logical router can not associate with more than "
-                           "one DCI: %s" % lr_uuid)
+                           "one inter_fabric type DCI: %s" % lr_uuid)
                     return False, (400, msg)
             init_fab = None
             for pr_ref in read_result.get('physical_router_refs') or []:
@@ -78,12 +260,65 @@ class DataCenterInterconnectServer(ResourceMixin, DataCenterInterconnect):
         return True, ''
 
     @classmethod
+    def pre_dbe_create(cls, tenant_name, obj_dict, db_conn):
+        # make sure referenced LRs belongs to different fabrics
+        return cls._validate_dci_lrs_fabrics_based_on_dcitype(
+            db_conn, obj_dict)
+
+    @classmethod
     def pre_dbe_update(cls, id, fq_name, obj_dict, db_conn, **kwargs):
-        ok, read_result = cls.dbe_read(db_conn, 'data_center_interconnect',
-                                       id, obj_fields=['logical_router_refs'])
+        ok, read_result = cls.dbe_read(
+            db_conn, 'data_center_interconnect', id,
+            obj_fields=['logical_router_refs',
+                        'data_center_interconnect_type'
+                        ])
         if not ok:
             return ok, read_result
-
-        # make sure referenced LRs belongs to different fabrics
-        return cls._make_sure_lrs_belongs_to_different_fabrics(
-            db_conn, read_result)
+        # changes to DCI type not allowed
+        old_dci_type = read_result.get('data_center_interconnect_type')
+        new_dci_type = obj_dict.get('data_center_interconnect_type')
+        intrafabric = False
+        if old_dci_type and old_dci_type == 'intra_fabric':
+            intrafabric = True
+        if old_dci_type and new_dci_type:
+            if old_dci_type != new_dci_type:
+                return False, (
+                    403, "Cannot change data_center_interconnect_type. Please"
+                         " specify data_center_interconnect_type as '%s'" %
+                    old_dci_type)
+        # for intra_fabric, changes to source LR (first LR) not allowed
+        if intrafabric is True and obj_dict.get('logical_router_refs'):
+            def _get_source_lr(lr_refs, check_single_lr):
+                source_lr = None
+                for lr in lr_refs or []:
+                    if cls._is_this_src_lr(lr):
+                        if check_single_lr is False:
+                            source_lr = lr.get('uuid')
+                            return True, source_lr
+                        if source_lr is None:
+                            source_lr = lr.get('uuid')
+                        elif source_lr != lr.get('uuid'):
+                            return False, source_lr
+                if source_lr is None:
+                    return False, source_lr
+                return True, source_lr
+            ok, new_src_lr = _get_source_lr(
+                obj_dict.get('logical_router_refs'), True)
+            if new_src_lr is None:
+                return False, (
+                    403, "No Source LR specified in "
+                         "logical_router_refs for intra_fabric type "
+                         "data_center_interconnect.")
+            if not ok:
+                return False, (
+                    403, "More than one Source LR not allowed for "
+                         "intra_fabric type data_center_interconnect.")
+            _, old_src_lr = _get_source_lr(
+                read_result.get('logical_router_refs'), False)
+            if old_src_lr is not None and new_src_lr != old_src_lr:
+                return False, (
+                    403, "Cannot change Source LR for intra_fabric type"
+                         "data_center_interconnect.")
+        # make sure referenced LRs belongs to fabrics based on dcitype
+        return cls._validate_dci_lrs_fabrics_based_on_dcitype(
+            db_conn, obj_dict)
--- src/config/api-server/vnc_cfg_api_server/resources/routing_policy.py
+++ src/config/api-server/vnc_cfg_api_server/resources/routing_policy.py
@@ -3,6 +3,7 @@
 #
 
 from builtins import str
+import re
 
 from cfgm_common.exceptions import VncError
 from vnc_api.gen.resource_common import RoutingPolicy
@@ -11,13 +12,134 @@ from vnc_cfg_api_server.resources._resource_base import ResourceMixin
 
 
 class RoutingPolicyServer(ResourceMixin, RoutingPolicy):
-    @classmethod
-    def pre_dbe_create(cls, tenant_name, obj_dict, db_conn):
-        asn_list = None
+
+    @staticmethod
+    def _check_routefilter_range_string(rfvalue):
+        res = None
+        if rfvalue:
+            res = re.match(
+                r'^\/([1-9]|[1-2]\d|3[0-2])-\/([1-9]|[1-2]\d|3[0-2])$',
+                rfvalue)
+        if not res:
+            msg = ("route-filter value %s invalid. prefix-length-range "
+                   "should be in format '/minlength-/maxlength'" % rfvalue)
+            return False, (400, msg)
+        return True, ''
+
+    @staticmethod
+    def _check_aspath_string(aspath, aspath_type):
+        if aspath:
+            res = re.match(r'^\d+( \d+)*$', aspath)
+            if not res:
+                msg = ("%s has invalid value %s. valid format is "
+                       "AS number(s) seperated by spaces'" %
+                       (aspath_type, aspath))
+                return False, (400, msg)
+        return True, ''
+
+    @staticmethod
+    def _check_network_device_routing_policy_term(cls, term, term_type,
+                                                  db_conn):
+        """Validate network-device type terms.
+
+        """
+        if not term or not term_type or term_type != 'network-device':
+            return True, ''
+        match_condition = term.get('term_match_condition')
+        if match_condition:
+            protocol = match_condition.get('protocol')
+            if protocol:
+                allowed_protocol = set(['bgp', 'static', 'aggregate', 'pim',
+                                       'direct', 'evpn', 'ospf', 'ospf3'])
+                for p in protocol:
+                    if p not in allowed_protocol:
+                        msg = ('%s protocol value is not valid value for '
+                               'network-device term' % p)
+                        return False, (400, msg)
+            nlri_route_type = match_condition.get('nlri_route_type')
+            for nlri in nlri_route_type or []:
+                if nlri < 1 or nlri > 10:
+                    msg = ('%s nlri-route-type is not valid value, '
+                           'value should be in range of 1 to 10' %
+                           nlri)
+                    return False, (400, msg)
+            prefix_list = match_condition.get('prefix_list')
+            for prefix in prefix_list or []:
+                irt_list = prefix.get('interface_route_table_uuid')
+                for irt in irt_list or []:
+                    ok, read_result = cls.dbe_read(
+                        db_conn, 'interface_route_table', irt)
+                    if not ok:
+                        return ok, read_result
+            route_filter_type = match_condition.get('route_filter')
+            if route_filter_type:
+                for route_filter in route_filter_type.get(
+                        'route_filter_properties') or []:
+                    if route_filter.get('route') is None:
+                        msg = ('route_filter must have non-empty route value '
+                               'for network-device term')
+                        return False, (400, msg)
+                    route_type = route_filter.get('route_type')
+                    if route_type is None:
+                        msg = ('route_filter must have valid route_type '
+                               'for network-device term')
+                        return False, (400, msg)
+                    route_tvalue = route_filter.get('route_type_value')
+                    if route_type == 'prefix-length-range':
+                        ok, result = cls._check_routefilter_range_string(
+                            route_tvalue)
+                        if not ok:
+                            return ok, result
+                    elif route_type == 'through' or route_type == 'upto':
+                        res = None
+                        if route_tvalue:
+                            if route_type == 'upto':
+                                res = re.match(
+                                    r'^\/([1-9]|[1-2]\d|3[0-2])$',
+                                    route_tvalue)
+                            else:
+                                if len(route_tvalue) > 0:
+                                    res = True
+                        if not res:
+                            if route_type == 'upto':
+                                msg = ("route-filter type value '%s' is"
+                                       " invalid. '%s' should be in "
+                                       "format '/number'" % (route_tvalue,
+                                                             route_type))
+                            else:
+                                msg = ("route-filter type value for "
+                                       "'%s' should be non-empty "
+                                       "string." % route_type)
+                            return False, (400, msg)
+        action_list = term.get('term_action_list')
+        if action_list:
+            aspath_e = action_list.get('as_path_expand')
+            ok, result = cls._check_aspath_string(aspath_e, 'as_path_expand')
+            if not ok:
+                return ok, result
+            aspath_e = action_list.get('as_path_prepend')
+            ok, result = cls._check_aspath_string(aspath_e, 'as_path_prepend')
+            if not ok:
+                return ok, result
+
+        return True, ''
+
+    @staticmethod
+    def _validate_term_and_get_asnlist(cls, obj_dict, db_conn):
+        asn_list = []
         rp_entries = obj_dict.get('routing_policy_entries')
         if rp_entries:
-            term = rp_entries.get('term')[0]
-            if term:
+            term_type = rp_entries.get('term_type')
+            termlist = rp_entries.get('term') or []
+            if term_type != 'network-device':
+                if len(termlist) > 1:
+                    termlist = [termlist[0]]
+            for term in termlist:
+                # validate network-device type term,
+                ok, result = cls._check_network_device_routing_policy_term(
+                    cls, term, term_type, db_conn)
+                if not ok:
+                    return False, asn_list, result
                 action_list = term.get('term_action_list')
                 if action_list:
                     action = action_list.get('update')
@@ -26,7 +148,21 @@ class RoutingPolicyServer(ResourceMixin, RoutingPolicy):
                         if as_path:
                             expand = as_path.get('expand')
                             if expand:
-                                asn_list = expand.get('asn_list')
+                                tasn_list = expand.get('asn_list')
+                                if tasn_list:
+                                    asn_list.extend(tasn_list)
+        return True, asn_list, ''
+
+    @classmethod
+    def pre_dbe_create(cls, tenant_name, obj_dict, db_conn):
+        asn_list = None
+        try:
+            ok, asn_list, result = cls._validate_term_and_get_asnlist(
+                cls, obj_dict, db_conn)
+            if not ok:
+                return False, result
+        except Exception as e:
+            return False, (400, str(e))
 
         try:
             global_asn = cls.server.global_autonomous_system
@@ -36,4 +172,31 @@ class RoutingPolicyServer(ResourceMixin, RoutingPolicy):
         if asn_list and global_asn in asn_list:
             msg = ("ASN can't be same as global system config asn")
             return False, (400, msg)
+        return True, ""
+
+    @classmethod
+    def pre_dbe_update(cls, id, fq_name, obj_dict, db_conn, **kwargs):
+        ok, read_result = cls.dbe_read(db_conn, 'routing_policy', id,
+                                       obj_fields=['routing_policy_entries'])
+        if not ok:
+            return ok, read_result
+        # do not allow change in term type
+        old_rpe = read_result.get('routing_policy_entries')
+        if old_rpe:
+            old_term_type = old_rpe.get('term_type')
+            new_rpe = obj_dict.get('routing_policy_entries')
+            if new_rpe and old_term_type:
+                new_term_type = new_rpe.get('term_type')
+                if not new_term_type or new_term_type != old_term_type:
+                    return False, (
+                        403, "Cannot change term_type of routing policy! "
+                             "Please specify '%s' as a term_type in "
+                             "routing_policy_entries." % old_term_type)
+        try:
+            ok, _, result = cls._validate_term_and_get_asnlist(
+                cls, obj_dict, db_conn)
+            if not ok:
+                return False, result
+        except Exception as e:
+            return False, (400, str(e))
         return True, ""
