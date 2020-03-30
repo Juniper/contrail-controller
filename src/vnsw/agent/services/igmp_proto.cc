60649fd900477f0b826784fe37d0b182710eb7d9
Nagendra E S <esnagendra@juniper.net>
2020-03-30 08:59:58 +0530

    Check for IPAM NULL.
    The check for IPAM equal to NULL is needed at least because of the one
    scenario seen during testing.
    
    1. Launch 2 VN
    2. Launch one vm in net1
    3. Verify vm in contrail components
    4. Change the interface of vm to net2
    
    During above scenario, primary_ip_addr_ of VMI takes time to get
    updated and causes IPAM query to fail. Adding a safety check for
    the same.
    
    closes-jira-bug: CEM-13963
    Change-Id: Id8d00091ff64a6afdec5f3d2b8ac6b55b00e0513

--- src/vnsw/agent/services/igmp_proto.cc
+++ src/vnsw/agent/services/igmp_proto.cc
@@ -237,7 +237,13 @@ void IgmpProto::IncrSendStats(const VmInterface *vm_itf, bool tx_done) {
     state = static_cast<IgmpInfo::VnIgmpDBState *>(vn->GetState(
                                 vn->get_table_partition()->parent(),
                                 vn_listener_id()));
+    if (!state) {
+        return;
+    }
     const VnIpam *ipam = vn->GetIpam(vm_itf->primary_ip_addr());
+    if (!ipam) {
+        return;
+    }
     IgmpInfo::VnIgmpDBState::IgmpSubnetStateMap::const_iterator it =
                             state->igmp_state_map_.find(ipam->default_gw);
     if (it == state->igmp_state_map_.end()) {
@@ -267,6 +273,9 @@ const bool IgmpProto::GetItfStats(const VnEntry *vn, IpAddress gateway,
     IgmpInfo::VnIgmpDBState *state = NULL;
     state = static_cast<IgmpInfo::VnIgmpDBState *>(vn->GetState(
                             vn->get_table_partition()->parent(), vn_listener_id_));
+    if (!state) {
+        return false;
+    }
 
     IgmpInfo::VnIgmpDBState::IgmpSubnetStateMap::const_iterator it =
                             state->igmp_state_map_.find(ipam->default_gw);
@@ -283,8 +292,12 @@ const bool IgmpProto::GetItfStats(const VnEntry *vn, IpAddress gateway,
 
 void IgmpProto::ClearItfStats(const VnEntry *vn, IpAddress gateway) {
 
-    const VnIpam *ipam = vn->GetIpam(gateway);
     if (!vn) {
+        return;
+    }
+
+    const VnIpam *ipam = vn->GetIpam(gateway);
+    if (!ipam) {
         return;
     }
 
