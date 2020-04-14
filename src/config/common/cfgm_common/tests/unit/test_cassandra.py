6756045441e557fb158002376df2d86c6ccdb2f3
Sahid Orentino Ferdjaoui <sferdjaoui@juniper.net>
2020-04-14 21:46:13 +0200

    config/cassandra: use composition to separate API definitions with methods
    In this commit we are using composition to separate API definition
    with the common methods used by drivers.
    
    In complement we introduce unittest framework and implement a first
    common keyspace's method
    
    Signed-off-by: Sahid Orentino Ferdjaoui <sferdjaoui@juniper.net>
    Change-Id: I2b7bb0b817d4d069dedd908821101a498c9bbb31

---
+++ src/config/common/cfgm_common/tests/unit/test_cassandra.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (c) 2020 Juniper Networks, Inc. All rights reserved.
+#
+
+import unittest
+
+from cfgm_common.datastore import api as datastore_api
+
+
+class FakeDriver(datastore_api.CassandraDriver):
+    def get_cf_batch(self, keyspace_name, cf_name):
+        pass
+
+    def get_range(self, keyspace_name, cf_name):
+        pass
+
+    def multiget(self, keyspace_name, cf_name, keys, columns=None, start='',
+                 finish='', timestamp=False, num_columns=None):
+        pass
+
+    def get(self, keyspace_name, cf_name, key, columns=None, start='',
+            finish=''):
+        pass
+
+    def get_one_col(self, keyspace_name, cf_name, key, column):
+        pass
+
+    def insert(self, key, columns, keyspace_name=None, cf_name=None,
+               batch=None):
+        pass
+
+    def remove(self, key, columns=None, keyspace_name=None, cf_name=None,
+               batch=None):
+        pass
+
+
+class TestOptions(unittest.TestCase):
+
+    def test_keyspace_wipe(self):
+        drv = FakeDriver([])
+        self.assertEqual(
+            "my_keyspace", drv.keyspace("my_keyspace"))
+
+        drv = FakeDriver([], db_prefix='a_prefix')
+        self.assertEqual(
+            "a_prefix_my_keyspace", drv.keyspace("my_keyspace"))
