c394270813e9640b926d017573ae6c6dc8efd372
Sahid Orentino Ferdjaoui <sferdjaoui@juniper.net>
2020-04-20 12:47:01 +0200

    config/cassandra: move log_reponse_time and _generate_url to api
    Signed-off-by: Sahid Orentino Ferdjaoui <sferdjaoui@juniper.net>
    Change-Id: I54cfa87b0777683a25e9a168ccd28bc9d0a7b209

--- src/config/common/cfgm_common/datastore/drivers/cassandra_thrift.py
+++ src/config/common/cfgm_common/datastore/drivers/cassandra_thrift.py
@@ -59,11 +59,6 @@ class CassandraDriverThrift(datastore_api.CassandraDriver):
     def __init__(self, server_list, **options):
         super(CassandraDriverThrift, self).__init__(server_list, **options)
 
-        self.log_response_time = self.options.log_response_time
-
-        # if no generate_url is specified, use a dummy function that always
-        # returns an empty string
-        self._generate_url = self.options.generate_url or (lambda x, y: '')
         self._cf_dict = {}
         if ((datastore_api.UUID_KEYSPACE_NAME not in self.options.ro_keyspaces) and
             (datastore_api.UUID_KEYSPACE_NAME not in self.options.rw_keyspaces)):
