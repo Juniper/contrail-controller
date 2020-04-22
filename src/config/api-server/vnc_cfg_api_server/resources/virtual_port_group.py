70461eafa719e8ab5f9bba57792b256643a880ee
Nagendra Maynattamai <npchandran@juniper.net>
2020-04-22 03:40:43 -0700

    xInstead of retrieving VN and VLAN-ID info from VMI add annotations to VPG when processing each VMI closes-jira-bug: CEM-14078 closes-jira-bug: CEM-15355
    1. When processig a VMI for a VPG, retrieve VN and VLAN-ID
       and add them as an annotation to VPG object
    2. These annotations are then used to compare incoming
       VN/VLAN-ID of the VMI
    3. Employ Zookeeper lock at both VPG and Fabric level
       to ensure data consistency
    4. Add annotations during dbe_resync so upgrade cases
       are handled
    5. Fabric level validations of VN:VLAN uniqueness can be
       skipped if disable_vlan_vn_uniqueness_check is True
    
    Depends-On: I6a6520c03592fb6c4bbc4c2e51e78892d2830405
    Change-Id: I015d3663315b5d60482d55fa7a76dec4a9505acc

--- src/config/api-server/vnc_cfg_api_server/resources/virtual_port_group.py
+++ src/config/api-server/vnc_cfg_api_server/resources/virtual_port_group.py
@@ -120,18 +120,16 @@ class VirtualPortGroupServer(ResourceMixin, VirtualPortGroup):
     def pre_dbe_update(cls, id, fq_name, obj_dict, db_conn, **kwargs):
         # Handling both deletion and addition of interfaces here
 
-        # compute the already existing physical interface refs for the
-        # vpg object
-
-        ok, result = db_conn.dbe_read(
-            obj_type='virtual_port_group',
-            obj_id=obj_dict['uuid'],
-            obj_fields=['physical_interface_refs'])
-
-        if not ok:
-            return ok, (400, result)
-
         if obj_dict.get('physical_interface_refs'):
+            # compute the already existing physical interface refs for the
+            # vpg object
+            ok, result = db_conn.dbe_read(
+                obj_type='virtual_port_group',
+                obj_id=obj_dict['uuid'],
+                obj_fields=['physical_interface_refs'])
+            if not ok:
+                return ok, (400, result)
+
             ok, res = cls.update_physical_intf_type(obj_dict, result)
             if not ok:
                 return ok, res
