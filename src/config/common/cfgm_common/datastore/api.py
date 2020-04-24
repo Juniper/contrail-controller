6e5e1a9f37e23d29308cec20f5d53a38d17f7ff3
Sahid Orentino Ferdjaoui <sferdjaoui@juniper.net>
2020-04-24 13:20:49 +0200

    config/cassandra: collecting profiling statistics of Cassandra calls
    In our effort to build a driver that supports Python3 we also want to
    improve the current performances of our Cassandra model and
    implementation.
    
    In this commit, we add support of profiling Cassandra calls from the
    driver perspectives.
    
    Using `CONTRAIL_PROFILE_CASSANDRA=1` before starting the service will
    enable the feature. It is to note that collecting calls statistics is
    CPU time-consuming as IO consuming. We should not use this feature in
    production or during a reasonable amount of time.
    
    The statistics collected will be stored in files. The results may be
    read using the `pstats` modules or with software like kcachegrind to
    help finding bottlenecks.
    
    CONTRAIL_PROFILE_CASSANDRA=1 python -m unittest vnc_cfg_api_server.tests.test_vnc_cfg_api_server.TestVncCfgApiServer
    
    Signed-off-by: Sahid Orentino Ferdjaoui <sferdjaoui@juniper.net>
    Change-Id: Iab0231f6cb22404e7cb5a37dfbdc061d9b0885fb

--- src/config/common/cfgm_common/datastore/api.py
+++ src/config/common/cfgm_common/datastore/api.py
@@ -1,6 +1,9 @@
 import abc
 import collections
 import copy
+import cProfile
+import functools
+import os
 import six
 
 from sandesh_common.vns import constants as vns_constants
@@ -69,12 +72,49 @@ OptionsType = collections.namedtuple(
     'Options', OptionsDefault.keys())
 
 
+# Defines base class to trace drivers calls to Cassandra.  The results
+# will be stored in files `profile.cassandra.<function>.trace`. They
+# can be read using `pstats` or more evoluated tools like kcachegrind
+# to find bottlenecks.
+
+class Trace(object):
+    def __init__(self):
+        self._trace_track = {}
+        self._trace_enabled = bool(int(
+            # When defined and equal to 1, profiling the executions.
+            os.getenv('CONTRAIL_PROFILE_CASSANDRA', 0)))
+
+    @staticmethod
+    def trace(f):
+        @functools.wraps(f)
+        def wrapped(self, *args, **kwargs):
+            if self._trace_enabled:
+                if f.__name__ not in self._trace_track:
+                    self._trace_track[f.__name__] = cProfile.Profile()
+
+                p = self._trace_track[f.__name__]
+                p.enable()
+                r = f(self, *args, **kwargs)
+                p.disable()
+
+                # TODO((sahid): We dump the information after each call. a bit
+                # expensive, a better way could be found later.
+                p.dump_stats("profile.cassandra.{}.trace".format(
+                    f.__name__))
+                return r
+            else:
+                return f(self, *args, **kwargs)
+        return wrapped
+
+
 # Defines API that drivers should implement.
 
 @six.add_metaclass(abc.ABCMeta)
-class API(object):
+class API(Trace):
 
     def __init__(self, server_list, **options):
+        super(API, self).__init__()
+
         self.options = copy.deepcopy(OptionsDefault)
         self.options.update(
             # This to filter inputs that are None, in that case we
@@ -97,6 +137,7 @@ class API(object):
     def _Get_CF_Batch(self, cf_name, keyspace_name=None):
         pass
 
+    @Trace.trace
     def get_cf_batch(self, cf_name, keyspace_name=None):
         """Get batch object bind to a column family used in insert/remove"""
         return self._Get_CF_Batch(cf_name=cf_name, keyspace_name=keyspace_name)
@@ -105,6 +146,7 @@ class API(object):
     def _Get_Range(self, cf_name, columns=None, column_count=100000):
         pass
 
+    @Trace.trace
     def get_range(self, cf_name, columns=None, column_count=100000):
         """List all column family rows"""
         return self._Get_Range(
@@ -115,6 +157,7 @@ class API(object):
                   timestamp=False, num_columns=None):
         pass
 
+    @Trace.trace
     def multiget(self, cf_name, keys, columns=None, start='', finish='',
                  timestamp=False, num_columns=None):
         """List multiple rows on a column family"""
@@ -127,6 +170,7 @@ class API(object):
              finish=''):
         pass
 
+    @Trace.trace
     def get(self, cf_name, key, columns=None, start='', finish=''):
         """Fetch one row in a column family"""
         return self._Get(
@@ -136,6 +180,7 @@ class API(object):
     def _XGet(self, cf_name, key, columns=None, start='', finish=''):
         pass
 
+    @Trace.trace
     def xget(self, cf_name, key, columns=None, start='', finish=''):
         """Like get but creates a generator that pages over the columns automatically."""
         return self._XGet(
@@ -145,6 +190,7 @@ class API(object):
     def _Get_Count(self, cf_name, key, start='', finish='', keyspace_name=None):
         pass
 
+    @Trace.trace
     def get_count(self, cf_name, key, start='', finish='', keyspace_name=None):
         """Count rows in a column family"""
         return self._Get_Count(
@@ -155,6 +201,7 @@ class API(object):
     def _Get_One_Col(self, cf_name, key, column):
         pass
 
+    @Trace.trace
     def get_one_col(self, cf_name, key, column):
         """Fetch one column of a row in a column family"""
         return self._Get_One_Col(cf_name=cf_name, key=key, column=column)
@@ -164,6 +211,7 @@ class API(object):
                 batch=None, column_family=None):
         pass
 
+    @Trace.trace
     def insert(self, key, columns, keyspace_name=None, cf_name=None,
                batch=None, column_family=None):
         """Insert columns with value in a row in a column family"""
@@ -175,6 +223,7 @@ class API(object):
                 batch=None, column_family=None):
         pass
 
+    @Trace.trace
     def remove(self, key, columns=None, keyspace_name=None, cf_name=None,
                batch=None, column_family=None):
         """Remove a specified row or a set of columns within the row"""
