3c31a643d6d9fd9ee60352f477cbd55e0078931a
akshayam <akshayam@juniper.net>
2020-04-27 16:45:24 +0000

    [API] VN (routed/tenant) can not belong to multiple LR's
    Except the case when the vn is a shared VN, a VN cannot belong
    to more than one LR in the same fabric at anytime. Implementing
    this check on api-server based on approach for performance optimization.
    
    Modified code for factoring in fabric uuid based LR check.
    
    Modified code for factoring in the following cases:
    1. Fabric update / delete as part of LR update
    2. VN update / delete as part of LR update
    3. Finding fabric info based on PR if not directly available
    4. Modified UTs based on this new check.
    
    Change-Id: Iacfea1fe0ac05ec7740e0924fb46f21b2b1558a3
    Depends-On: I688c09a02f794b0a35bad1497170a95e4aee69b4
    closes-jira-bug: CEM-12445
    target-release: R2008

--- src/config/fabric-ansible/ansible-playbooks/filter_plugins/fabric.py
+++ src/config/fabric-ansible/ansible-playbooks/filter_plugins/fabric.py
@@ -685,6 +685,12 @@ class FilterModule(object):
             fabric_info.get('node_profiles')
         )
 
+        # create default logical router to be used to attach routed
+        # virtual-networks which are part of master routing table on
+        # leaf/spine devices
+        self._add_default_logical_router(
+            vnc_api, fabric_info.get('fabric_fq_name'))
+
         # add intent-map object
         self._add_intent_maps(
             vnc_api,
@@ -1044,6 +1050,42 @@ class FilterModule(object):
         vnc_api.fabric_update(fabric_obj)
         _task_done()
     # end _add_node_profiles
+
+    @staticmethod
+    def _add_default_logical_router(vnc_api,
+                                    fabric_fq_name):
+        """Add default logical router
+
+        :param vnc_api: <vnc_api.VncApi>
+        :return: <vnc_api.gen.resource_client.LogicalRouter>
+        """
+        lr_fq_name = [
+            'default-domain',
+            'default-project',
+            fabric_fq_name[-1] + '-master-LR']
+
+        # read default project, master LR belongs to default project
+        def_project = vnc_api.project_read(['default-domain', 'default-project'])
+        _task_log("creating logical router network, parent %s"
+                  % (def_project.get_uuid()))
+
+        master_lr = LogicalRouter(
+            name=lr_fq_name[-1],
+            fq_name=lr_fq_name,
+            logical_router_gateway_external=False,
+            logical_router_type='vxlan-routing',
+            parent_obj=def_project)
+        try:
+            vnc_api.logical_router_create(master_lr)
+        except RefsExistError as ex:
+            _task_log(
+                "Logical router already exists or other conflict: %s"
+                % (str(ex)))
+            vnc_api.logical_router_update(master_lr)
+
+        master_lr = vnc_api.logical_router_read(fq_name=lr_fq_name)
+        _task_done()
+        return master_lr
 
     @staticmethod
     def _add_intent_maps(vnc_api, fabric_obj):
