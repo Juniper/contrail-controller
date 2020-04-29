e038799646022d2b258b4f382919b6757a9dee89
akshayam <akshayam@juniper.net>
2020-04-29 20:53:21 +0000

    [Fabric] CEM should not allow ERB-UCAST-Gateway role to be configured along with CRB-Gateway/CRB-MCAST-Gateway/DCI-Gateway/DC-Gateway or PNF-Servicechain role
    Because ERB-UCAST-GW uses Anycast-IP && do-not-advertise while all other roles
    CRB-Gateway/CRB-MCAST-Gateway/DCI-Gateway/DC-Gateway or PNF-Servicechain use VGA && no-gateway-community
    
    Also removing UCAST role for edge routed device functional groups
    
    Change-Id: Icb1a09d62c50d2ffe9342f16751f3418c9a7a2be
    target-release: R2005
    closes-jira-bug: CEM-14815

--- src/config/fabric-ansible/ansible-playbooks/filter_plugins/fabric.py
+++ src/config/fabric-ansible/ansible-playbooks/filter_plugins/fabric.py
@@ -2025,7 +2025,8 @@ class FilterModule(object):
                         'physical_router_underlay_managed',
                         'display_name',
                         'annotations',
-                        'physical_router_role'
+                        'physical_router_role',
+                        'routing_bridging_roles'
                     ]
                 )
                 device_roles['device_obj'] = device_obj
@@ -2400,9 +2401,75 @@ class FilterModule(object):
             vnc_api, fabric_fq_name, role_assignments
         )
 
-        self._validate_ucast_mcast_role_exclusive(role_assignments)
+        self._validate_ucast_with_other_roles(role_assignments)
     # end _validate_role_assignments
 
+    @staticmethod
+    def _validate_ucast_with_other_roles(role_assignments):
+        """Validate ucast mcast, crb/dc/dci-gw and pnf roles exclusive.
+
+        This method validates UCAST and a set of other roles are not assigned
+        to the same device
+        """
+        for device_roles in role_assignments:
+            device_obj = device_roles.get('device_obj')
+            rb_roles = list(device_roles.get('routing_bridging_roles', []))
+            if device_obj.get_routing_bridging_roles():
+                assigned_roles = device_obj.get_routing_bridging_roles(). \
+                                     get_rb_roles() or []
+                rb_roles += assigned_roles
+
+            has_ucast_role = has_mcast_role = has_pnf_role = \
+                has_crb_gw_role = has_dc_gw_role = has_dci_gw_role = False
+
+            for r in rb_roles:
+                if 'ucast' in r.lower():
+                    has_ucast_role = True
+                elif 'mcast' in r.lower():
+                    has_mcast_role = True
+                elif 'pnf' in r.lower():
+                    has_pnf_role = True
+                elif 'crb-gateway' in r.lower():
+                    has_crb_gw_role = True
+                elif 'dc-gateway' in r.lower():
+                    has_dc_gw_role = True
+                elif 'dci-gateway' in r.lower():
+                    has_dci_gw_role = True
+
+            err_msg = None
+
+            if has_ucast_role:
+                if has_mcast_role:
+                    err_msg = 'Cannot assign a ERB-UCAST-Gateway role' \
+                              ' and a CRB-MCAST-Gateway' \
+                              ' role to the same device: %s' \
+                              % device_obj.get_fq_name()[-1]
+
+                elif has_pnf_role:
+                    err_msg = 'Cannot assign a ERB-UCAST-Gateway role ' \
+                              'and a PNF-Servicechain role ' \
+                              'to the same device: %s' \
+                              % device_obj.get_fq_name()[-1]
+
+                elif has_crb_gw_role:
+                    err_msg = 'Cannot assign a UCAST role and a ' \
+                              'CRB-Gateway role to the same device: %s' \
+                              % device_obj.get_fq_name()[-1]
+
+                elif has_dc_gw_role:
+                    err_msg = 'Cannot assign a UCAST role and a DC-Gateway' \
+                              ' role to the same device: %s' \
+                              % device_obj.get_fq_name()[-1]
+
+                elif has_dci_gw_role:
+                    err_msg = 'Cannot assign a UCAST role and a ' \
+                              'DCI-Gateway role to the same device: %s' \
+                              % device_obj.get_fq_name()[-1]
+
+            if err_msg:
+                raise ValueError(err_msg)
+
+    # end _validate_ucast_with_other_roles
     @staticmethod
     def _validate_against_supported_roles(role_assignments):
         """Validate against supported roles.
@@ -2505,29 +2572,6 @@ class FilterModule(object):
                 'device and retry the role assignment' % str(ex)
             )
     # end _validate_rr_role_assigned
-
-    def _validate_ucast_mcast_role_exclusive(self, role_assignments):
-        """Validate ucast mcast role exclusive.
-
-        This method validates that both UCAST and MCAST roles are not assigned
-        to the same device
-        """
-        for device_roles in role_assignments:
-            device_obj = device_roles.get('device_obj')
-            rb_roles = list(device_roles.get('routing_bridging_roles', []))
-            if device_obj.get_routing_bridging_roles():
-                assigned_roles = device_obj.get_routing_bridging_roles().\
-                    get_rb_roles() or []
-                rb_roles += assigned_roles
-
-            has_ucast_role = any('ucast' in r.lower() for r in rb_roles)
-            has_mcast_role = any('mcast' in r.lower() for r in rb_roles)
-
-            if has_ucast_role and has_mcast_role:
-                raise ValueError('Cannot assign a UCAST role and a MCAST role '
-                                 'to the same device: %s' %
-                                 device_obj.get_fq_name()[-1])
-    # end _validate_ucast_mcast_role_exclusive
 
     def _validate_fabric_rr_role_assigned(
             self, vnc_api, fabric_fq_name,
