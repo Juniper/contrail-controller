8b4f6556d85a0bb67913bb057f1e829284ad23d9
Andrii Moshurenko <amoshurenko@juniper.net>
2020-04-29 06:40:53 -0700

    Add binaries-script
    address review comments
    
    Change-Id: I6bba55980d7b7f32d514b54aac80c88e434c06fc

---
+++ src/cat/get-binaries.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+FILE=$1
+RELEASE=$2
+
+export FILE
+export RELEASE
+input=$FILE
+if [ -z "${RELEASE}"] 
+then
+    while IFS= read -r line
+    do
+        echo "http://svl-artifactory.juniper.net/artifactory/contrail-static-prod/"$line"/contrail-vrouter-agent.tgz"
+        echo "http://svl-artifactory.juniper.net/artifactory/contrail-static-prod/"$line"/control-node.tgz"
+    done < "$input"
+else
+    echo "http://svl-artifactory.juniper.net/artifactory/contrail-static-prod/"$RELEASE"/contrail-vrouter-agent.tgz"
+    echo "http://svl-artifactory.juniper.net/artifactory/contrail-static-prod/"$RELEASE"/control-node.tgz"
+fi
