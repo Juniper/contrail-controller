e062d99778d2f00289a1934b31c9daea4dd7cc49
Nagendra Prasath <npchandran@juniper.net>
2020-05-13 14:15:14 -0700

    THIS IS A TEST, DO NOT MERGE
    Revert "make cfgm-common compatible with pip 20.1"
    
    This reverts commit 50987b0d187d3debfd3b90a78428efc0cc81de54.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: I46248d6da920f375617184ca55db7f2cbc0bef6b

--- src/config/common/setup.py
+++ src/config/common/setup.py
@@ -2,15 +2,11 @@
 # Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
 #
 
-import re
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
 from setuptools import setup, find_packages
-
-
-def requirements(filename):
-    with open(filename) as f:
-        lines = f.read().splitlines()
-    c = re.compile(r'\s*#.*')
-    return filter(bool, map(lambda y: c.sub('', y).strip(), lines))
 
 
 setup(
@@ -34,8 +30,8 @@ setup(
     ],
     packages=find_packages(),
     package_data={'': ['*.xml']},
-    install_requires=requirements('requirements.txt'),
-    tests_require=requirements('test-requirements.txt'),
+    install_requires=[str(req.req) for req in parse_requirements('requirements.txt', session='hack')],
+    tests_require=[str(req.req) for req in parse_requirements('test-requirements.txt', session='hack')],
     test_suite='cfgm_common.tests',
     keywords='contrail vnc utils',
 )
