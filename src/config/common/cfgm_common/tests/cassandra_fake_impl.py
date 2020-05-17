ef7c62a01ca99900e9b7520430b23a15886e3b2c
Sahid Orentino Ferdjaoui <sferdjaoui@juniper.net>
2020-05-17 16:11:55 +0200

    config/cassandra: impl/use fake cassandra driver/server thrift version
    Change-Id: I06afaeee7cd37cc9203c1532ca739273ee939674

---
+++ src/config/common/cfgm_common/tests/cassandra_fake_impl.py
@@ -0,0 +1,359 @@
+#
+# Copyright (c) 2020 Juniper Networks, Inc. All rights reserved.
+#
+
+import collections
+from datetime import datetime
+from collections import OrderedDict
+import contextlib
+import copy
+import itertools
+import mock
+import six
+import sys
+
+from cfgm_common.exceptions import NoIdError, DatabaseUnavailableError, VncError
+from cfgm_common.datastore import api as datastore_api
+from cfgm_common import jsonutils as json
+
+
+try:
+    # Even if we don't want import pycassa anymore. Only
+    # CassandraDriverThrift should doing that. We have to because some
+    # part of the code like db_manage.py is still refering some
+    # pycassa objects.
+    import pycassa.NotFoundException
+
+    BaseExceptionClass = pycassa.NotFoundException
+except:
+    BaseExceptionClass = Exception
+
+class NotFoundException(BaseExceptionClass):
+    pass
+
+
+def reset():
+    CassandraFakeServer.__keyspaces__ = {}
+
+
+# This is implementating a *kind* of Cassandra server to run tests, in
+# future we should remove that and is real instamce of Cassandra.
+class CassandraFakeServer(object):
+    # The servers share the keyspaces
+    __keyspaces__ = {}
+
+    @classmethod
+    def create_keyspace(cls, name):
+        if name not in cls.__keyspaces__:
+            cls.__keyspaces__[name] = _CassandraFakeServerKeyspace(name)
+        return cls.__keyspaces__[name]
+
+    @classmethod
+    def drop_keyspace(cls, name):
+        if name in cls.__keyspaces__:
+            del cls.__keyspaces__[name]
+
+    @classmethod
+    @contextlib.contextmanager
+    def patch_keyspace(cls, ks_name, ks_val=None):
+        try:
+            orig_ks_val = cls.__keyspaces__[ks_name]
+            orig_existed = True
+        except KeyError:
+            orig_existed = False
+
+        try:
+            cls.create_keyspace(ks_name)
+            cls.__keyspaces__[ks_name].__tables__ = ks_val
+            yield
+        finally:
+            if orig_existed:
+                cls.__keyspaces__[ks_name].__tables__ = orig_ks_val
+            else:
+                cls.drop_keyspace(ks_name)
+
+class _CassandraFakeServerKeyspace(object):
+    def __init__(self, name):
+        self.name = name
+        self.__tables__ = {}
+
+    def create_table(self, name):
+        if name not in self.__tables__:
+            self.__tables__[name] = _CassandraFakeServerTable(
+                self.name, name)
+        return self.__tables__[name]
+
+    def drop_table(self, name):
+        if name in self.__tables__:
+            del self.__tables__[name]
+
+
+class _CassandraFakeServerTable(object):
+    def __init__(self, keyspace, name):
+        self.keyspace = keyspace
+        self.name = name
+
+        self.__rows__ = {}
+
+        # Thrift related
+        ColumnFamilyDef = collections.namedtuple('ColumnFamilyDef', 'keyspace')
+        self._cfdef = ColumnFamilyDef(keyspace)
+
+    def send(self):
+        pass
+
+    def get(self, key, columns=None, column_start=None, column_finish=None,
+            column_count=0, include_timestamp=False, include_ttl=False):
+        if not isinstance(key, six.string_types):
+            raise TypeError("A str or unicode value was expected, but %s "
+                            "was received instead (%s)"
+                            % (key.__class__.__name__, str(key)))
+        if isinstance(key, six.binary_type):
+            key = key.decode('utf-8')
+        if key not in self.__rows__:
+            raise NotFoundException
+        if columns:
+            col_dict = {}
+            for col_name in columns:
+                try:
+                    col_value = self.__rows__[key][col_name][0]
+                except KeyError:
+                    if len(columns) > 1:
+                        continue
+                    else:
+                        raise NotFoundException
+                if include_timestamp or include_ttl:
+                    ret = (col_value,)
+                    if include_timestamp:
+                        col_tstamp = self.__rows__[key][col_name][1]
+                        ret += (col_tstamp,)
+                    if include_ttl:
+                        col_ttl = self.__rows__[key][col_name][2]
+                        ret += (col_ttl,)
+                    col_dict[col_name] = ret
+                else:
+                    col_dict[col_name] = col_value
+        else:
+            col_dict = {}
+            for col_name in list(self.__rows__[key].keys()):
+                if not self._column_within_range(
+                        col_name, column_start, column_finish):
+                    continue
+
+                col_value = self.__rows__[key][col_name][0]
+                if include_timestamp or include_ttl:
+                    ret = (col_value,)
+                    if include_timestamp:
+                        col_tstamp = self.__rows__[key][col_name][1]
+                        ret += (col_tstamp,)
+                    if include_ttl:
+                        col_ttl = self.__rows__[key][col_name][2]
+                        ret += (col_ttl,)
+                    col_dict[col_name] = ret
+                else:
+                    col_dict[col_name] = col_value
+
+        if len(col_dict) == 0:
+            raise NotFoundException
+
+        sorted_col_dict = OrderedDict(
+            (k, col_dict[k]) for k in sorted(col_dict))
+        return sorted_col_dict
+
+    def multiget(
+        self, keys, columns=None, column_start=None, column_finish=None,
+            column_count=0, include_timestamp=False):
+        result = {}
+        for key in keys:
+            try:
+                col_dict = self.get(key, columns, column_start, column_finish,
+                                    column_count, include_timestamp)
+                result[key] = col_dict
+            except NotFoundException:
+                pass
+
+        return result
+
+    def insert(self, key, col_dict, ttl=None):
+        if key not in self.__rows__:
+            self.__rows__[key] = {}
+
+        tstamp = (datetime.utcnow() - datetime(1970, 1, 1)).total_seconds()
+        for col_name in list(col_dict.keys()):
+            self.__rows__[key][col_name] = (col_dict[col_name], tstamp, ttl)
+
+    def remove(self, key, columns=None):
+        try:
+            if columns:
+                # for each entry in col_name delete each that element
+                for col_name in columns:
+                    del self.__rows__[key][col_name]
+            elif columns is None:
+                del self.__rows__[key]
+        except KeyError:
+            # pycassa remove ignores non-existing keys
+            pass
+
+    def get_range(self, *args, **kwargs):
+        columns = kwargs.get('columns', None)
+        column_start = kwargs.get('column_start', None)
+        column_finish = kwargs.get('column_finish', None)
+        column_count = kwargs.get('column_count', 0)
+        include_timestamp = kwargs.get('include_timestamp', False)
+
+        for key in self.__rows__:
+            try:
+                col_dict = self.get(key, columns, column_start, column_finish,
+                                    column_count, include_timestamp)
+                yield (key, col_dict)
+            except NotFoundException:
+                pass
+
+    def xget(self, key, column_start=None, column_finish=None,
+             column_count=0, include_timestamp=False, include_ttl=False):
+        try:
+            col_dict = self.get(key,
+                                column_start=column_start,
+                                column_finish=column_finish,
+                                column_count=column_count,
+                                include_timestamp=include_timestamp,
+                                include_ttl=include_ttl)
+        except NotFoundException:
+            col_dict = {}
+
+        for k, v in list(col_dict.items()):
+            yield (k, v)
+
+    def get_count(self, key, column_start=None, column_finish=None):
+        col_names = []
+        if key in self.__rows__:
+            col_names = list(self.__rows__[key].keys())
+
+        counter = 0
+        for col_name in col_names:
+            if self._column_within_range(
+                    col_name, column_start, column_finish):
+                counter += 1
+
+        return counter
+
+    def _column_within_range(self, column_name, column_start, column_finish):
+        if type(column_start) is tuple:
+            for i in range(len(column_start), len(column_name)):
+                column_start = column_start + (column_name[i],)
+        if type(column_finish) is tuple:
+            for i in range(len(column_finish), len(column_name)):
+                column_finish = column_finish + (column_name[i],)
+
+        if column_start and column_name < column_start:
+            return False
+        if column_finish and column_name > column_finish:
+            return False
+
+        return True
+
+    @contextlib.contextmanager
+    def patch_cf(self, new_contents=None):
+        orig_contents = CassandraFakeServer.__keyspaces__[self.keyspace].__tables__[self.name].__rows__
+        try:
+            CassandraFakeServer.__keyspaces__[self.keyspace].__tables__[self.name].__rows__ = new_contents
+            yield
+        finally:
+            CassandraFakeServer.__keyspaces__[self.keyspace].__tables__[self.name].__rows__ = orig_contents
+
+    @contextlib.contextmanager
+    def patch_row(self, key, new_columns=None):
+        ctx = PatchContext(self)
+        try:
+            ctx.patch([('row', (key, new_columns))])
+            yield
+        finally:
+            ctx.unpatch()
+
+    @contextlib.contextmanager
+    def patch_column(self, key, col_name, col_val=None):
+        ctx = PatchContext(self)
+        try:
+            ctx.patch([('column', (key, col_name, col_val))])
+            yield
+        finally:
+            ctx.unpatch()
+
+    @contextlib.contextmanager
+    def patches(self, patch_list):
+        ctx = PatchContext(self)
+        try:
+            ctx.patch(patch_list)
+            yield
+        finally:
+            ctx.unpatch()
+
+
+class PatchContext(object):
+    def __init__(self, cf):
+        self.cf = cf
+        self.patches = []  # stack of patches
+
+    def patch(self, patch_list):
+        cf = self.cf
+        for patch_type, patch_info in patch_list:
+            patched = {}
+            if patch_type == 'row':
+                patched['type'] = 'row'
+                row_key, new_columns = patch_info
+                patched['row_key'] = row_key
+                if row_key in cf.__rows__:
+                    patched['row_existed'] = True
+                    patched['orig_cols'] = copy.deepcopy(cf.__rows__[row_key])
+                    if new_columns is None:
+                        # simulates absence of key in cf
+                        del cf.__rows__[row_key]
+                    else:
+                        cf.__rows__[row_key] = new_columns
+                else:  # row didn't exist, create one
+                    patched['row_existed'] = False
+                    cf.insert(row_key, new_columns)
+            elif patch_type == 'column':
+                patched['type'] = 'column'
+                row_key, col_name, col_val = patch_info
+                patched['row_key'] = row_key
+                patched['col_name'] = col_name
+                if col_name in cf.__rows__[row_key]:
+                    patched['col_existed'] = True
+                    patched['orig_col_val'] = copy.deepcopy(
+                        cf.__rows__[row_key][col_name])
+                    if col_val is None:
+                        # simulates absence of col
+                        del cf.__rows__[row_key][col_name]
+                    else:
+                        cf.insert(row_key, {col_name: col_val})
+                else:  # column didn't exist, create one
+                    patched['col_existed'] = False
+                    cf.insert(row_key, {col_name: col_val})
+            else:
+                raise Exception(
+                    "Unknown patch type %s in patching" % patch_type)
+
+            self.patches.append(patched)
+
+    def unpatch(self):
+        cf = self.cf
+        for patched in reversed(self.patches):
+            patch_type = patched['type']
+            row_key = patched['row_key']
+            if patch_type == 'row':
+                if patched['row_existed']:
+                    cf.__rows__[row_key] = patched['orig_cols']
+                else:
+                    del cf.__rows__[row_key]
+            elif patch_type == 'column':
+                col_name = patched['col_name']
+                if patched['col_existed']:
+                    cf.__rows__[row_key][col_name] = patched['orig_col_val']
+                else:
+                    del cf.__rows__[row_key][col_name]
+            else:
+                raise Exception(
+                    "Unknown patch type %s in un-patching" % patch_type)
+
+
