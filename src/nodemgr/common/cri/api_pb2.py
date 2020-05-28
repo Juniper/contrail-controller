b222d7c295902e81ea7d5b606da67b74ea03da45
root <shrike_@mail.ru>
2020-05-28 16:48:52 -0400

    workarounds python3 imports from a relative path
    use python3-specific import format to load modules from their
    relative paths.
    
    Change-Id: I67003e52345afab54352b2db307d1b12798bcda5

--- src/nodemgr/common/cri/api_pb2.py
+++ src/nodemgr/common/cri/api_pb2.py
@@ -12,7 +12,7 @@ from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 
 
-import gogo_pb2 as gogo__pb2
+from . import gogo_pb2 as gogo__pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
