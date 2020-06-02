c82dd81041d4a661a41c1890176316ed14206ffb
Andrey Pavlov <andrey-mp@yandex.ru>
2020-06-02 19:17:56 +0300

    install py3 files for kubemanager to pack them later at rpm stage
    partial-jira-bug: CEM-7933
    Change-Id: I10d3c16da5555d72e44eaca8ec314f6ebab4864e

--- src/container/kube-manager/setup.py
+++ src/container/kube-manager/setup.py
@@ -4,13 +4,23 @@
 
 import re
 from setuptools import setup, find_packages
+import sys
+
+PY2 = sys.version_info.major < 3
 
 
 def requirements(filename):
     with open(filename) as f:
         lines = f.read().splitlines()
     c = re.compile(r'\s*#.*')
-    return list(filter(bool, map(lambda y: c.sub('', y).strip(), lines)))
+    result = list(filter(bool, map(lambda y: c.sub('', y).strip(), lines)))
+    if PY2 and 'gevent<1.5.0' in result:
+        # current UT doesn't work with gevent==1.4.0 for python2
+        # and gevent==1.1.2 can't be used with python3
+        # Apply this workaround as markers are not supported in requirements.txt
+        result.remove('gevent<1.5.0')
+        result.append('gevent==1.1.2')
+    return result
 
 
 setup(
@@ -35,7 +45,7 @@ setup(
     entry_points = {
         # Please update sandesh/common/vns.sandesh on process name change
         'console_scripts' : [
-            'contrail-kube-manager = kube_manager.kube_manager:main',
+            'contrail-kube-manager{} = kube_manager.kube_manager:main'.format("" if PY2 else "-py3"),
         ],
     },
 )
