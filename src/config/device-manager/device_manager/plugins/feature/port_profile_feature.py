5c890199577f0af95c36ef972836467e8cc3461c
akshayam <akshayam@juniper.net>
2020-06-05 20:01:20 +0000

    [DM][Fabric] Adding port profile, physical and logical interface attributes
    This story tracks adding few more properties to port profile and
    interface objects.
    
    1. Refactoring existing storm control code to port profiles for
       including other port profile properties.
    
    Change-Id: I69dae1cfe2d5f3837736b8a55919f4645f88ffb4
    closes-jira-bug: CEM-13247
    target-release: R2008

--- src/config/device-manager/device_manager/plugins/feature/port_profile_feature.py
+++ src/config/device-manager/device_manager/plugins/feature/port_profile_feature.py
@@ -21,29 +21,30 @@ from .feature_base import FeatureBase
 import gevent # noqa
 
 
-class StormControlFeature(FeatureBase):
+class PortProfileFeature(FeatureBase):
 
     @classmethod
     def feature_name(cls):
-        return 'storm-control'
+        return 'port-profile'
     # end feature_name
 
     def __init__(self, logger, physical_router, configs):
         self.pi_list = None
-        self.sc_map = None
-        super(StormControlFeature, self).__init__(logger, physical_router,
+        self.pp_map = None
+        super(PortProfileFeature, self).__init__(logger, physical_router,
                                                   configs)
     # end __init__
 
-    def _add_to_sc_map(self, sc_name, sc_obj):
-        if sc_name in self.sc_map:
+    def _add_to_pp_map(self, pp_name, pp_obj):
+        if pp_name in self.pp_map:
             return
         else:
-            self.sc_map[sc_name] = sc_obj
-    # end _add_to_sc_map
+            self.pp_map[pp_name] = pp_obj
+    # end _add_to_pp_map
 
-    def _build_storm_control_interface_config(self):
+    def _build_port_profile_interface_config(self):
         pr = self._physical_router
+
         for vpg_uuid in pr.virtual_port_groups or []:
             vpg_obj = VirtualPortGroupDM.get(vpg_uuid)
             if not vpg_obj:
@@ -52,28 +53,46 @@ class StormControlFeature(FeatureBase):
             for pp_uuid in pp_list or []:
                 pp = PortProfileDM.get(pp_uuid)
                 if pp:
-                    sc_uuid = pp.storm_control_profile
-                    scp = StormControlProfileDM.get(sc_uuid)
-                    if scp:
-                        self._build_storm_control_config(scp)
-                        sc_name = ((scp.fq_name[-1].replace(' ', '-')) + "-" +
-                                   scp.fq_name[-2])
-                        for pi_uuid in vpg_obj.physical_interfaces or []:
-                            if pi_uuid not in pr.physical_interfaces:
-                                continue
-                            ae_id = vpg_obj.pi_ae_map.get(pi_uuid)
-                            if ae_id is not None:
-                                ae_intf_name = 'ae' + str(ae_id)
-                                pi = PhysicalInterface(name=ae_intf_name)
-                            else:
-                                pi_obj = PhysicalInterfaceDM.get(pi_uuid)
-                                pi = PhysicalInterface(name=pi_obj.name)
-                            if pi not in self.pi_list:
-                                self.pi_list.add(pi)
-                                pi.set_storm_control_profile(sc_name)
+                    pp_name = pp.fq_name[-1] + "-" + pp.fq_name[-2]
+                    pp_obj = PortProfile(name=pp_name)
+
+                    for pi_uuid in vpg_obj.physical_interfaces or []:
+                        if pi_uuid not in pr.physical_interfaces:
+                            continue
+                        ae_id = vpg_obj.pi_ae_map.get(pi_uuid)
+                        if ae_id is not None:
+                            ae_intf_name = 'ae' + str(ae_id)
+                            pi = PhysicalInterface(name=ae_intf_name)
+                        else:
+                            pi_obj = PhysicalInterfaceDM.get(pi_uuid)
+                            pi = PhysicalInterface(name=pi_obj.name)
+                        if pi not in self.pi_list:
+                            self.pi_list.add(pi)
+                            pi.set_port_profile(pp_name)
+
+                    # storm control feature is only supported with enterprise
+                    # style configs. User can on-board PR with ERB role in a SP
+                    # style fabric, which supports only enterprise style config
+                    if (not self._is_enterprise_style(pr) and
+                       not pr.is_erb_only()):
+                        self._build_storm_control_interface_config(
+                            pp, pp_obj)
+
+                    # TODO
+                    # self._build_port_profile_parameters_config(pp, pp_obj)
+                    #
+                    # self._build_qos_config(pp, pp_obj)
+
+                    self._add_to_pp_map(pp_name, pp_obj)
+
+    def _build_storm_control_interface_config(self, pp, pp_obj):
+        sc_uuid = pp.storm_control_profile
+        scp = StormControlProfileDM.get(sc_uuid)
+        if scp:
+            self._build_storm_control_config(scp, pp_obj)
     # end _build_storm_control_interface_config
 
-    def _build_storm_control_config(self, scp):
+    def _build_storm_control_config(self, scp, pp_obj):
         sc_name = (scp.fq_name[-1].replace(' ', '-')) + "-" + scp.fq_name[-2]
         sc = StormControl(name=sc_name)
         params = scp.storm_control_params
@@ -93,30 +112,23 @@ class StormControlFeature(FeatureBase):
             sc.set_traffic_type(traffic_type)
             sc.set_recovery_timeout(params.get('recovery_timeout'))
             sc.set_actions(params.get('storm_control_actions'))
-        self._add_to_sc_map(sc_name, sc)
+        pp_obj.set_storm_control_profile(sc)
     # end _build_storm_control_config
 
     def feature_config(self, **kwargs):
         self.pi_list = set()
-        self.sc_map = OrderedDict()
+        self.pp_map = OrderedDict()
         feature_config = Feature(name=self.feature_name())
 
-        # storm control feature is only supported with enterprise
-        # style configs. User can on-board PR with ERB role in a SP
-        # style fabric, which supports only enterprise style config
-        if (not self._is_enterprise_style(self._physical_router) and
-           not self._physical_router.is_erb_only()):
-            return feature_config
-
-        self._build_storm_control_interface_config()
+        self._build_port_profile_interface_config()
 
         for pi in set(self.pi_list):
             feature_config.add_physical_interfaces(pi)
 
-        for sc_name in self.sc_map:
-            feature_config.add_storm_control(self.sc_map[sc_name])
+        for pp_name in self.pp_map:
+            feature_config.add_port_profile(self.pp_map[pp_name])
 
         return feature_config
     # end feature_config
 
-# end StormControlFeature
+# end PortProfileFeature
