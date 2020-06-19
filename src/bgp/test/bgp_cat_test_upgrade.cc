8037b3052298a29f2648ba3f1b8ab05ccbfd342e
Tapas Mallick <tmallick@juniper.net>
2020-06-19 04:56:47 -0700

    Use previously saved binaries from artifactory to run ZIU N-X to N upgrade test-cases
    Update-1 : Not running the test-cases if svl-artifactory is not reachable
    
    Update-2 : Running the ZIU test-cases as part of separate test-package only when there is a change in schema
    
    Update-3 : Adding changes for config file generation in CAT
    
    Update-4 : Added changes for support of add-virtual-port and made structural changes to code
    
    Update-5 : Added changes for Integration with config-tests
    
    Update-6 : Added changes for creating a separate binary for CAT-Control-node here since it was causing merge-conflict.
    
    closes-jira-bug: CEM-16243
    Change-Id: I8bd6890a1daaa61cd78d25a14e135b6667c76c2f

---
+++ src/bgp/test/bgp_cat_test_upgrade.cc
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2020 Juniper Networks, Inc. All rights reserved.
+ */
+
+#include <boost/filesystem.hpp>
+#include <unistd.h>
+
+#include "base/test/task_test_util.h"
+#include "bgp/bgp_log.h"
+#include "testing/gunit.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+class BgpCatTestUpgrade : public ::testing::Test {
+protected:
+    void run(const string &path);
+};
+
+
+void BgpCatTestUpgrade::run(const string &path) {
+    pid_t child = 0;
+    char *const argv[4] = {
+        (char *) "go",
+        (char *) "test",
+        (char *) "-cover",
+        NULL
+    };
+
+    if (!(child = vfork())) {
+        chdir(path.c_str());
+        execv("../../../../third_party/go/bin/go", argv);
+    }
+    int status = 0;
+    waitpid(child, &status, 0);
+    if (WEXITSTATUS(status))
+        cout << path << " failed" << endl;
+    EXPECT_EQ(0, WEXITSTATUS(status));
+}
+
+TEST_F(BgpCatTestUpgrade, BasicGoLang) {
+    run("controller/src/cat/config");
+    run("controller/src/cat/controlnode");
+    run("controller/src/cat/agent");
+    run("controller/src/cat/test_upgrade");
+
+}
+
+static void SetUp() {
+    system("./src/contrail-api-client/generateds/generateDS.py -f -o "
+           "controller/src/cat/types -g golang-api "
+           "src/contrail-api-client/schema/ietf-l3vpn-schema.xsd");
+    system("./src/contrail-api-client/generateds/generateDS.py -f -o "
+           "controller/src/cat/types -g golang-api "
+           "src/contrail-api-client/schema/bgp_schema.xsd");
+    system("./src/contrail-api-client/generateds/generateDS.py -f -o "
+           "controller/src/cat/types -g golang-api "
+           "src/contrail-api-client/schema/vnc_cfg.xsd");
+
+    boost::filesystem::path cwd(boost::filesystem::current_path());
+    setenv("GOPATH", cwd.string().c_str(), true);
+}
+
+static void TearDown() {
+    system("rm -rf controller/src/cat/types");
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    SetUp();
+    int result = RUN_ALL_TESTS();
+    TearDown();
+    return result;
+}
