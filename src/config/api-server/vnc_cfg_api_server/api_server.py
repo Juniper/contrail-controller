48462c14528479cfd5f279d843d0446f5b6a6594
Sayali Mane <smane@juniper.net>
2020-07-24 17:26:35 +0000

    WIP: Re-design of Service-Provider Validation
    Closes-Jira-Bug: CEM-17565
    
    Change-Id: I1407dd898c956af621d647141bd191ad5e512f5d
    Signed-off-by: Sayali Mane <smane@juniper.net>

--- src/config/api-server/vnc_cfg_api_server/api_server.py
+++ src/config/api-server/vnc_cfg_api_server/api_server.py
@@ -1072,6 +1072,7 @@ class VncApiServer(object):
 
             get_context().set_state('PRE_DBE_CREATE')
             # type-specific hook
+            # Save old data in case of VLAN_update, pass on to post_dbe_create/post_dbe_update
             (ok, result) = r_class.pre_dbe_create(
                     tenant_name, obj_dict, db_conn)
             if not ok:
@@ -4365,11 +4366,16 @@ class VncApiServer(object):
             _req_obj_dict = {}
             if req_obj_dict:
                 _req_obj_dict = req_obj_dict
-            (ok, result) = r_class.pre_dbe_update(
+            (ok, update_result, zk_update_kwargs) = r_class.pre_dbe_update(
                 obj_uuid, obj_fq_name, _req_obj_dict, self._db_conn,
                 prop_collection_updates=req_prop_coll_updates)
             if not ok:
-                return (ok, result)
+                if update_result:
+                    return (ok, update_result)
+                else:
+                    return (ok, zk_update_kwargs)
+            zk_update_kwargs = zk_update_kwargs or {}
+
             attr_to_publish = None
             if isinstance(result, dict):
                 attr_to_publish = result
@@ -4435,7 +4441,7 @@ class VncApiServer(object):
             # type-specific hook
             (ok, result) = r_class.post_dbe_update(
                 obj_uuid, obj_fq_name, _req_obj_dict, self._db_conn,
-                prop_collection_updates=req_prop_coll_updates)
+                prop_collection_updates=req_prop_coll_updates, **zk_update_kwargs)
             if not ok:
                 return (ok, result)
 
