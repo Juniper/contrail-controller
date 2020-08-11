4fb494ffd84b7cbe9250e7c7bdecbb99d397e206
sangarshp <sangarshp@juniper.net>
2020-08-11 14:21:12 +0530

    Add separate route walker for disconnected peer paths
    root cause:
    same route walker is used for notifying peer paths and for
    deleting peer paths when peer goes down.
    it is possible that route walker object in BGP peer object
    gets overwritten with peernotify route walk request after
    enqueeueing request for delete peer route walk request.
    
    this issue is seen when config controll is down which triggers
    both route walk requests. refer CEM-14391 for more details on
    root cause.
    
    fix:
    add separate route walker for deleting peer paths of inactive
    controller.
    
    Closes-Jira-Bug: CEM-14391
    Change-Id: Ib69aae95fca15e87f9ca8652645e5a3ebecab6ec

--- src/vnsw/agent/oper/peer.h
+++ src/vnsw/agent/oper/peer.h
@@ -178,10 +178,13 @@ public:
     void DeleteStale();
     void AllocDeleteStaleWalker();
     void ReleaseDeleteStaleWalker();
+    void AllocDeletePeerWalker();
+    void ReleaseDeletePeerWalker();
     void StopDeleteStale();
 
     ControllerRouteWalker *route_walker() const;
     ControllerRouteWalker *delete_stale_walker() const;
+    ControllerRouteWalker *delete_peer_walker() const;
 
     //Helper routines to get export state for vrf and route
     DBState *GetVrfExportState(DBTablePartBase *partition,
@@ -196,6 +199,7 @@ public:
     uint64_t ChannelSequenceNumber() const;
     void set_route_walker_cb(WalkDoneCb cb);
     void set_delete_stale_walker_cb(WalkDoneCb cb);
+    void set_delete_peer_walker_cb(WalkDoneCb cb);
 
 private:
     AgentXmppChannel *channel_;
@@ -203,9 +207,11 @@ private:
     DBTableBase::ListenerId id_;
     uint32_t setup_time_;
     AgentRouteWalkerPtr route_walker_;
+    AgentRouteWalkerPtr delete_peer_walker_;
     AgentRouteWalkerPtr delete_stale_walker_;
     WalkDoneCb route_walker_cb_;
     WalkDoneCb delete_stale_walker_cb_;
+    WalkDoneCb delete_peer_walker_cb_;
     DISALLOW_COPY_AND_ASSIGN(BgpPeer);
 };
 
