f6190f99c426623e01a6c23e4fe6c1f70ba567c6
pandita-juniper <pandita@juniper.net>
2020-09-01 03:22:34 +0000

    [DM] Initial changes for 5 stage fabric
    closes-jira-bug: CEM-18680
    target-release: R2011
    Change-Id: I8000685657bb1d34062412db4025799764006886

---
+++ src/config/device-manager/device_manager/plugins/ansible/overlay/overlay_conf.py
@@ -0,0 +1,45 @@
+<<<<<<< HEAD   (4b0f63 [Migration/Juniper/contrail-controller] Remove content and a)
+=======
+#
+# Copyright (c) 2014 Juniper Networks, Inc. All rights reserved.
+#
+
+"""This file implements abstract config generation."""
+
+from abstract_device_api.abstract_device_xsd import *
+
+from .ansible_role_common import AnsibleRoleCommon
+
+
+class OverlayConf(AnsibleRoleCommon):
+    """Public class for Overlay config."""
+
+    _roles = ['leaf', 'spine', 'superspine']
+
+    def __init__(self, logger, params={}):
+        """Init routine for overlay config."""
+        super(OverlayConf, self).__init__(logger, params)
+    # end __init__
+
+    @classmethod
+    def register(cls):
+        """."""
+        qconf = {
+              "roles": cls._roles,
+              "class": cls
+            }
+        return super(OverlayConf, cls).register(qconf)
+    # end register
+
+    def push_conf(self, feature_configs=None, is_delete=False):
+        """."""
+        if not self.physical_router:
+            return 0
+        if is_delete:
+            return self.send_conf(is_delete=True)
+        self.set_common_config()
+        return self.send_conf(feature_configs=feature_configs)
+    # end push_conf
+
+# end LeafConf
+>>>>>>> CHANGE (38d481 [DM] Initial changes for 5 stage fabric)
