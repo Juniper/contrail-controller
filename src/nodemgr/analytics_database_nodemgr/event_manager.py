a5b34038a80189cb8059e12164a27990b685b7d7
Manish Sahu <msahu@juniper.net>
2020-09-08 00:01:36 +0530

    multiple repair processes on the Cassandra DB in Contrail AnalyticsDB nodes
    Currently we are running repair on all nodes at the same time which is causing
    Cassandra db going to problematic state.
    Hence we need to run repair in all nodes at interval gap of 2hr.
    
    Closes-Jira-Bug: CEM-15348
    Change-Id: I81b161c54a8fc57ff6fa1b1eb0f62b16041d5041

--- src/nodemgr/analytics_database_nodemgr/event_manager.py
+++ src/nodemgr/analytics_database_nodemgr/event_manager.py
@@ -28,6 +28,7 @@ class AnalyticsDatabaseEventManager(EventManager):
             config.db_port, config.db_jmx_port, config.db_use_ssl,
             config.db_user, config.db_password,
             self.process_info_manager, hostname=config.hostname)
+        self.node_idx = None
 
     def get_failbits_nodespecific_desc(self, fail_status_bits):
         return self.cassandra_mgr.get_failbits_nodespecific_desc(
@@ -36,6 +37,13 @@ class AnalyticsDatabaseEventManager(EventManager):
     def do_periodic_events(self):
         self.cassandra_mgr.database_periodic(self)
         # Perform nodetool repair every cassandra_repair_interval hours
-        if self.tick_count % (60 * self.cassandra_repair_interval) == 0:
-            self.cassandra_mgr.repair(self)
+        if self.node_idx is None:
+            self.node_idx = self.cassandra_mgr.get_cassandra_node_idx(self)
+        if self.node_idx >= 0:
+            # We need to make sure that we run don't repair in all the nodes
+            # at the same time because in that case there is a possibility that
+            # all nodes try to repair same sstable and the repair may hang.
+            # Hence we need to run repair in all the nodes at interval gap of 4hr
+            if self.tick_count % (60 * self.cassandra_repair_interval) == self.node_idx * 60 * 4:
+                self.cassandra_mgr.repair(self)
         super(AnalyticsDatabaseEventManager, self).do_periodic_events()
