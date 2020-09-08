bd0c5e2ee76f3690add64e02eb35c5a2e0c2b469
Andrey Pavlov <andrey-mp@yandex.ru>
2020-09-08 16:55:04 +0300

    [Migration/Juniper/contrail-controller] Remove content and add readme about migration.
    Automated change
    
    Depends-On: I008189b83d0ef4da9cc7e51c40a2c98855c3e0a5
    Depends-On: I4d09da9cf444d93b42cf9eac6c4a251e0e46b36b
    Depends-On: I6ff31ea928b5ba212130daff13df7d62cb7a59fa
    Depends-On: I82b51bb4b63d6359f1480fc4004e5a0559dc1dee
    Depends-On: Ib2abf3e994e94dbd43ac29640f0550fdf321161b
    Depends-On: Ica55ef9864c247cf486d5efcf9bab9eeeaf939e4
    Change-Id: Ia7c754a699055dd6590986bbec100f27569cb602

--- lib/gunit/gunit_nowarn.h
+++
@@ -1,6 +0,0 @@
-#ifndef __TESTING_GUNIT_H__
-#define __TESTING_GUNIT_H__
-
-#include <gmock/gmock.h>
-
-#endif
--- src/route/table.cc
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2013 Juniper Networks, Inc. All rights reserved.
- */
-
-#include "route/table.h"
-
-#include "db/db.h"
-#include "db/db_entry.h"
