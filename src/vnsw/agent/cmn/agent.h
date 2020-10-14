4fbe61ca9ff4626a5a77482b94edf189ac401b9e
ShashankAgarwal <shashanka@juniper.net>
2020-10-14 15:09:33 +0530

    Backporting of tcp keepalive for ports 8087 and 9091 from master to 3.2
    closes Jira: CEM-16349
    Change-Id: I4b0081929942c8e77a57e9af5b59d9913b6d2384

--- src/vnsw/agent/cmn/agent.h
+++ src/vnsw/agent/cmn/agent.h
@@ -300,6 +300,17 @@ public:
     static const uint32_t kFlowUpdateTokens = 16;
     static const uint8_t kInvalidQueueId = 255;
     static const uint8_t kMinAapPrefixLen = 24;
+
+    static const bool kDefaultMetadataTcpKAEn = true;
+    static const int kDefaultMetadataTcpKAIdleTime = 7200;
+    static const int kDefaultMetadataTcpKAProbes = 9;
+    static const int kDefaultMetadataTcpKAInterval = 175;
+
+    static const bool kDefaultPortIpcTcpKAEn = true;
+    static const int kDefaultPortIpcTcpKAIdleTime = 7200;
+    static const int kDefaultPortIpcTcpKAProbes = 9;
+    static const int kDefaultPortIpcTcpKAInterval = 175;
+
     enum ForwardingMode {
         NONE,
         L2_L3,
